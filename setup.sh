df4fc05b180ff49655d360bdc6752f896a881faf
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-04-11 11:02:31 +0200

    Install eframe dependencies in setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,11 @@ elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 elif [ -x "$(command -v apk)" ];     then sudo apk add --no-cache $packagesNeeded
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
 
+# Extra eframe dependencies needed on Linux and Fedora Rawhide:
+if [ -x "$(command -v apt-get)" ]; then sudo apt-get install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev
+elif [ -x "$(command -v dnf)" ];   then sudo dnf install clang clang-devel clang-tools-extra speech-dispatcher-devel libxkbcommon-devel pkg-config openssl-devel libxcb-devel
+fi
+
 set -x
 
 rustup install 1.60.0
