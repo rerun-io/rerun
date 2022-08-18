b0d7e66fa543202c662f0b8f77e01f3d0eec0db4
Nikolaus West <niko@grapic.co>
2022-08-18 15:13:32 +0200

    Python example: render a dummy color car
    Performance still ok on data generation:
    %timeit depth_images = example.generate_dummy_data(100)
    82.7 ms ± 7.2 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)

--- crates/re_sdk_python/example.py
+++ crates/re_sdk_python/example.py
@@ -15,6 +15,8 @@ import rerun_sdk as rerun
 
 class DummyCar:
     """Class representing a dummy car for generating dummy data to log."""
+    BODY_COLOR = (79, 91, 102)
+    TIRES_COLOR = (0, 0, 0)
 
     def __init__(self, center: Tuple[int, int], size: Tuple[int, int], distance_mm: float):
         self.center = np.array(center)
@@ -22,29 +24,70 @@ class DummyCar:
         self.distance_mm = distance_mm
 
     @property
-    def min(self) -> np.array:
+    def min(self) -> np.ndarray:
         return self.center - self.size / 2
 
     @property
-    def max(self) -> np.array:
+    def max(self) -> np.ndarray:
         return self.center + self.size / 2
 
     def drive_one_step(self):
         self.center[0] += 5
         self.distance_mm -= 1
 
-    def draw_depth(self, depth_image_mm: np.array) -> np.array:
-        cv2.rectangle(depth_image_mm, self.min.astype(int), self.max.astype(int),
+    def draw(self, depth_image_mm: np.ndarray, rgb: np.ndarray):
+        # 1. Draw the tires of the car
+        tire_radius = (self.size[1] * (1./5)).astype(int)
+        tire_center_y = self.max[1] - tire_radius
+        tire_distance_mm = self.distance_mm - 200
+
+        # 1.1 Draw the left tire
+        left_tire_center = np.array(
+            (self.center[0] - 2 * tire_radius, tire_center_y)).astype(int)
+        cv2.circle(depth_image_mm, left_tire_center, tire_radius,
+                   tire_distance_mm, cv2.FILLED)
+        cv2.circle(rgb, left_tire_center, tire_radius,
+                   DummyCar.TIRES_COLOR, cv2.FILLED)
+
+        # 1.2 Draw the right tire
+        right_tire_center = np.array(
+            (self.center[0] + 2 * tire_radius, tire_center_y)).astype(int)
+        cv2.circle(depth_image_mm, right_tire_center, tire_radius,
+                   tire_distance_mm, cv2.FILLED)
+        cv2.circle(rgb, right_tire_center, tire_radius,
+                   DummyCar.TIRES_COLOR, cv2.FILLED)
+
+        # 2. Draw the body
+        body_section_height = self.size[1] * (2./5)
+
+        # 2.1 Draw the top part of the car
+        top_section_distance_mm = self.distance_mm - 100
+        top_section_width = self.size[0] * (3./5)
+        top_min = self.min + \
+            np.array((self.size[1] - top_section_width / 2, 0))
+        top_max = top_min + np.array((top_section_width, body_section_height))
+        cv2.rectangle(depth_image_mm, top_min.astype(int), top_max.astype(int),
+                      top_section_distance_mm, cv2.FILLED)
+        cv2.rectangle(rgb, top_min.astype(int), top_max.astype(int),
+                      DummyCar.BODY_COLOR, cv2.FILLED)
+
+        # 2.2 Draw the middle part of the car
+        middle_min = self.min + np.array((0, body_section_height))
+        middle_max = middle_min + \
+            np.array((self.size[0], body_section_height))
+        cv2.rectangle(depth_image_mm, middle_min.astype(int), middle_max.astype(int),
                       self.distance_mm, cv2.FILLED)
-        return depth_image_mm
+        cv2.rectangle(rgb, middle_min.astype(int), middle_max.astype(int),
+                      DummyCar.BODY_COLOR, cv2.FILLED)
 
 
 @dataclass
 class SampleFrame:
     """Holds data for a single frame of data."""
     frame_idx: int
-    depth_image_mm: np.array
-    car_bbox: Tuple[np.array, np.array]
+    depth_image_mm: np.ndarray
+    rgb_image: np.ndarray
+    car_bbox: Tuple[np.ndarray, np.ndarray]
 
 
 def generate_dummy_data(num_frames: int) -> Sequence[SampleFrame]:
@@ -57,15 +100,27 @@ def generate_dummy_data(num_frames: int) -> Sequence[SampleFrame]:
     _, yv = np.meshgrid(np.arange(0, im_w), np.arange(0, im_h))
 
     # Background image as a simple slanted plane
+    # 1. Depth
     depth_background_mm = 1000.0 / (0.1 + yv/im_h)
+
+    # 2. Color
+    sand_color = (194, 178, 128)
+    intensity = 1.0 / depth_background_mm
+    intensity /= intensity.max()
+    rgb_background = intensity[:, :, np.newaxis] * \
+        np.array(sand_color)[np.newaxis, np.newaxis, :]
+    rgb_background = rgb_background.astype(np.uint8)
 
     # Generate `num_frames` sample data
     car = DummyCar(center=(140, 100), size=(200, 100), distance_mm=4000)
     samples = []  # type: List[SampleFrame]
     for i in range(num_frames):
-        depth_image_mm = car.draw_depth(depth_background_mm.copy())
+        depth_image_mm = depth_background_mm.copy()
+        rgb = rgb_background.copy()
+        car.draw(depth_image_mm=depth_image_mm, rgb=rgb)
         sample = SampleFrame(frame_idx=i,
                              depth_image_mm=depth_image_mm,
+                             rgb_image=rgb,
                              car_bbox=(car.min, car.size))
         samples.append(sample)
         car.drive_one_step()
@@ -82,7 +137,9 @@ def log_dummy_data(args):
         rerun.set_time_sequence("frame_nr", sample.frame_idx)
 
         # The depth image is in millimeters, so we set meter=1000
-        rerun.log_depth_image("depth", sample.depth_image_mmmage, meter=1000)
+        rerun.log_depth_image("depth", sample.depth_image_mm, meter=1000)
+
+        rerun.log_image("rgb", sample.rgb_image)
 
         ((car_x, car_y), (car_w, car_h)) = sample.car_bbox
         rerun.log_bbox("bbox", [car_x, car_y], [car_w, car_h], "A car")
@@ -90,11 +147,6 @@ def log_dummy_data(args):
     if False:
         depth_img = cv2.imread('depth_image.pgm', cv2.IMREAD_UNCHANGED)
         rerun.log_depth_image("depth_image", depth_img, meter=10_000)
-
-    if True:
-        image = cv2.imread('crates/re_viewer/data/logo_dark_mode.png',
-                           cv2.IMREAD_UNCHANGED)
-        rerun.log_image("logo", image, space="rgb")
 
     if True:
         positions = []
