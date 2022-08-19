28f7640e5e3a66a2c8a63e69db54667cb93caf10
Nikolaus West <nikolaus.west@me.com>
2022-08-19 15:31:52 +0200

    Extend Python example with fake rgb and point clouds that matches the depth images. (#40)
    * Python example: break out data generation for simple timing tests
    
    Before making any changes:
    %timeit depth_images = example.generate_dummy_data(100)
    3.39 s ± 128 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
    
    * Python example: refactor and vectorize rendering
    
    Roughly a 100x speedup.
    
    %timeit depth_images = example.generate_dummy_data(100)
    37.2 ms ± 6.53 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
    
    * Python example: render a dummy color car
    
    Performance still ok on data generation:
    %timeit depth_images = example.generate_dummy_data(100)
    82.7 ms ± 7.2 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
    
    * Python example: backproject depth image
    
    %timeit depth_images = example.generate_dummy_data(100)
    256 ms ± 30.1 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
    
    * Python example: Create samples directly before log
    
    * Add name to TODOs to fix python lint error
    
    * Python example: move overview function to the top

--- crates/re_sdk_python/example.py
+++ crates/re_sdk_python/example.py
@@ -1,8 +1,7 @@
-"""
-Shows how to use the rerun SDK.
-"""
+"""Shows how to use the rerun SDK."""
 
-import math
+from dataclasses import dataclass
+from typing import Iterator, Tuple
 
 import argparse
 import cv2
@@ -11,76 +10,165 @@ import numpy as np
 import rerun_sdk as rerun
 
 
-def car_bbox(frame_nr):
-    """returns a bbox for a car"""
-    (x, y) = (40 + frame_nr * 5, 50)
-    (w, h) = (200, 100)
-    return ((x, y), (w, h))
+def log_dummy_data(args):
+    """Log a few frames of generated dummy data to show how the Rerun SDK is used."""
+    NUM_FRAMES = 20
 
-
-def generate_depth_image(frame_nr):
-    """
-    Return a depth image in millimeter units
-    """
-    ((car_x, car_y), (car_w, car_h)) = car_bbox(frame_nr)
-    car_dist = 4000 - frame_nr
-
-    # Generate some fake data:
-    w = 480
-    h = 270
-
-    depth_image = np.zeros([h, w])
-    for y in range(h):
-        for x in range(w):
-            d = 1000.0 / (0.1 + y/h)
-            if car_x <= x <= car_x + car_w and car_y <= y <= car_y + car_h:
-                d = car_dist
-            depth_image[(y, x)] = d
-
-    return depth_image
-
-
-def log(args):
-    NUM_FRAMES = 40
-    depth_images = [generate_depth_image(i) for i in range(NUM_FRAMES)]
-
-    for frame_nr in range(NUM_FRAMES):
+    for sample in generate_dummy_data(num_frames=NUM_FRAMES):
         # This will assign logged objects a "time source" called `frame_nr`.
         # In the viewer you can select how to view objects - by frame_nr or the built-in `log_time`.
-        rerun.set_time_sequence("frame_nr", frame_nr)
+        rerun.set_time_sequence("frame_nr", sample.frame_idx)
 
         # The depth image is in millimeters, so we set meter=1000
-        depth_image = depth_images[frame_nr]
-        rerun.log_depth_image("depth", depth_image, meter=1000)
+        rerun.log_depth_image("depth", sample.depth_image_mm, meter=1000)
 
-        ((car_x, car_y), (car_w, car_h)) = car_bbox(frame_nr)
-        rerun.log_bbox("bbox", [car_x, car_y], [car_w, car_h], "A car")
-
-    if False:
-        depth_img = cv2.imread('depth_image.pgm', cv2.IMREAD_UNCHANGED)
-        rerun.log_depth_image("depth_image", depth_img, meter=10_000)
-
-    if True:
-        image = cv2.imread('crates/re_viewer/data/logo_dark_mode.png',
-                           cv2.IMREAD_UNCHANGED)
-        rerun.log_image("logo", image, space="rgb")
-
-    if True:
-        positions = []
-        for i in range(1000):
-            angle = 6.28 * i / 64
-            r = 1.0
-            x = r * math.cos(angle) + 18.0
-            y = r * math.sin(angle) + 16.0
-            z = i / 64.0
-            positions.append([x, y, z])
-        positions = np.array(positions)
-
-        # Same color for all points in this case, but you can also have a different color for each point:
+        # TODO(nikolausWest): Make setting the color here optional
         rgba = [200, 0, 100, 200]
         colors = np.array([rgba])
+        # TODO(nikolausWest): Figure out why this copy is needed.
+        rerun.log_points("depth3D", sample.point_cloud.copy(), colors)
 
-        rerun.log_points("point3d", positions, colors)
+        rerun.log_image("rgb", sample.rgb_image)
+
+        ((car_x, car_y), (car_w, car_h)) = sample.car_bbox
+        rerun.log_bbox("bbox", [car_x, car_y], [car_w, car_h], "A car")
+
+
+class DummyCar:
+    """Class representing a dummy car for generating dummy data to log."""
+    BODY_COLOR = (79, 91, 102)
+    TIRES_COLOR = (0, 0, 0)
+
+    def __init__(self, center: Tuple[int, int], size: Tuple[int, int], distance_mm: float):
+        self.center = np.array(center)
+        self.size = np.array(size)
+        self.distance_mm = distance_mm
+
+    @property
+    def min(self) -> np.ndarray:
+        return self.center - self.size / 2
+
+    @property
+    def max(self) -> np.ndarray:
+        return self.center + self.size / 2
+
+    def drive_one_step(self):
+        self.center[0] += 5
+        self.distance_mm -= 1
+
+    def draw(self, depth_image_mm: np.ndarray, rgb: np.ndarray):
+        # 1. Draw the tires of the car
+        tire_radius = (self.size[1] * (1./5)).astype(int)
+        tire_center_y = self.max[1] - tire_radius
+        tire_distance_mm = self.distance_mm + 200
+
+        # 1.1 Draw the left tire
+        left_tire_center = np.array(
+            (self.center[0] - 2 * tire_radius, tire_center_y)).astype(int)
+        cv2.circle(depth_image_mm, left_tire_center, tire_radius,
+                   tire_distance_mm, cv2.FILLED)
+        cv2.circle(rgb, left_tire_center, tire_radius,
+                   DummyCar.TIRES_COLOR, cv2.FILLED)
+
+        # 1.2 Draw the right tire
+        right_tire_center = np.array(
+            (self.center[0] + 2 * tire_radius, tire_center_y)).astype(int)
+        cv2.circle(depth_image_mm, right_tire_center, tire_radius,
+                   tire_distance_mm, cv2.FILLED)
+        cv2.circle(rgb, right_tire_center, tire_radius,
+                   DummyCar.TIRES_COLOR, cv2.FILLED)
+
+        # 2. Draw the body
+        body_section_height = self.size[1] * (2./5)
+
+        # 2.1 Draw the top part of the car
+        top_section_distance_mm = self.distance_mm + 100
+        top_section_width = self.size[0] * (3./5)
+        top_min = self.min + \
+            np.array((self.size[1] - top_section_width / 2, 0))
+        top_max = top_min + np.array((top_section_width, body_section_height))
+        cv2.rectangle(depth_image_mm, top_min.astype(int), top_max.astype(int),
+                      top_section_distance_mm, cv2.FILLED)
+        cv2.rectangle(rgb, top_min.astype(int), top_max.astype(int),
+                      DummyCar.BODY_COLOR, cv2.FILLED)
+
+        # 2.2 Draw the middle part of the car
+        middle_min = self.min + np.array((0, body_section_height))
+        middle_max = middle_min + \
+            np.array((self.size[0], body_section_height))
+        cv2.rectangle(depth_image_mm, middle_min.astype(int), middle_max.astype(int),
+                      self.distance_mm, cv2.FILLED)
+        cv2.rectangle(rgb, middle_min.astype(int), middle_max.astype(int),
+                      DummyCar.BODY_COLOR, cv2.FILLED)
+
+
+def back_project(depth_image_mm: np.ndarray, u_coords: np.ndarray, v_coords: np.ndarray) -> np.ndarray:
+    """Given a depth image, and simple assumptions on camera parameters, generate a matching point cloud.
+        - `depth_image_mm`: Depth image expressed in millimeters
+        - `u_coords`: array of same shape as `depth_image_mm` containing the u coordinate of each pixel
+        - `v_coords`: array of same shape as `depth_image_mm` containing the v coordinate of each pixel
+
+    """
+    h, w = depth_image_mm.shape
+    u_center = w / 2
+    v_center = h / 2
+    focal_length = (h * w) ** 0.5
+
+    z = depth_image_mm.reshape(-1) / 1000.
+    x = (u_coords.reshape(-1).astype(float) - u_center) * z / focal_length
+    y = (v_coords.reshape(-1).astype(float) - v_center) * z / focal_length
+
+    back_projected = np.vstack((x, y, z)).T
+    return back_projected
+
+
+@ dataclass
+class SampleFrame:
+    """Holds data for a single frame of data."""
+    frame_idx: int
+    depth_image_mm: np.ndarray
+    point_cloud: np.ndarray
+    rgb_image: np.ndarray
+    car_bbox: Tuple[np.ndarray, np.ndarray]
+
+
+def generate_dummy_data(num_frames: int) -> Iterator[SampleFrame]:
+    """This function generates dummy data to log."""
+    # Generate some fake data
+    im_w = 480
+    im_h = 270
+
+    # Pre-generate image containing the x and y coordinates per pixel
+    u_coords, v_coords = np.meshgrid(np.arange(0, im_w), np.arange(0, im_h))
+
+    # Background image as a simple slanted plane
+    # 1. Depth
+    depth_background_mm = 1000.0 * 1. / (0.01 + 0.4*v_coords/im_h)
+
+    # 2. Color
+    sand_color = (194, 178, 128)
+    intensity = 1.0 / depth_background_mm
+    intensity /= intensity.max()
+    rgb_background = intensity[:, :, np.newaxis] * \
+        np.array(sand_color)[np.newaxis, np.newaxis, :]
+    rgb_background = rgb_background.astype(np.uint8)
+
+    # Generate `num_frames` sample data
+    car = DummyCar(center=(140, 100), size=(200, 100), distance_mm=4000)
+    for i in range(num_frames):
+        depth_image_mm = depth_background_mm.copy()
+        rgb = rgb_background.copy()
+        car.draw(depth_image_mm=depth_image_mm, rgb=rgb)
+        point_cloud = back_project(
+            depth_image_mm=depth_image_mm, u_coords=u_coords, v_coords=v_coords)
+        sample = SampleFrame(frame_idx=i,
+                             depth_image_mm=depth_image_mm,
+                             point_cloud=point_cloud,
+                             rgb_image=rgb,
+                             car_bbox=(car.min, car.size))
+
+        yield sample
+        car.drive_one_step()
 
 
 if __name__ == '__main__':
@@ -100,7 +188,7 @@ if __name__ == '__main__':
 
     print(rerun.info())
 
-    log(args)
+    log_dummy_data(args)
 
     if not args.connect:
         rerun.show()
