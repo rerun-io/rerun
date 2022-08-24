6fdce6bd2be407fe70ee09610cbc78155a4b8c0e
Nikolaus West <niko@grapic.co>
2022-08-24 14:15:59 +0200

    Check.sh runs pytest and mypi (#53)

--- check.sh
+++ check.sh
@@ -43,6 +43,9 @@ cargo deny check
 
 source crates/re_sdk_python/setup_env.sh
 maturin build -m crates/re_sdk_python/Cargo.toml
+pip3 install ./target/wheels/*.whl
+mypy crates/re_sdk_python
+pytest crates/re_sdk_python
 
 # -----------------
 
