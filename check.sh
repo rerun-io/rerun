55236138a394c96c09990716c6f87dca0dbab648
Nikolaus West <niko@grapic.co>
2022-08-24 12:42:07 +0200

    Fix missing pip install of python sdk

--- check.sh
+++ check.sh
@@ -43,6 +43,7 @@ cargo deny check
 
 source crates/re_sdk_python/setup_env.sh
 maturin build -m crates/re_sdk_python/Cargo.toml
+pip3 install ./target/wheels/*.whl
 mypy crates/re_sdk_python
 pytest crates/re_sdk_python
 
