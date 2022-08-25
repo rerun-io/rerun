rerun-io/rerun

commit 037245ed895a2f924d69619aca6d55a15169323f

Refactor how vectors of data is stored (#61)
Previously we had special Data types for a vector of points and line segments.

This PR simplifies this to instead use the type-erased `DataVec`.

This makes the code simpler, more general, and more dynamic -
but it moves type checking into a later staged.

It's very likely we want to refactor and generalize this further,
for instance to support tensors.

crates/re_data_store/src/objects.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-use re_log_types::{FieldName, MsgId, ObjPath};
+use re_log_types::{DataVec, FieldName, MsgId, ObjPath};
 
 pub use re_log_types::objects::*;
 
@@ -307,22 +307,24 @@ impl<'s> Path3D<'s> {
             ("space", "color", "stroke_width"),
             |obj_path: &ObjPath,
              msg_id: &MsgId,
-             points: &Vec<[f32; 3]>,
+             points: &DataVec,
              space: Option<&ObjPath>,
              color: Option<&[u8; 4]>,
              stroke_width: Option<&f32>| {
-                out.path3d.0.push(Object {
-                    props: ObjectProps {
-                        msg_id,
-                        space,
-                        color: color.copied(),
-                        obj_path,
-                    },
-                    data: Path3D {
-                        points,
-                        stroke_width: stroke_width.copied(),
-                    },
-                });
+                if let Some(points) = as_vec_of_vec3("Path3D::points", points) {
+                    out.path3d.0.push(Object {
+                        props: ObjectProps {
+                            msg_id,
+                            space,
+                            color: color.copied(),
+                            obj_path,
+                        },
+                        data: Path3D {
+                            points,
+                            stroke_width: stroke_width.copied(),
+                        },
+                    });
+                }
             },
         );
     }
@@ -330,7 +332,8 @@ impl<'s> Path3D<'s> {
 
 #[derive(Copy, Clone, Debug)]
 pub struct LineSegments2D<'s> {
-    pub line_segments: &'s Vec<[[f32; 2]; 2]>,
+    /// Connected pair-wise even-odd.
+    pub points: &'s Vec<[f32; 2]>,
     pub stroke_width: Option<f32>,
 }
 
@@ -349,22 +352,24 @@ impl<'s> LineSegments2D<'s> {
             ("space", "color", "stroke_width"),
             |obj_path: &ObjPath,
              msg_id: &MsgId,
-             line_segments: &Vec<[[f32; 2]; 2]>,
+             points: &DataVec,
              space: Option<&ObjPath>,
              color: Option<&[u8; 4]>,
              stroke_width: Option<&f32>| {
-                out.line_segments2d.0.push(Object {
-                    props: ObjectProps {
-                        msg_id,
-                        space,
-                        color: color.copied(),
-                        obj_path,
-                    },
-                    data: LineSegments2D {
-                        line_segments,
-                        stroke_width: stroke_width.copied(),
-                    },
-                });
+                if let Some(points) = as_vec_of_vec2("LineSegments2D::points", points) {
+                    out.line_segments2d.0.push(Object {
+                        props: ObjectProps {
+                            msg_id,
+                            space,
+                            color: color.copied(),
+                            obj_path,
+                        },
+                        data: LineSegments2D {
+                            points,
+                            stroke_width: stroke_width.copied(),
+                        },
+                    });
+                }
             },
         );
     }
@@ -372,7 +377,8 @@ impl<'s> LineSegments2D<'s> {
 
 #[derive(Copy, Clone, Debug)]
 pub struct LineSegments3D<'s> {
-    pub line_segments: &'s Vec<[[f32; 3]; 2]>,
+    /// Connected pair-wise even-odd.
+    pub points: &'s Vec<[f32; 3]>,
     pub stroke_width: Option<f32>,
 }
 
@@ -391,22 +397,24 @@ impl<'s> LineSegments3D<'s> {
             ("space", "color", "stroke_width"),
             |obj_path: &ObjPath,
              msg_id: &MsgId,
-             line_segments: &Vec<[[f32; 3]; 2]>,
+             points: &DataVec,
              space: Option<&ObjPath>,
              color: Option<&[u8; 4]>,
              stroke_width: Option<&f32>| {
-                out.line_segments3d.0.push(Object {
-                    props: ObjectProps {
-                        msg_id,
-                        space,
-                        color: color.copied(),
-                        obj_path,
-                    },
-                    data: LineSegments3D {
-                        line_segments,
-                        stroke_width: stroke_width.copied(),
-                    },
-                });
+                if let Some(points) = as_vec_of_vec3("LineSegments3D::points", points) {
+                    out.line_segments3d.0.push(Object {
+                        props: ObjectProps {
+                            msg_id,
+                            space,
+                            color: color.copied(),
+                            obj_path,
+                        },
+                        data: LineSegments3D {
+                            points,
+                            stroke_width: stroke_width.copied(),
+                        },
+                    });
+                }
             },
         );
     }
@@ -719,5 +727,31 @@ impl<'s> SpacePartitioner<'s> {
         partitioned.insert(current_space, current_objects);
         partitioned.retain(|_, objects| !objects.is_empty());
         partitioned
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+fn as_vec_of_vec2<'s>(what: &str, data_vec: &'s DataVec) -> Option<&'s Vec<[f32; 2]>> {
+    if let DataVec::Vec2(vec) = data_vec {
+        Some(vec)
+    } else {
+        log_once::warn_once!(
+            "Expected {what} to be Vec<Vec2>, got Vec<{:?}>",
+            data_vec.element_data_type()
+        );
+        None
+    }
+}
+
+fn as_vec_of_vec3<'s>(what: &str, data_vec: &'s DataVec) -> Option<&'s Vec<[f32; 3]>> {
+    if let DataVec::Vec3(vec) = data_vec {
+        Some(vec)
+    } else {
+        log_once::warn_once!(
+            "Expected {what} to be Vec<Vec3>, got Vec<{:?}>",
+            data_vec.element_data_type()
+        );
+        None
     }
 }

crates/re_sdk_python/src/python_bridge.rs
@@ -302,7 +302,7 @@ fn log_points(
                 let colors: Vec<[u8; 4]> = colors
                     .as_slice()
                     .unwrap()
-                    .chunks(3)
+                    .chunks_exact(3)
                     .into_iter()
                     .map(|chunk| [chunk[0], chunk[1], chunk[2], 255])
                     .collect();
@@ -317,7 +317,7 @@ fn log_points(
                 let colors: Vec<[u8; 4]> = colors
                     .as_slice()
                     .unwrap()
-                    .chunks(4)
+                    .chunks_exact(4)
                     .into_iter()
                     .map(|chunk| [chunk[0], chunk[1], chunk[2], chunk[3]])
                     .collect();
@@ -341,7 +341,7 @@ fn log_points(
             let data: Vec<[f32; 2]> = positions
                 .as_slice()
                 .unwrap()
-                .chunks(2)
+                .chunks_exact(2)
                 .into_iter()
                 .map(|chunk| [chunk[0] as f32, chunk[1] as f32])
                 .collect();
@@ -351,7 +351,7 @@ fn log_points(
             let data: Vec<[f32; 3]> = positions
                 .as_slice()
                 .unwrap()
-                .chunks(3)
+                .chunks_exact(3)
                 .into_iter()
                 .map(|chunk| [chunk[0] as f32, chunk[1] as f32, chunk[2] as f32])
                 .collect();
@@ -406,7 +406,7 @@ fn log_path(
     let positions: Vec<[f32; 3]> = positions
         .as_slice()
         .unwrap()
-        .chunks(3)
+        .chunks_exact(3)
         .into_iter()
         .map(|chunk| [chunk[0], chunk[1], chunk[2]])
         .collect();
@@ -415,7 +415,7 @@ fn log_path(
         msg_id: MsgId::random(),
         time_point: time_point.clone(),
         data_path: DataPath::new(obj_path.clone(), "points".into()),
-        data: LoggedData::Single(Data::Path3D(positions)),
+        data: LoggedData::Single(Data::DataVec(DataVec::Vec3(positions))),
     }));
 
     if let Some(color) = color {
@@ -468,21 +468,21 @@ fn log_line_segments(
             let positions = positions
                 .as_slice()
                 .unwrap()
-                .chunks(4)
+                .chunks_exact(2)
                 .into_iter()
-                .map(|c| [[c[0], c[1]], [c[2], c[3]]])
+                .map(|c| [c[0], c[1]])
                 .collect();
-            (2, Data::LineSegments2D(positions))
+            (2, Data::DataVec(DataVec::Vec2(positions)))
         }
         [_, 3] => {
             let positions = positions
                 .as_slice()
                 .unwrap()
-                .chunks(6)
+                .chunks_exact(3)
                 .into_iter()
-                .map(|c| [[c[0], c[1], c[2]], [c[3], c[4], c[5]]])
+                .map(|c| [c[0], c[1], c[2]])
                 .collect();
-            (3, Data::LineSegments3D(positions))
+            (3, Data::DataVec(DataVec::Vec3(positions)))
         }
         _ => {
             return Err(PyTypeError::new_err(format!(

crates/re_log_types/src/objects.rs
@@ -26,12 +26,12 @@ impl ObjectType {
             Self::Image => &["space", "color", "tensor", "meter"],
             Self::Point2D => &["space", "color", "pos", "radius"],
             Self::BBox2D => &["space", "color", "bbox", "stroke_width", "label"],
-            Self::LineSegments2D => &["space", "color", "line_segments", "stroke_width"],
+            Self::LineSegments2D => &["space", "color", "points", "stroke_width"],
 
             Self::Point3D => &["space", "color", "pos", "radius"],
             Self::Box3D => &["space", "color", "obb", "stroke_width"],
             Self::Path3D => &["space", "color", "points", "stroke_width"],
-            Self::LineSegments3D => &["space", "color", "line_segments", "stroke_width"],
+            Self::LineSegments3D => &["space", "color", "points", "stroke_width"],
             Self::Mesh3D => &["space", "color", "mesh"],
             Self::Camera => &["space", "color", "camera"],
         }

crates/re_data_store/Cargo.toml
@@ -12,6 +12,8 @@ re_log_types = { path = "../re_log_types" }
 
 ahash = "0.8"
 itertools = "0.10"
+log = "0.4"
+log-once = "0.3"
 nohash-hasher = "0.2"
 tracing = "0.1"
 

crates/re_data_store/src/storage.rs
@@ -379,15 +379,13 @@ impl<Time: 'static + Copy + Ord> DataStoreTypeErased<Time> {
             DataType::Color => handle_type!(Color, data_types::Color),
             DataType::Vec2 => handle_type!(Vec2, data_types::Vec2),
             DataType::BBox2D => handle_type!(BBox2D, re_log_types::BBox2D),
-            DataType::LineSegments2D => handle_type!(LineSegments2D, data_types::LineSegments2D),
             DataType::Vec3 => handle_type!(Vec3, data_types::Vec3),
             DataType::Box3 => handle_type!(Box3, re_log_types::Box3),
-            DataType::Path3D => handle_type!(Path3D, data_types::Path3D),
-            DataType::LineSegments3D => handle_type!(LineSegments3D, data_types::LineSegments3D),
             DataType::Mesh3D => handle_type!(Mesh3D, re_log_types::Mesh3D),
             DataType::Camera => handle_type!(Camera, re_log_types::Camera),
             DataType::Tensor => handle_type!(Tensor, re_log_types::Tensor),
             DataType::Space => handle_type!(Space, ObjPath),
+            DataType::DataVec => handle_type!(DataVec, DataVec),
         }
     }
 }

crates/re_log_types/src/data.rs
@@ -2,7 +2,7 @@ use crate::{impl_into_enum, ObjPath};
 
 // ----------------------------------------------------------------------------
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub enum DataType {
     // 1D:
@@ -15,20 +15,21 @@ pub enum DataType {
     // 2D:
     Vec2,
     BBox2D,
-    LineSegments2D,
 
     // ----------------------------
     // 3D:
     Vec3,
     Box3,
-    Path3D,
-    LineSegments3D,
     Mesh3D,
     Camera,
 
     // ----------------------------
     // N-D:
     Tensor,
+
+    /// A homogenous vector of data,
+    /// represented by [`DataVec`]
+    DataVec,
 
     // ----------------------------
     Space,
@@ -47,10 +48,9 @@ pub mod data_types {
     use super::DataTrait;
     use super::DataType;
 
-    /// For batches
-    impl<T: DataTrait> DataTrait for Vec<T> {
+    impl DataTrait for super::DataVec {
         fn data_typ() -> DataType {
-            T::data_typ()
+            DataType::DataVec
         }
     }
 
@@ -95,13 +95,6 @@ pub mod data_types {
         }
     }
 
-    pub type LineSegments2D = Vec<LineSegment2D>;
-    pub type LineSegment2D = [Vec2; 2];
-    impl DataTrait for LineSegment2D {
-        fn data_typ() -> DataType {
-            DataType::LineSegments2D
-        }
-    }
     impl DataTrait for crate::Tensor {
         fn data_typ() -> DataType {
             DataType::Tensor
@@ -120,16 +113,6 @@ pub mod data_types {
     impl DataTrait for crate::Box3 {
         fn data_typ() -> DataType {
             DataType::Box3
-        }
-    }
-
-    pub type Path3D = Vec<Vec3>;
-
-    pub type LineSegments3D = Vec<LineSegment3D>;
-    pub type LineSegment3D = [Vec3; 2];
-    impl DataTrait for LineSegment3D {
-        fn data_typ() -> DataType {
-            DataType::LineSegments3D
         }
     }
 
@@ -169,20 +152,20 @@ pub enum Data {
     // 2D:
     Vec2(data_types::Vec2),
     BBox2D(BBox2D),
-    LineSegments2D(data_types::LineSegments2D),
 
     // ----------------------------
     // 3D:
     Vec3(data_types::Vec3),
     Box3(Box3),
-    Path3D(data_types::Path3D),
-    LineSegments3D(data_types::LineSegments3D),
     Mesh3D(Mesh3D),
     Camera(Camera),
 
     // ----------------------------
     // N-D:
     Tensor(Tensor),
+
+    /// Homogenous vector
+    DataVec(DataVec),
 
     // ----------------------------
     // Meta:
@@ -201,16 +184,14 @@ impl Data {
 
             Self::Vec2(_) => DataType::Vec2,
             Self::BBox2D(_) => DataType::BBox2D,
-            Self::LineSegments2D(_) => DataType::LineSegments2D,
 
             Self::Vec3(_) => DataType::Vec3,
             Self::Box3(_) => DataType::Box3,
-            Self::Path3D(_) => DataType::Path3D,
-            Self::LineSegments3D(_) => DataType::LineSegments3D,
             Self::Mesh3D(_) => DataType::Mesh3D,
             Self::Camera(_) => DataType::Camera,
 
             Self::Tensor(_) => DataType::Tensor,
+            Self::DataVec(_) => DataType::DataVec,
 
             Self::Space(_) => DataType::Space,
         }
@@ -229,6 +210,7 @@ impl_into_enum!(ObjPath, Data, Space);
 // ----------------------------------------------------------------------------
 
 /// Vectorized, type-erased version of [`Data`].
+// TODO(emilk): we should generalize this to a tensor.
 #[derive(Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub enum DataVec {
@@ -239,16 +221,16 @@ pub enum DataVec {
 
     Vec2(Vec<data_types::Vec2>),
     BBox2D(Vec<BBox2D>),
-    LineSegments2D(Vec<data_types::LineSegments2D>),
 
     Vec3(Vec<data_types::Vec3>),
     Box3(Vec<Box3>),
-    Path3D(Vec<data_types::Path3D>),
-    LineSegments3D(Vec<data_types::LineSegments3D>),
     Mesh3D(Vec<Mesh3D>),
     Camera(Vec<Camera>),
 
     Tensor(Vec<Tensor>),
+
+    /// A vector of [`DataVec`] (vector of vectors)
+    DataVec(Vec<DataVec>),
 
     Space(Vec<ObjPath>),
 }
@@ -270,14 +252,12 @@ macro_rules! data_map(
             Data::String($value) => $action,
             Data::Vec2($value) => $action,
             Data::BBox2D($value) => $action,
-            Data::LineSegments2D($value) => $action,
             Data::Vec3($value) => $action,
             Data::Box3($value) => $action,
-            Data::Path3D($value) => $action,
-            Data::LineSegments3D($value) => $action,
             Data::Mesh3D($value) => $action,
             Data::Camera($value) => $action,
             Data::Tensor($value) => $action,
+            Data::DataVec($value) => $action,
             Data::Space($value) => $action,
         }
     });
@@ -300,14 +280,12 @@ macro_rules! data_vec_map(
             DataVec::String($vec) => $action,
             DataVec::Vec2($vec) => $action,
             DataVec::BBox2D($vec) => $action,
-            DataVec::LineSegments2D($vec) => $action,
-            DataVec::Tensor($vec) => $action,
             DataVec::Vec3($vec) => $action,
             DataVec::Box3($vec) => $action,
-            DataVec::Path3D($vec) => $action,
-            DataVec::LineSegments3D($vec) => $action,
             DataVec::Mesh3D($vec) => $action,
             DataVec::Camera($vec) => $action,
+            DataVec::Tensor($vec) => $action,
+            DataVec::DataVec($vec) => $action,
             DataVec::Space($vec) => $action,
         }
     });
@@ -315,7 +293,7 @@ macro_rules! data_vec_map(
 
 impl DataVec {
     #[inline]
-    pub fn data_type(&self) -> DataType {
+    pub fn element_data_type(&self) -> DataType {
         match self {
             Self::I32(_) => DataType::I32,
             Self::F32(_) => DataType::F32,
@@ -324,16 +302,14 @@ impl DataVec {
 
             Self::Vec2(_) => DataType::Vec2,
             Self::BBox2D(_) => DataType::BBox2D,
-            Self::LineSegments2D(_) => DataType::LineSegments2D,
 
             Self::Vec3(_) => DataType::Vec3,
             Self::Box3(_) => DataType::Box3,
-            Self::Path3D(_) => DataType::Path3D,
-            Self::LineSegments3D(_) => DataType::LineSegments3D,
             Self::Mesh3D(_) => DataType::Mesh3D,
             Self::Camera(_) => DataType::Camera,
 
             Self::Tensor(_) => DataType::Tensor,
+            Self::DataVec(_) => DataType::DataVec,
 
             Self::Space(_) => DataType::Space,
         }
@@ -356,16 +332,14 @@ impl DataVec {
 
             Self::Vec2(vec) => vec.last().cloned().map(Data::Vec2),
             Self::BBox2D(vec) => vec.last().cloned().map(Data::BBox2D),
-            Self::LineSegments2D(vec) => vec.last().cloned().map(Data::LineSegments2D),
 
             Self::Vec3(vec) => vec.last().cloned().map(Data::Vec3),
             Self::Box3(vec) => vec.last().cloned().map(Data::Box3),
-            Self::Path3D(vec) => vec.last().cloned().map(Data::Path3D),
-            Self::LineSegments3D(vec) => vec.last().cloned().map(Data::LineSegments3D),
             Self::Mesh3D(vec) => vec.last().cloned().map(Data::Mesh3D),
             Self::Camera(vec) => vec.last().cloned().map(Data::Camera),
 
             Self::Tensor(vec) => vec.last().cloned().map(Data::Tensor),
+            Self::DataVec(vec) => vec.last().cloned().map(Data::DataVec),
 
             Self::Space(vec) => vec.last().cloned().map(Data::Space),
         }
@@ -375,8 +349,8 @@ impl DataVec {
 impl std::fmt::Debug for DataVec {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("DataVec")
-            .field("data_type", &self.data_type())
             .field("len", &self.len())
+            .field("data_type", &self.element_data_type())
             .finish_non_exhaustive()
     }
 }

crates/re_log_types/src/lib.rs
@@ -254,7 +254,7 @@ impl LoggedData {
     pub fn data_type(&self) -> DataType {
         match self {
             Self::Single(data) | Self::BatchSplat(data) => data.data_type(),
-            Self::Batch { data, .. } => data.data_type(),
+            Self::Batch { data, .. } => data.element_data_type(),
         }
     }
 }

crates/re_viewer/src/misc.rs
@@ -102,9 +102,9 @@ pub fn calc_bbox_2d(objects: &re_data_store::Objects<'_>) -> emath::Rect {
     }
 
     for (_, obj) in objects.line_segments2d.iter() {
-        for [a, b] in obj.line_segments {
-            bbox.extend_with(a.into());
-            bbox.extend_with(b.into());
+        for pair in obj.points.chunks_exact(2) {
+            bbox.extend_with(pair[0].into());
+            bbox.extend_with(pair[1].into());
         }
     }
 
@@ -156,9 +156,9 @@ pub fn calc_bbox_3d(objects: &re_data_store::Objects<'_>) -> macaw::BoundingBox 
     }
 
     for (_, obj) in objects.line_segments3d.iter() {
-        for &[a, b] in obj.line_segments {
-            bbox.extend(a.into());
-            bbox.extend(b.into());
+        for pair in obj.points.chunks_exact(2) {
+            bbox.extend(pair[0].into());
+            bbox.extend(pair[1].into());
         }
     }
 

commit f04a822a081dae42e0b50a4118844f06066b9246

Fix logging of line segments that broke in #61

crates/re_data_store/src/objects.rs
@@ -347,7 +347,7 @@ impl<'s> LineSegments2D<'s> {
 
         visit_type_data_3(
             obj_store,
-            &FieldName::from("line_segments"),
+            &FieldName::from("points"),
             time_query,
             ("space", "color", "stroke_width"),
             |obj_path: &ObjPath,
@@ -392,7 +392,7 @@ impl<'s> LineSegments3D<'s> {
 
         visit_type_data_3(
             obj_store,
-            &FieldName::from("line_segments"),
+            &FieldName::from("points"),
             time_query,
             ("space", "color", "stroke_width"),
             |obj_path: &ObjPath,

crates/re_sdk_python/src/python_bridge.rs
@@ -507,7 +507,7 @@ fn log_line_segments(
     sdk.send(LogMsg::DataMsg(DataMsg {
         msg_id: MsgId::random(),
         time_point: time_point.clone(),
-        data_path: DataPath::new(obj_path.clone(), "line_segments".into()),
+        data_path: DataPath::new(obj_path.clone(), "points".into()),
         data: LoggedData::Single(positions),
     }));
 