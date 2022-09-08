2433310357c0a1f01a5cb2a37e3d2344e88cf772
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-09-08 09:13:41 +0200

    Add -y flag to apt-get install, and add patchelf
    patchelf is required to build the python wheel

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ cd "$script_path/.."
 set -x
 
 # eframe dependencies needed on run on Linux and Fedora Rawhide:
-if [ -x "$(command -v apt-get)" ]; then sudo apt-get install libgtk-3-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev
+if [ -x "$(command -v apt-get)" ]; then sudo apt-get -y install libgtk-3-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev patchelf
 elif [ -x "$(command -v dnf)" ];   then sudo dnf install clang clang-devel clang-tools-extra speech-dispatcher-devel libxkbcommon-devel pkg-config openssl-devel libxcb-devel
 fi
 
--- scripts/setup_web.sh
+++ scripts/setup_web.sh
@@ -10,7 +10,7 @@ cargo install wasm-bindgen-cli --version 0.2.82
 # Required by build_web.sh:
 packagesNeeded='binaryen jq'
 if [ -x "$(command -v brew)" ];      then brew install $packagesNeeded
-elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get -y install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 elif [ -x "$(command -v apk)" ];     then sudo apk add --no-cache $packagesNeeded
