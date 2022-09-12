46508b3c600db20f221dca0df39e00730c91af76
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-09-12 16:09:05 +0200

    refactor: replace a `mut` with `std::iter::successors`

--- crates/re_error/src/lib.rs
+++ crates/re_error/src/lib.rs
@@ -2,12 +2,11 @@
 ///
 /// Always use this when displaying an error.
 pub fn format(error: impl AsRef<dyn std::error::Error>) -> String {
-    fn format_impl(mut error: &dyn std::error::Error) -> String {
+    fn format_impl(error: &dyn std::error::Error) -> String {
         let mut string = error.to_string();
-        while let Some(source) = error.source() {
+        for source in std::iter::successors(error.source(), |error| error.source()) {
             string.push_str(" -> ");
             string.push_str(&source.to_string());
-            error = source;
         }
         string
     }
