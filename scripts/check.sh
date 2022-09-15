2586b2428e83b690cb65a25a95d9d979e717e974
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-09-15 11:36:18 +0200

    remove --lib from some CI commands

--- scripts/check.sh
+++ scripts/check.sh
@@ -10,17 +10,17 @@ export RUSTFLAGS="-D warnings"
 export RUSTDOCFLAGS="-D warnings -D rustdoc::missing_crate_level_docs" # https://github.com/ericseppanen/cargo-cranky/issues/8
 
 cargo check --all-targets --all-features
-cargo check -p re_viewer --all-features --lib --target wasm32-unknown-unknown
+cargo check -p re_viewer --all-features --target wasm32-unknown-unknown
 cargo fmt --all -- --check
 cargo cranky --all-targets --all-features -- -D warnings
 cargo test --all-targets --all-features
 cargo test --doc --all-features # checks all doc-tests
 
-cargo doc --lib --no-deps --all-features
+cargo doc --no-deps --all-features
 cargo doc --document-private-items --no-deps --all-features
 
 (cd crates/re_log_types && cargo check --no-default-features)
-(cd crates/re_viewer && cargo check --no-default-features --lib)
+(cd crates/re_viewer && cargo check --no-default-features)
 (cd crates/re_web_server && cargo check --no-default-features)
 (cd crates/re_ws_comms && cargo check --no-default-features)
 (cd crates/rerun && cargo check --no-default-features)
