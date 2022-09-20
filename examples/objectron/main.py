cb9b8ef0956f9342cc0d988e748f3b773cb72593
Clement Rey <cr.rey.clement@gmail.com>
2022-09-20 11:27:45 +0200

    logging cameras and getting familiar with the whole numpy/scipy thingy

--- examples/objectron/main.py
+++ examples/objectron/main.py
@@ -2,19 +2,19 @@
 
 """Example of using the Rerun SDK to log the Objectron dataset."""
 
-import os
 import argparse
+import math
 import numpy as np
+import os
 
 from pathlib import Path
 
 import rerun_sdk as rerun
 
+from proto.research.compvideo.arcapture import ARFrame, ARCamera
+
 def relpath(path: Path):
-    return os.path.join(
-        os.path.abspath(os.path.dirname(__file__)),
-        path
-    )
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), path)
 
 def log_dir(dirpath: Path, nb_frames: int):
     log_geometry(dirpath, nb_frames)
@@ -32,7 +32,6 @@ def log_geometry(dirpath: Path, nb_frames: int):
         next_len = int.from_bytes(data[:4], byteorder='little', signed=False)
         data = data[4:]
 
-        from proto.research.compvideo.arcapture import ARFrame
         frame = ARFrame().parse(data[:next_len])
         data = data[next_len:]
 
@@ -40,8 +39,10 @@ def log_geometry(dirpath: Path, nb_frames: int):
         rerun.set_time_seconds("time", frame.timestamp)
 
         log_image(os.path.join(dirpath, f"video/{frame_idx}.jpg"))
+        log_camera(frame.camera)
 
         frame_idx += 1
+
 
 def log_image(path: str):
     print(f"logging image: {path}")
@@ -52,6 +53,43 @@ def log_image(path: str):
 
     rgb = np.asarray(img)
     rerun.log_image("video", rgb, space="image")
+
+
+def log_camera(cam: ARCamera):
+    world_from_cam = np.asarray(cam.transform).reshape((4, 4)).T
+    translation = world_from_cam[3][:3]
+    intrinsics = np.asarray(cam.intrinsics).reshape((3, 3)).T
+    (w, h) = (cam.image_resolution_width, cam.image_resolution_height)
+
+    from scipy.spatial.transform import Rotation as R
+    rot = R.from_euler("YXZ", [
+        cam.euler_angles.yaw,
+        cam.euler_angles.pitch,
+        cam.euler_angles.roll,
+    ])
+    # rot = R.from_matrix(np.resize(world_from_cam, (3, 3)))
+
+    ## Because the dataset was collected in portrait:
+    swizzle_x_y = np.asarray([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+    intrinsics = swizzle_x_y.dot(intrinsics.dot(swizzle_x_y))
+    axis = R.from_rotvec((math.tau / 4.0) * np.array([0.0, 0.0, -1.0]))
+    rot = rot * axis
+    (w, h) = (h, w)
+
+    print(rot.as_quat())
+    print(translation)
+    print(intrinsics)
+    print(w, h)
+
+    ## TODO: something fishy going on with those quaternions
+    rerun.log_camera("camera",
+                     resolution=[w, h],
+                     intrinsics=intrinsics,
+                     rotation_q=rot.as_quat(),
+                     position=translation,
+                     camera_space_convention=rerun.CameraSpaceConvention.X_RIGHT_Y_UP_Z_BACK,
+                     space="world",
+                     target_space="image")
 
 
 if __name__ == '__main__':
--- examples/objectron/setup.sh
+++ examples/objectron/setup.sh
@@ -1,2 +1,3 @@
-pip install requests "betterproto[compiler]"
+## TODO: should this really be here?
+pip install requests "betterproto[compiler]" pillow scipy
 (cd ./proto && protoc -I . --python_betterproto_out=. *.proto)
