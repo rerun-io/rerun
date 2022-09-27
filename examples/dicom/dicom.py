924d67828718dfb67707a34ec5ba37482a26bb12
Nikolaus West <niko@grapic.co>
2022-09-27 17:08:58 +0200

    Basic diacom example working: one slice per log

--- examples/dicom/dicom.py
+++ examples/dicom/dicom.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env pythong3
 
 import argparse
-from typing import Final, Iterable
+from typing import Final, Iterable, Tuple
 import pydicom as dicom
-import dicom_numpy
+import dicom_numpy   # type: ignore
 import numpy as np
 import os
 from pathlib import Path
@@ -11,21 +11,26 @@ import numpy.typing as npt
 
 import rerun_sdk as rerun
 
-DATA_FILES: Final = Path(os.path.dirname(__file__)).joinpath("data").glob("*.dcm")
+DATASET_DIR: Final = Path(os.path.dirname(__file__)) / "dataset"
 
 # extract voxel data
-def extract_voxel_data(dicom_files: Iterable[Path]) -> npt.NDArray[np.int16]:
-    datasets = [dicom.read_file(f) for f in dicom_files]
+def extract_voxel_data(dicom_files: Iterable[Path]) -> Tuple[npt.NDArray[np.int16], npt.NDArray[np.float32]]:
+    slices = [dicom.read_file(f) for f in dicom_files]
     try:
-        voxel_ndarray, ijk_to_xyz = dicom_numpy.combine_slices(datasets)
+        voxel_ndarray, ijk_to_xyz = dicom_numpy.combine_slices(slices, rescale=True)
     except dicom_numpy.DicomImportException as e:
     # invalid DICOM data
         raise
-    return voxel_ndarray
 
-def log_dicom_data():
-    data = extract_voxel_data(DATA_FILES)
-    print(data.dtype, data.shape)
+    return voxel_ndarray, ijk_to_xyz
+
+
+def list_viacom_files(dir: Path) -> Iterable[Path]:
+    for path, _, files in os.walk(dir):
+        for f in files:
+            if f.endswith(".dcm"):
+                yield Path(path) / f
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(
@@ -38,7 +43,14 @@ def main() -> None:
                         help='Save data to a .rrd file at this path')
     args = parser.parse_args()
 
-    log_dicom_data()
+    dicom_files = list_viacom_files(DATASET_DIR)
+    voxels_volume, _ = extract_voxel_data(dicom_files)
+
+    for slice_idx in range(voxels_volume.shape[-1]):
+        rerun.set_time_sequence("slice_idx", slice_idx)
+        rerun.log_image("vicom/slice", voxels_volume[:, :, slice_idx], space="mri/xy")
+
+    rerun.show()
 
 
 if __name__ == '__main__':
---
+++ examples/dicom/download_dataset.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import io
+import os
+from pathlib import Path
+from typing import Final
+import zipfile
+
+import requests
+
+
+DATASET_DIR: Final = Path(os.path.dirname(__file__)) / "dataset"
+
+
+def download_dataset():
+    url = "https://storage.googleapis.com/rerun-example-datasets/dicom.zip"
+
+    os.makedirs(DATASET_DIR.absolute(), exist_ok=True)
+
+    print(f"downloading dataset…")
+    resp = requests.get(url, stream=True)
+    z = zipfile.ZipFile(io.BytesIO(resp.content))
+    z.extractall(DATASET_DIR.absolute())
+
+download_dataset()
