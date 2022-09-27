d046e6f86991b5cb2b7f9180b94c2fc78e7d80b9
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-09-27 22:46:15 +0200

    Do slicing and show slice as image … has bug

--- crates/re_viewer/src/ui/view_tensor.rs
+++ crates/re_viewer/src/ui/view_tensor.rs
@@ -1,6 +1,9 @@
 use re_data_store::ObjPath;
 use re_log_types::Tensor;
 
+use egui::Color32;
+use itertools::Itertools as _;
+
 use crate::misc::ViewerContext;
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
@@ -81,6 +84,64 @@ pub(crate) fn view_tensor(
         if len > 1 {
             let slice = state.selectors.entry(dim_idx).or_default();
             ui.add(egui::Slider::new(slice, 0..=len - 1).text(name));
+        }
+    }
+
+    if let Ok(tensor) = re_tensor_ops::as_ndarray::<f32>(tensor) {
+        let slice_info_elems = (0..tensor.ndim())
+            .map(|dim| {
+                if let Some(selector) = state.selectors.get(&dim) {
+                    ndarray::SliceInfoElem::Index(*selector as _)
+                } else {
+                    ndarray::SliceInfoElem::Slice {
+                        start: 0,
+                        end: None,
+                        step: 1,
+                    }
+                }
+            })
+            .collect_vec();
+        let slice = tensor.slice(slice_info_elems.as_slice());
+        ui.monospace(format!("Slice shape: {:?}", slice.shape()));
+
+        if slice.ndim() == 2 {
+            let (width, height) = (slice.shape()[0], slice.shape()[1]); // TODO: from the rank-mapping
+
+            let mut min = f32::INFINITY;
+            let mut max = f32::NEG_INFINITY;
+            for &value in &slice {
+                min = min.min(value);
+                max = max.max(value);
+            }
+
+            ui.monospace(format!("Data range: [{min} - {max}]"));
+
+            let mut image = egui::ColorImage::new([width, height], Color32::TRANSPARENT);
+
+            assert_eq!(image.pixels.len(), slice.iter().count());
+            for (pixel, value) in itertools::izip!(&mut image.pixels, &slice) {
+                let lum = egui::remap(*value, min..=max, 0.0..=255.0).round() as u8;
+                *pixel = Color32::from_gray(lum);
+            }
+
+            if ui.button("Save image").clicked() {
+                let image = image::RgbaImage::from_raw(
+                    width as _,
+                    height as _,
+                    bytemuck::cast_slice(&image.pixels).to_vec(),
+                )
+                .unwrap();
+                let image = image::DynamicImage::ImageRgba8(image);
+                let path = "tensor_slice.png";
+                image.save(path).unwrap();
+                re_log::info!("Saved to {path:?}");
+            }
+
+            let texture = ui
+                .ctx()
+                .load_texture("tensor_slice", image, egui::TextureFilter::Linear); // TODO: cache - don't call every frame
+
+            ui.image(texture.id(), texture.size_vec2());
         }
     }
 }
