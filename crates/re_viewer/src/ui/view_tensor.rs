7d626f40fe3519c431bb124f62099018fc4bb9df
Andreas Reich <andreas@rerun.io>
2022-09-28 14:53:59 +0200

    work in progress rank mapper ui

--- crates/re_viewer/src/ui/view_tensor.rs
+++ crates/re_viewer/src/ui/view_tensor.rs
@@ -1,4 +1,4 @@
-use re_log_types::{Tensor, TensorDataType};
+use re_log_types::{Tensor, TensorDataType, TensorDimension};
 
 use egui::{Color32, ColorImage};
 use itertools::Itertools as _;
@@ -54,12 +54,246 @@ impl RankMapping {
     }
 }
 
-fn rank_mapping_ui(ui: &mut egui::Ui, rank_mapping: &mut RankMapping) {
-    // TODO(emilk): Add a proper rank mapping GUI here
-    if ui.button("transpose").clicked() {
-        std::mem::swap(&mut rank_mapping.width, &mut rank_mapping.height);
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum DragDropAddress {
+    None,
+    Width,
+    Height,
+    Channel,
+    Selector(usize),
+    NewSelector,
+}
+
+fn tensor_dimension_ui<'a>(
+    ui: &mut egui::Ui,
+    bound_dim_idx: Option<usize>,
+    location: DragDropAddress,
+    shape: &[TensorDimension],
+    drop_source: &mut DragDropAddress,
+    drop_target: &mut DragDropAddress,
+) {
+    // TODO: don't accept everythingp
+    let response = dimension_drop_target(ui, true, |ui| {
+        ui.set_min_size(egui::vec2(80., 15.));
+
+        if let Some(dim_idx) = bound_dim_idx.to_owned() {
+            let dim = &shape[dim_idx];
+            // TODO: Does this need to be globally unique?
+            let dim_ui_id = egui::Id::new("tensor_dimension_ui").with(dim_idx);
+
+            let tmp: String;
+            let display_name = if dim.name.is_empty() {
+                tmp = format!("{}", dim_idx);
+                &tmp
+            } else {
+                &dim.name
+            };
+
+            drag_source(ui, dim_ui_id, |ui| {
+                ui.label(format!("▓ {} ({})", display_name, dim.size));
+            });
+
+            if ui.memory().is_being_dragged(dim_ui_id) {
+                *drop_source = location;
+            }
+        }
+    })
+    .response;
+
+    let is_being_dragged = ui.memory().is_anything_being_dragged();
+    if is_being_dragged && response.hovered() {
+        *drop_target = location;
     }
+}
+
+pub fn drag_source(ui: &mut egui::Ui, id: egui::Id, body: impl FnOnce(&mut egui::Ui)) {
+    let is_being_dragged = ui.memory().is_being_dragged(id);
+
+    if !is_being_dragged {
+        let response = ui.scope(body).response;
+
+        // Check for drags:
+        let response = ui.interact(response.rect, id, egui::Sense::drag());
+        if response.hovered() {
+            ui.output().cursor_icon = egui::CursorIcon::Grab;
+        }
+    } else {
+        ui.output().cursor_icon = egui::CursorIcon::Grabbing;
+
+        // Paint the body to a new layer:
+        let layer_id = egui::LayerId::new(egui::Order::Tooltip, id);
+        let response = ui.with_layer_id(layer_id, body).response;
+
+        // Now we move the visuals of the body to where the mouse is.
+        // Normally you need to decide a location for a widget first,
+        // because otherwise that widget cannot interact with the mouse.
+        // However, a dragged component cannot be interacted with anyway
+        // (anything with `Order::Tooltip` always gets an empty [`Response`])
+        // So this is fine!
+
+        if let Some(pointer_pos) = ui.ctx().pointer_interact_pos() {
+            let delta = pointer_pos - response.rect.center();
+            ui.ctx().translate_layer(layer_id, delta);
+        }
+    }
+}
+
+// Draws rectangle for a drop landing zone for dimensions
+// TODO: We should make this code reusable.
+pub fn dimension_drop_target<R>(
+    ui: &mut egui::Ui,
+    can_accept_what_is_being_dragged: bool,
+    body: impl FnOnce(&mut egui::Ui) -> R,
+) -> egui::InnerResponse<R> {
+    let is_being_dragged = ui.memory().is_anything_being_dragged();
+
+    let margin = egui::Vec2::splat(4.0);
+
+    let outer_rect_bounds = ui.available_rect_before_wrap();
+    let inner_rect = outer_rect_bounds.shrink2(margin);
+    let where_to_put_background = ui.painter().add(egui::Shape::Noop);
+    let mut content_ui = ui.child_ui(inner_rect, *ui.layout());
+    let ret = body(&mut content_ui);
+    let outer_rect =
+        egui::Rect::from_min_max(outer_rect_bounds.min, content_ui.min_rect().max + margin);
+    let (rect, response) = ui.allocate_at_least(outer_rect.size(), egui::Sense::hover());
+
+    let style = if is_being_dragged && can_accept_what_is_being_dragged && response.hovered() {
+        ui.visuals().widgets.active
+    } else {
+        ui.visuals().widgets.inactive
+    };
+
+    let mut fill = style.bg_fill;
+    let mut stroke = style.bg_stroke;
+    if is_being_dragged && !can_accept_what_is_being_dragged {
+        // gray out:
+        fill = egui::color::tint_color_towards(fill, ui.visuals().window_fill());
+        stroke.color = egui::color::tint_color_towards(stroke.color, ui.visuals().window_fill());
+    }
+
+    ui.painter().set(
+        where_to_put_background,
+        egui::epaint::RectShape {
+            rounding: style.rounding,
+            fill,
+            stroke,
+            rect,
+        },
+    );
+
+    egui::InnerResponse::new(ret, response)
+}
+
+fn rank_mapping_ui(ui: &mut egui::Ui, rank_mapping: &mut RankMapping, shape: &[TensorDimension]) {
+    ui.label("TODO");
     ui.monospace(format!("{rank_mapping:?}"));
+
+    let mut drop_source = DragDropAddress::None;
+    let mut drop_target = DragDropAddress::None;
+
+    ui.columns(2, |columns| {
+        {
+            let ui = &mut columns[0];
+            ui.heading("Image:");
+            egui::Grid::new("imagegrid").num_columns(2).show(ui, |ui| {
+                ui.label("Width:");
+                tensor_dimension_ui(
+                    ui,
+                    rank_mapping.width,
+                    DragDropAddress::Width,
+                    shape,
+                    &mut drop_source,
+                    &mut drop_target,
+                );
+                ui.end_row();
+
+                ui.label("Height:");
+                tensor_dimension_ui(
+                    ui,
+                    rank_mapping.height,
+                    DragDropAddress::Height,
+                    shape,
+                    &mut drop_source,
+                    &mut drop_target,
+                );
+                ui.end_row();
+
+                ui.label("Channel:");
+                tensor_dimension_ui(
+                    ui,
+                    rank_mapping.channel,
+                    DragDropAddress::Channel,
+                    shape,
+                    &mut drop_source,
+                    &mut drop_target,
+                );
+                ui.end_row();
+            });
+        }
+        {
+            let ui = &mut columns[1];
+            ui.heading("Selectors:");
+            egui::Grid::new("selectiongrid")
+                .num_columns(1)
+                .show(ui, |ui| {
+                    for (selector_idx, &mut dim_idx) in
+                        rank_mapping.selectors.iter_mut().enumerate()
+                    {
+                        tensor_dimension_ui(
+                            ui,
+                            Some(dim_idx),
+                            DragDropAddress::Selector(selector_idx),
+                            shape,
+                            &mut drop_source,
+                            &mut drop_target,
+                        );
+                        ui.end_row();
+                    }
+                    tensor_dimension_ui(
+                        ui,
+                        None,
+                        DragDropAddress::NewSelector,
+                        shape,
+                        &mut drop_source,
+                        &mut drop_target,
+                    );
+                    ui.end_row();
+                });
+        }
+    });
+
+    // persist drag/drop
+    if drop_target != DragDropAddress::None && drop_source != DragDropAddress::None {
+        let read_from_address = |address| match address {
+            DragDropAddress::None => unreachable!(),
+            DragDropAddress::Width => rank_mapping.width,
+            DragDropAddress::Height => rank_mapping.height,
+            DragDropAddress::Channel => rank_mapping.channel,
+            DragDropAddress::Selector(selector_idx) => Some(rank_mapping.selectors[selector_idx]),
+            DragDropAddress::NewSelector => None,
+        };
+        let previous_value_source = read_from_address(drop_source);
+        let previous_value_target = read_from_address(drop_target);
+
+        let mut write_to_address = |address, dim_idx| match address {
+            DragDropAddress::None => unreachable!(),
+            DragDropAddress::Width => rank_mapping.width = dim_idx,
+            DragDropAddress::Height => rank_mapping.height = dim_idx,
+            DragDropAddress::Channel => rank_mapping.channel = dim_idx,
+            DragDropAddress::Selector(selector_idx) => {
+                if let Some(dim_idx) = dim_idx {
+                    rank_mapping.selectors[selector_idx] = dim_idx;
+                } else {
+                    rank_mapping.selectors.remove(selector_idx);
+                }
+            }
+            // NewSelector can only be a drop *target*, therefore dim_idx can't be None!
+            DragDropAddress::NewSelector => rank_mapping.selectors.push(dim_idx.unwrap()),
+        };
+        write_to_address(drop_source, previous_value_target);
+        write_to_address(drop_target, previous_value_source);
+    }
 }
 
 // ----------------------------------------------------------------------------
@@ -117,7 +351,7 @@ pub(crate) fn view_tensor(ui: &mut egui::Ui, state: &mut TensorViewState, tensor
     ui.monospace(format!("dtype: {:?}", tensor.dtype));
 
     ui.collapsing("Rank Mapping", |ui| {
-        rank_mapping_ui(ui, &mut state.rank_mapping);
+        rank_mapping_ui(ui, &mut state.rank_mapping, &tensor.shape);
     });
     color_mapping_ui(ui, &mut state.color_mapping);
 
