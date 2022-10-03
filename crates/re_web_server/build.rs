1ca188d10ef2284126df7bf67764c849ac9e2c0e
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-10-03 21:16:26 +0200

    Attempt to build Web Viewer WASM from re_web_server/build.rs
    The problem is that we start a cargo build while one is already
    in progress. This makes `cargo` hang with:
    
    `Blocking waiting for file lock on build directory`

--- crates/re_web_server/build.rs
+++ crates/re_web_server/build.rs
@@ -1,3 +1,5 @@
+use std::process::Stdio;
+
 // Mapping to cargo:rerun-if-changed with glob support
 fn rerun_if_changed(path: &str) {
     for path in glob::glob(path).unwrap() {
@@ -15,8 +17,18 @@ fn main() {
     rerun_if_changed("../../crates/re_viewer/Cargo.toml");
     rerun_if_changed("../../crates/re_viewer/src/**/*.rs");
 
-    // Disabled, because it hangs :(
-    // std::process::Command::new("../../scripts/build_web.sh")
-    //     .output()
-    //     .expect("failed to build viewer for web");
+    if std::env::var("PROFILE").unwrap() == "release" {
+        std::process::Command::new("../../scripts/build_web.sh")
+            .arg("--optimize")
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .expect("failed to build viewer for web");
+    } else {
+        std::process::Command::new("../../scripts/build_web.sh")
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output()
+            .expect("failed to build viewer for web");
+    }
 }
