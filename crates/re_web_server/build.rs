91209b8e9e34bfbc9f21081a3e35c978b4d7a4e3
Clement Rey <cr.rey.clement@gmail.com>
2022-10-12 09:43:42 +0200

    addressing PR comments

--- crates/re_web_server/build.rs
+++ crates/re_web_server/build.rs
@@ -1,9 +1,11 @@
-use cargo_metadata::{CargoOpt, MetadataCommand, Package, PackageId};
+use cargo_metadata::{CargoOpt, Metadata, MetadataCommand, Package, PackageId};
 use std::{
     collections::{HashMap, HashSet},
     ffi::OsString,
     process::Stdio,
 };
+
+// ---
 
 // Mapping to cargo:rerun-if-changed with glob support
 fn rerun_if_changed(path: &str) {
@@ -12,73 +14,86 @@ fn rerun_if_changed(path: &str) {
     }
 }
 
-/// Generates all the appropriate `cargo:rerun-if-changed` lines so that all locally
-/// patched dependency of package `pkg_name` are properly tracked.
-fn rerun_if_patched(pkg_name: &str) {
-    let metadata = MetadataCommand::new()
-        .features(CargoOpt::AllFeatures)
-        .exec()
-        .unwrap();
+// ---
 
-    let pkgs = metadata
-        .packages
-        .iter()
-        .map(|pkg| (pkg.name.as_str(), pkg))
-        .collect::<HashMap<_, _>>();
-    let pkgs_workspace = metadata.workspace_packages();
-    let pkgs_workspace = pkgs_workspace
-        .iter()
-        .map(|pkg| (pkg.name.as_str(), *pkg))
-        .collect::<HashMap<_, _>>();
-
-    let mut tracked = HashSet::new();
-
-    let pkg = pkgs.values().find(|pkg| pkg.name == pkg_name).unwrap();
-    track_patched_deps(&pkgs, &pkgs_workspace, &mut tracked, pkg);
+struct Packages<'a> {
+    pkgs: HashMap<&'a str, &'a Package>,
+    pkgs_workspace: HashMap<&'a str, &'a Package>,
 }
 
-/// Recursively walk the tree of dependencies of the given `root` package, making sure
-/// to track all potentially modified, locally patched dependencies.
-fn track_patched_deps(
-    pkgs: &HashMap<&str, &Package>,
-    pkgs_workspace: &HashMap<&str, &Package>,
-    tracked: &mut HashSet<PackageId>,
-    root: &Package,
-) {
-    for dep_pkg in root
-        .dependencies
-        .iter()
-        // NOTE: We'd like to just use `dep.source`/`dep.path`, unfortunately they do not
-        // account for crate patches at this level, so we build our own little index
-        // and use that instead.
-        .filter_map(|dep| pkgs.get(dep.name.as_str()))
-    {
-        let is_in_workspace = pkgs_workspace.contains_key(dep_pkg.name.as_str());
-        let exists_on_local_disk = dep_pkg.source.is_none();
-        if !is_in_workspace && exists_on_local_disk {
-            let mut dep_path = dep_pkg.manifest_path.clone();
-            dep_path.pop();
+impl<'a> Packages<'a> {
+    pub fn from_metadata(metadata: &'a Metadata) -> Self {
+        let pkgs = metadata
+            .packages
+            .iter()
+            .map(|pkg| (pkg.name.as_str(), pkg))
+            .collect::<HashMap<_, _>>();
 
-            rerun_if_changed(dep_path.join("Cargo.toml").as_ref());
-            rerun_if_changed(dep_path.join("Cargo.lock").as_ref());
-            rerun_if_changed(dep_path.join("**/*.rs").as_ref());
+        let pkgs_workspace = metadata.workspace_packages();
+        let pkgs_workspace = pkgs_workspace
+            .iter()
+            .map(|pkg| (pkg.name.as_str(), *pkg))
+            .collect::<HashMap<_, _>>();
+
+        Self {
+            pkgs,
+            pkgs_workspace,
         }
+    }
 
-        if tracked.insert(dep_pkg.id.clone()) {
-            track_patched_deps(pkgs, pkgs_workspace, tracked, dep_pkg);
+    /// Generates all the appropriate `cargo:rerun-if-changed` lines so that all locally
+    /// patched dependency of package `pkg_name` are properly tracked.
+    pub fn rerun_if_patched(&self, pkg_name: &str) {
+        let mut tracked = HashSet::new();
+        let pkg = self.pkgs.values().find(|pkg| pkg.name == pkg_name).unwrap();
+        self.track_patched_deps(&mut tracked, pkg);
+    }
+
+    /// Recursively walk the tree of dependencies of the given `root` package, making sure
+    /// to track all potentially modified, locally patched dependencies.
+    fn track_patched_deps(&self, tracked: &mut HashSet<PackageId>, root: &Package) {
+        for dep_pkg in root
+            .dependencies
+            .iter()
+            // NOTE: We'd like to just use `dep.source`/`dep.path`, unfortunately they do not
+            // account for crate patches at this level, so we build our own little index
+            // and use that instead.
+            .filter_map(|dep| self.pkgs.get(dep.name.as_str()))
+        {
+            let is_in_workspace = self.pkgs_workspace.contains_key(dep_pkg.name.as_str());
+            let exists_on_local_disk = dep_pkg.source.is_none();
+            if !is_in_workspace && exists_on_local_disk {
+                let mut dep_path = dep_pkg.manifest_path.clone();
+                dep_path.pop();
+
+                rerun_if_changed(dep_path.join("Cargo.toml").as_ref());
+                rerun_if_changed(dep_path.join("**/*.rs").as_ref());
+            }
+
+            if tracked.insert(dep_pkg.id.clone()) {
+                self.track_patched_deps(tracked, dep_pkg);
+            }
         }
     }
 }
+
+// ---
 
 fn main() {
     // Rebuild the web-viewer WASM,
     // because the web_server library bundles it with `include_bytes!`
 
+    let metadata = MetadataCommand::new()
+        .features(CargoOpt::AllFeatures)
+        .exec()
+        .unwrap();
+
     rerun_if_changed("../../web_viewer/favicon.ico");
     rerun_if_changed("../../web_viewer/index.html");
     rerun_if_changed("../../web_viewer/sw.js");
 
-    rerun_if_patched("re_viewer");
+    let pkgs = Packages::from_metadata(&metadata);
+    pkgs.rerun_if_patched("re_viewer");
 
     if std::env::var("CARGO_FEATURE___CI").is_ok() {
         // This saves a lot of CI time.
