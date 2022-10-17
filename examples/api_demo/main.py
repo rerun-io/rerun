cc24d251c0949bf75436c1345a44a90f9f6c468d
Jeremy Leibs <jeremy@rerun.io>
2022-10-18 01:49:06 +0200

    Use the path 'labels' instead of 'seg' in the example

--- examples/api_demo/main.py
+++ examples/api_demo/main.py
@@ -46,20 +46,20 @@ def run_segmentation(args: argparse.Namespace) -> None:
     segmentation_img[10:20, 30:50] = 13
     segmentation_img[80:100, 60:80] = 42
     segmentation_img[20:50, 90:110] = 99
-    rerun.log_image("img", segmentation_img, legend="seg")
+    rerun.log_image("img", segmentation_img, legend="labels")
 
     # Log an initial segmentation map with arbitrary colors
-    rerun.log_segmentation_map("seg", {13: "label1", 42: "label2", 99: "label3"})
+    rerun.log_segmentation_map("labels", {13: "label1", 42: "label2", 99: "label3"})
 
     # Log an updated segmentation map with specific colors
     rerun.set_time_seconds("sim_time", 2)
     rerun.log_segmentation_map(
-        "seg", {13: ("label1", (255, 0, 0)), 42: ("label2", (0, 255, 0)), 99: ("label3", (0, 0, 255))}
+        "labels", {13: ("label1", (255, 0, 0)), 42: ("label2", (0, 255, 0)), 99: ("label3", (0, 0, 255))}
     )
 
     # Log with a mixture of set and unset colors / labels
     rerun.set_time_seconds("sim_time", 3)
-    rerun.log_segmentation_map("seg", {13: Mapping(color=(255, 0, 0)), 42: ("label2", (0, 255, 0)), 99: "label3"})
+    rerun.log_segmentation_map("labels", {13: Mapping(color=(255, 0, 0)), 42: ("label2", (0, 255, 0)), 99: "label3"})
 
 
 def main() -> None:
