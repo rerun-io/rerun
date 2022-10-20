bda259b78abd3397196f8060ff562c09bc53de37
Clement Rey <cr.rey.clement@gmail.com>
2022-10-20 11:47:44 +0200

    standalone cross-platform example for re_renderer (#209)
    * standalone cross-platform example for re_renderer
    
    * moving re_renderer examples over there
    
    * easily run examples on the web
    
    * moving run_wasm stuff outta crates/
    
    * some more css for run_wasm
    
    * good enough for now
    
    * auto-open browser tab
    
    * cleanin up
    
    * whats going on here?
    
    * matching new APIs from main
    
    * fixing bytemuck gate
    
    * setting up a cam
    
    * excluse run_wasm from deny check
    
    * smarter tab opener
    
    * missed one
    
    * minor clarification
    
    * addressing PR comments
    
    * this too
    
    Co-authored-by: Emil Ernerfeldt <emil@rerun.io>

---
+++ run_wasm/README.md
@@ -0,0 +1,11 @@
+# run-wasm
+
+This provides an [`xtask`](https://github.com/matklad/cargo-xtask) that makes it trivial to run web-based examples.
+
+It relies on [`cargo-run-wasm`](https://github.com/rukai/cargo-run-wasm) to run the `wasm-bindgen` machinery, generate HTML files and all of that good stuff.
+This is a temporary solution while we're in the process of building our own `xtask` tools.
+
+You can try it e.g. with the standalone re_renderer demo:
+```
+cargo run-wasm --example renderer_standalone
+```
---
+++ run_wasm/src/main.rs
@@ -0,0 +1,127 @@
+//! Intended to be used as an xtask in order to make it trivial to run web-based examples.
+//!
+//! This is a temporary solution while we're in the process of building our own xtask tools.
+
+use std::{net::ToSocketAddrs, time::Duration};
+
+fn main() {
+    // TODO(cmc): Why is this not taking the full screen?
+    const CSS: &str = r#"
+        html {
+            /* Remove touch delay: */
+            touch-action: manipulation;
+        }
+
+        body {
+            /* Light mode background color for what is not covered by the egui canvas,
+            or where the egui canvas is translucent. */
+            background: #909090;
+        }
+
+        @media (prefers-color-scheme: dark) {
+            body {
+                /* Dark mode background color for what is not covered by the egui canvas,
+                or where the egui canvas is translucent. */
+                background: #404040;
+            }
+        }
+
+        /* Allow canvas to fill entire web page: */
+        html,
+        body {
+            overflow: hidden;
+            margin: 0 !important;
+            padding: 0 !important;
+            height: 100%;
+            width: 100%;
+        }
+
+        /* Position canvas in center-top: */
+        canvas {
+            margin-right: auto;
+            margin-left: auto;
+            display: block;
+            position: absolute;
+            top: 0%;
+            left: 50%;
+            transform: translate(-50%, 0%);
+        }
+
+        .centered {
+            margin-right: auto;
+            margin-left: auto;
+            display: block;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: #f0f0f0;
+            font-size: 24px;
+            font-family: Ubuntu-Light, Helvetica, sans-serif;
+            text-align: center;
+        }
+
+        /* ---------------------------------------------- */
+        /* Loading animation from https://loading.io/css/ */
+        .lds-dual-ring {
+            display: inline-block;
+            width: 24px;
+            height: 24px;
+        }
+
+        .lds-dual-ring:after {
+            content: " ";
+            display: block;
+            width: 24px;
+            height: 24px;
+            margin: 0px;
+            border-radius: 50%;
+            border: 3px solid #fff;
+            border-color: #fff transparent #fff transparent;
+            animation: lds-dual-ring 1.2s linear infinite;
+        }
+
+        @keyframes lds-dual-ring {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    "#;
+
+    use pico_args::Arguments;
+    let mut args = Arguments::from_env();
+    let host: Option<String> = args.opt_value_from_str("--host").unwrap();
+    let port: Option<String> = args.opt_value_from_str("--port").unwrap();
+    let host = host.as_deref().unwrap_or("localhost");
+    let port = port.as_deref().unwrap_or("8000");
+
+    std::thread::spawn(|| {
+        cargo_run_wasm::run_wasm_with_css(CSS);
+    });
+
+    // Wait for the server to be up before opening a browser tab.
+    let addr = format!("{host}:{port}")
+        .to_socket_addrs()
+        .unwrap()
+        .next()
+        .unwrap();
+    loop {
+        // TODO(cmc): this will make the webserver embedded within cargo-run-wasm complain
+        // a bit but eh... that's only temporary.
+        if std::net::TcpStream::connect(addr).is_ok() {
+            break;
+        }
+        std::thread::sleep(Duration::from_millis(200));
+    }
+
+    // Open browser tab.
+    let viewer_url = format!("http://{host}:{port}",);
+    webbrowser::open(&viewer_url).ok();
+    println!("Opening browser at {viewer_url}");
+
+    std::thread::sleep(Duration::from_secs(u64::MAX));
+}
