4517626829a5dc3bdfed115da72829359ab40743
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-10-22 15:41:02 +0200

    Remember what recording the experimental viewport was associated with

--- crates/re_viewer/src/app.rs
+++ crates/re_viewer/src/app.rs
@@ -221,6 +221,9 @@ impl eframe::App for App {
             self.state
                 .viewport_panel
                 .retain(|recording_id, _| self.log_dbs.contains_key(recording_id));
+            self.state
+                .experimental_viewport_panel
+                .retain(|recording_id, _| self.log_dbs.contains_key(recording_id));
         }
 
         file_saver_progress_ui(egui_ctx, self); // toasts for background file saver
@@ -357,7 +360,8 @@ struct AppState {
     /// Configuration for the current recording (found in [`LogDb`]).
     recording_configs: IntMap<RecordingId, RecordingConfig>,
 
-    experimental_viewport_panel: crate::ui::experimental_viewport_panel::ExperimentalViewportPanel,
+    experimental_viewport_panel:
+        IntMap<RecordingId, crate::ui::experimental_viewport_panel::ExperimentalViewportPanel>,
     viewport_panel: IntMap<RecordingId, crate::viewport_panel::ViewportPanel>,
 
     panel_selection: PanelSelection,
@@ -421,7 +425,10 @@ impl AppState {
                 .entry(*selected_recording_id)
                 .or_default()
                 .ui(&mut ctx, ui),
-            PanelSelection::ExperimentalViewport => experimental_viewport_panel.ui(&mut ctx, ui),
+            PanelSelection::ExperimentalViewport => experimental_viewport_panel
+                .entry(*selected_recording_id)
+                .or_default()
+                .ui(&mut ctx, ui),
             PanelSelection::EventLog => event_log_view.ui(&mut ctx, ui),
         });
 
