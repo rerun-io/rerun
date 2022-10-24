83fc72cf76a6c7d3929c8e7aff18b224b3189515
Jeremy Leibs <jeremy@rerun.io>
2022-10-24 17:30:27 +0200

    Convert from lazy_static -> once_cell

--- crates/re_data_store/src/batch.rs
+++ crates/re_data_store/src/batch.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
 use itertools::Either;
-use lazy_static::lazy_static;
 use nohash_hasher::IntMap;
+use once_cell::sync::OnceCell;
 
 use re_log_types::{Index, IndexHash};
 
@@ -79,11 +79,11 @@ impl<T: Clone> Batch<T> {
     pub fn new_sequential(data: &[T]) -> Result<Self, BadBatchError> {
         crate::profile_function!(std::any::type_name::<T>());
 
-        if data.len() > *MAX_SEQUENTIAL_BATCH {
-            re_log::warn!("Could not process sequential batch of len {}. Max supported sequential batch size: {}", data.len(), *MAX_SEQUENTIAL_BATCH);
+        if data.len() > MAX_SEQUENTIAL_BATCH {
+            re_log::warn!("Could not process sequential batch of len {}. Max supported sequential batch size: {}", data.len(), MAX_SEQUENTIAL_BATCH);
         }
 
-        let vec = data[..std::cmp::min(data.len(), *MAX_SEQUENTIAL_BATCH)].to_vec();
+        let vec = data[..std::cmp::min(data.len(), MAX_SEQUENTIAL_BATCH)].to_vec();
 
         Ok(Self {
             store: BatchStorage::SequentialBatch(vec),
@@ -91,27 +91,51 @@ impl<T: Clone> Batch<T> {
     }
 }
 
-// TODO(jleibs) This is hacky but we simply pre-allocate the index hashings and
-// reverse maps arbitrarily up to a million. Ideally we would auto-grow these
-// entities to match the largest dense set we need to work with but that gets
-// into some complex lifetime questions.
-lazy_static! {
-    static ref MAX_SEQUENTIAL_BATCH: usize = 1000000;
-    static ref HASHED_INDEX: Vec<IndexHash> = (0..*MAX_SEQUENTIAL_BATCH)
-        .map(|i| IndexHash::hash(&Index::Sequence(i as u64)))
-        .collect();
-    static ref REVERSE_INDEX_HASH_MAP: IntMap<IndexHash, usize> = HASHED_INDEX
-        .iter()
-        .enumerate()
-        .map(|(index, hash)| (*hash, index))
-        .collect();
+static MAX_SEQUENTIAL_BATCH: usize = 1_000_000;
+struct SequentialIndexHash {
+    hashed_indices: Vec<IndexHash>,
+    reverse_index_hash_map: IntMap<IndexHash, usize>,
+}
+
+impl SequentialIndexHash {
+    fn global() -> &'static SequentialIndexHash {
+        static INSTANCE: OnceCell<SequentialIndexHash> = OnceCell::new();
+        INSTANCE.get_or_init(|| {
+            let hashed_indices: Vec<IndexHash> = (0..MAX_SEQUENTIAL_BATCH)
+                .map(|i| IndexHash::hash(&Index::Sequence(i as u64)))
+                .collect();
+
+            let reverse_index_hash_map = hashed_indices
+                .iter()
+                .enumerate()
+                .map(|(index, hash)| (*hash, index))
+                .collect();
+
+            SequentialIndexHash {
+                hashed_indices,
+                reverse_index_hash_map,
+            }
+        })
+    }
+
+    fn hashes_up_to(len: usize) -> &'static [IndexHash] {
+        let global = Self::global();
+        &global.hashed_indices[..len]
+    }
+
+    fn reverse_hash(index: &IndexHash) -> Option<&usize> {
+        let global = Self::global();
+        global.reverse_index_hash_map.get(index)
+    }
 }
 
 impl<T> Batch<T> {
     #[inline]
     pub fn get(&self, index: &IndexHash) -> Option<&T> {
         match &self.store {
-            BatchStorage::SequentialBatch(vec) => vec.get(*REVERSE_INDEX_HASH_MAP.get(index)?),
+            BatchStorage::SequentialBatch(vec) => {
+                vec.get(*SequentialIndexHash::reverse_hash(index)?)
+            }
             BatchStorage::IndexedBatch(map) => map.get(index),
         }
     }
@@ -144,9 +168,10 @@ impl<T> Batch<T> {
     #[inline]
     pub fn iter(&self) -> impl ExactSizeIterator<Item = (&IndexHash, &T)> {
         match &self.store {
-            BatchStorage::SequentialBatch(vec) => {
-                Either::Left(std::iter::zip(&HASHED_INDEX[..vec.len()], vec))
-            }
+            BatchStorage::SequentialBatch(vec) => Either::Left(std::iter::zip(
+                SequentialIndexHash::hashes_up_to(vec.len()),
+                vec,
+            )),
             BatchStorage::IndexedBatch(map) => Either::Right(map.iter()),
         }
     }
