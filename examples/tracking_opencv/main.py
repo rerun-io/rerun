41aa4df398816b5fa6e712b4f951611e8b422c7e
Nikolaus West <niko@grapic.co>
2022-10-27 15:24:36 +0200

    Detections and tracking in separate spaces

--- examples/tracking_opencv/main.py
+++ examples/tracking_opencv/main.py
@@ -13,6 +13,8 @@ import rerun_sdk as rerun
 DATASET_DIR: Final = Path(os.path.dirname(__file__)) / "dataset"
 CACHE_DIR: Final = Path(os.path.dirname(__file__)) / "cache"
 
+DOWNSCALE_FACTOR = 2
+
 os.environ["TRANSFORMERS_CACHE"] = str(CACHE_DIR.absolute())
 from transformers import DetrFeatureExtractor, DetrForObjectDetection
 
@@ -29,7 +31,6 @@ cap = cv.VideoCapture(video_path)
 frame_idx = 0
 
 tracker = None
-detection_is_visible = False
 while cap.isOpened():
     ret, bgr = cap.read()
     rerun.set_time_sequence("frame", frame_idx)
@@ -42,47 +43,48 @@ while cap.isOpened():
     rerun.log_image("image", rgb)
 
     if tracker is None or frame_idx % 40 == 0:
-        pil_im = Image.fromarray(rgb)
+        height, width, _ = rgb.shape
+        small_size = (int(width / DOWNSCALE_FACTOR), int(height / DOWNSCALE_FACTOR))
+        rgb_small = cv.resize(rgb, small_size)
 
-        inputs = feature_extractor(images=pil_im, return_tensors="pt")
+        rerun.log_image("image/downscaled", rgb_small)
+        rerun.log_unknown_transform("image/downscaled")  # Note: Haven't implemented 2D transforms yet.
+
+        pil_im_smal = Image.fromarray(rgb_small)
+
+        inputs = feature_extractor(images=pil_im_smal, return_tensors="pt")
         outputs = model(**inputs)
 
-        target_sizes = torch.tensor([pil_im.size[::-1]])
+        target_sizes = torch.tensor([pil_im_smal.size[::-1]])
         results = feature_extractor.post_process_object_detection(outputs, target_sizes=target_sizes)[0]
 
         labels = results["labels"].detach().cpu().numpy()
-        boxes = results["boxes"].detach().cpu().numpy()
+        boxes_small = results["boxes"].detach().cpu().numpy()
 
-        car_boxes = boxes[labels == car_id, :]
-        num_cars = car_boxes.shape[0]
+        car_boxes_small = boxes_small[labels == car_id, :]
+        num_cars = car_boxes_small.shape[0]
         labels = ["car" for _ in range(num_cars)]
-        # TODO(Niko): Remove workaround for PRO-213 and PRO-214
-        # rerun.log_rect("image/detections", car_boxes, rect_format=rerun.RectFormat.XYXY, label=labels)
+        rerun.log_rects(
+            "image/downscaled/detections", car_boxes_small, rect_format=rerun.RectFormat.XYXY, labels=labels
+        )
 
         if num_cars > 0:
-            top_car_bbox = car_boxes[0, :]
-            # TODO(Niko): Remove workaround for PRO-213 and PRO-214
-            rerun.log_rect("image/detections", top_car_bbox, rect_format=rerun.RectFormat.XYXY, label="car")
-            rerun.set_visible("image/detections", True)
-            detection_is_visible = True
-            x_min, y_min, x_max, y_max = top_car_bbox.tolist()
-            bbox_xywh = [x_min, y_min, x_max - x_min, y_max - y_min]
-            bbox_xywh = [int(val) for val in bbox_xywh]
+            top_car_bbox_small = car_boxes_small[0, :]
+
+            x_min, y_min, x_max, y_max = top_car_bbox_small.tolist()
+            bbox_small_xywh = [x_min, y_min, x_max - x_min, y_max - y_min]
+
+            bbox_xywh = [int(val * DOWNSCALE_FACTOR) for val in bbox_small_xywh]
             tracker = cv.TrackerCSRT_create()
             tracker.init(bgr, bbox_xywh)
 
-            rerun.log_rect("image/tracked", bbox_xywh, rect_format=rerun.RectFormat.XYWH, label="tracked_car")
+            rerun.log_rect("image/tracked", bbox_xywh, rect_format=rerun.RectFormat.XYWH, label="car")
     else:
-        if detection_is_visible:
-            rerun.set_visible("image/detections", False)
-            detection_is_visible = False
-
         success, bbox_xywh = tracker.update(bgr)
 
         if success:
-            rerun.log_rect("image/tracked", bbox_xywh, rect_format=rerun.RectFormat.XYWH, label="tracked_car")
+            rerun.log_rect("image/tracked", bbox_xywh, rect_format=rerun.RectFormat.XYWH, label="car")
         else:
-            tracking = False
-            rerun.set_visible("image/tracked", False)
+            tracker = None
 
     frame_idx += 1
