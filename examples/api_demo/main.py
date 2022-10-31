f2058abb3883e9561cb20b7a2f705359c7ada862
Jeremy Leibs <jeremy@rerun.io>
2022-10-31 18:50:48 +0100

    Add new api example including logging and clearing rects

--- examples/api_demo/main.py
+++ examples/api_demo/main.py
@@ -14,7 +14,7 @@ from typing import Any, Final
 
 import numpy as np
 import rerun_sdk as rerun
-from rerun_sdk import ClassDescription
+from rerun_sdk import ClassDescription, RectFormat
 
 
 def run_misc() -> None:
@@ -42,22 +42,22 @@ def run_segmentation() -> None:
     segmentation_img[10:20, 30:50] = 13
     segmentation_img[80:100, 60:80] = 42
     segmentation_img[20:50, 90:110] = 99
-    rerun.log_segmentation_image("img", segmentation_img, "labels")
+    rerun.log_segmentation_image("img", segmentation_img, "img/labels")
 
     # Log an initial segmentation map with arbitrary colors
     rerun.set_time_seconds("sim_time", 2)
-    rerun.log_class_descriptions("labels", [(13, "label1"), (42, "label2"), (99, "label3")])
+    rerun.log_class_descriptions("img/labels", [(13, "label1"), (42, "label2"), (99, "label3")])
 
     # Log an updated segmentation map with specific colors
     rerun.set_time_seconds("sim_time", 3)
     rerun.log_class_descriptions(
-        "labels", [(13, "label1", (255, 0, 0)), (42, "label2", (0, 255, 0)), (99, "label3", (0, 0, 255))]
+        "img/labels", [(13, "label1", (255, 0, 0)), (42, "label2", (0, 255, 0)), (99, "label3", (0, 0, 255))]
     )
 
     # Log with a mixture of set and unset colors / labels
     rerun.set_time_seconds("sim_time", 4)
     rerun.log_class_descriptions(
-        "labels",
+        "img/labels",
         [ClassDescription(13, color=(255, 0, 0)), (42, "label2", (0, 255, 0)), ClassDescription(99, label="label3")],
     )
 
@@ -76,12 +76,33 @@ def run_set_visible() -> None:
     rerun.set_visible("rect/1", True)
 
 
+def run_rects() -> None:
+    import random
+
+    rerun.set_time_seconds("sim_time", 1)
+
+    # Add an image
+    img = np.zeros([1024, 1024, 3], dtype="uint8")
+    img[:, :] = (128, 128, 128)
+    rerun.log_image("img", img)
+
+    # 20 random rectangles
+    rerun.set_time_seconds("sim_time", 2)
+    rects_x = [random.randrange(0, 1024) for _ in range(20)]
+    rects_y = [random.randrange(0, 1024) for _ in range(20)]
+    rects_w = [random.randrange(0, 1024 - x + 1) for x in rects_x]
+    rects_h = [random.randrange(0, 1024 - y + 1) for y in rects_y]
+    rects = [(x, y, w, h) for x, y, w, h in zip(rects_x, rects_y, rects_w, rects_h)]
+    colors = np.array([[random.randrange(255) for _ in range(3)] for _ in range(20)])
+    rerun.log_rects("img/rects", rects, colors=colors, rect_format=RectFormat.XYWH)
+
+    # Clear the rectangles by logging an empty set
+    rerun.set_time_seconds("sim_time", 3)
+    rerun.log_rects("img/rects", [])
+
+
 def main() -> None:
-    demos = {
-        "misc": run_misc,
-        "segmentation": run_segmentation,
-        "set_visible": run_set_visible,
-    }
+    demos = {"misc": run_misc, "segmentation": run_segmentation, "set_visible": run_set_visible, "rects": run_rects}
 
     parser = argparse.ArgumentParser(description="Logs rich data using the Rerun SDK.")
     parser.add_argument(
