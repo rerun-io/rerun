6fb2f627babed1137b328f6f4301eee902d1b2ec
Clement Rey <cr.rey.clement@gmail.com>
2022-11-09 10:47:11 +0100

    tough questions

--- crates/re_viewer/src/ui/view_plot/ui.rs
+++ crates/re_viewer/src/ui/view_plot/ui.rs
@@ -1,5 +1,5 @@
 use crate::ViewerContext;
-use egui::plot::{Legend, Line, Plot, PlotPoints};
+use egui::plot::{Legend, Line, Plot, PlotPoints, Points};
 use egui::Color32;
 use re_log_types::{LogMsg, TimePoint};
 
@@ -27,6 +27,35 @@ pub(crate) fn view_plot(
 
     // TODO:
     // - x legend (using timeline name)
+    // - y legend (using obj_path or label if available)
+    // - plug in Legend/ClassDescr?
+    // - position marker
+    // - log_scalars doesn't make sense!
+    // - does the time thing behave correctly here? what about multi-timeline setups?
+    // - document how spaces work for plots
+    //
+    // A scalar _literally_ cannot be timeless: we wouldn't even have an x value to work with!
+    //
+    // - what does the timequery look like?
+    //   we're always timed and always sticky,
+    // - what happens when the scalar changes color?
+    // - what happens when the scalar changes label?
+    //
+    // what about stuff that has nothing to do with points, e.g. the kind of plot, or whether
+    // we want a reference hline/vline to appear?
+    // - Sometimes it's nice to still set it at the scalar-level, so that things can evolve
+    //   over time.
+    // - on the other hand, do you really want each scalar to reassert the fact that this is
+    //   a bar chart or whatever? (whether from a logic perspective, or a storage perspective)
+    //
+    // what happens when you want to set one color on the scalar itself, and another for line
+    // that goes through all of these scalars?
+    //
+    // Shall these things use some kind of metadata API that applies to a whole obj_path,
+    // similar to class descriptions? E.g. we expose some kind of `log_plot_config`?
+    // Does this tie in with Jeremy's work on annotation contexts?
+    //
+    // Or should it all be handled by blueprints somehow..? Or both?!
 
     let lines = scene
         .plots
@@ -34,7 +63,8 @@ pub(crate) fn view_plot(
         .map(|(obj_path, plot)| {
             let points = plot.iter().map(|s| [s.time as _, s.value]).collect();
             let points = PlotPoints::new(points);
-            let line = Line::new(points).name(obj_path);
+            // let line = Line::new(points).name(obj_path);
+            let line = Points::new(points).radius(2.0);
             (obj_path, line)
         })
         .collect::<Vec<_>>();
@@ -43,7 +73,8 @@ pub(crate) fn view_plot(
         .legend(Legend::default())
         .show(ui, move |plot_ui| {
             for (obj_path, line) in lines {
-                plot_ui.line(line)
+                // plot_ui.line(line)
+                plot_ui.points(line)
             }
         })
         .response
