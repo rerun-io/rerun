b6cb0a1e898439fc9e343deccf715a42f20d2df0
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-11-10 14:33:42 +0100

    Time panel: tweak what is open by default

--- crates/re_viewer/src/ui/time_panel.rs
+++ crates/re_viewer/src/ui/time_panel.rs
@@ -250,8 +250,7 @@ impl TimePanel {
         };
 
         let collapsing_header_id = ui.make_persistent_id(&tree.path);
-        let default_open = tree.path.len() <= 1 && tree.num_children_and_fields() <= 10
-            || tree.num_children_and_fields() <= 2;
+        let default_open = tree.path.len() <= 1 && !tree.is_leaf();
         let (header_response, _, body_returned) =
             egui::collapsing_header::CollapsingState::load_with_default_open(
                 ui.ctx(),
