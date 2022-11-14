02609f62fe7a74620e89630547e4b616ecb228e6
Emil Ernerfeldt <emil@rerun.io>
2022-11-14 21:39:27 +0100

    Add missing dependencies to setup.sh (#297)
    * Clean up setup.sh
    
    * Add missing dependencies to setup.sh
    
    * Clarify setup step

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,8 +7,30 @@ cd "$script_path/.."
 set -x
 
 # eframe dependencies needed on run on Linux and Fedora Rawhide:
-if [ -x "$(command -v apt-get)" ]; then sudo apt-get -y install libgtk-3-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev patchelf
-elif [ -x "$(command -v dnf)" ];   then sudo dnf install clang clang-devel clang-tools-extra speech-dispatcher-devel libxkbcommon-devel pkg-config openssl-devel libxcb-devel
+if [ -x "$(command -v apt-get)" ]; then
+    sudo apt-get -y install \
+        libatk-bridge2.0 \
+        libfontconfig1-dev \
+        libfreetype6-dev \
+        libglib2.0-dev \
+        libgtk-3-dev \
+        libspeechd-dev \
+        libssl-dev \
+        libxcb-render0-dev \
+        libxcb-shape0-dev \
+        libxcb-xfixes0-dev \
+        libxkbcommon-dev \
+        patchelf
+elif [ -x "$(command -v dnf)" ];   then
+    sudo dnf install \
+        clang \
+        clang-devel \
+        clang-tools-extra \
+        libxcb-devel \
+        libxkbcommon-devel \
+        openssl-devel \
+        pkg-config \
+        speech-dispatcher-devel
 fi
 
 # Needed to compile and check the code:
