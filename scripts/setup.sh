5255bbfe30d54e1b5756c4f73f1decfcbe5b648b
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-11-14 20:59:59 +0100

    Clean up setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,8 +7,26 @@ cd "$script_path/.."
 set -x
 
 # eframe dependencies needed on run on Linux and Fedora Rawhide:
-if [ -x "$(command -v apt-get)" ]; then sudo apt-get -y install libgtk-3-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev patchelf
-elif [ -x "$(command -v dnf)" ];   then sudo dnf install clang clang-devel clang-tools-extra speech-dispatcher-devel libxkbcommon-devel pkg-config openssl-devel libxcb-devel
+if [ -x "$(command -v apt-get)" ]; then
+    sudo apt-get -y install \
+        libgtk-3-dev \
+        libspeechd-dev \
+        libssl-dev \
+        libxcb-render0-dev \
+        libxcb-shape0-dev \
+        libxcb-xfixes0-dev \
+        libxkbcommon-dev \
+        patchelf
+elif [ -x "$(command -v dnf)" ];   then
+    sudo dnf install \
+        clang \
+        clang-devel \
+        clang-tools-extra \
+        libxcb-devel \
+        libxkbcommon-devel \
+        openssl-devel \
+        pkg-config \
+        speech-dispatcher-devel
 fi
 
 # Needed to compile and check the code:
