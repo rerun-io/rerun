1813ef01d0ecc00d8f4a119d0200ebda7882ae1c
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-11-14 21:01:46 +0100

    Add missing dependencies to setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,6 +9,10 @@ set -x
 # eframe dependencies needed on run on Linux and Fedora Rawhide:
 if [ -x "$(command -v apt-get)" ]; then
     sudo apt-get -y install \
+        libatk-bridge2.0 \
+        libfontconfig1-dev \
+        libfreetype6-dev \
+        libglib2.0-dev \
         libgtk-3-dev \
         libspeechd-dev \
         libssl-dev \
