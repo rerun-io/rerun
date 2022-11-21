29c345f539c5a165fe8678990f7cf8e7eacd1ae6
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-11-21 13:55:09 +0100

    Move MemoryHistory to re_memory

--- crates/re_memory/src/lib.rs
+++ crates/re_memory/src/lib.rs
@@ -1,8 +1,10 @@
 //! Crate for tracking memory use.
+mod memory_history;
 mod memory_use;
 mod tracking_allocator;
 
 pub use {
+    memory_history::MemoryHistory,
     memory_use::MemoryUse,
     tracking_allocator::{global_allocs_and_bytes, TrackingAllocator},
 };
--- crates/re_viewer/src/ui/memory_panel.rs
+++ crates/re_viewer/src/ui/memory_panel.rs
@@ -1,4 +1,4 @@
-use re_memory::MemoryUse;
+use re_memory::{MemoryHistory, MemoryUse};
 
 // ----------------------------------------------------------------------------
 
@@ -58,58 +58,6 @@ impl MemoryLimit {
         }
 
         false
-    }
-}
-
-// ----------------------------------------------------------------------------
-
-/// Returns monotonically increasing time in seconds.
-#[inline]
-fn now_sec() -> f64 {
-    use instant::Instant;
-    use once_cell::sync::Lazy;
-
-    static START_INSTANT: Lazy<Instant> = Lazy::new(Instant::now);
-    START_INSTANT.elapsed().as_nanos() as f64 / 1e9
-}
-
-// ----------------------------------------------------------------------------
-
-/// Tracks memory use over time.
-struct MemoryHistory {
-    pub gross: egui::util::History<i64>,
-    pub net: egui::util::History<i64>,
-}
-
-impl Default for MemoryHistory {
-    fn default() -> Self {
-        let max_elems = 128 * 1024;
-        let max_seconds = f32::INFINITY;
-        Self {
-            gross: egui::util::History::new(0..max_elems, max_seconds),
-            net: egui::util::History::new(0..max_elems, max_seconds),
-        }
-    }
-}
-
-impl MemoryHistory {
-    pub fn is_empty(&self) -> bool {
-        let Self { gross, net } = self;
-        gross.is_empty() && net.is_empty()
-    }
-
-    /// Add data to history
-    pub fn capture(&mut self) {
-        let mem_use = MemoryUse::capture();
-        if mem_use.gross.is_some() || mem_use.net.is_some() {
-            let now = now_sec();
-            if let Some(gross) = mem_use.gross {
-                self.gross.add(now, gross);
-            }
-            if let Some(net) = mem_use.net {
-                self.net.add(now, net);
-            }
-        }
     }
 }
 
