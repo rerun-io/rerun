ccdffe4bb0567c40002a2bb0a85d9c6b530ef4d5
Andreas Reich <andreas@rerun.io>
2022-11-21 16:32:41 +0100

    python formatting

--- examples/api_demo/main.py
+++ examples/api_demo/main.py
@@ -97,12 +97,17 @@ def run_points_3d() -> None:
     rerun.set_time_seconds("sim_time", 1)
     rerun.log_point("3d_points/single_point_unlabeled", np.array([10.0, 0.0, 0.0]))
     rerun.log_point("3d_points/single_point_labeled", np.array([0.0, 0.0, 0.0]), label="labeled point")
-    rerun.log_points("3d_points/spiral_small",
-        np.array([[math.sin(i*0.2) * 5, math.cos(i*0.2) * 5 + 10.0, i * 4.0 - 5.0] for i in range(9)]),
-        labels=[str(i) for i in range(9)])
-    rerun.log_points("3d_points/spiral_big",
-        np.array([[math.sin(i*0.2) * 5, math.cos(i*0.2) * 5 - 10.0, i * 0.4 - 5.0] for i in range(100)]),
-        labels=[str(i) for i in range(100)])
+    rerun.log_points(
+        "3d_points/spiral_small",
+        np.array([[math.sin(i * 0.2) * 5, math.cos(i * 0.2) * 5 + 10.0, i * 4.0 - 5.0] for i in range(9)]),
+        labels=[str(i) for i in range(9)],
+    )
+    rerun.log_points(
+        "3d_points/spiral_big",
+        np.array([[math.sin(i * 0.2) * 5, math.cos(i * 0.2) * 5 - 10.0, i * 0.4 - 5.0] for i in range(100)]),
+        labels=[str(i) for i in range(100)],
+    )
+
 
 def run_rects() -> None:
     import random
