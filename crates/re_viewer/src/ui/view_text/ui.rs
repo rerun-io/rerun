372beef7af603b1962d1b313a9af14f23790f6fe
Clement Rey <cr.rey.clement@gmail.com>
2022-11-22 12:43:04 +0100

    i.. like it? there, said it

--- crates/re_viewer/src/ui/view_text/ui.rs
+++ crates/re_viewer/src/ui/view_text/ui.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeMap;
 
-use egui::{Checkbox, Color32, NumExt as _, RichText};
+use ahash::HashMap;
+use egui::{Checkbox, Color32, Label, NumExt as _, Rect, RichText, TextStyle};
 
 use re_data_store::{ObjPath, Timeline};
 use re_log_types::{LogMsg, TimePoint};
@@ -26,7 +27,6 @@ pub struct ViewTextState {
 #[derive(Debug, Clone, Default, serde::Deserialize, serde::Serialize)]
 pub struct ViewTextFilters {
     // TODO:
-    // - dealing with emptiness
     // - heuristics
     //
     // TODO:
@@ -59,7 +59,6 @@ pub struct ViewTextFilters {
     pub show_timelines: BTreeMap<Timeline, bool>,
     pub show_obj_path: bool,
     pub show_log_level: bool,
-    pub show_body: bool,
 
     // Column filters
     pub filter_obj_paths: BTreeMap<ObjPath, bool>,
@@ -71,15 +70,17 @@ impl ViewTextFilters {
         crate::profile_function!();
 
         for timeline in ctx.log_db.time_points.0.keys() {
-            self.show_timelines.entry(timeline.clone()).or_default();
+            self.show_timelines.entry(timeline.clone()).or_insert(true);
         }
 
         for obj_path in text_entries.iter().map(|te| &te.obj_path) {
-            self.filter_obj_paths.entry(obj_path.clone()).or_default();
+            self.filter_obj_paths
+                .entry(obj_path.clone())
+                .or_insert(true);
         }
 
         for level in text_entries.iter().filter_map(|te| te.level.as_ref()) {
-            self.filter_log_levels.entry(level.clone()).or_default();
+            self.filter_log_levels.entry(level.clone()).or_insert(true);
         }
     }
 
@@ -93,7 +94,6 @@ impl ViewTextFilters {
                     show_timelines,
                     show_obj_path,
                     show_log_level,
-                    show_body,
                     filter_obj_paths: _,
                     filter_log_levels: _,
                 } = self;
@@ -103,7 +103,6 @@ impl ViewTextFilters {
                 }
                 *show_obj_path = false;
                 *show_log_level = false;
-                *show_body = false;
             }
         });
 
@@ -114,7 +113,6 @@ impl ViewTextFilters {
         }
         ui.checkbox(&mut self.show_obj_path, "Object path");
         ui.checkbox(&mut self.show_log_level, "Log level");
-        ui.checkbox(&mut self.show_body, "Body");
 
         ui.add_space(4.0);
 
@@ -125,7 +123,6 @@ impl ViewTextFilters {
                     show_timelines: _,
                     show_obj_path: _,
                     show_log_level: _,
-                    show_body: _,
                     filter_obj_paths,
                     filter_log_levels,
                 } = self;
@@ -150,7 +147,12 @@ impl ViewTextFilters {
             }
         });
         for (obj_path, visible) in &mut self.filter_obj_paths {
-            ui.checkbox(visible, format!("'{obj_path}'"));
+            ui.horizontal(|ui| {
+                ui.checkbox(visible, "");
+                if ui.selectable_label(false, &obj_path.to_string()).clicked() {
+                    *visible = !*visible;
+                }
+            });
         }
 
         ui.add_space(2.0);
@@ -248,15 +250,84 @@ fn show_table(
     text_entries: &[TextEntry],
     scroll_to_row: Option<usize>,
 ) {
+    // TODO: auto-resize columns
+
+    const OBJ_PATH_HEADER: &str = "ObjPath";
+    const LOG_LEVEL_HEADER: &str = "Level";
+    const BODY_HEADER: &str = "Body";
+
+    const EXTRA_SPACE_FACTOR: f32 = 1.20;
+
+    let current_timeline = *ctx.rec_cfg.time_ctrl.timeline();
+    let current_time = ctx.rec_cfg.time_ctrl.time().map(|tr| tr.floor());
+
+    // Note: got to do all those size calculations _before_ the ui gets borrowed by the builder.
+
+    let renderer_width = |ui: &egui::Ui, text: String| {
+        let font_id = TextStyle::Body.resolve(ui.style());
+        ui.fonts()
+            .layout_delayed_color(text, font_id, f32::MAX)
+            .size()
+            .x
+    };
+
+    let timelines = state
+        .filters
+        .show_timelines
+        .iter()
+        .filter_map(|(timeline, visible)| visible.then_some(timeline))
+        .map(|timeline| (timeline, renderer_width(ui, timeline.name().to_string())))
+        .map(|(timeline, width)| {
+            let inner_width = text_entries
+                .first()
+                .and_then(|te| {
+                    get_time_point(ctx, te)
+                        .map(|tp| tp.0.get(timeline).copied())
+                        .flatten()
+                })
+                .map(|v| renderer_width(ui, timeline.typ().format(v)))
+                .unwrap_or(f32::MIN);
+            (timeline, f32::max(width, inner_width) * EXTRA_SPACE_FACTOR)
+        })
+        .collect::<Vec<_>>();
+
+    let obj_path_header_size = renderer_width(ui, OBJ_PATH_HEADER.to_string());
+    let obj_path_size = state
+        .filters
+        .filter_obj_paths
+        .keys() // all of them, visible or not!
+        .map(|obj_path| renderer_width(ui, obj_path.to_string()))
+        .chain(std::iter::once(obj_path_header_size))
+        .max_by(|a, b| a.total_cmp(&b))
+        .unwrap_or(50.0)
+        * EXTRA_SPACE_FACTOR;
+
+    let log_level_header_size = renderer_width(ui, LOG_LEVEL_HEADER.to_string());
+    let log_level_size = state
+        .filters
+        .filter_log_levels
+        .keys() // all of them, visible or not!
+        .map(|log_level| renderer_width(ui, log_level.to_string()))
+        .chain(std::iter::once(log_level_header_size))
+        .max_by(|a, b| a.total_cmp(&b))
+        .unwrap_or(50.0)
+        * EXTRA_SPACE_FACTOR;
+
+    let body_header_size = renderer_width(ui, BODY_HEADER.to_string());
+    let body_size = text_entries
+        .iter() // all of them, visible or not!
+        .map(|te| renderer_width(ui, te.body.clone())) // TODO: clone :/
+        .chain(std::iter::once(body_header_size))
+        .max_by(|a, b| a.total_cmp(&b))
+        .unwrap_or(50.0)
+        * EXTRA_SPACE_FACTOR;
+
     use egui_extras::Size;
     const ROW_HEIGHT: f32 = 18.0;
     const HEADER_HEIGHT: f32 = 20.0;
 
     let max_content_height = ui.available_height() - HEADER_HEIGHT;
     let item_spacing = ui.spacing().item_spacing;
-
-    let current_timeline = *ctx.rec_cfg.time_ctrl.timeline();
-    let current_time = ctx.rec_cfg.time_ctrl.time().map(|tr| tr.floor());
 
     let mut builder = egui_extras::TableBuilder::new(ui)
         .striped(true)
@@ -281,41 +352,33 @@ fn show_table(
 
     let mut current_time_y = None;
 
-    let timelines = state
-        .filters
-        .show_timelines
-        .iter()
-        .filter_map(|(timeline, visible)| visible.then_some(timeline))
-        .collect::<Vec<_>>();
-
-    // TODO(cmc): these column sizes have to be derived from the actual contents.
-    let mut builder = builder
-        .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-        .columns(
-            Size::initial(140.0).at_least(50.0), // timelines
-            timelines.len(),
-        ); // time(s)
-
-    if state.filters.show_obj_path {
-        builder = builder.column(Size::initial(120.0).at_least(50.0)); // path
+    let mut builder = builder.cell_layout(egui::Layout::left_to_right(egui::Align::Center));
+    {
+        // timeline(s)
+        for (_, size) in &timelines {
+            builder = builder.column(Size::initial(*size));
+        }
+        // object path
+        if state.filters.show_obj_path {
+            builder = builder.column(Size::initial(obj_path_size));
+        }
+        // log level
+        if state.filters.show_log_level {
+            builder = builder.column(Size::initial(log_level_size));
+        }
+        // body
+        builder = builder.column(Size::remainder().at_least(body_size));
     }
-    if state.filters.show_log_level {
-        builder = builder.column(Size::initial(50.0).at_least(50.0)); // level
-    }
-    if state.filters.show_body {
-        builder = builder.column(Size::remainder().at_least(200.0)); // body
-    }
-
     builder
         .header(HEADER_HEIGHT, |mut header| {
-            for timeline in &timelines {
+            for (timeline, _) in &timelines {
                 header.col(|ui| {
                     ctx.timeline_button(ui, timeline);
                 });
             }
             if state.filters.show_obj_path {
                 header.col(|ui| {
-                    ui.strong("Path");
+                    ui.strong(OBJ_PATH_HEADER);
                 });
             }
             if state.filters.show_log_level {
@@ -323,11 +386,9 @@ fn show_table(
                     ui.strong("Level");
                 });
             }
-            if state.filters.show_body {
-                header.col(|ui| {
-                    ui.strong("Body");
-                });
-            }
+            header.col(|ui| {
+                ui.strong("Body");
+            });
         })
         .body(|body| {
             body.rows(ROW_HEIGHT, text_entries.len(), |index, mut row| {
@@ -348,8 +409,8 @@ fn show_table(
                     return;
                 };
 
-                // time(s)
-                for timeline in &timelines {
+                // timeline(s)
+                for (timeline, _) in &timelines {
                     row.col(|ui| {
                         if let Some(value) = time_point.0.get(timeline).copied() {
                             if let Some(current_time) = current_time {
@@ -385,16 +446,14 @@ fn show_table(
                 }
 
                 // body
-                if state.filters.show_body {
-                    row.col(|ui| {
-                        if let Some(c) = text_entry.color {
-                            let color = Color32::from_rgba_unmultiplied(c[0], c[1], c[2], c[3]);
-                            ui.colored_label(color, &text_entry.body);
-                        } else {
-                            ui.label(&text_entry.body);
-                        }
-                    });
-                }
+                row.col(|ui| {
+                    if let Some(c) = text_entry.color {
+                        let color = Color32::from_rgba_unmultiplied(c[0], c[1], c[2], c[3]);
+                        ui.colored_label(color, &text_entry.body);
+                    } else {
+                        ui.label(&text_entry.body);
+                    }
+                });
             });
         });
 
