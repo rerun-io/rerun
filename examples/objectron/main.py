1749f493a314e2ccf0f533a4c5ac11726c56bb2f
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-11-24 15:32:05 +0100

    objectron: remind user to run --connect with --run-forever

--- examples/objectron/main.py
+++ examples/objectron/main.py
@@ -284,6 +284,9 @@ def main() -> None:
 
     args = parser.parse_args()
 
+    if args.run_forever:
+        assert args.connect, "If you run forever, you need to --connect"
+
     rerun.init("objectron")
 
     if args.connect:
