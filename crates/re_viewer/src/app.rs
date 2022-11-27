93bd325fd423f7fc328e80e015bf02c14fce9519
Emil Ernerfeldt <emil@rerun.io>
2022-11-27 22:49:36 +0100

    Show frame time in top panel (#366)
    * Fix cause of spurious wake-up
    
    * Show frame time in top panel
    
    * spelling, and remove "excluding rendering"
    
    Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>
    
    * cargo fmt
    
    Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>

--- crates/re_viewer/src/app.rs
+++ crates/re_viewer/src/app.rs
@@ -3,6 +3,7 @@ use std::any::Any;
 use ahash::HashMap;
 use egui_extras::RetainedImage;
 use egui_notify::Toasts;
+use instant::Instant;
 use itertools::Itertools as _;
 use nohash_hasher::IntMap;
 use poll_promise::Promise;
@@ -51,6 +52,9 @@ pub struct App {
     memory_panel_open: bool,
 
     latest_queue_interest: instant::Instant,
+
+    /// Measures how long a frame takes to paint
+    frame_time_history: egui::util::History<f32>,
 }
 
 impl App {
@@ -141,6 +145,8 @@ impl App {
             memory_panel_open: false,
 
             latest_queue_interest: instant::Instant::now(), // TODO(emilk): `Instant::MIN` when we have our own `Instant` that supports it.
+
+            frame_time_history: egui::util::History::new(1..100, 0.5),
         }
     }
 
@@ -240,6 +246,8 @@ impl eframe::App for App {
     }
 
     fn update(&mut self, egui_ctx: &egui::Context, frame: &mut eframe::Frame) {
+        let frame_start = Instant::now();
+
         self.memory_panel.update(); // do first, before doing too many allocations
 
         #[cfg(not(target_arch = "wasm32"))]
@@ -300,6 +308,9 @@ impl eframe::App for App {
 
         self.handle_dropping_files(egui_ctx);
         self.toasts.show(egui_ctx);
+
+        self.frame_time_history
+            .add(egui_ctx.input().time, frame_start.elapsed().as_secs_f32());
     }
 }
 
@@ -724,14 +735,37 @@ fn top_bar_ui(ui: &mut egui::Ui, frame: &mut eframe::Frame, app: &mut App) {
         );
     }
 
+    if let Some(frame_time) = app.frame_time_history.average() {
+        ui.separator();
+        let ms = frame_time * 1e3;
+
+        let visuals = ui.visuals();
+        let color = if ms < 15.0 {
+            visuals.weak_text_color()
+        } else {
+            visuals.warn_fg_color
+        };
+
+        // we use monospace so the width doesn't fluctuate as the numbers change.
+        let text = format!("{ms:.1} ms");
+        ui.label(egui::RichText::new(text).monospace().color(color))
+            .on_hover_text("CPU time used by Rerun Viewer each frame. Lower is better.");
+    }
+
     if let Some(count) = re_memory::accounting_allocator::global_allocs() {
         ui.separator();
-        ui.weak(re_memory::util::format_bytes(count.size as _))
-            .on_hover_text(format!(
-                "Rerun Viewer is using {} of RAM in {} separate allocations.",
-                re_memory::util::format_bytes(count.size as _),
-                format_usize(count.count),
-            ));
+        // we use monospace so the width doesn't fluctuate as the numbers change.
+        let bytes_used_text = re_memory::util::format_bytes(count.size as _);
+        ui.label(
+            egui::RichText::new(&bytes_used_text)
+                .monospace()
+                .color(ui.visuals().weak_text_color()),
+        )
+        .on_hover_text(format!(
+            "Rerun Viewer is using {} of RAM in {} separate allocations.",
+            bytes_used_text,
+            format_usize(count.count),
+        ));
     }
 
     if let Some(rx) = &app.rx {
