5c2f8796beb11204df3fdbef7781319e11a16106
Andreas Reich <andreas@rerun.io>
2022-11-29 15:37:48 +0100

    lines for the 2d sample

--- crates/re_renderer/examples/2d.rs
+++ crates/re_renderer/examples/2d.rs
@@ -1,4 +1,9 @@
-use re_renderer::view_builder::{self, ViewBuilder};
+use re_renderer::{
+    renderer::{LineDrawable, LineStrip, LineStripFlags},
+    view_builder::{self, ViewBuilder},
+};
+
+use smallvec::smallvec;
 
 mod framework;
 
@@ -30,6 +35,55 @@ impl framework::Example for Render2D {
                 ),
             )
             .unwrap();
+
+        let screen_size = glam::vec2(
+            surface_configuration.width as f32,
+            surface_configuration.height as f32,
+        );
+
+        let line_radius = 5.0;
+        let line_drawable = LineDrawable::new(
+            re_ctx,
+            &[
+                // Green lines filling border
+                LineStrip {
+                    points: smallvec![
+                        glam::vec3(line_radius, line_radius, 0.0),
+                        glam::vec3(screen_size.x - line_radius, line_radius, 0.0),
+                        glam::vec3(
+                            screen_size.x - line_radius,
+                            screen_size.y - line_radius,
+                            0.0
+                        ),
+                        glam::vec3(line_radius, screen_size.y - line_radius, 0.0),
+                        glam::vec3(line_radius, line_radius, 0.0),
+                    ],
+                    radius: line_radius,
+                    color: [50, 255, 50, 255],
+                    flags: LineStripFlags::empty(),
+                },
+                // Blue lines around the top left quarter.
+                // TODO(andreas): This lines should be on top now, but they're below (for me at least, surprised there is no z-fighting)
+                LineStrip {
+                    points: smallvec![
+                        glam::vec3(line_radius, line_radius, 0.0),
+                        glam::vec3(screen_size.x * 0.5 - line_radius, line_radius, 0.0),
+                        glam::vec3(
+                            screen_size.x * 0.5 - line_radius,
+                            screen_size.y * 0.5 - line_radius,
+                            0.0
+                        ),
+                        glam::vec3(line_radius, screen_size.y * 0.5 - line_radius, 0.0),
+                        glam::vec3(line_radius, line_radius, 0.0),
+                    ],
+                    radius: line_radius,
+                    color: [50, 50, 255, 255],
+                    flags: LineStripFlags::empty(),
+                },
+            ],
+        )
+        .unwrap();
+        view_builder.queue_draw(&line_drawable);
 
         let command_buffer = view_builder.draw(re_ctx).unwrap();
 
