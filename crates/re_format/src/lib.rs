b11d2e5bcfe3b99a89c763374bc75a59e9092eea
Clement Rey <cr.rey.clement@gmail.com>
2022-12-06 12:56:43 +0100

    more formatting & parsing tools

--- crates/re_format/src/lib.rs
+++ crates/re_format/src/lib.rs
@@ -1,6 +1,6 @@
 //! Miscellaneous formatting tools.
 
-// ---
+// --- Numbers ---
 
 /// Using thousands separators for readability.
 pub fn format_usize(number: usize) -> String {
@@ -34,9 +34,9 @@ fn test_format_usize() {
     assert_eq!(format_usize(1_234_567), "1 234 567");
 }
 
-// ---
+// --- Bytes ---
 
-/// Pretty format bytes, e.g.
+/// Pretty format bytes in base10, e.g.
 ///
 /// ```
 /// # use re_format::format_bytes;
@@ -63,27 +63,213 @@ pub fn format_bytes(number_of_bytes: f64) -> String {
         format!("{:.*} GB", decimals, number_of_bytes / 1_000_000_000.0)
     }
 }
+pub fn format_bytes_base10(number_of_bytes: f64) -> String {
+    format_bytes(number_of_bytes)
+}
 
-pub fn parse_bytes(limit: &str) -> Option<i64> {
-    if let Some(kb) = limit.strip_suffix("kB") {
+#[test]
+fn test_format_bytes() {
+    let test_cases = [
+        (999.0, "999 B"),
+        (1000.0, "1.0 kB"),
+        (1001.0, "1.0 kB"),
+        (999_999.0, "1000 kB"),
+        (1_000_000.0, "1.0 MB"),
+        (1_000_000.0, "1.0 MB"),
+        (999_999_999.0, "1000 MB"),
+        (1_000_000_000.0, "1.0 GB"),
+        (1_000_000_000.0, "1.0 GB"),
+        (999_999_999_999.0, "1000 GB"),
+        (1_000_000_000_000.0, "1000 GB"),
+        (1_000_000_000_000.0, "1000 GB"),
+        (123.0, "123 B"),
+        (12_345.0, "12 kB"),
+        (1_234_567.0, "1.2 MB"),
+        (123_456_789.0, "123 MB"),
+    ];
+
+    for (value, expected) in test_cases {
+        assert_eq!(expected, format_bytes(value));
+    }
+}
+
+/// Pretty format bytes in base2, e.g.
+///
+/// ```
+/// # use re_format::format_bytes_base2;
+/// assert_eq!(format_bytes_base2(123.0), "123 B");
+/// assert_eq!(format_bytes_base2(12_345.0), "12.1 kiB");
+/// assert_eq!(format_bytes_base2(1_234_567.0), "1.2 MiB");
+/// assert_eq!(format_bytes_base2(123_456_789.0), "117.7 MiB");
+/// ```
+pub fn format_bytes_base2(number_of_bytes: f64) -> String {
+    if number_of_bytes < 0.0 {
+        return format!("-{}", format_bytes(-number_of_bytes));
+    }
+
+    if number_of_bytes < (1 << 10) as f64 {
+        format!("{:.0} B", number_of_bytes)
+    } else if number_of_bytes < (1 << 20) as f64 {
+        let decimals = (number_of_bytes < (1 << 18) as f64) as usize;
+        format!("{:.*} kiB", decimals, number_of_bytes / (1 << 10) as f64)
+    } else if number_of_bytes < (1 << 30) as f64 {
+        let decimals = (number_of_bytes < (1 << 28) as f64) as usize;
+        format!("{:.*} MiB", decimals, number_of_bytes / (1 << 20) as f64)
+    } else {
+        let decimals = (number_of_bytes < (1 << 31) as f64) as usize;
+        format!("{:.*} GiB", decimals, number_of_bytes / (1 << 30) as f64)
+    }
+}
+
+#[test]
+fn test_format_bytes_base2() {
+    let test_cases = [
+        (999.0, "999 B"),
+        (1000.0, "1000 B"),
+        (1001.0, "1001 B"),
+        (1023.0, "1023 B"),
+        (1024.0, "1.0 kiB"),
+        (1025.0, "1.0 kiB"),
+        (1024f64.powi(2) - 1.0, "1024 kiB"),
+        (1024f64.powi(2) + 0.0, "1.0 MiB"),
+        (1024f64.powi(2) + 1.0, "1.0 MiB"),
+        (1024f64.powi(3) - 1.0, "1024 MiB"),
+        (1024f64.powi(3) + 0.0, "1 GiB"),
+        (1024f64.powi(3) + 1.0, "1 GiB"),
+        (1024f64.powi(4) - 1.0, "1024 GiB"),
+        (1024f64.powi(4) + 0.0, "1024 GiB"),
+        (1024f64.powi(4) + 1.0, "1024 GiB"),
+        (123.0, "123 B"),
+        (12_345.0, "12.1 kiB"),
+        (1_234_567.0, "1.2 MiB"),
+        (123_456_789.0, "117.7 MiB"),
+    ];
+
+    for (value, expected) in test_cases {
+        assert_eq!(expected, format_bytes_base2(value));
+    }
+}
+
+pub fn parse_bytes_base10(bytes: &str) -> Option<i64> {
+    if let Some(kb) = bytes.strip_suffix("kB") {
         Some(kb.parse::<i64>().ok()? * 1_000)
-    } else if let Some(mb) = limit.strip_suffix("MB") {
+    } else if let Some(mb) = bytes.strip_suffix("MB") {
         Some(mb.parse::<i64>().ok()? * 1_000_000)
-    } else if let Some(gb) = limit.strip_suffix("GB") {
+    } else if let Some(gb) = bytes.strip_suffix("GB") {
         Some(gb.parse::<i64>().ok()? * 1_000_000_000)
-    } else if let Some(tb) = limit.strip_suffix("TB") {
+    } else if let Some(tb) = bytes.strip_suffix("TB") {
         Some(tb.parse::<i64>().ok()? * 1_000_000_000_000)
+    } else if let Some(b) = bytes.strip_suffix('B') {
+        Some(b.parse::<i64>().ok()?)
     } else {
         None
     }
 }
 
 #[test]
-fn test_parse_bytes() {
-    assert_eq!(parse_bytes("10MB"), Some(10_000_000));
+fn test_parse_bytes_base10() {
+    let test_cases = [
+        ("999B", 999),
+        ("1000B", 1_000),
+        ("1kB", 1_000),
+        ("1000kB", 1_000_000),
+        ("1MB", 1_000_000),
+        ("1000MB", 1_000_000_000),
+        ("1GB", 1_000_000_000),
+        ("1000GB", 1_000_000_000_000),
+        ("1TB", 1_000_000_000_000),
+        ("1000TB", 1_000_000_000_000_000),
+        ("123B", 123),
+        ("12kB", 12_000),
+        ("123MB", 123_000_000),
+    ];
+    for (value, expected) in test_cases {
+        assert_eq!(Some(expected), parse_bytes_base10(value));
+    }
 }
 
-// ---
+pub fn parse_bytes_base2(bytes: &str) -> Option<i64> {
+    if let Some(kb) = bytes.strip_suffix("kiB") {
+        Some(kb.parse::<i64>().ok()? * 1024)
+    } else if let Some(mb) = bytes.strip_suffix("MiB") {
+        Some(mb.parse::<i64>().ok()? * 1024 * 1024)
+    } else if let Some(gb) = bytes.strip_suffix("GiB") {
+        Some(gb.parse::<i64>().ok()? * 1024 * 1024 * 1024)
+    } else if let Some(tb) = bytes.strip_suffix("TiB") {
+        Some(tb.parse::<i64>().ok()? * 1024 * 1024 * 1024 * 1024)
+    } else if let Some(b) = bytes.strip_suffix('B') {
+        Some(b.parse::<i64>().ok()?)
+    } else {
+        None
+    }
+}
+
+#[test]
+fn test_parse_bytes_base2() {
+    let test_cases = [
+        ("999B", 999),
+        ("1023B", 1_023),
+        ("1024B", 1_024),
+        ("1kiB", 1_024),
+        ("1000kiB", 1_000 * 1024),
+        ("1MiB", 1024 * 1024),
+        ("1000MiB", 1_000 * 1024 * 1024),
+        ("1GiB", 1024 * 1024 * 1024),
+        ("1000GiB", 1_000 * 1024 * 1024 * 1024),
+        ("1TiB", 1024 * 1024 * 1024 * 1024),
+        ("1000TiB", 1_000 * 1024 * 1024 * 1024 * 1024),
+        ("123B", 123),
+        ("12kiB", 12 * 1024),
+        ("123MiB", 123 * 1024 * 1024),
+    ];
+    for (value, expected) in test_cases {
+        assert_eq!(Some(expected), parse_bytes_base2(value));
+    }
+}
+
+pub fn parse_bytes(bytes: &str) -> Option<i64> {
+    parse_bytes_base10(bytes).or_else(|| parse_bytes_base2(bytes))
+}
+
+#[test]
+fn test_parse_bytes() {
+    let test_cases = [
+        // base10
+        ("999B", 999),
+        ("1000B", 1_000),
+        ("1kB", 1_000),
+        ("1000kB", 1_000_000),
+        ("1MB", 1_000_000),
+        ("1000MB", 1_000_000_000),
+        ("1GB", 1_000_000_000),
+        ("1000GB", 1_000_000_000_000),
+        ("1TB", 1_000_000_000_000),
+        ("1000TB", 1_000_000_000_000_000),
+        ("123B", 123),
+        ("12kB", 12_000),
+        ("123MB", 123_000_000),
+        // base2
+        ("999B", 999),
+        ("1023B", 1_023),
+        ("1024B", 1_024),
+        ("1kiB", 1_024),
+        ("1000kiB", 1_000 * 1024),
+        ("1MiB", 1024 * 1024),
+        ("1000MiB", 1_000 * 1024 * 1024),
+        ("1GiB", 1024 * 1024 * 1024),
+        ("1000GiB", 1_000 * 1024 * 1024 * 1024),
+        ("1TiB", 1024 * 1024 * 1024 * 1024),
+        ("1000TiB", 1_000 * 1024 * 1024 * 1024 * 1024),
+        ("123B", 123),
+        ("12kiB", 12 * 1024),
+        ("123MiB", 123 * 1024 * 1024),
+    ];
+    for (value, expected) in test_cases {
+        assert_eq!(Some(expected), parse_bytes(value));
+    }
+}
+
+// --- Durations ---
 
 pub fn parse_duration(duration: &str) -> Result<f32, String> {
     fn parse_num(s: &str) -> Result<f32, String> {
