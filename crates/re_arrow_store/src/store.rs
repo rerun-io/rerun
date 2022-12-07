cb2f4b3d885a0faefc7d7c4db243145a9b65d240
Clement Rey <cr.rey.clement@gmail.com>
2022-12-07 11:08:10 +0100

    forgot about doc test...

--- crates/re_arrow_store/src/store.rs
+++ crates/re_arrow_store/src/store.rs
@@ -29,7 +29,7 @@ pub struct DataStoreConfig {
     /// larger than the threshold, then that bucket will become larger than the threshold, and
     /// we will split from there on.
     ///
-    /// See [`Self::const_default`] for defaults.
+    /// See [`Self::DEFAULT`] for defaults.
     pub component_bucket_size_bytes: u64,
     /// The maximum number of rows in a component bucket before triggering a split.
     ///
@@ -40,7 +40,7 @@ pub struct DataStoreConfig {
     /// The lower the number, the more fine-grained the garbage collection is, at the cost of more
     /// metadata overhead.
     ///
-    /// See [`Self::const_default`] for defaults.
+    /// See [`Self::DEFAULT`] for defaults.
     pub component_bucket_nb_rows: u64,
 }
 
