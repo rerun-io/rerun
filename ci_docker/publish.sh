ecff726134a1a01973b555350a2aa4e202926e5a
Jeremy Leibs <jeremy@rerun.io>
2022-12-07 15:23:45 +0100

    Push to jleibs dockerhub
    Need to make this a rerun repo with group permissions

--- ci_docker/publish.sh
+++ ci_docker/publish.sh
@@ -1,3 +1,3 @@
-docker build -t ci_docker:0.1 .                                                         
-docker tag ci_docker:0.1 emilkrerun/ci_docker:0.1     
-docker push emilkrerun/ci_docker:0.1   
+docker build -t ci_docker:0.1 .
+docker tag ci_docker:0.1 jleibsrerun/ci_docker:0.1
+docker push jleibsrerun/ci_docker:0.1
