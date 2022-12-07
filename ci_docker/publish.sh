8c951d04c21730b3c2d0856aa8a0d08005c6c189
Jeremy Leibs <jeremy@rerun.io>
2022-12-07 19:00:13 +0100

    Switch to rerunio dockerhub and cleanup publish script

--- ci_docker/publish.sh
+++ ci_docker/publish.sh
@@ -1,3 +1,18 @@
-docker build -t ci_docker:0.1 .
-docker tag ci_docker:0.1 jleibsrerun/ci_docker:0.1
-docker push jleibsrerun/ci_docker:0.1
+VERSION=0.1
+
+# The build needs to run from top of repo to access the requirments.txt
+cd `git rev-parse --show-toplevel`
+
+# Pull :latest so we have the correct cache
+docker pull rerunio/ci_docker
+
+# Build the image
+docker build -t ci_docker -f ci_docker/Dockerfile .
+
+# Tag latest and version
+docker tag ci_docker rerunio/ci_docker
+docker tag ci_docker rerunio/ci_docker:$VERSION
+
+# Push the images back up
+docker push rerunio/ci_docker
+docker push rerunio/ci_docker:$VERSION
