f92e02f110ae77aed5b9b2732f1df960b78f36a5
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-12-13 10:37:47 +0100

    Use more correct rounding

--- crates/re_ui/src/lib.rs
+++ crates/re_ui/src/lib.rs
@@ -74,9 +74,16 @@ impl ReUi {
         16.0
     }
 
-    /// Default border radius
-    pub fn rounding() -> f32 {
+    pub fn window_rounding() -> f32 {
+        12.0
+    }
+
+    pub fn normal_rounding() -> f32 {
         6.0
+    }
+
+    pub fn small_rounding() -> f32 {
+        4.0
     }
 
     pub fn table_line_height() -> f32 {
@@ -111,7 +118,7 @@ impl ReUi {
         egui::Frame {
             inner_margin: egui::style::Margin::same(2.0),
             outer_margin: egui::style::Margin::same(4.0),
-            rounding: Self::rounding().into(),
+            rounding: Self::small_rounding().into(),
             fill: style.visuals.window_fill(),
             stroke: style.visuals.window_stroke(),
             ..Default::default()
