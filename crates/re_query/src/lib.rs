dfdb6bc5759dac8f2436f0ea8a295465db49f8d3
Jeremy Leibs <jeremy@rerun.io>
2022-12-15 18:19:58 +0100

    Cleanup PR feedback

--- crates/re_query/src/lib.rs
+++ crates/re_query/src/lib.rs
@@ -2,5 +2,7 @@
 //! TODO(jleibs) better crate documentation.
 
 mod query;
-pub use self::query::get_component_with_instance_ids;
-pub use self::query::query_entity_with_primary;
+pub use self::query::{get_component_with_instances, query_entity_with_primary};
+
+// Used for doc-tests
+pub use self::query::__populate_example_store;
--- crates/re_query/src/query.rs
+++ crates/re_query/src/query.rs
@@ -8,13 +8,79 @@ pub enum QueryError {
     PrimaryNotFound,
 
     #[error("Error executing Polars Query")]
-    ArrowSerializationError(#[from] PolarsError),
+    PolarsError(#[from] PolarsError),
 }
 
 pub type Result<T> = std::result::Result<T, QueryError>;
 
-/// Retrieves a single component and its corresponding instance-ids
-pub fn get_component_with_instance_ids(
+/// Helper used to create an example store we can use for querying in doctests
+pub fn __populate_example_store() -> DataStore {
+    use re_log_types::{
+        datagen::build_frame_nr,
+        field_types::{ColorRGBA, Point2D},
+        msg_bundle::try_build_msg_bundle2,
+        MsgId,
+    };
+
+    let mut store = DataStore::default();
+
+    let ent_path = "point";
+    let timepoint = [build_frame_nr(123)];
+
+    let instances = vec![Instance(42), Instance(96)];
+    let points = vec![Point2D { x: 1.0, y: 2.0 }, Point2D { x: 3.0, y: 4.0 }];
+
+    let bundle =
+        try_build_msg_bundle2(MsgId::random(), ent_path, timepoint, (&instances, &points)).unwrap();
+    store.insert(&bundle).unwrap();
+
+    let instances = vec![Instance(96)];
+    let colors = vec![ColorRGBA(0xff000000)];
+    let bundle =
+        try_build_msg_bundle2(MsgId::random(), ent_path, timepoint, (instances, colors)).unwrap();
+    store.insert(&bundle).unwrap();
+
+    store
+}
+
+/// Retrieves a [`polars::DataFrame`] for a [`Component`] with its corresponding
+/// [`Instance`] values.
+/// ```
+/// # use re_arrow_store::{TimelineQuery, TimeQuery};
+/// # use re_log_types::{Timeline, field_types::Point2D, msg_bundle::Component};
+/// # let store = re_query::__populate_example_store();
+///
+/// let ent_path = "point";
+/// let timeline_query = TimelineQuery::new(
+///   Timeline::new_sequence("frame_nr"),
+///   TimeQuery::LatestAt(123.into()),
+/// );
+///
+/// let df = re_query::get_component_with_instances(
+///   &store,
+///   &timeline_query,
+///   &ent_path.into(),
+///   Point2D::NAME,
+/// )
+/// .unwrap();
+///
+/// println!("{:?}", df);
+/// ```
+///
+/// Outputs:
+/// ```text
+/// ┌──────────┬───────────┐
+/// │ instance ┆ point2d   │
+/// │ ---      ┆ ---       │
+/// │ u64      ┆ struct[2] │
+/// ╞══════════╪═══════════╡
+/// │ 42       ┆ {1.0,2.0} │
+/// ├╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌┤
+/// │ 96       ┆ {3.0,4.0} │
+/// └──────────┴───────────┘
+/// ```
+///
+pub fn get_component_with_instances(
     store: &DataStore,
     timeline_query: &TimelineQuery,
     ent_path: &ObjPath,
@@ -36,17 +102,72 @@ pub fn get_component_with_instance_ids(
         .collect();
 
     let df = DataFrame::new(series?)?;
+    let exploded = df.explode(df.get_column_names())?;
 
-    Ok(df.explode(df.get_column_names())?)
+    Ok(exploded)
+}
+
+/// If a `DataFrame` has no `Instance` column create one from the row numbers
+fn add_instances_if_needed(df: DataFrame) -> LazyFrame {
+    // Note: we add a row-column temporarily which has u32 type, and then convert it
+    // to the correctly named "instance" row with the correct type and drop the original
+    // row.
+    match df.column(Instance::NAME) {
+        Ok(_) => df.lazy(),
+        Err(_) => df
+            .lazy()
+            .with_row_count("tmp_row", None)
+            .with_column(col("tmp_row").cast(DataType::UInt64).alias(Instance::NAME))
+            .drop_columns(["tmp_row"]),
+    }
 }
 
 /// Retrieve an entity as a polars Dataframe
 ///
-/// An entity has a primary component which is expected to always be present.
-/// The size of the batch will be equal to the size of the primary component.
+/// An entity has a primary [`Component`] which is expected to always be
+/// present. The length of the batch will be equal to the length of the primary
+/// component.
 ///
-/// The remainingComponents are joined based on the `Instance` id. If `Instance`
-/// is not available, it's treated as an integer sequence of the correct length.
+/// The remaining components are joined based on their instances. If those not
+/// available, they are implicitly treated as an integer sequence of the correct
+/// length.
+///
+/// ```
+/// # use re_arrow_store::{TimelineQuery, TimeQuery};
+/// # use re_log_types::{Timeline, field_types::{Point2D, ColorRGBA}, msg_bundle::Component};
+/// # let store = re_query::__populate_example_store();
+///
+/// let ent_path = "point";
+/// let timeline_query = TimelineQuery::new(
+///   Timeline::new_sequence("frame_nr"),
+///   TimeQuery::LatestAt(123.into()),
+/// );
+///
+/// let df = re_query::query_entity_with_primary(
+///   &store,
+///   &timeline_query,
+///   &ent_path.into(),
+///   Point2D::NAME,
+///   &[ColorRGBA::NAME],
+/// )
+/// .unwrap();
+///
+/// println!("{:?}", df);
+/// ```
+///
+/// Outputs:
+/// ```text
+/// ┌──────────┬───────────┬────────────┐
+/// │ instance ┆ point2d   ┆ colorrgba  │
+/// │ ---      ┆ ---       ┆ ---        │
+/// │ u64      ┆ struct[2] ┆ u32        │
+/// ╞══════════╪═══════════╪════════════╡
+/// │ 42       ┆ {1.0,2.0} ┆ null       │
+/// ├╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌┤
+/// │ 96       ┆ {3.0,4.0} ┆ 4278190080 │
+/// └──────────┴───────────┴────────────┘
+/// ```
+///
 pub fn query_entity_with_primary<const N: usize>(
     store: &DataStore,
     timeline_query: &TimelineQuery,
@@ -54,36 +175,22 @@ pub fn query_entity_with_primary<const N: usize>(
     primary: ComponentNameRef<'_>,
     components: &[ComponentNameRef<'_>; N],
 ) -> Result<DataFrame> {
-    let df = get_component_with_instance_ids(store, timeline_query, ent_path, primary)?;
+    let df = get_component_with_instances(store, timeline_query, ent_path, primary)?;
 
     // TODO(jleibs): lots of room for optimization here. Once "instance" is
     // guaranteed to be sorted we should be able to leverage this during the
     // join. Series have a SetSorted option to specify this. join_asof might be
     // the right place to start digging.
 
-    let ldf = match df.column(Instance::NAME) {
-        Ok(_) => df.lazy(),
-        Err(_) => df
-            .lazy()
-            .with_row_count("row", None)
-            .with_column(col("row").cast(DataType::UInt64).alias(Instance::NAME))
-            .drop_columns(["row"]),
-    };
+    let ldf = add_instances_if_needed(df);
 
     let joined = components
         .iter()
         .fold(Ok(ldf), |ldf: Result<LazyFrame>, component| {
             let component =
-                get_component_with_instance_ids(store, timeline_query, ent_path, component)?;
+                get_component_with_instances(store, timeline_query, ent_path, component)?;
 
-            let lazy_component = match component.column(Instance::NAME) {
-                Ok(_) => component.lazy(),
-                Err(_) => component
-                    .lazy()
-                    .with_row_count("row", None)
-                    .with_column(col("row").cast(DataType::UInt64).alias(Instance::NAME))
-                    .drop_columns(["row"]),
-            };
+            let lazy_component = add_instances_if_needed(component);
 
             Ok(ldf?.left_join(lazy_component, col(Instance::NAME), col(Instance::NAME)))
         })?
@@ -131,6 +238,25 @@ fn compare_df(df1: &DataFrame, df2: &DataFrame) {
     cols2.sort();
 
     assert_eq!(df1.select(cols1).unwrap(), df2.select(cols2).unwrap());
+}
+
+#[test]
+fn component_with_instances() {
+    use re_arrow_store::{TimeQuery, TimelineQuery};
+    use re_log_types::{field_types::Point2D, msg_bundle::Component, Timeline};
+
+    let store = __populate_example_store();
+
+    let ent_path = "point";
+    let timeline_query = TimelineQuery::new(
+        Timeline::new_sequence("frame_nr"),
+        TimeQuery::LatestAt(123.into()),
+    );
+
+    let df = get_component_with_instances(&store, &timeline_query, &ent_path.into(), Point2D::NAME)
+        .unwrap();
+
+    eprintln!("{:?}", df);
 }
 
 #[test]
