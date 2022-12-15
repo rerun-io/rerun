b3044ff9519d224b3319eb41398d5f49189734b8
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-12-15 17:37:49 +0100

    Revert to the old visibility buttons. They are ugly, but readable

--- crates/re_viewer/src/ui/viewport.rs
+++ crates/re_viewer/src/ui/viewport.rs
@@ -445,10 +445,10 @@ fn visibility_button(ui: &mut egui::Ui, enabled: bool, visible: &mut bool) -> eg
     ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
         ui.set_enabled(enabled);
         if enabled {
-            ui.add(re_ui::toggle_switch(visible))
+            ui.toggle_value(visible, "👁")
         } else {
             let mut always_false = false;
-            ui.add(re_ui::toggle_switch(&mut always_false))
+            ui.toggle_value(&mut always_false, "👁")
         }
         .on_hover_text("Toggle visibility")
     })
