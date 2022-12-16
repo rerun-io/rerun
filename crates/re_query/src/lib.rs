942d108d5ff110dfbc90d636daa898dea67f4e4a
Jeremy Leibs <jeremy@rerun.io>
2022-12-16 01:24:17 +0100

    Rearrange mods

--- crates/re_query/src/lib.rs
+++ crates/re_query/src/lib.rs
@@ -1,12 +1,11 @@
 //! Provide query-centric access to the `re_arrow_store`
 //! TODO(jleibs) better crate documentation.
 
+pub mod dataframe_util;
 mod query;
+mod visit;
+
 pub use self::query::{get_component_with_instances, query_entity_with_primary};
 // Used for doc-tests
 pub use self::query::__populate_example_store;
-
-mod visit;
 pub use self::visit::{iter_column, visit_component, visit_components2, visit_components3};
-
-pub mod dataframe_util;
