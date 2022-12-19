abc84dec6878ccbea1aaca32b5c7392493772341
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-12-19 11:46:10 +0100

    Update egui with some plot improvements

--- crates/re_viewer/src/ui/view_bar_chart/ui.rs
+++ crates/re_viewer/src/ui/view_bar_chart/ui.rs
@@ -24,34 +24,33 @@ pub(crate) fn view_bar_chart(
 ) -> egui::Response {
     use egui::plot::{Bar, BarChart, Legend, Plot};
 
-    let stroke = egui::Stroke::new(1.0, ui.visuals().extreme_bg_color); // Same as plot background fill
-
     Plot::new("bar_chart_plot")
         .legend(Legend::default())
+        .clamp_grid(true)
         .show(ui, |plot_ui| {
             for (instance_id, bar_chart_values) in &scene.charts {
                 let [r, g, b, a] = auto_color(hash(instance_id) as _);
                 let color = egui::Color32::from_rgba_unmultiplied(r, g, b, a);
                 let fill = color.gamma_multiply(0.75).additive(); // make sure overlapping bars are obvious
 
-                let chart = BarChart::new(
-                    bar_chart_values
-                        .values
-                        .iter()
-                        .enumerate()
-                        .map(|(i, value)| {
-                            Bar::new(i as f64 + 0.5, *value as f64)
-                                .width(1.0)
-                                .name(format!("{instance_id} #{i}"))
-                                .fill(fill)
-                                .stroke(stroke)
-                        })
-                        .collect(),
-                )
-                .name(instance_id.to_string())
-                .color(color);
-
-                plot_ui.bar_chart(chart);
+                plot_ui.bar_chart(
+                    BarChart::new(
+                        bar_chart_values
+                            .values
+                            .iter()
+                            .enumerate()
+                            .map(|(i, value)| {
+                                Bar::new(i as f64 + 0.5, *value as f64)
+                                    .width(0.95)
+                                    .name(format!("{instance_id} #{i}"))
+                                    .fill(fill)
+                                    .stroke(egui::Stroke::NONE)
+                            })
+                            .collect(),
+                    )
+                    .name(instance_id.to_string())
+                    .color(color),
+                );
             }
         })
         .response
