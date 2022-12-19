f178dfa644b2091c8e1bb0081e9bfb41e341df2b
Clement Rey <cr.rey.clement@gmail.com>
2022-12-20 00:45:45 +0100

    i happen to like where this is going

--- crates/re_arrow_store/tests/data_store.rs
+++ crates/re_arrow_store/tests/data_store.rs
@@ -2,14 +2,13 @@
 //!
 //! Testing & demonstrating expected usage of the datastore APIs, no funny stuff.
 
-use arrow2::array::Array;
-
+use arrow2::array::{Array, UInt64Array};
 use polars_core::{prelude::*, series::Series};
 use re_arrow_store::{DataStore, TimeQuery, TimelineQuery};
 use re_log_types::{
-    datagen::{build_frame_nr, build_instances, build_some_rects},
+    datagen::{build_frame_nr, build_instances, build_some_point2d, build_some_rects},
     field_types::{Instance, Point2D, Rect2D},
-    msg_bundle::Component as _,
+    msg_bundle::{Component as _, MsgBundle},
     ComponentName, MsgId, ObjPath as EntityPath, TimeType, Timeline,
 };
 
@@ -37,11 +36,17 @@ fn latest_at() {
 
     let ent_path = EntityPath::from("this/that");
 
-    let bundle1 = test_bundle!(ent_path @ [build_frame_nr(1)] => [
-        build_instances(3),
-        build_some_rects(3),
-    ]);
+    let (instances1, rects1) = (build_instances(3), build_some_rects(3));
+    let bundle1 = test_bundle!(ent_path @ [build_frame_nr(1)] => [instances1.clone(), rects1]);
     store.insert(&bundle1).unwrap();
+
+    let points2 = build_some_point2d(3);
+    let bundle2 = test_bundle!(ent_path @ [build_frame_nr(2)] => [instances1, points2]);
+    store.insert(&bundle2).unwrap();
+
+    let points3 = build_some_point2d(10);
+    let bundle3 = test_bundle!(ent_path @ [build_frame_nr(3)] => [points3]);
+    store.insert(&bundle3).unwrap();
 
     if let err @ Err(_) = store.sanity_check() {
         store.sort_indices();
@@ -49,22 +54,44 @@ fn latest_at() {
         err.unwrap();
     }
 
+    assert_scenario(&store, &ent_path, 0, &[]);
+    assert_scenario(&store, &ent_path, 1, &[(Rect2D::name(), &bundle1)]);
+    assert_scenario(
+        &store,
+        &ent_path,
+        2,
+        &[(Rect2D::name(), &bundle1), (Point2D::name(), &bundle2)],
+    );
+    // TODO: that is where implicit instances should be shown to work!
+    // assert_scenario(
+    //     &store,
+    //     &ent_path,
+    //     3,
+    //     &[(Rect2D::name(), &bundle1), (Point2D::name(), &bundle3)],
+    // );
+}
+
+fn assert_scenario(
+    store: &DataStore,
+    ent_path: &EntityPath,
+    frame_nr: i64,
+    bundles: &[(ComponentName, &MsgBundle)],
+) {
     let timeline_frame_nr = Timeline::new("frame_nr", TimeType::Sequence);
     let components_all = &[Rect2D::name(), Point2D::name()];
 
     let df = joined_query(
-        &store,
-        &TimelineQuery::new(timeline_frame_nr, TimeQuery::LatestAt(1)),
-        &ent_path,
+        store,
+        &TimelineQuery::new(timeline_frame_nr, TimeQuery::LatestAt(frame_nr)),
+        ent_path,
         components_all,
     );
-    let df_expected = new_expected_df(&[(
-        Rect2D::name(),
-        (&*bundle1.components[0].value, &*bundle1.components[1].value),
-    )]);
-    dbg!(df);
-    dbg!(df_expected);
+
+    let df_expected = new_expected_df(bundles);
+    assert_eq!(df_expected, df);
 }
+
+// TODO(cmc): range API tests!
 
 // --- Helpers ---
 
@@ -83,13 +110,13 @@ fn joined_query(
 
     dfs.reduce(|acc, df| {
         acc.left_join(
-            &df,
+            dbg!(&df),
             [Instance::name().as_str()],
             [Instance::name().as_str()],
         )
         .unwrap()
     })
-    .unwrap()
+    .unwrap_or_default()
 }
 
 /// Query a single component and its clustering key, returns a `DataFrame`.
@@ -119,17 +146,38 @@ fn query(
     df
 }
 
-/// `components` is `&[(name, (instance_data, component_data))]`
-#[allow(clippy::type_complexity)]
-fn new_expected_df(components: &[(ComponentName, (&dyn Array, &dyn Array))]) -> DataFrame {
-    let df = components
+// TODO: doc
+fn new_expected_df(bundles: &[(ComponentName, &MsgBundle)]) -> DataFrame {
+    bundles
         .iter()
-        .map(|(component, (instance_data, component_data))| {
-            DataFrame::new(vec![
-                Series::try_from((Instance::name().as_str(), instance_data.to_boxed())).unwrap(),
-                Series::try_from((component.as_str(), component_data.to_boxed())).unwrap(),
-            ])
-            .unwrap()
+        .map(|(component, bundle)| {
+            let df = if bundle.components.len() == 1 {
+                DataFrame::new(vec![
+                    Series::try_from((
+                        Instance::name().as_str(),
+                        UInt64Array::from_vec(
+                            (0..bundle.components[0].value.len() as u64).collect(),
+                        )
+                        .to_boxed(),
+                    ))
+                    .unwrap(),
+                    Series::try_from((component.as_str(), bundle.components[0].value.to_boxed()))
+                        .unwrap(),
+                ])
+                .unwrap()
+            } else {
+                DataFrame::new(vec![
+                    Series::try_from((
+                        Instance::name().as_str(),
+                        bundle.components[0].value.to_boxed(),
+                    ))
+                    .unwrap(),
+                    Series::try_from((component.as_str(), bundle.components[1].value.to_boxed()))
+                        .unwrap(),
+                ])
+                .unwrap()
+            };
+            df.explode(df.get_column_names()).unwrap()
         })
         .reduce(|acc, df| {
             acc.left_join(
@@ -139,6 +187,5 @@ fn new_expected_df(components: &[(ComponentName, (&dyn Array, &dyn Array))]) -> 
             )
             .unwrap()
         })
-        .unwrap();
-    df.explode(df.get_column_names()).unwrap()
+        .unwrap_or_default()
 }
