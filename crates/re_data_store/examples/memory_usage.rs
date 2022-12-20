7d528db157afbaa29318ee4213bfbaf852766cb3
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-12-20 17:00:14 +0100

    Add batches too

--- crates/re_data_store/examples/memory_usage.rs
+++ crates/re_data_store/examples/memory_usage.rs
@@ -252,7 +252,8 @@ fn log_messages() {
     use re_log_types::{
         datagen::{build_frame_nr, build_some_point2d},
         msg_bundle::try_build_msg_bundle1,
-        ArrowMsg, Data, DataMsg, DataPath, FieldName, LogMsg, TimeInt, TimePoint, Timeline,
+        ArrowMsg, BatchIndex, Data, DataMsg, DataPath, DataVec, FieldName, LogMsg, LoggedData,
+        TimeInt, TimePoint, Timeline,
     };
 
     fn encode_log_msg(log_msg: &LogMsg) -> Vec<u8> {
@@ -262,9 +263,12 @@ fn log_messages() {
     }
 
     const POS: [f32; 2] = [2.0, 3.0];
+    const NUM_POINTS: usize = 1_000;
 
     let timeline = Timeline::new_sequence("frame_nr");
     let pos_field_name = FieldName::from("pos");
+    let mut time_point = TimePoint::default();
+    time_point.insert(timeline, TimeInt::from(0));
 
     {
         let used_bytes_start = GLOBAL_ALLOCATOR.used_bytes();
@@ -276,10 +280,8 @@ fn log_messages() {
 
     {
         let used_bytes_start = GLOBAL_ALLOCATOR.used_bytes();
-        let mut time_point = TimePoint::default();
-        time_point.insert(timeline, TimeInt::from(0));
         let log_msg = Box::new(LogMsg::DataMsg(DataMsg {
-            msg_id: MsgId::ZERO,
+            msg_id: MsgId::random(),
             time_point: time_point.clone(),
             data_path: DataPath::new(obj_path!("points"), pos_field_name),
             data: Data::Vec2(POS).into(),
@@ -295,7 +297,7 @@ fn log_messages() {
         let used_bytes_start = GLOBAL_ALLOCATOR.used_bytes();
         let msg_bundle = Box::new(
             try_build_msg_bundle1(
-                MsgId::ZERO,
+                MsgId::random(),
                 obj_path!("points"),
                 [build_frame_nr(0)],
                 build_some_point2d(1),
@@ -308,6 +310,49 @@ fn log_messages() {
         println!("Arrow MsgBundle containing a Pos2 uses {msg_bundle_bytes} bytes in RAM");
         println!(
             "Arrow LogMsg containing a Pos2 uses {log_msg_bytes} bytes in RAM, and {} bytes encoded",
+            encode_log_msg(&log_msg).len()
+        );
+    }
+
+    {
+        let used_bytes_start = GLOBAL_ALLOCATOR.used_bytes();
+        let log_msg = Box::new(LogMsg::DataMsg(DataMsg {
+            msg_id: MsgId::random(),
+            time_point: time_point.clone(),
+            data_path: DataPath::new(obj_path!("points"), pos_field_name),
+            data: LoggedData::Batch {
+                indices: BatchIndex::SequentialIndex(NUM_POINTS),
+                data: DataVec::Vec2(
+                    (0..NUM_POINTS)
+                        .map(|i| [i as f32 / 7.0, i as f32 / 11.0]) // not easily encoded, but zstd does it anyway!
+                        .collect(),
+                ),
+            },
+        }));
+        let log_msg_bytes = GLOBAL_ALLOCATOR.used_bytes() - used_bytes_start;
+        println!(
+            "Classic LogMsg containing {NUM_POINTS}x Pos2 uses {log_msg_bytes} bytes in RAM, and {} bytes encoded",
+            encode_log_msg(&log_msg).len()
+        );
+    }
+
+    {
+        let used_bytes_start = GLOBAL_ALLOCATOR.used_bytes();
+        let msg_bundle = Box::new(
+            try_build_msg_bundle1(
+                MsgId::random(),
+                obj_path!("points"),
+                [build_frame_nr(0)],
+                build_some_point2d(NUM_POINTS),
+            )
+            .unwrap(),
+        );
+        let msg_bundle_bytes = GLOBAL_ALLOCATOR.used_bytes() - used_bytes_start;
+        let log_msg = Box::new(LogMsg::ArrowMsg(ArrowMsg::try_from(*msg_bundle).unwrap()));
+        let log_msg_bytes = GLOBAL_ALLOCATOR.used_bytes() - used_bytes_start;
+        println!("Arrow MsgBundle containing a Pos2 uses {msg_bundle_bytes} bytes in RAM");
+        println!(
+            "Arrow LogMsg containing {NUM_POINTS}x Pos2 uses {log_msg_bytes} bytes in RAM, and {} bytes encoded",
             encode_log_msg(&log_msg).len()
         );
     }
