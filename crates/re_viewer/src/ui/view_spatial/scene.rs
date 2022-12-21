bbe328660ecb5931df2d5ab77c0e569af7b13d66
Andreas Reich <andreas@rerun.io>
2022-12-21 14:05:27 +0100

    re-enable 2d image layering - analyzes image planes now

--- crates/re_viewer/src/ui/view_spatial/scene.rs
+++ crates/re_viewer/src/ui/view_spatial/scene.rs
@@ -846,24 +846,57 @@ impl SceneSpatial {
             );
         }
 
-        // TODO: layered image handling
-        // let total_num_images = self.primitives.textured_rectangles.len();
-        // for (image_idx, img) in self.primitives.textured_rectangles.iter_mut().enumerate() {
-        //     img.top_left_corner_position.z += glam::vec3(
-        //         0.0,
-        //         0.0,
-        //         // We use RDF (X=Right, Y=Down, Z=Forward) for 2D spaces, so we want lower Z in order to put images on top
-        //         (total_num_images - image_idx - 1) as f32 * 0.1,
-        //     );
+        // Handle layered rectangles that are on (roughly) the same plane and were logged in sequence.
+        // First, group by similar plane.
+        let rects_grouped_by_plane = {
+            let mut cur_plane = macaw::Plane3::from_normal_dist(Vec3::NAN, std::f32::NAN);
+            let mut rectangle_group = Vec::new();
+            self.primitives
+                .textured_rectangles
+                .iter_mut()
+                .batching(move |it| {
+                    for rect in it.by_ref() {
+                        let prev_plane = cur_plane;
+                        cur_plane = macaw::Plane3::from_normal_point(
+                            rect.extent_u.cross(rect.extent_v),
+                            rect.top_left_corner_position,
+                        )
+                        .normalized();
 
-        //     let opacity = if image_idx == 0 {
-        //         1.0 // bottom image
-        //     } else {
-        //         // make top images transparent
-        //         1.0 / total_num_images.at_most(20) as f32 // avoid precision problems in framebuffer
-        //     };
-        //     img.multiplicative_tint = img.multiplicative_tint.multiply(opacity);
-        // }
+                        // Are the image planes too unsimilar? Then this is a new group.
+                        if !rectangle_group.is_empty()
+                            && prev_plane.normal.dot(cur_plane.normal) < 0.99
+                            && (prev_plane.d - cur_plane.d) < 0.01
+                        {
+                            let mut previous_group = vec![rect];
+                            std::mem::swap(&mut previous_group, &mut rectangle_group);
+                            return Some((cur_plane, previous_group));
+                        }
+                        rectangle_group.push(rect);
+                    }
+                    if !rectangle_group.is_empty() {
+                        Some((cur_plane, rectangle_group.drain(..).collect()))
+                    } else {
+                        None
+                    }
+                })
+        };
+        // Then, change opacity & transformation for planes within group except the base plane.
+        for (plane, mut grouped_rects) in rects_grouped_by_plane {
+            let total_num_images = grouped_rects.len();
+            for (idx, rect) in grouped_rects.iter_mut().enumerate() {
+                // Move a bit to avoid z fighting.
+                rect.top_left_corner_position +=
+                    plane.normal * (total_num_images - idx - 1) as f32 * 0.1;
+                // make top images transparent
+                let opacity = if idx == 0 {
+                    1.0
+                } else {
+                    1.0 / total_num_images.at_most(20) as f32
+                }; // avoid precision problems in framebuffer
+                rect.multiplicative_tint = rect.multiplicative_tint.multiply(opacity);
+            }
+        }
     }
 
     fn load_boxes_2d(
