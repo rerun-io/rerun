5f246e615c2ba19ae04fdb54f68c13bd2763adca
John Hughes <john@rerun.io>
2022-12-21 11:20:55 +0100

    Reorganize the python sdk into seperate modules

---
+++ rerun_py/rerun/log/file.py
@@ -0,0 +1,83 @@
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import Optional, Sequence
+
+import numpy as np
+import numpy.typing as npt
+
+from rerun import rerun_bindings  # type: ignore[attr-defined]
+
+__all__ = [
+    "MeshFormat",
+    "ImageFormat",
+    "log_mesh_file",
+    "log_image_file",
+]
+
+
+class MeshFormat(Enum):
+    # Needs some way of logging materials too, or adding some default material to the
+    # viewer.
+    # GLTF = "GLTF"
+
+    # Binary glTF
+    GLB = "GLB"
+
+    # Needs some way of logging materials too, or adding some default material to the
+    # viewer.
+    # Wavefront .obj
+    OBJ = "OBJ"
+
+
+@dataclass
+class ImageFormat(Enum):
+    # """ jpeg """"
+    JPEG = "jpeg"
+
+
+def log_mesh_file(
+    obj_path: str,
+    mesh_format: MeshFormat,
+    mesh_file: bytes,
+    *,
+    transform: Optional[npt.NDArray[np.float32]] = None,
+    timeless: bool = False,
+) -> None:
+    """
+    Log the contents of a mesh file (.gltf, .glb, .obj, …).
+
+    `transform` is an optional 3x4 affine transform matrix applied to the mesh.
+
+    Example:
+    -------
+    ```
+    # Move mesh 10 units along the X axis.
+    transform=np.array([
+        [1, 0, 0, 10],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0]])
+    ```
+
+    """
+    if transform is None:
+        transform = np.empty(shape=(0, 0), dtype=np.float32)
+    else:
+        transform = np.require(transform, dtype="float32")
+
+    rerun_bindings.log_mesh_file(obj_path, mesh_format.value, mesh_file, transform, timeless)
+
+
+def log_image_file(
+    obj_path: str,
+    img_path: Path,
+    img_format: Optional[ImageFormat] = None,
+    timeless: bool = False,
+) -> None:
+    """
+    Log the contents of an image file (only JPEGs supported for now).
+
+    If no `img_format` is specified, we will try and guess it.
+    """
+    img_format = getattr(img_format, "value", None)
+    rerun_bindings.log_image_file(obj_path, img_path, img_format, timeless)
---
+++ rerun_py/rerun/log/image.py
@@ -0,0 +1,97 @@
+from typing import Iterable, Optional, Sequence, Union
+
+import numpy as np
+import numpy.typing as npt
+from rerun.log import Colors
+from rerun.log.tensor import _log_tensor, log_tensor
+
+from rerun import rerun_bindings  # type: ignore[attr-defined]
+
+__all__ = [
+    "log_image",
+    "log_depth_image",
+    "log_segmentation_image",
+]
+
+
+def log_image(
+    obj_path: str,
+    image: Colors,
+    *,
+    timeless: bool = False,
+) -> None:
+    """
+    Log a gray or color image.
+
+    The image should either have 1, 3 or 4 channels (gray, RGB or RGBA).
+
+    Supported `dtype`s:
+    * uint8: color components should be in 0-255 sRGB gamma space, except for alpha which should be in 0-255 linear
+    space.
+    * uint16: color components should be in 0-65535 sRGB gamma space, except for alpha which should be in 0-65535
+    linear space.
+    * float32/float64: all color components should be in 0-1 linear space.
+
+    """
+    # Catch some errors early:
+    if len(image.shape) < 2 or 3 < len(image.shape):
+        raise TypeError(f"Expected image, got array of shape {image.shape}")
+
+    if len(image.shape) == 3:
+        depth = image.shape[2]
+        if depth not in (1, 3, 4):
+            raise TypeError(
+                f"Expected image depth of 1 (gray), 3 (RGB) or 4 (RGBA). Instead got array of shape {image.shape}"
+            )
+
+    log_tensor(obj_path, image, timeless=timeless)
+
+
+def log_depth_image(
+    obj_path: str,
+    image: Colors,
+    *,
+    meter: Optional[float] = None,
+    timeless: bool = False,
+) -> None:
+    """
+    Log a depth image.
+
+    The image must be a 2D array. Supported `dtype`:s are: uint8, uint16, float32, float64
+
+    meter: How long is a meter in the given dtype?
+           For instance: with uint16, perhaps meter=1000 which would mean
+           you have millimeter precision and a range of up to ~65 meters (2^16 / 1000).
+
+    """
+    # Catch some errors early:
+    if len(image.shape) != 2:
+        raise TypeError(f"Expected 2D depth image, got array of shape {image.shape}")
+
+    log_tensor(obj_path, image, meter=meter, timeless=timeless)
+
+
+def log_segmentation_image(
+    obj_path: str,
+    image: npt.ArrayLike,
+    *,
+    timeless: bool = False,
+) -> None:
+    """
+    Log an image made up of integer class-ids.
+
+    The image should have 1 channel, i.e. be either `H x W` or `H x W x 1`.
+    """
+    if not isinstance(image, np.ndarray):
+        image = np.array(image, dtype=np.uint16)
+
+    # Catch some errors early:
+    if len(image.shape) < 2 or 3 < len(image.shape):
+        raise TypeError(f"Expected image, got array of shape {image.shape}")
+
+    if len(image.shape) == 3:
+        depth = image.shape[2]
+        if depth != 1:
+            raise TypeError(f"Expected image depth of 1. Instead got array of shape {image.shape}")
+
+    _log_tensor(obj_path, tensor=image, meaning=rerun_bindings.TensorDataMeaning.ClassId, timeless=timeless)
