a575c737e3b55b44d2a9971af2ab5bdb496c340e
Nikolaus West <niko@rerun.io>
2022-12-22 16:32:25 +0100

    make _log_tensor compatible with pytorch tensors

--- rerun_py/rerun/log/tensor.py
+++ rerun_py/rerun/log/tensor.py
@@ -29,15 +29,40 @@ def log_tensor(
 def _log_tensor(
     obj_path: str,
     tensor: npt.NDArray[TensorDType],
-    names: Optional[Iterable[str]] = None,
+    names: Optional[Iterable[Optional[str]]] = None,
     meter: Optional[float] = None,
     meaning: bindings.TensorDataMeaning = None,
     timeless: bool = False,
 ) -> None:
     """Log a general tensor, perhaps with named dimensions."""
+
+    # Duck-typing way to handle pytorch tensors
+    try:
+        # Get dimension names if available
+        if names is None:
+            names = tensor.names  # type: ignore[attr-defined]
+
+            # TODO(#631): Remove this check when we can handle lists of optional names.
+            names = list(names)
+            if names.count(None) == len(names):
+                names = None
+
+        # Make available to the cpu
+        tensor = tensor.detach().cpu()  # type: ignore[attr-defined]
+    except AttributeError:
+        pass
+
+    # Handle non-numpy arrays (like Pillow images or torch tensors)
+    tensor = np.array(tensor, copy=False)
+
     if names is not None:
         names = list(names)
-        assert len(tensor.shape) == len(names)
+
+        if len(tensor.shape) != len(names):
+            logging.warning(
+                f"len(tensor.shape) = len({tensor.shape}) = {len(tensor.shape)} != len(names) = len({names}) = {len(names)}. Dropping tensor dimension names."
+            )
+            names = None
 
     SUPPORTED_DTYPES: Any = [
         np.uint8,
@@ -54,7 +79,8 @@ def _log_tensor(
     ]
 
     if tensor.dtype not in SUPPORTED_DTYPES:
-        raise TypeError(f"Unsupported dtype: {tensor.dtype}. Expected a numeric type.")
+        logging.warning(f"Unsupported dtype: {tensor.dtype}. Expected a numeric type. Skipping this tensor.")
+        return
 
     if EXP_ARROW.classic_log_gate():
         bindings.log_tensor(obj_path, tensor, names, meter, meaning, timeless)
