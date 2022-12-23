b79ddc34b9396e2ea112c37973e989ff080c6b96
Nikolaus West <niko@rerun.io>
2022-12-23 16:05:47 +0100

    TODO nit

--- examples/stable_diffusion/huggingface_pipeline.py
+++ examples/stable_diffusion/huggingface_pipeline.py
@@ -567,7 +567,7 @@ class StableDiffusionDepth2ImgPipeline(DiffusionPipeline):
         )
         rr.log_tensor("diffusion/latents", latents, names=["b", "c", "h", "w"])
 
-        # 8. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
+        # 8. Prepare extra step kwargs. TODO(original author): Logic should ideally just be moved out of the pipeline
         extra_step_kwargs = self.prepare_extra_step_kwargs(generator, eta)
 
         # 9. Denoising loop
