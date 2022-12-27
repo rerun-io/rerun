2f4181f62ea5c5379e29d2bb5186a6e7e90ffddd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-12-27 13:32:11 +0100

    Move Command and CommandPalette into re_ui proper

---
+++ crates/re_ui/src/command.rs
@@ -0,0 +1,86 @@
+use egui::{Key, KeyboardShortcut, Modifiers};
+
+/// All the commands we support.
+///
+/// Most are available in the GUI,
+/// some have keyboard shortcuts,
+/// and all are visible in the [`crate::CommandPalette`].
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, strum_macros::EnumIter)]
+pub enum Command {
+    /// In the order they show up in the command palette by default!
+    Save,
+    SaveSelection,
+    Open,
+    Quit,
+    ResetViewer,
+    ShowProfiler,
+
+    ToggleMemoryPanel,
+    ToggleBlueprintPanel,
+    ToggleSelectionPanel,
+    ToggleTimePanel,
+    ToggleFullscreen,
+
+    SelectionPrevious,
+    SelectionNext,
+
+    ToggleCommandPalette,
+}
+
+impl Command {
+    pub fn text(&self) -> &'static str {
+        match self {
+            Command::Save => "Save",
+            Command::SaveSelection => "Save selection",
+            Command::Open => "Open",
+            Command::Quit => "Quit",
+            Command::ResetViewer => "Reset viewer",
+            Command::ShowProfiler => "Show profiler",
+            Command::ToggleMemoryPanel => "Toggle memory panel",
+            Command::ToggleBlueprintPanel => "Toggle blueprint panel",
+            Command::ToggleSelectionPanel => "Toggle selection panel",
+            Command::ToggleTimePanel => "Toggle time panel",
+            Command::ToggleFullscreen => "Toggle fullscreen",
+            Command::SelectionPrevious => "Selection previous",
+            Command::SelectionNext => "Selection next",
+            Command::ToggleCommandPalette => "Toggle command palette",
+        }
+    }
+
+    pub fn kb_shortcut(&self) -> Option<KeyboardShortcut> {
+        fn cmd(key: Key) -> KeyboardShortcut {
+            KeyboardShortcut::new(Modifiers::COMMAND, key)
+        }
+
+        fn cmd_shift(key: Key) -> KeyboardShortcut {
+            KeyboardShortcut::new(Modifiers::COMMAND.plus(Modifiers::SHIFT), key)
+        }
+
+        fn ctrl_shift(key: Key) -> KeyboardShortcut {
+            KeyboardShortcut::new(Modifiers::CTRL.plus(Modifiers::SHIFT), key)
+        }
+
+        match self {
+            Command::Save => Some(cmd(Key::S)),
+            Command::SaveSelection => Some(cmd_shift(Key::S)),
+            Command::Open => Some(KeyboardShortcut::new(Modifiers::COMMAND, Key::O)),
+
+            #[cfg(all(not(target_arch = "wasm32"), target_os = "windows"))]
+            Command::Quit => Some(KeyboardShortcut::new(Modifiers::ALT, Key::F4)),
+
+            #[cfg(all(not(target_arch = "wasm32"), not(target_os = "windows")))]
+            Command::Quit => Some(KeyboardShortcut::new(Modifiers::COMMAND, Key::Q)),
+
+            Command::ResetViewer => Some(ctrl_shift(Key::R)),
+            Command::ShowProfiler => Some(ctrl_shift(Key::P)),
+            Command::ToggleMemoryPanel => Some(ctrl_shift(Key::M)),
+            Command::ToggleBlueprintPanel => Some(ctrl_shift(Key::B)),
+            Command::ToggleSelectionPanel => Some(ctrl_shift(Key::S)),
+            Command::ToggleTimePanel => Some(ctrl_shift(Key::T)),
+            Command::ToggleFullscreen => Some(ctrl_shift(Key::ArrowLeft)),
+            Command::SelectionPrevious => Some(ctrl_shift(Key::ArrowRight)),
+            Command::SelectionNext => Some(KeyboardShortcut::new(Modifiers::NONE, Key::F11)),
+            Command::ToggleCommandPalette => Some(cmd(Key::P)),
+        }
+    }
+}
---
+++ crates/re_ui/src/command_palette.rs
@@ -0,0 +1,150 @@
+use egui::{Align2, Key};
+
+use crate::Command;
+
+#[derive(Default)]
+pub struct CommandPalette {
+    visible: bool,
+    text: String,
+    selected_alternative: usize,
+}
+
+impl CommandPalette {
+    /// Show the command palette, if it is visible.
+    /// Also listens for the keyboard shortcut to toggle it.
+    #[must_use = "Returns the command that was selected"]
+    pub fn show(&mut self, egui_ctx: &egui::Context) -> Option<Command> {
+        let kb_shortcut = Command::ToggleCommandPalette
+            .kb_shortcut()
+            .expect("We need a keybloard shortcut for the command palette");
+        self.visible ^= egui_ctx.input_mut().consume_shortcut(&kb_shortcut);
+        self.visible &= !egui_ctx
+            .input_mut()
+            .consume_key(Default::default(), Key::Escape);
+        if !self.visible {
+            return None;
+        }
+
+        let max_height = 320.0;
+        let y = egui_ctx.input().screen_rect().center().y - 0.5 * max_height;
+
+        egui::Window::new("Command Palette")
+            .title_bar(false)
+            .anchor(Align2::CENTER_TOP, [0.0, y])
+            .fixed_size([350.0, max_height])
+            .show(egui_ctx, |ui| self.window_content(ui))?
+            .inner?
+    }
+
+    #[must_use = "Returns the command that was selected"]
+    fn window_content(&mut self, ui: &mut egui::Ui) -> Option<Command> {
+        // Check _before_ we add the `TextEdit`, so it doesn't steal it.
+        let enter_pressed = ui.input_mut().consume_key(Default::default(), Key::Enter);
+        if enter_pressed {
+            self.visible = false;
+        }
+
+        let text_response = ui.add(
+            egui::TextEdit::singleline(&mut self.text)
+                .desired_width(f32::INFINITY)
+                .lock_focus(true),
+        );
+        text_response.request_focus();
+        if text_response.changed() {
+            self.selected_alternative = 0;
+        }
+
+        egui::ScrollArea::vertical()
+            .auto_shrink([false, true])
+            .show(ui, |ui| self.alternatives(ui, enter_pressed))
+            .inner
+    }
+
+    #[must_use = "Returns the command that was selected"]
+    fn alternatives(&mut self, ui: &mut egui::Ui, enter_pressed: bool) -> Option<Command> {
+        use strum::IntoEnumIterator as _;
+        let mut num_alternatives: usize = 0;
+
+        // TODO(emilk): nicer filtering
+        let filter = self.text.to_lowercase();
+
+        let item_height = 16.0;
+        let font_id = egui::TextStyle::Button.resolve(ui.style());
+
+        let mut selected_command = None;
+
+        for (i, command) in Command::iter()
+            .filter(|alt| alt.text().to_lowercase().contains(&filter))
+            .enumerate()
+        {
+            let text = command.text();
+            let kb_shortcut = command
+                .kb_shortcut()
+                .map(|shortcut| ui.ctx().format_shortcut(&shortcut))
+                .unwrap_or_default();
+
+            let (rect, response) = ui.allocate_at_least(
+                egui::vec2(ui.available_width(), item_height),
+                egui::Sense::click(),
+            );
+
+            if response.clicked() {
+                selected_command = Some(command);
+            }
+
+            let selected = i == self.selected_alternative;
+            let style = ui.style().interact_selectable(&response, selected);
+
+            if selected {
+                ui.painter()
+                    .rect_filled(rect, style.rounding, ui.visuals().selection.bg_fill);
+
+                if enter_pressed {
+                    selected_command = Some(command);
+                }
+
+                ui.scroll_to_rect(rect, None);
+            }
+
+            // TODO(emilk): shorten long text using '…'
+            ui.painter().text(
+                rect.left_center(),
+                Align2::LEFT_CENTER,
+                text,
+                font_id.clone(),
+                style.text_color(),
+            );
+            ui.painter().text(
+                rect.right_center(),
+                Align2::RIGHT_CENTER,
+                kb_shortcut,
+                font_id.clone(),
+                if selected {
+                    style.text_color()
+                } else {
+                    ui.visuals().weak_text_color()
+                },
+            );
+
+            num_alternatives += 1;
+        }
+
+        // Move up/down in the list:
+
+        self.selected_alternative = self.selected_alternative.saturating_sub(
+            ui.input_mut()
+                .count_and_consume_key(Default::default(), Key::ArrowUp),
+        );
+
+        self.selected_alternative = self.selected_alternative.saturating_add(
+            ui.input_mut()
+                .count_and_consume_key(Default::default(), Key::ArrowDown),
+        );
+
+        self.selected_alternative = self
+            .selected_alternative
+            .clamp(0, num_alternatives.saturating_sub(1));
+
+        selected_command
+    }
+}
