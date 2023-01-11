884a23ccfb09f101ea8f4c7ee0e683e98f1cd889
Jeremy Leibs <jeremy@rerun.io>
2023-01-11 22:45:22 +0100

    Add a unit-test

--- crates/re_query/tests/query_tests.rs
+++ crates/re_query/tests/query_tests.rs
@@ -270,3 +270,74 @@ fn missing_column_join_query() {
         let _used = entity_view;
     }
 }
+
+#[test]
+fn splatted_query() {
+    let mut store = DataStore::new(Instance::name(), Default::default());
+
+    let ent_path = "point";
+    let timepoint = [build_frame_nr(123.into())];
+
+    // Create some points with implicit instances
+    let points = vec![Point2D { x: 1.0, y: 2.0 }, Point2D { x: 3.0, y: 4.0 }];
+    let bundle = try_build_msg_bundle1(MsgId::random(), ent_path, timepoint, &points).unwrap();
+    store.insert(&bundle).unwrap();
+
+    // Assign all of them a color via splat
+    let color_instances = vec![Instance::splat()];
+    let colors = vec![ColorRGBA(0xff000000)];
+    let bundle = try_build_msg_bundle2(
+        MsgId::random(),
+        ent_path,
+        timepoint,
+        (color_instances, colors),
+    )
+    .unwrap();
+    store.insert(&bundle).unwrap();
+
+    // Retrieve the view
+    let timeline_query = re_arrow_store::LatestAtQuery::new(timepoint[0].0, timepoint[0].1);
+
+    let entity_view = query_entity_with_primary::<Point2D>(
+        &store,
+        &timeline_query,
+        &ent_path.into(),
+        &[ColorRGBA::name()],
+    )
+    .unwrap();
+
+    // We expect this to generate the following `DataFrame`
+    // ┌──────────┬───────────┬────────────┐
+    // │ instance ┆ point2d   ┆ colorrgba  │
+    // │ ---      ┆ ---       ┆ ---        │
+    // │ u64      ┆ struct[2] ┆ u32        │
+    // ╞══════════╪═══════════╪════════════╡
+    // │ 0        ┆ {1.0,2.0} ┆ 4278190080 │
+    // ├╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌┤
+    // │ 1        ┆ {3.0,4.0} ┆ 4278190080 │
+    // └──────────┴───────────┴────────────┘
+
+    #[cfg(feature = "polars")]
+    {
+        use re_query::dataframe_util::df_builder3;
+
+        // Build expected df manually
+        let instances = vec![Some(Instance(0)), Some(Instance(1))];
+        let points = vec![
+            Some(Point2D { x: 1.0, y: 2.0 }),
+            Some(Point2D { x: 3.0, y: 4.0 }),
+        ];
+        let colors = vec![Some(ColorRGBA(0xff000000)), Some(ColorRGBA(0xff000000))];
+        let expected = df_builder3(&instances, &points, &colors).unwrap();
+
+        //eprintln!("{:?}", df);
+        //eprintln!("{:?}", expected);
+
+        common::compare_df(&expected, &entity_view.as_df2::<ColorRGBA>().unwrap());
+    }
+    #[cfg(not(feature = "polars"))]
+    {
+        //TODO(jleibs): non-polars test validation
+        let _used = entity_view;
+    }
+}
