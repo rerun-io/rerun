53baf087bd7773b86612d10c3d93e068ad7ea905
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-01-16 12:06:46 +0100

    Brighter tooltip background

--- crates/re_ui/src/design_tokens.rs
+++ crates/re_ui/src/design_tokens.rs
@@ -60,7 +60,8 @@ fn apply_design_tokens(ctx: &egui::Context) -> DesignTokens {
     }
 
     let panel_bg_color = get_aliased_color(&json, "{Alias.Color.Surface.Default.value}");
-    let floating_color = get_aliased_color(&json, "{Alias.Color.Surface.Floating.value}");
+    // let floating_color = get_aliased_color(&json, "{Alias.Color.Surface.Floating.value}");
+    let floating_color = Color32::from_gray(38); // HACK: the one in design_tokens.json is too similar to bottom_bar_color
 
     egui_style.visuals.widgets.noninteractive.bg_fill = panel_bg_color;
 
@@ -83,7 +84,7 @@ fn apply_design_tokens(ctx: &egui::Context) -> DesignTokens {
     egui_style.visuals.popup_shadow = egui::epaint::Shadow::NONE;
     egui_style.visuals.window_shadow = egui::epaint::Shadow::NONE;
 
-    egui_style.visuals.window_fill = floating_color;
+    egui_style.visuals.window_fill = floating_color; // tooltips and menus
     egui_style.visuals.window_stroke = egui::Stroke::NONE;
     egui_style.visuals.panel_fill = panel_bg_color;
 
