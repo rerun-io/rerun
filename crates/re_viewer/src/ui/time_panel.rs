6aabf5c0e37b8f4709ae7ae5f4ee2d790dc0bce0
Emil Ernerfeldt <emil@rerun.io>
2023-01-16 17:25:45 +0100

    Add "Streams" title to the timeline panel (#782)
    * Brighter tooltip background
    * Time panel: full-width separator lines
    * Add "Streams" text with tooltip to the timeline panel

--- crates/re_viewer/src/ui/time_panel.rs
+++ crates/re_viewer/src/ui/time_panel.rs
@@ -117,7 +117,7 @@ impl TimePanel {
 
                         // Draw separator between top bar and the rest:
                         ui.painter().hline(
-                            rop_row_rect.x_range(),
+                            0.0..=rop_row_rect.right(),
                             rop_row_rect.bottom(),
                             ui.visuals().widgets.noninteractive.bg_stroke,
                         );
@@ -209,7 +209,20 @@ impl TimePanel {
 
         let timeline_rect = {
             let top = ui.min_rect().bottom();
-            ui.add_space(32.0);
+
+            let size = egui::vec2(self.prev_col_width, 28.0);
+            ui.allocate_ui_with_layout(size, egui::Layout::top_down(egui::Align::Center), |ui| {
+                ui.set_min_size(size);
+                ui.add_space(4.0); // hack to vertically center the text
+                ui.strong("Streams");
+            })
+            .response
+            .on_hover_text(
+                "A hierarchial view of the paths used during logging.\n\
+                        \n\
+                        On the right you can see when there was a log event for a stream.",
+            );
+
             let bottom = ui.min_rect().bottom();
             Rect::from_x_y_ranges(time_fg_x_range.clone(), top..=bottom)
         };
@@ -226,7 +239,11 @@ impl TimePanel {
         let time_bg_area_painter = ui.painter().with_clip_rect(time_bg_area_rect);
         let time_area_painter = ui.painter().with_clip_rect(time_fg_area_rect);
 
-        ui.separator();
+        ui.painter().hline(
+            0.0..=ui.max_rect().right(),
+            timeline_rect.bottom(),
+            ui.visuals().widgets.noninteractive.bg_stroke,
+        );
 
         paint_time_ranges_and_ticks(
             &self.time_ranges_ui,
