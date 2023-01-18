4f9d040430a7c07584f29bb0f241f32b221dd3ac
Jeremy Leibs <jeremy@rerun.io>
2023-01-18 18:07:55 +0100

    Make arrow the default on debug builds

--- crates/rerun_sdk/src/session.rs
+++ crates/rerun_sdk/src/session.rs
@@ -16,10 +16,10 @@ enum StoreSelect {
 }
 
 // TODO(#707): Make this the default for Debug-builds, and eventually release builds
-//#[cfg(debug_assertions)]
-//const DEFAULT_STORE: StoreSelect = StoreSelect::Arrow;
+#[cfg(debug_assertions)]
+const DEFAULT_STORE: StoreSelect = StoreSelect::Arrow;
 
-//#[cfg(not(debug_assertions))]
+#[cfg(not(debug_assertions))]
 const DEFAULT_STORE: StoreSelect = StoreSelect::Classic;
 
 lazy_static! {
