b727531f545b51f5e47ffdd59da20f8e1622387e
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-01-19 16:43:35 +0100

    Better MsgId DataUi

--- crates/re_viewer/src/ui/data_ui/msg_id.rs
+++ crates/re_viewer/src/ui/data_ui/msg_id.rs
@@ -8,13 +8,13 @@ impl DataUi for MsgId {
     fn data_ui(&self, ctx: &mut ViewerContext<'_>, ui: &mut egui::Ui, preview: Preview) {
         match preview {
             Preview::Small | Preview::MaxHeight(_) => {
-                ui.label(self.to_string());
+                ctx.msg_id_button(ui, *self);
             }
             Preview::Large => {
                 if let Some(msg) = ctx.log_db.get_log_msg(self) {
                     msg.data_ui(ctx, ui, preview);
                 } else {
-                    ui.label(self.to_string());
+                    ctx.msg_id_button(ui, *self);
                 }
             }
         }
