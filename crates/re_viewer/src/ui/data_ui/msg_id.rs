22353d714c54440a6944c5ed4f3cef2632f2e07c
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-01-19 16:10:13 +0100

    Implement Small DataUi for MsgId

--- crates/re_viewer/src/ui/data_ui/msg_id.rs
+++ crates/re_viewer/src/ui/data_ui/msg_id.rs
@@ -6,8 +6,17 @@ use super::{DataUi, Preview};
 
 impl DataUi for MsgId {
     fn data_ui(&self, ctx: &mut ViewerContext<'_>, ui: &mut egui::Ui, preview: Preview) {
-        if let Some(msg) = ctx.log_db.get_log_msg(self) {
-            msg.data_ui(ctx, ui, preview);
+        match preview {
+            Preview::Small | Preview::MaxHeight(_) => {
+                ui.label(self.to_string());
+            }
+            Preview::Large => {
+                if let Some(msg) = ctx.log_db.get_log_msg(self) {
+                    msg.data_ui(ctx, ui, preview);
+                } else {
+                    ui.label(self.to_string());
+                }
+            }
         }
     }
 }
