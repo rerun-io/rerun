a44b795b7c772a1c2efe1cd3c09e970e8e23239a
Andreas Reich <andreas@rerun.io>
2023-01-19 09:34:18 +0100

    Fix double click on non-camera object no longer focusing Fixes #817

--- crates/re_viewer/src/ui/view_spatial/ui_3d.rs
+++ crates/re_viewer/src/ui/view_spatial/ui_3d.rs
@@ -400,6 +400,11 @@ pub fn view_3d(
                 // TODO(andreas): Associate current space view
                 .map(Selection::Instance)
         }));
+        state.state_3d.hovered_point = picking_result
+            .opaque_hit
+            .as_ref()
+            .or_else(|| picking_result.transparent_hits.last())
+            .map(|hit| picking_result.space_position(hit));
 
         project_onto_other_spaces(ctx, &scene.space_cameras, &mut state.state_3d, space);
     }
