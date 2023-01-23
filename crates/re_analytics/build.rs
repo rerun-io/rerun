d5bc474632b57f695460c8a622338562d8748bd8
Clement Rey <cr.rey.clement@gmail.com>
2023-01-23 15:15:43 +0100

    git hash and cleaned up target triple

--- crates/re_analytics/build.rs
+++ crates/re_analytics/build.rs
@@ -1,7 +1,18 @@
+use std::process::Command;
+
 fn main() {
-    let out_dir = std::env::var_os("OUT_DIR").unwrap();
-    let target = std::env::var("TARGET").unwrap();
-    let dest_path = std::path::Path::new(&out_dir).join("target.rs");
-    std::fs::write(&dest_path, format!("\"{target}\"")).unwrap();
+    // target triple
+    println!(
+        "cargo:rustc-env=__RERUN_TARGET_TRIPLE={}",
+        std::env::var("TARGET").unwrap()
+    );
     println!("cargo:rerun-if-changed=build.rs");
+
+    // git hash
+    let output = Command::new("git")
+        .args(["rev-parse", "HEAD"])
+        .output()
+        .unwrap();
+    let git_hash = String::from_utf8(output.stdout).unwrap();
+    println!("cargo:rustc-env=__RERUN_GIT_HASH={}", git_hash);
 }
--- crates/re_analytics/src/events.rs
+++ crates/re_analytics/src/events.rs
@@ -10,11 +10,13 @@ impl Event {
     pub fn update_metadata() -> Self {
         let rerun_version = env!("CARGO_PKG_VERSION").to_owned();
         let rust_version = env!("CARGO_PKG_RUST_VERSION").to_owned();
-        let target = include!(concat!(env!("OUT_DIR"), "/target.rs")).to_owned();
+        let target_triple = env!("__RERUN_TARGET_TRIPLE").to_owned();
+        let git_hash = env!("__RERUN_GIT_HASH").to_owned();
         Self::update("update_metadata".into())
             .with_prop("rerun_version".into(), rerun_version)
             .with_prop("rust_version".into(), rust_version)
-            .with_prop("target".into(), target)
+            .with_prop("target".into(), target_triple)
+            .with_prop("git_hash".into(), git_hash)
     }
 
     // TODO(cmc): start_method
