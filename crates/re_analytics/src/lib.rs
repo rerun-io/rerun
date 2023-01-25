b03ded283d4a9871c150a5167f64fe35ea6e8da2
Nikolaus West <niko@grapic.co>
2023-01-25 20:20:03 +0100

    Review improvement: disclaimer
    Co-authored-by: Emil Ernerfeldt <emil@rerun.io>

--- crates/re_analytics/src/lib.rs
+++ crates/re_analytics/src/lib.rs
@@ -79,6 +79,7 @@ const DISCLAIMER: &str = "
       will never be collected.
     - Usage data we do collect will be sent to and stored in servers within the EU.
     - We don't log IP addresses.
+    - We don't log your user name, file paths, or any personal identifiable data
     - For more details and instructions on how to opt out,
       run: `rerun analytics details` or `python -m rerun analytics details`.
 
