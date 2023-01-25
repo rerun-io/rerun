f991997ca3eceeeaf3c948c8a6a37ad890ccf452
Andreas Reich <andreas@rerun.io>
2023-01-25 18:57:32 +0100

    comment link fixes

--- crates/re_viewer/src/misc/color_map.rs
+++ crates/re_viewer/src/misc/color_map.rs
@@ -1,13 +1,12 @@
 /// Converts float from 0.0..=1.0 into a color using Turbo.
 ///
-/// The Turbo color map described here:
-/// https://ai.googleblog.com/2019/08/turbo-improved-rainbow-colormap-for.html
+/// The Turbo color map described [here](https://ai.googleblog.com/2019/08/turbo-improved-rainbow-colormap-for.html)
 ///
 /// Turbo color map is originally a lookup table!
 /// I.e. for any value not captured we'd need to interpolate.
 ///
 /// Instead, we use this polynomial approximation.
-/// https://gist.github.com/mikhailov-work/0d177465a8151eb6ede1768d51d476c7
+/// [see here](https://gist.github.com/mikhailov-work/0d177465a8151eb6ede1768d51d476c7)
 #[allow(clippy::excessive_precision)]
 pub fn turbo_color_map(x: f32) -> egui::Color32 {
     let red_vec4 = glam::vec4(0.13572138, 4.61539260, -42.66032258, 132.13108234);
