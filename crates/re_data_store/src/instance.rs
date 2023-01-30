rerun-io/rerun

commit 633ba3e1bf23d691d2ef57bdbdad48f9c03bfb08

Remove a lot of classical stuff (#988)
* Remove Session::register_type

* Remove TypeMsg

* Remove Session::send_data

* Remove classical text scene

* Remove DataMsg

* Remove classic loading of tensors and annotations

* Remove ObjDb::store

* Remove add_data_msg

* Remove classical DataStore

* Remove unused Error cases

* Properly throttle arrow messages

* Remove ObjectType

* Remove LoggedData

* Remove DataVec

* Remove DataTrait

* Remove Data

* Remove DataType

* Tweak how view categorization is done

* Remove docstrings referring to dead stuff

* remove batch.rs

* rename DataPath::field_name to component_name

* Remove FieldOrComponent

* Fix doclink

* Remove FieldName

* Remove ObjTypePath and IndexPath

crates/re_data_store/src/instance.rs
@@ -137,13 +137,10 @@ impl InstanceIdHash {
 
     pub fn resolve(&self, obj_db: &ObjDb) -> Option<InstanceId> {
         match self.arrow_instance {
-            None => Some(InstanceId {
-                obj_path: obj_db.obj_path_from_hash(&self.obj_path_hash).cloned()?,
-                instance_index: obj_db
-                    .store
-                    .index_from_hash(&self.instance_index_hash)
-                    .cloned(),
-            }),
+            None => {
+                re_log::error_once!("Found classical InstanceIdHash");
+                None
+            }
             Some(arrow_instance) => Some(InstanceId {
                 obj_path: obj_db.obj_path_from_hash(&self.obj_path_hash).cloned()?,
                 instance_index: Some(Index::ArrowInstance(arrow_instance)),

crates/re_data_store/src/stores/mod.rs
@@ -1,9 +1,0 @@
-mod data_store;
-mod field_store;
-mod obj_store;
-mod timeline_store;
-
-pub use data_store::*;
-pub use field_store::*;
-pub use obj_store::*;
-pub use timeline_store::*;

crates/re_log_types/src/path/mod.rs
@@ -5,28 +5,15 @@
 //! or an [`Index`].
 //!
 //! The [`Index`]es are for tables, arrays etc.
-//! You can split an [`ObjPath`] into the names and the indices,
-//! and then you get a [`ObjTypePath`] and an [`IndexPath`], like so:
-//!
-//! * [`ObjPath`]:     `camera / "left" / points / #42`
-//! * [`ObjTypePath`]: `camera / *      / points / *`
-//! * [`IndexPath`]:   `       / "left" /       / #42`
-//!
-//! Every object with the same [`ObjTypePath`] have
-//! the same [`crate::ObjectType`] (hence the name).
 
 mod data_path;
-mod index_path;
 mod obj_path;
 mod obj_path_impl;
-mod obj_type_path;
 mod parse_path;
 
-pub use data_path::{DataPath, FieldOrComponent};
-pub use index_path::{IndexPath, IndexPathHash};
+pub use data_path::DataPath;
 pub use obj_path::{ObjPath, ObjPathHash};
-pub use obj_path_impl::{ObjPathCompRef, ObjPathImpl};
-pub use obj_type_path::ObjTypePath;
+pub use obj_path_impl::ObjPathImpl;
 pub use parse_path::{parse_obj_path, PathParseError};
 
 use re_string_interner::InternedString;
@@ -34,8 +21,8 @@ use re_string_interner::InternedString;
 use crate::Index;
 
 re_string_interner::declare_new_type!(
-    /// The name of an object field, e.g. `pos` or `color`.
-    pub struct FieldName;
+    /// The name of an object component, e.g. `pos` or `color`.
+    pub struct ComponentName;
 );
 
 // ----------------------------------------------------------------------------
@@ -49,15 +36,6 @@ pub enum ObjPathComp {
 
     /// Array/table/map member. Each member must be of the same type (homogenous).
     Index(Index),
-}
-
-impl ObjPathComp {
-    pub fn to_type_path_comp(&self) -> ObjTypePathComp {
-        match self {
-            Self::Name(name) => ObjTypePathComp::Name(*name),
-            Self::Index(_) => ObjTypePathComp::Index,
-        }
-    }
 }
 
 impl std::fmt::Display for ObjPathComp {
@@ -87,29 +65,6 @@ impl From<Index> for ObjPathComp {
     #[inline]
     fn from(comp: Index) -> Self {
         Self::Index(comp)
-    }
-}
-
-// ----------------------------------------------------------------------------
-
-/// The different parts that make up a [`ObjTypePath`].
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
-pub enum ObjTypePathComp {
-    /// Struct member
-    Name(InternedString),
-
-    /// Table (array/map) member.
-    /// Tables are homogenous, so it is the same type path for all.
-    Index,
-}
-
-impl std::fmt::Display for ObjTypePathComp {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Name(name) => name.fmt(f),
-            Self::Index => '*'.fmt(f),
-        }
     }
 }
 

crates/re_viewer/src/ui/mod.rs
@@ -34,21 +34,12 @@ pub use self::selection_history::{HistoricalSelection, SelectionHistory};
 pub use self::viewport::Viewport;
 
 pub(crate) use data_ui::UiVerbosity;
-use re_log_types::FieldOrComponent;
 
-pub fn format_component_name(name: &re_data_store::FieldName) -> String {
+pub fn format_component_name(name: &re_data_store::ComponentName) -> String {
     let name = name.as_str();
     if let Some(name) = name.strip_prefix("rerun.") {
         name.into()
     } else {
         name.into()
-    }
-}
-
-pub fn format_field_or_component_name(name: &FieldOrComponent) -> String {
-    match name {
-        FieldOrComponent::Field(name) | FieldOrComponent::Component(name) => {
-            format_component_name(name)
-        }
     }
 }

crates/re_data_store/Cargo.toml
@@ -42,7 +42,3 @@ rand = "0.8"
 
 [lib]
 bench = false
-
-[[bench]]
-name = "obj_query_benchmark"
-harness = false

crates/re_data_store/benches/obj_query_benchmark.rs
@@ -1,248 +1,0 @@
-#[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
-use criterion::{criterion_group, criterion_main, Criterion};
-use itertools::Itertools;
-use nohash_hasher::IntMap;
-
-use re_data_store::{
-    query::visit_type_data_2, DataStore, FieldName, Index, ObjTypePath, ObjTypePathComp, TimeInt,
-    TimeQuery, Timeline,
-};
-use re_log_types::{
-    obj_path, BatchIndex, Data, DataMsg, DataPath, DataVec, IndexHash, LoggedData, MsgId,
-    ObjectType, TimePoint, TimeType,
-};
-
-#[cfg(not(debug_assertions))]
-const NUM_FRAMES: i64 = 1_000;
-#[cfg(not(debug_assertions))]
-const NUM_POINTS: i64 = 1_000;
-
-// `cargo test` also runs the benchmark setup code, so make sure they run quickly:
-#[cfg(debug_assertions)]
-const NUM_FRAMES: i64 = 1;
-#[cfg(debug_assertions)]
-const NUM_POINTS: i64 = 1;
-
-fn timeline() -> Timeline {
-    Timeline::new("frame", TimeType::Sequence)
-}
-
-#[allow(dead_code)]
-#[derive(Clone, Copy)]
-struct Point {
-    pos: [f32; 3],
-    color: Option<[u8; 4]>,
-}
-
-fn do_query(obj_types: &IntMap<ObjTypePath, ObjectType>, data_store: &DataStore) -> Vec<Point> {
-    let time_query = TimeQuery::LatestAt(NUM_FRAMES / 2);
-    let timeline_store = data_store.get(&timeline()).unwrap();
-
-    let mut points = Vec::new();
-
-    for (obj_path, obj_store) in timeline_store.iter() {
-        let _ = obj_types.get(obj_path.obj_type_path()).unwrap();
-        visit_type_data_2(
-            obj_store,
-            &FieldName::from("pos"),
-            &time_query,
-            ("color", "radius"),
-            |_instance_index: Option<&IndexHash>,
-             _time: i64,
-             _msg_id: &MsgId,
-             pos: &[f32; 3],
-             color: Option<&[u8; 4]>,
-             _radius: Option<&f32>| {
-                points.push(Point {
-                    pos: *pos,
-                    color: color.cloned(),
-                });
-            },
-        );
-    }
-
-    assert_eq!(points.len(), NUM_POINTS as usize);
-    points
-}
-
-fn mono_data_messages() -> Vec<DataMsg> {
-    let mut messages = Vec::with_capacity((NUM_FRAMES * NUM_POINTS * 3) as _);
-    for frame_idx in 0..NUM_FRAMES {
-        for point_idx in 0..NUM_POINTS {
-            let mut time_point = TimePoint::default();
-            time_point.insert(timeline(), TimeInt::from(frame_idx));
-
-            let obj_path = obj_path!("points", Index::Sequence(point_idx as _));
-
-            messages.push(DataMsg {
-                msg_id: MsgId::random(),
-                time_point: time_point.clone(),
-                data_path: DataPath::new(obj_path.clone(), "pos".into()),
-                data: LoggedData::Single(Data::Vec3([1.0, 2.0, 3.0])),
-            });
-            messages.push(DataMsg {
-                msg_id: MsgId::random(),
-                time_point: time_point.clone(),
-                data_path: DataPath::new(obj_path.clone(), "color".into()),
-                data: LoggedData::Single(Data::Color([255, 255, 255, 255])),
-            });
-        }
-    }
-    messages
-}
-
-enum BatchType {
-    Sequential,
-    FullIndex,
-}
-
-fn batch_data_messages(batch_type: &BatchType) -> Vec<DataMsg> {
-    let positions = vec![[1.0, 2.0, 3.0]; NUM_POINTS as usize];
-    let colors = vec![[255; 4]; NUM_POINTS as usize];
-    let indices = match batch_type {
-        BatchType::Sequential => BatchIndex::SequentialIndex(NUM_POINTS as usize),
-        BatchType::FullIndex => BatchIndex::FullIndex(
-            (0..NUM_POINTS)
-                .map(|pi| Index::Sequence(pi as _))
-                .collect_vec(),
-        ),
-    };
-
-    let mut messages = Vec::with_capacity((NUM_FRAMES * 3) as _);
-
-    for frame_idx in 0..NUM_FRAMES {
-        let mut time_point = TimePoint::default();
-        time_point.insert(timeline(), TimeInt::from(frame_idx));
-
-        let obj_path = obj_path!("points");
-
-        messages.push(DataMsg {
-            msg_id: MsgId::random(),
-            time_point: time_point.clone(),
-            data_path: DataPath::new(obj_path.clone(), "pos".into()),
-            data: LoggedData::Batch {
-                indices: indices.clone(),
-                data: DataVec::Vec3(positions.clone()),
-            },
-        });
-        messages.push(DataMsg {
-            msg_id: MsgId::random(),
-            time_point: time_point.clone(),
-            data_path: DataPath::new(obj_path.clone(), "color".into()),
-            data: LoggedData::Batch {
-                indices: indices.clone(),
-                data: DataVec::Color(colors.clone()),
-            },
-        });
-    }
-
-    messages
-}
-
-fn insert_data(data_messages: &[DataMsg]) -> DataStore {
-    let mut store = DataStore::default();
-    for msg in data_messages {
-        store.insert_data_msg(msg).unwrap();
-    }
-    store
-}
-
-fn obj_mono_points(c: &mut Criterion) {
-    let data_messages = mono_data_messages();
-
-    let mut obj_types = IntMap::default();
-    obj_types.insert(
-        ObjTypePath::new(vec![
-            ObjTypePathComp::Name("points".into()),
-            ObjTypePathComp::Index,
-        ]),
-        ObjectType::Point3D,
-    );
-
-    {
-        let mut group = c.benchmark_group("obj_mono_points");
-        group.throughput(criterion::Throughput::Elements(
-            (NUM_POINTS * NUM_FRAMES) as _,
-        ));
-        group.bench_function("insert", |b| {
-            b.iter(|| insert_data(&data_messages));
-        });
-    }
-
-    {
-        let mut group = c.benchmark_group("obj_mono_points");
-        group.throughput(criterion::Throughput::Elements(NUM_POINTS as _));
-        let store = insert_data(&data_messages);
-        group.bench_function("query", |b| {
-            b.iter(|| do_query(&obj_types, &store));
-        });
-    }
-}
-
-fn obj_batch_points(c: &mut Criterion) {
-    let data_messages = batch_data_messages(&BatchType::FullIndex);
-
-    let mut obj_types = IntMap::default();
-    obj_types.insert(
-        ObjTypePath::new(vec![ObjTypePathComp::Name("points".into())]),
-        ObjectType::Point3D,
-    );
-
-    {
-        let mut group = c.benchmark_group("obj_batch_points");
-        group.throughput(criterion::Throughput::Elements(
-            (NUM_POINTS * NUM_FRAMES) as _,
-        ));
-        group.bench_function("insert", |b| {
-            b.iter(|| insert_data(&data_messages));
-        });
-    }
-
-    {
-        let mut group = c.benchmark_group("obj_batch_points");
-        group.throughput(criterion::Throughput::Elements(NUM_POINTS as _));
-        let store = insert_data(&data_messages);
-        group.bench_function("query", |b| {
-            b.iter(|| do_query(&obj_types, &store));
-        });
-    }
-}
-
-fn obj_batch_points_sequential(c: &mut Criterion) {
-    let data_messages = batch_data_messages(&BatchType::Sequential);
-
-    let mut obj_types = IntMap::default();
-    obj_types.insert(
-        ObjTypePath::new(vec![ObjTypePathComp::Name("points".into())]),
-        ObjectType::Point3D,
-    );
-
-    {
-        let mut group = c.benchmark_group("obj_batch_points_sequential");
-        group.throughput(criterion::Throughput::Elements(
-            (NUM_POINTS * NUM_FRAMES) as _,
-        ));
-        group.bench_function("insert", |b| {
-            b.iter(|| insert_data(&data_messages));
-        });
-    }
-
-    {
-        let mut group = c.benchmark_group("obj_batch_points_sequential");
-        group.throughput(criterion::Throughput::Elements(NUM_POINTS as _));
-        let store = insert_data(&data_messages);
-        group.bench_function("query", |b| {
-            b.iter(|| do_query(&obj_types, &store));
-        });
-    }
-}
-
-criterion_group!(
-    benches,
-    obj_mono_points,
-    obj_batch_points,
-    obj_batch_points_sequential
-);
-criterion_main!(benches);

crates/re_data_store/examples/memory_usage.rs
@@ -4,12 +4,12 @@ thread_local! {
     static LIVE_BYTES_IN_THREAD: AtomicUsize = AtomicUsize::new(0);
 }
 
-pub struct TrackingAllocator {
+struct TrackingAllocator {
     allocator: std::alloc::System,
 }
 
 #[global_allocator]
-pub static GLOBAL_ALLOCATOR: TrackingAllocator = TrackingAllocator {
+static GLOBAL_ALLOCATOR: TrackingAllocator = TrackingAllocator {
     allocator: std::alloc::System,
 };
 
@@ -48,201 +48,17 @@ fn live_bytes() -> usize {
 
 // ----------------------------------------------------------------------------
 
-use re_data_store::{BatchOrSplat, Index, ObjPath, TimelineStore};
 use re_log_types::{obj_path, MsgId};
 
-use itertools::Itertools as _;
-
 fn main() {
-    tracking_points();
-    big_clouds();
-    big_clouds_batched();
-    big_clouds_sequential_batched();
     log_messages();
-}
-
-fn obj_path_mono(camera: u64, index: u64) -> ObjPath {
-    obj_path!(
-        "camera",
-        Index::Sequence(camera),
-        "point",
-        Index::Sequence(index),
-    )
-}
-
-fn obj_path_batch(camera: u64) -> ObjPath {
-    obj_path!("camera", Index::Sequence(camera), "points",)
-}
-
-const OPTIMAL_BYTES_PER_POINT: usize = 3 * std::mem::size_of::<f32>(); // [f32; 3]
-
-pub static GLOBAL_MUTEXT: Option<std::sync::Mutex<()>> = None;
-
-fn tracking_points() {
-    let used_bytes_start = live_bytes();
-
-    const NUM_FRAMES: usize = 10_000;
-    const OVERLAP: usize = 100;
-
-    let mut num_points = 0;
-
-    let mut store = TimelineStore::default();
-    for frame in 0..NUM_FRAMES {
-        for offset in 0..OVERLAP {
-            store
-                .insert_mono::<[f32; 3]>(
-                    obj_path_mono(0, (frame + offset) as _),
-                    "pos".into(),
-                    frame,
-                    MsgId::random(),
-                    Some([1.0, 2.0, 3.0]),
-                )
-                .unwrap();
-            num_points += 1;
-        }
-    }
-
-    let used_bytes = live_bytes() - used_bytes_start;
-
-    let bytes_per_point = used_bytes as f32 / num_points as f32;
-    let overhead_factor = bytes_per_point / OPTIMAL_BYTES_PER_POINT as f32;
-
-    // NOTE: we are storing history for each point, so we will never get to OPTIMAL_BYTES_PER_POINT.
-    println!(
-        "individual points overhead_factor: {overhead_factor} (should ideally be just above 1)"
-    );
-}
-
-fn big_clouds() {
-    let used_bytes_start = live_bytes();
-
-    const NUM_CAMERAS: usize = 4;
-    const NUM_FRAMES: usize = 100;
-    const NUM_POINTS_PER_CAMERA: usize = 1_000;
-
-    let mut store = TimelineStore::default();
-    let mut frame = 0;
-    let mut num_points = 0;
-    while frame < NUM_FRAMES {
-        for camera in 0..NUM_CAMERAS {
-            for point in 0..NUM_POINTS_PER_CAMERA {
-                store
-                    .insert_mono::<[f32; 3]>(
-                        obj_path_mono(camera as _, point as _),
-                        "pos".into(),
-                        frame,
-                        MsgId::random(),
-                        Some([1.0, 2.0, 3.0]),
-                    )
-                    .unwrap();
-                num_points += 1;
-            }
-            frame += 1;
-        }
-    }
-
-    let used_bytes = live_bytes() - used_bytes_start;
-
-    let bytes_per_point = used_bytes as f32 / num_points as f32;
-    let overhead_factor = bytes_per_point / OPTIMAL_BYTES_PER_POINT as f32;
-
-    // NOTE: we are storing history for each point, so we will never get to OPTIMAL_BYTES_PER_POINT.
-    println!("big clouds overhead_factor: {overhead_factor} (should ideally be just above 1)");
-}
-
-fn big_clouds_batched() {
-    let used_bytes_start = live_bytes();
-
-    const NUM_CAMERAS: usize = 4;
-    const NUM_FRAMES: usize = 100;
-    const NUM_POINTS_PER_CAMERA: usize = 1_000;
-
-    let indices = (0..NUM_POINTS_PER_CAMERA)
-        .map(|i| Index::Sequence(i as _))
-        .collect_vec();
-    let point: [f32; 3] = [1.0, 2.0, 3.0];
-    let positions = vec![point; NUM_POINTS_PER_CAMERA];
-
-    let mut store = TimelineStore::default();
-    let mut frame = 0;
-    let mut num_points = 0;
-    while frame < NUM_FRAMES {
-        for camera in 0..NUM_CAMERAS {
-            let batch = BatchOrSplat::new_batch(&indices, &positions).unwrap();
-            store
-                .insert_batch::<[f32; 3]>(
-                    obj_path_batch(camera as _),
-                    "pos".into(),
-                    frame,
-                    MsgId::random(),
-                    batch,
-                )
-                .unwrap();
-
-            num_points += NUM_POINTS_PER_CAMERA;
-
-            frame += 1;
-        }
-    }
-
-    let used_bytes = live_bytes() - used_bytes_start;
-
-    let bytes_per_point = used_bytes as f32 / num_points as f32;
-    let overhead_factor = bytes_per_point / OPTIMAL_BYTES_PER_POINT as f32;
-
-    // Since we are only storing history for the entire batch, we should be able to approach OPTIMAL_BYTES_PER_POINT.
-    println!(
-        "big clouds batched overhead_factor: {overhead_factor} (should ideally be just above 1)"
-    );
-}
-
-fn big_clouds_sequential_batched() {
-    let used_bytes_start = live_bytes();
-
-    const NUM_CAMERAS: usize = 4;
-    const NUM_FRAMES: usize = 100;
-    const NUM_POINTS_PER_CAMERA: usize = 1_000;
-
-    let point: [f32; 3] = [1.0, 2.0, 3.0];
-    let positions = vec![point; NUM_POINTS_PER_CAMERA];
-
-    let mut store = TimelineStore::default();
-    let mut frame = 0;
-    let mut num_points = 0;
-    while frame < NUM_FRAMES {
-        for camera in 0..NUM_CAMERAS {
-            let batch = BatchOrSplat::new_sequential_batch(&positions).unwrap();
-            store
-                .insert_batch::<[f32; 3]>(
-                    obj_path_batch(camera as _),
-                    "pos".into(),
-                    frame,
-                    MsgId::random(),
-                    batch,
-                )
-                .unwrap();
-
-            num_points += NUM_POINTS_PER_CAMERA;
-
-            frame += 1;
-        }
-    }
-
-    let used_bytes = live_bytes() - used_bytes_start;
-
-    let bytes_per_point = used_bytes as f32 / num_points as f32;
-    let overhead_factor = bytes_per_point / OPTIMAL_BYTES_PER_POINT as f32;
-
-    // Since we are only storing history for the entire batch, we should be able to approach OPTIMAL_BYTES_PER_POINT.
-    println!("big clouds sequential batched overhead_factor: {overhead_factor} (should ideally be just above 1)");
 }
 
 fn log_messages() {
     use re_log_types::{
         datagen::{build_frame_nr, build_some_point2d},
         msg_bundle::try_build_msg_bundle1,
-        ArrowMsg, BatchIndex, Data, DataMsg, DataPath, DataVec, FieldName, LogMsg, LoggedData,
-        TimeInt, TimePoint, Timeline,
+        ArrowMsg, LogMsg, TimeInt, TimePoint, Timeline,
     };
 
     // Note: we use Box in this function so that we also count the "static"
@@ -274,11 +90,9 @@ fn log_messages() {
         bytes_used
     }
 
-    const POS: [f32; 2] = [2.0, 3.0];
     const NUM_POINTS: usize = 1_000;
 
     let timeline = Timeline::new_sequence("frame_nr");
-    let pos_field_name = FieldName::from("pos");
     let mut time_point = TimePoint::default();
     time_point.insert(timeline, TimeInt::from(0));
 
@@ -288,22 +102,6 @@ fn log_messages() {
         let used_bytes = live_bytes() - used_bytes_start;
         println!("Short ObjPath uses {used_bytes} bytes in RAM");
         drop(obj_path);
-    }
-
-    {
-        let used_bytes_start = live_bytes();
-        let log_msg = Box::new(LogMsg::DataMsg(DataMsg {
-            msg_id: MsgId::random(),
-            time_point: time_point.clone(),
-            data_path: DataPath::new(obj_path!("points"), pos_field_name),
-            data: Data::Vec2(POS).into(),
-        }));
-        let log_msg_bytes = live_bytes() - used_bytes_start;
-        let encoded = encode_log_msg(&log_msg);
-        println!(
-            "Classic LogMsg containing a Pos2 uses {}-{log_msg_bytes} bytes in RAM, and {} bytes encoded",
-            size_decoded(&encoded), encoded.len()
-        );
     }
 
     {
@@ -324,32 +122,6 @@ fn log_messages() {
         let encoded = encode_log_msg(&log_msg);
         println!(
             "Arrow LogMsg containing a Pos2 uses {}-{log_msg_bytes} bytes in RAM, and {} bytes encoded",
-            size_decoded(&encoded), encoded.len()
-        );
-    }
-
-    {
-        use rand::Rng as _;
-        let mut rng = rand::thread_rng();
-
-        let used_bytes_start = live_bytes();
-        let log_msg = Box::new(LogMsg::DataMsg(DataMsg {
-            msg_id: MsgId::random(),
-            time_point: time_point.clone(),
-            data_path: DataPath::new(obj_path!("points"), pos_field_name),
-            data: LoggedData::Batch {
-                indices: BatchIndex::SequentialIndex(NUM_POINTS),
-                data: DataVec::Vec2(
-                    (0..NUM_POINTS)
-                        .map(|_| [rng.gen_range(0.0..10.0), rng.gen_range(0.0..10.0)])
-                        .collect(),
-                ),
-            },
-        }));
-        let log_msg_bytes = live_bytes() - used_bytes_start;
-        let encoded = encode_log_msg(&log_msg);
-        println!(
-            "Classic LogMsg containing {NUM_POINTS}x Pos2 uses {}-{log_msg_bytes} bytes in RAM, and {} bytes encoded",
             size_decoded(&encoded), encoded.len()
         );
     }

commit 630d8cbe9732418c3c06ca2d99a65acb2d6f5119

Fix hovering bug introduced in The Great Removal
Bug introduced by https://github.com/rerun-io/rerun/pull/988

Next up I'll refactor and simplify `InstanceId` and `InstanceIdHash`

crates/re_data_store/src/instance.rs
@@ -107,15 +107,17 @@ impl InstanceIdHash {
         arrow_instance: None,
     };
 
+    /// Indicate the whole entity.
     #[inline]
-    pub fn from_path_and_index(entity_path: &EntityPath, instance_index: IndexHash) -> Self {
+    pub fn from_path(entity_path: &EntityPath) -> Self {
         Self {
             entity_path_hash: entity_path.hash(),
-            instance_index_hash: instance_index,
+            instance_index_hash: IndexHash::NONE,
             arrow_instance: None,
         }
     }
 
+    /// Indicate a specific instance of the entity.
     #[inline]
     pub fn from_path_and_arrow_instance(
         entity_path: &EntityPath,
@@ -139,18 +141,16 @@ impl InstanceIdHash {
     }
 
     pub fn resolve(&self, entity_db: &EntityDb) -> Option<InstanceId> {
-        match self.arrow_instance {
-            None => {
-                re_log::error_once!("Found classical InstanceIdHash");
-                None
-            }
-            Some(arrow_instance) => Some(InstanceId {
-                entity_path: entity_db
-                    .entity_path_from_hash(&self.entity_path_hash)
-                    .cloned()?,
-                instance_index: Some(Index::ArrowInstance(arrow_instance)),
-            }),
-        }
+        let entity_path = entity_db
+            .entity_path_from_hash(&self.entity_path_hash)
+            .cloned()?;
+
+        let instance_index = self.arrow_instance.map(Index::ArrowInstance);
+
+        Some(InstanceId {
+            entity_path,
+            instance_index,
+        })
     }
 
     /// Does this entity match this instance id?

crates/re_viewer/src/ui/view_spatial/scene/mod.rs
@@ -4,7 +4,7 @@ use ahash::HashMap;
 use re_data_store::{EntityPath, InstanceIdHash};
 use re_log_types::{
     component_types::{ClassId, KeypointId, Tensor},
-    IndexHash, MeshId,
+    MeshId,
 };
 use re_renderer::{Color32, Size};
 
@@ -126,16 +126,9 @@ pub struct SceneSpatial {
     pub space_cameras: Vec<SpaceCamera3D>,
 }
 
-fn instance_hash_if_interactive(
-    entity_path: &EntityPath,
-    instance_index: Option<&IndexHash>,
-    interactive: bool,
-) -> InstanceIdHash {
+fn instance_hash_if_interactive(entity_path: &EntityPath, interactive: bool) -> InstanceIdHash {
     if interactive {
-        InstanceIdHash::from_path_and_index(
-            entity_path,
-            instance_index.copied().unwrap_or(IndexHash::NONE),
-        )
+        InstanceIdHash::from_path(entity_path)
     } else {
         InstanceIdHash::NONE
     }
@@ -255,7 +248,7 @@ impl SceneSpatial {
         interactive: bool,
     ) {
         // Generate keypoint connections if any.
-        let instance_hash = instance_hash_if_interactive(entity_path, None, interactive);
+        let instance_hash = instance_hash_if_interactive(entity_path, interactive);
 
         let mut line_batch = self.primitives.line_strips.batch("keypoint connections");
 

crates/re_viewer/src/ui/view_spatial/scene/scene_part/mod.rs
@@ -52,7 +52,7 @@ pub fn instance_hash_for_picking<T: re_log_types::msg_bundle::Component>(
 ) -> InstanceIdHash {
     if props.interactive {
         if entity_view.num_instances() == 1 || !entity_highlight.any_selection_highlight() {
-            InstanceIdHash::from_path_and_index(ent_path, re_log_types::IndexHash::NONE)
+            InstanceIdHash::from_path(ent_path)
         } else {
             InstanceIdHash::from_path_and_arrow_instance(ent_path, &instance)
         }