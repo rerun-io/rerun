618186d1a8e6395ad4d8224b647fae5c8910f5dd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-01-30 16:33:25 +0100

    Fix wrong benchmark_group name

--- crates/re_tuid/benches/bench_tuid.rs
+++ crates/re_tuid/benches/bench_tuid.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn bench_tuid(c: &mut Criterion) {
-    let mut group = c.benchmark_group("obj_batch_points_sequential");
+    let mut group = c.benchmark_group("tuid");
     group.throughput(criterion::Throughput::Elements(1));
     group.bench_function("Tuid::random", |b| {
         b.iter(|| criterion::black_box(re_tuid::Tuid::random()));
