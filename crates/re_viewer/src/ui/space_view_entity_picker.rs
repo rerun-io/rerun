7e085f32b0af8691c624afbed26c4d44f9f619af
Andreas Reich <andreas@rerun.io>
2023-01-31 18:32:04 +0100

    improve enable/disable states

--- crates/re_viewer/src/ui/space_view_entity_picker.rs
+++ crates/re_viewer/src/ui/space_view_entity_picker.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use nohash_hasher::IntMap;
 use re_arrow_store::Timeline;
 use re_data_store::{EntityPath, EntityTree, InstancePath};
 
@@ -71,6 +72,8 @@ fn add_entities_ui(ctx: &mut ViewerContext<'_>, ui: &mut egui::Ui, space_view: &
     let spaces_info = SpaceInfoCollection::new(&ctx.log_db.entity_db);
     // TODO(andreas): remove use space_view.root_path, just show everything
     if let Some(tree) = ctx.log_db.entity_db.tree.subtree(&space_view.root_path) {
+        let entities_add_info = create_entity_add_info(ctx, tree, space_view, &spaces_info);
+
         add_entities_tree_ui(
             ctx,
             ui,
@@ -78,7 +81,7 @@ fn add_entities_ui(ctx: &mut ViewerContext<'_>, ui: &mut egui::Ui, space_view: &
             &tree.path.to_string(),
             tree,
             space_view,
-            0,
+            &entities_add_info,
         );
     }
 }
@@ -90,7 +93,7 @@ fn add_entities_tree_ui(
     name: &str,
     tree: &EntityTree,
     space_view: &mut SpaceView,
-    level: u32,
+    entities_add_info: &IntMap<EntityPath, EntityAddInfo>,
 ) {
     if tree.is_leaf() {
         add_entities_line_ui(
@@ -100,8 +103,10 @@ fn add_entities_tree_ui(
             &format!("🔹 {name}"),
             &tree.path,
             space_view,
+            entities_add_info,
         );
     } else {
+        let level = tree.path.len();
         let default_open = space_view.space_path.is_descendant_of(&tree.path)
             || tree.children.len() <= 3
             || level < 1;
@@ -111,10 +116,17 @@ fn add_entities_tree_ui(
             default_open,
         )
         .show_header(ui, |ui| {
-            add_entities_line_ui(ctx, ui, spaces_info, name, &tree.path, space_view);
+            add_entities_line_ui(
+                ctx,
+                ui,
+                spaces_info,
+                name,
+                &tree.path,
+                space_view,
+                entities_add_info,
+            );
         })
         .body(|ui| {
-            let level = level + 1;
             for (path_comp, child_tree) in tree.children.iter().sorted_by_key(|(_, child_tree)| {
                 // Put descendants of the space path always first
                 i32::from(
@@ -129,7 +141,7 @@ fn add_entities_tree_ui(
                     &path_comp.to_string(),
                     child_tree,
                     space_view,
-                    level,
+                    entities_add_info,
                 );
             }
         });
@@ -143,6 +155,7 @@ fn add_entities_line_ui(
     name: &str,
     entity_path: &EntityPath,
     space_view: &mut SpaceView,
+    entities_add_info: &IntMap<EntityPath, EntityAddInfo>,
 ) {
     ui.horizontal(|ui| {
         let space_view_id = if space_view.data_blueprint.contains_entity(entity_path) {
@@ -150,21 +163,26 @@ fn add_entities_line_ui(
         } else {
             None
         };
+        let add_info = entities_add_info.get(entity_path).unwrap();
 
-        let widget_text = if entity_path == &space_view.space_path {
-            egui::RichText::new(name).strong()
-        } else {
-            egui::RichText::new(name)
-        };
-        let response = ctx.instance_path_button_to(
-            ui,
-            space_view_id,
-            &InstancePath::entity_splat(entity_path.clone()),
-            widget_text,
-        );
-        if entity_path == &space_view.space_path {
-            response.highlight();
-        }
+        // To be safe, let's put the add/remove button outside of this enabled block for the name display.
+        // (i.e. if for some reason it was already added, we want it to be still be removable!)
+        ui.add_enabled_ui(add_info.can_add_self_or_any_child, |ui| {
+            let widget_text = if entity_path == &space_view.space_path {
+                egui::RichText::new(name).strong()
+            } else {
+                egui::RichText::new(name)
+            };
+            let response = ctx.instance_path_button_to(
+                ui,
+                space_view_id,
+                &InstancePath::entity_splat(entity_path.clone()),
+                widget_text,
+            );
+            if entity_path == &space_view.space_path {
+                response.highlight();
+            }
+        });
 
         ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
             let entity_tree = &ctx.log_db.entity_db.tree;
@@ -173,7 +191,7 @@ fn add_entities_line_ui(
                 if ctx
                     .re_ui
                     .small_icon_button(ui, &re_ui::icons::REMOVE)
-                    .on_hover_text("Remove this Entity from the Space View")
+                    .on_hover_text("Remove this Entity and all its children from the Space View")
                     .clicked()
                 {
                     // Remove all entities at and under this path
@@ -186,28 +204,14 @@ fn add_entities_line_ui(
                         });
                 }
             } else {
-                let entity_category =
-                    categorize_entity_path(Timeline::log_time(), ctx.log_db, entity_path);
-                let cannot_add_reason = if entity_category.contains(space_view.category) {
-                    spaces_info
-                        .is_reachable_by_transform(entity_path, &space_view.space_path)
-                        .map_err(|reason| reason.to_string())
-                        .err()
-                } else if entity_category.is_empty() {
-                    Some("Entity does not have any components".to_owned())
-                } else {
-                    Some(
-                        "Entity category can't be displayed by this type of spatial view"
-                            .to_owned(),
-                    )
-                };
-
                 let response = ui
-                    .add_enabled_ui(cannot_add_reason.is_none(), |ui| {
+                    .add_enabled_ui(add_info.can_add_self_or_any_child, |ui| {
                         let response = ctx
                             .re_ui
                             .small_icon_button(ui, &re_ui::icons::ADD)
-                            .on_hover_text("Add this Entity to the Space View");
+                            .on_hover_text(
+                                "Add this Entity and all its children to the Space View",
+                            );
                         if response.clicked() {
                             // Insert the entity it space_view and all its children as far as they haven't been added yet
                             let mut entities = Vec::new();
@@ -238,12 +242,69 @@ fn add_entities_line_ui(
                     })
                     .response;
 
-                if let Some(cannot_add_reason) = cannot_add_reason {
+                if let Some(cannot_add_reason) = &add_info.cannot_add_reason {
                     response.on_hover_text(cannot_add_reason);
                 }
             }
         });
     });
+}
+
+#[derive(Default)]
+struct EntityAddInfo {
+    _categories: enumset::EnumSet<ViewCategory>, // TODO(andreas): Should use this to display icons
+    cannot_add_reason: Option<String>,
+    can_add_self_or_any_child: bool,
+}
+
+fn create_entity_add_info(
+    ctx: &mut ViewerContext<'_>,
+    tree: &EntityTree,
+    space_view: &mut SpaceView,
+    spaces_info: &SpaceInfoCollection,
+) -> IntMap<EntityPath, EntityAddInfo> {
+    let mut meta_data: IntMap<EntityPath, EntityAddInfo> = IntMap::default();
+
+    tree.visit_children_recursively(&mut |entity_path| {
+        let categories = categorize_entity_path(Timeline::log_time(), ctx.log_db, entity_path);
+        let cannot_add_reason = if categories.contains(space_view.category) {
+            spaces_info
+                .is_reachable_by_transform(entity_path, &space_view.space_path)
+                .map_err(|reason| reason.to_string())
+                .err()
+        } else if categories.is_empty() {
+            Some("Entity does not have any components".to_owned())
+        } else {
+            Some(format!(
+                "Entity can't be displayed by this type of Space View {}",
+                space_view.category
+            ))
+        };
+
+        if cannot_add_reason.is_none() {
+            // Mark parents that there is something that can be added.
+            let mut path = entity_path.clone();
+            while let Some(parent) = path.parent() {
+                meta_data
+                    .entry(parent.clone())
+                    .or_default()
+                    .can_add_self_or_any_child = true;
+                path = parent;
+            }
+        }
+
+        let can_add_self_or_any_child = cannot_add_reason.is_none();
+        meta_data.insert(
+            entity_path.clone(),
+            EntityAddInfo {
+                _categories: categories,
+                cannot_add_reason,
+                can_add_self_or_any_child,
+            },
+        );
+    });
+
+    meta_data
 }
 
 fn title_bar(re_ui: &re_ui::ReUi, ui: &mut egui::Ui, title: &str, open: &mut bool) {
