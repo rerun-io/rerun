6ea166e098219e3dedfc316a5c312d129f0661b2
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-02-02 15:31:03 +0100

    Less right-ward drift

--- crates/re_viewer/src/ui/data_ui/component.rs
+++ crates/re_viewer/src/ui/data_ui/component.rs
@@ -32,6 +32,14 @@ impl DataUi for EntityComponentWithInstances {
     ) {
         crate::profile_function!(self.component_name().full_name());
 
+        let mut instance_keys = match self.component_data.iter_instance_keys() {
+            Ok(instance_keys) => instance_keys,
+            Err(err) => {
+                ui.label(ctx.re_ui.error_text(format!("Error: {err}")));
+                return;
+            }
+        };
+
         let num_instances = self.num_instances();
 
         let max_elems = match verbosity {
@@ -40,73 +48,66 @@ impl DataUi for EntityComponentWithInstances {
             crate::ui::UiVerbosity::All => 20,
         };
 
-        match self.component_data.iter_instance_keys() {
-            Ok(mut instance_keys) => {
-                if num_instances == 0 {
-                    ui.weak("(empty)");
-                } else if num_instances == 1 {
-                    if let Some(instance_key) = instance_keys.next() {
-                        ctx.component_ui_registry.ui(
-                            ctx,
-                            ui,
-                            verbosity,
-                            query,
-                            &self.component_data,
-                            &instance_key,
-                        );
-                    } else {
-                        ui.label(ctx.re_ui.error_text("Error: missing instance key"));
-                    }
-                } else if max_elems == 1 {
-                    ui.label(format!("{num_instances} values"));
-                } else {
-                    egui_extras::TableBuilder::new(ui)
-                        .resizable(false)
-                        .vscroll(true)
-                        .auto_shrink([false, true])
-                        .max_scroll_height(100.0)
-                        .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                        .columns(egui_extras::Column::auto(), 2)
-                        .header(re_ui::ReUi::table_header_height(), |mut header| {
-                            re_ui::ReUi::setup_table_header(&mut header);
-                            header.col(|ui| {
-                                ui.label("Instance key");
-                            });
-                            header.col(|ui| {
-                                ui.label(self.component_name().short_name());
-                            });
-                        })
-                        .body(|mut body| {
-                            re_ui::ReUi::setup_table_body(&mut body);
-                            let row_height = re_ui::ReUi::table_line_height();
-                            body.rows(row_height, num_instances, |index, mut row| {
-                                if let Some(instance_key) = self
-                                    .component_data
-                                    .iter_instance_keys()
-                                    .ok()
-                                    .and_then(|mut keys| keys.nth(index))
-                                {
-                                    row.col(|ui| {
-                                        ui.label(instance_key.to_string());
-                                    });
-                                    row.col(|ui| {
-                                        ctx.component_ui_registry.ui(
-                                            ctx,
-                                            ui,
-                                            crate::ui::UiVerbosity::Small,
-                                            query,
-                                            &self.component_data,
-                                            &instance_key,
-                                        );
-                                    });
-                                }
-                            });
-                        });
-                }
+        if num_instances == 0 {
+            ui.weak("(empty)");
+        } else if num_instances == 1 {
+            if let Some(instance_key) = instance_keys.next() {
+                ctx.component_ui_registry.ui(
+                    ctx,
+                    ui,
+                    verbosity,
+                    query,
+                    &self.component_data,
+                    &instance_key,
+                );
+            } else {
+                ui.label(ctx.re_ui.error_text("Error: missing instance key"));
             }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(format!("Error: {err}")));
-            }
+        } else if max_elems == 1 {
+            ui.label(format!("{num_instances} values"));
+        } else {
+            egui_extras::TableBuilder::new(ui)
+                .resizable(false)
+                .vscroll(true)
+                .auto_shrink([false, true])
+                .max_scroll_height(100.0)
+                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                .columns(egui_extras::Column::auto(), 2)
+                .header(re_ui::ReUi::table_header_height(), |mut header| {
+                    re_ui::ReUi::setup_table_header(&mut header);
+                    header.col(|ui| {
+                        ui.label("Instance key");
+                    });
+                    header.col(|ui| {
+                        ui.label(self.component_name().short_name());
+                    });
+                })
+                .body(|mut body| {
+                    re_ui::ReUi::setup_table_body(&mut body);
+                    let row_height = re_ui::ReUi::table_line_height();
+                    body.rows(row_height, num_instances, |index, mut row| {
+                        if let Some(instance_key) = self
+                            .component_data
+                            .iter_instance_keys()
+                            .ok()
+                            .and_then(|mut keys| keys.nth(index))
+                        {
+                            row.col(|ui| {
+                                ui.label(instance_key.to_string());
+                            });
+                            row.col(|ui| {
+                                ctx.component_ui_registry.ui(
+                                    ctx,
+                                    ui,
+                                    crate::ui::UiVerbosity::Small,
+                                    query,
+                                    &self.component_data,
+                                    &instance_key,
+                                );
+                            });
+                        }
+                    });
+                });
         }
     }
 }
