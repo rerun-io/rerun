92933c039b6969b0764a9d1322525846f98f993d
Jeremy Leibs <jeremy@rerun.io>
2023-02-05 05:20:16 +0100

    Ship rerun package inside of a rerun_sdk folder (#1085)
    * Ship rerun package inside of a rerun_sdk folder

--- rerun_py/rerun_sdk/rerun/__main__.py
+++ rerun_py/rerun_sdk/rerun/__main__.py
@@ -2,13 +2,13 @@
 
 import sys
 
-from rerun import rerun_bindings, unregister_shutdown  # type: ignore[attr-defined]
+from rerun import bindings, unregister_shutdown  # type: ignore[attr-defined]
 
 
 def main() -> None:
     # We don't need to call shutdown in this case. Rust should be handling everything
     unregister_shutdown()
-    exit(rerun_bindings.main(sys.argv))
+    exit(bindings.main(sys.argv))
 
 
 if __name__ == "__main__":
