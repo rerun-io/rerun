0348a7131e6db8097490ceb7c31eaad5e2b69844
Emil Ernerfeldt <emil@rerun.io>
2023-02-06 15:56:34 +0100

    Entity
    Co-authored-by: Andreas Reich <andreas@rerun.io>

--- crates/re_viewer/src/ui/selection_panel.rs
+++ crates/re_viewer/src/ui/selection_panel.rs
@@ -388,7 +388,7 @@ fn entity_props_ui(
                             .speed(speed)
                             .suffix("s"),
                     )
-                    .on_hover_text("Include this much history of the entity in the Space View");
+                    .on_hover_text("Include this much history of the Entity in the Space View.");
                     visible_history.nanos = (time_sec * 1e9).round() as _;
                 }
                 TimeType::Sequence => {
