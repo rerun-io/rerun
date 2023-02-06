4fbd9b5efe6cb7df4ec68626f12ab660bd746755
Emil Ernerfeldt <emil@rerun.io>
2023-02-06 14:32:07 +0100

    Use less "arrow" in re_viewer (#1105)
    * Rename EntityDb::arrow_store to data_store
    * arrow_query -> latest_at_query

--- crates/re_viewer/src/ui/view_spatial/scene/scene_part/boxes2d.rs
+++ crates/re_viewer/src/ui/view_spatial/scene/scene_part/boxes2d.rs
@@ -102,7 +102,7 @@ impl ScenePart for Boxes2DPart {
             let entity_highlight = highlights.entity_highlight(ent_path.hash());
 
             match query_primary_with_history::<Rect2D, 6>(
-                &ctx.log_db.entity_db.arrow_store,
+                &ctx.log_db.entity_db.data_store,
                 &query.timeline,
                 &query.latest_at,
                 &props.visible_history,
