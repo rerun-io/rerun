e322f2b9a972c3906a2f67465d6b2fda75ca7187
Clement Rey <cr.rey.clement@gmail.com>
2023-02-08 18:50:58 +0100

    use new show() in barebone examples

--- examples/minimal/src/main.rs
+++ examples/minimal/src/main.rs
@@ -1,17 +1,15 @@
 //! Demonstrates the most barebone usage of the Rerun SDK.
 
-use rerun::{MsgSender, MsgSenderError, Point3D, Session};
+use rerun::{MsgSender, Point3D, Session};
 
-fn main() -> Result<(), MsgSenderError> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut session = Session::new();
 
     MsgSender::new("my_point")
         .with_component(&[Point3D::new(1., 1., 1.)])?
         .send(&mut session)?;
 
-    // TODO: make that friendlier for the first impression
-    let log_messages = session.drain_log_messages_buffer();
-    rerun::viewer::show(log_messages).unwrap();
+    session.show()?;
 
     Ok(())
 }
