73d0a3a6cd0b40f5f511d391954b6b4c91c04a6f
Jeremy Leibs <jeremy@rerun.io>
2023-02-09 16:28:43 +0100

    Start TcpStream in an Init state (#1157)
    * Start TcpStream in an Init state
    
    * Improve docstrings
    
    * fix typo

--- crates/re_sdk_comms/src/tcp_client.rs
+++ crates/re_sdk_comms/src/tcp_client.rs
@@ -3,10 +3,47 @@ use std::{
     net::{SocketAddr, TcpStream},
 };
 
+/// State of the [`TcpStream`]
+///
+/// Because the [`TcpClient`] lazily connects on [`TcpClient::send`], it needs a
+/// very simple state machine to track the state of the connection. A trinary
+/// state is used to specifically differentiate between
+/// [`TcpStreamState::Pending`] which is still a nominal state for any new tcp
+/// connection, and [`TcpStreamState::Disconnected`] which implies either a
+/// failure to connect, or an error on an already established stream.
+enum TcpStreamState {
+    /// The [`TcpStream`] is yet to be connected.
+    ///
+    /// Behavior: Try to connect on next [`TcpClient::send()`]
+    ///
+    /// Transitions:
+    ///  - Pending -> Connected on successful connection.
+    ///  - Pending -> Disconnected on failed connection.
+    Pending,
+
+    /// A healthy [`TcpStream`] ready to send packets
+    ///
+    /// Behavior: Send packets on [`TcpClient::send()`]
+    ///
+    /// Transitions:
+    ///  - Connected -> Disconnected on send error
+    ///  - Connected -> Pending on [`TcpClient::set_addr`]
+    Connected(TcpStream),
+
+    /// A broken [`TcpStream`] which experienced a failure to connect or send
+    ///
+    /// Behavior: Try to re-connect on next [`TcpClient::send()`]
+    ///
+    /// Transitions:
+    ///  - Disconnected -> Connected on successful connection.
+    ///  - Disconnected -> Pending on [`TcpClient::set_addr`]
+    Disconnected,
+}
+
 /// Connect to a rerun server and send log messages.
 pub struct TcpClient {
     addrs: Vec<SocketAddr>,
-    stream: Option<TcpStream>,
+    stream_state: TcpStreamState,
 }
 
 impl Default for TcpClient {
@@ -19,7 +56,7 @@ impl TcpClient {
     pub fn new(addr: SocketAddr) -> Self {
         Self {
             addrs: vec![addr],
-            stream: None,
+            stream_state: TcpStreamState::Pending,
         }
     }
 
@@ -27,26 +64,28 @@ impl TcpClient {
         let addrs = vec![addr];
         if addrs != self.addrs {
             self.addrs = addrs;
-            self.stream = None;
+            self.stream_state = TcpStreamState::Pending;
         }
     }
 
     /// return `false` on failure. Does nothing if already connected.
     pub fn connect(&mut self) -> anyhow::Result<()> {
-        if self.stream.is_some() {
+        if let TcpStreamState::Connected(_) = self.stream_state {
             Ok(())
         } else {
             re_log::debug!("Connecting to {:?}…", self.addrs);
             match TcpStream::connect(&self.addrs[..]) {
                 Ok(mut stream) => {
                     if let Err(err) = stream.write(&crate::PROTOCOL_VERSION.to_le_bytes()) {
+                        self.stream_state = TcpStreamState::Disconnected;
                         anyhow::bail!("Failed to send to Rerun server at {:?}: {err}", self.addrs);
                     } else {
-                        self.stream = Some(stream);
+                        self.stream_state = TcpStreamState::Connected(stream);
                         Ok(())
                     }
                 }
                 Err(err) => {
+                    self.stream_state = TcpStreamState::Disconnected;
                     anyhow::bail!(
                         "Failed to connect to Rerun server at {:?}: {err}",
                         self.addrs
@@ -62,15 +101,15 @@ impl TcpClient {
 
         self.connect()?;
 
-        if let Some(stream) = &mut self.stream {
+        if let TcpStreamState::Connected(stream) = &mut self.stream_state {
             re_log::trace!("Sending a packet of size {}…", packet.len());
             if let Err(err) = stream.write(&(packet.len() as u32).to_le_bytes()) {
-                self.stream = None;
+                self.stream_state = TcpStreamState::Disconnected;
                 anyhow::bail!("Failed to send to Rerun server at {:?}: {err}", self.addrs);
             }
 
             if let Err(err) = stream.write(packet) {
-                self.stream = None;
+                self.stream_state = TcpStreamState::Disconnected;
                 anyhow::bail!("Failed to send to Rerun server at {:?}: {err}", self.addrs);
             }
 
@@ -82,15 +121,22 @@ impl TcpClient {
 
     /// Wait until all logged data have been sent.
     pub fn flush(&mut self) {
-        if let Some(stream) = &mut self.stream {
+        if let TcpStreamState::Connected(stream) = &mut self.stream_state {
             if let Err(err) = stream.flush() {
                 re_log::warn!("Failed to flush: {err}");
+                self.stream_state = TcpStreamState::Disconnected;
             }
         }
         re_log::trace!("TCP stream flushed.");
     }
 
-    pub fn is_connected(&self) -> bool {
-        self.stream.is_some()
+    /// Check if the underlying [`TcpStream`] has entered the [`TcpStreamState::Disconnected`] state
+    ///
+    /// Note that this only occurs after a failure to connect or a failure to send.
+    pub fn has_disconnected(&self) -> bool {
+        match self.stream_state {
+            TcpStreamState::Pending | TcpStreamState::Connected(_) => false,
+            TcpStreamState::Disconnected => true,
+        }
     }
 }
