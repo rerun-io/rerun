5d6400bcaf74c6f3f18032e37525dc7f9fccf675
Jeremy Leibs <jeremy@rerun.io>
2023-02-09 17:16:53 +0100

    Lints

--- rerun_py/rerun_sdk/rerun_demo/data.py
+++ rerun_py/rerun_sdk/rerun_demo/data.py
@@ -1,4 +1,4 @@
-""" Simple data to be used for rerun demos. """
+"""Simple data to be used for rerun demos."""
 
 from collections import namedtuple
 
@@ -10,7 +10,8 @@ ColorGrid = namedtuple("ColorGrid", ["positions", "colors"])
 
 
 def build_color_grid(x_count=6, y_count=6, z_count=6):
-    """Create a cube of points with colors.
+    """
+    Create a cube of points with colors.
 
     The total point cloud will have x_count * y_count * z_count points.
 
@@ -18,6 +19,7 @@ def build_color_grid(x_count=6, y_count=6, z_count=6):
     ----------
     x_count, y_count, z_count:
         Number of points in each dimension.
+
     """
     x, y, z = np.meshgrid(np.linspace(-10, 10, x_count), np.linspace(-10, 10, y_count), np.linspace(-10, 10, z_count))
     r, g, b = np.meshgrid(np.linspace(0, 255, x_count), np.linspace(0, 255, y_count), np.linspace(0, 255, z_count))
@@ -34,7 +36,8 @@ RectPyramid = namedtuple("RectPyramid", ["rects", "format", "colors"])
 
 
 def build_rect_pyramid(count=20, width=100, height=100):
-    """Create a stack of N colored rectangles.
+    """
+    Create a stack of N colored rectangles.
 
     Parameters
     ----------
@@ -44,6 +47,7 @@ def build_rect_pyramid(count=20, width=100, height=100):
         Width of the base of the pyramid.
     height:
         Height of the pyramid.
+
     """
     x = np.zeros(count)
     y = np.linspace(0, height, count)
