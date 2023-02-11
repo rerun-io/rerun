9506e1d699e641cd02f091f17284afa3547cedda
Emil Ernerfeldt <emil@rerun.io>
2023-02-11 06:14:27 +0100

    Enable `--web-viewer` in the Python Wheels (#1182)
    * Make 'web' a default feature of the python sdk
    
    * Add sanity-checking to crates/re_web_server/build.rs
    
    * Allow crates/re_web_server/build.rs to be run from any CWD
    
    * Run setup_web.sh on the Python CI
    
    * Turn on the web feature on the CI
    
    * Only enable the web feature when building pypi package
    
    * Improve setup_web.sh
    
    * Add winget package manager to setup_web.sh
    
    * Print out what is happening
    
    * Add shell: bash
    
    * Try getting wasm-opt into the windows build
    
    * Correct path to exe
    
    * run setup_web.sh but allow it to fail
    
    ---------
    
    Co-authored-by: Jeremy Leibs <jeremy@rerun.io>

--- scripts/setup_web.sh
+++ scripts/setup_web.sh
@@ -6,19 +6,26 @@
 set -eu
 script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$script_path/.."
+set -x
 
-# Pre-requisites:
+# For compiling to Wasm:
 rustup target add wasm32-unknown-unknown
+
+# For generating JS bindings:
 cargo install wasm-bindgen-cli --version 0.2.84
 
-# Required by build_web.sh:
-packagesNeeded='binaryen jq'
+# For local tests with `start_server.sh`:
+# cargo install basic-http-server
+
+# binaryen gives us wasm-opt, for optimizing the an .wasm file for speed and size
+packagesNeeded='binaryen'
 if [ -x "$(command -v brew)" ];      then brew install $packagesNeeded
 elif [ -x "$(command -v apt-get)" ]; then sudo apt-get -y install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 elif [ -x "$(command -v apk)" ];     then sudo apk add --no-cache $packagesNeeded
-else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
-
-# For local tests with `start_server.sh`:
-cargo install basic-http-server
+elif [ -x "$(command -v winget)" ];  then sudo winget add --no-cache $packagesNeeded
+else
+    echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2;
+    exit 1
+fi
