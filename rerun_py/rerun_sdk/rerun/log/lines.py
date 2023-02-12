a5a6a01fcb92a2b49d62ce0ab3c5f616ca0069aa
Jeremy Leibs <jeremy@rerun.io>
2023-02-12 17:02:44 +0100

    Always log the primary component last so it is joined properly (#1217)
    * Always log splats before the primary component
    * Make comment clear that the important thing is primary last not splat first

--- rerun_py/rerun_sdk/rerun/log/lines.py
+++ rerun_py/rerun_sdk/rerun/log/lines.py
@@ -91,12 +91,13 @@ def log_line_strip(
     if ext:
         _add_extension_components(instanced, splats, ext, None)
 
-    if instanced:
-        bindings.log_arrow_msg(entity_path, components=instanced, timeless=timeless)
-
     if splats:
         splats["rerun.instance_key"] = InstanceArray.splat()
         bindings.log_arrow_msg(entity_path, components=splats, timeless=timeless)
+
+    # Always the primary component last so range-based queries will include the other data. See(#1215)
+    if instanced:
+        bindings.log_arrow_msg(entity_path, components=instanced, timeless=timeless)
 
 
 def log_line_segments(
@@ -174,9 +175,10 @@ def log_line_segments(
     if ext:
         _add_extension_components(instanced, splats, ext, None)
 
-    if instanced:
-        bindings.log_arrow_msg(entity_path, components=instanced, timeless=timeless)
-
     if splats:
         splats["rerun.instance_key"] = InstanceArray.splat()
         bindings.log_arrow_msg(entity_path, components=splats, timeless=timeless)
+
+    # Always the primary component last so range-based queries will include the other data. See(#1215)
+    if instanced:
+        bindings.log_arrow_msg(entity_path, components=instanced, timeless=timeless)
