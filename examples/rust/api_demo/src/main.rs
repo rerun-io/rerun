7a5974760a6e9a2c5c40eb4b5972daeee63bdbe4
Clement Rey <cr.rey.clement@gmail.com>
2023-02-13 21:08:57 +0100

    who are you and what do you want from me

--- examples/rust/api_demo/src/main.rs
+++ examples/rust/api_demo/src/main.rs
@@ -1,4 +1,6 @@
-//! Minimal examples of Rerun SDK usage.
+//! An amalgamation of various usages of the API with synthetic "data" without any particular focus.
+//!
+//! It uses a lot of different aspects of the Rerun API in order to test it.
 //!
 //! Run all demos:
 //! ```
