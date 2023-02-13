a20fc69d826396246c09c1fee19e4dbaaa7e6718
Clement Rey <cr.rey.clement@gmail.com>
2023-02-13 12:38:12 +0100

    dealing with git hashes in all situations

--- crates/re_analytics/build.rs
+++ crates/re_analytics/build.rs
@@ -1,5 +1,28 @@
 use std::process::Command;
 
+// Situations to consider
+// ----------------------
+//
+// # Using the published crate
+//
+// The published crate carries its version around, which in turns gives us the git tag, which makes
+// the commit hash irrelevant.
+// We still need to compute _something_ so that we can actually build, but that value will be
+// ignored when the crate is built by the end user anyhow.
+//
+// # Working directly within the workspace
+//
+// When working within the workspace, we can simply try and call `git` and we're done.
+//
+// # Using an unpublished crate (e.g. `path = "..."` or `git = "..."` or `[patch.crates-io]`)
+//
+// In these cases we may or may not have access to the workspace (e.g. a `path = ...` import likely
+// will, while a crate patch won't).
+//
+// This is not an issue however, as we can simply try and see what we get.
+// If we manage to compute a commit hash, great, otherwise we still have the crate version to
+// fallback on.
+
 fn main() {
     // target triple
     println!(
@@ -7,32 +30,31 @@ fn main() {
         std::env::var("TARGET").unwrap()
     );
 
-    if std::env::var("IS_IN_RERUN_WORKSPACE") != Ok("yes".to_owned()) {
-        // If we're outside the workspace, we just can't know... but we still need to set the
-        // envvar to the something, else we wouldn't be able to compile.
-        println!("cargo:rustc-env=__RERUN_GIT_HASH=<unknown>",);
-        return;
+    match git_hash() {
+        Ok(git_hash) => {
+            println!("cargo:rustc-env=__RERUN_GIT_HASH={git_hash}");
+            for path in glob::glob("../../.git/refs/heads/**").unwrap() {
+                println!("cargo:rerun-if-changed={}", path.unwrap().to_string_lossy());
+            }
+        }
+        Err(_) => println!("cargo:rustc-env=__RERUN_GIT_HASH=<unknown>"),
+    }
+}
+
+fn git_hash() -> anyhow::Result<String> {
+    let output = Command::new("git").args(["rev-parse", "HEAD"]).output()?;
+
+    let git_hash = String::from_utf8(output.stdout)?;
+    let git_hash = git_hash.trim();
+    if git_hash.is_empty() {
+        return Ok("<unknown>".into());
     }
 
-    // git hash
-    let output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-    let git_hash = git_hash.trim();
     let clean = Command::new("git")
         .args(["diff-files", "--quiet"])
-        .output()
-        .unwrap()
+        .output()?
         .status
         .success();
-    println!(
-        "cargo:rustc-env=__RERUN_GIT_HASH={}{}",
-        git_hash,
-        if clean { "" } else { "-dirty" }
-    );
-    for path in glob::glob("../../.git/refs/heads/**").unwrap() {
-        println!("cargo:rerun-if-changed={}", path.unwrap().to_string_lossy());
-    }
+
+    Ok(format!("{}{}", git_hash, if clean { "" } else { "-dirty" }))
 }
