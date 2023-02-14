2d054ffad3ea821738cff2786d783c46538afc6f
John Hughes <john@rerun.io>
2023-02-14 12:32:30 +0100

    Code golf the minimal example

--- examples/python/minimal/main.py
+++ examples/python/minimal/main.py
@@ -3,17 +3,10 @@
 """Demonstrates the most barebone usage of the Rerun SDK."""
 
 import numpy as np
-
 import rerun as rr
 
-SIZE = 10
+positions = np.vstack([xyz.ravel() for xyz in np.mgrid[3 * [slice(-5, 5, 10j)]]]).T
+colors = np.vstack([rgb.ravel() for rgb in np.mgrid[3 * [slice(0, 255, 10j)]]]).astype(np.uint8).T
 
 rr.spawn()
-
-x, y, z = np.meshgrid(np.linspace(-5, 5, SIZE), np.linspace(-5, 5, SIZE), np.linspace(-5, 5, SIZE))
-positions = np.array(list(zip(x.reshape(-1), y.reshape(-1), z.reshape(-1))))
-
-r, g, b = np.meshgrid(np.linspace(0, 255, SIZE), np.linspace(0, 255, SIZE), np.linspace(0, 255, SIZE))
-colors = np.array(list(zip(r.reshape(-1), g.reshape(-1), b.reshape(-1))), dtype=np.uint8)
-
 rr.log_points("my_points", positions=positions, colors=colors)
