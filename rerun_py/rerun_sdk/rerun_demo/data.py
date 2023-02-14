2e628c47bee5f1c9fb14d6172c86cf85a89536b5
Jeremy Leibs <jeremy@rerun.io>
2023-02-14 20:18:53 +0100

    Add twist parameter to the demo cube (#1302)

--- rerun_py/rerun_sdk/rerun_demo/data.py
+++ rerun_py/rerun_sdk/rerun_demo/data.py
@@ -10,7 +10,7 @@ from rerun_demo.turbo import turbo_colormap_data
 ColorGrid = namedtuple("ColorGrid", ["positions", "colors"])
 
 
-def build_color_grid(x_count=10, y_count=10, z_count=10):
+def build_color_grid(x_count=10, y_count=10, z_count=10, twist=0):
     """
     Create a cube of points with colors.
 
@@ -20,18 +20,25 @@ def build_color_grid(x_count=10, y_count=10, z_count=10):
     ----------
     x_count, y_count, z_count:
         Number of points in each dimension.
+    twist:
+        Angle to twist from bottom to top of the cube
 
     """
-    positions = np.vstack(
-        [
-            xyz.ravel()
-            for xyz in np.mgrid[
-                slice(-10, 10, x_count * 1j),
-                slice(-10, 10, y_count * 1j),
-                slice(-10, 10, z_count * 1j),
-            ]
-        ]
-    )
+
+    grid = np.mgrid[
+        slice(-10, 10, x_count * 1j),
+        slice(-10, 10, y_count * 1j),
+        slice(-10, 10, z_count * 1j),
+    ]
+
+    angle = np.linspace(-float(twist) / 2, float(twist) / 2, z_count)
+    for z in range(z_count):
+        xv, yv, zv = grid[:, :, :, z]
+        rot_xv = xv * cos(angle[z]) - yv * sin(angle[z])
+        rot_yv = xv * sin(angle[z]) + yv * cos(angle[z])
+        grid[:, :, :, z] = [rot_xv, rot_yv, zv]
+
+    positions = np.vstack([xyz.ravel() for xyz in grid])
 
     colors = np.vstack(
         [
