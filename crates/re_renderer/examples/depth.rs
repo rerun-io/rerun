c170b1c7d7790de88daa928a46be07e392ad8abe
Clement Rey <cr.rey.clement@gmail.com>
2023-02-22 14:41:32 +0100

    more, always more

--- crates/re_renderer/examples/depth.rs
+++ crates/re_renderer/examples/depth.rs
@@ -26,13 +26,21 @@ enum CameraControl {
     Manual,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum ProjectionKind {
+    Orthographic,
+    Perspective,
+}
+
 struct RenderVolumetric {
-    checkerboard: Vec<u8>,
-    checkerboard_size: Vec3,
-    checkerboard_dimensions: UVec3,
+    depth: DepthTexture,
+    albedo: AlbedoTexture,
 
     camera_control: CameraControl,
     camera_position: glam::Vec3,
+
+    depth_kind: DepthKind,
+    projection_kind: ProjectionKind,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -49,8 +57,6 @@ struct DepthTexture {
     // - 0.0 = near, 1.0 = far
     // - distance from camera
     d32: Vec<f32>,
-
-    kind: DepthKind,
 }
 impl DepthTexture {
     pub fn from_file(path: impl Into<PathBuf>, dimensions: Option<glam::UVec2>) -> Self {
@@ -82,27 +88,32 @@ impl DepthTexture {
         // - linear?
         // - inversed?
         // - distance from camera plane or distance from camera?
-        let mut kind = DepthKind::CameraPlane;
-        let mut is_linear = false;
+        let (mut is_linear, mut n, mut f) = (true, 0.0, 0.0);
         let mut is_reversed = false;
 
         if path.to_string_lossy().contains("teardown") {
-            kind = DepthKind::CameraPosition;
-            is_linear = false;
+            (is_linear, n, f) = (false, 0.2, 500.0);
             is_reversed = false;
         }
 
         if path.to_string_lossy().contains("nyud") {
-            kind = DepthKind::CameraPosition;
-            is_linear = true;
+            (is_linear, n, f) = (true, 0.2, 500.0);
             is_reversed = false;
         }
 
         // TODO: how does one do that with an infinite plane tho?
-        fn linearize_depth(z: f32) -> f32 {
-            let n = 0.1; // camera z near
-            let f = 1.0; // camera z far
-            n * f / (f + z * (n - f))
+
+        fn depth_to_view_depth(n: f32, f: f32, z: f32) -> f32 {
+            n * f / (f - z * (f - n))
+        }
+        fn view_depth_to_capped_linear(n: f32, f: f32, vz: f32) -> f32 {
+            let vz = f32::min(vz, f);
+            ((vz - n) / (f - n))
+        }
+
+        fn linearize_depth(n: f32, f: f32, z: f32) -> f32 {
+            let vd = n * f / (f - z * (f - n));
+            ((vd - n) / (f - n))
         }
 
         let dimensions = glam::UVec2::new(img.width(), img.height());
@@ -115,7 +126,9 @@ impl DepthTexture {
                     d = 1.0 - d;
                 }
                 if !is_linear {
-                    d = linearize_depth(d);
+                    // d = linearize_depth(n, f, d);
+                    d = depth_to_view_depth(n, f, d);
+                    d = view_depth_to_capped_linear(n, f * 0.05, d);
                 }
 
                 d
@@ -125,7 +138,6 @@ impl DepthTexture {
         Self {
             dimensions,
             d32: data,
-            kind,
         }
     }
 
@@ -164,28 +176,68 @@ impl framework::Example for RenderVolumetric {
 
     fn new(re_ctx: &mut re_renderer::RenderContext) -> Self {
         re_log::info!("Stop camera movement by pressing 'Space'");
+        re_log::info!("Change perspective by pressing 'P'");
+        re_log::info!("Change depth interpreation by pressing 'D'");
 
         // let depth = DepthTexture::from_file("/tmp/teardown_depthfull.png", Some((640, 640).into()));
         // let albedo = AlbedoTexture::from_file("/tmp/teardown_albedo.png", depth.dimensions.into());
         let depth = DepthTexture::from_file("/tmp/nyud_depth.pgm", None);
         let albedo = AlbedoTexture::from_file("/tmp/nyud_albedo.ppm", depth.dimensions.into());
 
+        let depth_kind = DepthKind::CameraPlane;
+        let projection_kind = ProjectionKind::Orthographic;
+
+        re_log::info!(?depth_kind, ?projection_kind, "current settings");
+
+        RenderVolumetric {
+            depth,
+            albedo,
+
+            camera_control: CameraControl::RotateAroundCenter,
+            camera_position: glam::Vec3::ZERO,
+
+            depth_kind,
+            projection_kind,
+        }
+    }
+
+    fn draw(
+        &mut self,
+        re_ctx: &mut re_renderer::RenderContext,
+        resolution: [u32; 2],
+        time: &framework::Time,
+        pixels_from_point: f32,
+    ) -> Vec<framework::ViewDrawResult> {
+        // volume_rgba8: Vec<u8>,
+        // volume_size: Vec3,
+        // volume_dimensions: UVec3,
+
+        let Self {
+            depth,
+            albedo,
+            camera_control,
+            camera_position,
+            depth_kind,
+            projection_kind,
+        } = self;
+
         let depth_size = depth.dimensions.as_vec2();
+
+        // TODO: we can pretend that the current texture is the image plane, thus we need to
+        // compute the dimensions of the far plane such that
+
+        // TODO: the only thing that should ever change, then, is the size
 
         // TODO: Z is arbitrary I guess?
         // TODO: what about the volume size in world space? is this actually arbitrary? I guess it
         //       can be computed in a way that makes sense, somehow..?
-        let volume_size = depth_size.extend(depth_size.x * 0.7) * 0.2;
+        let volume_size = depth_size.extend(650.0) * 0.2;
         // TODO: shouldnt have to be cubic
         let volume_dimensions = UVec3::new(640, 640, 640) / 4;
         // let vol_dimensions =
         //     UVec3::new(img.width(), img.height(), (img.width() as f32 * 0.7) as u32) / 4;
 
-        dbg!(depth_size);
-        dbg!(volume_size);
-        dbg!(volume_dimensions);
-
-        let mut faked = vec![
+        let mut volume_rgba8 = vec![
             0u8;
             (volume_dimensions.x * volume_dimensions.y * volume_dimensions.z * 4)
                 as usize
@@ -203,9 +255,22 @@ impl framework::Example for RenderVolumetric {
             // let texcoords_in_volume = texcoords.extend(1.0);
             let texcoords_in_volume = Vec3::new(texcoords.x, 1.0 - texcoords.y, 0.0);
 
-            let cam_npos_in_volume = match depth.kind {
-                DepthKind::CameraPlane => Vec3::new(texcoords.x, 1.0 - texcoords.y, 1.0),
-                DepthKind::CameraPosition => Vec3::new(0.5, 0.5, 1.0),
+            let cam_npos_in_volume = match *projection_kind {
+                ProjectionKind::Orthographic => texcoords_in_volume.xy().extend(1.0),
+                ProjectionKind::Perspective => Vec3::new(0.5, 0.5, 1.0),
+            };
+
+            let z = match (*projection_kind, *depth_kind) {
+                (ProjectionKind::Orthographic, DepthKind::CameraPlane) => z,
+                (ProjectionKind::Orthographic, DepthKind::CameraPosition) => {
+                    // TODO: compute planar-based
+                    z
+                }
+                (ProjectionKind::Perspective, DepthKind::CameraPlane) => {
+                    // TODO: compute position-based
+                    z
+                }
+                (ProjectionKind::Perspective, DepthKind::CameraPosition) => z,
             };
 
             let npos_in_volume =
@@ -219,49 +284,32 @@ impl framework::Example for RenderVolumetric {
                 + pos.z * volume_dimensions.x * volume_dimensions.y) as usize;
             let idx = idx * 4;
 
-            faked[idx..idx + 4].copy_from_slice(&albedo.get(x, y));
+            volume_rgba8[idx..idx + 4].copy_from_slice(&albedo.get(x, y));
 
             // let d = (z * 255.0) as u8;
             // faked[idx..idx + 4].copy_from_slice(&[d, d, d, 255]);
         }
 
-        RenderVolumetric {
-            checkerboard: faked,
-            checkerboard_size: volume_size,
-            checkerboard_dimensions: volume_dimensions,
-
-            camera_control: CameraControl::RotateAroundCenter,
-            camera_position: glam::Vec3::ZERO,
-        }
-    }
-
-    fn draw(
-        &mut self,
-        re_ctx: &mut re_renderer::RenderContext,
-        resolution: [u32; 2],
-        time: &framework::Time,
-        pixels_from_point: f32,
-    ) -> Vec<framework::ViewDrawResult> {
         // let seconds_since_startup = 0f32;
         let seconds_since_startup = time.seconds_since_startup();
-        if matches!(self.camera_control, CameraControl::RotateAroundCenter) {
-            self.camera_position = Vec3::new(
+        if matches!(camera_control, CameraControl::RotateAroundCenter) {
+            *camera_position = Vec3::new(
                 seconds_since_startup.sin(),
                 0.5,
                 seconds_since_startup.cos(),
-            ) * 100.0;
+            ) * volume_size.max_element();
         }
 
         let splits = framework::split_resolution(resolution, 1, 1).collect::<Vec<_>>();
 
         let mut bbox_builder = LineStripSeriesBuilder::<()>::default();
         let volume_instances = vec![{
-            let scale = glam::Mat4::from_scale(self.checkerboard_size);
+            let scale = glam::Mat4::from_scale(volume_size);
 
             let rotation = glam::Mat4::IDENTITY;
 
             let translation_center =
-                glam::Mat4::from_translation(-glam::Vec3::splat(0.5) * self.checkerboard_size);
+                glam::Mat4::from_translation(-glam::Vec3::splat(0.5) * volume_size);
 
             let world_from_model = rotation * translation_center * scale;
             let model_from_world = world_from_model.inverse();
@@ -276,9 +324,9 @@ impl framework::Example for RenderVolumetric {
             Volume {
                 world_from_model,
                 model_from_world,
-                size: self.checkerboard_size,
-                dimensions: self.checkerboard_dimensions,
-                data: self.checkerboard.clone(),
+                size: volume_size,
+                dimensions: volume_dimensions,
+                data: volume_rgba8,
             }
         }];
 
@@ -325,15 +373,31 @@ impl framework::Example for RenderVolumetric {
 
     fn on_keyboard_input(&mut self, input: winit::event::KeyboardInput) {
         match (input.state, input.virtual_keycode) {
-            (ElementState::Pressed, Some(VirtualKeyCode::Space)) => {
+            (ElementState::Released, Some(VirtualKeyCode::Space)) => {
                 self.camera_control = match self.camera_control {
                     CameraControl::RotateAroundCenter => CameraControl::Manual,
                     CameraControl::Manual => CameraControl::RotateAroundCenter,
                 };
             }
 
+            (ElementState::Released, Some(VirtualKeyCode::P)) => {
+                self.projection_kind = match self.projection_kind {
+                    ProjectionKind::Orthographic => ProjectionKind::Perspective,
+                    ProjectionKind::Perspective => ProjectionKind::Orthographic,
+                };
+            }
+
+            (ElementState::Released, Some(VirtualKeyCode::D)) => {
+                self.depth_kind = match self.depth_kind {
+                    DepthKind::CameraPlane => DepthKind::CameraPosition,
+                    DepthKind::CameraPosition => DepthKind::CameraPlane,
+                };
+            }
+
             _ => {}
         }
+
+        re_log::info!(depth_kind = ?self.depth_kind, projection_kind = ?self.projection_kind, "current settings");
     }
 }
 
