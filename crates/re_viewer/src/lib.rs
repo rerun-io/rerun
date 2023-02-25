bb2a23f30dd7d8e2c1a0abd3af0ea05fbdcbcee4
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-02-25 22:35:12 +0100

    Make the native viewer an optional dependency of rerun, re_sdk, rerun_py (#1391)
    * Make the native viewer an optional dependency of rerun, re_sdk, rerun_py
    
    * Fix feature flag checking in code
    
    * fix python's show() function
    
    * Better error message
    
    * Rename the "web" feature to "web_viewer"

--- crates/re_viewer/src/lib.rs
+++ crates/re_viewer/src/lib.rs
@@ -152,7 +152,7 @@ pub(crate) fn customize_eframe(cc: &eframe::CreationContext<'_>) -> re_ui::ReUi 
 // ---------------------------------------------------------------------------
 
 /// This wakes up the ui thread each time we receive a new message.
-#[cfg(not(feature = "web"))]
+#[cfg(not(feature = "web_viewer"))]
 #[cfg(not(target_arch = "wasm32"))]
 pub fn wake_up_ui_thread_on_each_msg<T: Send + 'static>(
     rx: re_smart_channel::Receiver<T>,
