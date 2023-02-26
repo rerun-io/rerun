7f81b4fb40a6a357dbda47e50f5247c168dcbbeb
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-02-26 20:05:22 +0100

    SDK: stream to disk with `save` feature (#1405)
    * Refactor LogMsg-encoder to be a `struct`
    
    * Small cleanup of Sender
    
    * Refactor Session: extract the web-viewer connection
    
    * Refactor Session::connect
    
    * Log SDK: stream log data to file on disk
    
    * Rust examples: --save streams to disk
    
    * Python examples: start streaming to disk right away
    
    * typo
    
    Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>
    
    ---------
    
    Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>

--- examples/rust/api_demo/src/main.rs
+++ examples/rust/api_demo/src/main.rs
@@ -656,7 +656,7 @@ fn main() -> anyhow::Result<()> {
 
     let mut session = rerun::Session::init("api_demo_rs", true);
 
-    let should_spawn = args.rerun.on_startup(&mut session);
+    let should_spawn = args.rerun.on_startup(&mut session)?;
     if should_spawn {
         return session
             .spawn(move |mut session| run(&mut session, &args))
@@ -665,7 +665,7 @@ fn main() -> anyhow::Result<()> {
 
     run(&mut session, &args)?;
 
-    args.rerun.on_teardown(&mut session)?;
+    args.rerun.on_teardown();
 
     Ok(())
 }
