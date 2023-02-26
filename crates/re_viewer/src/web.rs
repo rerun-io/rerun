705da722254dd4fb092e783ae41511e466b34946
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-02-26 16:14:43 +0100

    More information in `--version` (#1388)
    * Add the re_build_info and re_build_build_info crates
    
    * Add datetime
    
    * --version prints the full build info
    
    * Add git branch and clean/dirty flag
    
    * fix extra --dirty
    
    * Fix analytics link
    
    * Add note about not moving the viewer_analytics.rs file
    
    * Add About-menu with version number, target triplet, and build date
    
    also add link to rerun.io in it
    
    * Add help-button to rerun menu
    
    * Fix web build
    
    * Add crate-level docs
    
    * Rerun re_build_build_info when branch or commit hash changes
    
    * Fix merge-snafu
    
    * Ignore if the repo is dirty or clean

--- crates/re_viewer/src/web.rs
+++ crates/re_viewer/src/web.rs
@@ -27,11 +27,19 @@ pub async fn start(canvas_id: &str) -> std::result::Result<(), eframe::wasm_bind
         canvas_id,
         web_options,
         Box::new(move |cc| {
+            let build_info = re_build_info::build_info!();
             let app_env = crate::AppEnvironment::Web;
             let startup_options = crate::StartupOptions::default();
             let re_ui = crate::customize_eframe(cc);
             let url = get_url(&cc.integration_info);
-            let app = crate::RemoteViewerApp::new(app_env, startup_options, re_ui, cc.storage, url);
+            let app = crate::RemoteViewerApp::new(
+                build_info,
+                app_env,
+                startup_options,
+                re_ui,
+                cc.storage,
+                url,
+            );
             Box::new(app)
         }),
     )
