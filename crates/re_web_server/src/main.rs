3d012c7171a6096ba6371b6044ab4f4178518596
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-02-28 13:02:26 +0100

    re_web_server binary: print out where we are hosting

--- crates/re_web_server/src/main.rs
+++ crates/re_web_server/src/main.rs
@@ -5,5 +5,7 @@
 #[tokio::main]
 async fn main() {
     re_log::setup_native_logging();
-    re_web_server::WebServer::new(9090).serve().await.unwrap();
+    let port = 9090;
+    eprintln!("Hosting web-viewer on http://127.0.0.1:{port}");
+    re_web_server::WebServer::new(port).serve().await.unwrap();
 }
