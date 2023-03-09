8dfff299baba27d6e7608cbc4e7d4cb144279b99
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-03-09 10:30:51 +0100

    Simplify code with SessionBuilder::buffered

--- examples/rust/minimal/src/main.rs
+++ examples/rust/minimal/src/main.rs
@@ -4,16 +4,11 @@ use rerun::{
     components::{ColorRGBA, Point3D},
     demo_util::grid,
     external::glam,
-    MsgSender, Session, SessionBuilder,
+    MsgSender, SessionBuilder,
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (rerun_enabled, recording_info) = SessionBuilder::new("minimal_rs").finalize();
-    let session = if rerun_enabled {
-        Session::buffered(recording_info)
-    } else {
-        Session::disabled()
-    };
+    let session = SessionBuilder::new("minimal_rs").buffered();
 
     let points = grid(glam::Vec3::splat(-5.0), glam::Vec3::splat(5.0), 10)
         .map(Point3D::from)
