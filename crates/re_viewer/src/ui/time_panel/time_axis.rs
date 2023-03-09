385b4e0ef0bcd1d14913bed48f746cf73fd1e2df
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-03-09 18:59:37 +0100

    Add some TODO:s

--- crates/re_viewer/src/ui/time_panel/time_axis.rs
+++ crates/re_viewer/src/ui/time_panel/time_axis.rs
@@ -69,11 +69,11 @@ fn gap_size_heuristic(time_type: TimeType, times: &TimeHistogram) -> u64 {
         TimeType::Sequence => 9,
         TimeType::Time => TimeInt::from_milliseconds(100).as_i64() as _,
     };
+    let cutoff_size = min_gap_size; // TODO(emilk): we could make the cutoff_size even larger, which will be faster when there is a lot of data points.
 
     // Collect all gaps larger than our minimum gap size.
     let mut gap_sizes = {
         crate::profile_scope!("collect_gaps");
-        let cutoff_size = 1; // TODO
         times
             .range(.., cutoff_size)
             .tuple_windows()
@@ -112,7 +112,7 @@ fn gap_size_heuristic(time_type: TimeType, times: &TimeHistogram) -> u64 {
 /// Collapse any gaps larger or equals to the given threshold.
 fn create_ranges(times: &TimeHistogram, gap_threshold: u64) -> vec1::Vec1<TimeRange> {
     crate::profile_function!();
-    let cutoff_size = 1; // TODO
+    let cutoff_size = 1; // TODO(emilk): take larger steps when possible, to speed up the case when we have many data points
     let mut it = times.range(.., cutoff_size);
     let first_range = it.next().unwrap().0;
     let mut ranges = vec1::vec1![TimeRange::new(
