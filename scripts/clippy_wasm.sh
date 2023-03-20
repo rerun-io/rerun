6207499b0e9e54fa734ee609d9724e9d7b047bfd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-03-20 21:00:16 +0100

    Run clippy for wasm, with own clippy.toml config file

---
+++ scripts/clippy_wasm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This scripts run clippy on the wasm32-unknown-unknown target,
+# using a special clippy_wasm.toml config file which forbids a few more things.
+
+set -eu
+script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$script_path/.."
+set -x
+
+mv clippy.toml clippy.toml.bak
+cp clippy_wasm.toml clippy.toml
+
+function cleanup()
+{
+    mv clippy.toml.bak clippy.toml
+}
+
+trap cleanup EXIT
+
+cargo cranky --all-features --target wasm32-unknown-unknown --target-dir target_wasm -p re_viewer -- --deny warnings
