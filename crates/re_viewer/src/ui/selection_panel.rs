e321037562fc423f0ec9d8aa8e3056ebfaa7e62a
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-03-24 12:04:25 +0100

    spell fix

--- crates/re_viewer/src/ui/selection_panel.rs
+++ crates/re_viewer/src/ui/selection_panel.rs
@@ -573,7 +573,7 @@ fn backproject_radius_scale_ui(ui: &mut egui::Ui, property: &mut EditableAutoVal
         .on_hover_text(
             "Scales the radii of the points in the backprojected point cloud.\n\
             This is a factor of the projected pixel diameter. \
-            This means a scale of 0.5 will leave adjacent pixels at the same depth value jsut touching.\n\
+            This means a scale of 0.5 will leave adjacent pixels at the same depth value just touching.\n\
             Double-click to reset.",
         );
     if response.double_clicked() {
