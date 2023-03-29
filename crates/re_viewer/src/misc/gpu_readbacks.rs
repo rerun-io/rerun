4a07d542a5406b458c91d2e4982a0cbf86cd2fed
Andreas Reich <r_andreas2@web.de>
2023-03-29 10:32:16 +0200

    fix another web warning issue

--- crates/re_viewer/src/misc/gpu_readbacks.rs
+++ crates/re_viewer/src/misc/gpu_readbacks.rs
@@ -3,6 +3,7 @@ use re_renderer::ScheduledScreenshot;
 use crate::ui::SpaceViewId;
 
 #[derive(PartialEq, Eq, Clone, Copy)]
+#[allow(dead_code)] // Not used on the web.
 pub enum ScreenshotMode {
     /// The screenshot will be saved to disc and copied to the clipboard.
     SaveAndCopyToClipboard,
