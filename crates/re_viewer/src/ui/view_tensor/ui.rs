d301af92c03507978df25859595c597e448d0e5a
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-04-13 19:49:22 +0200

    Remove old ui code

--- crates/re_viewer/src/ui/view_tensor/ui.rs
+++ crates/re_viewer/src/ui/view_tensor/ui.rs
@@ -1,13 +1,10 @@
 use std::{collections::BTreeMap, fmt::Display};
 
 use eframe::emath::Align2;
-use egui::{epaint::TextShape, Color32, ColorImage, NumExt as _, Vec2};
-use ndarray::{Axis, Ix2};
+use egui::{epaint::TextShape, NumExt as _, Vec2};
+use ndarray::Axis;
 
-use re_log_types::{
-    component_types::{self, Tensor},
-    TensorDataType,
-};
+use re_log_types::component_types::{self, Tensor};
 use re_tensor_ops::dimension_mapping::{DimensionMapping, DimensionSelector};
 
 use crate::ui::data_ui::image::tensor_summary_ui_grid_contents;
@@ -152,14 +149,27 @@ pub(crate) fn view_tensor(
     };
 
     egui::ScrollArea::both().show(ui, |ui| {
-        let font_id = egui::TextStyle::Body.resolve(ui.style());
-
-        let (response, painter, image_rect) = paint_tensor_slice(ctx, ui, state, tensor).unwrap(); // TODO: don't unwrap
-
-        if !response.hovered() {
-            paint_axis_names(ui, &painter, image_rect, font_id, dimension_labels);
+        if let Err(err) = fun_name(ctx, ui, state, tensor, dimension_labels) {
+            ui.label(ctx.re_ui.error_text(err.to_string()));
         }
     });
+}
+
+fn fun_name(
+    ctx: &mut crate::misc::ViewerContext<'_>,
+    ui: &mut egui::Ui,
+    state: &mut ViewTensorState,
+    tensor: &Tensor,
+    dimension_labels: [(String, bool); 2],
+) -> anyhow::Result<()> {
+    let (response, painter, image_rect) = paint_tensor_slice(ctx, ui, state, tensor)?;
+
+    if !response.hovered() {
+        let font_id = egui::TextStyle::Body.resolve(ui.style());
+        paint_axis_names(ui, &painter, image_rect, font_id, dimension_labels);
+    }
+
+    Ok(())
 }
 
 fn paint_tensor_slice(
@@ -213,231 +223,6 @@ fn paint_tensor_slice(
     Ok((response, painter, image_rect))
 }
 
-fn tensor_ui(
-    ctx: &mut crate::misc::ViewerContext<'_>,
-    ui: &mut egui::Ui,
-    state: &mut ViewTensorState,
-    tensor: &Tensor,
-) {
-    let tensor_shape = tensor.shape();
-
-    let tensor_stats = ctx.cache.tensor_stats(tensor);
-    let range = tensor_stats.range;
-    let color_mapping = &state.color_mapping;
-
-    match tensor.dtype() {
-        TensorDataType::U8 => match ndarray::ArrayViewD::<u8>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: u8| {
-                    // We always use the full range for u8
-                    color_mapping.color_from_normalized(value as f32 / 255.0)
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::U16 => match ndarray::ArrayViewD::<u16>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: u16| {
-                    let (tensor_min, tensor_max) = range.unwrap_or((0.0, u16::MAX as f64)); // the cache should provide the range
-                    color_mapping.color_from_normalized(egui::remap(
-                        value as f32,
-                        tensor_min as f32..=tensor_max as f32,
-                        0.0..=1.0,
-                    ))
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::U32 => match ndarray::ArrayViewD::<u32>::try_from(tensor) {
-            Ok(tensor) => {
-                let (tensor_min, tensor_max) = range.unwrap_or((0.0, u32::MAX as f64)); // the cache should provide the range
-
-                let color_from_value = |value: u32| {
-                    color_mapping.color_from_normalized(egui::remap(
-                        value as f64,
-                        tensor_min..=tensor_max,
-                        0.0..=1.0,
-                    ) as f32)
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::U64 => match ndarray::ArrayViewD::<u64>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: u64| {
-                    let (tensor_min, tensor_max) = range.unwrap_or((0.0, u64::MAX as f64)); // the cache should provide the range
-                    color_mapping.color_from_normalized(egui::remap(
-                        value as f64,
-                        tensor_min..=tensor_max,
-                        0.0..=1.0,
-                    ) as f32)
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::I8 => match ndarray::ArrayViewD::<i8>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: i8| {
-                    // We always use the full range for i8:
-                    let (tensor_min, tensor_max) = (i8::MIN as f32, i8::MAX as f32);
-                    color_mapping.color_from_normalized(egui::remap(
-                        value as f32,
-                        tensor_min..=tensor_max,
-                        0.0..=1.0,
-                    ))
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::I16 => match ndarray::ArrayViewD::<i16>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: i16| {
-                    let (tensor_min, tensor_max) =
-                        range.unwrap_or((i16::MIN as f64, i16::MAX as f64)); // the cache should provide the range
-                    color_mapping.color_from_normalized(egui::remap(
-                        value as f32,
-                        tensor_min as f32..=tensor_max as f32,
-                        0.0..=1.0,
-                    ))
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::I32 => match ndarray::ArrayViewD::<i32>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: i32| {
-                    let (tensor_min, tensor_max) =
-                        range.unwrap_or((i32::MIN as f64, i32::MAX as f64)); // the cache should provide the range
-                    color_mapping.color_from_normalized(egui::remap(
-                        value as f64,
-                        tensor_min..=tensor_max,
-                        0.0..=1.0,
-                    ) as f32)
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::I64 => match ndarray::ArrayViewD::<i64>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: i64| {
-                    let (tensor_min, tensor_max) =
-                        range.unwrap_or((i64::MIN as f64, i64::MAX as f64)); // the cache should provide the range
-                    color_mapping.color_from_normalized(egui::remap(
-                        value as f64,
-                        tensor_min..=tensor_max,
-                        0.0..=1.0,
-                    ) as f32)
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::F16 => match ndarray::ArrayViewD::<half::f16>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: half::f16| {
-                    let (tensor_min, tensor_max) = range.unwrap_or((0.0, 1.0)); // the cache should provide the range
-                    color_mapping.color_from_normalized(egui::remap(
-                        value.to_f32(),
-                        tensor_min as f32..=tensor_max as f32,
-                        0.0..=1.0,
-                    ))
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::F32 => match ndarray::ArrayViewD::<f32>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: f32| {
-                    let (tensor_min, tensor_max) = range.unwrap_or((0.0, 1.0)); // the cache should provide the range
-                    color_mapping.color_from_normalized(egui::remap(
-                        value,
-                        tensor_min as f32..=tensor_max as f32,
-                        0.0..=1.0,
-                    ))
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-
-        TensorDataType::F64 => match ndarray::ArrayViewD::<f64>::try_from(tensor) {
-            Ok(tensor) => {
-                let color_from_value = |value: f64| {
-                    let (tensor_min, tensor_max) = range.unwrap_or((0.0, 1.0)); // the cache should provide the range
-                    color_mapping.color_from_normalized(egui::remap(
-                        value,
-                        tensor_min..=tensor_max,
-                        0.0..=1.0,
-                    ) as f32)
-                };
-
-                let slice = selected_tensor_slice(&state.slice, &tensor);
-                slice_ui(ctx, ui, state, tensor_shape, slice, color_from_value);
-            }
-            Err(err) => {
-                ui.label(ctx.re_ui.error_text(err.to_string()));
-            }
-        },
-    }
-}
-
 // ----------------------------------------------------------------------------
 
 // TODO(emilk): replace with the one from re_renderer
@@ -480,25 +265,6 @@ impl ColorMapping {
             Colormap::Greyscale => re_renderer::Colormap::Grayscale,
             Colormap::Turbo => re_renderer::Colormap::Turbo,
             Colormap::Virdis => re_renderer::Colormap::Viridis,
-        }
-    }
-
-    pub fn color_from_normalized(&self, f: f32) -> Color32 {
-        let f = f.powf(self.gamma);
-
-        match self.map {
-            Colormap::Greyscale => {
-                let lum = (f * 255.0 + 0.5) as u8;
-                Color32::from_gray(lum)
-            }
-            Colormap::Turbo => {
-                let [r, g, b, _] = re_renderer::colormap_turbo_srgb(f);
-                Color32::from_rgb(r, g, b)
-            }
-            Colormap::Virdis => {
-                let [r, g, b, _] = re_renderer::colormap_viridis_srgb(f);
-                Color32::from_rgb(r, g, b)
-            }
         }
     }
 
@@ -575,46 +341,6 @@ impl Default for TextureSettings {
                 minification: egui::TextureFilter::Linear,
             },
         }
-    }
-}
-
-// helpers
-impl TextureSettings {
-    fn paint_image(
-        &self,
-        ui: &mut egui::Ui,
-        margin: Vec2,
-        image: ColorImage,
-    ) -> (egui::Response, egui::Painter, egui::Rect) {
-        let img_size = egui::vec2(image.size[0] as _, image.size[1] as _);
-        let img_size = Vec2::max(Vec2::splat(1.0), img_size); // better safe than sorry
-        let desired_size = match self.scaling {
-            TextureScaling::Original => img_size + margin,
-            TextureScaling::Fill => {
-                let desired_size = ui.available_size() - margin;
-                if self.keep_aspect_ratio {
-                    let scale = (desired_size / img_size).min_elem();
-                    img_size * scale
-                } else {
-                    desired_size
-                }
-            }
-        };
-
-        // TODO(cmc): don't recreate texture unless necessary
-        let texture = ui.ctx().load_texture("tensor_slice", image, self.options);
-
-        let (response, painter) = ui.allocate_painter(desired_size, egui::Sense::hover());
-        let rect = response.rect;
-        let image_rect = egui::Rect::from_min_max(rect.min + margin, rect.max);
-
-        let mut mesh = egui::Mesh::with_texture(texture.id());
-        let uv = egui::Rect::from_min_max(egui::Pos2::ZERO, egui::pos2(1.0, 1.0));
-        mesh.add_rect_with_uv(image_rect, uv, Color32::WHITE);
-
-        painter.add(mesh);
-
-        (response, painter, image_rect)
     }
 }
 
@@ -718,93 +444,12 @@ pub fn selected_tensor_slice<'a, T: Copy>(
     slice
 }
 
-fn slice_ui<T: Copy>(
-    ctx: &mut crate::misc::ViewerContext<'_>,
-    ui: &mut egui::Ui,
-    view_state: &ViewTensorState,
-    tensor_shape: &[component_types::TensorDimension],
-    slice: ndarray::ArrayViewD<'_, T>,
-    color_from_value: impl Fn(T) -> Color32,
-) {
-    crate::profile_function!();
-
-    let ndims = slice.ndim();
-    if let Ok(slice) = slice.into_dimensionality::<Ix2>() {
-        let dimension_labels = {
-            let dm = &view_state.slice.dim_mapping;
-            [
-                (
-                    dimension_name(tensor_shape, dm.width.unwrap()),
-                    dm.invert_width,
-                ),
-                (
-                    dimension_name(tensor_shape, dm.height.unwrap()),
-                    dm.invert_height,
-                ),
-            ]
-        };
-
-        let image = into_image(&slice, color_from_value);
-        egui::ScrollArea::both().show(ui, |ui| {
-            image_ui(ui, view_state, image, dimension_labels);
-        });
-    } else {
-        ui.label(ctx.re_ui.error_text(format!(
-            "Only 2D slices supported at the moment, but slice ndim {ndims}"
-        )));
-    }
-}
-
 fn dimension_name(shape: &[component_types::TensorDimension], dim_idx: usize) -> String {
     let dim = &shape[dim_idx];
     dim.name.as_ref().map_or_else(
         || format!("Dimension {dim_idx} (size={})", dim.size),
         |name| format!("{name} (size={})", dim.size),
     )
-}
-
-fn into_image<T: Copy>(
-    slice: &ndarray::ArrayView2<'_, T>,
-    color_from_value: impl Fn(T) -> Color32,
-) -> ColorImage {
-    crate::profile_function!();
-
-    use ndarray::Dimension as _;
-    let (height, width) = slice.raw_dim().into_pattern();
-    let mut image = egui::ColorImage::new([width, height], Color32::DEBUG_COLOR);
-
-    let image_view =
-        ndarray::ArrayViewMut2::from_shape(slice.raw_dim(), image.pixels.as_mut_slice())
-            .expect("Mismatched length.");
-
-    crate::profile_scope!("color_mapper");
-    ndarray::Zip::from(image_view)
-        .and(slice)
-        .for_each(|pixel, value| {
-            *pixel = color_from_value(*value);
-        });
-
-    image
-}
-
-fn image_ui(
-    ui: &mut egui::Ui,
-    view_state: &ViewTensorState,
-    image: ColorImage,
-    dimension_labels: [(String, bool); 2],
-) {
-    crate::profile_function!();
-
-    let font_id = egui::TextStyle::Body.resolve(ui.style());
-
-    let margin = egui::vec2(0.0, 0.0);
-
-    let (response, painter, image_rect) =
-        view_state.texture_settings.paint_image(ui, margin, image);
-
-    if !response.hovered() {
-        paint_axis_names(ui, &painter, image_rect, font_id, dimension_labels);
-    }
 }
 
 fn paint_axis_names(
