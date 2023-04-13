9716235305055a84a2ef261c11b8b8e169ba4d4d
Andreas Reich <andreas@rerun.io>
2023-04-13 12:15:28 +0200

    Fix too many points crash (#1822)
    * Simplify point cloud builder and fix crash on too many points
    Fixes #1779
    * faster point cloud population by not chaining iterators with default values

--- crates/re_renderer/src/point_cloud_builder.rs
+++ crates/re_renderer/src/point_cloud_builder.rs
@@ -114,7 +114,6 @@ impl<'a> Drop for PointCloudBatchBuilder<'a> {
         if self.0.batches.last().unwrap().point_count == 0 {
             self.0.batches.pop();
         }
-        self.extend_defaults();
     }
 }
 
@@ -141,92 +140,110 @@ impl<'a> PointCloudBatchBuilder<'a> {
         self
     }
 
-    /// Each time we `add_points`, or upon builder drop, make sure that we
-    /// fill in any additional colors and user-data to have matched vectors.
-    fn extend_defaults(&mut self) {
-        if self.0.color_buffer.num_written() < self.0.vertices.len() {
-            self.0.color_buffer.extend(
-                std::iter::repeat(Color32::WHITE)
-                    .take(self.0.vertices.len() - self.0.color_buffer.num_written()),
-            );
-        }
-
-        if self.0.picking_instance_ids_buffer.num_written() < self.0.vertices.len() {
-            self.0
-                .picking_instance_ids_buffer
-                .extend(std::iter::repeat(Default::default()).take(
-                    self.0.vertices.len() - self.0.picking_instance_ids_buffer.num_written(),
-                ));
-        }
-    }
-
-    #[inline]
     /// Add several 3D points
     ///
     /// Returns a `PointBuilder` which can be used to set the colors, radii, and user-data for the points.
     ///
-    /// Params:
-    ///  - `size_hint`: The `PointBuilder` will pre-allocate buffers to accommodate up to this number of points.
-    ///                 The resulting point batch, will still be determined by the length of the iterator.
-    ///  - `positions`: An iterable of the positions of the collection of points
+    /// Will *always* add `num_points`, no matter how many elements are in the iterators.
+    /// Missing elements will be filled up with defaults (in case of positions that's the origin)
+    ///
+    /// TODO(#957): Clamps number of points to the allowed per-builder maximum.
+    #[inline]
     pub fn add_points(
-        &mut self,
-        size_hint: usize,
+        mut self,
+        mut num_points: usize,
         positions: impl Iterator<Item = glam::Vec3>,
-    ) -> PointsBuilder<'_> {
+        radii: impl Iterator<Item = Size>,
+        colors: impl Iterator<Item = Color32>,
+        picking_instance_ids: impl Iterator<Item = PickingLayerInstanceId>,
+    ) -> Self {
         // TODO(jleibs): Figure out if we can plumb-through proper support for `Iterator::size_hints()`
         // or potentially make `FixedSizedIterator` work correctly. This should be possible size the
         // underlying arrow structures are of known-size, but carries some complexity with the amount of
         // chaining, joining, filtering, etc. that happens along the way.
         crate::profile_function!();
 
-        self.extend_defaults();
-
         debug_assert_eq!(self.0.vertices.len(), self.0.color_buffer.num_written());
+        debug_assert_eq!(
+            self.0.vertices.len(),
+            self.0.picking_instance_ids_buffer.num_written()
+        );
 
-        let old_size = self.0.vertices.len();
-
-        self.0.vertices.reserve(size_hint);
-        self.0.vertices.extend(positions.map(|p| PointCloudVertex {
-            position: p,
-            radius: Size::AUTO,
-        }));
-
-        let num_points = self.0.vertices.len() - old_size;
+        if num_points + self.0.vertices.len() > PointCloudDrawData::MAX_NUM_POINTS {
+            re_log::error_once!(
+                "Reached maximum number of supported points of {}.
+     See also https://github.com/rerun-io/rerun/issues/957",
+                PointCloudDrawData::MAX_NUM_POINTS
+            );
+            num_points = PointCloudDrawData::MAX_NUM_POINTS - self.0.vertices.len();
+        }
+        if num_points == 0 {
+            return self;
+        }
         self.batch_mut().point_count += num_points as u32;
 
-        let new_range = old_size..self.0.vertices.len();
-
-        let max_points = self.0.vertices.len();
-
-        PointsBuilder {
-            vertices: &mut self.0.vertices[new_range],
-            max_points,
-            colors: &mut self.0.color_buffer,
-            picking_instance_ids: &mut self.0.picking_instance_ids_buffer,
-            additional_outline_mask_ids: &mut self
-                .0
-                .batches
-                .last_mut()
-                .unwrap()
-                .additional_outline_mask_ids_vertex_ranges,
-            start_vertex_index: old_size as _,
+        {
+            crate::profile_scope!("positions");
+            let num_before = self.0.vertices.len();
+            self.0.vertices.extend(
+                positions
+                    .take(num_points)
+                    .zip(radii.take(num_points))
+                    .map(|(position, radius)| PointCloudVertex { position, radius }),
+            );
+            // Fill up with defaults. Doing this in a separate step is faster than chaining the iterator.
+            let num_default = num_points - (self.0.vertices.len() - num_before);
+            self.0.vertices.extend(
+                std::iter::repeat(PointCloudVertex {
+                    position: glam::Vec3::ZERO,
+                    radius: Size::AUTO,
+                })
+                .take(num_default),
+            );
         }
+        {
+            crate::profile_scope!("colors");
+            let num_written = self.0.color_buffer.extend(colors.take(num_points));
+            // Fill up with defaults. Doing this in a separate step is faster than chaining the iterator.
+            self.0
+                .color_buffer
+                .extend(std::iter::repeat(Color32::TRANSPARENT).take(num_points - num_written));
+        }
+        {
+            crate::profile_scope!("picking_instance_ids");
+            let num_written = self
+                .0
+                .picking_instance_ids_buffer
+                .extend(picking_instance_ids.take(num_points));
+            // Fill up with defaults. Doing this in a separate step is faster than chaining the iterator.
+            self.0.picking_instance_ids_buffer.extend(
+                std::iter::repeat(PickingLayerInstanceId::default()).take(num_points - num_written),
+            );
+        }
+
+        self
     }
 
     /// Adds several 2D points. Uses an autogenerated depth value, the same for all points passed.
     ///
-    /// Params:
-    ///  - `size_hint`: The `PointBuilder` will pre-allocate buffers to accommodate up to this number of points.
-    ///                 The resulting point batch, will be the size of the length of the `positions` iterator.
-    ///  - `positions`: An iterable of the positions of the collection of points
+    /// Will *always* add `num_points`, no matter how many elements are in the iterators.
+    /// Missing elements will be filled up with defaults (in case of positions that's the origin)
     #[inline]
     pub fn add_points_2d(
-        &mut self,
-        size_hint: usize,
+        self,
+        num_points: usize,
         positions: impl Iterator<Item = glam::Vec2>,
-    ) -> PointsBuilder<'_> {
-        self.add_points(size_hint, positions.map(|p| p.extend(0.0)))
+        radii: impl Iterator<Item = Size>,
+        colors: impl Iterator<Item = Color32>,
+        picking_instance_ids: impl Iterator<Item = PickingLayerInstanceId>,
+    ) -> Self {
+        self.add_points(
+            num_points,
+            positions.map(|p| p.extend(0.0)),
+            radii,
+            colors,
+            picking_instance_ids,
+        )
     }
 
     /// Set flags for this batch.
@@ -235,80 +252,25 @@ impl<'a> PointCloudBatchBuilder<'a> {
         self
     }
 
+    /// Sets the picking object id for the current batch.
     pub fn picking_object_id(mut self, picking_object_id: PickingLayerObjectId) -> Self {
         self.batch_mut().picking_object_id = picking_object_id;
         self
     }
-}
-
-pub struct PointsBuilder<'a> {
-    // Vertices is a slice, which radii will update
-    vertices: &'a mut [PointCloudVertex],
-    max_points: usize,
-    colors: &'a mut CpuWriteGpuReadBuffer<Color32>,
-    picking_instance_ids: &'a mut CpuWriteGpuReadBuffer<PickingLayerInstanceId>,
-    additional_outline_mask_ids: &'a mut Vec<(std::ops::Range<u32>, OutlineMaskPreference)>,
-    start_vertex_index: u32,
-}
-
-impl<'a> PointsBuilder<'a> {
-    /// Assigns radii to all points.
-    ///
-    /// This mustn't call this more than once.
-    ///
-    /// If the iterator doesn't cover all points, some will not be assigned.
-    /// If the iterator provides more values than there are points, the extra values will be ignored.
-    #[inline]
-    pub fn radii(self, radii: impl Iterator<Item = Size>) -> Self {
-        // TODO(andreas): This seems like an argument for moving radius
-        // to a separate storage
-        crate::profile_function!();
-        for (point, radius) in self.vertices.iter_mut().zip(radii) {
-            point.radius = radius;
-        }
-        self
-    }
-
-    /// Assigns colors to all points.
-    ///
-    /// This mustn't call this more than once.
-    ///
-    /// If the iterator doesn't cover all points, some will not be assigned.
-    /// If the iterator provides more values than there are points, the extra values will be ignored.
-    #[inline]
-    pub fn colors(self, colors: impl Iterator<Item = Color32>) -> Self {
-        crate::profile_function!();
-        self.colors
-            .extend(colors.take(self.max_points - self.colors.num_written()));
-        self
-    }
-
-    #[inline]
-    pub fn picking_instance_ids(
-        self,
-        picking_instance_ids: impl Iterator<Item = PickingLayerInstanceId>,
-    ) -> Self {
-        crate::profile_function!();
-        self.picking_instance_ids.extend(
-            picking_instance_ids.take(self.max_points - self.picking_instance_ids.num_written()),
-        );
-        self
-    }
 
     /// Pushes additional outline mask ids for a specific range of points.
-    /// The range is relative to this builder's range, not the entire batch.
+    /// The range is relative to this batch.
     ///
     /// Prefer the `overall_outline_mask_ids` setting to set the outline mask ids for the entire batch whenever possible!
     #[inline]
     pub fn push_additional_outline_mask_ids_for_range(
-        self,
+        mut self,
         range: std::ops::Range<u32>,
         ids: OutlineMaskPreference,
     ) -> Self {
-        self.additional_outline_mask_ids.push((
-            (range.start + self.start_vertex_index)..(range.end + self.start_vertex_index),
-            ids,
-        ));
+        self.batch_mut()
+            .additional_outline_mask_ids_vertex_ranges
+            .push((range, ids));
         self
     }
 }
--- crates/re_viewer/src/ui/view_spatial/scene/scene_part/points3d.rs
+++ crates/re_viewer/src/ui/view_spatial/scene/scene_part/points3d.rs
@@ -91,21 +91,18 @@ impl Points3DPart {
             let colors =
                 process_colors(entity_view, ent_path, &annotation_infos)?.collect::<Vec<_>>();
 
-            let instance_path_hashes_for_picking = {
-                crate::profile_scope!("instance_hashes");
-                entity_view
-                    .iter_instance_keys()?
-                    .map(|instance_key| {
-                        instance_path_hash_for_picking(
-                            ent_path,
-                            instance_key,
-                            entity_view,
-                            properties,
-                            entity_highlight.any_selection_highlight,
-                        )
-                    })
-                    .collect::<Vec<_>>()
-            };
+            let instance_path_hashes_for_picking = entity_view
+                .iter_instance_keys()?
+                .map(|instance_key| {
+                    instance_path_hash_for_picking(
+                        ent_path,
+                        instance_key,
+                        entity_view,
+                        properties,
+                        entity_highlight.any_selection_highlight,
+                    )
+                })
+                .collect::<Vec<_>>();
 
             scene.ui.labels.extend(Self::process_labels(
                 entity_view,
@@ -135,21 +132,30 @@ impl Points3DPart {
                     .filter_map(|pt| pt.map(glam::Vec3::from))
             };
 
-            let mut point_range_builder = point_batch
-                .add_points(entity_view.num_instances(), point_positions)
-                .colors(colors)
-                .radii(radii);
-            if properties.interactive {
-                point_range_builder = point_range_builder.picking_instance_ids(
-                    entity_view.iter_instance_keys()?.map(|instance_key| {
-                        instance_key_to_picking_id(
-                            instance_key,
-                            entity_view,
-                            entity_highlight.any_selection_highlight,
-                        )
-                    }),
-                );
-            }
+            let mut point_range_builder = if properties.interactive {
+                let picking_instance_ids = entity_view.iter_instance_keys()?.map(|instance_key| {
+                    instance_key_to_picking_id(
+                        instance_key,
+                        entity_view,
+                        entity_highlight.any_selection_highlight,
+                    )
+                });
+                point_batch.add_points(
+                    entity_view.num_instances(),
+                    point_positions,
+                    radii,
+                    colors,
+                    picking_instance_ids,
+                )
+            } else {
+                point_batch.add_points(
+                    entity_view.num_instances(),
+                    point_positions,
+                    radii,
+                    colors,
+                    std::iter::empty::<re_renderer::PickingLayerInstanceId>(),
+                )
+            };
 
             // Determine if there's any sub-ranges that need extra highlighting.
             {
