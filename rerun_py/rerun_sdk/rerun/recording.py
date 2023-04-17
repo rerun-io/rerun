715f6bca8fc0534431b8603c45286b2c2829c30f
Jeremy Leibs <jeremy@rerun.io>
2023-04-17 14:03:33 -0400

    [notebook] Make presentation_id consistent and use data-attribute for rrd (#1881)
    The presentation_id is used to avoid conflicts with multiple recordings in a single notebook.
    
    Also realized we should be using data-attribute instead of storing the data in the text-string. See: https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes

--- rerun_py/rerun_sdk/rerun/recording.py
+++ rerun_py/rerun_sdk/rerun/recording.py
@@ -34,34 +34,35 @@ class MemoryRecording:
         if app_location is None:
             app_location = bindings.get_app_url()
 
-        random_string = "".join(random.choice(string.ascii_letters) for i in range(6))
+        # Use a random presentation ID to avoid collisions when multiple recordings are shown in the same notebook.
+        presentation_id = "".join(random.choice(string.ascii_letters) for i in range(6))
 
         base64_data = base64.b64encode(self.storage.get_rrd_as_bytes()).decode("utf-8")
 
         html_template = f"""
-        <div id="{random_string}_rrd" style="display: none;">{base64_data}</div>
-        <div id="{random_string}_error" style="display: none;"><p>Timed out waiting for {app_location} to load.</p>
+        <div id="{presentation_id}_rrd" style="display: none;" data-rrd="{base64_data}"></div>
+        <div id="{presentation_id}_error" style="display: none;"><p>Timed out waiting for {app_location} to load.</p>
         <p>Consider using <code>rr.self_host_assets()</code></p></div>
         <script>
-            timeout_{random_string} = setTimeout(() => {{
-                document.getElementById("{random_string}_error").style.display = 'block';
+            {presentation_id}_timeout = setTimeout(() => {{
+                document.getElementById("{presentation_id}_error").style.display = 'block';
             }}, {timeout_ms});
 
             window.addEventListener("message", function(rrd) {{
-                return async function onIframeReady_{random_string}(event) {{
-                    var iframe = document.getElementById("{random_string}");
+                return async function {presentation_id}_onIframeReady(event) {{
+                    var iframe = document.getElementById("{presentation_id}_iframe");
                     if (event.source === iframe.contentWindow) {{
-                        clearTimeout(timeout_{random_string});
-                        document.getElementById("{random_string}_error").style.display = 'none';
+                        clearTimeout({presentation_id}_timeout);
+                        document.getElementById("{presentation_id}_error").style.display = 'none';
                         iframe.style.display = 'inline';
-                        window.removeEventListener("message", onIframeReady_{random_string});
+                        window.removeEventListener("message", {presentation_id}_onIframeReady);
                         iframe.contentWindow.postMessage((await rrd), "*");
                     }}
                 }}
             }}(async function() {{
                 await new Promise(r => setTimeout(r, 0));
-                var div = document.getElementById("{random_string}_rrd");
-                var base64Data = div.textContent;
+                var div = document.getElementById("{presentation_id}_rrd");
+                var base64Data = div.dataset.rrd;
                 var intermediate = atob(base64Data);
                 var buff = new Uint8Array(intermediate.length);
                 for (var i = 0; i < intermediate.length; i++) {{
@@ -70,7 +71,8 @@ class MemoryRecording:
                 return buff;
             }}()));
         </script>
-        <iframe id="{random_string}" width="{width}" height="{height}" src="{app_location}?url=web_event://&persist=0"
+        <iframe id="{presentation_id}_iframe" width="{width}" height="{height}"
+            src="{app_location}?url=web_event://&persist=0"
             frameborder="0" style="display: none;" allowfullscreen=""></iframe>
         """
 
