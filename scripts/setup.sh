d2122e7bb655317ffdddde2de61e3e9c649fcf66
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-04-20 22:41:45 +0200

    Use 1.69 everywhere

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -32,7 +32,7 @@ elif [ -x "$(command -v dnf)" ];   then
 fi
 
 # Needed to compile and check the code:
-rustup install 1.67.1
+rustup install 1.69.0
 ./scripts/setup_web.sh
 
 echo "setup.sh completed!"
