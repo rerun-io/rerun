e884038275785e06954d6b90c16796abf5aeb139
Jeremy Leibs <jeremy@rerun.io>
2023-04-29 22:01:55 +0200

    Chery-pick build changes into hotfix-0.5.1

--- scripts/clippy_wasm.sh
+++ scripts/clippy_wasm.sh
@@ -7,7 +7,7 @@ script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$script_path/.."
 set -x
 
-# Use clippy_wasm/clippy.toml
-export CLIPPY_CONF_DIR="clippy_wasm"
+# Use scripts/clippy_wasm/clippy.toml
+export CLIPPY_CONF_DIR="scripts/clippy_wasm"
 
 cargo cranky --all-features --target wasm32-unknown-unknown --target-dir target_wasm -p re_viewer -- --deny warnings
