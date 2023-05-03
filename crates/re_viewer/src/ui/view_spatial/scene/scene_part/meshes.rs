4b5aa3cfb3ee8a79484cb6a395baf5aff3f6f4d6
Andreas Reich <r_andreas2@web.de>
2023-05-03 16:37:23 +0200

    Remove unused MeshSourceData

--- crates/re_viewer/src/ui/view_spatial/scene/scene_part/meshes.rs
+++ crates/re_viewer/src/ui/view_spatial/scene/scene_part/meshes.rs
@@ -11,7 +11,7 @@ use crate::{
     misc::{SpaceViewHighlights, TransformCache, ViewerContext},
     ui::{
         scene::SceneQuery,
-        view_spatial::{MeshSource, MeshSourceData, SceneSpatial},
+        view_spatial::{MeshSource, SceneSpatial},
         DefaultColor,
     },
 };
@@ -49,11 +49,7 @@ impl MeshPart {
                 if let Some(mesh) = ctx
                     .cache
                     .mesh
-                    .load(
-                        &ent_path.to_string(),
-                        &MeshSourceData::Mesh3D(mesh),
-                        ctx.render_ctx,
-                    )
+                    .load(&ent_path.to_string(), &mesh, ctx.render_ctx)
                     .map(|cpu_mesh| MeshSource {
                         picking_instance_hash,
                         world_from_mesh: world_from_obj_affine,
