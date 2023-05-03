06be29a4c45232308563eb36b4230fe5ba418652
Jeremy Leibs <jeremy@rerun.io>
2023-05-04 01:41:48 +0200

    Update blueprint examples

--- examples/python/blueprint/blueprint.py
+++ examples/python/blueprint/blueprint.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 import rerun as rr
 
@@ -9,11 +10,15 @@ def main() -> None:
         "--init-logging", action="store_true", help="'Accidentally initialize the logging stream as well'"
     )
     parser.add_argument("--append", action="store_true", help="Append to the blueprint instead of overwriting it")
+    parser.add_argument("--enable-heuristics", action="store_true", help="Enable heuristics for the blueprint")
 
     args = parser.parse_args()
 
     rr.init("Space", init_blueprint=True, init_logging=args.init_logging, append_blueprint=args.append)
     rr.connect()
+
+    if args.enable_heuristics:
+        rr.enable_heuristics()
 
     rr.add_space_view("earth-centric", "transforms3d/sun/planet", ["transforms3d/sun", "transforms3d/sun/planet"])
 
