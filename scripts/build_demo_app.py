bf478f6e387dfbed1d75f719455408da990674df
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-05-10 11:56:52 +0200

    build demo app into `web_demo` dir
    the script:
    - cleans the `web_demo` dir
    - copies static assets (favicon, index.html, js, css, etc.)
    - uses `re_build_web_viewer` to build the WASM
      and copies the binaries to `web_demo`
    - runs the examples with `--save`
    - renders the example template for each example

--- scripts/build_demo_app.py
+++ scripts/build_demo_app.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
 
-"""Build `.rrd` files for all examples for deployment to app.rerun.io."""
+"""Build `demo.rerun.io`."""
 
 import os
 import shutil
 import subprocess
-from glob import glob
 from typing import List
 
 from jinja2 import Template
 
+BASE_PATH = "web_demo"
+SCRIPT_PATH = os.path.dirname(os.path.relpath(__file__))
+EXAMPLES = [
+    "api_demo",
+    "car",
+    "clock",
+    "colmap",
+    "deep_sdf",
+    "dicom",
+    "nyud",
+    "plots",
+    "raw_mesh",
+    "text_logging",
+]
+
 
 class Example:
-    def __init__(self, path: str):
-        self.path = path
-        self.name = os.path.basename(os.path.dirname(path))
+    def __init__(self, name: str):
+        self.path = os.path.join("examples/python", name, "main.py")
+        self.name = name
         self.source_url = f"https://github.com/rerun-io/rerun/tree/main/examples/python/{self.name}/main.py"
 
     def save(self) -> None:
         in_path = os.path.abspath(self.path)
-        out_dir = f"./web_viewer/examples/{self.name}"
+        out_dir = f"{BASE_PATH}/examples/{self.name}"
 
         print(f"\nRunning {in_path}, outputting to {out_dir}")
         os.makedirs(out_dir, exist_ok=True)
@@ -39,35 +53,53 @@ class Example:
             return "script_add_args" in f.read()
 
 
-def render_app_index(example: Example, all_examples: List[Example]) -> str:
-    template_path = os.path.join(os.path.dirname(os.path.relpath(__file__)), "templates/app_index.html")
+def clean() -> None:
+    shutil.rmtree(BASE_PATH, ignore_errors=True)
+
+
+def copy_static_assets() -> None:
+    shutil.copytree(os.path.join(SCRIPT_PATH, "demo_assets/static"), os.path.join(BASE_PATH))
+
+
+def build_and_copy_wasm() -> None:
+    subprocess.run(["cargo", "r", "-p", "re_build_web_viewer", "--", "--release"])
+    subprocess.run(["cargo", "r", "-p", "re_build_web_viewer", "--", "--debug"])
+
+    files = ["re_viewer_bg.wasm", "re_viewer_debug_bg.wasm", "re_viewer.js", "re_viewer_debug.js"]
+    for file in files:
+        shutil.copyfile(
+            os.path.join("web_viewer", file),
+            os.path.join(BASE_PATH, file),
+        )
+
+
+def build_examples() -> List[Example]:
+    examples: List[Example] = []
+    for path in EXAMPLES:
+        example = Example(path)
+        if example.supports_save():
+            example.save()
+            examples.append(example)
+    return examples
+
+
+def render_examples(examples: List[Example]) -> None:
+    template_path = os.path.join(SCRIPT_PATH, "demo_assets/templates/example.html")
     with open(template_path) as f:
         template = Template(f.read())
 
-    return template.render(example=example, examples=all_examples)
+    for example in examples:
+        index_path = f"{BASE_PATH}/examples/{example.name}/index.html"
+        with open(index_path, "w") as f:
+            f.write(template.render(example=example, examples=examples))
 
 
 def main() -> None:
-    assert os.path.exists("./web_viewer")
-
-    shutil.rmtree("./web_viewer/examples", ignore_errors=True)
-
-    # TODO(jprochazk): get these to work too
-    filter = ["ros", "opencv_canny", "stable_diffusion", "segment_anything"]
-
-    examples: List[Example] = []
-    # run examples to produce `.rrd` files
-    for path in glob("examples/python/**/main.py"):
-        example = Example(path)
-        if example.name not in filter and example.supports_save():
-            example.save()
-            examples.append(example)
-
-    # render templates
-    for example in examples:
-        index_path = f"./web_viewer/examples/{example.name}/index.html"
-        with open(index_path, "w") as f:
-            f.write(render_app_index(example, examples))
+    clean()
+    copy_static_assets()
+    build_and_copy_wasm()
+    examples = build_examples()
+    render_examples(examples)
 
 
 if __name__ == "__main__":
