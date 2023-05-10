4f44f3e5b95549b010214a6ed89d193cdf0c8573
Clement Rey <cr.rey.clement@gmail.com>
2023-05-10 09:36:57 +0200

    Update crates/re_viewer/src/app.rs
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- crates/re_viewer/src/app.rs
+++ crates/re_viewer/src/app.rs
@@ -1372,10 +1372,7 @@ fn memory_use_label_ui(ui: &mut egui::Ui, gpu_resource_stats: &WgpuResourcePoolS
             .on_hover_ui(|ui| add_contents_on_hover(ui))
             .clicked()
         {
-            #[cfg(not(target_arch = "wasm32"))]
-            re_viewer_context::Clipboard::with(|clipboard| {
-                clipboard.set_text(CODE.into());
-            });
+            ui.ctx().output_mut(|o| o.copied_text = CODE.clone());
         }
     }
 
