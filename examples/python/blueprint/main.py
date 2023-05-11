48bfa80e5c20151426d52fd5eda2b4eb833f7bdb
Jeremy Leibs <jeremy@rerun.io>
2023-05-11 03:53:55 +0200

    Py-lint

--- examples/python/blueprint/main.py
+++ examples/python/blueprint/main.py
@@ -11,7 +11,9 @@ def main() -> None:
 
     parser.add_argument("--blueprint-only", action="store_true", help="Only send the blueprint")
     parser.add_argument("--skip-blueprint", action="store_true", help="Don't send the blueprint")
-    parser.add_argument("--append-default", action="store_true", help="Append to the default blueprint instead of replacing it")
+    parser.add_argument(
+        "--append-default", action="store_true", help="Append to the default blueprint instead of replacing it"
+    )
     parser.add_argument("--auto-space-views", action="store_true", help="Automatically add space views")
 
     args = parser.parse_args()
@@ -27,11 +29,10 @@ def main() -> None:
     if not args.blueprint_only:
         img = np.zeros([128, 128, 3], dtype="uint8")
         for i in range(8):
-            img[(i*16)+4:(i*16)+12,:] = (0, 0, 200)
+            img[(i * 16) + 4 : (i * 16) + 12, :] = (0, 0, 200)
         rr.log_image("image", img)
         rr.log_rect("rect/0", [16, 16, 64, 64], label="Rect1", color=(255, 0, 0))
         rr.log_rect("rect/1", [48, 48, 64, 64], label="Rect2", color=(0, 255, 0))
-
 
     if not args.skip_blueprint:
         if args.auto_space_views:
