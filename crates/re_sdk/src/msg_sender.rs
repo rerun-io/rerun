03101bb2b7eaa5d45a5b90377dac7cfed516ea6e
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-05-12 11:20:52 +0200

    Add MsgSender::into_log_msg

--- crates/re_sdk/src/msg_sender.rs
+++ crates/re_sdk/src/msg_sender.rs
@@ -1,4 +1,4 @@
-use re_log_types::{component_types::InstanceKey, DataRow, DataTableError, RowId};
+use re_log_types::{component_types::InstanceKey, DataRow, DataTableError, RecordingId, RowId};
 
 use crate::{
     log::DataCell,
@@ -328,6 +328,19 @@ impl MsgSender {
         }
 
         Ok(())
+    }
+
+    /// Turns the current message into a single [`re_log_types::LogMsg`]
+    pub fn into_log_msg(
+        self,
+        recording_id: RecordingId,
+    ) -> Result<re_log_types::LogMsg, DataTableError> {
+        let data_table = re_log_types::DataTable::from_rows(
+            re_log_types::TableId::random(),
+            self.into_rows().into_iter().flatten(),
+        );
+        let arrow_msg = data_table.to_arrow_msg()?;
+        Ok(re_log_types::LogMsg::ArrowMsg(recording_id, arrow_msg))
     }
 
     fn into_rows(self) -> [Option<DataRow>; 2] {
