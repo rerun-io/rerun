b0932170375a568e364372010f26b2583a8dc31b
Clement Rey <cr.rey.clement@gmail.com>
2023-05-17 17:26:46 +0200

    fmt

--- scripts/fetch_crashes.py
+++ scripts/fetch_crashes.py
@@ -121,7 +121,8 @@ for res in results:
     backtrace = res["properties"].pop("callstack").encode("utf-8").strip()
     backtraces[backtrace].append(res.pop("properties"))
 
-count_uniques = lambda backtrace: len(set([prop["user_id"] for prop in backtrace[1]]))
+def count_uniques(backtrace):
+    return len(set([prop["user_id"] for prop in backtrace[1]]))
 
 backtraces = list(backtraces.items())
 backtraces.sort(key=count_uniques, reverse=True)
