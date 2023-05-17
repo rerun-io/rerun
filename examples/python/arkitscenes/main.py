fix quaternion logging in examples

examples/python/arkitscenes/main.py
@@ -290,7 +290,7 @@ def read_camera_from_world(traj_string: str) -> Tuple[str, rr.TranslationRotatio
     translation = np.asarray([float(tokens[4]), float(tokens[5]), float(tokens[6])])
 
     # Create tuple in format log_transform3d expects
-    camera_from_world = rr.TranslationRotationScale3D(translation, rotation.as_quat())
+    camera_from_world = rr.TranslationRotationScale3D(translation, rr.Quaternion(xyzw=rotation.as_quat()))
 
     return (ts, camera_from_world)
 

examples/python/ros/main.py
@@ -158,7 +158,9 @@ class TurtleSubscriber(Node):  # type: ignore[misc]
             tf = self.tf_buffer.lookup_transform(parent_frame, child_frame, time, timeout=Duration(seconds=0.1))
             t = tf.transform.translation
             q = tf.transform.rotation
-            rr.log_transform3d(path, rr.TranslationRotationScale3D([t.x, t.y, t.z], [q.x, q.y, q.z, q.w]))
+            rr.log_transform3d(
+                path, rr.TranslationRotationScale3D([t.x, t.y, t.z], rr.Quaternion([q.x, q.y, q.z, q.w]))
+            )
         except TransformException as ex:
             print("Failed to get transform: {}".format(ex))
 