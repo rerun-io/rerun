09c255cb7d9511c2e34314c32426d8692588cb75
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2023-05-19 09:27:09 +0200

    Fix setup scripts for ARM Mac/MacPort configuration (#2169)

--- scripts/setup_web.sh
+++ scripts/setup_web.sh
@@ -23,6 +23,7 @@ cargo install wasm-bindgen-cli --git https://github.com/rerun-io/wasm-bindgen.gi
 # binaryen gives us wasm-opt, for optimizing the an .wasm file for speed and size
 packagesNeeded='binaryen'
 if [ -x "$(command -v brew)" ];      then brew install $packagesNeeded
+elif [ -x "$(command -v port)" ];    then sudo port install $packagesNeeded
 elif [ -x "$(command -v apt-get)" ]; then sudo apt-get -y install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
