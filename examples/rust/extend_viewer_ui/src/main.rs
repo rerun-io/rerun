db9dbd035abc79237a473c6c5b507dc4312f190a
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-05-29 16:57:57 +0200

    Better comments, disembiguating the different types of logging

--- examples/rust/extend_viewer_ui/src/main.rs
+++ examples/rust/extend_viewer_ui/src/main.rs
@@ -13,14 +13,14 @@ static GLOBAL: re_memory::AccountingAllocator<mimalloc::MiMalloc> =
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Direct `log` calls to stderr
+    // Direct calls using the `log` crate to stderr. Control with `RUST_LOG=debug` etc.
     re_log::setup_native_logging();
 
-    // Log crashes and signals, and send them to analytics if
-    // the `analytics` feature is on in `Cargo.toml`
+    // Install handlers for panics and crashes that prints to stderr and send
+    // them to Rerun analytics (if the `analytics` feature is on in `Cargo.toml`).
     re_crash_handler::install_crash_handlers(re_viewer::build_info());
 
-    // Listen for TCP connections from logging SDKs.
+    // Listen for TCP connections from Rerun's logging SDKs.
     // There are other ways of "feeding" the viewer though - all you need is a `re_smart_channel::Receiver`.
     let rx = re_sdk_comms::serve(
         "0.0.0.0",
