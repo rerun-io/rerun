a705d7d7f1eaa28c4494d0c143d08c0600472245
Andreas Reich <r_andreas2@web.de>
2023-05-29 21:25:49 +0200

    Add re_space_view crate

---
+++ crates/re_space_view/README.md
@@ -0,0 +1,11 @@
+# re_time_panel
+
+Part of the [`rerun`](https://github.com/rerun-io/rerun) family of crates.
+
+[![Latest version](https://img.shields.io/crates/v/re_space_view.svg)](https://crates.io/crates/re_space_view)
+[![Documentation](https://docs.rs/re_space_view/badge.svg)](https://docs.rs/re_space_view)
+![MIT](https://img.shields.io/badge/license-MIT-blue.svg)
+![Apache](https://img.shields.io/badge/license-Apache-blue.svg)
+
+Defines a framework & utilities for space views in the Rerun viewer.
+Does not implement any concrete space view.
---
+++ crates/re_space_view/src/lib.rs
@@ -0,0 +1,25 @@
+//! Rerun Time Panel
+//!
+//! Defines a framework & utilities for space views in the Rerun viewer.
+//! Does not implement any concrete space view.
+// ---------------------------------------------------------------------------
+
+/// Profiling macro for feature "puffin"
+#[doc(hidden)]
+#[macro_export]
+macro_rules! profile_function {
+    ($($arg: tt)*) => {
+        #[cfg(not(target_arch = "wasm32"))]
+        puffin::profile_function!($($arg)*);
+    };
+}
+
+/// Profiling macro for feature "puffin"
+#[doc(hidden)]
+#[macro_export]
+macro_rules! profile_scope {
+    ($($arg: tt)*) => {
+        #[cfg(not(target_arch = "wasm32"))]
+        puffin::profile_scope!($($arg)*);
+    };
+}
