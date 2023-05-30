5ae6c24f6960a0267bf7c97a7ca5f149159a733c
Andreas Reich <r_andreas2@web.de>
2023-05-30 21:14:54 +0200

    update publish script

--- scripts/publish_crates.sh
+++ scripts/publish_crates.sh
@@ -117,8 +117,9 @@ cargo publish $FLAGS -p re_web_viewer_server
 cargo publish $FLAGS -p re_viewer_context
 cargo publish $FLAGS -p re_data_ui
 cargo publish $FLAGS -p re_time_panel
-cargo publish $FLAGS -p re_space_view
 cargo publish $FLAGS -p re_viewport
+cargo publish $FLAGS -p re_space_view_text
+cargo publish $FLAGS -p re_space_view_text_box
 cargo publish $FLAGS -p re_viewer
 cargo publish $FLAGS -p re_sdk
 cargo publish $FLAGS -p rerun
