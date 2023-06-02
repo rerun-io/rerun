e0ac6cc01753b2b22ecc1d4f26fda4f77c3f4482
Andreas Reich <r_andreas2@web.de>
2023-06-02 09:23:58 +0200

    better error handling for queuable draw data

--- crates/re_renderer/src/queuable_draw_data.rs
+++ crates/re_renderer/src/queuable_draw_data.rs
@@ -1,17 +1,27 @@
-use anyhow::Context;
-
 use crate::{
     draw_phases::DrawPhase,
     renderer::{DrawData, Renderer},
     RenderContext,
 };
 
+#[derive(thiserror::Error, Debug)]
+pub enum QueueableDrawDataError {
+    #[error("Failed to retrieve renderer of type {0}")]
+    FailedToRetrieveRenderer(&'static str),
+
+    #[error("Mismatching draw data type, expected {0}")]
+    UnexpectedDrawDataType(&'static str),
+
+    #[error(transparent)]
+    DrawError(#[from] anyhow::Error),
+}
+
 type DrawFn = dyn for<'a, 'b> Fn(
         &'b RenderContext,
         DrawPhase,
         &'a mut wgpu::RenderPass<'b>,
         &'b dyn std::any::Any,
-    ) -> anyhow::Result<()>
+    ) -> Result<(), QueueableDrawDataError>
     + Sync
     + Send;
 
@@ -28,13 +38,17 @@ impl<D: DrawData + Sync + Send + 'static> From<D> for QueueableDrawData {
         QueueableDrawData {
             draw_func: Box::new(move |ctx, phase, pass, draw_data| {
                 let renderers = ctx.renderers.read();
-                let renderer = renderers
-                    .get::<D::Renderer>()
-                    .context("failed to retrieve renderer")?;
-                let draw_data = draw_data
-                    .downcast_ref::<D>()
-                    .expect("passed wrong type of draw data");
-                renderer.draw(&ctx.gpu_resources, phase, pass, draw_data)
+                let renderer = renderers.get::<D::Renderer>().ok_or(
+                    QueueableDrawDataError::FailedToRetrieveRenderer(std::any::type_name::<
+                        D::Renderer,
+                    >()),
+                )?;
+                let draw_data = draw_data.downcast_ref::<D>().ok_or(
+                    QueueableDrawDataError::UnexpectedDrawDataType(std::any::type_name::<D>()),
+                )?;
+                renderer
+                    .draw(&ctx.gpu_resources, phase, pass, draw_data)
+                    .map_err(QueueableDrawDataError::from)
             }),
             draw_data: Box::new(draw_data),
             renderer_name: std::any::type_name::<D::Renderer>(),
