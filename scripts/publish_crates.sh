ad695b599b902295bac0545ff56c924599819c5e
Andreas Reich <r_andreas2@web.de>
2023-06-07 10:23:08 +0200

    update crate publish script

--- scripts/publish_crates.sh
+++ scripts/publish_crates.sh
@@ -121,6 +121,7 @@ cargo publish $FLAGS -p re_data_ui
 cargo publish $FLAGS -p re_time_panel
 cargo publish $FLAGS -p re_space_view
 cargo publish $FLAGS -p re_viewport
+cargo publish $FLAGS -p re_space_view_bar_chart
 cargo publish $FLAGS -p re_space_view_text
 cargo publish $FLAGS -p re_space_view_text_box
 cargo publish $FLAGS -p re_space_view_spatial
