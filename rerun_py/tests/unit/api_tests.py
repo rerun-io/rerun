0d131555ab3b5dc9df1ce1326b5965526cc4870b
Clement Rey <cr.rey.clement@gmail.com>
2023-06-09 17:11:56 +0200

    remove dead code

--- rerun_py/tests/unit/api_tests.py
+++
@@ -1,8 +0,0 @@
-import rerun as rr
-from rerun.log.text import LogLevel
-
-
-def test_text() -> None:
-    rr.log_text_entry("path", "text", level=None)
-    rr.log_text_entry("path", "text", level=LogLevel.INFO)
-    rr.log_text_entry("path", None, level=LogLevel.INFO)  # type: ignore[arg-type]
