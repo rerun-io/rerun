942af69d6f226ed9eb55016aa33613140fb77adf
Clement Rey <cr.rey.clement@gmail.com>
2023-06-12 23:21:11 +0200

    Fix blueprint sync by bringing back `store_one_component` (#2396)
    Turns out `store_one_component` is more than a datastore helper... it
    actually updates a whole bunch of secondary/derived datastructures at
    the same time.
    
    At some point we should really start moving towards a reactive model
    where secondary indices merely subscribe to the changelog of a single
    source of truth and react appropriately.
    
    Supersedes #2395

--- crates/re_data_store/src/store_db.rs
+++ crates/re_data_store/src/store_db.rs
@@ -76,6 +76,9 @@ impl EntityDb {
     }
 
     // TODO(jleibs): If this shouldn't be public, chain together other setters
+    // TODO(cmc): Updates of secondary datastructures should be the result of subscribing to the
+    // datastore's changelog and reacting to these changes appropriately. We shouldn't be creating
+    // many sources of truth.
     pub fn try_add_data_row(&mut self, row: &DataRow) -> Result<(), Error> {
         for (&timeline, &time_int) in row.timepoint().iter() {
             self.times_per_timeline.insert(timeline, time_int);
