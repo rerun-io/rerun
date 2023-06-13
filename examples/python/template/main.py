327a1e598384e0f6770064ae439baa62062d82f9
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-06-13 22:32:03 +0200

    remove log forwarding

--- examples/python/template/main.py
+++ examples/python/template/main.py
@@ -11,19 +11,8 @@ pip install -r examples/python/template/requirements.txt
 from __future__ import annotations
 
 import argparse
-import logging
 
 import rerun as rr  # pip install rerun-sdk
-
-
-def setup_logging() -> None:
-    # Forward all text logs to Rerun by attaching a handler directly to the root logger,
-    # which will catch events from all loggers going forward (propagation is on by
-    # default).
-    #
-    # For more info: https://docs.python.org/3/howto/logging.html#handlers
-    logging.getLogger().addHandler(rr.log.text.LoggingHandler())
-    logging.getLogger().setLevel(-1)
 
 
 def main() -> None:
@@ -32,8 +21,6 @@ def main() -> None:
     args = parser.parse_args()
 
     rr.script_setup(args, "my_example_name")
-
-    setup_logging()
 
     # ... example code
 
