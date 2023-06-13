61a44e5a4ccebd655aa3f957ace01d6322afd4c4
Clement Rey <cr.rey.clement@gmail.com>
2023-06-13 16:29:25 +0200

    tests and extensions for components.InstanceKey

--- rerun_py/rerun_sdk/rerun2/components/instance_key_ext.py
+++ rerun_py/rerun_sdk/rerun2/components/instance_key_ext.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-__all__ = ["InstanceKeyArrayExt"]
-
 from typing import Any, Sequence
 
 import numpy as np
@@ -10,12 +8,10 @@ import pyarrow as pa
 
 class InstanceKeyArrayExt:
     @staticmethod
-    def _from_similar(
-        data: Any | None, *, mono: type, mono_aliases: Any, many: type, many_aliases: Any, arrow: type
-    ) -> pa.Array:
+    def _from_similar(data: Any | None, *, mono: type, mono_aliases: type, many: type, many_aliases: type, arrow: type):
         if isinstance(data, Sequence) and (len(data) > 0 and isinstance(data[0], mono)):
             array = np.asarray([datum.value for datum in data], np.uint64)
         else:
-            array = np.asarray(data, dtype=np.uint64).flatten()
+            array = np.require(np.asarray(data), np.uint64).flatten()
 
         return arrow().wrap_array(pa.array(array, type=arrow().storage_type))
