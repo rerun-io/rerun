fdf1a7169133adca5cbb69968489883ddbd8affe
Antoine Beyeler <antoine@rerun.io>
2023-06-13 12:18:03 +0200

    Added support for creating multi-resolution stacks to upload_image.py

--- scripts/upload_image.py
+++ scripts/upload_image.py
@@ -19,42 +19,149 @@ If you get this error:
 Then run `python3 -m pip install cryptography==38.0.4`
 (https://levelup.gitconnected.com/fix-attributeerror-module-lib-has-no-attribute-openssl-521a35d83769)
 """
+
+# TODO:
+#  - pngcrush everything?
+#  - document how to use this script
+
 from __future__ import annotations
 
 import argparse
 import hashlib
+import logging
 import mimetypes
-import os
+from io import BytesIO
+from pathlib import Path
+from typing import BinaryIO
 
+import PIL
+import PIL.Image
 from google.cloud import storage
+from PIL.Image import Image, Resampling
+
+SIZES = [
+    480,
+    768,
+    1024,
+    1200,
+]
 
 
-def content_hash(path: str) -> str:
+class Uploader:
+    def __init__(self):
+        gcs = storage.Client("rerun-open")
+        self.bucket = gcs.bucket("rerun-static-img")
+
+    def upload_file(self, path: Path) -> str:
+        path = path.resolve()
+        digest = content_hash(path)
+
+        object_name = f"{digest}_{path.name}"
+        content_type, content_encoding = mimetypes.guess_type(path)
+        with open(path, "rb") as f:
+            self.upload_data(f, object_name, content_type, content_encoding)
+
+        return object_name
+
+    def upload_stack(self, image_path: Path) -> str:
+        """Create a multi-resolution stack and upload it."""
+
+        image = PIL.Image.open(image_path)
+        image_format = image.format
+        file_ext = image_path.suffix
+        content_type, _ = mimetypes.guess_type(image_path)
+
+        logging.info(f"Base image width: {image.width}px")
+
+        # build image stack
+        image_stack: list[tuple[str, int | None, Image]] = []
+        for width in SIZES:
+            if image.width > width:
+                logging.info(f"Resizing to: {width}px")
+                new_image = image.resize(
+                    size=(width, int(width * image.height / image.width)), resample=Resampling.LANCZOS
+                )
+
+                image_stack.append((f"{image_path.stem}_{width}w", width, new_image))
+
+        image_stack.append((f"{image_path.stem}_full", None, image))
+
+        html_str = "<picture>\n"
+
+        # upload images
+        for name, width, image in image_stack:
+            with BytesIO() as buffer:
+                image.save(buffer, image_format)
+                buffer.seek(0)
+                digest = content_hash(buffer)
+                buffer.seek(0)
+
+                object_name = f"{digest}_{name}{file_ext}"
+                logging.info(f"Uploading image: {object_name} (size: {buffer.getbuffer().nbytes} bytes)")
+                self.upload_data(buffer, object_name, content_type, None)
+
+                if width is not None:
+                    html_str += (
+                        f'  <source media="(min-width: {width}px)" srcset="https://static.rerun.io/{object_name}">\n'
+                    )
+                else:
+                    html_str += f'  <img src="https://static.rerun.io/{object_name}" alt="">\n'
+
+        html_str += "</picture>"
+        return html_str
+
+    def upload_data(
+        self, data: BinaryIO, name: str, content_type: str | None = None, content_encoding: str | None = None
+    ):
+        """Low-level upload of data."""
+
+        logging.debug(f"Uploading {name} (type: {content_type}, encoding: {content_encoding})")
+        destination = self.bucket.blob(name)
+        destination.content_type = content_type
+        destination.content_encoding = content_encoding
+        destination.upload_from_file(data)
+
+
+def content_hash(data: Path | BinaryIO) -> str:
     h = hashlib.sha1()
     b = bytearray(128 * 1024)
     mv = memoryview(b)
-    with open(path, "rb", buffering=0) as f:
+
+    def update(f: BinaryIO) -> None:
         while n := f.readinto(mv):
             h.update(mv[:n])
+
+    if isinstance(data, Path):
+        with open(data, "rb", buffering=0) as f:
+            update(f)
+    else:
+        update(data)
+
     return h.hexdigest()
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Upload an image.")
-    parser.add_argument("path", type=str, help="Path to the image.")
+    parser.add_argument("path", type=Path, help="Path to the image.")
+    parser.add_argument(
+        "--single", action="store_true", help="Upload a single image instead of creating a multi-resolution stack."
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging.")
     args = parser.parse_args()
 
-    hash = content_hash(args.path)
-    object_name = f"{hash}_{os.path.basename(args.path)}"
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
-    gcs = storage.Client("rerun-open")
-    bucket = gcs.bucket("rerun-static-img")
-    destination = bucket.blob(object_name)
-    destination.content_type, destination.content_encoding = mimetypes.guess_type(args.path)
-    with open(args.path, "rb") as f:
-        destination.upload_from_file(f)
+    uploader = Uploader()
 
-    print(f"https://static.rerun.io/{object_name}")
+    if args.single:
+        object_name = uploader.upload_file(args.path)
+        print(f"https://static.rerun.io/{object_name}")
+    else:
+        html_str = uploader.upload_stack(args.path)
+        print("\n" + html_str)
 
 
 if __name__ == "__main__":
