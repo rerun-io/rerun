1bb25dff27889b66f11ee8cec7235208c4614ea7
Clement Rey <cr.rey.clement@gmail.com>
2023-06-13 11:52:57 +0200

    tests and extensions for components.Radius

--- rerun_py/rerun_sdk/rerun2/components/radius_ext.py
+++ rerun_py/rerun_sdk/rerun2/components/radius_ext.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["Point2DArrayExt"]
+__all__ = ["RadiusArrayExt"]
 
 from typing import Any, Sequence
 
@@ -8,12 +8,12 @@ import numpy as np
 import pyarrow as pa
 
 
-class Point2DArrayExt:
+class RadiusArrayExt:
     @staticmethod
     def _from_similar(data: Any | None, *, mono: type, mono_aliases: type, many: type, many_aliases: type, arrow: type):
         if isinstance(data, Sequence) and (len(data) > 0 and isinstance(data[0], mono)):
-            arrays = [np.asarray(datum) for datum in data]
+            array = np.asarray([radius.value for radius in data], np.float32)
         else:
-            arrays = np.require(np.asarray(data), np.float32).reshape((-1, 2)).tolist()
+            array = np.require(np.asarray(data), np.float32).flatten()
 
-        return arrow().wrap_array(pa.array(arrays, type=arrow().storage_type))
+        return arrow().wrap_array(pa.array(array, type=arrow().storage_type))
