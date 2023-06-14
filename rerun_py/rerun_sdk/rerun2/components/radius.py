725f88d78ecd35ef25619e3c888b21fcc748339c
Clement Rey <cr.rey.clement@gmail.com>
2023-06-14 11:53:44 +0200

    force regen this so the PR train makes sense

--- rerun_py/rerun_sdk/rerun2/components/radius.py
+++ rerun_py/rerun_sdk/rerun2/components/radius.py
@@ -21,50 +21,45 @@ class Radius:
     def __array__(self):
         return np.asarray(self.value)
 
+    RadiusLike = Union[Radius, float]
 
-RadiusLike = Union[Radius, float]
+    RadiusArrayLike = Union[RadiusLike, Sequence[RadiusLike], npt.NDArray[np.float32]]
 
-RadiusArrayLike = Union[RadiusLike, Sequence[RadiusLike], npt.NDArray[np.float32]]
+    # --- Arrow support ---
 
+    from rerun2.components.radius_ext import RadiusArrayExt  # noqa: E402
 
-# --- Arrow support ---
+    class RadiusType(pa.ExtensionType):
+        def __init__(self: type[pa.ExtensionType]) -> None:
+            pa.ExtensionType.__init__(self, pa.float32(), "rerun.components.Radius")
 
-from rerun2.components.radius_ext import RadiusArrayExt  # noqa: E402
+        def __arrow_ext_serialize__(self: type[pa.ExtensionType]) -> bytes:
+            # since we don't have a parameterized type, we don't need extra metadata to be deserialized
+            return b""
 
+        @classmethod
+        def __arrow_ext_deserialize__(
+            cls: type[pa.ExtensionType], storage_type: Any, serialized: Any
+        ) -> type[pa.ExtensionType]:
+            # return an instance of this subclass given the serialized metadata.
+            return RadiusType()
 
-class RadiusType(pa.ExtensionType):
-    def __init__(self: type[pa.ExtensionType]) -> None:
-        pa.ExtensionType.__init__(self, pa.float32(), "rerun.components.Radius")
+        def __arrow_ext_class__(self: type[pa.ExtensionType]) -> type[pa.ExtensionArray]:
+            return RadiusArray
 
-    def __arrow_ext_serialize__(self: type[pa.ExtensionType]) -> bytes:
-        # since we don't have a parameterized type, we don't need extra metadata to be deserialized
-        return b""
+    pa.register_extension_type(RadiusType())
 
-    @classmethod
-    def __arrow_ext_deserialize__(
-        cls: type[pa.ExtensionType], storage_type: Any, serialized: Any
-    ) -> type[pa.ExtensionType]:
-        # return an instance of this subclass given the serialized metadata.
-        return RadiusType()
-
-    def __arrow_ext_class__(self: type[pa.ExtensionType]) -> type[pa.ExtensionArray]:
-        return RadiusArray
-
-
-pa.register_extension_type(RadiusType())
-
-
-class RadiusArray(pa.ExtensionArray, RadiusArrayExt):  # type: ignore[misc]
-    @staticmethod
-    def from_similar(data: RadiusArrayLike | None):
-        if data is None:
-            return RadiusType().wrap_array(pa.array([], type=RadiusType().storage_type))
-        else:
-            return RadiusArrayExt._from_similar(
-                data,
-                mono=Radius,
-                mono_aliases=RadiusLike,
-                many=RadiusArray,
-                many_aliases=RadiusArrayLike,
-                arrow=RadiusType,
-            )
+    class RadiusArray(pa.ExtensionArray, RadiusArrayExt):  # type: ignore[misc]
+        @staticmethod
+        def from_similar(data: RadiusArrayLike | None):
+            if data is None:
+                return RadiusType().wrap_array(pa.array([], type=RadiusType().storage_type))
+            else:
+                return RadiusArrayExt._from_similar(
+                    data,
+                    mono=Radius,
+                    mono_aliases=RadiusLike,
+                    many=RadiusArray,
+                    many_aliases=RadiusArrayLike,
+                    arrow=RadiusType,
+                )
