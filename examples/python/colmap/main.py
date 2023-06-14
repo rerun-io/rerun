rerun-io/rerun

commit bf3725b3596ce0274c6c79549772cca727c69dd0

Add `jpeg_quality` parameter to `log_image` (#2418)
Closes https://github.com/rerun-io/rerun/issues/2175

### What
You can now do `log_image("image", image, jpeg_quality=75)` to compress
your logged images so they take up less space on disk and in RAM.

I also updated the UI so you can actually tell whether or not a tensor
was compressed.

This makes a many of our example data lot smaller. For instance,
`tracking_hf_opencv` goes from 753 MB to 101MB.

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)

<!-- This line will get updated when the PR build summary job finishes.
-->
PR Build Summary: https://build.rerun.io/pr/2418

<!-- pr-link-docs:start -->
Docs preview: https://rerun.io/preview/194b4be/docs
Examples preview: https://rerun.io/preview/194b4be/examples
<!-- pr-link-docs:end -->

examples/python/colmap/main.py
@@ -160,9 +160,7 @@ def read_and_log_sparse_reconstruction(dataset_path: Path, filter_output: bool, 
         if resize:
             img = cv2.imread(str(image_file))
             img = cv2.resize(img, resize)
-            jpeg_quality = [int(cv2.IMWRITE_JPEG_QUALITY), 75]
-            _, encimg = cv2.imencode(".jpg", img, jpeg_quality)
-            rr.log_image_file("camera/image", img_bytes=encimg)
+            rr.log_image("camera/image", img, jpeg_quality=75)
         else:
             rr.log_image_file("camera/image", img_path=dataset_path / "images" / image.name)
 

examples/python/arkitscenes/main.py
@@ -396,7 +396,7 @@ def log_arkit(recording_path: Path, include_highres: bool) -> None:
                 colors_list,
             )
 
-            rr.log_image(f"{lowres_posed_entity_id}/rgb", rgb)
+            rr.log_image(f"{lowres_posed_entity_id}/rgb", rgb, jpeg_quality=95)
             rr.log_depth_image(f"{lowres_posed_entity_id}/depth", depth, meter=1000)
 
         # log the high res camera
@@ -420,7 +420,7 @@ def log_arkit(recording_path: Path, include_highres: bool) -> None:
             highres_depth = cv2.imread(f"{depth_dir}/{video_id}_{frame_timestamp}.png", cv2.IMREAD_ANYDEPTH)
 
             highres_rgb = cv2.cvtColor(highres_bgr, cv2.COLOR_BGR2RGB)
-            rr.log_image(f"{highres_entity_id}/rgb", highres_rgb)
+            rr.log_image(f"{highres_entity_id}/rgb", highres_rgb, jpeg_quality=75)
             rr.log_depth_image(f"{highres_entity_id}/depth", highres_depth, meter=1000)
 
 

examples/python/mp_pose/main.py
@@ -45,7 +45,7 @@ def track_pose(video_path: str, segment: bool) -> None:
             rgb = cv2.cvtColor(bgr_frame.data, cv2.COLOR_BGR2RGB)
             rr.set_time_seconds("time", bgr_frame.time)
             rr.set_time_sequence("frame_idx", bgr_frame.idx)
-            rr.log_image("video/rgb", rgb)
+            rr.log_image("video/rgb", rgb, jpeg_quality=75)
 
             results = pose.process(rgb)
             h, w, _ = rgb.shape

examples/python/nyud/main.py
@@ -84,7 +84,7 @@ def log_nyud_data(recording_path: Path, subset_idx: int = 0) -> None:
             if f.filename.endswith(".ppm"):
                 buf = archive.read(f)
                 img_rgb = read_image_rgb(buf)
-                rr.log_image("world/camera/image/rgb", img_rgb)
+                rr.log_image("world/camera/image/rgb", img_rgb, jpeg_quality=95)
 
             elif f.filename.endswith(".pgm"):
                 buf = archive.read(f)

examples/python/tracking_hf_opencv/main.py
@@ -85,7 +85,7 @@ class Detector:
         _, _, scaled_height, scaled_width = inputs["pixel_values"].shape
         scaled_size = (scaled_width, scaled_height)
         rgb_scaled = cv2.resize(rgb, scaled_size)
-        rr.log_image("image_scaled/rgb", rgb_scaled)
+        rr.log_image("image_scaled/rgb", rgb_scaled, jpeg_quality=95)
 
         logging.debug("Pass image to detection network")
         outputs = self.model(**inputs)
@@ -333,7 +333,7 @@ def track_objects(video_path: str) -> None:
             break
 
         rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
-        rr.log_image("image/rgb", rgb)
+        rr.log_image("image/rgb", rgb, jpeg_quality=95)
 
         if not trackers or frame_idx % 40 == 0:
             detections = detector.detect_objects_to_track(rgb=rgb, frame_idx=frame_idx)

.github/workflows/reusable_build_and_test_wheels.yml
@@ -243,7 +243,7 @@ jobs:
         # TODO(jleibs): understand why deps can't be installed in the same step as the wheel
         shell: bash
         run: |
-          pip install deprecated numpy>=1.23 pyarrow==10.0.1 pytest==7.1.2
+          pip install deprecated numpy>=1.23 pillow>=9.5.0 pyarrow==10.0.1 pytest==7.1.2
 
       - name: Install built wheel
         if: needs.set-config.outputs.RUN_TESTS == 'true'

crates/re_data_ui/src/image.rs
@@ -55,7 +55,7 @@ fn tensor_ui(
     verbosity: UiVerbosity,
     entity_path: &re_data_store::EntityPath,
     annotations: &Annotations,
-    _encoded_tensor: &Tensor,
+    original_tensor: &Tensor,
     tensor: &DecodedTensor,
 ) {
     // See if we can convert the tensor to a GPU texture.
@@ -100,18 +100,25 @@ fn tensor_ui(
                 }
 
                 ui.label(format!(
-                    "{} x {}",
+                    "{} x {}{}",
                     tensor.dtype(),
-                    format_tensor_shape_single_line(tensor.shape())
+                    format_tensor_shape_single_line(tensor.shape()),
+                    if original_tensor.data.is_compressed_image() {
+                        " (compressed)"
+                    } else {
+                        ""
+                    }
                 ))
-                .on_hover_ui(|ui| tensor_summary_ui(ctx.re_ui, ui, tensor, &tensor_stats));
+                .on_hover_ui(|ui| {
+                    tensor_summary_ui(ctx.re_ui, ui, original_tensor, tensor, &tensor_stats);
+                });
             });
         }
 
         UiVerbosity::All | UiVerbosity::Reduced => {
             ui.vertical(|ui| {
                 ui.set_min_width(100.0);
-                tensor_summary_ui(ctx.re_ui, ui, tensor, &tensor_stats);
+                tensor_summary_ui(ctx.re_ui, ui, original_tensor, tensor, &tensor_stats);
 
                 if let Some(texture) = &texture_result {
                     let max_size = ui
@@ -145,9 +152,9 @@ fn tensor_ui(
 
                     // TODO(emilk): support copying and saving images on web
                     #[cfg(not(target_arch = "wasm32"))]
-                    if _encoded_tensor.data.is_compressed_image() || tensor.could_be_dynamic_image()
+                    if original_tensor.data.is_compressed_image() || tensor.could_be_dynamic_image()
                     {
-                        copy_and_save_image_ui(ui, tensor, _encoded_tensor);
+                        copy_and_save_image_ui(ui, tensor, original_tensor);
                     }
 
                     if let Some([_h, _w, channels]) = tensor.image_height_width_channels() {
@@ -204,16 +211,17 @@ fn show_image_at_max_size(
 pub fn tensor_summary_ui_grid_contents(
     re_ui: &re_ui::ReUi,
     ui: &mut egui::Ui,
-    tensor: &Tensor,
+    original_tensor: &Tensor,
+    tensor: &DecodedTensor,
     tensor_stats: &TensorStats,
 ) {
     let Tensor {
         tensor_id: _,
         shape,
-        data,
+        data: _,
         meaning,
         meter,
-    } = tensor;
+    } = tensor.inner();
 
     re_ui
         .grid_left_hand_label(ui, "Data type")
@@ -256,7 +264,7 @@ pub fn tensor_summary_ui_grid_contents(
         ui.end_row();
     }
 
-    match data {
+    match &original_tensor.data {
         re_components::TensorData::U8(_)
         | re_components::TensorData::U16(_)
         | re_components::TensorData::U32(_)
@@ -309,13 +317,14 @@ pub fn tensor_summary_ui_grid_contents(
 pub fn tensor_summary_ui(
     re_ui: &re_ui::ReUi,
     ui: &mut egui::Ui,
-    tensor: &Tensor,
+    original_tensor: &Tensor,
+    tensor: &DecodedTensor,
     tensor_stats: &TensorStats,
 ) {
     egui::Grid::new("tensor_summary_ui")
         .num_columns(2)
         .show(ui, |ui| {
-            tensor_summary_ui_grid_contents(re_ui, ui, tensor, tensor_stats);
+            tensor_summary_ui_grid_contents(re_ui, ui, original_tensor, tensor, tensor_stats);
         });
 }
 

crates/re_space_view_tensor/src/space_view_class.rs
@@ -94,7 +94,7 @@ impl PerTensorState {
         ctx.re_ui
             .selection_grid(ui, "tensor_selection_ui")
             .show(ui, |ui| {
-                tensor_summary_ui_grid_contents(ctx.re_ui, ui, tensor, &tensor_stats);
+                tensor_summary_ui_grid_contents(ctx.re_ui, ui, tensor, tensor, &tensor_stats);
                 self.texture_settings.ui(ctx.re_ui, ui);
                 self.color_mapping.ui(ctx.render_ctx, ctx.re_ui, ui);
             });

commit 951e84a18c9a1730824db74833568e4632223256

Fix BGR/RGB mixup in colmap demo (#2430)
### What
Bug introduced in https://github.com/rerun-io/rerun/pull/2418

It's back to being red:

![image](https://github.com/rerun-io/rerun/assets/1148717/9ce4ab6b-a782-4b0d-9f75-2c905bf3313e)


### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)

<!-- This line will get updated when the PR build summary job finishes.
-->
PR Build Summary: https://build.rerun.io/pr/2430

<!-- pr-link-docs:start -->
Docs preview: https://rerun.io/preview/df06d4c/docs
Examples preview: https://rerun.io/preview/df06d4c/examples
<!-- pr-link-docs:end -->

examples/python/colmap/main.py
@@ -158,9 +158,10 @@ def read_and_log_sparse_reconstruction(dataset_path: Path, filter_output: bool, 
         )
 
         if resize:
-            img = cv2.imread(str(image_file))
-            img = cv2.resize(img, resize)
-            rr.log_image("camera/image", img, jpeg_quality=75)
+            bgr = cv2.imread(str(image_file))
+            bgr = cv2.resize(bgr, resize)
+            rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+            rr.log_image("camera/image", rgb, jpeg_quality=75)
         else:
             rr.log_image_file("camera/image", img_path=dataset_path / "images" / image.name)
 