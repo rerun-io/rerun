3816baa1cc31a62593a1aa4aaf639e1d098e4a24
Andreas Reich <r_andreas2@web.de>
2023-06-15 15:25:31 +0200

    slight python example improvement

--- docs/code-examples/annotation-context/example.py
+++ docs/code-examples/annotation-context/example.py
@@ -1,7 +1,6 @@
 # Annotation context with two classes, using two labled classes, of which ones defines a color.
-# Applies to all entities below "masks".
 rr.log_annotation_context(
-    "masks",
+    "masks",  # Applies to all entities below "masks".
     [
         rr.AnnotationInfo(id=0, label="Background"),
         rr.AnnotationInfo(id=1, label="Person", color=(0, 0, 0)),
@@ -9,12 +8,11 @@ rr.log_annotation_context(
 )
 
 # Annotation context with simple keypoints & keypoint connections.
-# Applies to all entities below "detections".
 rr.log_annotation_context(
-    "detections",
+    "detections",  # Applies to all entities below "detections".
     rr.ClassDescription(
         info=rr.AnnotationInfo(label="Snake"),
-        keypoint_annotations=[rr.AnnotationInfo(id=i, color=(255 / 10 * i, 0, 0)) for i in range(10)],
+        keypoint_annotations=[rr.AnnotationInfo(id=i, color=(0, 255 / 9 * i, 0)) for i in range(10)],
         keypoint_connections=[(i, i + 1) for i in range(9)],
     ),
 )
