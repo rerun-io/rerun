c75ec580487434a6dd4a91c13eb090c25c458edc
Andreas Reich <andreas@rerun.io>
2023-06-16 11:11:02 +0200

    Document annotation context in manual (#2453)
    <!--
    Open the PR up as a draft until you feel it is ready for a proper
    review.
    
    Do not make PR:s from your own `main` branch, as that makes it difficult
    for reviewers to add their own fixes.
    
    Add any improvements to the branch as new commits to make it easier for
    reviewers to follow the progress. All commits will be squashed to a
    single commit once the PR is merged into `main`.
    
    Make sure you mention any issues that this PR closes in the description,
    as well as any other related issues.
    
    To get an auto-generated PR description you can put "copilot:summary" or
    "copilot:walkthrough" anywhere.
    -->
    
    ### What
    
    Fixes  #1337 (😎)
    *  #1337
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    
    <!-- This line will get updated when the PR build summary job finishes.
    -->
    PR Build Summary: https://build.rerun.io/pr/2453
    
    <!-- pr-link-docs:start -->
    Docs preview: https://rerun.io/preview/9711742/docs
    Examples preview: https://rerun.io/preview/9711742/examples
    <!-- pr-link-docs:end -->

---
+++ docs/code-examples/annotation-context/example.py
@@ -0,0 +1,18 @@
+# Annotation context with two classes, using two labeled classes, of which ones defines a color.
+rr.log_annotation_context(
+    "masks",  # Applies to all entities below "masks".
+    [
+        rr.AnnotationInfo(id=0, label="Background"),
+        rr.AnnotationInfo(id=1, label="Person", color=(255, 0, 0)),
+    ],
+)
+
+# Annotation context with simple keypoints & keypoint connections.
+rr.log_annotation_context(
+    "detections",  # Applies to all entities below "detections".
+    rr.ClassDescription(
+        info=rr.AnnotationInfo(label="Snake"),
+        keypoint_annotations=[rr.AnnotationInfo(id=i, color=(0, 255 / 9 * i, 0)) for i in range(10)],
+        keypoint_connections=[(i, i + 1) for i in range(9)],
+    ),
+)
