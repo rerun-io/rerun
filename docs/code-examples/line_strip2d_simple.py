4b18c4b939f7119b66220422127d831454e5094b
Jeremy Leibs <jeremy@rerun.io>
2023-06-19 06:43:43 -0400

    Flesh out archetype docs and simple code examples for more types (#2467)
    ### What
    
    Continues following the pattern established from:
    - https://github.com/rerun-io/rerun/pull/2438
    
    Contributes to:
     - https://github.com/rerun-io/rerun/issues/2382
    
    Preview:
    -
    https://landing-2yxomjvlz-rerun.vercel.app/preview/d8d222a/docs/reference/data_types
    
    ### Checklist
    * [ ] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [ ] I've included a screenshot or gif (if applicable)
    
    <!-- This line will get updated when the PR build summary job finishes.
    -->
    PR Build Summary: https://build.rerun.io/pr/2467
    
    <!-- pr-link-docs:start -->
    Docs preview: https://rerun.io/preview/5eba986/docs
    Examples preview: https://rerun.io/preview/5eba986/examples
    <!-- pr-link-docs:end -->
    
    ---------
    
    Co-authored-by: Nikolaus West <niko@rerun.io>
    Co-authored-by: Nikolaus West <niko@grapic.co>

---
+++ docs/code-examples/line_strip2d_simple.py
@@ -0,0 +1,12 @@
+"""Log a simple line strip."""
+import rerun as rr
+
+rr.init("linestrip2d", spawn=True)
+
+rr.log_line_strip(
+    "simple",
+    [[0, 0], [2, 1], [4, -1], [6, 0]],
+)
+
+# Log an extra rect to set the view bounds
+rr.log_rect("bounds", [3, 0, 8, 6], rect_format=rr.RectFormat.XCYCWH)
