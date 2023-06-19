444e4017e1dfba582123be17fcf6982baca7d9fc
Nikolaus West <niko@rerun.io>
2023-06-19 12:42:32 +0200

    Simple code example for transform3d

---
+++ docs/code-examples/transform3d_simple.py
@@ -0,0 +1,20 @@
+"""Log a translation between two arrows."""
+import rerun as rr
+
+rr.init("transform", spawn=True)
+
+origin = [0, 0, 0]
+base_vector = [0, 1, 0]
+
+rr.log_arrow("base", origin=origin, vector=base_vector)
+
+rr.log_transform3d("base/translated", rr.Translation3D([1, 0, 0]))
+
+rr.log_arrow("base/translated", origin=origin, vector=base_vector)
+
+rr.log_transform3d(
+    "base/rotated_scaled",
+    rr.TranslationRotationScale3D(rotation=rr.RotationAxisAngle(axis=[0, 0, 1], radians=3.14 / 4), scale=rr.Scale3D(2)),
+)
+
+rr.log_arrow("base/rotated_scaled", origin=origin, vector=base_vector)
--- docs/content/reference/data_types/transform3d.md
+++ docs/content/reference/data_types/transform3d.md
@@ -2,6 +2,8 @@
 title: Transform3D
 order: 9
 ---
+`Transform3D` represents an affine 3D transformation between an entity and it's parent space in the entity hierarchy. Read more about spaces and transforms [here](../../concepts/spaces-and-transforms.md)
+
 ## Components and APIs
 Primary component: `transform3d`
 
@@ -9,3 +11,6 @@ Python APIs: [log_transform3d](https://ref.rerun.io/docs/python/latest/common/tr
 
 
 Rust API: [Transform3D](https://docs.rs/rerun/latest/rerun/components/struct.Transform3D.html)
+
+## Simple Example
+code-example: transform3d_simple
