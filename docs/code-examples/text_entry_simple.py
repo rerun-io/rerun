049bce27316dd24eb5adc580ece8202fe8b7d8df
Nikolaus West <niko@grapic.co>
2023-06-19 16:48:15 +0200

    Fill out archetype docs: Transform3d, Pinhole, Scalar, TextEntry (#2473)
    Simple first pass on adding python examples for archetypes:
    
    - [x] Transform3d
    - [x] Pinhole
    - [x] Scalar
    - [x] TextEntry
    
    ---------
    
    Co-authored-by: Jeremy Leibs <jeremy@rerun.io>

---
+++ docs/code-examples/text_entry_simple.py
@@ -0,0 +1,14 @@
+"""Log some text entries."""
+import logging
+
+import rerun as rr
+
+rr.init("text_entry", spawn=True)
+
+# Log a direct entry directly
+rr.log_text_entry("logs", "this entry has loglevel TRACE", level=rr.LogLevel.TRACE)
+
+# Or log via a logging handler
+logging.getLogger().addHandler(rr.LoggingHandler("logs/handler"))
+logging.getLogger().setLevel(-1)
+logging.info("This log got added through a `LoggingHandler`")
