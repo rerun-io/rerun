948b472d16b6409c47cc9306024e317bfa7a8fcf
Nikolaus West <niko@rerun.io>
2023-06-20 11:26:16 +0200

    Split depth image docs example into simple and 3d

--- docs/code-examples/depth_image_simple.py
+++ docs/code-examples/depth_image_simple.py
@@ -8,23 +8,7 @@ image = 65535 * np.ones((200, 300), dtype=np.uint16)
 image[50:150, 50:150] = 20000
 image[130:180, 100:280] = 45000
 
-
 rr.init("depth_image", spawn=True)
 
-# We need a camera to register the depth image to
-focal_length = 200
-rr.log_pinhole(
-    "world/camera",
-    child_from_parent=np.array(
-        (
-            (focal_length, 0, image.shape[1] / 2),
-            (0, focal_length, image.shape[0] / 2),
-            (0, 0, 1),
-        ),
-    ),
-    width=image.shape[1],
-    height=image.shape[0],
-)
-
 # Log the tensor, assigning names to each dimension
-rr.log_depth_image("world/camera/depth", image, meter=10000.0)
+rr.log_depth_image("depth", image, meter=10000.0)
