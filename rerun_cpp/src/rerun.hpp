a4e8576b04e30789f51ea0b43de11eae84c54489
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-07-06 17:07:41 +0200

    Add some docs

--- rerun_cpp/src/rerun.hpp
+++ rerun_cpp/src/rerun.hpp
@@ -16,6 +16,10 @@ namespace rr {
 namespace rr {
     arrow::Result<std::shared_ptr<arrow::Table>> points3(size_t num_points, const float* xyz);
 
+    /// Encode the given arrow table in the Arrow IPC encapsulated message format.
+    ///
+    /// * <https://arrow.apache.org/docs/format/Columnar.html#format-ipc>
+    /// * <https://wesm.github.io/arrow-site-test/format/IPC.html#encapsulated-message-format>
     arrow::Result<std::shared_ptr<arrow::Buffer>> ipc_from_table(const arrow::Table& table);
 } // namespace rr
 
