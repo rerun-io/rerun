881964534eda8cb8fb6914e6e10feb07309040de
Antoine Beyeler <antoine@rerun.io>
2023-07-10 11:22:21 +0200

    Made TranslationAndMat3x3 fields nullable + misc cleaning

--- rerun_py/tests/unit/test_transform3d.py
+++ rerun_py/tests/unit/test_transform3d.py
@@ -51,7 +51,7 @@ def assert_correct_rotation3d(rot: rr.dt.Rotation3D | None) -> None:
         assert np.all(rot.inner.xyzw == np.array([1.0, 2.0, 3.0, 4.0]))
         assert rot.inner.xyzw.dtype == np.float32
     elif isinstance(rot.inner, rr.dt.RotationAxisAngle):
-        # TODO(ab): np.array-typed fields should be provided with a `eq` method that uses `np.all`
+        # TODO(#2650): np.array-typed fields should be provided with a `eq` method that uses `np.all`
         assert np.all(rot.inner.axis.xyz == np.array([1.0, 2.0, 3.0]))
         assert rot.inner.axis.xyz.dtype == np.float32
         assert rot.inner.angle == rr.dt.Angle(4.0)
@@ -97,45 +97,54 @@ def test_rotation3d(input: rr.dt.Rotation3DLike) -> None:
 
 @pytest.mark.parametrize("input", VEC_3D_INPUT)
 def test_translation_rotation_translation(input: rr.dt.Vec3DLike) -> None:
-    # TODO(ab): from_parent should be optional
-    trs = rr.dt.TranslationRotationScale3D(translation=input, from_parent=False)
+    trs = rr.dt.TranslationRotationScale3D(translation=input)
     assert_correct_vec3d(trs.translation)
 
 
 @pytest.mark.parametrize("input", SCALE_3D_INPUT)
 def test_translation_rotation_scale(input: rr.dt.Scale3DLike) -> None:
-    # TODO(ab): from_parent should be optional
-    trs = rr.dt.TranslationRotationScale3D(scale=input, from_parent=False)
+    trs = rr.dt.TranslationRotationScale3D(scale=input)
     assert_correct_scale3d(trs.scale)
 
-    # TODO(ab): from_parent should be optional
-    trs = rr.dt.TranslationRotationScale3D(scale=rr.dt.Scale3D(input), from_parent=False)
+    trs = rr.dt.TranslationRotationScale3D(scale=rr.dt.Scale3D(input))
     assert_correct_scale3d(trs.scale)
 
 
 @pytest.mark.parametrize("input", ROTATION_3D_INPUT)
 def test_translation_rotation_rotation(input: rr.dt.Rotation3DLike) -> None:
-    # TODO(ab): from_parent should be optional
-    trs = rr.dt.TranslationRotationScale3D(rotation=input, from_parent=False)
+    trs = rr.dt.TranslationRotationScale3D(rotation=input)
     assert_correct_rotation3d(trs.rotation)
 
-    # TODO(ab): from_parent should be optional
-    trs = rr.dt.TranslationRotationScale3D(rotation=rr.dt.Rotation3D(input), from_parent=False)
+    trs = rr.dt.TranslationRotationScale3D(rotation=rr.dt.Rotation3D(input))
     assert_correct_rotation3d(trs.rotation)
 
 
-@pytest.mark.parametrize("trans", VEC_3D_INPUT)
-@pytest.mark.parametrize("mat", MAT_3X3_INPUT)
-def test_translation_and_mat3x3(trans: rr.dt.Vec3DLike, mat: rr.dt.Mat3x3Like) -> None:
-    # TODO(ab): from_parent should be optional
-    tm = rr.dt.TranslationAndMat3x3(translation=trans, matrix=mat, from_parent=False)
-    assert_correct_vec3d(tm.translation)
-    assert_correct_mat3x3(tm.matrix)
+def test_translation_rotation_from_parent() -> None:
+    # TODO(#2641): default should be False, not None
+    assert rr.dt.TranslationRotationScale3D().from_parent is None
+    assert rr.dt.TranslationRotationScale3D(from_parent=True).from_parent
+    assert not rr.dt.TranslationRotationScale3D(from_parent=False).from_parent
 
 
-# TODO(ab): clean me
-# def test_playground():
-#     print(rr.dt.TranslationRotationScale3D(translation=[1, 3, 4]))
+@pytest.mark.parametrize("trans", VEC_3D_INPUT + [None])
+@pytest.mark.parametrize("mat", MAT_3X3_INPUT + [None])
+def test_translation_and_mat3x3(trans: rr.dt.Vec3DLike | None, mat: rr.dt.Mat3x3Like | None) -> None:
+    tm = rr.dt.TranslationAndMat3x3(translation=trans, matrix=mat)
+    if trans is None:
+        assert tm.translation is None
+    else:
+        assert_correct_vec3d(tm.translation)
+    if mat is None:
+        assert tm.matrix is None
+    else:
+        assert_correct_mat3x3(tm.matrix)
+
+
+def test_translation_and_mat3x3_from_parent() -> None:
+    # TODO(#2641): default should be False, not None
+    assert rr.dt.TranslationAndMat3x3().from_parent is None
+    assert rr.dt.TranslationAndMat3x3(from_parent=True).from_parent
+    assert not rr.dt.TranslationAndMat3x3(from_parent=False).from_parent
 
 
 @pytest.mark.skip(reason="from_similar() for transforms not yet implemented")
