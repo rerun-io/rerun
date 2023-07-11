rerun-io/rerun

commit c792540672b17aa8d361b4bc77415a0888846892

Double-click to select entity (#2504)
Closes https://github.com/rerun-io/rerun/issues/2214

Hovering a point cloud now hovers individual points. Clicking once
selects a point (a single instance). Double-clicking selects the entire
point-cloud (the entire entity).

### What
With this PR, we always set the full instance key in the GPU picking
buffer. When reading we decide whether to look at both entity and
instance (hover/click single instances) or to only look at the entity
path (select whole point-cloud on double-click).

Previously what was put in the GPU picking buffer depended on what was
already selected. The new code is a lot simpler.

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)

<!-- This line will get updated when the PR build summary job finishes.
-->
PR Build Summary: https://build.rerun.io/pr/2504

<!-- pr-link-docs:start -->
Docs preview: https://rerun.io/preview/c7f9332/docs
Examples preview: https://rerun.io/preview/c7f9332/examples
<!-- pr-link-docs:end -->

crates/re_data_ui/src/item_ui.rs
@@ -254,9 +254,9 @@ pub fn select_hovered_on_click(
 
     if response.clicked() {
         if response.ctx.input(|i| i.modifiers.command) {
-            selection_state.toggle_selection(selection_state.hovered().to_vec());
+            selection_state.toggle_selection(items.to_vec());
         } else {
-            selection_state.set_selection(selection_state.hovered().clone().into_iter());
+            selection_state.set_selection(items.iter().cloned());
         }
     }
 }

crates/re_space_view_spatial/src/ui.rs
@@ -29,6 +29,12 @@ use crate::{
     ui_3d::{view_3d, SpaceSpecs},
 };
 
+/// Default auto point radius in UI points.
+const AUTO_POINT_RADIUS: f32 = 1.5;
+
+/// Default auto line radius in UI points.
+const AUTO_LINE_RADIUS: f32 = 1.5;
+
 /// Describes how the scene is navigated, determining if it is a 2D or 3D experience.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
 pub enum SpatialNavigationMode {
@@ -121,10 +127,10 @@ impl SpatialSpaceViewState {
     pub fn auto_size_config(&self) -> re_renderer::AutoSizeConfig {
         let mut config = self.auto_size_config;
         if config.point_radius.is_auto() {
-            config.point_radius = re_renderer::Size::new_points(1.5); // default point radius
+            config.point_radius = re_renderer::Size::new_points(AUTO_POINT_RADIUS);
         }
         if config.line_radius.is_auto() {
-            config.line_radius = re_renderer::Size::new_points(1.5); // default line radius
+            config.line_radius = re_renderer::Size::new_points(AUTO_LINE_RADIUS);
         }
         config
     }
@@ -156,7 +162,7 @@ impl SpatialSpaceViewState {
         let size = self.scene_bbox_accum.size();
         let mut sorted_components = size.to_array();
         sorted_components.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        // Median is more robust against outlier in one direction (as such pretty pour still though)
+        // Median is more robust against outlier in one direction (as such pretty poor still though)
         let median_extent = sorted_components[1];
         // sqrt would make more sense but using a smaller root works better in practice.
         let heuristic1 =
@@ -683,8 +689,8 @@ pub fn picking(
 
     let picking_rect_size =
         super::scene::PickingContext::UI_INTERACTION_RADIUS * parent_ui.ctx().pixels_per_point();
-    // Make the picking rect bigger than necessary so we can use it to counter act delays.
-    // (by the time the picking rectangle read back, the cursor may have moved on).
+    // Make the picking rect bigger than necessary so we can use it to counter-act delays.
+    // (by the time the picking rectangle is read back, the cursor may have moved on).
     let picking_rect_size = (picking_rect_size * 2.0)
         .ceil()
         .at_least(8.0)
@@ -718,6 +724,11 @@ pub fn picking(
     for hit in &picking_result.hits {
         let Some(mut instance_path) = hit.instance_path_hash.resolve(&ctx.store_db.entity_db)
             else { continue; };
+
+        if response.double_clicked() {
+            // Select entire entity on double-click:
+            instance_path.instance_key = re_log_types::InstanceKey::SPLAT;
+        }
 
         if scene
             .context

crates/re_log_types/src/instance_key.rs
@@ -19,17 +19,7 @@ use crate::Component;
 /// assert_eq!(InstanceKey::data_type(), DataType::UInt64);
 /// ```
 #[derive(
-    Copy,
-    Clone,
-    Debug,
-    Hash,
-    PartialEq,
-    Eq,
-    PartialOrd,
-    Ord,
-    ArrowField,
-    ArrowSerialize,
-    ArrowDeserialize,
+    Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, ArrowField, ArrowSerialize, ArrowDeserialize,
 )]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[arrow_field(transparent)]
@@ -76,6 +66,17 @@ impl InstanceKey {
     /// Retrieves 2d image coordinates (x, y) encoded in an instance key
     pub fn to_2d_image_coordinate(self, image_width: u64) -> [u32; 2] {
         [(self.0 % image_width) as u32, (self.0 / image_width) as u32]
+    }
+}
+
+impl std::fmt::Debug for InstanceKey {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.is_splat() {
+            "splat".fmt(f)
+        } else {
+            self.0.fmt(f)
+        }
     }
 }
 

crates/re_renderer/src/view_builder.rs
@@ -17,6 +17,13 @@ use crate::{
     DebugLabel, RectInt, Rgba, Size,
 };
 
+// These are only used if the point sizes are not already set.
+// Rerun always sets explicit sizes, so these are only used for other
+// users of re_renderer (mostly examples).
+// Unit: UI points.
+const AUTO_POINT_RADIUS: f32 = 2.5;
+const AUTO_LINE_RADIUS: f32 = 1.5;
+
 #[derive(thiserror::Error, Debug)]
 pub enum ViewBuilderError {
     #[error("Screenshot was already scheduled.")]
@@ -418,12 +425,12 @@ impl ViewBuilder {
         let projection_from_world = projection_from_view * view_from_world;
 
         let auto_size_points = if config.auto_size_config.point_radius.is_auto() {
-            Size::new_points(2.5)
+            Size::new_points(AUTO_POINT_RADIUS)
         } else {
             config.auto_size_config.point_radius
         };
         let auto_size_lines = if config.auto_size_config.line_radius.is_auto() {
-            Size::new_points(1.5)
+            Size::new_points(AUTO_LINE_RADIUS)
         } else {
             config.auto_size_config.line_radius
         };

crates/re_space_view_spatial/src/scene/parts/arrows3d.rs
@@ -6,7 +6,7 @@ use re_viewer_context::{
     ArchetypeDefinition, DefaultColor, ScenePart, SceneQuery, SpaceViewHighlights, ViewerContext,
 };
 
-use super::{instance_key_to_picking_id, SpatialScenePartData, SpatialSpaceViewState};
+use super::{picking_id_from_instance_key, SpatialScenePartData, SpatialSpaceViewState};
 use crate::{
     scene::{
         contexts::{SpatialSceneContext, SpatialSceneEntityContext},
@@ -74,11 +74,7 @@ impl Arrows3DPart {
                             | LineStripFlags::FLAG_CAP_START_ROUND
                             | LineStripFlags::FLAG_CAP_START_EXTEND_OUTWARDS,
                     )
-                    .picking_instance_id(instance_key_to_picking_id(
-                        instance_key,
-                        ent_view.num_instances(),
-                        ent_context.highlight.any_selection_highlight,
-                    ));
+                    .picking_instance_id(picking_id_from_instance_key(instance_key));
 
                 if let Some(outline_mask_ids) = ent_context.highlight.instances.get(&instance_key) {
                     segment.outline_mask_ids(*outline_mask_ids);

crates/re_space_view_spatial/src/scene/parts/boxes2d.rs
@@ -6,10 +6,6 @@ use re_viewer_context::{
     ArchetypeDefinition, DefaultColor, ScenePart, SceneQuery, SpaceViewHighlights, ViewerContext,
 };
 
-use super::{
-    instance_key_to_picking_id, instance_path_hash_for_picking, SpatialScenePartData,
-    SpatialSpaceViewState,
-};
 use crate::{
     scene::{
         contexts::{SpatialSceneContext, SpatialSceneEntityContext},
@@ -18,6 +14,8 @@ use crate::{
     },
     SpatialSpaceView,
 };
+
+use super::{picking_id_from_instance_key, SpatialScenePartData, SpatialSpaceViewState};
 
 #[derive(Default)]
 pub struct Boxes2DPart(SpatialScenePartData);
@@ -47,12 +45,8 @@ impl Boxes2DPart {
              radius: Option<Radius>,
              label: Option<Label>,
              class_id: Option<ClassId>| {
-                let instance_hash = instance_path_hash_for_picking(
-                    ent_path,
-                    instance_key,
-                    ent_view.num_instances(),
-                    ent_context.highlight.any_selection_highlight,
-                );
+                let instance_hash =
+                    re_data_store::InstancePathHash::instance(ent_path, instance_key);
 
                 let annotation_info = ent_context
                     .annotations
@@ -81,11 +75,7 @@ impl Boxes2DPart {
                     )
                     .color(color)
                     .radius(radius)
-                    .picking_instance_id(instance_key_to_picking_id(
-                        instance_key,
-                        ent_view.num_instances(),
-                        ent_context.highlight.any_selection_highlight,
-                    ));
+                    .picking_instance_id(picking_id_from_instance_key(instance_key));
 
                 if let Some(outline_mask_ids) = ent_context
                     .highlight

crates/re_space_view_spatial/src/scene/parts/boxes3d.rs
@@ -17,10 +17,7 @@ use crate::{
     SpatialSpaceView,
 };
 
-use super::{
-    instance_key_to_picking_id, instance_path_hash_for_picking, SpatialScenePartData,
-    SpatialSpaceViewState,
-};
+use super::{picking_id_from_instance_key, SpatialScenePartData, SpatialSpaceViewState};
 
 #[derive(Default)]
 pub struct Boxes3DPart(SpatialScenePartData);
@@ -67,11 +64,7 @@ impl Boxes3DPart {
                 .add_box_outline(transform)
                 .radius(radius)
                 .color(color)
-                .picking_instance_id(instance_key_to_picking_id(
-                    instance_key,
-                    ent_view.num_instances(),
-                    ent_context.highlight.any_selection_highlight,
-                ));
+                .picking_instance_id(picking_id_from_instance_key(instance_key));
 
             if let Some(outline_mask_ids) = ent_context.highlight.instances.get(&instance_key) {
                 box_lines.outline_mask_ids(*outline_mask_ids);
@@ -84,11 +77,9 @@ impl Boxes3DPart {
                         ent_context.world_from_obj.transform_point3(tran),
                     ),
                     color,
-                    labeled_instance: instance_path_hash_for_picking(
+                    labeled_instance: re_data_store::InstancePathHash::instance(
                         ent_path,
                         instance_key,
-                        ent_view.num_instances(),
-                        ent_context.highlight.any_selection_highlight,
                     ),
                 });
             }

crates/re_space_view_spatial/src/scene/parts/cameras.rs
@@ -8,11 +8,12 @@ use re_viewer_context::{ArchetypeDefinition, ScenePart, TimeControl};
 use re_viewer_context::{SceneQuery, ViewerContext};
 use re_viewer_context::{SpaceViewHighlights, SpaceViewOutlineMasks};
 
-use super::{instance_path_hash_for_picking, SpatialScenePartData, SpatialSpaceViewState};
 use crate::{
     instance_hash_conversions::picking_layer_id_from_instance_path_hash,
     scene::contexts::SpatialSceneContext, space_camera_3d::SpaceCamera3D, SpatialSpaceView,
 };
+
+use super::{SpatialScenePartData, SpatialSpaceViewState};
 
 const CAMERA_COLOR: re_renderer::Color32 = re_renderer::Color32::from_rgb(150, 150, 150);
 
@@ -152,13 +153,8 @@ impl CamerasPart {
         ];
 
         let radius = re_renderer::Size::new_points(1.0);
-        let num_instances = 1; // There is only ever one instance of `Transform` per entity.
-        let instance_path_for_picking = instance_path_hash_for_picking(
-            ent_path,
-            instance_key,
-            num_instances,
-            entity_highlight.any_selection_highlight,
-        );
+        let instance_path_for_picking =
+            re_data_store::InstancePathHash::instance(ent_path, instance_key);
         let instance_layer_id = picking_layer_id_from_instance_path_hash(instance_path_for_picking);
 
         let mut line_builder = scene_context.shared_render_builders.lines();

commit da81c90a74b3921b010e004960e495f77c3c1ba4

Fix clicking object with single instance (of every component) selecting instance instead of entity (#2573)
### What

Fixes a regression introduced by #2504

Single click on box in objectron:
Before:

![image](https://github.com/rerun-io/rerun/assets/1220815/2ec91fa9-1120-42b0-bf5f-ba5b4a1f290b)

After:

![image](https://github.com/rerun-io/rerun/assets/1220815/93746403-d22d-4626-97fc-642a550aa36c)


tested with `python ./scripts/run_all.py`, in particular checked on
selection history and point clouds behaving as they should via #2504

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/2573) (if
applicable)

- [PR Build Summary](https://build.rerun.io/pr/2573)
- [Docs
preview](https://rerun.io/preview/pr%3Aandreas%2Ffix-single-instance-select/docs)
- [Examples
preview](https://rerun.io/preview/pr%3Aandreas%2Ffix-single-instance-select/examples)

crates/re_data_ui/src/item_ui.rs
@@ -5,8 +5,7 @@
 use re_data_store::InstancePath;
 use re_log_types::{ComponentPath, EntityPath, TimeInt, Timeline};
 use re_viewer_context::{
-    DataBlueprintGroupHandle, HoverHighlight, Item, SelectionState, SpaceViewId, UiVerbosity,
-    ViewerContext,
+    DataBlueprintGroupHandle, HoverHighlight, Item, SpaceViewId, UiVerbosity, ViewerContext,
 };
 
 use super::DataUi;
@@ -109,7 +108,7 @@ pub fn instance_path_button_to(
             instance_path.data_ui(ctx, ui, UiVerbosity::Reduced, &ctx.current_query());
         });
 
-    cursor_interact_with_selectable(ctx.selection_state_mut(), response, item)
+    cursor_interact_with_selectable(ctx, response, item)
 }
 
 /// Show a component path and make it selectable.
@@ -135,7 +134,7 @@ pub fn component_path_button_to(
 ) -> egui::Response {
     let item = Item::ComponentPath(component_path.clone());
     let response = ui.selectable_label(ctx.selection().contains(&item), text);
-    cursor_interact_with_selectable(ctx.selection_state_mut(), response, item)
+    cursor_interact_with_selectable(ctx, response, item)
 }
 
 pub fn data_blueprint_group_button_to(
@@ -155,7 +154,7 @@ pub fn data_blueprint_group_button_to(
             ctx.selection().contains(&item),
         )
         .on_hover_text("Group");
-    cursor_interact_with_selectable(ctx.selection_state_mut(), response, item)
+    cursor_interact_with_selectable(ctx, response, item)
 }
 
 pub fn data_blueprint_button_to(
@@ -176,7 +175,7 @@ pub fn data_blueprint_button_to(
             ui.label(format!("Path: {entity_path}"));
             entity_path.data_ui(ctx, ui, UiVerbosity::Reduced, &ctx.current_query());
         });
-    cursor_interact_with_selectable(ctx.selection_state_mut(), response, item)
+    cursor_interact_with_selectable(ctx, response, item)
 }
 
 pub fn time_button(
@@ -225,14 +224,14 @@ pub fn timeline_button_to(
 
 // TODO(andreas): Move elsewhere, this is not directly part of the item_ui.
 pub fn cursor_interact_with_selectable(
-    selection_state: &mut SelectionState,
+    ctx: &mut ViewerContext<'_>,
     response: egui::Response,
     item: Item,
 ) -> egui::Response {
     let is_item_hovered =
-        selection_state.highlight_for_ui_element(&item) == HoverHighlight::Hovered;
+        ctx.selection_state().highlight_for_ui_element(&item) == HoverHighlight::Hovered;
 
-    select_hovered_on_click(&response, selection_state, &[item]);
+    select_hovered_on_click(ctx, &response, &[item]);
     // TODO(andreas): How to deal with shift click for selecting ranges?
 
     if is_item_hovered {
@@ -244,19 +243,22 @@ pub fn cursor_interact_with_selectable(
 
 // TODO(andreas): Move elsewhere, this is not directly part of the item_ui.
 pub fn select_hovered_on_click(
+    ctx: &mut ViewerContext<'_>,
     response: &egui::Response,
-    selection_state: &mut SelectionState,
     items: &[Item],
 ) {
+    re_tracing::profile_function!();
+
     if response.hovered() {
-        selection_state.set_hovered(items.iter().cloned());
+        ctx.selection_state_mut().set_hovered(items.iter().cloned());
     }
 
     if response.clicked() {
         if response.ctx.input(|i| i.modifiers.command) {
-            selection_state.toggle_selection(items.to_vec());
+            ctx.selection_state_mut().toggle_selection(items.to_vec());
         } else {
-            selection_state.set_selection(items.iter().cloned());
+            ctx.selection_state_mut()
+                .set_selection(items.iter().cloned());
         }
     }
 }

crates/re_space_view_spatial/src/ui.rs
@@ -11,8 +11,9 @@ use re_format::format_f32;
 use re_renderer::OutlineConfig;
 use re_space_view::ScreenshotMode;
 use re_viewer_context::{
-    HoverHighlight, HoveredSpace, Item, SelectionHighlight, SpaceViewHighlights, SpaceViewId,
-    SpaceViewState, TensorDecodeCache, TensorStatsCache, UiVerbosity, ViewerContext,
+    resolve_mono_instance_path, HoverHighlight, HoveredSpace, Item, SelectionHighlight,
+    SpaceViewHighlights, SpaceViewId, SpaceViewState, TensorDecodeCache, TensorStatsCache,
+    UiVerbosity, ViewerContext,
 };
 
 use super::{
@@ -731,8 +732,8 @@ pub fn picking(
         let picked_image_with_coords = if hit.hit_type == PickingHitType::TexturedRect
             || is_depth_cloud
         {
-            let store = &ctx.store_db.entity_db.data_store;
-            store
+            ctx.store_db
+                .store()
                 .query_latest_component::<Tensor>(&instance_path.entity_path, &ctx.current_query())
                 .and_then(|tensor| {
                     // If we're here because of back-projection, but this wasn't actually a depth image, drop out.
@@ -758,6 +759,12 @@ pub fn picking(
         if picked_image_with_coords.is_some() {
             // We don't support selecting pixels yet.
             instance_path.instance_key = re_log_types::InstanceKey::SPLAT;
+        } else {
+            instance_path = resolve_mono_instance_path(
+                &ctx.current_query(),
+                ctx.store_db.store(),
+                &instance_path,
+            );
         }
 
         hovered_items.push(Item::InstancePath(
@@ -844,8 +851,7 @@ pub fn picking(
         };
     }
 
-    item_ui::select_hovered_on_click(&response, ctx.selection_state_mut(), &hovered_items);
-    ctx.set_hovered(hovered_items.into_iter());
+    item_ui::select_hovered_on_click(ctx, &response, &hovered_items);
 
     let hovered_space = match state.nav_mode.get() {
         SpatialNavigationMode::TwoD => HoveredSpace::TwoD {

crates/re_time_panel/src/data_density_graph.rs
@@ -370,7 +370,7 @@ pub fn data_density_graph_ui(
     time_histogram: &TimeHistogram,
     row_rect: Rect,
     time_ranges_ui: &TimeRangesUi,
-    item: Item,
+    item: &Item,
 ) {
     re_tracing::profile_function!();
 
@@ -470,7 +470,7 @@ pub fn data_density_graph_ui(
         data_dentity_graph_painter,
         row_rect.y_range(),
         time_area_painter,
-        graph_color(ctx, &item, ui),
+        graph_color(ctx, item, ui),
         hovered_x_range,
     );
 
@@ -487,7 +487,7 @@ pub fn data_density_graph_ui(
             show_row_ids_tooltip(
                 ctx,
                 ui.ctx(),
-                &item,
+                item,
                 hovered_time_range,
                 num_hovered_messages,
             );

crates/re_time_panel/src/lib.rs
@@ -448,7 +448,7 @@ impl TimePanel {
                 num_messages_at_time,
                 row_rect,
                 &self.time_ranges_ui,
-                item,
+                &item,
             );
         }
     }
@@ -544,7 +544,7 @@ impl TimePanel {
                         messages_over_time,
                         row_rect,
                         &self.time_ranges_ui,
-                        item,
+                        &item,
                     );
                 }
             }

crates/re_viewer_context/src/item.rs
@@ -120,3 +120,55 @@ impl std::iter::IntoIterator for ItemCollection {
         self.items.into_iter()
     }
 }
+
+/// If the given item refers to the first element of an instance with a single element, resolve to a splatted entity path.
+pub fn resolve_mono_instance_path_item(
+    query: &re_arrow_store::LatestAtQuery,
+    store: &re_arrow_store::DataStore,
+    item: &Item,
+) -> Item {
+    // Resolve to entity path if there's only a single instance.
+    match item {
+        Item::InstancePath(space_view, instance) => Item::InstancePath(
+            *space_view,
+            resolve_mono_instance_path(query, store, instance),
+        ),
+        Item::ComponentPath(_) | Item::SpaceView(_) | Item::DataBlueprintGroup(_, _) => {
+            item.clone()
+        }
+    }
+}
+
+/// If the given path refers to the first element of an instance with a single element, resolve to a splatted entity path.
+pub fn resolve_mono_instance_path(
+    query: &re_arrow_store::LatestAtQuery,
+    store: &re_arrow_store::DataStore,
+    instance: &re_data_store::InstancePath,
+) -> re_data_store::InstancePath {
+    re_tracing::profile_function!();
+
+    if instance.instance_key.0 == 0 {
+        let Some(components) = store
+                .all_components(&query.timeline, &instance.entity_path) else {
+            // No components at all, return splatted entity.
+            return re_data_store::InstancePath::entity_splat(instance.entity_path.clone());
+        };
+        for component in components {
+            if let Some((_row_id, instances)) = re_query::get_component_with_instances(
+                store,
+                query,
+                &instance.entity_path,
+                component,
+            ) {
+                if instances.len() > 1 {
+                    return instance.clone();
+                }
+            }
+        }
+
+        // All instances had only a single element or less, resolve to splatted entity.
+        return re_data_store::InstancePath::entity_splat(instance.entity_path.clone());
+    }
+
+    instance.clone()
+}

crates/re_viewer_context/src/lib.rs
@@ -29,7 +29,7 @@ pub use command_sender::{
     command_channel, CommandReceiver, CommandSender, SystemCommand, SystemCommandSender,
 };
 pub use component_ui_registry::{ComponentUiRegistry, UiVerbosity};
-pub use item::{Item, ItemCollection};
+pub use item::{resolve_mono_instance_path, resolve_mono_instance_path_item, Item, ItemCollection};
 pub use selection_history::SelectionHistory;
 pub use selection_state::{
     HoverHighlight, HoveredSpace, InteractionHighlight, SelectionHighlight, SelectionState,

crates/re_viewer_context/src/viewer_context.rs
@@ -1,8 +1,8 @@
 use re_data_store::store_db::StoreDb;
 
 use crate::{
-    AppOptions, Caches, CommandSender, ComponentUiRegistry, Item, ItemCollection, SelectionState,
-    SpaceViewClassRegistry, StoreContext, TimeControl,
+    item::resolve_mono_instance_path_item, AppOptions, Caches, CommandSender, ComponentUiRegistry,
+    Item, ItemCollection, SelectionState, SpaceViewClassRegistry, StoreContext, TimeControl,
 };
 
 /// Common things needed by many parts of the viewer.
@@ -45,8 +45,14 @@ impl<'a> ViewerContext<'a> {
     /// Sets a single selection, updating history as needed.
     ///
     /// Returns the previous selection.
-    pub fn set_single_selection(&mut self, item: Item) -> ItemCollection {
-        self.rec_cfg.selection_state.set_single_selection(item)
+    pub fn set_single_selection(&mut self, item: &Item) -> ItemCollection {
+        self.rec_cfg
+            .selection_state
+            .set_single_selection(resolve_mono_instance_path_item(
+                &self.rec_cfg.time_ctrl.current_query(),
+                self.store_db.store(),
+                item,
+            ))
     }
 
     /// Returns the current selection.
@@ -60,8 +66,16 @@ impl<'a> ViewerContext<'a> {
     }
 
     /// Set the hovered objects. Will be in [`Self::hovered`] on the next frame.
-    pub fn set_hovered(&mut self, hovered: impl Iterator<Item = Item>) {
-        self.rec_cfg.selection_state.set_hovered(hovered);
+    pub fn set_hovered<'b>(&mut self, hovered: impl Iterator<Item = &'b Item>) {
+        self.rec_cfg
+            .selection_state
+            .set_hovered(hovered.map(|item| {
+                resolve_mono_instance_path_item(
+                    &self.rec_cfg.time_ctrl.current_query(),
+                    self.store_db.store(),
+                    item,
+                )
+            }));
     }
 
     pub fn selection_state(&self) -> &SelectionState {

crates/re_viewport/src/lib.rs
@@ -51,6 +51,6 @@ pub mod item_ui {
                 is_selected,
             )
             .on_hover_text("Space View");
-        item_ui::cursor_interact_with_selectable(ctx.selection_state_mut(), response, item)
+        item_ui::cursor_interact_with_selectable(ctx, response, item)
     }
 }

crates/re_viewport/src/viewport.rs
@@ -262,7 +262,7 @@ impl<'a, 'b> egui_tiles::Behavior<SpaceViewId> for TabViewer<'a, 'b> {
         if button_response.clicked() {
             if let Some(egui_tiles::Tile::Pane(space_view_id)) = tiles.get(tile_id) {
                 self.ctx
-                    .set_single_selection(Item::SpaceView(*space_view_id));
+                    .set_single_selection(&Item::SpaceView(*space_view_id));
             }
         }
     }
@@ -315,7 +315,7 @@ impl<'a, 'b> egui_tiles::Behavior<SpaceViewId> for TabViewer<'a, 'b> {
             {
                 *self.maximized = Some(space_view_id);
                 self.ctx
-                    .set_single_selection(Item::SpaceView(space_view_id));
+                    .set_single_selection(&Item::SpaceView(space_view_id));
             }
         }
 

crates/re_viewport/src/viewport_blueprint_ui.rs
@@ -366,7 +366,7 @@ impl ViewportBlueprint<'_> {
                 {
                     ui.close_menu();
                     let new_space_view_id = self.add_space_view(space_view);
-                    ctx.set_single_selection(Item::SpaceView(new_space_view_id));
+                    ctx.set_single_selection(&Item::SpaceView(new_space_view_id));
                 }
             }
         })

examples/rust/custom_space_view/src/color_coordinates_space_view.rs
@@ -247,8 +247,8 @@ fn color_space_ui(
                 instance.data_ui(ctx, ui, UiVerbosity::Reduced, &ctx.current_query());
             });
             item_ui::select_hovered_on_click(
+                ctx,
                 &interact,
-                ctx.selection_state_mut(),
                 &[Item::InstancePath(Some(space_view_id), instance)],
             );
         }