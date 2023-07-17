1dacafea800cc03caf692d0ff35222dd5a536d32
Clement Rey <cr.rey.clement@gmail.com>
2023-07-17 20:56:27 +0200

    implement rust2rust roundtrip unit test for transforms

---
+++ crates/re_types/tests/transform3d.rs
@@ -0,0 +1,122 @@
+use std::{collections::HashMap, f32::consts::PI};
+
+use re_types::{
+    archetypes::Transform3D,
+    components,
+    datatypes::{
+        self, Angle, Rotation3D, RotationAxisAngle, Scale3D, TranslationRotationScale3D, Vec3D,
+    },
+    Archetype as _,
+};
+
+#[test]
+fn roundtrip() {
+    let all_expected = [
+        Transform3D {
+            transform: components::Transform3D(datatypes::Transform3D::TranslationRotationScale(
+                TranslationRotationScale3D {
+                    translation: None,
+                    rotation: None,
+                    scale: None,
+                    from_parent: false,
+                },
+            )),
+        }, //
+        Transform3D {
+            transform: components::Transform3D(datatypes::Transform3D::TranslationRotationScale(
+                TranslationRotationScale3D {
+                    translation: Some(Vec3D([1.0, 2.0, 3.0])),
+                    rotation: None,
+                    scale: Some(Scale3D::Uniform(42.0)),
+                    from_parent: true,
+                },
+            )),
+        }, //
+        Transform3D {
+            transform: components::Transform3D(datatypes::Transform3D::TranslationRotationScale(
+                TranslationRotationScale3D {
+                    translation: Some(Vec3D([1.0, 2.0, 3.0])),
+                    rotation: Some(Rotation3D::AxisAngle(RotationAxisAngle {
+                        axis: Vec3D([0.2, 0.2, 0.8]),
+                        angle: Angle::Radians(PI),
+                    })),
+                    scale: None,
+                    from_parent: false,
+                },
+            )),
+        }, //
+        Transform3D {
+            transform: components::Transform3D(datatypes::Transform3D::TranslationRotationScale(
+                TranslationRotationScale3D {
+                    translation: Some(Vec3D([1.0, 2.0, 3.0])),
+                    rotation: Some(Rotation3D::AxisAngle(RotationAxisAngle {
+                        axis: Vec3D([0.2, 0.2, 0.8]),
+                        angle: Angle::Radians(PI),
+                    })),
+                    scale: Some(Scale3D::Uniform(42.0)),
+                    from_parent: true,
+                },
+            )),
+        }, //
+    ];
+
+    let all_arch = [
+        Transform3D::new(datatypes::Transform3D::TranslationRotationScale(
+            TranslationRotationScale3D::IDENTITY,
+        )), //
+        Transform3D::new(datatypes::Transform3D::TranslationRotationScale(
+            TranslationRotationScale3D {
+                translation: Some([1.0, 2.0, 3.0].into()),
+                scale: Some(Scale3D::Uniform(42.0)),
+                ..Default::default()
+            }
+            .from_parent(),
+        )), //
+        Transform3D::new(datatypes::Transform3D::TranslationRotationScale(
+            TranslationRotationScale3D::rigid(
+                [1.0, 2.0, 3.0],
+                RotationAxisAngle::new([0.2, 0.2, 0.8], Angle::Radians(PI)),
+            ),
+        )), //
+        Transform3D::new(datatypes::Transform3D::TranslationRotationScale(
+            TranslationRotationScale3D::affine(
+                [1.0, 2.0, 3.0],
+                RotationAxisAngle::new([0.2, 0.2, 0.8], Angle::Radians(PI)),
+                42.0,
+            )
+            .from_parent(),
+        )), //
+    ];
+
+    let expected_extensions: HashMap<_, _> = [
+        ("transform", vec!["rerun.components.Transform3D"]), //
+    ]
+    .into();
+
+    for (expected, arch) in all_expected.into_iter().zip(all_arch) {
+        similar_asserts::assert_eq!(expected, arch);
+
+        eprintln!("arch = {arch:#?}");
+        let serialized = arch.to_arrow();
+        for (field, array) in &serialized {
+            // NOTE: Keep those around please, very useful when debugging.
+            // eprintln!("field = {field:#?}");
+            // eprintln!("array = {array:#?}");
+            eprintln!("{} = {array:#?}", field.name);
+
+            // TODO(cmc): Re-enable extensions and these assertions once `arrow2-convert`
+            // has been fully replaced.
+            if false {
+                util::assert_extensions(
+                    &**array,
+                    expected_extensions[field.name.as_str()].as_slice(),
+                );
+            }
+        }
+
+        let deserialized = Transform3D::from_arrow(serialized);
+        similar_asserts::assert_eq!(expected, deserialized);
+    }
+}
+
+mod util;
