37841a1921aef3a0c326992435a3fcccdf5ae46a
Jan Procházka <1665677+jprochazk@users.noreply.github.com>
2023-07-18 10:58:53 +0200

    Weekly crates.io release (#2710)
    <!--
    Open the PR up as a draft until you feel it is ready for a proper
    review.
    
    Do not make PR:s from your own `main` branch, as that makes it difficult
    for reviewers to add their own fixes.
    
    Add any improvements to the branch as new commits to make it easier for
    reviewers to follow the progress. All commits will be squashed to a
    single commit once the PR is merged into `main`.
    
    Make sure you mention any issues that this PR closes in the description,
    as well as any other related issues.
    
    To get an auto-generated PR description you can put "copilot:summary" or
    "copilot:walkthrough" anywhere.
    -->
    
    ### What
    
    Part of https://github.com/rerun-io/rerun/issues/1343
    
    - Adds `get-version` command to `scripts/ci/crates.py`, which can be
    used to query the current version specified in the root `Cargo.toml`
    - Adds the `weekly_release` workflow, which runs at 2 PM every monday.
    It will:
      - Bump the crate versions, and create a pull request
      - Once the pull request is merged, it will release all crates
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/2710) (if
    applicable)
    
    - [PR Build Summary](https://build.rerun.io/pr/2710)
    - [Docs
    preview](https://rerun.io/preview/pr%3Ajan%2Frelease-crates-weekly/docs)
    - [Examples
    preview](https://rerun.io/preview/pr%3Ajan%2Frelease-crates-weekly/examples)

--- scripts/ci/crates.py
+++ scripts/ci/crates.py
@@ -20,7 +20,9 @@ from __future__ import annotations
 
 import argparse
 import os.path
+import shutil
 import subprocess
+import sys
 from enum import Enum
 from glob import glob
 from pathlib import Path
@@ -33,9 +35,11 @@ from colorama import Fore
 from colorama import init as colorama_init
 from semver import VersionInfo
 
+CARGO_PATH = shutil.which("cargo") or "cargo"
+
 
 def cargo(args: str, cwd: str | Path | None = None, env: dict[str, Any] = {}) -> Any:
-    subprocess.check_output(["cargo"] + args.split(), cwd=cwd, env=env)
+    subprocess.check_output([CARGO_PATH] + args.split(), cwd=cwd, env=env)
 
 
 class Crate:
@@ -330,6 +334,14 @@ def publish(dry_run: bool, token: str) -> None:
                 sleep((1 - elapsed_s))
 
 
+def get_version() -> None:
+    root: dict[str, Any] = tomlkit.parse(Path("Cargo.toml").read_text())
+    current_version = VersionInfo.parse(root["workspace"]["package"]["version"])
+
+    sys.stdout.write(str(current_version))
+    sys.stdout.flush()
+
+
 def main() -> None:
     colorama_init()
     parser = argparse.ArgumentParser(description="Generate a PR summary page")
@@ -341,8 +353,11 @@ def main() -> None:
     publish_parser.add_argument("--token", type=str, help="crates.io token")
     publish_parser.add_argument("--dry-run", action="store_true", help="Display the execution plan")
     publish_parser.add_argument("--allow-dirty", action="store_true", help="Allow uncommitted changes")
+    cmds_parser.add_parser("get-version", help="Get the current crate version")
     args = parser.parse_args()
 
+    if args.cmd == "get-version":
+        get_version()
     if args.cmd == "version":
         version(args.dry_run, args.bump)
     if args.cmd == "publish":
