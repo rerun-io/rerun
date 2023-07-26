rerun-io/rerun

commit 9ca1f3bcf2eef17fb6f475e8412b26af03cbda92

Disabled expansion effects in the UI (#2796)
### What

Fixes #2727
Fixes #2726
Fixes #2730

Questions for the reviewers:
- This affects the popup menu controls. Due to their styling the
expansion effect was invisible, but resulted in the greyed hover area to
be larger by 2px all around. The difference is visible in the videos
below. Is that OK? @martenbjork
- This PR removes the expansion effect from the blueprint tree's
triangles, and the play/pause/etc. buttons (desired, not shown in the
videos below). Did I miss some other, undesirable side-effect elsewhere?

Before:


https://github.com/rerun-io/rerun/assets/49431240/9990dc45-0294-4e04-9331-94eccba4a1d7

After:


https://github.com/rerun-io/rerun/assets/49431240/4fa29bf8-e9cf-4224-8c23-3427474576cc

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/2796) (if
applicable)

- [PR Build Summary](https://build.rerun.io/pr/2796)
- [Docs
preview](https://rerun.io/preview/pr%3Aantoine%2Fno-expansion-2727/docs)
- [Examples
preview](https://rerun.io/preview/pr%3Aantoine%2Fno-expansion-2727/examples)

crates/re_ui/src/design_tokens.rs
@@ -116,10 +116,10 @@ fn apply_design_tokens(ctx: &egui::Context) -> DesignTokens {
     }
 
     {
-        // Expand hovered and active button frames:
-        egui_style.visuals.widgets.hovered.expansion = 2.0;
-        egui_style.visuals.widgets.active.expansion = 2.0;
-        egui_style.visuals.widgets.open.expansion = 2.0;
+        // Turn off any expansion in the UI (see https://github.com/rerun-io/rerun/issues/2727)
+        egui_style.visuals.widgets.hovered.expansion = 0.0;
+        egui_style.visuals.widgets.active.expansion = 0.0;
+        egui_style.visuals.widgets.open.expansion = 0.0;
     }
 
     egui_style.visuals.selection.bg_fill =

commit c3487e21f28b778e29a6b2ee2642bb7e8ac2f5eb

Fix selection highlighting in blueprint tree view (#2824)
### What

Before:

<img width="170" alt="image"
src="https://github.com/rerun-io/rerun/assets/49431240/3e2c9525-88d9-450d-8f58-b84f13a3ba4d">

After:

<img width="264" alt="image"
src="https://github.com/rerun-io/rerun/assets/49431240/93437750-ff7b-42e5-8332-507cb1300e5c">

This PR also reverts #2796 (fixes #2828).


#### Changes
- Blue area is properly drawn
- Size of the hover/selection effect is fixed (after being broken in
#2796)

Fixes #2728

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/2824) (if
applicable)

- [PR Build Summary](https://build.rerun.io/pr/2824)
- [Docs
preview](https://rerun.io/preview/pr%3Aantoine%2Fblueprint_tree_selection_2728/docs)
- [Examples
preview](https://rerun.io/preview/pr%3Aantoine%2Fblueprint_tree_selection_2728/examples)

crates/re_ui/src/design_tokens.rs
@@ -116,10 +116,9 @@ fn apply_design_tokens(ctx: &egui::Context) -> DesignTokens {
     }
 
     {
-        // Turn off any expansion in the UI (see https://github.com/rerun-io/rerun/issues/2727)
-        egui_style.visuals.widgets.hovered.expansion = 0.0;
-        egui_style.visuals.widgets.active.expansion = 0.0;
-        egui_style.visuals.widgets.open.expansion = 0.0;
+        egui_style.visuals.widgets.hovered.expansion = 2.0;
+        egui_style.visuals.widgets.active.expansion = 2.0;
+        egui_style.visuals.widgets.open.expansion = 2.0;
     }
 
     egui_style.visuals.selection.bg_fill =

crates/re_viewport/src/viewport_blueprint_ui.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use re_data_store::InstancePath;
 use re_data_ui::item_ui;
 use re_space_view::DataBlueprintGroup;
 use re_viewer_context::{DataBlueprintGroupHandle, Item, SpaceViewId, ViewerContext};
@@ -104,6 +105,7 @@ impl ViewportBlueprint<'_> {
                 ui,
                 true,
                 visible,
+                false,
                 |ui| ui.label(format!("{:?}", container.kind())),
                 |re_ui, ui| {
                     visibility_changed =
@@ -144,6 +146,8 @@ impl ViewportBlueprint<'_> {
         let mut visibility_changed = false;
         let mut action = TreeAction::Keep;
         let mut visible = self.tree.is_visible(tile_id);
+        let item = Item::SpaceView(space_view.id);
+        let is_selected = ctx.selection().contains(&item);
 
         let root_group = space_view.data_blueprint.root_group();
         let default_open = Self::default_open_for_group(root_group);
@@ -159,6 +163,7 @@ impl ViewportBlueprint<'_> {
                 ui,
                 true,
                 visible,
+                is_selected,
                 |ui| {
                     let response = crate::item_ui::space_view_button(ctx, ui, space_view);
                     if response.clicked() {
@@ -224,6 +229,11 @@ impl ViewportBlueprint<'_> {
                 continue;
             }
 
+            let is_selected = ctx.selection().contains(&Item::InstancePath(
+                Some(space_view.id),
+                InstancePath::entity_splat(entity_path.clone()),
+            ));
+
             ui.horizontal(|ui| {
                 let mut properties = space_view
                     .data_blueprint
@@ -234,6 +244,7 @@ impl ViewportBlueprint<'_> {
                     ui,
                     group_is_visible,
                     properties.visible,
+                    is_selected,
                     |ui| {
                         let name = entity_path.iter().last().unwrap().to_string();
                         let label = format!("🔹 {name}");
@@ -278,6 +289,11 @@ impl ViewportBlueprint<'_> {
                     continue;
                 };
 
+            let is_selected = ctx.selection().contains(&Item::DataBlueprintGroup(
+                space_view.id,
+                *child_group_handle,
+            ));
+
             let mut remove_group = false;
             let default_open = Self::default_open_for_group(child_group);
             egui::collapsing_header::CollapsingState::load_with_default_open(
@@ -291,6 +307,7 @@ impl ViewportBlueprint<'_> {
                     ui,
                     group_is_visible,
                     child_group.properties_individual.visible,
+                    is_selected,
                     |ui| {
                         item_ui::data_blueprint_group_button_to(
                             ctx,
@@ -388,11 +405,13 @@ fn focus_tab(tree: &mut egui_tiles::Tree<SpaceViewId>, tab: &SpaceViewId) {
 /// and show a visibility button if the row is hovered.
 ///
 /// Returns true if visibility changed.
+#[allow(clippy::fn_params_excessive_bools)]
 fn blueprint_row_with_buttons(
     re_ui: &re_ui::ReUi,
     ui: &mut egui::Ui,
     enabled: bool,
     visible: bool,
+    selected: bool,
     add_content: impl FnOnce(&mut egui::Ui) -> egui::Response,
     add_on_hover_buttons: impl FnOnce(&re_ui::ReUi, &mut egui::Ui),
 ) {
@@ -459,13 +478,19 @@ fn blueprint_row_with_buttons(
 
     // The main button might have been highlighted because what it was referring
     // to was hovered somewhere else, and then we also want it highlighted here.
-    if button_hovered || main_button_response.highlighted() {
+    if button_hovered || main_button_response.highlighted() || selected {
         // Highlight the row:
         let visuals = ui.visuals().widgets.hovered;
+
+        let bg_fill = if selected {
+            ui.style().visuals.selection.bg_fill
+        } else {
+            visuals.bg_fill
+        };
         let hover_rect = main_button_rect.expand(visuals.expansion);
         ui.painter().set(
             where_to_add_hover_rect,
-            egui::Shape::rect_filled(hover_rect, visuals.rounding, visuals.bg_fill),
+            egui::Shape::rect_filled(hover_rect, visuals.rounding, bg_fill),
         );
     }
 }