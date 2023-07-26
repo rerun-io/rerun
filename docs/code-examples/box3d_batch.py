a272eee6004f9f6569ce21b16a1a4400c674c73f
Jeremy Leibs <jeremy@rerun.io>
2023-07-26 17:32:57 +0200

    Split lines

--- docs/code-examples/box3d_batch.py
+++ docs/code-examples/box3d_batch.py
@@ -14,7 +14,10 @@ rr.log_annotation_context(
 rr.log_obbs(
     "batch",
     half_sizes=[[2.0, 2.0, 1.0], [1.0, 1.0, 0.5]],
-    rotations_q=[Rotation.from_euler("xyz", [0, 0, 0]).as_quat(), Rotation.from_euler("xyz", [0, 0, 45]).as_quat()],
+    rotations_q=[
+        Rotation.from_euler("xyz", [0, 0, 0]).as_quat(),
+        Rotation.from_euler("xyz", [0, 0, 45]).as_quat(),
+    ],
     positions=[[2, 0, 0], [-2, 0, 0]],
     stroke_widths=0.05,
     class_ids=[2, 1],
