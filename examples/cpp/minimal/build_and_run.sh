739278597d7e05b061cddfc9f8e9f2f547e33a95
Andreas Reich <r_andreas2@web.de>
2023-08-01 12:26:50 +0200

    explicitely specify make files for build_and_run

--- examples/cpp/minimal/build_and_run.sh
+++ examples/cpp/minimal/build_and_run.sh
@@ -9,7 +9,7 @@ num_threads=$(getconf _NPROCESSORS_ONLN)
 
 mkdir -p build
 pushd build
-    cmake -DCMAKE_BUILD_TYPE=Debug ..
+    cmake  -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
     cmake --build . --config Debug --target rerun_example -j ${num_threads}
 popd
 
