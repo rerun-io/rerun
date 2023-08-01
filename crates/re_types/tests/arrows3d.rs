a48e87a7e829d660c332345726e400f6385cff7d
Clement Rey <cr.rey.clement@gmail.com>
2023-08-01 09:21:38 +0200

    added Rust unit test

--- crates/re_types/tests/arrows3d.rs
+++ crates/re_types/tests/arrows3d.rs
@@ -1,17 +1,17 @@
 use std::collections::HashMap;
 
-use re_types::{archetypes::Points3D, components, Archetype as _};
+use re_types::{archetypes::Arrows3D, components, Archetype as _};
 
 #[test]
 fn roundtrip() {
-    let expected = Points3D {
-        points: vec![
-            components::Point3D::new(1.0, 2.0, 3.0), //
-            components::Point3D::new(4.0, 5.0, 6.0),
+    let expected = Arrows3D {
+        arrows: vec![
+            components::Arrow3D::new([1.0, 2.0, 3.0], [4.0, 5.0, 6.0]), //
+            components::Arrow3D::new([10.0, 20.0, 30.0], [40.0, 50.0, 60.0]), //
         ],
         radii: Some(vec![
-            components::Radius(42.0), //
-            components::Radius(43.0),
+            components::Radius(1.0), //
+            components::Radius(10.0),
         ]),
         colors: Some(vec![
             components::Color::from_unmultiplied_rgba(0xAA, 0x00, 0x00, 0xCC), //
@@ -25,32 +25,29 @@ fn roundtrip() {
             components::ClassId(126), //
             components::ClassId(127), //
         ]),
-        keypoint_ids: Some(vec![
-            components::KeypointId(2), //
-            components::KeypointId(3), //
-        ]),
         instance_keys: Some(vec![
             components::InstanceKey(u64::MAX - 1), //
             components::InstanceKey(u64::MAX),
         ]),
     };
 
-    let arch = Points3D::new([(1.0, 2.0, 3.0), (4.0, 5.0, 6.0)])
-        .with_radii([42.0, 43.0])
-        .with_colors([0xAA0000CC, 0x00BB00DD])
-        .with_labels(["hello", "friend"])
-        .with_class_ids([126, 127])
-        .with_keypoint_ids([2, 3])
-        .with_instance_keys([u64::MAX - 1, u64::MAX]);
+    let arch = Arrows3D::new([
+        components::Arrow3D::new([1.0, 2.0, 3.0], [4.0, 5.0, 6.0]), //
+        components::Arrow3D::new([10.0, 20.0, 30.0], [40.0, 50.0, 60.0]), //
+    ])
+    .with_radii([1.0, 10.0])
+    .with_colors([0xAA0000CC, 0x00BB00DD])
+    .with_labels(["hello", "friend"])
+    .with_class_ids([126, 127])
+    .with_instance_keys([u64::MAX - 1, u64::MAX]);
     similar_asserts::assert_eq!(expected, arch);
 
     let expected_extensions: HashMap<_, _> = [
-        ("points", vec!["rerun.components.Point3D"]),
+        ("arrows", vec!["rerun.components.Arrow3D"]),
         ("radii", vec!["rerun.components.Radius"]),
         ("colors", vec!["rerun.components.Color"]),
         ("labels", vec!["rerun.components.Label"]),
         ("class_ids", vec!["rerun.components.ClassId"]),
-        ("keypoint_ids", vec!["rerun.components.KeypointId"]),
         ("instance_keys", vec!["rerun.components.InstanceKey"]),
     ]
     .into();
@@ -73,7 +70,7 @@ fn roundtrip() {
         }
     }
 
-    let deserialized = Points3D::from_arrow(serialized);
+    let deserialized = Arrows3D::from_arrow(serialized);
     similar_asserts::assert_eq!(expected, deserialized);
 }
 
