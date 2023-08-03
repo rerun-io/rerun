bd828bcbf99f320d62ee53df6c40e7bb65aa6a15
Andreas Reich <r_andreas2@web.de>
2023-08-03 19:00:51 +0200

    fix negative radii  in random point example

--- docs/code-examples/point3d_random_v2.cpp
+++ docs/code-examples/point3d_random_v2.cpp
@@ -11,20 +11,21 @@ int main() {
     rr_stream.connect("127.0.0.1:9876");
 
     std::default_random_engine gen;
-    std::uniform_real_distribution<float> dist(-5.0, 5.0);
-    std::uniform_int_distribution<uint8_t> dist_byte(0, 255);
+    std::uniform_real_distribution<float> dist_pos(-5.0, 5.0);
+    std::uniform_real_distribution<float> dist_radius(0.1, 1.0);
+    std::uniform_int_distribution<uint8_t> dist_color(0, 255);
 
     std::vector<rr::components::Point3D> points3d(10);
     std::generate(points3d.begin(), points3d.end(), [&] {
-        return rr::datatypes::Vec3D{dist(gen), dist(gen), dist(gen)};
+        return rr::datatypes::Vec3D{dist_pos(gen), dist_pos(gen), dist_pos(gen)};
     });
     std::vector<rr::components::Color> colors(10);
     std::generate(colors.begin(), colors.end(), [&] {
         // TODO(andreas): provide a `rgb` factory method.
-        return (dist_byte(gen) << 24) + (dist_byte(gen) << 16) + (dist_byte(gen) << 8);
+        return (dist_color(gen) << 24) + (dist_color(gen) << 16) + (dist_color(gen) << 8);
     });
     std::vector<rr::components::Radius> radii(10);
-    std::generate(radii.begin(), radii.end(), [&] { return dist(gen); });
+    std::generate(radii.begin(), radii.end(), [&] { return dist_radius(gen); });
 
     rr_stream.log(
         "random",
