rerun-io/rerun

commit aa1822df413f5b78162b4667356381ef2d3d3407

If a user hasn't interacted with the camera, continue to resize to the orbit-eye (#2839)
### What
Our initial orbit-eye state is sensitive to the timing of what content
is on screen when the scene is initialized.

There appears to be maybe be a regression that stems from *faster* scene
initialization, potentially interacting pathologically with the fact
that our blueprint doesn't yet store the camera position. In any case,
there are easy-to-reproduce situations where our timeline hasn't yet
updated from the initial time when the scene is first populated, meaning
many scenes just end up with the empty-scene initizalization for the
orbit-eye, and then stick there even as content is added to the scene.

This only appears to affect examples that run live via `.connect()`.
Using the viewer with a pre-saved rrd file (either locally or on the
web) mitigates the issue -- presumably because this causes our timestamp
to be updated sooner.

However, even if we initialize timeline differently relative to view
creation, it's very easy to create scenarios where you log a small piece
of content followed by a large piece of content which ends up
significantly outside the FOV.

### The Solution

We now track whether any user interaction has occured, and if it hasn't
continue to interpolate to the new target eye, bringing new content into
the field of view of the scene.

### Examples of worse behavior
It's worth noting the behavior of arkitscenes does seem like a
regression relative to the current behavior. I don't love the scene
bouncing around that way.

https://demo.rerun.io/pr/2839/examples/arkit_scenes/


### Examples of improved behavior
However, the behavior of linetrip example and colmap were arguably
totally broken before and this seems like a huge improvement:

```
python docs/code-examples/line_strip3d_batch.py
```
Before   |  After
:----:|:-----:

crates/re_space_view_spatial/src/ui_3d.rs
@@ -36,6 +36,7 @@ use super::eye::{Eye, OrbitEye};
 #[derive(Clone)]
 pub struct View3DState {
     pub orbit_eye: Option<OrbitEye>,
+    pub did_interact_with_eye: bool,
 
     /// Currently tracked camera.
     pub tracked_camera: Option<EntityPath>,
@@ -59,6 +60,7 @@ impl Default for View3DState {
     fn default() -> Self {
         Self {
             orbit_eye: Default::default(),
+            did_interact_with_eye: false,
             tracked_camera: None,
             camera_before_tracked_camera: None,
             eye_interpolation: Default::default(),
@@ -94,6 +96,13 @@ impl View3DState {
         space_cameras: &[SpaceCamera3D],
         view_coordinates: Option<ViewCoordinates>,
     ) -> &mut OrbitEye {
+        // If the user has not interacted with the eye-camera yet, continue to
+        // interpolate to the new default eye. This gives much better robustness
+        // with scenes that grow over time.
+        if !self.did_interact_with_eye {
+            self.interpolate_to_orbit_eye(default_eye(scene_bbox_accum, &view_coordinates));
+        }
+
         let orbit_camera = self
             .orbit_eye
             .get_or_insert_with(|| default_eye(scene_bbox_accum, &view_coordinates));
@@ -315,6 +324,7 @@ pub fn view_3d(
     let eye = orbit_eye.to_eye();
 
     if did_interact_with_eye {
+        state.state_3d.did_interact_with_eye = true;
         state.state_3d.eye_interpolation = None;
         state.state_3d.tracked_camera = None;
         state.state_3d.camera_before_tracked_camera = None;

commit f453f372edae3c790d5002187739681f8e0ac9b6

Fix always redrawing in the presence of a 3D space view (#2900)
* Fixes #2897

### What

Fix regression introduced in #2839
This landed in 0.8. Candidate for potential 0.8.1 release if we decide
to do one.

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/2900) (if
applicable)

- [PR Build Summary](https://build.rerun.io/pr/2900)
- [Docs
preview](https://rerun.io/preview/pr%3Aandreas%2Ffix-redraw/docs)
- [Examples
preview](https://rerun.io/preview/pr%3Aandreas%2Ffix-redraw/examples)

crates/re_space_view_spatial/src/ui_3d.rs
@@ -146,8 +146,8 @@ impl View3DState {
 
             if let Some(target_orbit) = &cam_interpolation.target_orbit {
                 *orbit_camera = cam_interpolation.start.lerp(target_orbit, t);
-            } else if let Some(target_camera) = &cam_interpolation.target_eye {
-                let camera = cam_interpolation.start.to_eye().lerp(target_camera, t);
+            } else if let Some(target_eye) = &cam_interpolation.target_eye {
+                let camera = cam_interpolation.start.to_eye().lerp(target_eye, t);
                 orbit_camera.copy_from_eye(&camera);
             } else {
                 self.eye_interpolation = None;
@@ -163,16 +163,21 @@ impl View3DState {
     }
 
     fn interpolate_to_eye(&mut self, target: Eye) {
-        if let Some(start) = self.orbit_eye {
-            let target_time = EyeInterpolation::target_time(&start.to_eye(), &target);
-            self.spin = false; // the user wants to move the camera somewhere, so stop spinning
-            self.eye_interpolation = Some(EyeInterpolation {
-                elapsed_time: 0.0,
-                target_time,
-                start,
-                target_orbit: None,
-                target_eye: Some(target),
-            });
+        if let Some(start) = self.orbit_eye.as_mut() {
+            // the user wants to move the camera somewhere, so stop spinning
+            self.spin = false;
+
+            if let Some(target_time) = EyeInterpolation::target_time(&start.to_eye(), &target) {
+                self.eye_interpolation = Some(EyeInterpolation {
+                    elapsed_time: 0.0,
+                    target_time,
+                    start: *start,
+                    target_orbit: None,
+                    target_eye: Some(target),
+                });
+            } else {
+                start.copy_from_eye(&target);
+            }
         } else {
             // shouldn't really happen (`self.orbit_eye` is only `None` for the first frame).
         }
@@ -180,15 +185,22 @@ impl View3DState {
 
     fn interpolate_to_orbit_eye(&mut self, target: OrbitEye) {
         if let Some(start) = self.orbit_eye {
-            let target_time = EyeInterpolation::target_time(&start.to_eye(), &target.to_eye());
-            self.spin = false; // the user wants to move the camera somewhere, so stop spinning
-            self.eye_interpolation = Some(EyeInterpolation {
-                elapsed_time: 0.0,
-                target_time,
-                start,
-                target_orbit: Some(target),
-                target_eye: None,
-            });
+            // the user wants to move the camera somewhere, so stop spinning
+            self.spin = false;
+
+            if let Some(target_time) =
+                EyeInterpolation::target_time(&start.to_eye(), &target.to_eye())
+            {
+                self.eye_interpolation = Some(EyeInterpolation {
+                    elapsed_time: 0.0,
+                    target_time,
+                    start,
+                    target_orbit: Some(target),
+                    target_eye: None,
+                });
+            } else {
+                self.orbit_eye = Some(target);
+            }
         } else {
             self.orbit_eye = Some(target);
         }
@@ -205,14 +217,23 @@ struct EyeInterpolation {
 }
 
 impl EyeInterpolation {
-    pub fn target_time(start: &Eye, stop: &Eye) -> f32 {
+    pub fn target_time(start: &Eye, stop: &Eye) -> Option<f32> {
         // Take more time if the rotation is big:
         let angle_difference = start
             .world_from_rub_view
             .rotation()
             .angle_between(stop.world_from_rub_view.rotation());
 
-        egui::remap_clamp(angle_difference, 0.0..=std::f32::consts::PI, 0.2..=0.7)
+        // Threshold to avoid doing pointless interpolations that trigger frame requests.
+        if angle_difference < 0.01 && start.pos_in_world().distance(stop.pos_in_world()) < 0.0001 {
+            None
+        } else {
+            Some(egui::remap_clamp(
+                angle_difference,
+                0.0..=std::f32::consts::PI,
+                0.2..=0.7,
+            ))
+        }
     }
 }
 