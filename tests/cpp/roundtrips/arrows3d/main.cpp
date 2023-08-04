1bbdeb2034683490886a4a1685d13395f0985761
Andreas Reich <r_andreas2@web.de>
2023-08-04 18:03:10 +0200

    extension for labels

--- tests/cpp/roundtrips/arrows3d/main.cpp
+++ tests/cpp/roundtrips/arrows3d/main.cpp
@@ -7,13 +7,13 @@ int main(int argc, char** argv) {
     auto rec_stream = rr::RecordingStream("roundtrip_arrows3d");
     rec_stream.save(argv[1]);
 
-    rec_stream.log_archetype(
+    rec_stream.log(
         "arrows3d",
         rr::archetypes::Arrows3D({{4.0f, 5.0f, 6.0f}, {40.0f, 50.0f, 60.0f}})
             .with_origins({{1.0f, 2.0f, 3.0f}, {10.0f, 20.0f, 30.0f}})
             .with_radii({0.1f, 1.0f})
             .with_colors({0xAA0000CC, 0x00BB00DD})
-            .with_labels({std::string("hello"), std::string("friend")})
+            .with_labels({"hello", "friend"})
             .with_class_ids({126, 127})
             .with_instance_keys({66, 666})
     );
--- tests/cpp/roundtrips/points3d/main.cpp
+++ tests/cpp/roundtrips/points3d/main.cpp
@@ -7,12 +7,12 @@ int main(int argc, char** argv) {
     auto rec_stream = rr::RecordingStream("roundtrip_points3d");
     rec_stream.save(argv[1]);
 
-    rec_stream.log_archetype(
+    rec_stream.log(
         "points3d",
         rr::archetypes::Points3D({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}})
             .with_radii({0.42f, 0.43f})
             .with_colors({0xAA0000CC, 0x00BB00DD})
-            .with_labels({std::string("hello"), std::string("friend")})
+            .with_labels({"hello", "friend"})
             .with_class_ids({126, 127})
             .with_keypoint_ids({2, 3})
             .with_instance_keys({66, 666})
