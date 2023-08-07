c0b46fa14d5dc896056e9b9fb24ec44742c4f681
Clement Rey <cr.rey.clement@gmail.com>
2023-08-07 19:12:46 +0200

    add rerun script harness to DNA example

--- examples/python/dna/main.py
+++ examples/python/dna/main.py
@@ -6,7 +6,7 @@ The example from our Getting Started page.
 """
 from __future__ import annotations
 
-import sys
+import argparse
 from math import tau
 
 import numpy as np
@@ -14,36 +14,46 @@ import rerun as rr  # pip install rerun-sdk
 from rerun_demo.data import build_color_spiral
 from rerun_demo.util import bounce_lerp, interleave
 
-# sanity-check since all other example scripts take arguments:
-assert len(sys.argv) == 1, f"{sys.argv[0]} does not take any arguments"
 
-rr.init("DNA Abacus")
+def log_data():
+    rr.set_time_seconds("stable_time", 0)
 
-rr.spawn()
-rr.set_time_seconds("stable_time", 0)
+    NUM_POINTS = 100
 
-NUM_POINTS = 100
+    # points and colors are both np.array((NUM_POINTS, 3))
+    points1, colors1 = build_color_spiral(NUM_POINTS)
+    points2, colors2 = build_color_spiral(NUM_POINTS, angular_offset=tau * 0.5)
+    rr.log_points("helix/structure/left", points1, colors=colors1, radii=0.08)
+    rr.log_points("helix/structure/right", points2, colors=colors2, radii=0.08)
 
-# points and colors are both np.array((NUM_POINTS, 3))
-points1, colors1 = build_color_spiral(NUM_POINTS)
-points2, colors2 = build_color_spiral(NUM_POINTS, angular_offset=tau * 0.5)
-rr.log_points("dna/structure/left", points1, colors=colors1, radii=0.08)
-rr.log_points("dna/structure/right", points2, colors=colors2, radii=0.08)
+    points = interleave(points1, points2)
+    rr.log_line_segments("helix/structure/scaffolding", points, color=[128, 128, 128])
 
-points = interleave(points1, points2)
-rr.log_line_segments("dna/structure/scaffolding", points, color=[128, 128, 128])
+    time_offsets = np.random.rand(NUM_POINTS)
+    for i in range(400):
+        time = i * 0.01
+        rr.set_time_seconds("stable_time", time)
 
-time_offsets = np.random.rand(NUM_POINTS)
-for i in range(400):
-    time = i * 0.01
-    rr.set_time_seconds("stable_time", time)
+        times = np.repeat(time, NUM_POINTS) + time_offsets
+        beads = [bounce_lerp(points1[n], points2[n], times[n]) for n in range(NUM_POINTS)]
+        colors = [[int(bounce_lerp(80, 230, times[n] * 2))] for n in range(NUM_POINTS)]
+        rr.log_points("helix/structure/scaffolding/beads", beads, radii=0.06, colors=np.repeat(colors, 3, axis=-1))
 
-    times = np.repeat(time, NUM_POINTS) + time_offsets
-    beads = [bounce_lerp(points1[n], points2[n], times[n]) for n in range(NUM_POINTS)]
-    colors = [[int(bounce_lerp(80, 230, times[n] * 2))] for n in range(NUM_POINTS)]
-    rr.log_points("dna/structure/scaffolding/beads", beads, radii=0.06, colors=np.repeat(colors, 3, axis=-1))
+        rr.log_transform3d(
+            "helix/structure",
+            rr.RotationAxisAngle(axis=[0, 0, 1], radians=time / 4.0 * tau),
+        )
 
-    rr.log_transform3d(
-        "dna/structure",
-        rr.RotationAxisAngle(axis=[0, 0, 1], radians=time / 4.0 * tau),
-    )
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Logs rich data using the Rerun SDK.")
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+
+    rr.script_setup(args, "helix Abacus")
+    log_data()
+    rr.script_teardown(args)
+
+
+if __name__ == "__main__":
+    main()
