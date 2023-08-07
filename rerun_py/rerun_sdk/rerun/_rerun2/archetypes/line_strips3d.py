38537d402c4bcaa93b5f4a873c35b56883aa72fc
Clement Rey <cr.rey.clement@gmail.com>
2023-08-07 17:51:01 +0200

    re-implement LineStrip3D's space view part with new archetype

--- rerun_py/rerun_sdk/rerun/_rerun2/archetypes/line_strips3d.py
+++ rerun_py/rerun_sdk/rerun/_rerun2/archetypes/line_strips3d.py
@@ -22,31 +22,35 @@ class LineStrips3D(Archetype):
     Many strips:
     ```python
     import rerun as rr
+    import rerun.experimental as rr2
 
     rr.init("line_strip3d", spawn=True)
 
-    rr.log_line_strips_3d(
+    rr2.log(
        "batch",
-       [
+       rr2.LineStrips3D(
            [
-               [0, 0, 2],
-               [1, 0, 2],
-               [1, 1, 2],
-               [0, 1, 2],
+               [
+                   [0, 0, 2],
+                   [1, 0, 2],
+                   [1, 1, 2],
+                   [0, 1, 2],
+               ],
+               [
+                   [0, 0, 0],
+                   [0, 0, 1],
+                   [1, 0, 0],
+                   [1, 0, 1],
+                   [1, 1, 0],
+                   [1, 1, 1],
+                   [0, 1, 0],
+                   [0, 1, 1],
+               ],
            ],
-           [
-               [0, 0, 0],
-               [0, 0, 1],
-               [1, 0, 0],
-               [1, 0, 1],
-               [1, 1, 0],
-               [1, 1, 1],
-               [0, 1, 0],
-               [0, 1, 1],
-           ],
-       ],
-       colors=[[255, 0, 0], [0, 255, 0]],
-       stroke_widths=[0.025, 0.005],
+           colors=[[255, 0, 0], [0, 255, 0]],
+           radii=[0.025, 0.005],
+           labels=["one strip here", "and one strip there"],
+       ),
     )
     ```
 
