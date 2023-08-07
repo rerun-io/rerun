8520614f2346b371ca7fea4b0b921f4330aa9cd5
Clement Rey <cr.rey.clement@gmail.com>
2023-08-07 18:56:02 +0200

    reimplement legacy Python APIs by delegating to archetypes

--- rerun_py/rerun_sdk/rerun/log/lines.py
+++ rerun_py/rerun_sdk/rerun/log/lines.py
@@ -4,20 +4,13 @@ from typing import Any, Iterable
 
 import numpy as np
 import numpy.typing as npt
-from deprecated import deprecated
 
-from rerun import bindings
-from rerun.components import instance_key_splat
-from rerun.components.draw_order import DrawOrderArray
-from rerun.components.linestrip import LineStrip2DArray, LineStrip3DArray
-from rerun.log import Color, Colors, _normalize_colors, _normalize_radii
+from rerun.log import Color, Colors, _normalize_radii
 from rerun.log.error_utils import _send_warning
-from rerun.log.extension_components import _add_extension_components
 from rerun.log.log_decorator import log_decorator
 from rerun.recording_stream import RecordingStream
 
 __all__ = [
-    "log_path",
     "log_line_strip",
     "log_line_strips_2d",
     "log_line_strips_3d",
@@ -25,26 +18,10 @@ __all__ = [
 ]
 
 
-@deprecated(version="0.2.0", reason="Use log_line_strip instead")
-def log_path(
-    entity_path: str,
-    positions: npt.ArrayLike | None,
-    *,
-    stroke_width: float | None = None,
-    color: Color | None = None,
-    ext: dict[str, Any] | None = None,
-    timeless: bool = False,
-    recording: RecordingStream | None = None,
-) -> None:
-    log_line_strip(
-        entity_path, positions, stroke_width=stroke_width, color=color, ext=ext, timeless=timeless, recording=recording
-    )
-
-
 @log_decorator
 def log_line_strip(
     entity_path: str,
-    positions: npt.ArrayLike | None,
+    positions: npt.ArrayLike,
     *,
     stroke_width: float | None = None,
     color: Color | None = None,
@@ -90,52 +67,40 @@ def log_line_strip(
         See also: [`rerun.init`][], [`rerun.set_global_data_recording`][].
 
     """
-    from rerun.experimental import cmp as rrc
+    from rerun.experimental import LineStrips2D, LineStrips3D, log
+
+    if positions is None:
+        raise ValueError("`positions` argument must be set")
 
     recording = RecordingStream.to_native(recording)
 
-    if positions is not None:
-        positions = np.require(positions, dtype="float32")
+    stroke_widths = _normalize_radii(stroke_width)
+    radii = stroke_widths / 2.0
 
-    instanced: dict[str, Any] = {}
-    splats: dict[str, Any] = {}
-
-    if positions is not None:
-        if positions.shape[1] == 2:
-            instanced["rerun.linestrip2d"] = LineStrip2DArray.from_numpy_arrays([positions])
-        elif positions.shape[1] == 3:
-            instanced["rerun.linestrip3d"] = LineStrip3DArray.from_numpy_arrays([positions])
-        else:
-            raise TypeError("Positions should be either Nx2 or Nx3")
-
-    if color is not None:
-        colors = _normalize_colors(color)
-        instanced["rerun.colorrgba"] = rrc.ColorArray.from_similar(colors).storage
-
-    # We store the stroke_width in radius
-    if stroke_width:
-        radii = _normalize_radii([stroke_width / 2])
-        instanced["rerun.radius"] = rrc.RadiusArray.from_similar(radii).storage
-
-    if draw_order is not None:
-        instanced["rerun.draw_order"] = DrawOrderArray.splat(draw_order)
-
-    if ext:
-        _add_extension_components(instanced, splats, ext, None)
-
-    if splats:
-        splats["rerun.instance_key"] = instance_key_splat()
-        bindings.log_arrow_msg(entity_path, components=splats, timeless=timeless, recording=recording)
-
-    # Always the primary component last so range-based queries will include the other data. See(#1215)
-    if instanced:
-        bindings.log_arrow_msg(entity_path, components=instanced, timeless=timeless, recording=recording)
+    positions = np.require(positions, dtype="float32")
+    if positions.shape[1] == 2:
+        strips2d = LineStrips2D(
+            [positions],
+            radii=radii,
+            colors=color,
+            draw_order=draw_order,
+        )
+        return log(entity_path, strips2d, ext=ext, timeless=timeless, recording=recording)
+    elif positions.shape[1] == 3:
+        strips3d = LineStrips3D(
+            [positions],
+            radii=radii,
+            colors=color,
+        )
+        return log(entity_path, strips3d, ext=ext, timeless=timeless, recording=recording)
+    else:
+        raise TypeError("Positions should be either Nx2 or Nx3")
 
 
 @log_decorator
 def log_line_strips_2d(
     entity_path: str,
-    line_strips: Iterable[npt.ArrayLike] | None,
+    line_strips: Iterable[npt.ArrayLike],
     *,
     identifiers: npt.ArrayLike | None = None,
     stroke_widths: npt.ArrayLike | None = None,
@@ -187,13 +152,12 @@ def log_line_strips_2d(
         See also: [`rerun.init`][], [`rerun.set_global_data_recording`][].
 
     """
-    from rerun.experimental import cmp as rrc
+    from rerun.experimental import LineStrips2D, log
+
+    if line_strips is None:
+        raise ValueError("`line_strips` argument must be set")
 
     recording = RecordingStream.to_native(recording)
-
-    colors = _normalize_colors(colors)
-    stroke_widths = _normalize_radii(stroke_widths)
-    radii = stroke_widths / 2.0
 
     identifiers_np = np.array((), dtype="uint64")
     if identifiers is not None:
@@ -202,57 +166,27 @@ def log_line_strips_2d(
         except ValueError:
             _send_warning("Only integer identifiers supported", 1)
 
-    # 0 = instanced, 1 = splat
-    comps = [{}, {}]  # type: ignore[var-annotated]
+    stroke_widths = _normalize_radii(stroke_widths)
+    radii = stroke_widths / 2.0
 
-    if line_strips is not None:
-        line_strip_arrs = [np.require(line, dtype="float32") for line in line_strips]
-        dims = [line.shape[1] for line in line_strip_arrs]
-
-        if any(d != 2 for d in dims):
-            raise ValueError("All line strips must be Nx2")
-
-        comps[0]["rerun.linestrip2d"] = LineStrip2DArray.from_numpy_arrays(line_strip_arrs)
-
-    if len(identifiers_np):
-        comps[0]["rerun.instance_key"] = rrc.InstanceKeyArray.from_similar(identifiers_np).storage
-
-    if len(colors):
-        from rerun.experimental import cmp as rrc
-
-        is_splat = len(colors.shape) == 1
-        if is_splat:
-            colors = colors.reshape(1, len(colors))
-        comps[is_splat]["rerun.colorrgba"] = rrc.ColorArray.from_similar(colors).storage
-
-    # We store the stroke_width in radius
-    if len(radii):
-        is_splat = len(radii) == 1
-        comps[is_splat]["rerun.radius"] = rrc.RadiusArray.from_similar(radii).storage
-
-    if draw_order is not None:
-        comps[1]["rerun.draw_order"] = DrawOrderArray.splat(draw_order)
-
-    if ext:
-        _add_extension_components(comps[0], comps[1], ext, identifiers_np)
-
-    if comps[1]:
-        comps[1]["rerun.instance_key"] = instance_key_splat()
-        bindings.log_arrow_msg(entity_path, components=comps[1], timeless=timeless, recording=recording)
-
-    # Always the primary component last so range-based queries will include the other data. See(#1215)
-    bindings.log_arrow_msg(entity_path, components=comps[0], timeless=timeless, recording=recording)
+    arch = LineStrips2D(
+        line_strips,
+        radii=radii,
+        colors=colors,
+        draw_order=draw_order,
+        instance_keys=identifiers_np,
+    )
+    return log(entity_path, arch, ext=ext, timeless=timeless, recording=recording)
 
 
 @log_decorator
 def log_line_strips_3d(
     entity_path: str,
-    line_strips: Iterable[npt.ArrayLike] | None,
+    line_strips: Iterable[npt.ArrayLike],
     *,
     identifiers: npt.ArrayLike | None = None,
     stroke_widths: npt.ArrayLike | None = None,
     colors: Color | Colors | None = None,
-    draw_order: float | None = None,
     ext: dict[str, Any] | None = None,
     timeless: bool = False,
     recording: RecordingStream | None = None,
@@ -285,10 +219,6 @@ def log_line_strips_3d(
     colors:
         Optional colors of the lines.
         RGB or RGBA in sRGB gamma-space as either 0-1 floats or 0-255 integers, with separate alpha.
-    draw_order:
-        An optional floating point value that specifies the 2D drawing order.
-        Objects with higher values are drawn on top of those with lower values.
-        The default for lines is 20.0.
     ext:
         Optional dictionary of extension components. See [rerun.log_extension_components][]
     timeless:
@@ -299,13 +229,12 @@ def log_line_strips_3d(
         See also: [`rerun.init`][], [`rerun.set_global_data_recording`][].
 
     """
-    from rerun.experimental import cmp as rrc
+    from rerun.experimental import LineStrips3D, log
+
+    if line_strips is None:
+        raise ValueError("`line_strips` argument must be set")
 
     recording = RecordingStream.to_native(recording)
-
-    colors = _normalize_colors(colors)
-    stroke_widths = _normalize_radii(stroke_widths)
-    radii = stroke_widths / 2.0
 
     identifiers_np = np.array((), dtype="uint64")
     if identifiers is not None:
@@ -314,46 +243,16 @@ def log_line_strips_3d(
         except ValueError:
             _send_warning("Only integer identifiers supported", 1)
 
-    # 0 = instanced, 1 = splat
-    comps = [{}, {}]  # type: ignore[var-annotated]
+    stroke_widths = _normalize_radii(stroke_widths)
+    radii = stroke_widths / 2.0
 
-    if line_strips is not None:
-        line_strip_arrs = [np.require(line, dtype="float32") for line in line_strips]
-        dims = [line.shape[1] for line in line_strip_arrs]
-
-        if any(d != 3 for d in dims):
-            raise ValueError("All line strips must be Nx3")
-
-        comps[0]["rerun.linestrip3d"] = LineStrip3DArray.from_numpy_arrays(line_strip_arrs)
-
-    if len(identifiers_np):
-        comps[0]["rerun.instance_key"] = rrc.InstanceKeyArray.from_similar(identifiers_np).storage
-
-    if len(colors):
-        from rerun.experimental import cmp as rrc
-
-        is_splat = len(colors.shape) == 1
-        if is_splat:
-            colors = colors.reshape(1, len(colors))
-        comps[is_splat]["rerun.colorrgba"] = rrc.ColorArray.from_similar(colors).storage
-
-    # We store the stroke_width in radius
-    if len(radii):
-        is_splat = len(radii) == 1
-        comps[is_splat]["rerun.radius"] = rrc.RadiusArray.from_similar(radii).storage
-
-    if draw_order is not None:
-        comps[1]["rerun.draw_order"] = DrawOrderArray.splat(draw_order)
-
-    if ext:
-        _add_extension_components(comps[0], comps[1], ext, identifiers_np)
-
-    if comps[1]:
-        comps[1]["rerun.instance_key"] = instance_key_splat()
-        bindings.log_arrow_msg(entity_path, components=comps[1], timeless=timeless, recording=recording)
-
-    # Always the primary component last so range-based queries will include the other data. See(#1215)
-    bindings.log_arrow_msg(entity_path, components=comps[0], timeless=timeless, recording=recording)
+    arch = LineStrips3D(
+        line_strips,
+        radii=radii,
+        colors=colors,
+        instance_keys=identifiers_np,
+    )
+    return log(entity_path, arch, ext=ext, timeless=timeless, recording=recording)
 
 
 @log_decorator
@@ -404,55 +303,37 @@ def log_line_segments(
         See also: [`rerun.init`][], [`rerun.set_global_data_recording`][].
 
     """
-    from rerun.experimental import cmp as rrc
+    from rerun.experimental import LineStrips2D, LineStrips3D, log
+
+    if positions is None:
+        raise ValueError("`positions` argument must be set")
 
     recording = RecordingStream.to_native(recording)
 
-    if positions is None:
-        positions = np.require([], dtype="float32")
     positions = np.require(positions, dtype="float32")
-
-    instanced: dict[str, Any] = {}
-    splats: dict[str, Any] = {}
-
-    if positions is not None:
-        # If not a multiple of 2, drop the last row
-        if len(positions) % 2:
-            positions = positions[:-1]
-        if positions.ndim > 1 and positions.shape[1] == 2:
-            # Reshape even-odd pairs into a collection of line-strips of length2
-            # [[a00, a01], [a10, a11], [b00, b01], [b10, b11]]
-            # -> [[[a00, a01], [a10, a11]], [[b00, b01], [b10, b11]]]
-            positions = positions.reshape([len(positions) // 2, 2, 2])
-            instanced["rerun.linestrip2d"] = LineStrip2DArray.from_numpy_arrays(positions)
-        elif positions.ndim > 1 and positions.shape[1] == 3:
-            # Same as above but for 3d points
-            positions = positions.reshape([len(positions) // 2, 2, 3])
-            instanced["rerun.linestrip3d"] = LineStrip3DArray.from_numpy_arrays(positions)
-        else:
-            raise TypeError("Positions should be either Nx2 or Nx3")
-
-    # The current API splats both color and stroke-width, though the data-model doesn't
-    # require that we do so.
-    if color is not None:
-        colors = _normalize_colors(color)
-        splats["rerun.colorrgba"] = rrc.ColorArray.from_similar(colors).storage
-
-    # We store the stroke_width in radius
-    if stroke_width:
-        radii = _normalize_radii([stroke_width / 2])
-        splats["rerun.radius"] = rrc.RadiusArray.from_similar(radii).storage
-
-    if draw_order is not None:
-        instanced["rerun.draw_order"] = DrawOrderArray.splat(draw_order)
-
-    if ext:
-        _add_extension_components(instanced, splats, ext, None)
-
-    if splats:
-        splats["rerun.instance_key"] = instance_key_splat()
-        bindings.log_arrow_msg(entity_path, components=splats, timeless=timeless, recording=recording)
-
-    # Always the primary component last so range-based queries will include the other data. See(#1215)
-    if instanced:
-        bindings.log_arrow_msg(entity_path, components=instanced, timeless=timeless, recording=recording)
+    # If not a multiple of 2, drop the last row
+    if len(positions) % 2:
+        positions = positions[:-1]
+    if positions.ndim > 1 and positions.shape[1] == 2:
+        # Reshape even-odd pairs into a collection of line-strips of length2
+        # [[a00, a01], [a10, a11], [b00, b01], [b10, b11]]
+        # -> [[[a00, a01], [a10, a11]], [[b00, b01], [b10, b11]]]
+        positions = positions.reshape([len(positions) // 2, 2, 2])
+        strips2d = LineStrips2D(
+            positions,
+            radii=stroke_width * 0.5 if stroke_width is not None else None,
+            colors=color,
+            draw_order=draw_order,
+        )
+        return log(entity_path, strips2d, ext=ext, timeless=timeless, recording=recording)
+    elif positions.ndim > 1 and positions.shape[1] == 3:
+        # Same as above but for 3d points
+        positions = positions.reshape([len(positions) // 2, 2, 3])
+        strips3d = LineStrips3D(
+            positions,
+            radii=stroke_width * 0.5 if stroke_width is not None else None,
+            colors=color,
+        )
+        return log(entity_path, strips3d, ext=ext, timeless=timeless, recording=recording)
+    else:
+        raise TypeError("Positions should be either Nx2 or Nx3")
