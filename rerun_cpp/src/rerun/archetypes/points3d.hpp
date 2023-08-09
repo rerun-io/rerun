b975407e43c4ea38bf2279e4b540660d169c6445
Andreas Reich <andreas@rerun.io>
2023-08-09 14:46:25 +0200

    Fix quotes in generated C++ doc strings (#2943)
    ### What
    
    Check codegen'd files why this was necessary :)
    Came up on #2940 but was a pre-existing problem
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/2943) (if
    applicable)
    
    - [PR Build Summary](https://build.rerun.io/pr/2943)
    - [Docs
    preview](https://rerun.io/preview/pr%3Aandreas%2Fcpp%2Ffix-escaped-doc-comments/docs)
    - [Examples
    preview](https://rerun.io/preview/pr%3Aandreas%2Fcpp%2Ffix-escaped-doc-comments/examples)

--- rerun_cpp/src/rerun/archetypes/points3d.hpp
+++ rerun_cpp/src/rerun/archetypes/points3d.hpp
@@ -32,10 +32,10 @@ namespace rerun {
         /// namespace rr = rerun;
         ///
         /// int main() {
-        ///    auto rr_stream = rr::RecordingStream(\"points3d_simple\");
-        ///    rr_stream.connect(\"127.0.0.1:9876\");
+        ///    auto rr_stream = rr::RecordingStream("points3d_simple");
+        ///    rr_stream.connect("127.0.0.1:9876");
         ///
-        ///    rr_stream.log(\"points\", rr::archetypes::Points3D({{0.0f, 0.0f, 0.0f},
+        ///    rr_stream.log("points", rr::archetypes::Points3D({{0.0f, 0.0f, 0.0f},
         ///    {1.0f, 1.0f, 1.0f}}));
         /// }
         ///```
