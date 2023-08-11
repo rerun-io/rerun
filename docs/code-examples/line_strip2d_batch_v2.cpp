be609cbee72790d05a7b880d0910931c46edf1cd
Andreas Reich <andreas@rerun.io>
2023-08-11 09:41:19 +0200

    Update docs/code-examples/line_strip2d_batch_v2.cpp
    Co-authored-by: Jeremy Leibs <jeremy@rerun.io>

--- docs/code-examples/line_strip2d_batch_v2.cpp
+++ docs/code-examples/line_strip2d_batch_v2.cpp
@@ -8,7 +8,7 @@ int main() {
     auto rr_stream = rr::RecordingStream("line_strip2d");
     rr_stream.connect("127.0.0.1:9876");
 
-    std::vector<rr::datatypes::Vec2D> strip1 = {{0.f, 0.f}, {2.f, 1.f}, {4.f, -1.f}, {6.f, 0.}};
+    std::vector<rr::datatypes::Vec2D> strip1 = {{0.f, 0.f}, {2.f, 1.f}, {4.f, -1.f}, {6.f, 0.f}};
     std::vector<rr::datatypes::Vec2D> strip2 =
         {{0.f, 3.f}, {1.f, 4.f}, {2.f, 2.f}, {3.f, 4.f}, {4.f, 2.f}, {5.f, 4.f}, {6.f, 3.f}};
     rr_stream.log(
