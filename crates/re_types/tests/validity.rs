3b836c109d259153141c1af3c3aa1e88b5ddd2f9
Jeremy Leibs <jeremy@rerun.io>
2023-08-14 16:35:41 +0200

    Add unit test to ensure validity checks do the right thing

---
+++ crates/re_types/tests/validity.rs
@@ -0,0 +1,40 @@
+use re_types::{
+    components::{self, Point2D},
+    DeserializationError, Loggable,
+};
+
+#[test]
+fn validity_checks() {
+    let good_non_nullable = vec![
+        components::Point2D::new(1.0, 2.0), //
+        components::Point2D::new(3.0, 4.0), //
+    ];
+
+    let serialized = Point2D::try_to_arrow(good_non_nullable, None).unwrap();
+    let deserialized = Point2D::try_from_arrow(serialized.as_ref());
+    assert!(deserialized.is_ok());
+
+    let good_nullable = vec![
+        Some(components::Point2D::new(1.0, 2.0)), //
+        Some(components::Point2D::new(3.0, 4.0)), //
+    ];
+
+    let serialized = Point2D::try_to_arrow_opt(good_nullable, None).unwrap();
+    let deserialized = Point2D::try_from_arrow(serialized.as_ref());
+    assert!(deserialized.is_ok());
+
+    let bad = vec![
+        Some(components::Point2D::new(1.0, 2.0)), //
+        None,
+    ];
+
+    let serialized = Point2D::try_to_arrow_opt(bad, None).unwrap();
+    let deserialized = Point2D::try_from_arrow(serialized.as_ref());
+    assert!(deserialized.is_err());
+    assert!(matches!(
+        deserialized.err().unwrap(),
+        DeserializationError::MissingData { .. }
+    ));
+}
+
+mod util;
