3f9f2b91e5c7aaceb05f73cc269143d042388b13
Jeremy Leibs <jeremy@rerun.io>
2023-08-16 21:14:40 +0200

    Multiple tensor tests

--- crates/re_types/tests/tensor.rs
+++ crates/re_types/tests/tensor.rs
@@ -8,7 +8,7 @@ use re_types::{
 
 #[test]
 fn tensor_roundtrip() {
-    let expected = Tensor {
+    let all_expected = [Tensor {
         data: TensorData {
             id: TensorId {
                 id: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
@@ -27,9 +27,9 @@ fn tensor_roundtrip() {
         }
         .into(),
         meaning: Some(TensorMeaning::ClassId(true).into()),
-    };
+    }];
 
-    let arch = Tensor::new(TensorData {
+    let all_arch = [Tensor::new(TensorData {
         id: TensorId {
             id: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
         },
@@ -45,7 +45,7 @@ fn tensor_roundtrip() {
         ],
         buffer: TensorBuffer::U8(vec![1, 2, 3, 4, 5, 6].into()),
     })
-    .with_meaning(TensorMeaning::ClassId(true));
+    .with_meaning(TensorMeaning::ClassId(true))];
 
     let expected_extensions: HashMap<_, _> = [
         ("data", vec!["rerun.components.TensorData"]),
@@ -53,26 +53,28 @@ fn tensor_roundtrip() {
     ]
     .into();
 
-    eprintln!("arch = {arch:#?}");
-    let serialized = arch.to_arrow();
-    for (field, array) in &serialized {
-        // NOTE: Keep those around please, very useful when debugging.
-        // eprintln!("field = {field:#?}");
-        // eprintln!("array = {array:#?}");
-        eprintln!("{} = {array:#?}", field.name);
+    for (expected, arch) in all_expected.into_iter().zip(all_arch) {
+        eprintln!("arch = {arch:#?}");
+        let serialized = arch.to_arrow();
+        for (field, array) in &serialized {
+            // NOTE: Keep those around please, very useful when debugging.
+            // eprintln!("field = {field:#?}");
+            // eprintln!("array = {array:#?}");
+            eprintln!("{} = {array:#?}", field.name);
 
-        // TODO(cmc): Re-enable extensions and these assertions once `arrow2-convert`
-        // has been fully replaced.
-        if false {
-            util::assert_extensions(
-                &**array,
-                expected_extensions[field.name.as_str()].as_slice(),
-            );
+            // TODO(cmc): Re-enable extensions and these assertions once `arrow2-convert`
+            // has been fully replaced.
+            if false {
+                util::assert_extensions(
+                    &**array,
+                    expected_extensions[field.name.as_str()].as_slice(),
+                );
+            }
         }
-    }
 
-    let deserialized = Tensor::from_arrow(serialized);
-    similar_asserts::assert_eq!(expected, deserialized);
+        let deserialized = Tensor::from_arrow(serialized);
+        similar_asserts::assert_eq!(expected, deserialized);
+    }
 }
 
 mod util;
