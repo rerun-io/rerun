a3ba0a07e1f259f9b5e92b66b503a3c7862beecd
Andreas Reich <r_andreas2@web.de>
2023-08-22 09:57:55 +0200

    comment fixes

--- crates/re_viewer_context/src/space_view/named_system.rs
+++ crates/re_viewer_context/src/space_view/named_system.rs
@@ -3,7 +3,7 @@ use std::collections::{BTreeMap, BTreeSet};
 use re_log_types::EntityPath;
 
 re_string_interner::declare_new_type!(
-    /// Unique name for a system within a given SpaceViewClass.
+    /// Unique name for a system within a given [`crate::SpaceViewClass`].
     ///
     /// Note that this is *not* unique across the entire application.
     #[derive(serde::Deserialize, serde::Serialize)]
@@ -19,6 +19,12 @@ impl ViewSystemName {
 
 pub type PerSystemEntities = BTreeMap<ViewSystemName, BTreeSet<EntityPath>>;
 
+/// Trait for naming/identifying [`crate::ViewPartSystem`]s & [`crate::ViewContextSystem`]s.
+///
+/// Required to be implemented for registration.
 pub trait NamedViewSystem {
+    /// Unique name for a system within a given [`crate::SpaceViewClass`].
+    ///
+    /// Note that this is *not* unique across the entire application.
     fn name() -> ViewSystemName;
 }
