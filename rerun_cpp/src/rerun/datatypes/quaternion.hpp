596b11c21a3d8470ddba00d4c24aef965b45869a
Jeremy Leibs <jeremy@rerun.io>
2023-08-24 17:24:35 +0200

    Add doc note

--- rerun_cpp/src/rerun/datatypes/quaternion.hpp
+++ rerun_cpp/src/rerun/datatypes/quaternion.hpp
@@ -17,6 +17,9 @@ namespace arrow {
 namespace rerun {
     namespace datatypes {
         /// A Quaternion represented by 4 real numbers.
+        ///
+        /// Note: although the x,y,z,w components of the quaternion will be passed through to the
+        /// datastore as provided, when used in the viewer Quaternions will always be normalized.
         struct Quaternion {
             float xyzw[4];
 
