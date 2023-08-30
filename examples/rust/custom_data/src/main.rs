a42c53b11ba259fdebcf7f91a2dd7eb19765e6ac
Clement Rey <cr.rey.clement@gmail.com>
2023-08-30 17:45:50 +0200

    introduce custom_data example (making this compile is the goal of this PR!)

---
+++ examples/rust/custom_data/src/main.rs
@@ -0,0 +1,118 @@
+//! Demonstrates how to implement custom archetypes and components, and extend existing ones.
+
+use rerun::{
+    archetypes::Points3D,
+    datatypes::Float32,
+    demo_util::grid,
+    external::{arrow2, glam, re_types},
+    Archetype, ArchetypeName, ComponentList, ComponentName, Loggable, MsgSender,
+    RecordingStreamBuilder,
+};
+
+// ---
+
+/// A custom [`Archetype`] that extends Rerun's builtin [`Points3D`] archetype with extra
+/// [`rerun::Component`]s.
+struct CustomPoints3D {
+    points3d: Points3D,
+    confidences: Option<Vec<Confidence>>,
+}
+
+impl Archetype for CustomPoints3D {
+    fn name() -> ArchetypeName {
+        "user.CustomPoints3D".into()
+    }
+
+    fn required_components() -> std::borrow::Cow<'static, [rerun::ComponentName]> {
+        Points3D::required_components()
+    }
+
+    fn recommended_components() -> std::borrow::Cow<'static, [rerun::ComponentName]> {
+        Points3D::recommended_components()
+            .iter()
+            .copied()
+            .chain([Confidence::name()])
+            .collect::<Vec<_>>()
+            .into()
+    }
+
+    fn optional_components() -> std::borrow::Cow<'static, [rerun::ComponentName]> {
+        Points3D::optional_components()
+    }
+
+    fn num_instances(&self) -> usize {
+        self.points3d.num_instances()
+    }
+
+    fn as_component_lists(&self) -> Vec<&dyn ComponentList> {
+        // TODO(#3159): need an easy way to get a CustomPoints3DIndicator component in here!
+        self.points3d
+            .as_component_lists()
+            .into_iter()
+            .chain(
+                std::iter::once(self.confidences.as_ref().map(|v| v as &dyn ComponentList))
+                    .flatten(),
+            )
+            .collect()
+    }
+}
+
+// ---
+
+/// A custom [`rerun::Component`] that is backed by a builtin [`Float32`] scalar [`rerun::Datatype`].
+#[derive(Debug, Clone, Copy)]
+struct Confidence(Float32);
+
+impl From<f32> for Confidence {
+    fn from(v: f32) -> Self {
+        Self(Float32(v))
+    }
+}
+
+impl Loggable for Confidence {
+    type Name = ComponentName;
+
+    fn name() -> Self::Name {
+        "user.Confidence".into()
+    }
+
+    fn arrow_datatype() -> arrow2::datatypes::DataType {
+        Float32::arrow_datatype()
+    }
+
+    fn try_to_arrow_opt<'a>(
+        data: impl IntoIterator<Item = Option<impl Into<std::borrow::Cow<'a, Self>>>>,
+    ) -> re_types::SerializationResult<Box<dyn arrow2::array::Array>>
+    where
+        Self: 'a,
+    {
+        Float32::try_to_arrow_opt(data.into_iter().map(|opt| opt.map(Into::into).map(|c| c.0)))
+    }
+}
+
+// ---
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (rec, storage) = RecordingStreamBuilder::new("rerun_example_custom_data").memory()?;
+
+    MsgSender::from_archetype(
+        "left/my_confident_point_cloud",
+        &CustomPoints3D {
+            points3d: Points3D::new(grid(glam::Vec3::splat(-10.0), glam::Vec3::splat(10.0), 10)),
+            confidences: Some(vec![42f32.into()]),
+        },
+    )?
+    .send(&rec)?;
+
+    MsgSender::from_archetype(
+        "right/my_polarized_point_cloud",
+        &CustomPoints3D {
+            points3d: Points3D::new(grid(glam::Vec3::splat(-10.0), glam::Vec3::splat(10.0), 10)),
+            confidences: Some((0..1000).map(|i| i as f32).map(Into::into).collect()),
+        },
+    )?
+    .send(&rec)?;
+
+    rerun::native_viewer::show(storage.take())?;
+    Ok(())
+}
