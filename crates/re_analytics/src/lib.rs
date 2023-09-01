717a2a0809b433d25a1799baaf9315479d6c1799
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-09-01 11:59:59 +0200

    move posthog events into their own module

--- crates/re_analytics/src/lib.rs
+++ crates/re_analytics/src/lib.rs
@@ -25,6 +25,9 @@ use web::{Pipeline, PipelineError};
 #[cfg(not(target_arch = "wasm32"))]
 pub mod cli;
 
+mod event;
+use event::{PostHogBatch, PostHogEvent};
+
 // ----------------------------------------------------------------------------
 
 use std::borrow::Cow;
@@ -306,90 +309,6 @@ impl Analytics {
             }
 
             pipeline.record(event);
-        }
-    }
-}
-
-/// The "public" API key can be obtained at <https://eu.posthog.com/project/settings#project-api-key>.
-/// Make sure you are logged in to the right organization and have the correct project open.
-/// Unfortunately that stuff is client-side routed, and there's no way to link directly to the right place.
-const PUBLIC_POSTHOG_PROJECT_KEY: &str = "phc_sgKidIE4WYYFSJHd8LEYY1UZqASpnfQKeMqlJfSXwqg";
-
-#[derive(Debug, serde::Serialize)]
-#[serde(untagged)]
-enum PostHogEvent<'a> {
-    Capture(PostHogCaptureEvent<'a>),
-    Identify(PostHogIdentifyEvent<'a>),
-}
-
-impl<'a> PostHogEvent<'a> {
-    fn from_event(analytics_id: &'a str, session_id: &'a str, event: &'a Event) -> Self {
-        let properties = event.props.iter().map(|(name, value)| {
-            (
-                name.as_ref(),
-                match value {
-                    &Property::Integer(v) => v.into(),
-                    &Property::Float(v) => v.into(),
-                    Property::String(v) => v.as_str().into(),
-                    &Property::Bool(v) => v.into(),
-                },
-            )
-        });
-
-        match event.kind {
-            crate::EventKind::Append => Self::Capture(PostHogCaptureEvent {
-                timestamp: event.time_utc,
-                event: event.name.as_ref(),
-                distinct_id: analytics_id,
-                properties: properties
-                    .chain([("session_id", session_id.into())])
-                    .collect(),
-            }),
-            crate::EventKind::Update => Self::Identify(PostHogIdentifyEvent {
-                timestamp: event.time_utc,
-                event: "$identify",
-                distinct_id: analytics_id,
-                properties: [("session_id", session_id.into())].into(),
-                set: properties.collect(),
-            }),
-        }
-    }
-}
-
-// See https://posthog.com/docs/api/post-only-endpoints#capture.
-#[derive(Debug, serde::Serialize)]
-struct PostHogCaptureEvent<'a> {
-    #[serde(with = "::time::serde::rfc3339")]
-    timestamp: OffsetDateTime,
-    event: &'a str,
-    distinct_id: &'a str,
-    properties: HashMap<&'a str, serde_json::Value>,
-}
-
-// See https://posthog.com/docs/api/post-only-endpoints#identify.
-#[derive(Debug, serde::Serialize)]
-struct PostHogIdentifyEvent<'a> {
-    #[serde(with = "::time::serde::rfc3339")]
-    timestamp: OffsetDateTime,
-    event: &'a str,
-    distinct_id: &'a str,
-    properties: HashMap<&'a str, serde_json::Value>,
-    #[serde(rename = "$set")]
-    set: HashMap<&'a str, serde_json::Value>,
-}
-
-// See https://posthog.com/docs/api/post-only-endpoints#batch-events.
-#[derive(Debug, serde::Serialize)]
-struct PostHogBatch<'a> {
-    api_key: &'static str,
-    batch: &'a [PostHogEvent<'a>],
-}
-
-impl<'a> PostHogBatch<'a> {
-    fn from_events(events: &'a [PostHogEvent<'a>]) -> Self {
-        Self {
-            api_key: PUBLIC_POSTHOG_PROJECT_KEY,
-            batch: events,
         }
     }
 }
