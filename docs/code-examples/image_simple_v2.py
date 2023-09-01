f29498af56b652a0988250a10095f4d810645ccf
Jeremy Leibs <jeremy@rerun.io>
2023-09-02 01:54:11 +0200

    Code-example for image API

---
+++ docs/code-examples/image_simple_v2.py
@@ -0,0 +1,14 @@
+"""Create and log an image."""
+
+import numpy as np
+import rerun as rr
+import rerun.experimental as rr2
+
+# Create an image with Pillow
+image = np.zeros((200, 300, 3), dtype=np.uint8)
+image[:, :, 0] = 255
+image[50:150, 50:150] = (0, 255, 0)
+
+rr.init("rerun_example_images", spawn=True)
+
+rr2.log("simple", rr2.Image(image))
