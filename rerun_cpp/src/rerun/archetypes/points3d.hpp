cf8c5132aec865a0e50a5b7e415ad62e0bcf1374
Clement Rey <cr.rey.clement@gmail.com>
2023-09-05 11:26:58 +0200

    codegen

--- rerun_cpp/src/rerun/archetypes/points3d.hpp
+++ rerun_cpp/src/rerun/archetypes/points3d.hpp
@@ -33,10 +33,10 @@ namespace rerun {
         /// namespace rr = rerun;
         ///
         /// int main() {
-        ///    auto rr_stream = rr::RecordingStream("rerun_example_points3d_simple");
-        ///    rr_stream.connect("127.0.0.1:9876").throw_on_failure();
+        ///    auto rec = rr::RecordingStream("rerun_example_points3d_simple");
+        ///    rec.connect("127.0.0.1:9876").throw_on_failure();
         ///
-        ///    rr_stream.log("points", rr::Points3D({{0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}}));
+        ///    rec.log("points", rr::Points3D({{0.0f, 0.0f, 0.0f}, {1.0f, 1.0f, 1.0f}}));
         /// }
         ///```
         struct Points3D {
