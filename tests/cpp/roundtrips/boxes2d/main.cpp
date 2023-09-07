d7fe824e3300cd742ecfb6559e38815ecd956651
Andreas Reich <r_andreas2@web.de>
2023-09-07 12:05:41 +0100

    box2d roundtrip tests
    fix roundtrip tests
    
    rename roundtrip tests from box to boxes

---
+++ tests/cpp/roundtrips/boxes2d/main.cpp
@@ -0,0 +1,20 @@
+#include <rerun/archetypes/boxes2d.hpp>
+#include <rerun/recording_stream.hpp>
+
+namespace rr = rerun;
+
+int main(int argc, char** argv) {
+    auto rec = rr::RecordingStream("rerun_example_roundtrip_box2d");
+    rec.save(argv[1]).throw_on_failure();
+
+    rec.log(
+        "boxes2d",
+        rr::archetypes::Boxes2D({{10.f, 9.f}, {5.f, -5.f}})
+            .with_origins({{0.f, 0.f}, {-1.f, 1.f}})
+            .with_colors({0xAA0000CC, 0x00BB00DD})
+            .with_labels({"hello", "friend"})
+            .with_draw_order(300.0)
+            .with_class_ids({126, 127})
+            .with_instance_keys({66, 666})
+    );
+}
---
+++ tests/rust/roundtrips/boxes2d/src/main.rs
@@ -0,0 +1,41 @@
+//! Logs a `Box2D` archetype for roundtrip checks.
+
+use rerun::{archetypes::Boxes2D, external::re_log, RecordingStream};
+
+#[derive(Debug, clap::Parser)]
+#[clap(author, version, about)]
+struct Args {
+    #[command(flatten)]
+    rerun: rerun::clap::RerunArgs,
+}
+
+fn run(rec: &RecordingStream, _args: &Args) -> anyhow::Result<()> {
+    rec.log(
+        "boxes2d",
+        &Boxes2D::new([[10., 9.], [5., -5.]])
+            .with_origins([[0., 0.], [-1., 1.]])
+            .with_colors([0xAA0000CC, 0x00BB00DD])
+            .with_labels(["hello", "friend"])
+            .with_draw_order(300.0)
+            .with_class_ids([126, 127])
+            .with_instance_keys([66, 666]),
+    )?;
+
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    re_log::setup_native_logging();
+
+    use clap::Parser as _;
+    let args = Args::parse();
+
+    let default_enabled = true;
+    args.rerun.clone().run(
+        "rerun_example_roundtrip_box2d",
+        default_enabled,
+        move |rec| {
+            run(&rec, &args).unwrap();
+        },
+    )
+}
