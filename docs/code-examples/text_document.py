eced5df69b6d841247fea5b249c245588cec5026
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-09-07 11:34:40 +0200

    less commas

--- docs/code-examples/text_document.py
+++ docs/code-examples/text_document.py
@@ -6,9 +6,4 @@ import rerun.experimental as rr2
 
 rr.init("rerun_example_text_document", spawn=True)
 
-rr2.log(
-    "text_document",
-    rr2.TextDocument(
-        body="Hello, TextDocument!",
-    ),
-)
+rr2.log("text_document", rr2.TextDocument(body="Hello, TextDocument!"))
--- tests/python/roundtrips/text_document/main.py
+++ tests/python/roundtrips/text_document/main.py
@@ -17,12 +17,7 @@ def main() -> None:
 
     rr.script_setup(args, "rerun_example_roundtrip_text_document")
 
-    rr2.log(
-        "text_document",
-        rr2.TextDocument(
-            body="Hello, TextDocument!",
-        ),
-    )
+    rr2.log("text_document", rr2.TextDocument(body="Hello, TextDocument!"))
 
     rr.script_teardown(args)
 
