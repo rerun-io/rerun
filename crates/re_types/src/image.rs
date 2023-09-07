fa11eecfd3d8eae4d09012927ac10830117a7640
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-09-07 14:01:07 +0200

    Clean up and unit-test find_non_empty_dim_indices

--- crates/re_types/src/image.rs
+++ crates/re_types/src/image.rs
@@ -1,4 +1,4 @@
-use smallvec::SmallVec;
+use smallvec::{smallvec, SmallVec};
 
 use crate::datatypes::{TensorData, TensorDimension};
 
@@ -11,43 +11,91 @@ pub enum ImageConstructionError<T: TryInto<TensorData>> {
     BadImageShape(Vec<TensorDimension>),
 }
 
-// Returns the indices of an appropriate set of non-empty dimensions
+/// Returns the indices of an appropriate set of dimensions.
+///
+/// Ignores leading and trailing 1-sized dimensions.
+///
+/// For instance: `[1, 640, 480, 3, 1]` would return `[1, 2, 3]`,
+/// the indices of the `[640, 480, 3]` dimensions.
 pub fn find_non_empty_dim_indices(shape: &Vec<TensorDimension>) -> SmallVec<[usize; 4]> {
-    if shape.len() < 2 {
-        return SmallVec::<_>::new();
+    match shape.len() {
+        0 => return smallvec![],
+        1 => return smallvec![0],
+        2 => return smallvec![0, 1],
+        _ => {}
     }
 
-    let mut iter_non_empty =
+    // Find a range of non-unit dimensions.
+    // [1, 1, 1, 640, 480, 3, 1, 1, 1]
+    //           ^---------^   goal range
+
+    let mut non_unit_indices =
         shape
             .iter()
             .enumerate()
             .filter_map(|(ind, dim)| if dim.size != 1 { Some(ind) } else { None });
 
-    // 0 must be valid since shape isn't empty or we would have returned an Err above
-    let mut first_non_empty = iter_non_empty.next().unwrap_or(0);
-    let mut last_non_empty = iter_non_empty.last().unwrap_or(first_non_empty);
+    // 0 is always a valid index.
+    let mut min = non_unit_indices.next().unwrap_or(0);
+    let mut max = non_unit_indices.last().unwrap_or(min);
 
     // Note, these are inclusive ranges.
 
     // First, empty inner dimensions are more likely to be intentional than empty outer dimensions.
     // Grow to a min-size of 2.
     // (1x1x3x1) -> 3x1 mono rather than 1x1x3 RGB
-    while last_non_empty - first_non_empty < 1 && last_non_empty < (shape.len() - 1) {
-        last_non_empty += 1;
+    while max == min && max + 1 < shape.len() {
+        max += 1;
     }
 
     // Next, consider empty outer dimensions if we still need them.
     // Grow up to 3 if the inner dimension is already 3 or 4 (Color Images)
     // Otherwise, only grow up to 2.
     // (1x1x3) -> 1x1x3 rgb rather than 1x3 mono
-    let target = match shape[last_non_empty].size {
-        3 | 4 => 2,
-        _ => 1,
+    let target_len = match shape[max].size {
+        3 | 4 => 3,
+        _ => 2,
     };
 
-    while last_non_empty - first_non_empty < target && first_non_empty > 0 {
-        first_non_empty -= 1;
+    while max - min + 1 < target_len && 0 < min {
+        min -= 1;
     }
 
-    (first_non_empty..=last_non_empty).collect()
+    (min..=max).collect()
+}
+
+#[test]
+fn test_find_non_empty_dim_indices() {
+    fn expect(shape: &[u64], expected: &[usize]) {
+        let dim = shape
+            .iter()
+            .map(|s| TensorDimension {
+                size: *s,
+                name: None,
+            })
+            .collect();
+        let got = find_non_empty_dim_indices(&dim);
+        assert!(
+            got.as_slice() == expected,
+            "Input: {shape:?}, got {got:?}, expected {expected:?}"
+        );
+    }
+
+    expect(&[], &[]);
+    expect(&[0], &[0]);
+    expect(&[1], &[0]);
+    expect(&[100], &[0]);
+
+    expect(&[640, 480], &[0, 1]);
+    expect(&[640, 480, 1], &[0, 1]);
+    expect(&[640, 480, 1, 1], &[0, 1]);
+    expect(&[640, 480, 3], &[0, 1, 2]);
+    expect(&[1, 640, 480], &[1, 2]);
+    expect(&[1, 640, 480, 3, 1], &[1, 2, 3]);
+    expect(&[1, 3, 640, 480, 1], &[1, 2, 3]);
+    expect(&[1, 1, 640, 480], &[2, 3]);
+    expect(&[1, 1, 640, 480, 1, 1], &[2, 3]);
+
+    expect(&[1, 1, 3], &[0, 1, 2]);
+    expect(&[1, 1, 3, 1], &[2, 3]);
 }
--- rerun_py/rerun_sdk/rerun/_rerun2/_validators.py
+++ rerun_py/rerun_sdk/rerun/_rerun2/_validators.py
@@ -1,36 +1,65 @@
 from __future__ import annotations
 
 
-# This code follows closely from `image_ext.rs`
+# This code is a straight port from Rust.
 def find_non_empty_dim_indices(shape: list[int]) -> list[int]:
     """Returns the indices of an appropriate set of non-empty dimensions."""
-    if len(shape) < 2:
+    if len(shape) <= 2:
         return list(range(len(shape)))
 
-    indices = list(d[0] for d in filter(lambda d: d[1] != 1, enumerate(shape)))
+    # Find a range of non-unit dimensions.
+    # [1, 1, 1, 640, 480, 3, 1, 1, 1]
+    #           ^---------^   goal range
 
-    # 0 must be valid since shape isn't empty or we would have returned an Err above
-    first_non_empty = next(iter(indices), 0)
-    last_non_empty = next(reversed(indices), first_non_empty)
+    non_unit_indices = list(d[0] for d in filter(lambda d: d[1] != 1, enumerate(shape)))
+
+    # 0 is always a valid index.
+    min = next(iter(non_unit_indices), 0)
+    max = next(reversed(non_unit_indices), min)
 
     # Note, these are inclusive ranges
 
     # First, empty inner dimensions are more likely to be intentional than empty outer dimensions.
     # Grow to a min-size of 2.
     # (1x1x3x1) -> 3x1 mono rather than 1x1x3 RGB
-    while (last_non_empty - first_non_empty) < 1 and last_non_empty < (len(shape) - 1):
-        print(f"{last_non_empty} {first_non_empty} {len(shape)}")
-        last_non_empty += 1
+    while max - min < 1 and max + 1 < len(shape):
+        print(f"{max} {min} {len(shape)}")
+        max += 1
 
-    target = 1
-    if shape[last_non_empty] in (3, 4):
-        target = 2
+    target_len = 2
+    if shape[max] in (3, 4):
+        target_len = 3
 
     # Next, consider empty outer dimensions if we still need them.
     # Grow up to 3 if the inner dimension is already 3 or 4 (Color Images)
     # Otherwise, only grow up to 2.
     # (1x1x3) -> 1x1x3 rgb rather than 1x3 mono
-    while (last_non_empty - first_non_empty) < target and first_non_empty > 0:
-        first_non_empty -= 1
+    while max - min + 1 < target_len and 0 < min:
+        min -= 1
 
-    return list(range(first_non_empty, last_non_empty + 1))
+    return list(range(min, max + 1))
+
+
+if __name__ == "__main__":
+    # Unit-test
+    def expect(input: list[int], expected: list[int]) -> None:
+        got = find_non_empty_dim_indices(input)
+        assert got == expected, f"input: {input}, expected {expected}, got {got}"
+
+    expect([], [])
+    expect([0], [0])
+    expect([1], [0])
+    expect([100], [0])
+
+    expect([640, 480], [0, 1])
+    expect([640, 480, 1], [0, 1])
+    expect([640, 480, 1, 1], [0, 1])
+    expect([640, 480, 3], [0, 1, 2])
+    expect([1, 640, 480], [1, 2])
+    expect([1, 640, 480, 3, 1], [1, 2, 3])
+    expect([1, 3, 640, 480, 1], [1, 2, 3])
+    expect([1, 1, 640, 480], [2, 3])
+    expect([1, 1, 640, 480, 1, 1], [2, 3])
+
+    expect([1, 1, 3], [0, 1, 2])
+    expect([1, 1, 3, 1], [2, 3])
