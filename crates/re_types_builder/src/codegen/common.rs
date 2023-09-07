12d19e57ebd42851553be4868ec18aeabbd1100b
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-09-07 11:38:55 +0200

    Fix typo in remove_old_files_from_folder

--- crates/re_types_builder/src/codegen/common.rs
+++ crates/re_types_builder/src/codegen/common.rs
@@ -82,7 +82,7 @@ pub fn remove_old_files_from_folder(folder_path: Utf8PathBuf, filepaths: &BTreeS
         if let Some(stem) = filepath.as_str().strip_suffix("_ext.cpp") {
             let generated_hpp_path = Utf8PathBuf::try_from(format!("{stem}.hpp")).unwrap();
             assert!(
-                filepath.exists(),
+                generated_hpp_path.exists(),
                 "Found orphaned {filepath} with no matching {generated_hpp_path}"
             );
             continue;
