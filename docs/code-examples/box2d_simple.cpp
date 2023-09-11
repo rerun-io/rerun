0a1713e36dc58ddae28143cc84feecfdee0b750b
Andreas Reich <r_andreas2@web.de>
2023-09-11 17:48:13 +0200

    C++ boxes2d extension & example

--- docs/code-examples/box2d_simple.cpp
+++ docs/code-examples/box2d_simple.cpp
@@ -1,1 +1,15 @@
-// TODO:
+// Log some simple 2D boxes.
+
+#include <rerun.hpp>
+
+namespace rr = rerun;
+
+int main() {
+    auto rec = rr::RecordingStream("rerun_example_rect2d");
+    rec.connect("127.0.0.1:9876").throw_on_failure();
+
+    rec.log("simple", rr::Boxes2D::from_xywh({{-1.f, -1.f}}, {{2.f, 2.f}}));
+
+    // Log an extra rect to set the view bounds
+    rec.log("bounds", rr::Boxes2D({2.f, 1.5f}));
+}
---
+++ rerun_cpp/src/rerun/archetypes/boxes2d_ext.cpp
@@ -0,0 +1,46 @@
+#include "boxes2d.hpp"
+
+// #define EDIT_EXTENSION
+
+namespace rerun {
+    namespace archetypes {
+
+#ifdef EDIT_EXTENSION
+        // [CODEGEN COPY TO HEADER START]
+
+        /// Creates new `Boxes2D` with `half_extents`] and [`Self::centers`] created
+        /// from minimum X/Y and width + height.
+        ///
+        /// Does *not* preserve data as-is and instead creates centers and half-extents from the
+        /// input data.
+        static Boxes2D from_xywh(
+            const std::vector<datatypes::Vec2D>& xy, const std::vector<datatypes::Vec2D>& extents
+        );
+
+        // [CODEGEN COPY TO HEADER END]
+#endif
+
+        Boxes2D Boxes2D::from_xywh(
+            const std::vector<datatypes::Vec2D>& xy, const std::vector<datatypes::Vec2D>& extents
+        ) {
+            std::vector<components::HalfExtents2D> half_extents;
+            half_extents.reserve(extents.size());
+            for (const auto& wh : extents) {
+                half_extents.emplace_back(wh.x() / 2.0, wh.y() / 2.0);
+            }
+
+            auto num_centers = std::min(xy.size(), half_extents.size());
+            std::vector<components::Origin2D> centers;
+            centers.reserve(num_centers);
+            for (size_t i = 0; i < num_centers; ++i) {
+                centers.emplace_back(
+                    xy[i].x() + half_extents[i].x(),
+                    xy[i].y() + half_extents[i].y()
+                );
+            }
+
+            return Boxes2D(half_extents).with_centers(centers);
+        }
+
+    } // namespace archetypes
+} // namespace rerun
