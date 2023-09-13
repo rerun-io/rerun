91fad3bda43034dc863d1ddac52c7540c959b7a6
Andreas Reich <r_andreas2@web.de>
2023-09-13 16:58:42 +0200

    roundtrip tests
    nicer looking roundtrip tests

---
+++ tests/cpp/roundtrips/boxes3d/main.cpp
@@ -0,0 +1,28 @@
+
+#include <rerun/archetypes/boxes3d.hpp>
+#include <rerun/recording_stream.hpp>
+
+namespace rr = rerun;
+
+int main(int argc, char** argv) {
+    auto rec = rr::RecordingStream("rerun_example_roundtrip_box3d");
+    rec.save(argv[1]).throw_on_failure();
+
+    rec.log(
+        "boxes3d",
+        rr::archetypes::Boxes3D::from_half_sizes({{10.f, 9.f, 8.f}, {5.f, -5.f, 5.f}})
+            .with_centers({{0.f, 0.f, 0.f}, {-1.f, 1.f, -2.f}})
+            .with_rotations({
+                rr::datatypes::Quaternion(0.f, 1.f, 2.f, 3.f),
+                rr::datatypes::RotationAxisAngle(
+                    {0.f, 1.f, 2.f},
+                    rerun::datatypes::Angle::degrees(45.f)
+                ),
+            })
+            .with_colors({0xAA0000CC, 0x00BB00DD})
+            .with_labels({"hello", "friend"})
+            .with_radii({0.1f, 0.01f})
+            .with_class_ids({126, 127})
+            .with_instance_keys({66, 666})
+    );
+}
