348e6bca5ff7bd5198ce0b4488639d9442de2e0c
Jeremy Leibs <jeremy@rerun.io>
2023-09-13 17:57:11 +0200

    Migrate InstanceKey

--- rerun_py/rerun_sdk/rerun/_rerun2/components/_overrides/__init__.py
+++ rerun_py/rerun_sdk/rerun/_rerun2/components/_overrides/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from .instance_key import instance_key__native_to_pa_array_override
 from .line_strip2d import line_strip2d__native_to_pa_array_override
 from .line_strip3d import line_strip3d__native_to_pa_array_override
 from .radius import radius__native_to_pa_array_override
---
+++ rerun_py/rerun_sdk/rerun/_rerun2/components/instance_key_ext.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+import pyarrow as pa
+
+if TYPE_CHECKING:
+    from . import InstanceKeyArrayLike
+
+
+class InstanceKeyExt:
+    @staticmethod
+    def native_to_pa_array_override(data: InstanceKeyArrayLike, data_type: pa.DataType) -> pa.Array:
+        array = np.asarray(data, dtype=np.uint64).flatten()
+        return pa.array(array, type=data_type)
