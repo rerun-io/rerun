08b3ab803bd4a4aa81ff1a8d7a33e7f551a73711
Jeremy Leibs <jeremy@rerun.io>
2023-09-13 17:56:02 +0200

    Migrate DrawOrder

--- rerun_py/rerun_sdk/rerun/_rerun2/components/_overrides/__init__.py
+++ rerun_py/rerun_sdk/rerun/_rerun2/components/_overrides/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from .draw_order import draw_order__native_to_pa_array_override
 from .instance_key import instance_key__native_to_pa_array_override
 from .line_strip2d import line_strip2d__native_to_pa_array_override
 from .line_strip3d import line_strip3d__native_to_pa_array_override
