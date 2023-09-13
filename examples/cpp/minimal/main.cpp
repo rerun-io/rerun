69eb3a954df7f1186df8e0d03079b61f7c3da2f9
Andreas Reich <r_andreas2@web.de>
2023-09-13 13:26:03 +0200

    cpp example fixes

--- examples/cpp/minimal/main.cpp
+++ examples/cpp/minimal/main.cpp
@@ -34,7 +34,10 @@ int main(int argc, char** argv) {
     rr_stream.log_component_batches(
         "2d/points",
         3,
-        std::vector{rrc::Point2D(0.0f, 0.0f), rrc::Point2D(1.0f, 3.0f), rrc::Point2D(5.0f, 5.0f)},
+        std::vector{
+            rrc::Position2D(0.0f, 0.0f),
+            rrc::Position2D(1.0f, 3.0f),
+            rrc::Position2D(5.0f, 5.0f)},
         std::array{rrc::Color(0xFF0000FF), rrc::Color(0x00FF00FF), rrc::Color(0x0000FFFF)},
         c_style_array
     );
