c7a029eff8d07660dbe97b9615b7d767c5ec69b1
Andreas Reich <r_andreas2@web.de>
2023-09-18 16:22:36 +0200

    box3d python extensions

---
+++ rerun_py/rerun_sdk/rerun/_rerun2/archetypes/boxes3d_ext.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import Any
+
+import numpy as np
+
+from ..datatypes import Vec3DArrayLike
+
+
+class Boxes3DExt:
+    def __init__(
+        self: Any, *, sizes: Vec3DArrayLike | None = None, mins: Vec3DArrayLike | None = None, **kwargs
+    ) -> None:
+        if sizes is not None:
+            if kwargs.get("half_sizes") is not None:
+                raise ValueError("Cannot specify both `sizes` and `half_sizes` at the same time.")
+
+            sizes = np.asarray(sizes, dtype=np.float32)
+            kwargs["half_sizes"] = sizes / 2.0
+
+        if mins is not None:
+            if kwargs.get("centers") is not None:
+                raise ValueError("Cannot specify both `mins` and `centers` at the same time.")
+
+            # already converted `sizes` to `half_sizes`
+            if kwargs.get("half_sizes") is None:
+                raise ValueError("Cannot specify `mins` without `sizes` or `half_sizes`.")
+
+            mins = np.asarray(mins, dtype=np.float32)
+            half_sizes = np.asarray(kwargs["half_sizes"], dtype=np.float32)
+            kwargs["centers"] = mins + half_sizes
+
+        self.__attrs_init__(**kwargs)
