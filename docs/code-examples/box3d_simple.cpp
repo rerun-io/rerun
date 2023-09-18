41cf1a96c201d4a1f55392cd378b613b316bd4c1
Andreas Reich <r_andreas2@web.de>
2023-09-18 21:02:34 +0200

    fix incorrectly named box3d entity in sample

--- docs/code-examples/box3d_simple.cpp
+++ docs/code-examples/box3d_simple.cpp
@@ -8,5 +8,5 @@ int main() {
     auto rec = rr::RecordingStream("rerun_example_box3d_simple");
     rec.connect("127.0.0.1:9876").throw_on_failure();
 
-    rec.log("bounds", rr::Boxes3D::from_half_sizes({{2.f, 2.f, 1.0f}}));
+    rec.log("simple", rr::Boxes3D::from_half_sizes({{2.f, 2.f, 1.0f}}));
 }
