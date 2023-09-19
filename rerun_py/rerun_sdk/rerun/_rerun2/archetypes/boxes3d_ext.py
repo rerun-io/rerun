787103e19aacc88a1431a621bd43a057e5953fad
Andreas Reich <r_andreas2@web.de>
2023-09-19 09:32:45 +0200

    more strongly typed init method on box2d/3d

--- rerun_py/rerun_sdk/rerun/_rerun2/archetypes/boxes3d_ext.py
+++ rerun_py/rerun_sdk/rerun/_rerun2/archetypes/boxes3d_ext.py
@@ -9,25 +9,31 @@ from ..datatypes import Vec3DArrayLike
 
 class Boxes3DExt:
     def __init__(
-        self: Any, *, sizes: Vec3DArrayLike | None = None, mins: Vec3DArrayLike | None = None, **kwargs: Any
+        self: Any,
+        *,
+        sizes: Vec3DArrayLike | None = None,
+        mins: Vec3DArrayLike | None = None,
+        centers: Vec3DArrayLike | None = None,
+        half_sizes: Vec3DArrayLike | None = None,
+        **kwargs: Any,
     ) -> None:
         if sizes is not None:
-            if kwargs.get("half_sizes") is not None:
+            if half_sizes is not None:
                 raise ValueError("Cannot specify both `sizes` and `half_sizes` at the same time.")
 
             sizes = np.asarray(sizes, dtype=np.float32)
-            kwargs["half_sizes"] = sizes / 2.0
+            half_sizes = sizes / 2.0
 
         if mins is not None:
-            if kwargs.get("centers") is not None:
+            if centers is not None:
                 raise ValueError("Cannot specify both `mins` and `centers` at the same time.")
 
             # already converted `sizes` to `half_sizes`
-            if kwargs.get("half_sizes") is None:
+            if half_sizes is None:
                 raise ValueError("Cannot specify `mins` without `sizes` or `half_sizes`.")
 
             mins = np.asarray(mins, dtype=np.float32)
-            half_sizes = np.asarray(kwargs["half_sizes"], dtype=np.float32)
-            kwargs["centers"] = mins + half_sizes
+            half_sizes = np.asarray(half_sizes, dtype=np.float32)
+            centers = mins + half_sizes
 
-        self.__attrs_init__(**kwargs)
+        self.__attrs_init__(half_sizes=half_sizes, centers=centers, **kwargs)
