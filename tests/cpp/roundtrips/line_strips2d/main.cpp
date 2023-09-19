6bcc5ea22c7871a65c4e110dbb1fb51f5ba2755e
Andreas Reich <r_andreas2@web.de>
2023-09-19 10:51:36 +0200

    fix linestrips2d roundtrip

--- tests/cpp/roundtrips/line_strips2d/main.cpp
+++ tests/cpp/roundtrips/line_strips2d/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
     rec.save(argv[1]).throw_on_failure();
 
     rec.log(
-        "line_strips3d",
+        "line_strips2d",
         rr::archetypes::LineStrips2D({rr::components::LineStrip2D({{0.f, 0.f}, {2.f, 1.f}}),
                                       rr::components::LineStrip2D({{4.f, -1.f}, {6.f, 0.f}})})
             .with_radii({0.42f, 0.43f})
