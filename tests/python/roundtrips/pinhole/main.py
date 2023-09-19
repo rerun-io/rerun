63c31f68d7715a9794071c53f3714848c80bb6b1
Andreas Reich <r_andreas2@web.de>
2023-09-19 11:03:14 +0200

    roundtrip tests for pinhole

---
+++ tests/python/roundtrips/pinhole/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Logs a `Pinhole` archetype for roundtrip checks."""
+
+from __future__ import annotations
+
+import argparse
+
+import rerun as rr
+import rerun.experimental as rr2
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Logs rich data using the Rerun SDK.")
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+
+    rr.script_setup(args, "rerun_example_roundtrip_pinhole")
+
+    rr2.log(
+        "pinhole",
+        rr2.Pinhole(image_from_cam=[[3.0, 0.0, 1.5], [0.0, 3.0, 1.5], [0.0, 0.0, 1.0]], resolution=[3840, 2160]),
+    )
+
+    rr.script_teardown(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/rust/roundtrips/pinhole/src/main.rs
@@ -0,0 +1,36 @@
+//! Logs a `Pinhole` archetype for roundtrip checks.
+
+use rerun::{archetypes::Pinhole, datatypes::Mat3x3, external::re_log, RecordingStream};
+
+#[derive(Debug, clap::Parser)]
+#[clap(author, version, about)]
+struct Args {
+    #[command(flatten)]
+    rerun: rerun::clap::RerunArgs,
+}
+
+fn run(rec: &RecordingStream, _args: &Args) -> anyhow::Result<()> {
+    rec.log(
+        "pinhole",
+        &Pinhole::new(Mat3x3::from([[3., 0., 1.5], [0., 3., 1.5], [0., 0., 1.]]))
+            .with_resolution([3840., 2160.]),
+    )?;
+
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    re_log::setup_native_logging();
+
+    use clap::Parser as _;
+    let args = Args::parse();
+
+    let default_enabled = true;
+    args.rerun.clone().run(
+        "rerun_example_roundtrip_pinhole",
+        default_enabled,
+        move |rec| {
+            run(&rec, &args).unwrap();
+        },
+    )
+}
