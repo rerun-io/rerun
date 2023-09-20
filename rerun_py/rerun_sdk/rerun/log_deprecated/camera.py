4fdd18a76ad44dc17ff33e536d4781413b6cc53c
Andreas Reich <r_andreas2@web.de>
2023-09-20 19:35:05 +0200

    fix broken import

--- rerun_py/rerun_sdk/rerun/log_deprecated/camera.py
+++ rerun_py/rerun_sdk/rerun/log_deprecated/camera.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import numpy.typing as npt
 
 from rerun import bindings
-from rerun.log.log_decorator import log_decorator
+from rerun.log_deprecated.log_decorator import log_decorator
 from rerun.recording_stream import RecordingStream
 
 __all__ = [
