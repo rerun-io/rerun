1115a6701d7844f0ee08fa518ac6178956c78541
Andreas Reich <r_andreas2@web.de>
2023-09-21 16:36:37 +0200

    slightly adjust scalar_simple example

--- docs/code-examples/scalar_simple.py
+++ docs/code-examples/scalar_simple.py
@@ -6,6 +6,6 @@ import rerun.experimental as rr2
 
 rr.init("rerun_example_scalar", spawn=True)
 
-for step in range(0, 100):
+for step in range(0, 64):
     rr.set_time_sequence("step", step)
-    rr2.log("scalar", rr2.TimeSeriesScalar(math.sin(step / 20.0)))
+    rr2.log("scalar", rr2.TimeSeriesScalar(math.sin(step / 10.0)))
--- docs/code-examples/scalar_simple.rs
+++ docs/code-examples/scalar_simple.rs
@@ -5,9 +5,9 @@ use rerun::{archetypes::TimeSeriesScalar, RecordingStreamBuilder};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (rec, storage) = RecordingStreamBuilder::new("rerun_example_scalar").memory()?;
 
-    for step in 0..100 {
+    for step in 0..64 {
         rec.set_time_sequence("step", step);
-        rec.log("scalar", &TimeSeriesScalar::new((step as f64 / 20.0).sin()))?;
+        rec.log("scalar", &TimeSeriesScalar::new((step as f64 / 10.0).sin()))?;
     }
 
     rerun::native_viewer::show(storage.take())?;
