af1e7b9098b2cac1904b2b751befe8b170d700df
Andreas Reich <r_andreas2@web.de>
2023-09-21 19:48:13 +0200

    Generate `From` for trivial component & datatypes.
    And remove a bunch of trivial `new` for tuple types.

--- crates/re_types/src/datatypes/class_id_ext.rs
+++ crates/re_types/src/datatypes/class_id_ext.rs
@@ -1,12 +1,5 @@
 use super::ClassId;
 
-impl From<u16> for ClassId {
-    #[inline]
-    fn from(value: u16) -> Self {
-        Self(value)
-    }
-}
-
 impl std::fmt::Display for ClassId {
     #[inline]
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
--- crates/re_types/src/datatypes/keypoint_id_ext.rs
+++ crates/re_types/src/datatypes/keypoint_id_ext.rs
@@ -1,12 +1,5 @@
 use super::KeypointId;
 
-impl From<u16> for KeypointId {
-    #[inline]
-    fn from(value: u16) -> Self {
-        Self(value)
-    }
-}
-
 impl std::fmt::Display for KeypointId {
     #[inline]
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
