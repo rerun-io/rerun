b9bccc15a6741ad491a945020ae1dca5a9e8adfd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-09-21 15:29:00 +0200

    Add markdown as code-example

--- docs/code-examples/text_document.py
+++ docs/code-examples/text_document.py
@@ -6,11 +6,49 @@ import rerun.experimental as rr2
 
 rr.init("rerun_example_text_document", spawn=True)
 
-rr2.log("text_document", rr2.TextDocument(body="Hello, TextDocument!"))
+rr2.log("text_document", rr2.TextDocument("Hello, TextDocument!"))
+
 rr2.log(
     "markdown",
     rr2.TextDocument(
-        body="# Hello\nMarkdown with `code`!\n\nA random image:\n\n![A random image](https://picsum.photos/640/480)",
-        media_type=rr2.cmp.MediaType.markdown(),
+        """
+# Hello Markdown!
+[Click here to see the raw text](recording://markdown.Text).
+
+Basic formatting:
+
+| **Feature**       | **Alternative** |
+| ----------------- | --------------- |
+| Plain             |                 |
+| *italics*         | _italics_       |
+| **bold**          | __bold__        |
+| ~~strikethrough~~ |                 |
+| `inline code`     |                 |
+
+----------------------------------
+
+Some code:
+```rs
+fn main() {
+    println!("Hello, world!");
+}
+```
+
+## Support
+- [x] [Commonmark](https://commonmark.org/help/) support
+- [x] GitHub-style strikethrough, tables, and checkboxes
+- [ ] Syntax highlighting
+
+## Links
+You can link to [an entity](recording://markdown),
+a [specific instance of an entity](recording://markdown[#0]),
+or a [specific component](recording://markdown.Text).
+
+Of course you can also have [normal https links](https://github.com/rerun-io/rerun), e.g. <https://rerun.io>.
+
+## Image
+![A random image](https://picsum.photos/640/480)
+""".strip(),
+        media_type="text/markdown",
     ),
 )
--- docs/code-examples/text_document.rs
+++ docs/code-examples/text_document.rs
@@ -8,15 +8,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (rec, storage) = RecordingStreamBuilder::new("rerun_example_text_document").memory()?;
 
     rec.log("text_document", &TextDocument::new("Hello, TextDocument!"))?;
+
     rec.log(
         "markdown",
         &TextDocument::new(
-            "# Hello\n\
-             Markdown with `code`!\n\
-             \n\
-             A random image:\n\
-             \n\
-             ![A random image](https://picsum.photos/640/480)",
+            r#"
+# Hello Markdown!
+[Click here to see the raw text](recording://markdown.Text).
+
+Basic formatting:
+
+| **Feature**       | **Alternative** |
+| ----------------- | --------------- |
+| Plain             |                 |
+| *italics*         | _italics_       |
+| **bold**          | __bold__        |
+| ~~strikethrough~~ |                 |
+| `inline code`     |                 |
+
+----------------------------------
+
+Some code:
+```rs
+fn main() {
+    println!("Hello, world!");
+}
+```
+
+## Support
+- [x] [Commonmark](https://commonmark.org/help/) support
+- [x] GitHub-style strikethrough, tables, and checkboxes
+- [ ] Syntax highlighting
+
+## Links
+You can link to [an entity](recording://markdown),
+a [specific instance of an entity](recording://markdown[#0]),
+or a [specific component](recording://markdown.Text).
+
+Of course you can also have [normal https links](https://github.com/rerun-io/rerun), e.g. <https://rerun.io>.
+
+## Image
+![A random image](https://picsum.photos/640/480)
+"#.trim(),
         )
         .with_media_type(MediaType::markdown()),
     )?;
