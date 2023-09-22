4b9e246dc0f86bcc467e448eae55576d592bc23e
Andreas Reich <r_andreas2@web.de>
2023-09-22 11:10:26 +0200

    scalar display no longer hardcodes precision

--- crates/re_types/src/components/scalar_ext.rs
+++ crates/re_types/src/components/scalar_ext.rs
@@ -2,6 +2,6 @@ use super::Scalar;
 
 impl std::fmt::Display for Scalar {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:.prec$}", self.0, prec = crate::DISPLAY_PRECISION)
+        self.0.fmt(f)
     }
 }
