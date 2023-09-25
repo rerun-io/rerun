17f39d366f72008b2605f46892b05c5ba871e276
Andreas Reich <andreas@rerun.io>
2023-09-25 14:04:47 +0200

    Fix Asset3D transform (#3439)
    ### What
    
    * Fixes #3426
    
    first commit is extended cleanup (+ fixing ignoring skew on Asset3d,
    which isn't relevant for this bugfix), second is the actual fix.
    
    
    
    Did an animated version of the submitted repro case because I couldn't
    see what's going on at first 😄
    
    
    https://github.com/rerun-io/rerun/assets/1220815/34796f06-ead2-4434-a35a-5e7e007944a4
    (this is after the fix)
    
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/3439) (if
    applicable)
    
    - [PR Build Summary](https://build.rerun.io/pr/3439)
    - [Docs
    preview](https://rerun.io/preview/14d00a078d15e8f518bcc1934b3e9a950e9ec544/docs)
    <!--DOCS-PREVIEW-->
    - [Examples
    preview](https://rerun.io/preview/14d00a078d15e8f518bcc1934b3e9a950e9ec544/examples)
    <!--EXAMPLES-PREVIEW-->
    - [Recent benchmark results](https://ref.rerun.io/dev/bench/)
    - [Wasm size tracking](https://ref.rerun.io/dev/sizes/)

--- crates/re_space_view_spatial/src/parts/meshes.rs
+++ crates/re_space_view_spatial/src/parts/meshes.rs
@@ -101,22 +101,23 @@ impl Mesh3DPart {
         if let Some(mesh) = mesh {
             re_tracing::profile_scope!("mesh instances");
 
-            instances.extend(
-                mesh.mesh_instances
-                    .iter()
-                    .map(move |mesh_instance| MeshInstance {
-                        gpu_mesh: mesh_instance.gpu_mesh.clone(),
-                        world_from_mesh: ent_context.world_from_obj * mesh_instance.world_from_mesh,
-                        outline_mask_ids,
-                        picking_layer_id: picking_layer_id_from_instance_path_hash(
-                            picking_instance_hash,
-                        ),
-                        ..Default::default()
-                    }),
-            );
+            instances.extend(mesh.mesh_instances.iter().map(move |mesh_instance| {
+                let entity_from_mesh = mesh_instance.world_from_mesh;
+                let world_from_mesh = ent_context.world_from_entity * entity_from_mesh;
+
+                MeshInstance {
+                    gpu_mesh: mesh_instance.gpu_mesh.clone(),
+                    world_from_mesh,
+                    outline_mask_ids,
+                    picking_layer_id: picking_layer_id_from_instance_path_hash(
+                        picking_instance_hash,
+                    ),
+                    ..Default::default()
+                }
+            }));
 
             self.0
-                .extend_bounding_box(*mesh.bbox(), ent_context.world_from_obj);
+                .extend_bounding_box(mesh.bbox(), ent_context.world_from_entity);
         };
 
         Ok(())
