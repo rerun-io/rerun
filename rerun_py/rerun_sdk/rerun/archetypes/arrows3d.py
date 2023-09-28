acde81ef1bd44685a0292d740a84ee535a619279
Andreas Reich <andreas@rerun.io>
2023-09-28 12:21:50 +0200

    Add python validators for vec2d/3d/4d shape mismatch (#3499)
    ### What
    
    * Fixes #3476
    
    Error now for
    ``` python
    rr.log("points", rr.Points2D(np.random.rand(101), colors=np.random.rand(100, 3)))
    ```
    ```
    Traceback (most recent call last):
      File "/Users/andreas/dev/rerun-io/rerun/../test.py", line 7, in <module>
        rr.log("points", rr.Points2D(np.random.rand(101), colors=np.random.rand(100, 3)))
                         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "<attrs generated init rerun.archetypes.points2d.Points2D>", line 3, in __init__
      File "/Users/andreas/dev/rerun-io/rerun/rerun_py/rerun_sdk/rerun/_baseclasses.py", line 188, in __init__
        pa_array = self._native_to_pa_array(data, self._ARROW_TYPE.storage_type)
                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/Users/andreas/dev/rerun-io/rerun/rerun_py/rerun_sdk/rerun/datatypes/vec2d.py", line 61, in _native_to_pa_array
        return Vec2DExt.native_to_pa_array_override(data, data_type)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/Users/andreas/dev/rerun-io/rerun/rerun_py/rerun_sdk/rerun/datatypes/vec2d_ext.py", line 30, in native_to_pa_array_override
        validate_vector_shape(points.shape, 2)
      File "/Users/andreas/dev/rerun-io/rerun/rerun_py/rerun_sdk/rerun/_validators.py", line 55, in validate_vector_shape
        raise ValueError(
    ValueError: Expected either flat array with a multiple of 2 elements or array with shape (`num_elements`, 2). Got instead (101,)
    ```
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/3499) (if
    applicable)
    
    - [PR Build Summary](https://build.rerun.io/pr/3499)
    - [Docs
    preview](https://rerun.io/preview/c73653d959c337441527867b02915cc573f9680e/docs)
    <!--DOCS-PREVIEW-->
    - [Examples
    preview](https://rerun.io/preview/c73653d959c337441527867b02915cc573f9680e/examples)
    <!--EXAMPLES-PREVIEW-->
    - [Recent benchmark results](https://ref.rerun.io/dev/bench/)
    - [Wasm size tracking](https://ref.rerun.io/dev/sizes/)

--- rerun_py/rerun_sdk/rerun/archetypes/arrows3d.py
+++ rerun_py/rerun_sdk/rerun/archetypes/arrows3d.py
@@ -31,7 +31,7 @@ class Arrows3D(Arrows3DExt, Archetype):
 
     lengths = np.log2(np.arange(0, 100) + 1)
     angles = np.arange(start=0, stop=tau, step=tau * 0.01)
-    origins = np.zeros((3, 100))
+    origins = np.zeros((100, 3))
     vectors = np.column_stack([np.sin(angles) * lengths, np.zeros(100), np.cos(angles) * lengths])
     colors = [[1.0 - c, c, 0.5, 0.5] for c in angles / tau]
 
