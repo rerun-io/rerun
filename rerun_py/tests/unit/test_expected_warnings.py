c6333b341415d52e0dccbb2e6f2b6db42f436f62
Jeremy Leibs <jeremy@rerun.io>
2023-09-29 15:28:05 +0200

    Add a comment

--- rerun_py/tests/unit/test_expected_warnings.py
+++ rerun_py/tests/unit/test_expected_warnings.py
@@ -13,6 +13,10 @@ def test_expected_warnings() -> None:
     # Always set strict mode to false in case it leaked from another test
     rr.set_strict_mode(False)
     with pytest.warns(RerunWarning) as warnings:
+        # Each of these calls will fail as they are executed and aggregate warnings into the single warnings recording.
+        # We then check that all the warnings were emitted in the expected order.
+        # If a log-call is expected to produce multiple warnings, add another tuple to the list that doesn't produce a warning,
+        # or else the offsets will get out of alignment.
         expected_warnings = [
             (
                 rr.log("points", rr.Points3D([1, 2, 3, 4, 5])),
