12ec562d365d5d41772185e8dbe152cffc2b0d28
Jeremy Leibs <jeremy@rerun.io>
2023-09-29 15:22:30 +0200

    Streamline writing multiple expectred warnings

--- rerun_py/tests/unit/test_expected_warnings.py
+++ rerun_py/tests/unit/test_expected_warnings.py
@@ -5,15 +5,25 @@ import rerun as rr
 from rerun.error_utils import RerunWarning
 
 rr.init("exceptions", spawn=False)
+# Make sure strict mode isn't leaking in from another context
 mem = rr.memory_recording()
 
 
-def test_points_warnings() -> None:
+def test_expected_warnings() -> None:
+    # Always set strict mode to false in case it leaked from another test
+    rr.set_strict_mode(False)
     with pytest.warns(RerunWarning) as warnings:
-        rr.log("points", rr.Points3D([1, 2, 3, 4, 5]))
+        expected_warnings = [
+            (
+                rr.log("points", rr.Points3D([1, 2, 3, 4, 5])),
+                "Expected either a flat array with a length multiple of 3 elements, or an array with shape (`num_elements`, 3). Shape of passed array was (5,).",
+            ),
+            (
+                rr.log("points", rr.Points2D([1, 2, 3, 4, 5])),
+                "Expected either a flat array with a length multiple of 2 elements, or an array with shape (`num_elements`, 2). Shape of passed array was (5,).",
+            ),
+        ]
 
-        assert len(warnings) == 1
-        assert (
-            "Expected either a flat array with a length multiple of 3 elements, or an array with shape (`num_elements`, 3). Shape of passed array was (5,)."
-            in str(warnings[0].message)
-        )
+        assert len(warnings) == len(expected_warnings)
+        for warning, (_, expected) in zip(warnings, expected_warnings):
+            assert expected in str(warning)
