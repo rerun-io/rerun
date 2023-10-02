74b5e946f3fcb016e94fbf62e5a4e128419b9604
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-10-02 10:39:54 +0200

    Show a PinholeProjection on just two lines, if possible

--- crates/re_data_ui/src/pinhole.rs
+++ crates/re_data_ui/src/pinhole.rs
@@ -11,6 +11,17 @@ impl DataUi for PinholeProjection {
         verbosity: UiVerbosity,
         query: &re_arrow_store::LatestAtQuery,
     ) {
+        if verbosity == UiVerbosity::Small {
+            // See if this is a trivial pinhole, and can be displayed as such:
+            let fl = self.focal_length_in_pixels();
+            let pp = self.principal_point();
+            if *self == Self::from_focal_length_and_principal_point(fl, pp) {
+                ui.label(format!("Focal length: {fl}\nPrincipal point: {pp}"))
+                    .on_hover_ui(|ui| self.data_ui(ctx, ui, UiVerbosity::Reduced, query));
+                return;
+            }
+        }
+
         self.0.data_ui(ctx, ui, verbosity, query);
     }
 }
