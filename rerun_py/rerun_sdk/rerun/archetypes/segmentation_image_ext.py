f8169032453b1df52aae1c730788be5081dce9e6
Jeremy Leibs <jeremy@rerun.io>
2023-10-03 00:28:13 +0200

    Look up the type codes

--- rerun_py/rerun_sdk/rerun/archetypes/segmentation_image_ext.py
+++ rerun_py/rerun_sdk/rerun/archetypes/segmentation_image_ext.py
@@ -5,9 +5,9 @@ from typing import TYPE_CHECKING
 import numpy as np
 import pyarrow as pa
 
-from rerun.datatypes.tensor_data_ext import _build_buffer_array
-
 from .._validators import find_non_empty_dim_indices
+from ..datatypes import TensorBufferType
+from ..datatypes.tensor_data_ext import _build_buffer_array
 from ..error_utils import _send_warning, catch_and_log_exceptions
 
 if TYPE_CHECKING:
@@ -16,6 +16,9 @@ if TYPE_CHECKING:
 
 
 class SegmentationImageExt:
+    U8_TYPE_ID = list(f.name for f in TensorBufferType().storage_type).index("U8")
+    U16_TYPE_ID = list(f.name for f in TensorBufferType().storage_type).index("U16")
+
     @staticmethod
     @catch_and_log_exceptions("SegmentationImage converter")
     def data__field_converter_override(data: TensorDataArrayLike) -> TensorDataBatch:
@@ -65,7 +68,7 @@ class SegmentationImageExt:
             ).cast(tensor_data_type.field("shape").type)
 
         buffer = tensor_data_arrow.storage.field(1)
-        if buffer[0].type_code not in (1, 2):
+        if buffer[0].type_code not in (SegmentationImageExt.U8_TYPE_ID, SegmentationImageExt.U16_TYPE_ID):
             np_buffer = np.require(buffer[0].value.values.to_numpy(), np.uint16)
             buffer = _build_buffer_array(np_buffer)
 
