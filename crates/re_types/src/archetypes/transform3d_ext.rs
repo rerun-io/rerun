0ac08c9be101a2101cccf99959362ef3dd4b3e09
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-10-02 18:55:21 +0200

    inline

--- crates/re_types/src/archetypes/transform3d_ext.rs
+++ crates/re_types/src/archetypes/transform3d_ext.rs
@@ -11,11 +11,13 @@ impl Transform3D {
     };
 
     /// From a translation.
+    #[inline]
     pub fn from_translation(translation: impl Into<Vec3D>) -> Self {
         Self::new(TranslationRotationScale3D::from_translation(translation))
     }
 
     /// From a translation applied after a rotation, known as a rigid transformation.
+    #[inline]
     pub fn from_translation_rotation(
         translation: impl Into<Vec3D>,
         rotation: impl Into<Rotation3D>,
@@ -27,6 +29,7 @@ impl Transform3D {
     }
 
     /// From a translation applied after a 3x3 matrix.
+    #[inline]
     pub fn from_translation_mat3x3(
         translation: impl Into<Vec3D>,
         mat3x3: impl Into<crate::datatypes::Mat3x3>,
