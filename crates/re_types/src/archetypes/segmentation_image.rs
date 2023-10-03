035ec5fb8e7678b8d1f409f3e322931f29ba66a3
Jeremy Leibs <jeremy@rerun.io>
2023-10-03 16:21:59 +0200

    codegen

--- crates/re_types/src/archetypes/segmentation_image.rs
+++ crates/re_types/src/archetypes/segmentation_image.rs
@@ -45,7 +45,7 @@
 ///     ]);
 ///
 ///     // log the annotation and the image
-///     rec.log("/", &annotation)?;
+///     rec.log_timeless("/", &annotation)?;
 ///
 ///     rec.log("image", &rerun::SegmentationImage::try_from(image)?)?;
 ///
