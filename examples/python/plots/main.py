abb9fd05f530dd0e15d3ddf789ab1c0dfb3ef8ef
Leonard Bruns <leonardb@kth.se>
2023-10-03 10:22:20 +0200

    Add description for plots example

--- examples/python/plots/main.py
+++ examples/python/plots/main.py
@@ -17,6 +17,41 @@ import numpy as np
 import rerun as rr  # pip install rerun-sdk
 
 
+DESCRIPTION = """
+# Plots
+
+This example shows various plot types that you can create using Rerun. Common
+usecases for such plots would be logging losses or metrics over time,
+histograms, or general function plots.
+
+## How it was made
+The full source code for this example is available
+[on GitHub](https://github.com/rerun-io/rerun/blob/latest/examples/python/plots/main.py).
+
+### Bar charts
+The [bar chart](recording://bar_chart) is created by logging the [rr.BarChart 
+archetype](https://www.rerun.io/docs/reference/data_types/archetypes/bar_chart).
+
+### Time series
+All other plots are created using the [rr.TimeSeriesScalar archetype]
+(https://www.rerun.io/docs/reference/data_types/archetypes/bar_chart) with
+different settings. Each plot is created by logging scalars at different time
+steps (i.e., the x-axis).
+
+For the [parabola](recording://curves/parabola) the radius and color is changed over
+time.
+
+[sin](recording://trig/sin) and [cos](recording://trig/cos) are logged with the
+same parent entity (i.e., `trig/{cos,sin}`) which will put them in the same
+view by default.
+
+For the [classification samples](recording://classification/samples)
+`rr.TimeSeriesScalar(..., scatter=True)` is used to create separate points that
+do not connect over time. Note, that in the other plots the logged scalars are
+connected over time by lines.
+""".strip()
+
+
 def clamp(n, smallest, largest):  # type: ignore[no-untyped-def]
     return max(smallest, min(n, largest))
 
@@ -68,14 +103,14 @@ def log_trig() -> None:
         rr.log("trig/cos", rr.TimeSeriesScalar(cos_of_t, label="cos(0.01t)", color=[0, 255, 0]))
 
 
-def log_segmentation() -> None:
+def log_classification() -> None:
     # Log a time series
     for t in range(0, 1000, 2):
         rr.set_time_sequence("frame_nr", t)
 
         f_of_t = (2 * 0.01 * t) + 2
         color = [255, 255, 0]
-        rr.log("segmentation/line", rr.TimeSeriesScalar(f_of_t, color=color, radius=3.0))
+        rr.log("classification/line", rr.TimeSeriesScalar(f_of_t, color=color, radius=3.0))
 
         g_of_t = f_of_t + random.uniform(-5.0, 5.0)
         if g_of_t < f_of_t - 1.5:
@@ -85,7 +120,7 @@ def log_segmentation() -> None:
         else:
             color = [255, 255, 255]
         radius = abs(g_of_t - f_of_t)
-        rr.log("segmentation/samples", rr.TimeSeriesScalar(g_of_t, color=color, scattered=True, radius=radius))
+        rr.log("classification/samples", rr.TimeSeriesScalar(g_of_t, color=color, scattered=True, radius=radius))
 
 
 def main() -> None:
@@ -97,10 +132,11 @@ def main() -> None:
 
     rr.script_setup(args, "rerun_example_plot")
 
+    rr.log("description", rr.TextDocument(DESCRIPTION, media_type=rr.MediaType.MARKDOWN), timeless=True)
     log_bar_chart()
     log_parabola()
     log_trig()
-    log_segmentation()
+    log_classification()
 
     rr.script_teardown(args)
 
