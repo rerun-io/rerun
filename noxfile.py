094bcc3cd7be5a4771592863d05c29b7cd339483
Antoine Beyeler <antoine@rerun.io>
2023-10-06 09:54:06 +0200

    Add support for Python 3.12

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,10 @@ from __future__ import annotations
 import nox  # type: ignore
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
+
+
+@nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.Session) -> None:
     """Run the Python test suite"""
     session.install("-r", "rerun_py/requirements-build.txt")
@@ -18,9 +21,33 @@ def tests(session: nox.Session) -> None:
     session.run("just", "py-test", external=True)
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=PYTHON_VERSIONS)
 def run_all(session: nox.Session) -> None:
     """Run all examples through the run_all.py script (pass args with: "-- <args>")"""
     # Note: the run_all.py scripts installs all dependencies itself. In particular, we can install from
     # examples/python/requirements.txt because it includes pyrealsense2, which is not available for mac.
     session.run("python", "scripts/run_all.py", "--install-requirements", *session.posargs)
+
+
+roundtrip_cpp_built = False
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def roundtrips(session: nox.Session) -> None:
+    """Run all roundtrip tests (C++ will be built only once / skip with: "-- --no-cpp-build")"""
+
+    global roundtrip_cpp_built
+
+    session.install("-r", "rerun_py/requirements-build.txt")
+    session.install("opencv-python")
+    session.install("./rerun_py")
+
+    extra_args = []
+    if roundtrip_cpp_built and "--no-cpp-build" not in session.posargs:
+        extra_args.append("--no-cpp-build")
+    extra_args.extend(session.posargs)
+
+    session.run("python", "tests/roundtrips.py", "--no-py-build", *extra_args)
+    session.run("python", "docs/code-examples/roundtrips.py", "--no-py-build", *extra_args)
+
+    roundtrip_cpp_built = True
