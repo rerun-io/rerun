0996bbac36f481cac43d9669fb42d1e44d13ce91
Andreas Reich <r_andreas2@web.de>
2023-10-10 18:04:38 +0200

    reenable manual indicator test

--- docs/code-examples/manual_indicator.cpp
+++ docs/code-examples/manual_indicator.cpp
@@ -22,14 +22,13 @@ int main() {
 
     // Specify both a Mesh3D and a Points3D indicator component so that the data is shown as both a
     // 3D mesh _and_ a point cloud by default.
-    // TODO:
-    // rec.log_component_batches(
-    //     "points_and_mesh",
-    //     3,
-    //     rr::Points3D::indicator(),
-    //     rr::Mesh3D::indicator(),
-    //     positions,
-    //     colors,
-    //     radii
-    // );
+    rec.log_component_batches(
+        "points_and_mesh",
+        3,
+        rr::Points3D::IndicatorComponent(),
+        rr::Mesh3D::IndicatorComponent(),
+        positions,
+        colors,
+        radii
+    );
 }
