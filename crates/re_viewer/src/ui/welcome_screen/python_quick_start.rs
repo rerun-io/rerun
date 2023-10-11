247585730fd929ad2e4242e8f23b70d701c38a00
Antoine Beyeler <antoine@rerun.io>
2023-10-11 17:24:20 +0200

    Work around #3612 by not using H1

--- crates/re_viewer/src/ui/welcome_screen/python_quick_start.rs
+++ crates/re_viewer/src/ui/welcome_screen/python_quick_start.rs
@@ -8,9 +8,10 @@ pub(super) fn python_quick_start(
     rec.log(
         "markdown",
         &re_sdk::TextDocument::new(
-            r#"# Python Quick Start
+            r#"
+## Python Quick Start
 
-## Installing the Rerun SDK
+### Installing the Rerun SDK
 
 The Rerun SDK is available on [PyPI](https://pypi.org/) under the
 [`rerun-sdk`](https://pypi.org/project/rerun-sdk/) name. It can be installed like any other
@@ -20,7 +21,7 @@ Python package:
 pip3 install rerun-sdk
 ```
 
-## Try out the viewer
+### Try out the viewer
 
 The Rerun SDK comes with a demo that can be used to try the viewer. You can send a demo recording
 to this viewer using the following command:
@@ -34,7 +35,7 @@ This will open a new recording that looks like this:
 ![Demo recording](https://static.rerun.io/quickstart2_simple_cube/632a8f1c79f70a2355fad294fe085291fcf3a8ae/768w.png)
 
 
-## Logging your own data
+### Logging your own data
 
 Instead of a pre-packaged demo, you can log your own data. Copy and paste the following snippet in a new
 Python file and execute it to create a new recording in this viewer:
@@ -68,7 +69,7 @@ rr.log(
 )
 ```
 
-## How does it work?
+### How does it work?
 
 TBC
 "#
