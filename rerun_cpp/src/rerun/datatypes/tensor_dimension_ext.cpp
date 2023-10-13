542fbec5d7950ca5097f8f4be65acb5a05226d5b
Andreas Reich <r_andreas2@web.de>
2023-10-13 11:13:43 +0200

    fix tensor dimension ctor

--- rerun_cpp/src/rerun/datatypes/tensor_dimension_ext.cpp
+++ rerun_cpp/src/rerun/datatypes/tensor_dimension_ext.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 #include "tensor_dimension.hpp"
 
 // #define EDIT_EXTENSION
@@ -8,8 +9,11 @@ namespace rerun {
 #ifdef EDIT_EXTENSION
         // [CODEGEN COPY TO HEADER START]
 
-        /// Nameless dimension
-        explicit TensorDimension(size_t size_) : size(size_) {}
+        /// Nameless dimension.
+        TensorDimension(size_t size_) : size(size_) {}
+
+        /// Dimension with name.
+        TensorDimension(size_t size_, std::string name_) : size(size_), name(std::move(name_)) {}
 
         // [CODEGEN COPY TO HEADER END]
 #endif
