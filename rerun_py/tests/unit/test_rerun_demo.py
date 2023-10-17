4410dd3f1f1bc9b36509c2bc516c685ec4b02676
Antoine Beyeler <antoine@rerun.io>
2023-10-17 18:06:42 +0200

    Fix lint

--- rerun_py/tests/unit/test_rerun_demo.py
+++ rerun_py/tests/unit/test_rerun_demo.py
@@ -1,13 +1,14 @@
+from __future__ import annotations
+
 import sys
 
 import pytest
 from rerun_demo import __main__ as main
 
-
 # fail for any deprecation warning
 pytestmark = pytest.mark.filterwarnings("error")
 
 
-def test_run_cube():
+def test_run_cube() -> None:
     sys.argv = ["prog", "--cube", "--headless"]
     main.main()
