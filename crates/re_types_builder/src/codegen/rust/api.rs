11654c9c77cb1e7feebe637f3234e4a041088539
Clement Rey <cr.rey.clement@gmail.com>
2023-10-18 11:14:50 +0200

    crate_name & module_name as methods

--- crates/re_types_builder/src/codegen/rust/api.rs
+++ crates/re_types_builder/src/codegen/rust/api.rs
@@ -86,8 +86,8 @@ impl RustCodeGenerator {
         // Generate folder contents:
         let ordered_objects = objects.ordered_objects(object_kind.into());
         for &obj in &ordered_objects {
-            let crate_name = super::util::crate_name(obj);
-            let module_name = super::util::module_name(obj);
+            let crate_name = obj.crate_name();
+            let module_name = obj.module_name();
 
             let crate_path = crates_root_path.join(&crate_name);
             let module_path = if obj.is_testing() {
@@ -119,8 +119,8 @@ impl RustCodeGenerator {
             let relevant_objs = &ordered_objects
                 .iter()
                 .filter(|obj| obj.is_testing() == is_testing)
-                .filter(|obj| super::util::crate_name(obj) == crate_name)
-                .filter(|obj| super::util::module_name(obj) == module_name)
+                .filter(|obj| obj.crate_name() == crate_name)
+                .filter(|obj| obj.module_name() == module_name)
                 .copied()
                 .collect_vec();
 
