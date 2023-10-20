2819bb1bd4dd3e0375fcff84035a5d08d7dbba4b
Antoine Beyeler <antoine@rerun.io>
2023-10-20 18:10:55 +0200

    Add script to generate RRD vs. screenshots comparison

---
+++ scripts/screenshot_compare/assets/static/sw.js
@@ -0,0 +1,27 @@
+var cacheName = 'rerun-viewer-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './index.js',
+  './index.css',
+  './re_viewer.js',
+  './re_viewer_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
