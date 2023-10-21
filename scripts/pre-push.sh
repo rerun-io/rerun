3652137a13a6b662f64d591233b5d192fd4500f2
Jeremy Leibs <jeremy@rerun.io>
2023-10-21 19:25:49 +0200

    Check for pixi

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -3,4 +3,10 @@
 # This script is intended to be called from the git pre-push hook.
 # See: hooks/README.md for more details.
 
-just fast-lint
+# Check if pixi is installed
+if ! command -v "pixi" > /dev/null 2>&1; then
+  echo "The rerun hooks require 'pixi', which is not installed or not in your PATH. Please run: 'cargo install pixi'."
+  exit 1
+fi
+
+pixi run fast-lint
