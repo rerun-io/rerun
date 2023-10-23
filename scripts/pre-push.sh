8e57f415a2a23a6250d400ad9f3aebb2a4d751c7
Jeremy Leibs <jeremy@rerun.io>
2023-10-23 12:24:09 -0400

    New helper script to run fast lints and pre-push hook that runs it (#3949)
    ### What
    
    Resolves:
     - https://github.com/rerun-io/rerun/issues/3738
    
    Builds out a micro-framework for running a bunch of lint-jobs in
    parallel as part of a local development cycle. Anything that we can run
    in a few seconds that would otherwise trip up CI should be a good
    contender for adding to this list.
    
    Takes advantage of the diff relative to the common ancestor to only
    check modified files.
    
    Also introduces a hooks directory for running as part of a pre-push
    hook.
    
    Examples:
    ```
    $ just fast-lint 
    pixi run python scripts/fast_lint.py 
    INFO:root:SKIP: lint-cpp-files (no modified files)
    INFO:root:SKIP: lint-rs-files (no modified files)
    INFO:root:PASS: lint-taplo in 0.27s
    INFO:root:PASS: lint-py-ruff in 0.28s
    INFO:root:PASS: lint-typos in 0.29s
    INFO:root:PASS: lint-py-black in 0.38s
    INFO:root:PASS: lint-rerun in 0.39s
    INFO:root:PASS: lint-py-mypy in 0.48s
    INFO:root:PASS: lint-py-blackdoc in 0.48s
    INFO:root:PASS: lint-codegen in 3.53s
    INFO:root:All lints passed in 3.54s
    ```
    
    Optionally skip some lints:
    ```
    pixi run python scripts/fast_lint.py --skip lint-codegen
    INFO:root:SKIP: lint-codegen (skipped manually)
    INFO:root:SKIP: lint-cpp-files (no modified files)
    INFO:root:SKIP: lint-rs-files (no modified files)
    INFO:root:PASS: lint-taplo in 0.27s
    INFO:root:PASS: lint-py-ruff in 0.28s
    INFO:root:PASS: lint-typos in 0.30s
    INFO:root:PASS: lint-rerun in 0.42s
    INFO:root:PASS: lint-py-black in 0.43s
    INFO:root:PASS: lint-py-blackdoc in 0.44s
    INFO:root:PASS: lint-py-mypy in 0.62s
    INFO:root:All lints passed in 0.63s
    ```
    
    Or force lints to run on the full tree without the change filters:
    ```
    $ just fast-lint --no-change-filter
    pixi run python scripts/fast_lint.py --no-change-filter
    INFO:root:SKIP: lint-cpp-files (no-change-filter not supported)
    INFO:root:PASS: lint-py-ruff in 0.29s
    INFO:root:PASS: lint-py-blackdoc in 0.64s
    INFO:root:PASS: lint-py-mypy in 0.75s
    INFO:root:PASS: lint-typos in 1.44s
    INFO:root:PASS: lint-taplo in 1.77s
    INFO:root:PASS: lint-py-black in 2.13s
    INFO:root:PASS: lint-rs-all in 3.89s
    INFO:root:PASS: lint-codegen in 4.92s
    INFO:root:PASS: lint-rerun in 5.43s
    INFO:root:All lints passed in 5.43s
    ```
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/3949) (if
    applicable)
    * [x] The PR title and labels are set such as to maximize their
    usefulness for the next release's CHANGELOG
    
    - [PR Build Summary](https://build.rerun.io/pr/3949)
    - [Docs
    preview](https://rerun.io/preview/6a955f8a54c2d868fe6d3e51ba16ff998d99b285/docs)
    <!--DOCS-PREVIEW-->
    - [Examples
    preview](https://rerun.io/preview/6a955f8a54c2d868fe6d3e51ba16ff998d99b285/examples)
    <!--EXAMPLES-PREVIEW-->
    - [Recent benchmark results](https://ref.rerun.io/dev/bench/)
    - [Wasm size tracking](https://ref.rerun.io/dev/sizes/)
    
    ---------
    
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

---
+++ scripts/pre-push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This script is intended to be called from the git pre-push hook.
+# See: hooks/README.md for more details.
+
+# Check if pixi is installed
+if ! command -v "pixi" > /dev/null 2>&1; then
+    echo "The rerun hooks require 'pixi', which is not installed or not in your PATH. Please run: 'cargo install pixi'."
+    exit 1
+fi
+
+while read local_ref local_sha remote_ref remote_sha; do
+    # Extract the branch name from the local reference
+    branch_name=$(echo "$local_ref" | sed 's/^refs\/heads\///')
+
+    # Get the name of the currently active branch
+    active_branch=$(git symbolic-ref --short HEAD)
+
+    # Check if the pushed branch matches the active branch
+    if [ "$branch_name" = "$active_branch" ]; then
+        exec pixi run fast-lint
+    else
+        echo "Skipping fast-lint because the pushed branch ($branch_name) does not match the active branch ($active_branch)."
+    fi
+done
