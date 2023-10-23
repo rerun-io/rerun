6e92a81857c8042d1cd0a6f8eef9dbdb7a23eca6
Jeremy Leibs <jeremy@rerun.io>
2023-10-23 19:35:50 +0200

    Remove ellipsis from docstring line

--- rerun_py/tests/unit/test_log.py
+++ rerun_py/tests/unit/test_log.py
@@ -4,7 +4,7 @@ import rerun as rr
 
 
 def test_log_point2d_basic() -> None:
-    """Basic test: logging a point shouldn't raise an exception…"""
+    """Basic test: logging a point shouldn't raise an exception."""
     points = rr.Points2D([(0, 0), (2, 2), (2, 2.5), (2.5, 2), (3, 4)], radii=0.5)
     rr.init("rerun_example_test_log")
     rr.log("points", points)
