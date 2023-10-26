2dab1b4207052a9bc80f13377db98c2151254b27
Jeremy Leibs <jeremy@rerun.io>
2023-10-26 16:32:53 +0200

    Remove env variable parsing

--- rerun_cpp/src/rerun/config.cpp
+++ rerun_cpp/src/rerun/config.cpp
@@ -11,27 +11,6 @@ namespace rerun {
         return global;
     }
 
-    RerunGlobalConfig::RerunGlobalConfig() {
-        // NOTE: we set the default value of default_enabled to match the environment
-        // variable, but in reality this doesn't actually do anything. The rust SDK
-        // will independently parse RERUN and disable `RecordingStream`s if it's set.
-        const char* envVarValue = std::getenv("RERUN");
-        if (envVarValue != nullptr) {
-            std::string envVarValueStr(envVarValue);
-            std::transform(
-                envVarValueStr.begin(),
-                envVarValueStr.end(),
-                envVarValueStr.begin(),
-                ::tolower
-            );
-            if (envVarValueStr == "1" || envVarValueStr == "true" || envVarValueStr == "yes") {
-                default_enabled.store(true, std::memory_order_seq_cst);
-            } else if (envVarValueStr == "0" || envVarValueStr == "false" || envVarValueStr == "no") {
-                default_enabled.store(false, std::memory_order_seq_cst);
-            }
-        } else {
-            default_enabled = true;
-        }
-    }
+    RerunGlobalConfig::RerunGlobalConfig() : default_enabled(true) {}
 
 } // namespace rerun
