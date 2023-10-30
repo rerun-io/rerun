19448e4f4f35dac429c5635717d3c4c3134c4812
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2023-10-30 16:17:51 +0100

    revert indentation change

--- tests/cpp/roundtrips/segmentation_image/main.cpp
+++ tests/cpp/roundtrips/segmentation_image/main.cpp
@@ -7,8 +7,8 @@ int main(int, char** argv) {
     const auto rec = rerun::RecordingStream("rerun_example_roundtrip_segmentation_image");
     rec.save(argv[1]).exit_on_failure()
 
-        // 3x2 image. Each pixel is incremented down each row
-        auto img = rerun::datatypes::TensorData({2, 3}, std::vector<uint8_t>{0, 1, 2, 3, 4, 5});
+    // 3x2 image. Each pixel is incremented down each row
+    auto img = rerun::datatypes::TensorData({2, 3}, std::vector<uint8_t>{0, 1, 2, 3, 4, 5});
 
     rec.log("segmentation_image", rerun::archetypes::SegmentationImage(img));
 }
