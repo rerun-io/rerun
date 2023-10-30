76fb5bc4de7b55fa8eae578a60f37ced89120406
Andreas Reich <r_andreas2@web.de>
2023-10-30 16:43:02 +0100

    fix another failed mass replace

--- tests/cpp/roundtrips/text_log/main.cpp
+++ tests/cpp/roundtrips/text_log/main.cpp
@@ -2,7 +2,8 @@
 
 int main(int, char** argv) {
     const auto rec = rerun::RecordingStream("rerun_example_roundtrip_text_log");
-    rec.save(argv[1]).exit_on_failure() rec.log("log", rerun::archetypes::TextLog("No level"));
+    rec.save(argv[1]).exit_on_failure();
+    rec.log("log", rerun::archetypes::TextLog("No level"));
     rec.log(
         "log",
         rerun::archetypes::TextLog("INFO level").with_level(rerun::components::TextLogLevel::INFO)
