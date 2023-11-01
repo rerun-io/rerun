7bf315386bdbb3ed33d3f86b8d40116ad5778326
Andreas Reich <andreas@rerun.io>
2023-11-01 13:47:12 +0100

    C++ Windows CI (#4110)
    ### What
    
    * Fixes #3756
    * Run VS2022 C++ tests as part of pull request CI.
    * decided to go pull request for now because it's very easy to break C++
    things on one compiler while you're on any of the other compilers
    * This is a 12min windows job currently which *also* has to build
    rerun_c for windows first
    * in the future we'd ideally just skip most C++ jobs if no cpp/hpp/h
    file has changed!
    * Enable warnings as errors for msvc
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested [demo.rerun.io](https://demo.rerun.io/pr/4110) (if
    applicable)
    * [x] The PR title and labels are set such as to maximize their
    usefulness for the next release's CHANGELOG
    
    - [PR Build Summary](https://build.rerun.io/pr/4110)
    - [Docs
    preview](https://rerun.io/preview/f5d9cc2f5b6ea8907e82a2bcd7ade265457beb49/docs)
    <!--DOCS-PREVIEW-->
    - [Examples
    preview](https://rerun.io/preview/f5d9cc2f5b6ea8907e82a2bcd7ade265457beb49/examples)
    <!--EXAMPLES-PREVIEW-->
    - [Recent benchmark results](https://ref.rerun.io/dev/bench/)
    - [Wasm size tracking](https://ref.rerun.io/dev/sizes/)

--- rerun_cpp/src/rerun/archetypes/pinhole.hpp
+++ rerun_cpp/src/rerun/archetypes/pinhole.hpp
@@ -38,7 +38,6 @@ namespace rerun {
         ///     rec.log("world/image", rerun::Pinhole::from_focal_length_and_resolution(3.0f, {3.0f, 3.0f}));
         ///
         ///     std::vector<uint8_t> random_data(3 * 3 * 3);
-        ///     std::generate(random_data.begin(), random_data.end(), std::rand);
         ///     std::generate(random_data.begin(), random_data.end(), [] {
         ///         return static_cast<uint8_t>(std::rand());
         ///     });
