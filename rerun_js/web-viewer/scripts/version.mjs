e07079e77da793dc38c61885ac195b6bccce5586
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-11-02 11:58:32 +0100

    fix versioning script

--- rerun_js/web-viewer/scripts/version.mjs
+++ rerun_js/web-viewer/scripts/version.mjs
@@ -11,7 +11,7 @@ const args = argv.join(" ");
 const root_dir = path.resolve(script_dir, "..");
 
 for (const pkg of packages) {
-  const cwd = path.join(root_dir, pkg);
+  const cwd = path.join(root_dir, pkg.path);
   $(`npm version ${args}`, { cwd });
 }
 
