83d7043862e6f587e0cad2c6e4cbd9d9bfaa6b64
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-11-02 13:42:34 +0100

    update dependency versions

--- rerun_js/web-viewer/scripts/version.mjs
+++ rerun_js/web-viewer/scripts/version.mjs
@@ -17,7 +17,19 @@ if (!isSemver(version)) {
 for (const pkg of packages) {
   const package_json_path = path.join(root_dir, pkg.path, "package.json");
   const package_json = JSON.parse(fs.readFileSync(package_json_path));
+
+  // update package version
   package_json.version = version;
+
+  // update dependency versions
+  if ("dependencies" in package_json) {
+    for (const dependency of Object.keys(package_json.dependencies)) {
+      if (dependency.startsWith("@rerun-io/web-viewer")) {
+        package_json.dependencies[dependency] = version;
+      }
+    }
+  }
+
   fs.writeFileSync(package_json_path, JSON.stringify(package_json, null, 2));
 }
 
