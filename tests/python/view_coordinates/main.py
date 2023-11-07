01bbdbce8224c6ee7c5006b2892986f236b86fa7
Clement Rey <cr.rey.clement@gmail.com>
2023-11-07 17:31:18 +0100

    Python: remove legacy APIs (#4037)
    - Fixes #3916 
    
    I've also added `AnnotationInfo` & `ClassDescription` at the root-level
    init.

--- tests/python/view_coordinates/main.py
+++ tests/python/view_coordinates/main.py
@@ -24,9 +24,7 @@ x = np.cos(lamd) * np.cos(phi)
 y = np.cos(lamd) * np.sin(phi)
 z = np.sin(lamd)
 unit_sphere_positions = np.transpose([x, y, z])
-rr.log_points(
-    "world/points", positions=unit_sphere_positions * radius, colors=np.abs(unit_sphere_positions), radii=0.01
-)
+rr.log("world/points", rr.Points3D(unit_sphere_positions * radius, colors=np.abs(unit_sphere_positions), radii=0.01))
 
 # RGB image that indicates orientation:
 rgb = np.zeros((50, 100, 3))
@@ -40,27 +38,29 @@ x, y = np.meshgrid(np.arange(0, 100), np.arange(0, 50))
 depth = 0.5 + 0.005 * x + 0.25 * np.sin(3.14 * y / 50 / 2)
 
 
-rr.log_view_coordinates("world", up="+Z")
+rr.log("world", rr.ViewCoordinates.RIGHT_HAND_Z_UP)
 
 
-def log_camera(origin: npt.ArrayLike, xyz: str, forward: npt.ArrayLike) -> None:
+def log_camera(origin: npt.ArrayLike, label: str, xyz: rr.components.ViewCoordinates, forward: npt.ArrayLike) -> None:
     [height, width, _channels] = rgb.shape
     f_len = (height * width) ** 0.5
-    cam_path = f"world/{xyz}"
-    pinhole_path = f"{cam_path}/{xyz}"
-    rr.log_point(f"{cam_path}/indicator", position=[0, 0, 0], color=[255, 255, 255], label=xyz)
-    rr.log_transform3d(cam_path, transform=rr.Translation3D(origin))
-    rr.log_arrow(cam_path + "/arrow", origin=[0, 0, 0], vector=forward, color=[255, 255, 255], width_scale=0.025)
-    rr.log_pinhole(
+    cam_path = f"world/{label}"
+    pinhole_path = f"{cam_path}/{label}"
+    rr.log(f"{cam_path}/indicator", rr.Points3D([0, 0, 0], colors=[255, 255, 255], labels=label))
+    rr.log(cam_path, rr.Transform3D(translation=origin))
+    rr.log(cam_path + "/arrow", rr.Arrows3D(origins=[0, 0, 0], vectors=forward, colors=[255, 255, 255], radii=0.025))
+    rr.log(
         pinhole_path,
-        width=width,
-        height=height,
-        focal_length_px=f_len,
-        principal_point_px=[width * 3 / 4, height * 3 / 4],  # test offset principal point
-        camera_xyz=xyz,
+        rr.Pinhole(
+            width=width,
+            height=height,
+            focal_length=f_len,
+            principal_point=[width * 3 / 4, height * 3 / 4],  # test offset principal point
+            camera_xyz=xyz,
+        ),
     )
-    rr.log_image(f"{pinhole_path}/rgb", rgb)
-    rr.log_depth_image(f"{pinhole_path}/depth", depth, meter=1.0)
+    rr.log(f"{pinhole_path}/rgb", rr.Image(rgb))
+    rr.log(f"{pinhole_path}/depth", rr.DepthImage(depth, meter=1.0))
 
 
 # Log a series of pinhole cameras only differing by their view coordinates and some offset.
@@ -68,22 +68,22 @@ def log_camera(origin: npt.ArrayLike, xyz: str, forward: npt.ArrayLike) -> None:
 
 s = 3  # spacing
 
-log_camera([0, 0, s], "RUB", forward=[0, 0, -1])
+log_camera([0, 0, s], "RUB", rr.ViewCoordinates.RUB, forward=[0, 0, -1])
 
 # All right-handed permutations of RDF:
-log_camera([s, -s, 0], "RDF", forward=[0, 0, 1])
-log_camera([s, 0, 0], "FRD", forward=[1, 0, 0])
-log_camera([s, s, 0], "DFR", forward=[0, 1, 0])
+log_camera([s, -s, 0], "RDF", rr.ViewCoordinates.RDF, forward=[0, 0, 1])
+log_camera([s, 0, 0], "FRD", rr.ViewCoordinates.FRD, forward=[1, 0, 0])
+log_camera([s, s, 0], "DFR", rr.ViewCoordinates.DFR, forward=[0, 1, 0])
 
 # All right-handed permutations of LUB:
-log_camera([0, -s, 0], "ULB", forward=[0, 0, -1])
-log_camera([0, 0, 0], "LBU", forward=[0, -1, 0])
-log_camera([0, s, 0], "BUL", forward=[-1, 0, 0])
+log_camera([0, -s, 0], "ULB", rr.ViewCoordinates.ULB, forward=[0, 0, -1])
+log_camera([0, 0, 0], "LBU", rr.ViewCoordinates.LBU, forward=[0, -1, 0])
+log_camera([0, s, 0], "BUL", rr.ViewCoordinates.BUL, forward=[-1, 0, 0])
 
 # All permutations of LUF:
-log_camera([-s, -s, 0], "LUF", forward=[0, 0, 1])
-log_camera([-s, 0, 0], "FLU", forward=[1, 0, 0])
-log_camera([-s, s, 0], "UFL", forward=[0, 1, 0])
+log_camera([-s, -s, 0], "LUF", rr.ViewCoordinates.LUF, forward=[0, 0, 1])
+log_camera([-s, 0, 0], "FLU", rr.ViewCoordinates.FLU, forward=[1, 0, 0])
+log_camera([-s, s, 0], "UFL", rr.ViewCoordinates.UFL, forward=[0, 1, 0])
 
 
 rr.script_teardown(args)
