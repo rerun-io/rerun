c366dc1ed2d4c49d3fb79f619b79f91184d8667b
Jeremy Leibs <jeremy@rerun.io>
2023-11-16 00:32:38 +0100

    New implementation for DataQueryBlueprint

--- crates/re_space_view/src/lib.rs
+++ crates/re_space_view/src/lib.rs
@@ -2,8 +2,10 @@
 //!
 //! Types & utilities for defining Space View classes and communicating with the Viewport.
 
+mod blueprint;
 pub mod controls;
 mod data_query;
+mod data_query_blueprint;
 mod screenshot;
 mod space_view_contents;
 mod unreachable_transform_reason;
@@ -11,6 +13,7 @@ mod unreachable_transform_reason;
 pub use data_query::{
     DataQuery, DataResultHandle, DataResultNode, DataResultTree, PropertyResolver,
 };
+pub use data_query_blueprint::DataQueryBlueprint;
 pub use screenshot::ScreenshotMode;
 pub use space_view_contents::{DataBlueprintGroup, SpaceViewContents};
 pub use unreachable_transform_reason::UnreachableTransformReason;
