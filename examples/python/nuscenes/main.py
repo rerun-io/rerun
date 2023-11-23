34891f34a89ba2812e7c500e52d3f54f44bce5e0
Leonard Bruns <leonardb@kth.se>
2023-11-23 18:14:07 +0100

    Clean up code

--- examples/python/nuscenes/main.py
+++ examples/python/nuscenes/main.py
@@ -1,8 +1,10 @@
 import argparse
 import pathlib
-import numpy as np
+from typing import Any, Union
 
+import numpy as np
 from nuscenes import nuscenes
+import numbers
 
 import rerun as rr
 
@@ -44,80 +46,96 @@ def log_nuscenes(root_dir: pathlib.Path, dataset_version: str, scene_name: str) 
     scene = next(s for s in nusc.scene if s["name"] == scene_name)
 
     # each sensor only has to be logged once, maintain set of already logged sensors
-    logged_sensor_tokens = set()
+    logged_sensor_tokens: set[str] = set()
 
     rr.log("world", rr.ViewCoordinates.RIGHT_HAND_Z_UP, timeless=True)
 
     current_sample = nusc.get("sample", scene["first_sample_token"])
     start_timestamp = current_sample["timestamp"]
     while True:
-        # log data
-        for sensor_name, data_token in current_sample["data"].items():
-            while True:
-                meta_data = nusc.get("sample_data", data_token)
-                sensor_token = meta_data["calibrated_sensor_token"]
-                if sensor_token not in logged_sensor_tokens:
-                    calibrated_sensor = nusc.get("calibrated_sensor", sensor_token)
-                    rotation_xyzw = np.roll(calibrated_sensor["rotation"], shift=-1)
-                    rr.log(
-                        f"world/ego_vehicle/{sensor_name}",
-                        rr.Transform3D(
-                            translation=calibrated_sensor["translation"],
-                            rotation=rr.Quaternion(xyzw=rotation_xyzw),
-                            from_parent=False,
-                        ),
-                        timeless=True,
-                    )
-                    logged_sensor_tokens.add(sensor_token)
-                    if len(calibrated_sensor["camera_intrinsic"]) != 0:
-                        rr.log(
-                            f"world/ego_vehicle/{sensor_name}",
-                            rr.Pinhole(
-                                image_from_camera=calibrated_sensor["camera_intrinsic"],
-                                width=meta_data["width"],
-                                height=meta_data["height"],
-                            ),
-                            timeless=True,
-                        )
-
-                rr.set_time_seconds("timestamp", (meta_data["timestamp"] - start_timestamp) * 1e-6)
-
-                data_file_path = root_dir / meta_data["filename"]
-
-                if meta_data["sensor_modality"] == "lidar":
-                    # log lidar points
-                    print(meta_data["ego_pose_token"])
-                    pointcloud = nuscenes.LidarPointCloud.from_file(str(data_file_path))
-                    points = pointcloud.points[:3].T  # shape after transposing: (num_points, 3)
-                    rr.log(f"world/ego_vehicle/{sensor_name}", rr.Points3D(points))
-
-                    ego_pose = nusc.get("ego_pose", meta_data["ego_pose_token"])
-                    rotation_xyzw = np.roll(ego_pose["rotation"], shift=-1)
-                    rr.log(
-                        "world/ego_vehicle",
-                        rr.Transform3D(
-                            translation=ego_pose["translation"],
-                            rotation=rr.Quaternion(xyzw=rotation_xyzw),
-                            from_parent=False,
-                        ),
-                    )
-                elif meta_data["sensor_modality"] == "radar":
-                    pointcloud = nuscenes.RadarPointCloud.from_file(str(data_file_path))
-                    points = pointcloud.points[:3].T  # shape after transposing: (num_points, 3)
-                    rr.log(f"world/ego_vehicle/{sensor_name}", rr.Points3D(points))
-                elif meta_data["sensor_modality"] == "camera":
-                    rr.log(f"world/ego_vehicle/{sensor_name}", rr.ImageEncoded(path=data_file_path))
-
-                data_token = meta_data["next"]
-                if data_token == "" or nusc.get("sample_data", data_token)["is_key_frame"]:
-                    break
-
-        # TODO optional log annotations
+        log_nuscenes_sample(current_sample, nusc, logged_sensor_tokens, start_timestamp)
 
         if current_sample["next"] == "":
             break
-
         current_sample = nusc.get("sample", current_sample["next"])
+
+
+def log_nuscenes_sample(
+    sample: dict[str, Any], nusc: nuscenes.NuScenes, logged_sensor_tokens: set[str], start_timestamp: numbers.Number
+) -> None:
+    # each sample is a keyframe with annotations
+
+    # log data
+    for sensor_name, sample_data_token in sample["data"].items():
+        # TODO optional log annotations
+        while True:
+            sample_data = nusc.get("sample_data", sample_data_token)
+            log_nuscenes_sample_data(sample_data, nusc, logged_sensor_tokens, start_timestamp)
+
+            sample_data_token = sample_data["next"]
+            if sample_data_token == "" or nusc.get("sample_data", sample_data_token)["is_key_frame"]:
+                break
+
+
+def log_nuscenes_sample_data(
+    sample_data: dict[str, Any],
+    nusc: nuscenes.NuScenes,
+    logged_sensor_tokens: set[str],
+    start_timestamp: numbers.Number,
+):
+    sensor_name = sample_data["channel"]
+    calibrated_sensor_token = sample_data["calibrated_sensor_token"]
+    if calibrated_sensor_token not in logged_sensor_tokens:
+        calibrated_sensor = nusc.get("calibrated_sensor", calibrated_sensor_token)
+        rotation_xyzw = np.roll(calibrated_sensor["rotation"], shift=-1)
+        rr.log(
+            f"world/ego_vehicle/{sensor_name}",
+            rr.Transform3D(
+                translation=calibrated_sensor["translation"],
+                rotation=rr.Quaternion(xyzw=rotation_xyzw),
+                from_parent=False,
+            ),
+            timeless=True,
+        )
+        logged_sensor_tokens.add(calibrated_sensor_token)
+        if len(calibrated_sensor["camera_intrinsic"]) != 0:
+            rr.log(
+                f"world/ego_vehicle/{sensor_name}",
+                rr.Pinhole(
+                    image_from_camera=calibrated_sensor["camera_intrinsic"],
+                    width=sample_data["width"],
+                    height=sample_data["height"],
+                ),
+                timeless=True,
+            )
+
+    rr.set_time_seconds("timestamp", (sample_data["timestamp"] - start_timestamp) * 1e-6)
+
+    data_file_path = nusc.dataroot / sample_data["filename"]
+
+    if sample_data["sensor_modality"] == "lidar":
+        # TODO optional color for lidar points (depth-like color map)
+        # log lidar points
+        pointcloud = nuscenes.LidarPointCloud.from_file(str(data_file_path))
+        points = pointcloud.points[:3].T  # shape after transposing: (num_points, 3)
+        rr.log(f"world/ego_vehicle/{sensor_name}", rr.Points3D(points))
+
+        ego_pose = nusc.get("ego_pose", sample_data["ego_pose_token"])
+        rotation_xyzw = np.roll(ego_pose["rotation"], shift=-1)
+        rr.log(
+            "world/ego_vehicle",
+            rr.Transform3D(
+                translation=ego_pose["translation"],
+                rotation=rr.Quaternion(xyzw=rotation_xyzw),
+                from_parent=False,
+            ),
+        )
+    elif sample_data["sensor_modality"] == "radar":
+        pointcloud = nuscenes.RadarPointCloud.from_file(str(data_file_path))
+        points = pointcloud.points[:3].T  # shape after transposing: (num_points, 3)
+        rr.log(f"world/ego_vehicle/{sensor_name}", rr.Points3D(points))
+    elif sample_data["sensor_modality"] == "camera":
+        rr.log(f"world/ego_vehicle/{sensor_name}", rr.ImageEncoded(path=data_file_path))
 
 
 def main() -> None:
@@ -129,7 +147,10 @@ def main() -> None:
         help="Root directory of nuScenes dataset",
     )
     parser.add_argument(
-        "--scene_name", type=str, default="scene-0061", help="Scene name to visualize (typically of form 'scene-xxxx')"
+        "--scene_name",
+        type=str,
+        default="scene-0061",
+        help="Scene name to visualize (typically of form 'scene-xxxx')",
     )
     parser.add_argument("--dataset_version", type=str, default="v1.0-mini", help="Scene id to visualize")
     rr.script_add_args(parser)
