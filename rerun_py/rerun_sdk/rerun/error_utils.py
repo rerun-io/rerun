da5de4b611777a2dc7e79df7f403187fda4701a6
Dvad <Dvad@users.noreply.github.com>
2023-11-25 16:10:30 -0800

    Always throw back KeyboardInterrupt to the user
    There are little reasons to emit a warning and keep-on going when a KeyboardInterrupt exception is received (i.e. was sent by the user). 
    I propose to let this one be passed to the client even in non strict mode

--- rerun_py/rerun_sdk/rerun/error_utils.py
+++ rerun_py/rerun_sdk/rerun/error_utils.py
@@ -205,7 +205,7 @@ class catch_and_log_exceptions:
         exc_tb: TracebackType | None,
     ) -> bool:
         try:
-            if exc_type is not None and not strict_mode():
+            if exc_type is not None and exc_type is not KeyboardInterrupt and not strict_mode():
                 if getattr(_rerun_exception_ctx, "pending_warnings", None) is None:
                     _rerun_exception_ctx.pending_warnings = []
 
