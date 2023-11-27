72b1b7eedaa4934a8a34d1ab498c9b7752dd8125
Andreas Reich <r_andreas2@web.de>
2023-11-27 10:27:50 +0100

    clarify comment on check_binary_and_header_version_match

--- rerun_cpp/src/rerun/sdk_info.hpp
+++ rerun_cpp/src/rerun/sdk_info.hpp
@@ -7,8 +7,10 @@ namespace rerun {
     /// The Rerun C++ SDK version as a human-readable string.
     const char* version_string();
 
-    /// Checks whether the version reported by the rerun_c binary matches `sdk_version_string`.
+    /// Internal check whether the version reported by the rerun_c binary matches `sdk_version_string`.
     ///
     /// This method is called on various C++ API entry points, calling `Error::handle` on the return value.
+    /// There is no need to call this method yourself unless you want to ensure that rerun_c binary and
+    /// rerun_c header versions match ahead of time.
     Error check_binary_and_header_version_match();
 } // namespace rerun
