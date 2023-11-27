27b3d0d982ac4fb8c5fdf4d4750c878580a2abb9
Clement Rey <cr.rey.clement@gmail.com>
2023-11-27 19:55:18 +0100

    make all the store building logic reusable

--- crates/re_arrow_store/benches/gc.rs
+++ crates/re_arrow_store/benches/gc.rs
@@ -8,7 +8,7 @@ use re_arrow_store::{
     DataStore, DataStoreConfig, GarbageCollectionOptions, GarbageCollectionTarget,
 };
 use re_log_types::{
-    build_frame_nr, build_log_time, DataRow, DataTable, EntityPath, RowId, TableId, Time,
+    build_frame_nr, build_log_time, DataRow, DataTable, EntityPath, RowId, TableId, Time, TimePoint,
 };
 use re_types::components::InstanceKey;
 use re_types_core::{AsComponents, ComponentBatch, ComponentName, Loggable as _};
@@ -20,15 +20,15 @@ criterion_main!(benches);
 
 #[cfg(not(debug_assertions))]
 mod constants {
-    pub const NUM_ENTITY_PATHS: i64 = 20;
-    pub const NUM_ROWS_PER_ENTITY_PATH: i64 = 10_000;
+    pub const NUM_ENTITY_PATHS: usize = 20;
+    pub const NUM_ROWS_PER_ENTITY_PATH: usize = 10_000;
 }
 
 // `cargo test` also runs the benchmark setup code, so make sure they run quickly:
 #[cfg(debug_assertions)]
 mod constants {
-    pub const NUM_ENTITY_PATHS: i64 = 1;
-    pub const NUM_ROWS_PER_ENTITY_PATH: i64 = 1;
+    pub const NUM_ENTITY_PATHS: usize = 1;
+    pub const NUM_ROWS_PER_ENTITY_PATH: usize = 1;
 }
 
 use constants::{NUM_ENTITY_PATHS, NUM_ROWS_PER_ENTITY_PATH};
@@ -50,7 +50,7 @@ fn plotting_dashboard(c: &mut Criterion) {
     const DROP_AT_LEAST: f64 = 0.3;
 
     let mut group = c.benchmark_group(format!(
-        "datastore/num_entities={NUM_ENTITY_PATHS}/num_rows_per_entity={NUM_ROWS_PER_ENTITY_PATH}/scalar_gc/drop_at_least={DROP_AT_LEAST}"
+        "datastore/num_entities={NUM_ENTITY_PATHS}/num_rows_per_entity={NUM_ROWS_PER_ENTITY_PATH}/plotting_dashboard/drop_at_least={DROP_AT_LEAST}"
     ));
     group.throughput(criterion::Throughput::Elements(
         ((NUM_ENTITY_PATHS * NUM_ROWS_PER_ENTITY_PATH) as f64 * DROP_AT_LEAST) as _,
@@ -65,12 +65,26 @@ fn plotting_dashboard(c: &mut Criterion) {
         dont_protect: Default::default(),
     };
 
+    let mut timegen = |i| {
+        [
+            build_log_time(Time::from_seconds_since_epoch(i as _)),
+            build_frame_nr((i as i64).into()),
+        ]
+        .into()
+    };
+
+    let mut datagen = |i| {
+        Box::new(re_types::archetypes::TimeSeriesScalar::new(i as f64)) as Box<dyn AsComponents>
+    };
+
     // Default config
     group.bench_function("default", |b| {
         let store = build_store(
             Default::default(),
             InstanceKey::name(),
-            false, // TODO: packed
+            false,
+            &mut timegen,
+            &mut datagen,
         );
         b.iter_batched(
             || store.clone(),
@@ -91,7 +105,9 @@ fn plotting_dashboard(c: &mut Criterion) {
                     ..Default::default()
                 },
                 InstanceKey::name(),
-                false, // TODO: packed
+                false,
+                &mut timegen,
+                &mut datagen,
             );
             b.iter_batched(
                 || store.clone(),
@@ -107,7 +123,17 @@ fn plotting_dashboard(c: &mut Criterion) {
 
 // --- Helpers ---
 
-fn build_store(config: DataStoreConfig, cluster_key: ComponentName, packed: bool) -> DataStore {
+fn build_store<FT, FD>(
+    config: DataStoreConfig,
+    cluster_key: ComponentName,
+    packed: bool,
+    timegen: &mut FT,
+    datagen: &mut FD,
+) -> DataStore
+where
+    FT: FnMut(usize) -> TimePoint,
+    FD: FnMut(usize) -> Box<dyn AsComponents>,
+{
     let mut store = DataStore::new(
         re_log_types::StoreId::random(re_log_types::StoreKind::Recording),
         cluster_key,
@@ -115,14 +141,15 @@ fn build_store(config: DataStoreConfig, cluster_key: ComponentName, packed: bool
     );
 
     let tables = (0..NUM_ENTITY_PATHS)
-        .map(|i| build_table(format!("entity_path_{i}").into(), packed))
+        .map(|i| build_table(format!("entity_path_{i}").into(), packed, timegen, datagen))
         .collect_vec();
     let mut rows_per_table = tables.iter().map(|table| table.to_rows()).collect_vec();
 
     // NOTE: interleave insertions between entities to more closely match real world scenarios.
     for _ in 0..NUM_ROWS_PER_ENTITY_PATH {
+        #[allow(clippy::needless_range_loop)] // readability
         for i in 0..NUM_ENTITY_PATHS {
-            let row = rows_per_table[i as usize].next().unwrap();
+            let row = rows_per_table[i].next().unwrap();
             store.insert_row(&row.unwrap()).unwrap();
         }
     }
@@ -130,20 +157,25 @@ fn build_store(config: DataStoreConfig, cluster_key: ComponentName, packed: bool
     store
 }
 
-fn build_table(entity_path: EntityPath, packed: bool) -> DataTable {
+fn build_table<FT, FD>(
+    entity_path: EntityPath,
+    packed: bool,
+    timegen: &mut FT,
+    datagen: &mut FD,
+) -> DataTable
+where
+    FT: FnMut(usize) -> TimePoint,
+    FD: FnMut(usize) -> Box<dyn AsComponents>,
+{
     let mut table = DataTable::from_rows(
         TableId::ZERO,
         (0..NUM_ROWS_PER_ENTITY_PATH).map(move |i| {
             DataRow::from_component_batches(
                 RowId::random(),
                 // NOTE: insert in multiple timelines to more closely match real world scenarios.
-                [
-                    build_log_time(Time::from_seconds_since_epoch(i as _)),
-                    build_frame_nr(i.into()),
-                ]
-                .into(),
+                timegen(i),
                 entity_path.clone(),
-                re_types::archetypes::TimeSeriesScalar::new(i as f64)
+                datagen(i)
                     .as_component_batches()
                     .iter()
                     .map(|batch| batch as &dyn ComponentBatch),
