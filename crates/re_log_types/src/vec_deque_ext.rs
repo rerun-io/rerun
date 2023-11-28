c42c78bd68e12f1d26cd979efaa3b6706ed15913
Clement Rey <cr.rey.clement@gmail.com>
2023-11-28 09:39:14 +0100

    add vecdeque removal extensions

--- crates/re_log_types/src/vec_deque_ext.rs
+++ crates/re_log_types/src/vec_deque_ext.rs
@@ -70,3 +70,74 @@ fn is_sorted() {
     v.sort();
     assert!(v.is_sorted());
 }
+
+// ---
+
+/// Extends [`VecDeque`] with extra removal routines.
+pub trait VecDequeRemovalExt<T> {
+    /// Removes an element from anywhere in the deque and returns it, replacing it with
+    /// whichever end element that this is closer to the removal point.
+    ///  
+    /// If `index` points to the front or back of the queue, the removal is guaranteed to preserve
+    /// ordering; otherwise it doesn not.
+    /// In either case, this is *O*(1).
+    ///
+    /// Returns `None` if `index` is out of bounds.
+    ///
+    /// Element at index 0 is the front of the queue.
+    fn swap_remove(&mut self, index: usize) -> Option<T>;
+}
+
+impl<T: Clone> VecDequeRemovalExt<T> for VecDeque<T> {
+    #[inline]
+    fn swap_remove(&mut self, index: usize) -> Option<T> {
+        if self.is_empty() {
+            return None;
+        }
+
+        if index == 0 {
+            let v = self.get(0).cloned();
+            self.rotate_left(1);
+            self.truncate(self.len() - 1);
+            v
+        } else if index + 1 == self.len() {
+            let v = self.get(index).cloned();
+            self.truncate(self.len() - 1);
+            v
+        } else if index < self.len() / 2 {
+            self.swap_remove_front(index)
+        } else {
+            self.swap_remove_back(index)
+        }
+    }
+}
+
+#[test]
+fn swap_remove() {
+    let mut v: VecDeque<i64> = vec![].into();
+
+    assert!(v.swap_remove(0).is_none());
+    assert!(v.is_sorted());
+
+    v.push_front(1);
+    assert!(v.is_sorted());
+
+    assert!(v.swap_remove(1).is_none());
+    assert_eq!(Some(1), v.swap_remove(0));
+    assert!(v.is_sorted());
+
+    v.extend([4, 5, 6, 7]);
+    assert!(v.is_sorted());
+
+    assert_eq!(Some(4), v.swap_remove(0));
+    assert!(v.is_sorted());
+
+    assert_eq!(Some(7), v.swap_remove(2));
+    assert!(v.is_sorted());
+
+    assert_eq!(Some(6), v.swap_remove(1));
+    assert!(v.is_sorted());
+
+    assert_eq!(Some(5), v.swap_remove(0));
+    assert!(v.is_sorted());
+}
