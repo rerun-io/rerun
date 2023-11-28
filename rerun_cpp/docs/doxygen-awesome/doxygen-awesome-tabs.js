a9ebc7540a441645497aac9ae04134d99e94ecbd
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-11-28 17:55:37 +0100

    format files

--- rerun_cpp/docs/doxygen-awesome/doxygen-awesome-tabs.js
+++ rerun_cpp/docs/doxygen-awesome/doxygen-awesome-tabs.js
@@ -28,43 +28,46 @@ SOFTWARE.
 */
 
 class DoxygenAwesomeTabs {
-
     static init() {
         window.addEventListener("load", () => {
-            document.querySelectorAll(".tabbed:not(:empty)").forEach((tabbed, tabbedIndex) => {
-                let tabLinkList = []
-                tabbed.querySelectorAll("li").forEach((tab, tabIndex) => {
-                    tab.id = "tab_" + tabbedIndex + "_" + tabIndex
-                    let header = tab.querySelector(".tab-title")
-                    let tabLink = document.createElement("button")
-                    tabLink.classList.add("tab-button")
-                    tabLink.appendChild(header)
-                    tabLink.addEventListener("click", () => {
-                        tabbed.querySelectorAll("li").forEach((tab) => {
-                            tab.classList.remove("selected")
-                        })
-                        tabLinkList.forEach((tabLink) => {
-                            tabLink.classList.remove("active")
-                        })
-                        tab.classList.add("selected")
-                        tabLink.classList.add("active")
-                    })
-                    tabLinkList.push(tabLink)
-                    if(tabIndex == 0) {
-                        tab.classList.add("selected")
-                        tabLink.classList.add("active")
-                    }
-                })
-                let tabsOverview = document.createElement("div")
-                tabsOverview.classList.add("tabs-overview")
-                let tabsOverviewContainer = document.createElement("div")
-                tabsOverviewContainer.classList.add("tabs-overview-container")
-                tabLinkList.forEach((tabLink) => {
-                    tabsOverview.appendChild(tabLink)
-                })
-                tabsOverviewContainer.appendChild(tabsOverview)
-                tabbed.before(tabsOverviewContainer)
-            })
-        })
+            document
+                .querySelectorAll(".tabbed:not(:empty)")
+                .forEach((tabbed, tabbedIndex) => {
+                    let tabLinkList = [];
+                    tabbed.querySelectorAll("li").forEach((tab, tabIndex) => {
+                        tab.id = "tab_" + tabbedIndex + "_" + tabIndex;
+                        let header = tab.querySelector(".tab-title");
+                        let tabLink = document.createElement("button");
+                        tabLink.classList.add("tab-button");
+                        tabLink.appendChild(header);
+                        tabLink.addEventListener("click", () => {
+                            tabbed.querySelectorAll("li").forEach((tab) => {
+                                tab.classList.remove("selected");
+                            });
+                            tabLinkList.forEach((tabLink) => {
+                                tabLink.classList.remove("active");
+                            });
+                            tab.classList.add("selected");
+                            tabLink.classList.add("active");
+                        });
+                        tabLinkList.push(tabLink);
+                        if (tabIndex == 0) {
+                            tab.classList.add("selected");
+                            tabLink.classList.add("active");
+                        }
+                    });
+                    let tabsOverview = document.createElement("div");
+                    tabsOverview.classList.add("tabs-overview");
+                    let tabsOverviewContainer = document.createElement("div");
+                    tabsOverviewContainer.classList.add(
+                        "tabs-overview-container",
+                    );
+                    tabLinkList.forEach((tabLink) => {
+                        tabsOverview.appendChild(tabLink);
+                    });
+                    tabsOverviewContainer.appendChild(tabsOverview);
+                    tabbed.before(tabsOverviewContainer);
+                });
+        });
     }
 }
--- rerun_js/web-viewer/react/index.js
+++ rerun_js/web-viewer/react/index.js
@@ -21,43 +21,42 @@ import * as rerun from "@rerun-io/web-viewer";
  * @param {Props} props
  */
 export default function WebViewer(props) {
-  const { width = "100%", height = "640px", rrd } = props;
+    const { width = "100%", height = "640px", rrd } = props;
 
-  /**
-   * Parent DOM node
-   * @type {React.RefObject<HTMLDivElement>}
-   */
-  const parent = useRef(null);
-  /**
-   * Web viewer instance
-   * @type {React.MutableRefObject<rerun.WebViewer | undefined>}
-   */
-  const viewer = useRef();
+    /**
+     * Parent DOM node
+     * @type {React.RefObject<HTMLDivElement>}
+     */
+    const parent = useRef(null);
+    /**
+     * Web viewer instance
+     * @type {React.MutableRefObject<rerun.WebViewer | undefined>}
+     */
+    const viewer = useRef();
 
-  useEffect(
-    () => {
-      if (parent.current) {
-        // Start the web viewer when the parent div is mounted to the DOM.
-        const w = new rerun.WebViewer();
-        w.start(rrd, parent.current);
-        viewer.current = w;
-        return () => {
-          // Stop the web viewer when the component is unmounted.
-          w.stop();
-          viewer.current = undefined;
-        };
-      }
-    },
-    // The web viewer will be restarted when:
-    // - `parent` is added/moved/removed in the DOM
-    // - `rrd` changes
-    [parent.current, rrd]
-  );
+    useEffect(
+        () => {
+            if (parent.current) {
+                // Start the web viewer when the parent div is mounted to the DOM.
+                const w = new rerun.WebViewer();
+                w.start(rrd, parent.current);
+                viewer.current = w;
+                return () => {
+                    // Stop the web viewer when the component is unmounted.
+                    w.stop();
+                    viewer.current = undefined;
+                };
+            }
+        },
+        // The web viewer will be restarted when:
+        // - `parent` is added/moved/removed in the DOM
+        // - `rrd` changes
+        [parent.current, rrd],
+    );
 
-  return React.createElement("div", {
-    className: "rerun-web-viewer",
-    style: { width, height, position: "relative" },
-    ref: parent,
-  });
+    return React.createElement("div", {
+        className: "rerun-web-viewer",
+        style: { width, height, position: "relative" },
+        ref: parent,
+    });
 }
-
