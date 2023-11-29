3189266e17ebc662410e82d2f95b5cef5a4e8883
Clement Rey <cr.rey.clement@gmail.com>
2023-11-29 20:12:48 +0100

    add batching toggle to GC settings

--- crates/re_arrow_store/tests/data_store.rs
+++ crates/re_arrow_store/tests/data_store.rs
@@ -932,6 +932,7 @@ fn gc_impl(store: &mut DataStore) {
             protect_latest: 0,
             purge_empty_tables: false,
             dont_protect: Default::default(),
+            enable_batching: false,
         });
         for event in store_events {
             assert!(store.get_msg_metadata(&event.row_id).is_none());
@@ -1011,6 +1012,7 @@ fn protected_gc_impl(store: &mut DataStore) {
         protect_latest: 1,
         purge_empty_tables: true,
         dont_protect: Default::default(),
+        enable_batching: false,
     });
 
     let mut assert_latest_components = |frame_nr: TimeInt, rows: &[(ComponentName, &DataRow)]| {
@@ -1107,6 +1109,7 @@ fn protected_gc_clear_impl(store: &mut DataStore) {
         protect_latest: 1,
         purge_empty_tables: true,
         dont_protect: Default::default(),
+        enable_batching: false,
     });
 
     let mut assert_latest_components = |frame_nr: TimeInt, rows: &[(ComponentName, &DataRow)]| {
@@ -1149,6 +1152,7 @@ fn protected_gc_clear_impl(store: &mut DataStore) {
         protect_latest: 1,
         purge_empty_tables: true,
         dont_protect: Default::default(),
+        enable_batching: false,
     });
 
     // No rows should remain because the table should have been purged
