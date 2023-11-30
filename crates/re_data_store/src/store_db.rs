daae645c0b5e983fc25e6cc85a680900ac9756db
Clement Rey <cr.rey.clement@gmail.com>
2023-11-30 09:02:24 +0100

    enable 3.5ms budget in main app

--- crates/re_data_store/src/store_db.rs
+++ crates/re_data_store/src/store_db.rs
@@ -59,6 +59,9 @@ const MAX_INSERT_ROW_ATTEMPTS: usize = 1_000;
 /// See [`insert_row_with_retries`].
 const DEFAULT_INSERT_ROW_STEP_SIZE: u64 = 100;
 
+/// See [`GarbageCollectionOptions::time_budget`].
+const GC_TIME_BUDGET: std::time::Duration = std::time::Duration::from_micros(3500); // empirical
+
 /// Inserts a [`DataRow`] into the [`DataStore`], retrying in case of duplicated `RowId`s.
 ///
 /// Retries a maximum of `num_attempts` times if the row couldn't be inserted because of a
@@ -434,7 +437,7 @@ impl StoreDb {
             .into_iter()
             .collect(),
             enable_batching: false,
-            time_budget: std::time::Duration::MAX,
+            time_budget: GC_TIME_BUDGET,
         });
     }
 
@@ -452,7 +455,7 @@ impl StoreDb {
             purge_empty_tables: false,
             dont_protect: Default::default(),
             enable_batching: false,
-            time_budget: std::time::Duration::MAX,
+            time_budget: GC_TIME_BUDGET,
         });
     }
 
