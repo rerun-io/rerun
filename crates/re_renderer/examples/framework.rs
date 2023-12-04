549cbd35e8a3f64f5ef04dba0fe69fad2598ced5
Andreas Reich <r_andreas2@web.de>
2023-12-04 21:49:47 +0100

    rust formatting

--- crates/re_renderer/examples/framework.rs
+++ crates/re_renderer/examples/framework.rs
@@ -277,7 +277,8 @@ impl<E: Example + 'static> Application<E> {
 
                     {
                         // Lock render pipelines for the lifetime of the composite pass.
-                        let render_pipelines = self.re_ctx.gpu_resources.render_pipelines.resources();
+                        let render_pipelines =
+                            self.re_ctx.gpu_resources.render_pipelines.resources();
 
                         let mut composite_pass =
                             composite_cmd_encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
