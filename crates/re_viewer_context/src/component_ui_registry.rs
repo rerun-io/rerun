4ac54641c5c2a67d2edcaf326dac02e7d5543afe
Antoine Beyeler <antoine@rerun.io>
2023-12-04 10:32:47 +0100

    Fix docs error

--- crates/re_viewer_context/src/component_ui_registry.rs
+++ crates/re_viewer_context/src/component_ui_registry.rs
@@ -21,14 +21,14 @@ pub enum UiVerbosity {
     /// Display everything as wide as available but limit height.
     ///
     /// This is used for the selection panel when multiple items are selected. When using a Table,
-    /// use the [`re_data_ui::table_for_verbosity`] function.
+    /// use the `re_data_ui::table_for_verbosity` function.
     MultiSelectionPanel,
 
     /// Display everything as wide as available, without height restrictions.
     ///
     /// This is used for the selection panel when only one item is selected. In this case, any
     /// scrolling is handled by the selection panel itself. When using a Table, use the
-    /// [`re_data_ui::table_for_verbosity`] function.
+    /// `re_data_ui::table_for_verbosity` function.
     SelectionPanel,
 }
 
