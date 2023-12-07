6a3c2e962fcc39c25e37d871340faf9306d5ca08
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-12-07 11:27:11 +0100

    update pip index job

--- scripts/ci/generate_prerelease_pip_index.py
+++ scripts/ci/generate_prerelease_pip_index.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 Script to generate a minimal pip index.
 
@@ -7,7 +9,7 @@ associated with a given commit.
 This is expected to be run by the `reusable_pip_index.yml` GitHub workflow.
 
 Requires the following packages:
-  pip install google-cloud-storage Jinja2 PyGithub
+  pip install google-cloud-storage Jinja2
 """
 from __future__ import annotations
 
@@ -20,7 +22,24 @@ from google.cloud import storage
 from jinja2 import Template
 
 
-def generate_pip_index(commit: str, upload: bool) -> None:
+def check_expected_wheels(wheels: list[str]) -> None:
+    missing = set(["windows", "macos_intel", "macos_arm", "linux"])
+
+    for wheel in wheels:
+        if "win_amd64" in wheel:
+            missing.remove("windows")
+        if "macosx" in wheel and "x86_64" in wheel:
+            missing.remove("macos_intel")
+        if "macosx" in wheel and "arm64" in wheel:
+            missing.remove("macos_arm")
+        if "manylinux" in wheel and "x86_64" in wheel:
+            missing.remove("linux")
+
+    if len(missing) != 0:
+        raise Exception(f"missing built wheels for the following platforms: {', '.join(missing)}")
+
+
+def generate_pip_index(title: str, dir: str, upload: bool, check: bool) -> None:
     # Initialize the GCS clients
     gcs_client = storage.Client()
 
@@ -28,20 +47,21 @@ def generate_pip_index(commit: str, upload: bool) -> None:
     found_builds = []
     wheels_bucket = gcs_client.bucket("rerun-builds")
 
-    commit_short = commit[:7]
-    print(f"Checking commit: {commit_short}...")
+    print(f'Checking path: "gs://rerun-builds/{dir}"...')
 
     found: dict[str, Any] = {}
 
     # Get the wheel files for the commit
-    wheel_blobs = list(wheels_bucket.list_blobs(prefix=f"commit/{commit_short}/wheels"))
+    wheel_blobs = list(wheels_bucket.list_blobs(prefix=dir))
     wheels = [blob.name.split("/")[-1] for blob in wheel_blobs if blob.name.endswith(".whl")]
-    if wheels:
-        print(f"Found wheels for commit: {commit_short}: {wheels}")
-        found["wheels"] = wheels
 
-    if found:
-        found["commit"] = commit_short
+    if check:
+        check_expected_wheels(wheels)
+
+    if len(wheels) > 0:
+        print(f"Found wheels: {wheels}")
+        found["wheels"] = wheels
+        found["title"] = title
         found_builds.append(found)
 
     template_path = os.path.join(os.path.dirname(os.path.relpath(__file__)), "templates/pip_index.html")
@@ -54,18 +74,22 @@ def generate_pip_index(commit: str, upload: bool) -> None:
     buffer.seek(0)
 
     if upload:
-        upload_blob = wheels_bucket.blob(f"commit/{commit_short}/wheels/index.html")
+        upload_blob = wheels_bucket.blob(f"{dir}/index.html")
         print(f"Uploading results to {upload_blob.name}")
         upload_blob.upload_from_file(buffer, content_type="text/html")
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Generate a minimal pip index")
-    parser.add_argument("--commit", required=True, help="Commit SHA")
+    parser.add_argument("--title", required=True, help="Index title")
+    parser.add_argument("--dir", required=True, help="GCS directory to search in the given bucket")
     parser.add_argument("--upload", action="store_true", help="Upload the index to GCS")
+    parser.add_argument(
+        "--check", action="store_true", help="Check if all required builds are present in the given bucket directory"
+    )
     args = parser.parse_args()
 
-    generate_pip_index(args.commit, args.upload)
+    generate_pip_index(args.title, args.dir, args.upload, args.check)
 
 
 if __name__ == "__main__":
