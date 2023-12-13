87491075eb27e943eb00df293cad63305fbe8165
Jeremy Leibs <jeremy@rerun.io>
2023-12-13 22:42:06 +0100

    No longer using SpaceViewContents

--- crates/re_space_view/src/lib.rs
+++ crates/re_space_view/src/lib.rs
@@ -7,11 +7,9 @@ pub mod controls;
 mod data_query;
 mod data_query_blueprint;
 mod screenshot;
-mod space_view_contents;
 mod unreachable_transform_reason;
 
 pub use data_query::{DataQuery, EntityOverrides, PropertyResolver, NOOP_RESOLVER};
 pub use data_query_blueprint::DataQueryBlueprint;
 pub use screenshot::ScreenshotMode;
-pub use space_view_contents::{DataBlueprintGroup, SpaceViewContents};
 pub use unreachable_transform_reason::UnreachableTransformReason;
