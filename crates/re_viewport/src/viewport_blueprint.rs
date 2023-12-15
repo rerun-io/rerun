db702308b1658fe691878c7976b445ba5623b72a
Jeremy Leibs <jeremy@rerun.io>
2023-12-15 17:45:38 +0100

    lint

--- crates/re_viewport/src/viewport_blueprint.rs
+++ crates/re_viewport/src/viewport_blueprint.rs
@@ -2,7 +2,7 @@ use std::collections::BTreeMap;
 
 use re_arrow_store::LatestAtQuery;
 use re_data_store::EntityPath;
-use re_log_types::{DataRow, RowId, TimePoint, Timeline};
+use re_log_types::Timeline;
 use re_query::query_archetype;
 use re_viewer_context::{Item, SpaceViewClassIdentifier, SpaceViewId, ViewerContext};
 
