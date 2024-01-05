fef34e71e18b9d16750d4bcd7729a15e4274a452
Clement Rey <cr.rey.clement@gmail.com>
2024-01-05 10:38:34 +0100

    add time-series/text-logs caching toggle

--- crates/re_viewer/src/ui/rerun_menu.rs
+++ crates/re_viewer/src/ui/rerun_menu.rs
@@ -342,6 +342,14 @@ fn experimental_feature_ui(
     re_ui
         .checkbox(
             ui,
+            &mut app_options.experimental_primary_caching_series,
+            "Primary caching: 2D & 3D point clouds",
+        )
+        .on_hover_text("Toggle primary caching for the time series & text logs space views.");
+
+    re_ui
+        .checkbox(
+            ui,
             &mut app_options.legacy_container_blueprint,
             "Use the legacy container blueprint storage for the viewport",
         )
