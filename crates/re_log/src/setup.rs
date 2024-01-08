8cabf4730326c1d0827addfb3fc5601f50686c5f
jprochazk <1665677+jprochazk@users.noreply.github.com>
2024-01-08 17:42:48 +0100

    web: add/remove receiver

--- crates/re_log/src/setup.rs
+++ crates/re_log/src/setup.rs
@@ -59,12 +59,21 @@ pub fn setup_native_logging() {
 
 #[cfg(target_arch = "wasm32")]
 pub fn setup_web_logging() {
+    use std::sync::atomic::{AtomicBool, Ordering};
+
+    static LOG_INIT: AtomicBool = AtomicBool::new(false);
+    if LOG_INIT.load(Ordering::SeqCst) {
+        return;
+    }
+
     crate::multi_logger::init().expect("Failed to set logger");
     log::set_max_level(log::LevelFilter::Debug);
     crate::add_boxed_logger(Box::new(crate::web_logger::WebLogger::new(
         log::LevelFilter::Debug,
     )))
     .expect("Failed to install logger");
+
+    LOG_INIT.store(true, Ordering::SeqCst);
 }
 
 // ----------------------------------------------------------------------------
