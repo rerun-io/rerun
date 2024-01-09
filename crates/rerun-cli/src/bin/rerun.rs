7970766697f52c03248210fb80d23732dde70b95
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-01-09 10:48:06 +0100

    `rerun-cli`: Format errors the way we want it

--- crates/rerun-cli/src/bin/rerun.rs
+++ crates/rerun-cli/src/bin/rerun.rs
@@ -17,7 +17,7 @@ static GLOBAL: AccountingAllocator<mimalloc::MiMalloc> =
     AccountingAllocator::new(mimalloc::MiMalloc);
 
 #[tokio::main]
-async fn main() -> anyhow::Result<std::process::ExitCode> {
+async fn main() -> std::process::ExitCode {
     re_log::setup_native_logging();
 
     // Name the rayon threads for the benefit of debuggers and profilers:
@@ -27,7 +27,14 @@ async fn main() -> anyhow::Result<std::process::ExitCode> {
         .unwrap();
 
     let build_info = re_build_info::build_info!();
-    rerun::run(build_info, rerun::CallSource::Cli, std::env::args())
-        .await
-        .map(std::process::ExitCode::from)
+
+    let result = rerun::run(build_info, rerun::CallSource::Cli, std::env::args()).await;
+
+    match result {
+        Ok(exit_code) => std::process::ExitCode::from(exit_code),
+        Err(err) => {
+            eprintln!("Error: {}", re_error::format(err));
+            std::process::ExitCode::FAILURE
+        }
+    }
 }
