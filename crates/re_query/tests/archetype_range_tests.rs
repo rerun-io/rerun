c2bc543ad9d3ca199c28f8521ff5902f175c9502
Clement Rey <cr.rey.clement@gmail.com>
2024-01-12 09:34:16 +0100

    do not bake latest-at semantics in uncache query APIs

--- crates/re_query/tests/archetype_range_tests.rs
+++ crates/re_query/tests/archetype_range_tests.rs
@@ -70,8 +70,6 @@ fn simple_range() {
 
     // --- First test: `(timepoint1, timepoint3]` ---
 
-    // The exclusion of `timepoint1` means latest-at semantics will kick in!
-
     let query = re_data_store::RangeQuery::new(
         timepoint1[0].0,
         TimeRange::new((timepoint1[0].1.as_i64() + 1).into(), timepoint3[0].1),
@@ -103,37 +101,9 @@ fn simple_range() {
     // └─────────────┴──────────────┴─────────────────┘
 
     {
-        // Frame #123
-
-        let arch_view = &results[0];
-        let time = arch_view.data_time().unwrap();
-
-        // Build expected df manually
-        let instances = vec![Some(InstanceKey(0)), Some(InstanceKey(1))];
-        let positions = vec![
-            Some(Position2D::new(1.0, 2.0)),
-            Some(Position2D::new(3.0, 4.0)),
-        ];
-        let colors = vec![None, Some(Color::from_rgb(255, 0, 0))];
-        let expected = DataCellRow(smallvec![
-            DataCell::from_native_sparse(instances),
-            DataCell::from_native_sparse(positions),
-            DataCell::from_native_sparse(colors)
-        ]);
-
-        //eprintln!("{df:?}");
-        //eprintln!("{expected:?}");
-
-        assert_eq!(TimeInt::from(123), time);
-        assert_eq!(
-            &expected,
-            &arch_view.to_data_cell_row_2::<Position2D, Color>().unwrap(),
-        );
-    }
-    {
         // Frame #323
 
-        let arch_view = &results[1];
+        let arch_view = &results[0];
         let time = arch_view.data_time().unwrap();
 
         // Build expected df manually
@@ -160,8 +130,6 @@ fn simple_range() {
     }
 
     // --- Second test: `[timepoint1, timepoint3]` ---
-
-    // The inclusion of `timepoint1` means latest-at semantics will _not_ kick in!
 
     let query = re_data_store::RangeQuery::new(
         timepoint1[0].0,
@@ -379,15 +347,6 @@ fn timeless_range() {
 
     // We expect this to generate the following `DataFrame`s:
     //
-    // Frame #123:
-    // ┌────────────────────┬───────────────┬─────────────────┐
-    // │ InstanceKey ┆ Point2D ┆ Color │
-    // ╞════════════════════╪═══════════════╪═════════════════╡
-    // │ 0                  ┆ {1.0,2.0}     ┆ null            │
-    // ├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┤
-    // │ 1                  ┆ {3.0,4.0}     ┆ 4278190080      │
-    // └────────────────────┴───────────────┴─────────────────┘
-    //
     // Frame #323:
     // ┌────────────────────┬───────────────┬─────────────────┐
     // │ InstanceKey ┆ Point2D ┆ Color │
@@ -398,37 +357,9 @@ fn timeless_range() {
     // └────────────────────┴───────────────┴─────────────────┘
 
     {
-        // Frame #123
-
-        let arch_view = &results[0];
-        let time = arch_view.data_time().unwrap();
-
-        // Build expected df manually
-        let instances = vec![Some(InstanceKey(0)), Some(InstanceKey(1))];
-        let positions = vec![
-            Some(Position2D::new(1.0, 2.0)),
-            Some(Position2D::new(3.0, 4.0)),
-        ];
-        let colors = vec![None, Some(Color::from_rgb(255, 0, 0))];
-        let expected = DataCellRow(smallvec![
-            DataCell::from_native_sparse(instances),
-            DataCell::from_native_sparse(positions),
-            DataCell::from_native_sparse(colors)
-        ]);
-
-        //eprintln!("{df:?}");
-        //eprintln!("{expected:?}");
-
-        assert_eq!(TimeInt::from(123), time);
-        assert_eq!(
-            &expected,
-            &arch_view.to_data_cell_row_2::<Position2D, Color>().unwrap(),
-        );
-    }
-    {
         // Frame #323
 
-        let arch_view = &results[1];
+        let arch_view = &results[0];
         let time = arch_view.data_time().unwrap();
 
         // Build expected df manually
@@ -456,8 +387,6 @@ fn timeless_range() {
 
     // --- Second test: `[timepoint1, timepoint3]` ---
 
-    // The inclusion of `timepoint1` means latest-at semantics will fall back to timeless data!
-
     let query = re_data_store::RangeQuery::new(
         timepoint1[0].0,
         TimeRange::new(timepoint1[0].1, timepoint3[0].1),
@@ -469,15 +398,6 @@ fn timeless_range() {
     let results = arch_views.collect::<Vec<_>>();
 
     // We expect this to generate the following `DataFrame`s:
-    //
-    // Frame #122:
-    // ┌────────────────────┬───────────────┬─────────────────┐
-    // │ InstanceKey ┆ Point2D ┆ Color │
-    // ╞════════════════════╪═══════════════╪═════════════════╡
-    // │ 0                  ┆ {10.0,20.0}   ┆ null            │
-    // ├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┤
-    // │ 1                  ┆ {30.0,40.0}   ┆ 4278190080      │
-    // └────────────────────┴───────────────┴─────────────────┘
     //
     // Frame #123:
     // ┌────────────────────┬───────────────┬─────────────────┐
@@ -498,36 +418,9 @@ fn timeless_range() {
     // └────────────────────┴───────────────┴─────────────────┘
 
     {
-        // Frame #122 (all timeless)
-
-        let arch_view = &results[0];
-        let time = arch_view.data_time().unwrap();
-
-        // Build expected df manually
-        let instances = vec![Some(InstanceKey(0)), Some(InstanceKey(1))];
-        let positions = vec![
-            Some(Position2D::new(10.0, 20.0)),
-            Some(Position2D::new(30.0, 40.0)),
-        ];
-        let colors = vec![None, Some(Color::from_rgb(255, 0, 0))];
-        let expected = DataCellRow(smallvec![
-            DataCell::from_native_sparse(instances),
-            DataCell::from_native_sparse(positions),
-            DataCell::from_native_sparse(colors)
-        ]);
-
-        //eprintln!("{df:?}");
-        //eprintln!("{expected:?}");
-
-        assert_eq!(TimeInt::from(122), time);
-        assert_eq!(
-            &expected,
-            &arch_view.to_data_cell_row_2::<Position2D, Color>().unwrap(),
-        );
-
         // Frame #123 (partially timeless)
 
-        let arch_view = &results[1];
+        let arch_view = &results[0];
         let time = arch_view.data_time().unwrap();
 
         // Build expected df manually
@@ -536,7 +429,7 @@ fn timeless_range() {
             Some(Position2D::new(1.0, 2.0)),
             Some(Position2D::new(3.0, 4.0)),
         ];
-        let colors = vec![None, Some(Color::from_rgb(255, 0, 0))];
+        let colors: Vec<Option<Color>> = vec![None, None];
         let expected = DataCellRow(smallvec![
             DataCell::from_native_sparse(instances),
             DataCell::from_native_sparse(positions),
@@ -555,7 +448,7 @@ fn timeless_range() {
     {
         // Frame #323
 
-        let arch_view = &results[2];
+        let arch_view = &results[1];
         let time = arch_view.data_time().unwrap();
 
         // Build expected df manually
@@ -802,8 +695,6 @@ fn simple_splatted_range() {
 
     // --- First test: `(timepoint1, timepoint3]` ---
 
-    // The exclusion of `timepoint1` means latest-at semantics will kick in!
-
     let query = re_data_store::RangeQuery::new(
         timepoint1[0].0,
         TimeRange::new((timepoint1[0].1.as_i64() + 1).into(), timepoint3[0].1),
@@ -816,15 +707,6 @@ fn simple_splatted_range() {
 
     // We expect this to generate the following `DataFrame`s:
     //
-    // Frame #123:
-    // ┌────────────────────┬───────────────┬─────────────────┐
-    // │ InstanceKey ┆ Point2D ┆ Color │
-    // ╞════════════════════╪═══════════════╪═════════════════╡
-    // │ 0                  ┆ {1.0,2.0}     ┆ null            │
-    // ├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┤
-    // │ 1                  ┆ {3.0,4.0}     ┆ 4278190080      │
-    // └────────────────────┴───────────────┴─────────────────┘
-    //
     // Frame #323:
     // ┌────────────────────┬───────────────┬─────────────────┐
     // │ InstanceKey ┆ Point2D ┆ Color │
@@ -834,41 +716,12 @@ fn simple_splatted_range() {
     // │ 1                  ┆ {30.0,40.0}   ┆ 16711680        │
     // └────────────────────┴───────────────┴─────────────────┘
 
-    assert_eq!(results.len(), 2);
-
-    {
-        // Frame #123
-
-        let arch_view = &results[0];
-        let time = arch_view.data_time().unwrap();
-
-        // Build expected df manually
-        let instances = vec![Some(InstanceKey(0)), Some(InstanceKey(1))];
-        let positions = vec![
-            Some(Position2D::new(1.0, 2.0)),
-            Some(Position2D::new(3.0, 4.0)),
-        ];
-        let colors = vec![None, Some(Color::from_rgb(255, 0, 0))];
-        let expected = DataCellRow(smallvec![
-            DataCell::from_native_sparse(instances),
-            DataCell::from_native_sparse(positions),
-            DataCell::from_native_sparse(colors)
-        ]);
-
-        //eprintln!("{df:?}");
-        //eprintln!("{expected:?}");
-
-        assert_eq!(TimeInt::from(123), time);
-        assert_eq!(
-            &expected,
-            &arch_view.to_data_cell_row_2::<Position2D, Color>().unwrap(),
-        );
-    }
+    assert_eq!(results.len(), 1);
 
     {
         // Frame #323
 
-        let arch_view = &results[1];
+        let arch_view = &results[0];
         let time = arch_view.data_time().unwrap();
 
         // Build expected df manually
