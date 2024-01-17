0d3593cada091e5dcdbd8577014be79d74875a17
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-01-17 09:45:08 +0100

    Improve env-var docs
    Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>

--- crates/rerun/src/run.rs
+++ crates/rerun/src/run.rs
@@ -20,7 +20,7 @@ const SHORT_ABOUT: &str = "The Rerun Viewer and Server";
 // Place the important help _last_, to make it most visible in the terminal.
 const EXAMPLES: &str = r#"
 Environment variables:
-    RERUN_SHADER_PATH         The search path for shader/shader-imports. WARNING: Shaders are embedded in some build configurations.
+    RERUN_SHADER_PATH         The search path for shader/shader-imports. Only available in developer builds.
     RERUN_TRACK_ALLOCATIONS   Track memory allocations to diagnose memory leaks in the viewer. WARNING: slows down the viewer by a lot!
     RUST_LOG                  Change the log level of the viewer, e.g. `RUST_LOG=debug`.
     WGPU_BACKEND              Overwrites the graphics backend used, must be one of `vulkan`, `metal`, `dx12`, `dx11`, or `gl`.
