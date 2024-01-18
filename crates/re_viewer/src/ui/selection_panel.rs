fbb35f6e5d8bd2e8980135e86bec5da30d384c52
Antoine Beyeler <antoine@rerun.io>
2024-01-18 16:49:41 +0100

    Keyboard navigation within the suggestion list

--- crates/re_viewer/src/ui/selection_panel.rs
+++ crates/re_viewer/src/ui/selection_panel.rs
@@ -1,4 +1,4 @@
-use egui::{NumExt as _, Ui};
+use egui::{Key, NumExt as _, Ui};
 use egui_tiles::{GridLayout, Tile};
 
 use re_data_ui::{image_meaning_for_entity, item_ui, DataUi};
@@ -527,7 +527,7 @@ fn space_view_top_level_properties(
 }
 
 /// State of the space origin widget.
-#[derive(Default, Clone, serde::Deserialize, serde::Serialize)]
+#[derive(Default, Clone)]
 enum SpaceOriginEditState {
     #[default]
     NotEditing,
@@ -538,6 +538,9 @@ enum SpaceOriginEditState {
 
         /// Did we just enter editing mode?
         entered_editing: bool,
+
+        /// The index of the currently selected suggestion (for keyboard navigation).
+        selected_suggestion: Option<usize>,
     },
 }
 
@@ -550,7 +553,7 @@ fn space_view_space_origin_widget_ui(
 ) {
     let is_editing_id = ui.make_persistent_id(space_view.id.hash());
     let mut state: SpaceOriginEditState =
-        ui.memory_mut(|mem| mem.data.get_persisted(is_editing_id).unwrap_or_default());
+        ui.memory_mut(|mem| mem.data.get_temp(is_editing_id).unwrap_or_default());
 
     match &mut state {
         SpaceOriginEditState::NotEditing => {
@@ -561,12 +564,14 @@ fn space_view_space_origin_widget_ui(
                 state = SpaceOriginEditState::Editing {
                     origin_string: space_origin_string,
                     entered_editing: true,
+                    selected_suggestion: None,
                 };
             }
         }
         SpaceOriginEditState::Editing {
             origin_string,
             entered_editing,
+            selected_suggestion,
         } => {
             let keep_editing = space_view_space_origin_widget_editing_ui(
                 ui,
@@ -575,6 +580,7 @@ fn space_view_space_origin_widget_ui(
                 origin_string,
                 *entered_editing,
                 space_view,
+                selected_suggestion,
             );
 
             if keep_editing {
@@ -585,7 +591,7 @@ fn space_view_space_origin_widget_ui(
         }
     }
 
-    ui.memory_mut(|mem| mem.data.insert_persisted(is_editing_id, state));
+    ui.memory_mut(|mem| mem.data.insert_temp(is_editing_id, state));
 }
 
 /// Display the space origin of a space view with it is in edit mode.
@@ -596,8 +602,79 @@ fn space_view_space_origin_widget_editing_ui(
     space_origin_string: &mut String,
     entered_editing: bool,
     space_view: &SpaceViewBlueprint,
+    selected_suggestion: &mut Option<usize>,
 ) -> bool {
     let mut keep_editing = true;
+
+    //
+    // Build and filter the suggestion lists
+    //
+
+    // All suggestions for this class of space views.
+    let space_view_suggestions =
+        re_viewport::space_view_heuristics::all_possible_space_views(ctx, spaces_info)
+            .into_iter()
+            .map(|(space_view, _)| space_view)
+            .filter(|this_space_view| {
+                this_space_view.class_identifier() == space_view.class_identifier()
+            })
+            .collect::<Vec<_>>();
+
+    // Filtered suggestions based on the current text edit content.
+    let filtered_space_view_suggestions = space_view_suggestions
+        .iter()
+        .filter(|suggested_space_view| {
+            suggested_space_view
+                .space_origin
+                .to_string()
+                .contains(&*space_origin_string)
+        })
+        .collect::<Vec<_>>();
+
+    //
+    // Move cursor with keyboard (must happen before text edit to capture the keystrokes
+    //
+
+    let mut arrow_down =
+        ui.input_mut(|i| i.count_and_consume_key(Default::default(), Key::ArrowDown));
+    let arrow_up = ui.input_mut(|i| i.count_and_consume_key(Default::default(), Key::ArrowUp));
+
+    // force spawn a selected suggestion if the down arrow is pressed
+    if arrow_down > 0 && selected_suggestion.is_none() {
+        *selected_suggestion = Some(0);
+        arrow_down -= 1;
+    }
+
+    *selected_suggestion = selected_suggestion.map(|mut selected_suggestion| {
+        selected_suggestion = selected_suggestion
+            .saturating_add(arrow_down)
+            .saturating_sub(arrow_up);
+        if !space_view_suggestions.is_empty() {
+            selected_suggestion =
+                selected_suggestion.at_most(filtered_space_view_suggestions.len() - 1);
+        }
+        selected_suggestion
+    });
+
+    //
+    // Handle enter key when a suggestion is selected
+    //
+
+    let enter_key_hit = ui.input(|i| i.key_pressed(egui::Key::Enter));
+
+    if let Some(selected_suggestion) = selected_suggestion {
+        if enter_key_hit {
+            *space_origin_string = filtered_space_view_suggestions[*selected_suggestion]
+                .space_origin
+                .to_string();
+            keep_editing = false;
+        }
+    }
+
+    //
+    // Draw the text edit
+    //
+
     let mut output = egui::TextEdit::singleline(space_origin_string).show(ui);
 
     if entered_editing {
@@ -614,11 +691,15 @@ fn space_view_space_origin_widget_editing_ui(
     }
 
     if output.response.lost_focus() {
-        if ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+        if enter_key_hit {
             space_view.set_origin(ctx, &space_origin_string.clone().into());
         }
         keep_editing = false;
     }
+
+    //
+    // Display popup with suggestions
+    //
 
     let popup_id = ui.make_persistent_id("suggestions");
     if output.response.has_focus() {
@@ -627,36 +708,23 @@ fn space_view_space_origin_widget_editing_ui(
 
     let suggestions_ui = |ui: &mut egui::Ui| {
         ui.spacing_mut().item_spacing.y = 0.0;
+        for (idx, suggested_space_view) in filtered_space_view_suggestions.iter().enumerate() {
+            let response = re_ui::list_item::ListItem::new(
+                ctx.re_ui,
+                suggested_space_view
+                    .space_origin
+                    .syntax_highlighted(ui.style()),
+            )
+            .force_hovered(*selected_suggestion == Some(idx))
+            .show(ui);
 
-        let mut excluded_count = 0;
-        for suggested_space_view in
-            re_viewport::space_view_heuristics::all_possible_space_views(ctx, spaces_info)
-                .into_iter()
-                .map(|(space_view, _)| space_view)
-                .filter(|this_space_view| {
-                    this_space_view.class_identifier() == space_view.class_identifier()
-                })
-        {
-            let suggested_space_origin_string = suggested_space_view.space_origin.to_string();
-
-            if suggested_space_origin_string.contains(&*space_origin_string) {
-                let response = re_ui::list_item::ListItem::new(
-                    ctx.re_ui,
-                    suggested_space_view
-                        .space_origin
-                        .syntax_highlighted(ui.style()),
-                )
-                .show(ui);
-
-                if response.clicked() {
-                    *space_origin_string = suggested_space_origin_string;
-                    space_view.set_origin(ctx, &space_origin_string.clone().into());
-                }
-            } else {
-                excluded_count += 1;
+            if response.clicked() {
+                *space_origin_string = suggested_space_view.space_origin.to_string();
+                space_view.set_origin(ctx, &space_origin_string.clone().into());
             }
         }
 
+        let excluded_count = space_view_suggestions.len() - filtered_space_view_suggestions.len();
         if excluded_count > 0 {
             re_ui::list_item::ListItem::new(
                 ctx.re_ui,
