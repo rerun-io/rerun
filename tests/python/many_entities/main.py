c4f7149a2cb375654d53d836536c868558da4a6f
Andreas Reich <r_andreas2@web.de>
2024-01-19 12:17:15 +0100

    update many entities test a bit

--- tests/python/many_entities/main.py
+++ tests/python/many_entities/main.py
@@ -1,11 +1,22 @@
 from __future__ import annotations
 
+import argparse
 import math
 
 import rerun as rr
 
-rr.init("rerun_example_benchmark_many_entities", spawn=True)
 
-for i in range(1000):
-    f = i * 0.1
-    rr.log("i" + str(i), rr.Points3D([math.sin(f), f, math.cos(f)]))
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Simple benchmark for many individual entitites.")
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+
+    rr.script_setup(args, "rerun_example_benchmark_many_entities")
+
+    for i in range(1000):
+        f = i * 0.1
+        rr.log("i" + str(i), rr.Points3D([math.sin(f), f, math.cos(f)]))
+
+
+if __name__ == "__main__":
+    main()
