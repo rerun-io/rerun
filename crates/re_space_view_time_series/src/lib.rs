4dc37c058a38fa37507207e5cb0a41fc61c124b0
Clement Rey <cr.rey.clement@gmail.com>
2024-01-24 18:29:07 +0100

    v2

--- crates/re_space_view_time_series/src/lib.rs
+++ crates/re_space_view_time_series/src/lib.rs
@@ -6,3 +6,15 @@ mod space_view_class;
 mod visualizer_system;
 
 pub use space_view_class::TimeSeriesSpaceView;
+
+/// Computes a deterministic, globally unique ID for the plot based on the ID of the space view
+/// itself.
+///
+/// Use it to access the plot's state from anywhere, e.g.:
+/// ```ignore
+/// let plot_mem = egui_plot::PlotMemory::load(egui_ctx, crate::plot_id(query.space_view_id));
+/// ```
+#[inline]
+pub(crate) fn plot_id(space_view_id: re_viewer_context::SpaceViewId) -> egui::Id {
+    egui::Id::new(format!("plot_{space_view_id}"))
+}
