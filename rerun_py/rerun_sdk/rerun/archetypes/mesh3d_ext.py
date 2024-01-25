66937225bcab32e16683b4883d1cf25f98fc1083
Andreas Reich <r_andreas2@web.de>
2024-01-25 18:46:43 +0100

    fix and update mesh3d python tests

--- rerun_py/rerun_sdk/rerun/archetypes/mesh3d_ext.py
+++ rerun_py/rerun_sdk/rerun/archetypes/mesh3d_ext.py
@@ -19,7 +19,7 @@ class Mesh3DExt:
         mesh_properties: datatypes.MeshPropertiesLike | None = None,
         vertex_normals: datatypes.Vec3DArrayLike | None = None,
         vertex_colors: datatypes.Rgba32ArrayLike | None = None,
-        vertex_texcoords: datatypes.Rgba32ArrayLike | None = None,
+        vertex_texcoords: datatypes.Vec2DArrayLike | None = None,
         mesh_material: datatypes.MaterialLike | None = None,
         class_ids: datatypes.ClassIdArrayLike | None = None,
         instance_keys: components.InstanceKeyArrayLike | None = None,
