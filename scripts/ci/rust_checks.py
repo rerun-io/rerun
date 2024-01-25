9e54ac8ae34136a0f8d59e4995f9f52e2151a12a
Andreas Reich <r_andreas2@web.de>
2024-01-25 14:11:30 +0100

    add timing overview to rust_checks.py

--- scripts/ci/rust_checks.py
+++ scripts/ci/rust_checks.py
@@ -8,21 +8,35 @@ import os
 import re
 import subprocess
 import sys
+import time
 
 
-def run_cargo(args: str) -> None:
-    print(f"Running `cargo {args}`")
-    result = subprocess.call(f"cargo {args}", shell=True)
+class Timing:
+    def __init__(self, command: str, start_time: float) -> None:
+        self.command = command
+        self.duration = time.time() - start_time
+
+
+def run_cargo(args: str) -> Timing:
+    command = f"cargo {args}"
+    print(f"Running '{command}'")
+    start = time.time()
+    result = subprocess.call(command, shell=True)
+
     if result != 0:
         sys.exit(result)
 
+    return Timing(command, start)
+
 
 def main() -> None:
-    # First check with --locked to make sure Cargo.lock is up to date.
-    run_cargo("check --locked --all-features")
+    timings = []
 
-    run_cargo("fmt --all -- --check")
-    run_cargo("cranky --all-targets --all-features -- --deny warnings")
+    # First check with --locked to make sure Cargo.lock is up to date.
+    timings.append(run_cargo("check --locked --all-features"))
+
+    timings.append(run_cargo("fmt --all -- --check"))
+    timings.append(run_cargo("cranky --all-targets --all-features -- --deny warnings"))
 
     # Since features are additive, check samples individually.
     for path, dirnames, filenames in os.walk("examples/rust"):
@@ -35,23 +49,29 @@ def main() -> None:
                 raise Exception(f"Failed to find package name in {path}/Cargo.toml")
             package_name = package_name_result.group(1)
 
-            run_cargo(f"check --no-default-features -p {package_name}")
-            run_cargo(f"check --all-features -p {package_name}")
+            timings.append(run_cargo(f"check --no-default-features -p {package_name}"))
+            timings.append(run_cargo(f"check --all-features -p {package_name}"))
 
     # Check a few important permutations of the feature flags for our `rerun` library:
-    run_cargo("cranky -p rerun --no-default-features")
-    run_cargo("cranky -p rerun --no-default-features --features sdk")
+    timings.append(run_cargo("cranky -p rerun --no-default-features"))
+    timings.append(run_cargo("cranky -p rerun --no-default-features --features sdk"))
 
     # Doc tests
-    run_cargo("doc --all-features")
-    run_cargo("doc --no-deps --all-features --workspace")
-    run_cargo("doc --document-private-items --no-deps --all-features --workspace")
+    timings.append(run_cargo("doc --all-features"))
+    timings.append(run_cargo("doc --no-deps --all-features --workspace"))
+    timings.append(run_cargo("doc --document-private-items --no-deps --all-features --workspace"))
 
     # Just a normal `cargo test` should always work:
-    run_cargo("test --all-targets")
+    timings.append(run_cargo("test --all-targets"))
 
     # Full test of everything:
-    run_cargo("test --all-targets --all-features")
+    timings.append(run_cargo("test --all-targets --all-features"))
+
+    # Print timings overview
+    print("-----------------")
+    print("Timings:")
+    for timing in timings:
+        print(f"{timing.duration:.2f}s \t {timing.command}")
 
 
 if __name__ == "__main__":
