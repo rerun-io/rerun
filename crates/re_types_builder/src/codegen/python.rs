48ac20183cdeb5779e367b015f2c1da0d3f29a82
Andreas Reich <r_andreas2@web.de>
2024-01-31 15:55:01 +0100

    lint fix

--- crates/re_types_builder/src/codegen/python.rs
+++ crates/re_types_builder/src/codegen/python.rs
@@ -1074,7 +1074,7 @@ fn quote_union_kind_from_fields(fields: &Vec<ObjectField>) -> String {
                 line.remove(0);
             }
         }
-        lines.push(format!("* \"{}\":", field.name));
+        lines.push(format!("* {:?}:", field.name));
         lines.extend(content.into_iter().map(|line| format!("    {line}")));
         lines.push(String::new());
     }
