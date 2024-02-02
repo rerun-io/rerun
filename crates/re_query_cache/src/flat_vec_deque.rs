28ea85679e79648080a32ff73b803e86c4e7e7f5
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-02-02 14:32:42 +0100

    Fix crashes in cache when encountering new component (#5016)
    ### What
    * Closes https://github.com/rerun-io/rerun/issues/5009
    * Very similar to https://github.com/rerun-io/rerun/pull/5007
    
    ---------
    
    Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>

--- crates/re_query_cache/src/flat_vec_deque.rs
+++ crates/re_query_cache/src/flat_vec_deque.rs
@@ -419,7 +419,7 @@ impl<T> FlatVecDeque<T> {
         self.push_back_deque(rhs);
         self.push_back_deque(right);
 
-        debug_assert!(self.iter_offset_ranges().all(|r| r.start < r.end));
+        debug_assert!(self.iter_offset_ranges().all(|r| r.start <= r.end));
     }
 }
 
@@ -638,11 +638,13 @@ impl<T> FlatVecDeque<T> {
     /// Shortens the deque, keeping all entries up to `entry_index` (excluded), and
     /// dropping the rest.
     ///
-    /// Panics if `entry_index` is out of bounds.
+    /// If `entry_index` is greater or equal to [`Self::num_entries`], this has no effect.
     #[inline]
     pub fn truncate(&mut self, entry_index: usize) {
-        self.values.truncate(self.value_offset(entry_index));
-        self.offsets.truncate(entry_index);
+        if entry_index < self.num_entries() {
+            self.values.truncate(self.value_offset(entry_index));
+            self.offsets.truncate(entry_index);
+        }
     }
 
     /// Removes the entry at `entry_index` from the deque.
