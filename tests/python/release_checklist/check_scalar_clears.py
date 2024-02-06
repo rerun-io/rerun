f1f603f6b3b7f8a703791bf73ba3d0788a4cdd09
Clement Rey <cr.rey.clement@gmail.com>
2024-02-06 17:22:04 +0100

    add scalar_clears to checklist

---
+++ tests/python/release_checklist/check_scalar_clears.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import os
+from argparse import Namespace
+from uuid import uuid4
+
+import rerun as rr
+
+README = """
+# Scalar clears
+
+This checks whether scalar time series correctly behave with `Clear`s.
+
+### Actions
+
+Look at the plot, you should see a big hole in the middle, where `Clear`s were logged.
+If so, you can close this recording.
+"""
+
+
+def log_readme() -> None:
+    rr.log("readme", rr.TextDocument(README, media_type=rr.MediaType.MARKDOWN), timeless=True)
+
+
+def log_plots() -> None:
+    from math import sin, tau
+
+    for t in range(0, int(tau * 2 * 10.0)):
+        rr.set_time_sequence("frame_nr", t)
+
+        sin_of_t = sin(float(t) / 10.0)
+        rr.log("sin", rr.Scalar(sin_of_t))
+
+        if t > 30 and t < 90:
+            rr.log("sin", rr.Clear(recursive=True))
+
+
+def run(args: Namespace) -> None:
+    # TODO(cmc): I have no idea why this works without specifying a `recording_id`, but
+    # I'm not gonna rely on it anyway.
+    rr.script_setup(args, f"{os.path.basename(__file__)}", recording_id=uuid4())
+
+    log_readme()
+    log_plots()
