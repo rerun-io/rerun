0cccb9a8f20d2134b8b8f469d37f45180a5dff11
Clement Rey <cr.rey.clement@gmail.com>
2024-02-06 17:38:06 +0100

    make individual checks runnable

--- tests/python/release_checklist/check_scalar_clears.py
+++ tests/python/release_checklist/check_scalar_clears.py
@@ -42,3 +42,12 @@ def run(args: Namespace) -> None:
 
     log_readme()
     log_plots()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Interactive release checklist")
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+    run(args)
