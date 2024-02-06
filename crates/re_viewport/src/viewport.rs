rerun-io/rerun

commit bc400a66903f80cdeafca9c2a97e249baf4988a0

Simplify viewport after tile drag-and-drop (#5044)
### What

This PR triggers a egui_tile tree simplification of the viewport upon
tile drag-and-drop to work around the spurious empty containers
typically left behind after a move. This happens only when the additive
workflow feature flag is set (otherwise, the on-going simplification
takes care of that).

This simplification is **agressive**, and may delete (empty) containers
that the user created on purpose, which is why we run it _only_ on
drag-and-drop. This is not ideal, but that's the best I can come up with
until we stop using egui_tiles as application-level data structure.
Still a net usability win.

* Part of #4996

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using newly built examples:
[app.rerun.io](https://app.rerun.io/pr/5044/index.html)
* Using examples from latest `main` build:
[app.rerun.io](https://app.rerun.io/pr/5044/index.html?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[app.rerun.io](https://app.rerun.io/pr/5044/index.html?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG

- [PR Build Summary](https://build.rerun.io/pr/5044)
- [Docs
preview](https://rerun.io/preview/267a55e088273ef961826f97d7d81c9552edcae2/docs)
<!--DOCS-PREVIEW-->
- [Examples
preview](https://rerun.io/preview/267a55e088273ef961826f97d7d81c9552edcae2/examples)
<!--EXAMPLES-PREVIEW-->
- [Recent benchmark results](https://build.rerun.io/graphs/crates.html)

crates/re_viewport/src/viewport.rs
@@ -5,7 +5,7 @@
 use std::collections::BTreeMap;
 
 use ahash::HashMap;
-use egui_tiles::Behavior as _;
+use egui_tiles::{Behavior as _, EditAction};
 use once_cell::sync::Lazy;
 
 use re_entity_db::EntityPropertyMap;
@@ -166,6 +166,11 @@ pub struct Viewport<'a, 'b> {
     /// We delay any modifications to the tree until the end of the frame,
     /// so that we don't mutate something while inspecting it.
     tree_action_receiver: std::sync::mpsc::Receiver<TreeAction>,
+
+    /// Tree action sender
+    ///
+    /// Used to pass along `TabViewer`.
+    tree_action_sender: std::sync::mpsc::Sender<TreeAction>,
 }
 
 impl<'a, 'b> Viewport<'a, 'b> {
@@ -174,6 +179,7 @@ impl<'a, 'b> Viewport<'a, 'b> {
         state: &'b mut ViewportState,
         space_view_class_registry: &SpaceViewClassRegistry,
         tree_action_receiver: std::sync::mpsc::Receiver<TreeAction>,
+        tree_action_sender: std::sync::mpsc::Sender<TreeAction>,
     ) -> Self {
         re_tracing::profile_function!();
 
@@ -196,6 +202,7 @@ impl<'a, 'b> Viewport<'a, 'b> {
             tree,
             tree_edited: edited,
             tree_action_receiver,
+            tree_action_sender,
         }
     }
 
@@ -271,6 +278,8 @@ impl<'a, 'b> Viewport<'a, 'b> {
                 edited: false,
                 executed_systems_per_space_view,
                 contents_per_tile_id,
+                tree_action_sender: self.tree_action_sender.clone(),
+                root_container_id: self.blueprint.root_container,
             };
 
             tree.ui(&mut tab_viewer, ui);
@@ -537,6 +546,8 @@ struct TabViewer<'a, 'b> {
     ctx: &'a ViewerContext<'b>,
     space_views: &'a BTreeMap<SpaceViewId, SpaceViewBlueprint>,
     maximized: &'a mut Option<SpaceViewId>,
+    root_container_id: Option<ContainerId>,
+    tree_action_sender: std::sync::mpsc::Sender<TreeAction>,
 
     /// List of query & system execution results for each space view.
     executed_systems_per_space_view: HashMap<SpaceViewId, (ViewQuery<'a>, SystemExecutionOutput)>,
@@ -770,8 +781,47 @@ impl<'a, 'b> egui_tiles::Behavior<SpaceViewId> for TabViewer<'a, 'b> {
 
     // Callbacks:
 
-    fn on_edit(&mut self) {
-        self.edited = true;
+    fn on_edit(&mut self, edit_action: egui_tiles::EditAction) {
+        match edit_action {
+            EditAction::TileResized | EditAction::TileDragged => {}
+            EditAction::TileDropped => {
+                // TODO(ab): when we finally stop using egui_tiles as application-level data
+                //                  structure, this work-around should be unnecessary.
+
+                // The continuous simplification options are considerably reduced when the additive
+                // workflow is enabled. Due to the egui_tiles -> blueprint synchronisation process,
+                // drag and drop operation often lead to many spurious empty containers. To work
+                // around this, we run a simplification pass when a drop occurs.
+                if self.ctx.app_options.experimental_additive_workflow {
+                    if let Some(root_container_id) = self.root_container_id {
+                        if self
+                            .tree_action_sender
+                            .send(TreeAction::SimplifyContainer(
+                                root_container_id,
+                                egui_tiles::SimplificationOptions {
+                                    prune_empty_tabs: true,
+                                    prune_empty_containers: false,
+                                    prune_single_child_tabs: true,
+                                    prune_single_child_containers: false,
+                                    all_panes_must_have_tabs: true,
+                                    join_nested_linear_containers: false,
+                                },
+                            ))
+                            .is_err()
+                        {
+                            re_log::warn_once!(
+                                "Channel between ViewportBlueprint and Viewport is broken"
+                            );
+                        }
+                    }
+                }
+
+                self.edited = true;
+            }
+            EditAction::TabSelected => {
+                self.edited = true;
+            }
+        }
     }
 }
 

Cargo.toml
@@ -295,6 +295,6 @@ emath = { git = "https://github.com/emilk/egui.git", rev = "c5352cf6c16b28d00432
 # egui-wgpu = { path = "../../egui/crates/egui-wgpu" }
 # emath = { path = "../../egui/crates/emath" }
 
-egui_tiles = { git = "https://github.com/rerun-io/egui_tiles", rev = "35e711283e7a021ca425d9fbd8e7581548971f49" } # master 2024-01-26
+egui_tiles = { git = "https://github.com/rerun-io/egui_tiles", rev = "be78596d3564f696b61a1ba14678b0aa0bfcdb2f" } # master 2024-02-05
 
 # egui_commonmark = { git = "https://github.com/rerun-io/egui_commonmark", rev = "3d83a92f995a1d18ab1172d0b129d496e0eedaae" } # Update to egui 0.25 https://github.com/lampsitter/egui_commonmark/pull/27

crates/re_viewer/src/app_state.rs
@@ -140,13 +140,17 @@ impl AppState {
         // this, which gives us interior mutability (only a shared reference of `ViewportBlueprint`
         // is available to the UI code) and, if needed in the future, concurrency.
         let (sender, receiver) = std::sync::mpsc::channel();
-        let viewport_blueprint =
-            ViewportBlueprint::try_from_db(store_context.blueprint, &blueprint_query, sender);
+        let viewport_blueprint = ViewportBlueprint::try_from_db(
+            store_context.blueprint,
+            &blueprint_query,
+            sender.clone(),
+        );
         let mut viewport = Viewport::new(
             &viewport_blueprint,
             viewport_state,
             space_view_class_registry,
             receiver,
+            sender,
         );
 
         // If the blueprint is invalid, reset it.

commit 7c45b6fa00fdd4f03a811a8a26c7876927d1a425

Fixes broken tile resize introduced by #5044 (#5064)
### What

- Fixes #5061

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using newly built examples:
[app.rerun.io](https://app.rerun.io/pr/5064/index.html)
* Using examples from latest `main` build:
[app.rerun.io](https://app.rerun.io/pr/5064/index.html?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[app.rerun.io](https://app.rerun.io/pr/5064/index.html?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG

- [PR Build Summary](https://build.rerun.io/pr/5064)
- [Docs
preview](https://rerun.io/preview/da3ca8a3ebf53647a43095c403a63b61007417c6/docs)
<!--DOCS-PREVIEW-->
- [Examples
preview](https://rerun.io/preview/da3ca8a3ebf53647a43095c403a63b61007417c6/examples)
<!--EXAMPLES-PREVIEW-->
- [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
- [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

crates/re_viewport/src/viewport.rs
@@ -797,7 +797,6 @@ impl<'a, 'b> egui_tiles::Behavior<SpaceViewId> for TabViewer<'a, 'b> {
 
     fn on_edit(&mut self, edit_action: egui_tiles::EditAction) {
         match edit_action {
-            EditAction::TileResized | EditAction::TileDragged => {}
             EditAction::TileDropped => {
                 // TODO(ab): when we finally stop using egui_tiles as application-level data
                 //                  structure, this work-around should be unnecessary.
@@ -832,8 +831,12 @@ impl<'a, 'b> egui_tiles::Behavior<SpaceViewId> for TabViewer<'a, 'b> {
 
                 self.edited = true;
             }
-            EditAction::TabSelected => {
+            EditAction::TabSelected | EditAction::TileResized => {
                 self.edited = true;
+            }
+            EditAction::TileDragged => {
+                // No synchronization needed, because TileDragged happens when a drag starts, so no tiles are actually
+                // modified. When the drag completes, then we get `TileDropped` and run the synchronization.
             }
         }
     }