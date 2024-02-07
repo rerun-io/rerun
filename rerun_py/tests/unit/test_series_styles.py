2d15625d3786ab72ca19f1082af4d6e603a71fc7
Jeremy Leibs <jeremy@rerun.io>
2024-02-07 20:35:48 +0100

    Test cases for SeriesLine as well

--- rerun_py/tests/unit/test_series_styles.py
+++ rerun_py/tests/unit/test_series_styles.py
@@ -1,7 +1,38 @@
 from __future__ import annotations
 
-from rerun.archetypes import SeriesPoint
-from rerun.components import Color, ColorBatch, MarkerShape, MarkerShapeBatch, MarkerSize, MarkerSizeBatch
+from rerun.archetypes import SeriesLine, SeriesPoint
+from rerun.components import (
+    Color,
+    ColorBatch,
+    MarkerShape,
+    MarkerShapeBatch,
+    MarkerSize,
+    MarkerSizeBatch,
+    Name,
+    NameBatch,
+    StrokeWidth,
+    StrokeWidthBatch,
+)
+
+
+def test_line_series() -> None:
+    inputs = [
+        SeriesLine(),
+        SeriesLine(color=[255, 0, 0]),
+        SeriesLine(color=0xFF0000FF),
+        SeriesLine(width=2),
+        SeriesLine(width=2.0),
+        SeriesLine(name="my plot"),
+        SeriesLine(color=[255, 0, 0], width=2, name="my plot"),
+    ]
+
+    for input in inputs:
+        if input.color is not None:
+            assert input.color == ColorBatch._optional([Color([255, 0, 0])])
+        if input.width is not None:
+            assert input.width == StrokeWidthBatch._optional([StrokeWidth(2.0)])
+        if input.name is not None:
+            assert input.name == NameBatch._optional([Name("my plot")])
 
 
 def test_point_series() -> None:
@@ -12,7 +43,8 @@ def test_point_series() -> None:
         SeriesPoint(marker_size=2),
         SeriesPoint(marker=MarkerShape.Diamond),
         SeriesPoint(marker="diamond"),
-        SeriesPoint(color=[255, 0, 0], marker_size=2, marker="diamond"),
+        SeriesPoint(name="my plot"),
+        SeriesPoint(color=[255, 0, 0], marker_size=2, marker="diamond", name="my plot"),
     ]
 
     for input in inputs:
@@ -22,6 +54,8 @@ def test_point_series() -> None:
             assert input.marker_size == MarkerSizeBatch._optional([MarkerSize(2.0)])
         if input.marker is not None:
             assert input.marker == MarkerShapeBatch._optional([MarkerShape.Diamond])
+        if input.name is not None:
+            assert input.name == NameBatch._optional([Name("my plot")])
 
 
 if __name__ == "__main__":
