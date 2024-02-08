afab83566de93ff303ab0b7647641457d7bc8cd4
Clement Rey <cr.rey.clement@gmail.com>
2024-02-08 12:37:14 +0100

    Fix broken CI (#5135)
    And remove `--quiet`, it makes CI debugging painful.

--- crates/re_types/src/archetypes/mesh3d.rs
+++ crates/re_types/src/archetypes/mesh3d.rs
@@ -37,7 +37,7 @@ use ::re_types_core::{DeserializationError, DeserializationResult};
 ///         &rerun::Mesh3D::new([[0.0, 1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
 ///             .with_vertex_normals([[0.0, 0.0, 1.0]])
 ///             .with_vertex_colors([0x0000FFFF, 0x00FF00FF, 0xFF0000FF])
-///             .with_mesh_properties(rerun::MeshProperties::from_triangle_indices([[2, 1, 0]]))
+///             .with_mesh_properties(rerun::MeshProperties::from_triangle_indices([[2, 1, 0]])),
 ///     )?;
 ///
 ///     Ok(())
