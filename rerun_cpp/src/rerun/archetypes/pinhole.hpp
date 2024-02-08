4536fefd8fd7468e67ec00c15188f7b9563bdaca
Clement Rey <cr.rey.clement@gmail.com>
2024-02-08 11:38:23 +0100

    codegen

--- rerun_cpp/src/rerun/archetypes/pinhole.hpp
+++ rerun_cpp/src/rerun/archetypes/pinhole.hpp
@@ -43,7 +43,7 @@ namespace rerun::archetypes {
     ///         return static_cast<uint8_t>(std::rand());
     ///     });
     ///
-    ///     rec.log("world/image", rerun::Image({3, 3}, random_data));
+    ///     rec.log("world/image", rerun::Image({3, 3, 3}, random_data));
     /// }
     /// ```
     struct Pinhole {
