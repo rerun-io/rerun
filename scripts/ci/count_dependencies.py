49b8ac6bf6b72b593dfa0200c96be3a58fdbb2df
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-02-19 17:12:17 +0100

    Add script to count crate dependencies

---
+++ scripts/ci/count_dependencies.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""
+Count the total number of dependendencies of a file (recursively).
+
+This produces the format for use in https://github.com/benchmark-action/github-action-benchmark.
+
+Use the script:
+    python3 scripts/ci/count_dependencies.py -p rerun --all-features
+    python3 scripts/ci/count_dependencies.py -p rerun --no-default-features
+"""
+from __future__ import annotations
+
+import argparse
+import json
+import os.path
+import sys
+from enum import Enum
+from pathlib import Path
+from typing import Any
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Count crate dependencies")
+
+    parser.add_argument("-p", required=True, type=str, help="Crate name")
+    parser.add_argument("--all-features", default=False, action="store_true", help="Use all features")
+    parser.add_argument("--no-default-features", default=False, action="store_true", help="Use no default features")
+
+    args = parser.parse_args()
+
+    crate = args.p
+    if args.all_features:
+        flags = "--all-features"
+    elif args.no_default_features:
+        flags = "--no-default-features"
+    else:
+        flags = ""
+    cmd = f'cargo tree --edges normal -p {crate} {flags} | tail -n +2 | rg "[a-z-_]+ " -o | sort -u | wc -l'
+    print(f"Running command: {cmd}", file=sys.stderr, flush=True)
+    count = int(os.popen(cmd).read().strip())
+    assert count > 0, f"Command failed. Maybe unknown crate? cmd: {cmd}"
+    print(json.dumps({"name": f"{crate} {flags}", "value": count, "unit": "crates"}))
+
+
+if __name__ == "__main__":
+    main()
