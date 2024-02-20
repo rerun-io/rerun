4a6a76a02141be3fe6f18da71df800e88f1754d0
Andreas Reich <r_andreas2@web.de>
2024-02-20 22:39:04 +0100

    add stl loader to re_renderer

---
+++ crates/re_renderer/src/importer/stl.rs
@@ -0,0 +1,105 @@
+use itertools::Itertools;
+use smallvec::smallvec;
+use tinystl::StlData;
+
+use crate::{mesh, renderer::MeshInstance, resource_managers::ResourceLifeTime, RenderContext};
+
+#[derive(thiserror::Error, Debug)]
+pub enum StlImportError {
+    #[error("Missing data in STL file.")]
+    MissingData,
+
+    #[error("Unexpected data in STL file at line {0}.")]
+    Unexpected(usize),
+
+    #[error("Failed to parse line {0} (usually due to a malformed vertex).")]
+    Parse(usize),
+
+    #[error("Failed to convert the number of triangles to a 32bit unsigned integer (as required by the STL specification).")]
+    TooManyFacets,
+
+    #[error("Failed to parse integer.")]
+    TryFromInt,
+
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error(transparent)]
+    MeshError(#[from] mesh::MeshError),
+
+    #[error(transparent)]
+    ResourceManagerError(#[from] crate::resource_managers::ResourceManagerError),
+}
+
+/// Load a [STL .stl file](https://en.wikipedia.org/wiki/STL_(file_format)) into the mesh manager.
+pub fn load_stl_from_buffer(
+    buffer: &[u8],
+    ctx: &RenderContext,
+) -> Result<Vec<MeshInstance>, StlImportError> {
+    re_tracing::profile_function!();
+
+    let cursor = std::io::Cursor::new(buffer);
+    let stl_data =
+        StlData::read_buffer(std::io::BufReader::new(cursor)).map_err(|err| match err {
+            tinystl::Error::MissingData => StlImportError::MissingData,
+            tinystl::Error::Unexpected(line) => StlImportError::Unexpected(line),
+            tinystl::Error::Parse(line) => StlImportError::Parse(line),
+            tinystl::Error::TooManyFacets(_) => StlImportError::TooManyFacets,
+            tinystl::Error::TryFromInt(_) => StlImportError::TryFromInt,
+            tinystl::Error::Io(err) => err.into(),
+        })?;
+
+    let StlData {
+        name,
+        triangles,
+        normals,
+        ..
+    } = stl_data;
+
+    let num_vertices = triangles.len() * 3;
+
+    let material = mesh::Material {
+        label: "default material".into(),
+        index_range: 0..num_vertices as u32,
+        albedo: ctx.texture_manager_2d.white_texture_unorm_handle().clone(),
+        albedo_multiplier: crate::Rgba::WHITE,
+    };
+
+    let mesh = mesh::Mesh {
+        label: name.into(),
+        triangle_indices: (0..num_vertices as u32)
+            .tuples::<(_, _, _)>()
+            .map(glam::UVec3::from)
+            .collect::<Vec<_>>(),
+        vertex_positions: bytemuck::cast_slice(&triangles).to_vec(),
+
+        // Normals on STL are per triangle, not per vertex.
+        // Yes, this makes STL always look faceted.
+        vertex_normals: normals
+            .into_iter()
+            .flat_map(|n| {
+                let n = glam::Vec3::from_array(n);
+                [n, n, n]
+            })
+            .collect(),
+
+        // STL has neither colors nor texcoords.
+        vertex_colors: vec![crate::Rgba32Unmul::WHITE; num_vertices],
+        vertex_texcoords: vec![glam::Vec2::ZERO; num_vertices],
+
+        materials: smallvec![material],
+    };
+
+    mesh.sanity_check()?;
+
+    let gpu_mesh = ctx
+        .mesh_manager
+        .write()
+        .create(ctx, &mesh, ResourceLifeTime::LongLived)?;
+
+    Ok(vec![MeshInstance {
+        gpu_mesh,
+        mesh: Some(std::sync::Arc::new(mesh)),
+        ..Default::default()
+    }])
+}
