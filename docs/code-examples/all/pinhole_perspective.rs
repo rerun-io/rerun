c639a7de9d911725d8bb1c113438c3cc46f12745
Clement Rey <cr.rey.clement@gmail.com>
2024-02-21 16:03:12 +0100

    Update docs/code-examples/all/pinhole_perspective.rs
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- docs/code-examples/all/pinhole_perspective.rs
+++ docs/code-examples/all/pinhole_perspective.rs
@@ -3,11 +3,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rec = rerun::RecordingStreamBuilder::new("rerun_example_pinhole_perspective").spawn()?;
 
-    let fov = 0.7853982;
+    let fov_y = 0.7853982;
     let aspect_ratio = 1.7777778;
     rec.log(
         "world/cam",
-        &rerun::Pinhole::from_fov_and_aspect_ratio(fov, aspect_ratio)
+        &rerun::Pinhole::from_fov_and_aspect_ratio(fov_y, aspect_ratio)
             .with_camera_xyz(rerun::components::ViewCoordinates::RUB),
     )?;
 
