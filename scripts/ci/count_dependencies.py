0223b21104bad205cfc5004aa33386d9b52fd603
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-02-21 09:25:12 +0100

    Add shortcomings

--- scripts/ci/count_dependencies.py
+++ scripts/ci/count_dependencies.py
@@ -8,6 +8,16 @@ This produces the format for use in https://github.com/benchmark-action/github-a
 Use the script:
     python3 scripts/ci/count_dependencies.py -p rerun --all-features
     python3 scripts/ci/count_dependencies.py -p rerun --no-default-features
+
+Unfortunately, this script under-counts compared to what `cargo build` outputs.
+There is also `cargo deps-list`, which also under-counts.
+For instance:
+
+* `scripts/ci/count_dependencies.py -p re_sdk --no-default-features`  => 118
+* `cargo deps-list -p re_sdk --no-default-features`                   => 165
+* `cargo check -p re_sdk --no-default-features`                       => 213
+
+So this script is more of a heurristic than an exact count.
 """
 from __future__ import annotations
 
