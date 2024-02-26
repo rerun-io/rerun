129a450ca4c9ac04171770967a43c0cf022ba843
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2024-02-26 13:23:52 +0100

    Context menu 4: add "Clone space view" action (#5265)
    ### What
    
    This PR adds "Clone Space View" to the context menu (when clicking a
    single space view). Also, now the the cloned space view appears just
    after the original space view, in the same container (previously it
    would typically appear at the end of the root container regardless of
    the position of the original space view).
    
    <img width="303" alt="image"
    src="https://github.com/rerun-io/rerun/assets/49431240/baea7ff7-4960-47b6-9aec-38b26b6c6be5">
    
    <br/>
    
    This PR also unearthed a bug that I'm surprised didn't surface earlier.
    The effect of `mark_user_interaction` wasn't visible until the next
    frame, as the value was only saved to blueprint, and not cached in the
    `ViewportBlurprint`. This could lead to an issue when `TreeAction` are
    processed end-of-frame and exposed to the "old" `mark_user_interaction`
    flag values (namely: unwanted reset of the tree). Those flags are now
    ~~`Mutex`es (to gain interior mutability)~~ `AtomicBool`s and updated in
    place (in addition to being saved to blueprint like before). cc @jleibs
    
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested the web demo (if applicable):
    * Using newly built examples:
    [app.rerun.io](https://app.rerun.io/pr/5265/index.html)
    * Using examples from latest `main` build:
    [app.rerun.io](https://app.rerun.io/pr/5265/index.html?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
    * Using full set of examples from `nightly` build:
    [app.rerun.io](https://app.rerun.io/pr/5265/index.html?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
    * [x] The PR title and labels are set such as to maximize their
    usefulness for the next release's CHANGELOG
    * [x] If applicable, add a new check to the [release
    checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
    
    - [PR Build Summary](https://build.rerun.io/pr/5265)
    - [Docs
    preview](https://rerun.io/preview/60bc7b0339ea578e528e1e7f9f8be76ff4b42b03/docs)
    <!--DOCS-PREVIEW-->
    - [Examples
    preview](https://rerun.io/preview/60bc7b0339ea578e528e1e7f9f8be76ff4b42b03/examples)
    <!--EXAMPLES-PREVIEW-->
    - [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
    - [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

--- crates/re_viewport/src/viewport.rs
+++ crates/re_viewport/src/viewport.rs
@@ -87,10 +87,10 @@ impl ViewportState {
 
 /// Mutation actions to perform on the tree at the end of the frame. These messages are sent by the mutation APIs from
 /// [`crate::ViewportBlueprint`].
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum TreeAction {
     /// Add a new space view to the provided container (or the root if `None`).
-    AddSpaceView(SpaceViewId, Option<ContainerId>),
+    AddSpaceView(SpaceViewId, Option<ContainerId>, Option<usize>),
 
     /// Add a new container of the provided kind to the provided container (or the root if `None`).
     AddContainer(egui_tiles::ContainerKind, Option<ContainerId>),
@@ -288,7 +288,7 @@ impl<'a, 'b> Viewport<'a, 'b> {
             // If so we can no longer automatically change the layout without discarding user edits.
             let is_dragging_a_tile = tree.dragged_id(ui.ctx()).is_some();
             if tab_viewer.edited || is_dragging_a_tile {
-                if blueprint.auto_layout {
+                if blueprint.auto_layout() {
                     re_log::trace!(
                         "The user is manipulating the egui_tiles tree - will no longer auto-layout"
                     );
@@ -331,7 +331,7 @@ impl<'a, 'b> Viewport<'a, 'b> {
             space_view.on_frame_start(ctx, space_view_state.as_mut(), auto_properties);
         }
 
-        if self.blueprint.auto_space_views {
+        if self.blueprint.auto_space_views() {
             self.spawn_heuristic_space_views(ctx);
         }
     }
@@ -392,6 +392,7 @@ impl<'a, 'b> Viewport<'a, 'b> {
                 }),
                 ctx,
                 None,
+                None,
             );
         }
     }
@@ -407,9 +408,10 @@ impl<'a, 'b> Viewport<'a, 'b> {
 
         // TODO(#4687): Be extra careful here. If we mark edited inappropriately we can create an infinite edit loop.
         for tree_action in self.tree_action_receiver.try_iter() {
+            re_log::trace!("Processing tree action: {tree_action:?}");
             match tree_action {
-                TreeAction::AddSpaceView(space_view_id, parent_container) => {
-                    if self.blueprint.auto_layout {
+                TreeAction::AddSpaceView(space_view_id, parent_container, position_in_parent) => {
+                    if self.blueprint.auto_layout() {
                         // Re-run the auto-layout next frame:
                         re_log::trace!(
                             "Added a space view with no user edits yet - will re-run auto-layout"
@@ -420,11 +422,20 @@ impl<'a, 'b> Viewport<'a, 'b> {
                         parent_container.or(self.blueprint.root_container)
                     {
                         let tile_id = self.tree.tiles.insert_pane(space_view_id);
+                        let container_tile_id = blueprint_id_to_tile_id(&parent_id);
                         if let Some(egui_tiles::Tile::Container(container)) =
-                            self.tree.tiles.get_mut(blueprint_id_to_tile_id(&parent_id))
+                            self.tree.tiles.get_mut(container_tile_id)
                         {
                             re_log::trace!("Inserting new space view into root container");
                             container.add_child(tile_id);
+                            if let Some(position_in_parent) = position_in_parent {
+                                self.tree.move_tile_to_container(
+                                    tile_id,
+                                    container_tile_id,
+                                    position_in_parent,
+                                    true,
+                                );
+                            }
                         } else {
                             re_log::trace!("Root was not a container - will re-run auto-layout");
                             reset = true;
--- crates/re_viewport/src/viewport_blueprint.rs
+++ crates/re_viewport/src/viewport_blueprint.rs
@@ -1,7 +1,9 @@
 use std::collections::BTreeMap;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 use ahash::HashMap;
 use egui_tiles::{SimplificationOptions, TileId};
+
 use re_data_store::LatestAtQuery;
 use re_entity_db::EntityPath;
 use re_query::query_archetype;
@@ -42,10 +44,13 @@ pub struct ViewportBlueprint {
     /// Whether the viewport layout is determined automatically.
     ///
     /// Set to `false` the first time the user messes around with the viewport blueprint.
-    pub auto_layout: bool,
+    /// Note: we use a mutex here because writes needs to be effective immediately during the frame.
+    auto_layout: AtomicBool,
 
-    /// Whether or not space views should be created automatically.
-    pub auto_space_views: bool,
+    /// Whether space views should be created automatically.
+    ///
+    /// Note: we use a mutex here because writes needs to be effective immediately during the frame.
+    auto_space_views: AtomicBool,
 
     /// Channel to pass Blueprint mutation messages back to the [`crate::Viewport`]
     tree_action_sender: std::sync::mpsc::Sender<TreeAction>,
@@ -139,8 +144,8 @@ impl ViewportBlueprint {
             root_container,
             tree,
             maximized,
-            auto_layout,
-            auto_space_views,
+            auto_layout: auto_layout.into(),
+            auto_space_views: auto_space_views.into(),
             tree_action_sender,
         }
 
@@ -239,7 +244,17 @@ impl ViewportBlueprint {
         let new_space_view = space_view.duplicate(ctx.store_context.blueprint, ctx.blueprint_query);
         let new_space_view_id = new_space_view.id;
 
-        self.add_space_views(std::iter::once(new_space_view), ctx, None);
+        let parent_and_pos =
+            self.find_parent_and_position_index(&Contents::SpaceView(*space_view_id));
+
+        self.add_space_views(
+            std::iter::once(new_space_view),
+            ctx,
+            parent_and_pos.map(|(parent, _)| parent),
+            parent_and_pos.map(|(_, pos)| pos),
+        );
+
+        self.mark_user_interaction(ctx);
 
         Some(new_space_view_id)
     }
@@ -267,7 +282,7 @@ impl ViewportBlueprint {
     }
 
     pub fn mark_user_interaction(&self, ctx: &ViewerContext<'_>) {
-        if self.auto_layout {
+        if self.auto_layout() {
             re_log::trace!("User edits - will no longer auto-layout");
         }
 
@@ -294,6 +309,7 @@ impl ViewportBlueprint {
         space_views: impl Iterator<Item = SpaceViewBlueprint>,
         ctx: &ViewerContext<'_>,
         parent_container: Option<ContainerId>,
+        position_in_parent: Option<usize>,
     ) -> Vec<SpaceViewId> {
         let mut new_ids: Vec<_> = vec![];
 
@@ -309,7 +325,11 @@ impl ViewportBlueprint {
 
         if !new_ids.is_empty() {
             for id in &new_ids {
-                self.send_tree_action(TreeAction::AddSpaceView(*id, parent_container));
+                self.send_tree_action(TreeAction::AddSpaceView(
+                    *id,
+                    parent_container,
+                    position_in_parent,
+                ));
             }
 
             let updated_ids: Vec<_> = self.space_views.keys().chain(new_ids.iter()).collect();
@@ -556,7 +576,7 @@ impl ViewportBlueprint {
             Contents::Container(container_id) => {
                 if let Some(container) = self.container(container_id) {
                     if visible != container.visible {
-                        if self.auto_layout {
+                        if self.auto_layout() {
                             re_log::trace!(
                                 "Container visibility changed - will no longer auto-layout"
                             );
@@ -574,7 +594,7 @@ impl ViewportBlueprint {
             Contents::SpaceView(space_view_id) => {
                 if let Some(space_view) = self.space_view(space_view_id) {
                     if visible != space_view.visible {
-                        if self.auto_layout {
+                        if self.auto_layout() {
                             re_log::trace!(
                                 "Space-view visibility changed - will no longer auto-layout"
                             );
@@ -617,18 +637,32 @@ impl ViewportBlueprint {
 
     #[inline]
     pub fn set_auto_layout(&self, value: bool, ctx: &ViewerContext<'_>) {
-        if self.auto_layout != value {
+        let old_value = self.auto_layout.swap(value, Ordering::SeqCst);
+
+        if old_value != value {
             let component = AutoLayout(value);
             ctx.save_blueprint_component(&VIEWPORT_PATH.into(), component);
         }
     }
 
     #[inline]
+    pub fn auto_layout(&self) -> bool {
+        self.auto_layout.load(Ordering::SeqCst)
+    }
+
+    #[inline]
     pub fn set_auto_space_views(&self, value: bool, ctx: &ViewerContext<'_>) {
-        if self.auto_space_views != value {
+        let old_value = self.auto_space_views.swap(value, Ordering::SeqCst);
+
+        if old_value != value {
             let component = AutoSpaceViews(value);
             ctx.save_blueprint_component(&VIEWPORT_PATH.into(), component);
         }
+    }
+
+    #[inline]
+    pub fn auto_space_views(&self) -> bool {
+        self.auto_space_views.load(Ordering::SeqCst)
     }
 
     #[inline]
