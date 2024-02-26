e50746217d611a7b75b236061b2aefb3c2273aa1
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-02-26 14:21:39 +0100

    Increase the max WebSocket frame limit for the native client (#5282)
    * Closes https://github.com/rerun-io/rerun/issues/5268
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested the web demo (if applicable):
    * Using newly built examples:
    [app.rerun.io](https://app.rerun.io/pr/5282/index.html)
    * Using examples from latest `main` build:
    [app.rerun.io](https://app.rerun.io/pr/5282/index.html?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
    * Using full set of examples from `nightly` build:
    [app.rerun.io](https://app.rerun.io/pr/5282/index.html?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
    * [x] The PR title and labels are set such as to maximize their
    usefulness for the next release's CHANGELOG
    * [x] If applicable, add a new check to the [release
    checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
    
    - [PR Build Summary](https://build.rerun.io/pr/5282)
    - [Docs
    preview](https://rerun.io/preview/d30ec257f0553f728aaf28fd500aa2a8633bd4c3/docs)
    <!--DOCS-PREVIEW-->
    - [Examples
    preview](https://rerun.io/preview/d30ec257f0553f728aaf28fd500aa2a8633bd4c3/examples)
    <!--EXAMPLES-PREVIEW-->
    - [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
    - [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

--- crates/re_ws_comms/src/client.rs
+++ crates/re_ws_comms/src/client.rs
@@ -10,8 +10,17 @@ pub fn viewer_to_server(
     url: String,
     on_binary_msg: impl Fn(Vec<u8>) -> ControlFlow<()> + Send + 'static,
 ) -> Result<()> {
+    let gigs = 1024 * 1024 * 1024;
+    let options = ewebsock::Options {
+        // This limits the size of one chunk of rerun log data when running a local websocket client.
+        // We set a very high limit, because we should be able to trust the server.
+        // See https://github.com/rerun-io/rerun/issues/5268 for more
+        max_incoming_frame_size: 2 * gigs,
+    };
+
     ewebsock::ws_receive(
         url.clone(),
+        options,
         Box::new(move |event: WsEvent| match event {
             WsEvent::Opened => {
                 re_log::info!("Connection to {url} established");
