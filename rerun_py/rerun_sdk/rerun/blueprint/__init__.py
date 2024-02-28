504f7e2f05bbe623d56b0de29459127439398311
Jeremy Leibs <jeremy@rerun.io>
2024-02-28 12:04:29 -0500

    Change the pattern for relative imports in scoped components (#5322)
    ### What
    
    This is a complex problem that probably warrants a different long-term
    solution, but this unblocks things for now.
    
    The normal pattern of `from ...blueprint import components` created a
    potential conflict when an archetype used a mix of components from
    blueprint and core components.
    
    The next fix we tried of just import `from ... import blueprint`
    appeared to work, but led to circular import errors on archetypes, since
    components would try to import blueprints which would include archetypes
    before components was initialized.
    
    We now only import the direct packages we need from the scope, but
    introduce an alias `{scope}_{type}` to avoid the collision.
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested the web demo (if applicable):
    * Using newly built examples:
    [app.rerun.io](https://app.rerun.io/pr/5322/index.html)
    * Using examples from latest `main` build:
    [app.rerun.io](https://app.rerun.io/pr/5322/index.html?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
    * Using full set of examples from `nightly` build:
    [app.rerun.io](https://app.rerun.io/pr/5322/index.html?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
    * [x] The PR title and labels are set such as to maximize their
    usefulness for the next release's CHANGELOG
    * [x] If applicable, add a new check to the [release
    checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
    
    - [PR Build Summary](https://build.rerun.io/pr/5322)
    - [Docs
    preview](https://rerun.io/preview/92be21320f9b2c0726c86dc1f5ccacd4719e3624/docs)
    <!--DOCS-PREVIEW-->
    - [Examples
    preview](https://rerun.io/preview/92be21320f9b2c0726c86dc1f5ccacd4719e3624/examples)
    <!--EXAMPLES-PREVIEW-->
    - [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
    - [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

--- rerun_py/rerun_sdk/rerun/blueprint/__init__.py
+++ rerun_py/rerun_sdk/rerun/blueprint/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from . import archetypes, components, datatypes
-
 __all__ = ["archetypes", "datatypes", "components"]
+
+from . import archetypes, components, datatypes
