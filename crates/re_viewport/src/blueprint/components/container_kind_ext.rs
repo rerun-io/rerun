315ccce57878cf060f016664c8be5ff36b0b1a8d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-02-28 16:38:49 +0100

    Replace ContainerKind with codegenerated enum

--- crates/re_viewport/src/blueprint/components/container_kind_ext.rs
+++ crates/re_viewport/src/blueprint/components/container_kind_ext.rs
@@ -3,22 +3,21 @@ use super::ContainerKind;
 impl From<egui_tiles::ContainerKind> for ContainerKind {
     fn from(value: egui_tiles::ContainerKind) -> Self {
         match value {
-            egui_tiles::ContainerKind::Tabs => Self(1),
-            egui_tiles::ContainerKind::Horizontal => Self(2),
-            egui_tiles::ContainerKind::Vertical => Self(3),
-            egui_tiles::ContainerKind::Grid => Self(4),
+            egui_tiles::ContainerKind::Tabs => Self::Tabs,
+            egui_tiles::ContainerKind::Horizontal => Self::Horizontal,
+            egui_tiles::ContainerKind::Vertical => Self::Vertical,
+            egui_tiles::ContainerKind::Grid => Self::Grid,
         }
     }
 }
 
 impl From<ContainerKind> for egui_tiles::ContainerKind {
     fn from(value: ContainerKind) -> Self {
-        match value.0 {
-            1 => egui_tiles::ContainerKind::Tabs,
-            2 => egui_tiles::ContainerKind::Horizontal,
-            3 => egui_tiles::ContainerKind::Vertical,
-            4 => egui_tiles::ContainerKind::Grid,
-            _ => egui_tiles::ContainerKind::default(),
+        match value {
+            ContainerKind::Tabs => Self::Tabs,
+            ContainerKind::Horizontal => Self::Horizontal,
+            ContainerKind::Vertical => Self::Vertical,
+            ContainerKind::Grid => Self::Grid,
         }
     }
 }
