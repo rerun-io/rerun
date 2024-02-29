8054264938871cdb8dfb1cdcc1e64aa69354bf5c
Antoine Beyeler <antoine@rerun.io>
2024-02-29 18:16:37 +0100

    Better api

--- crates/re_viewer_context/src/collapsed_id.rs
+++ crates/re_viewer_context/src/collapsed_id.rs
@@ -18,6 +18,7 @@ pub trait CollapsedIdScope {
 /// A collapsed identifier.
 ///
 /// This enum helps to generate scoped, stable [`egui::Id`]s for various pieces of data.
+#[derive(Debug, Clone)]
 pub enum CollapsedId<Scope: crate::collapsed_id::CollapsedIdScope> {
     Container(ContainerId),
     SpaceView(SpaceViewId),
@@ -32,6 +33,12 @@ pub enum CollapsedId<Scope: crate::collapsed_id::CollapsedIdScope> {
 
 impl<Scope: CollapsedIdScope> From<CollapsedId<Scope>> for egui::Id {
     fn from(id: CollapsedId<Scope>) -> Self {
+        (&id).into()
+    }
+}
+
+impl<Scope: CollapsedIdScope> From<&CollapsedId<Scope>> for egui::Id {
+    fn from(id: &CollapsedId<Scope>) -> Self {
         let base_id = match id {
             CollapsedId::Container(container_id) => egui::Id::new(container_id.hash()),
             CollapsedId::SpaceView(space_view_id) => egui::Id::new(space_view_id.hash()),
@@ -39,10 +46,34 @@ impl<Scope: CollapsedIdScope> From<CollapsedId<Scope>> for egui::Id {
                 egui::Id::new((space_view_id.hash(), entity_path.hash()))
             }
             CollapsedId::Entity(entity_path) => egui::Id::new(entity_path.hash64()),
-            CollapsedId::EguiId(id, _) => id,
+            CollapsedId::EguiId(id, _) => *id,
         };
 
         base_id.with(Scope::identifier())
+    }
+}
+
+impl<Scope: CollapsedIdScope> CollapsedId<Scope> {
+    /// Convert to an [`egui::Id`].
+    pub fn egui_id(&self) -> egui::Id {
+        self.into()
+    }
+
+    /// Check the collapsed state for the given ID.
+    pub fn is_open(&self, ctx: &egui::Context) -> Option<bool> {
+        egui::collapsing_header::CollapsingState::load(ctx, self.egui_id())
+            .map(|state| state.is_open())
+    }
+
+    /// Set the collapsed state for the given ID.
+    pub fn set_open(&self, ctx: &mut egui::Context, open: bool) {
+        let mut collapsing_state = egui::collapsing_header::CollapsingState::load_with_default_open(
+            ctx,
+            self.egui_id(),
+            false,
+        );
+        collapsing_state.set_open(open);
+        collapsing_state.store(ctx);
     }
 }
 
