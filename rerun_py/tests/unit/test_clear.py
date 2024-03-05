bfd1a67b0b17e37f2c4a0dc5da8eb034bd465734
Andreas Reich <r_andreas2@web.de>
2024-03-05 14:40:54 +0100

    add truthiness test to ClearIsRecursive test

--- rerun_py/tests/unit/test_clear.py
+++ rerun_py/tests/unit/test_clear.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+import numpy as np
 import rerun as rr
-from rerun.components import ClearIsRecursiveBatch
+from rerun.components import ClearIsRecursive, ClearIsRecursiveBatch
 
 
 def test_clear() -> None:
@@ -17,6 +18,15 @@ def test_clear() -> None:
 def test_clear_factory_methods() -> None:
     assert rr.Clear(recursive=True) == rr.Clear.recursive()
     assert rr.Clear(recursive=False) == rr.Clear.flat()
+
+
+def test_truthiness() -> None:
+    assert ClearIsRecursive(recursive=True)
+    assert not ClearIsRecursive(recursive=False)
+    assert np.array_equal(
+        np.array([ClearIsRecursive(recursive=True), ClearIsRecursive(recursive=False)], dtype=np.bool_),
+        np.array([True, False], dtype=np.bool_)
+    )
 
 
 if __name__ == "__main__":
