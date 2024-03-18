060b4b22159dbe5c956327150323411551bbe322
Clement Rey <cr.rey.clement@gmail.com>
2024-03-18 11:39:51 +0100

    update doc

--- crates/re_data_source/src/data_loader/mod.rs
+++ crates/re_data_source/src/data_loader/mod.rs
@@ -16,7 +16,8 @@ use re_log_types::{ArrowMsg, DataRow, EntityPath, LogMsg, TimePoint};
 /// * `--recording-id <store_id>`
 /// * `--opened-recording-id <opened_store_id>` (if set)
 /// * `--entity-path-prefix <entity_path_prefix>` (if set)
-/// * `--timeless` (if `timepoint` is set to the timeless timepoint)
+/// * `--static` (if `timepoint` is set to the timeless timepoint)
+/// * `--timeless` [deprecated] (if `timepoint` is set to the timeless timepoint)
 /// * `--time <timeline1>=<time1> <timeline2>=<time2> ...` (if `timepoint` contains temporal data)
 /// * `--sequence <timeline1>=<seq1> <timeline2>=<seq2> ...` (if `timepoint` contains sequence data)
 #[derive(Debug, Clone)]
