3f975fa346e253efe14781056ead4327e1e4c1ee
Andreas Reich <r_andreas2@web.de>
2024-03-19 10:56:08 +0100

    make blueprint example a little bit prettier

--- examples/python/blueprint/main.py
+++ examples/python/blueprint/main.py
@@ -10,7 +10,7 @@ from rerun.blueprint import Blueprint, BlueprintPanel, Grid, SelectionPanel, Spa
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Different options for how we might use blueprint")
+    parser = argparse.ArgumentParser(description="Different options for how to use blueprint")
 
     parser.add_argument("--skip-blueprint", action="store_true", help="Don't send the blueprint")
     parser.add_argument("--auto-space-views", action="store_true", help="Automatically add space views")
@@ -39,13 +39,12 @@ def main() -> None:
         )
 
     rr.init("rerun_example_blueprint", spawn=True, blueprint=blueprint)
+    img = np.array([[(147, 204, 234), (9, 31, 146)], [(9, 31, 146), (147, 204, 234)]], dtype=np.uint8)
+    img = np.tile(img, (8, 8, 1))
 
-    img = np.zeros([128, 128, 3], dtype="uint8")
-    for i in range(8):
-        img[(i * 16) + 4 : (i * 16) + 12, :] = (0, 0, 200)
     rr.log("image", rr.Image(img))
-    rr.log("rect/0", rr.Boxes2D(mins=[16, 16], sizes=[64, 64], labels="Rect0", colors=(255, 0, 0)))
-    rr.log("rect/1", rr.Boxes2D(mins=[48, 48], sizes=[64, 64], labels="Rect1", colors=(0, 255, 0)))
+    rr.log("rect/0", rr.Boxes2D(mins=[1, 1], sizes=[4, 4], labels="Rect0", colors=(255, 0, 0)))
+    rr.log("rect/1", rr.Boxes2D(mins=[6, 6], sizes=[4, 4], labels="Rect1", colors=(0, 255, 0)))
 
 
 if __name__ == "__main__":
