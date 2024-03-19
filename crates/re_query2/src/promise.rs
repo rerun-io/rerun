b3357a0512c314c916bf311d62efa961dc131415
Clement Rey <cr.rey.clement@gmail.com>
2024-03-19 09:55:02 +0100

    add Promise-related types

---
+++ crates/re_query2/src/promise.rs
@@ -0,0 +1,128 @@
+use std::sync::Arc;
+
+use re_log_types::DataCell;
+
+// ---
+
+/// Uniquely identifies a [`Promise`].
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct PromiseId(pub(crate) re_tuid::Tuid);
+
+impl std::fmt::Display for PromiseId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl PromiseId {
+    /// Create a new unique [`PromiseId`] based on the current time.
+    #[allow(clippy::new_without_default)]
+    #[inline]
+    pub fn new() -> Self {
+        Self(re_tuid::Tuid::new())
+    }
+}
+
+// ---
+
+/// A [`Promise`] turns a source [`DataCell`] into a new [`DataCell`] with the helper of a
+/// [`PromiseResolver`].
+///
+/// Each promise is uniquely identified via a [`PromiseId`].
+///
+/// [`Promise`]s can be cloned cheaply.
+#[derive(Debug, Clone)]
+pub struct Promise {
+    id: PromiseId,
+    source: DataCell,
+}
+
+impl Promise {
+    #[inline]
+    pub fn new(source: DataCell) -> Self {
+        Self {
+            id: PromiseId::new(),
+            source,
+        }
+    }
+}
+
+// ---
+
+/// Resolves and keeps track of [`Promise`]s.
+#[derive(Default)]
+pub struct PromiseResolver {}
+
+impl PromiseResolver {
+    /// Resolves the given [`Promise`].
+    ///
+    /// If the method returns [`PromiseResult::Pending`], you should call it again with the same
+    /// [`Promise`] until it either resolves it or fails irrecoverably.
+    ///
+    /// Once a [`Promise`] has left the `Pending` state, `resolve`ing it again is cached and
+    /// idempotent (the [`PromiseResolver`] keeps track of the state of all [`Promise`]s, both
+    /// pending and already resolved).
+    #[inline]
+    pub fn resolve(&mut self, promise: &Promise) -> PromiseResult<DataCell> {
+        _ = self;
+        _ = promise.id;
+        PromiseResult::Ready(promise.source.clone())
+    }
+}
+
+/// The result of resolving a [`Promise`] through a [`PromiseResolver`].
+#[derive(Clone)]
+pub enum PromiseResult<T> {
+    /// The resolution process is still in progress.
+    ///
+    /// Try calling [`PromiseResolver::resolve`] again.
+    Pending,
+
+    /// The [`Promise`] failed to resolve due to an irrecoverable error.
+    Error(Arc<dyn std::error::Error + Send + Sync>),
+
+    /// The [`Promise`] has been fully resolved.
+    Ready(T),
+}
+
+impl<T> PromiseResult<T> {
+    /// Applies the given transformation to the [`PromiseResult`] iff it's `Ready`.
+    #[inline]
+    pub fn map<B, F>(self, mut f: F) -> PromiseResult<B>
+    where
+        F: FnMut(T) -> B,
+    {
+        match self {
+            PromiseResult::Ready(v) => PromiseResult::Ready(f(v)),
+            PromiseResult::Pending => PromiseResult::Pending,
+            PromiseResult::Error(err) => PromiseResult::Error(err),
+        }
+    }
+
+    /// Unwraps the resolved result if it's `Ready`, panics otherwise.
+    #[inline]
+    pub fn unwrap(self) -> T {
+        match self {
+            PromiseResult::Ready(v) => v,
+            PromiseResult::Pending => panic!("tried to unwrap a pending `PromiseResult`"),
+            PromiseResult::Error(err) => {
+                panic!("tried to unwrap an errored `PromiseResult`: {err}")
+            }
+        }
+    }
+}
+
+impl<T, E: 'static + std::error::Error + Send + Sync> PromiseResult<Result<T, E>> {
+    /// Given a [`PromiseResult`] of a `Result`, flattens it down to a single layer [`PromiseResult`].
+    #[inline]
+    pub fn flatten(self) -> PromiseResult<T> {
+        match self {
+            PromiseResult::Ready(res) => match res {
+                Ok(v) => PromiseResult::Ready(v),
+                Err(err) => PromiseResult::Error(Arc::new(err) as _),
+            },
+            PromiseResult::Pending => PromiseResult::Pending,
+            PromiseResult::Error(err) => PromiseResult::Error(err),
+        }
+    }
+}
