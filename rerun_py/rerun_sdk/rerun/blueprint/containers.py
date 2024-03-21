dbb390bc71ddbccffad44028328ebf04663822a3
Antoine Beyeler <antoine@rerun.io>
2024-03-21 16:33:25 +0100

    Update API

--- rerun_py/rerun_sdk/rerun/blueprint/containers.py
+++ rerun_py/rerun_sdk/rerun/blueprint/containers.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import Optional
 
+from ..datatypes import Utf8Like
 from .api import Container, SpaceView
 from .components import ColumnShareArrayLike, RowShareArrayLike
 from .components.container_kind import ContainerKind
@@ -10,7 +11,12 @@ from .components.container_kind import ContainerKind
 class Horizontal(Container):
     """A horizontal container."""
 
-    def __init__(self, *contents: Container | SpaceView, column_shares: Optional[ColumnShareArrayLike] = None):
+    def __init__(
+        self,
+        *contents: Container | SpaceView,
+        column_shares: Optional[ColumnShareArrayLike] = None,
+        name: Utf8Like | None = None,
+    ):
         """
         Construct a new horizontal container.
 
@@ -21,15 +27,22 @@ class Horizontal(Container):
         column_shares
             The layout shares of the columns in the container. The share is used to determine what fraction of the total width each
             column should take up. The column with index `i` will take up the fraction `shares[i] / total_shares`.
+        name
+            The name of the container
 
         """
-        super().__init__(*contents, kind=ContainerKind.Horizontal, column_shares=column_shares)
+        super().__init__(*contents, kind=ContainerKind.Horizontal, column_shares=column_shares, name=name)
 
 
 class Vertical(Container):
     """A vertical container."""
 
-    def __init__(self, *contents: Container | SpaceView, row_shares: Optional[RowShareArrayLike] = None):
+    def __init__(
+        self,
+        *contents: Container | SpaceView,
+        row_shares: Optional[RowShareArrayLike] = None,
+        name: Utf8Like | None = None,
+    ):
         """
         Construct a new vertical container.
 
@@ -40,9 +53,11 @@ class Vertical(Container):
         row_shares
             The layout shares of the rows in the container. The share is used to determine what fraction of the total height each
             row should take up. The row with index `i` will take up the fraction `shares[i] / total_shares`.
+        name
+            The name of the container
 
         """
-        super().__init__(*contents, kind=ContainerKind.Vertical, row_shares=row_shares)
+        super().__init__(*contents, kind=ContainerKind.Vertical, row_shares=row_shares, name=name)
 
 
 class Grid(Container):
@@ -54,6 +69,7 @@ class Grid(Container):
         column_shares: Optional[ColumnShareArrayLike] = None,
         row_shares: Optional[RowShareArrayLike] = None,
         grid_columns: Optional[int] = None,
+        name: Utf8Like | None = None,
     ):
         """
         Construct a new grid container.
@@ -70,6 +86,8 @@ class Grid(Container):
             row should take up. The row with index `i` will take up the fraction `shares[i] / total_shares`.
         grid_columns
             The number of columns in the grid.
+        name
+            The name of the container
 
         """
         super().__init__(
@@ -78,13 +96,19 @@ class Grid(Container):
             column_shares=column_shares,
             row_shares=row_shares,
             grid_columns=grid_columns,
+            name=name,
         )
 
 
 class Tabs(Container):
     """A tab container."""
 
-    def __init__(self, *contents: Container | SpaceView, active_tab: Optional[int | str] = None):
+    def __init__(
+        self,
+        *contents: Container | SpaceView,
+        active_tab: Optional[int | str] = None,
+        name: Utf8Like | None = None,
+    ):
         """
         Construct a new tab container.
 
@@ -94,6 +118,8 @@ class Tabs(Container):
             All positional arguments are the contents of the container, which may be either other containers or space views.
         active_tab:
             The index or name of the active tab.
+        name
+            The name of the container
 
         """
-        super().__init__(*contents, kind=ContainerKind.Tabs, active_tab=active_tab)
+        super().__init__(*contents, kind=ContainerKind.Tabs, active_tab=active_tab, name=name)
