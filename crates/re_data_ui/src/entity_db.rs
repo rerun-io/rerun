b3b2b648f58f9c7661b3fc81480673177d936987
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-03-22 11:21:43 +0100

    Show "sibling" stores when a store is selected

--- crates/re_data_ui/src/entity_db.rs
+++ crates/re_data_ui/src/entity_db.rs
@@ -1,7 +1,13 @@
-impl crate::DataUi for re_entity_db::EntityDb {
+use re_entity_db::EntityDb;
+use re_log_types::StoreKind;
+use re_viewer_context::ViewerContext;
+
+use crate::item_ui::entity_db_button_ui;
+
+impl crate::DataUi for EntityDb {
     fn data_ui(
         &self,
-        ctx: &re_viewer_context::ViewerContext<'_>,
+        ctx: &ViewerContext<'_>,
         ui: &mut egui::Ui,
         verbosity: re_viewer_context::UiVerbosity,
         _query: &re_data_store::LatestAtQuery,
@@ -25,12 +31,6 @@ impl crate::DataUi for re_entity_db::EntityDb {
             re_ui.grid_left_hand_label(ui, &format!("{} ID", self.store_id().kind));
             ui.label(self.store_id().to_string());
             ui.end_row();
-
-            if let Some(data_source) = &self.data_source {
-                re_ui.grid_left_hand_label(ui, "Data source");
-                ui.label(data_source.to_string());
-                ui.end_row();
-            }
 
             if let Some(store_info) = self.store_info() {
                 let re_log_types::StoreInfo {
@@ -60,6 +60,71 @@ impl crate::DataUi for re_entity_db::EntityDb {
                     ui.label(store_kind.to_string());
                     ui.end_row();
                 }
+            }
+
+            if let Some(data_source) = &self.data_source {
+                re_ui.grid_left_hand_label(ui, "Data source");
+                ui.label(data_source.to_string());
+                ui.end_row();
+            }
+        });
+
+        sibling_stores_ui(ctx, ui, self);
+    }
+}
+
+/// Show the other stores in the same data source.
+fn sibling_stores_ui(ctx: &ViewerContext<'_>, ui: &mut egui::Ui, entity_db: &EntityDb) {
+    let Some(data_source) = &entity_db.data_source else {
+        return;
+    };
+
+    // Find other stores from the same data source
+    // (e.g. find the blueprint in this .rrd file, if any).
+    let mut other_recordings = vec![];
+    let mut other_blueprints = vec![];
+
+    for other in ctx
+        .store_context
+        .bundle
+        .entity_dbs_from_channel_source(data_source)
+    {
+        if other.store_id() == entity_db.store_id() {
+            continue;
+        }
+        match other.store_kind() {
+            StoreKind::Recording => {
+                other_recordings.push(other);
+            }
+            StoreKind::Blueprint => {
+                other_blueprints.push(other);
+            }
+        }
+    }
+
+    if !other_recordings.is_empty() {
+        ui.add_space(4.0);
+        if entity_db.store_kind() == StoreKind::Recording {
+            ui.strong("Other recordings in this data source");
+        } else {
+            ui.strong("Recordings in this data source");
+        }
+        ui.indent("recordings", |ui| {
+            for entity_db in other_recordings {
+                entity_db_button_ui(ctx, ui, entity_db, None);
+            }
+        });
+    }
+    if !other_blueprints.is_empty() {
+        ui.add_space(4.0);
+        if entity_db.store_kind() == StoreKind::Blueprint {
+            ui.strong("Other blueprints in this data source");
+        } else {
+            ui.strong("Blueprints in this data source");
+        }
+        ui.indent("blueprints", |ui| {
+            for entity_db in other_blueprints {
+                entity_db_button_ui(ctx, ui, entity_db, None);
             }
         });
     }
