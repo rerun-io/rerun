31ff400d9e6396939fe5ce7ef1fb5fc868bcb800
Jeremy Leibs <jeremy@rerun.io>
2024-03-29 10:10:32 -0400

    Fix the publish jobs to account for new linux-arm target

--- scripts/ci/generate_pr_summary.py
+++ scripts/ci/generate_pr_summary.py
@@ -51,10 +51,11 @@ def generate_pr_summary(github_token: str, github_repository: str, pr_number: in
 
         # Check if there are rerun_c libraries
         rerun_libraries_blobs = [
-            builds_bucket.blob(f"commit/{commit_short}/rerun_c/windows/rerun_c.lib"),
-            builds_bucket.blob(f"commit/{commit_short}/rerun_c/linux/librerun_c.a"),
-            builds_bucket.blob(f"commit/{commit_short}/rerun_c/macos-arm/librerun_c.a"),
-            builds_bucket.blob(f"commit/{commit_short}/rerun_c/macos-intel/librerun_c.a"),
+            builds_bucket.blob(f"commit/{commit_short}/rerun_c/windows-x64/rerun_c.lib"),
+            builds_bucket.blob(f"commit/{commit_short}/rerun_c/linux-arm64/librerun_c.a"),
+            builds_bucket.blob(f"commit/{commit_short}/rerun_c/linux-x64/librerun_c.a"),
+            builds_bucket.blob(f"commit/{commit_short}/rerun_c/macos-arm64/librerun_c.a"),
+            builds_bucket.blob(f"commit/{commit_short}/rerun_c/macos-x64/librerun_c.a"),
         ]
         rerun_libraries = [f"https://build.rerun.io/{blob.name}" for blob in rerun_libraries_blobs if blob.exists()]
         if rerun_libraries:
