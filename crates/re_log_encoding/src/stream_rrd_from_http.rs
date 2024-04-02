7015ab7982e988ea77f5d09825be332a560ea40a
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-04-02 21:51:35 +0200

    Improve logging when closing a connection

--- crates/re_log_encoding/src/stream_rrd_from_http.rs
+++ crates/re_log_encoding/src/stream_rrd_from_http.rs
@@ -15,7 +15,7 @@ pub fn stream_rrd_from_http_to_channel(
         re_smart_channel::SmartChannelSource::RrdHttpStream { url: url.clone() },
     );
     stream_rrd_from_http(
-        url,
+        url.clone(),
         Arc::new(move |msg| {
             if let Some(on_msg) = &on_msg {
                 on_msg();
@@ -25,17 +25,17 @@ pub fn stream_rrd_from_http_to_channel(
                     if tx.send(msg).is_ok() {
                         ControlFlow::Continue(())
                     } else {
-                        re_log::info!("Failed to send log message to viewer - closing");
+                        re_log::info_once!("Closing connection to {url}");
                         ControlFlow::Break(())
                     }
                 }
                 HttpMessage::Success => {
-                    tx.quit(None).warn_on_err_once("failed to send quit marker");
+                    tx.quit(None).warn_on_err_once("Failed to send quit marker");
                     ControlFlow::Break(())
                 }
                 HttpMessage::Failure(err) => {
                     tx.quit(Some(err))
-                        .warn_on_err_once("failed to send quit marker");
+                        .warn_on_err_once("Failed to send quit marker");
                     ControlFlow::Break(())
                 }
             }
