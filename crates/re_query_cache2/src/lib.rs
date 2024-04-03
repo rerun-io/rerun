fff50a9086fab05a16c906d3a209dd00e87089f5
Clement Rey <cr.rey.clement@gmail.com>
2024-04-03 12:04:27 +0200

    implement range caching

--- crates/re_query_cache2/src/lib.rs
+++ crates/re_query_cache2/src/lib.rs
@@ -3,18 +3,21 @@
 mod cache;
 mod flat_vec_deque;
 mod latest_at;
+mod range;
 
 pub use self::cache::{CacheKey, Caches};
 pub use self::flat_vec_deque::{ErasedFlatVecDeque, FlatVecDeque};
 pub use self::latest_at::{
     CachedLatestAtComponentResults, CachedLatestAtMonoResult, CachedLatestAtResults,
 };
+pub use self::range::{CachedRangeComponentResults, CachedRangeResults};
 
 pub(crate) use self::latest_at::LatestAtCache;
+pub(crate) use self::range::{CachedRangeComponentResultsInner, RangeCache};
 
 pub use re_query2::{
-    clamped_zip::*, Promise, PromiseId, PromiseResolver, PromiseResult, QueryError, Result,
-    ToArchetype,
+    clamped_zip::*, range_zip::*, Promise, PromiseId, PromiseResolver, PromiseResult, QueryError,
+    Result, ToArchetype,
 };
 
 pub mod external {
