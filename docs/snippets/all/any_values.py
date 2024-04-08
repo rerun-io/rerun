8071eedd126777cefcf80cf960ecfccd0bfbfe5d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-04-08 10:41:53 +0200

    0.15 migration guide for removed instance keys (#5830)
    ### What
    Write a migration guide for 0.15 covering the removal of instance keys
    from our archetypes.
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested the web demo (if applicable):
    * Using newly built examples:
    [rerun.io/viewer](https://rerun.io/viewer/pr/5830)
    * Using examples from latest `main` build:
    [rerun.io/viewer](https://rerun.io/viewer/pr/5830?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
    * Using full set of examples from `nightly` build:
    [rerun.io/viewer](https://rerun.io/viewer/pr/5830?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
    * [x] The PR title and labels are set such as to maximize their
    usefulness for the next release's CHANGELOG
    * [x] If applicable, add a new check to the [release
    checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
    
    - [PR Build Summary](https://build.rerun.io/pr/5830)
    - [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
    - [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)
    
    ---------
    
    Co-authored-by: Jeremy Leibs <jeremy@rerun.io>

--- docs/snippets/all/any_values.py
+++ docs/snippets/all/any_values.py
@@ -6,7 +6,7 @@ rr.init("rerun_example_any_values", spawn=True)
 rr.log(
     "any_values",
     rr.AnyValues(
-        foo=[1.2, 3.4, 5.6],
-        bar="hello world",
+        confidence=[1.2, 3.4, 5.6],
+        description="Bla bla bla…",
     ),
 )
