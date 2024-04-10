07dccbc992460f68a83fbe9c3f4ed9832ce4c6f2
Antoine Beyeler <antoine@rerun.io>
2024-04-10 11:25:31 +0200

    Exploration of dep management tools for our examples

--- examples/python/README.md
+++ examples/python/README.md
@@ -1,4 +1,173 @@
 # Rerun Python examples
+
+---
+
+## Example dependency management
+
+We need the following for a smooth example experience:
+- Per-example dependency specification: different example must be able to have conflicting dependencies.
+- Per-example Python version requirement: although the Rerun SDK has a well-defined Python compatibility range, specific example should be able to restrict it when required by their specific dependencies.
+- Per-example isolated environment: it follows from what precedes that each example should be run in their own venv.
+- Minimal overhead and as "standard" as possible.
+- (Nice to have) Lockfile.
+
+In addition, we should support both dev environment (install `rerun-sdk` from the working directory) and end-user environment (install `rerun-sdk` from PyPI).
+
+---
+
+## Using `uv`
+
+Example: `human_pose_tracking`
+
+### Tooling Installation
+
+```shell
+pipx install uv
+
+# or
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+```
+
+### Environment Setup
+
+Must be manually created but `uv` has facilities for that:
+
+```shell
+cd example/python/human_pose_tracking
+uv venv --python 3.11 
+source .venv/bin/activate
+uv pip install -r requirements.txt
+
+# to use local rerun-sdk (this triggers a maturin compilation)
+uv pip install -e ../../../rerun_py
+```
+
+⚠️There don't seem to be a way to specify Python version requirement for `uv` yet, so the user must pick a compatible python version.
+
+Note: since this is basically based on a (locked) requirement file, "standard" Python tooling will also work:
+
+```shell
+cd example/python/human_pose_tracking
+python3.11 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install -e ../../../rerun_py
+```
+
+In that sense, `uv` is only required for example authors (to update the `requirements.txt` file).
+
+### Run the example
+
+```shell
+python main.py
+```
+
+### Create/update the lockfile
+
+The direct dependencies are specified in `requirements.in`. From that, `uv` is able to generate a `requirements.txt` which essentially is a lockfile:
+
+```shell
+uv pip compile requirements.in -o requirements.txt
+```
+
+---
+
+## Using `poetry` with `package-mode = false`
+
+Example: `human_pose_tracking_poetry`
+
+Note: `package-mode = false` is very nice for lone scripts that don't need to be "pip installed" to run, just like our examples.  
+
+### Tooling Installation
+
+```shell
+pipx install poetry
+
+# or
+
+curl -sSL https://install.python-poetry.org | python3 -
+```
+
+### Environment Setup
+
+```shell
+poetry install
+
+# to use the local rerun-sdk (this triggers maturin compilation)
+poetry run pip install -e ../../../rerun_py
+```
+
+This will do the following:
+- Unless a venv is already active, it will create one using some python interpreter that it can find and is compatible with constraints listed in `pyproject.toml`.
+- Install all dependencies.
+- Create the `main` executable script as specified in the `pyproject.toml`.
+
+Additional facilities provided by Poetry:
+```shell
+poetry run ${ARGS}  #  run stuff inside the default environment
+poetry env use 3.8  # create another environment with the specified python version
+poetry env list     # list all available environments
+poetry shell        # activate the default environment
+```
+
+### Run the example
+
+```shell
+poetry run python main.py
+
+# or
+
+poetry shell  # activate the venv
+python main.py
+```
+
+### Create/update the lockfile
+
+```shell
+poetry update
+```
+
+---
+
+## Using `hatch`
+
+Example: `human_pose_tracking_hatch`
+
+`hatch` is similar to `poetry`, but with wider scope and a richer plug-in ecosystem to support it.
+
+### Tooling Installation
+
+```shell
+pipx install hatch
+```
+
+### Environment Setup
+
+Basically nothing, `hatch` creates a venv on the fly.
+
+To use the dev rerun-sdk:
+```shell
+hatch run pip install ../../../rerun_py
+```
+
+### Run the example
+
+```shell
+hatch run python main.py
+
+# or
+
+hatch shell
+python main.py 
+```
+
+
+
+
+---
+_original content follows_
+
 The simplest example is [`minimal`](minimal/main.py). You may want to start there!
 
 Read more about our examples at <https://www.rerun.io/examples>.
---
+++ examples/python/human_pose_tracking_poetry/main.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+"""Use the MediaPipe Pose solution to detect and track a human pose in video."""
+from __future__ import annotations
+
+import argparse
+import logging
+import os
+from contextlib import closing
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Final, Iterator
+
+import cv2
+import mediapipe as mp
+import numpy as np
+import numpy.typing as npt
+import requests
+import rerun as rr  # pip install rerun-sdk
+import rerun.blueprint as rrb
+
+DESCRIPTION = """
+# Human pose tracking
+This example uses Rerun to visualize the output of [MediaPipe](https://developers.google.com/mediapipe)-based tracking
+of a human pose in 2D and 3D.
+
+The full source code for this example is available
+[on GitHub](https://github.com/rerun-io/rerun/blob/latest/examples/python/human_pose_tracking).
+""".strip()
+
+EXAMPLE_DIR: Final = Path(os.path.dirname(__file__))
+DATASET_DIR: Final = EXAMPLE_DIR / "dataset" / "pose_movement"
+DATASET_URL_BASE: Final = "https://storage.googleapis.com/rerun-example-datasets/pose_movement"
+
+
+def track_pose(video_path: str, *, segment: bool, max_frame_count: int | None) -> None:
+    mp_pose = mp.solutions.pose
+
+    rr.log("description", rr.TextDocument(DESCRIPTION, media_type=rr.MediaType.MARKDOWN), static=True)
+
+    rr.log(
+        "/",
+        rr.AnnotationContext(
+            rr.ClassDescription(
+                info=rr.AnnotationInfo(id=1, label="Person"),
+                keypoint_annotations=[rr.AnnotationInfo(id=lm.value, label=lm.name) for lm in mp_pose.PoseLandmark],
+                keypoint_connections=mp_pose.POSE_CONNECTIONS,
+            )
+        ),
+        static=True,
+    )
+    # Use a separate annotation context for the segmentation mask.
+    rr.log(
+        "video/mask",
+        rr.AnnotationContext(
+            [
+                rr.AnnotationInfo(id=0, label="Background"),
+                rr.AnnotationInfo(id=1, label="Person", color=(0, 0, 0)),
+            ]
+        ),
+        static=True,
+    )
+    rr.log("person", rr.ViewCoordinates.RIGHT_HAND_Y_DOWN, static=True)
+
+    with closing(VideoSource(video_path)) as video_source, mp_pose.Pose(enable_segmentation=segment) as pose:
+        for idx, bgr_frame in enumerate(video_source.stream_bgr()):
+            if max_frame_count is not None and idx >= max_frame_count:
+                break
+
+            rgb = cv2.cvtColor(bgr_frame.data, cv2.COLOR_BGR2RGB)
+            rr.set_time_seconds("time", bgr_frame.time)
+            rr.set_time_sequence("frame_idx", bgr_frame.idx)
+            rr.log("video/rgb", rr.Image(rgb).compress(jpeg_quality=75))
+
+            results = pose.process(rgb)
+            h, w, _ = rgb.shape
+            landmark_positions_2d = read_landmark_positions_2d(results, w, h)
+            if landmark_positions_2d is not None:
+                rr.log(
+                    "video/pose/points",
+                    rr.Points2D(landmark_positions_2d, class_ids=1, keypoint_ids=mp_pose.PoseLandmark),
+                )
+
+            landmark_positions_3d = read_landmark_positions_3d(results)
+            if landmark_positions_3d is not None:
+                rr.log(
+                    "person/pose/points",
+                    rr.Points3D(landmark_positions_3d, class_ids=1, keypoint_ids=mp_pose.PoseLandmark),
+                )
+
+            segmentation_mask = results.segmentation_mask
+            if segmentation_mask is not None:
+                rr.log("video/mask", rr.SegmentationImage(segmentation_mask.astype(np.uint8)))
+
+
+def read_landmark_positions_2d(
+    results: Any,
+    image_width: int,
+    image_height: int,
+) -> npt.NDArray[np.float32] | None:
+    if results.pose_landmarks is None:
+        return None
+    else:
+        normalized_landmarks = [results.pose_landmarks.landmark[lm] for lm in mp.solutions.pose.PoseLandmark]
+        return np.array([(image_width * lm.x, image_height * lm.y) for lm in normalized_landmarks])
+
+
+def read_landmark_positions_3d(
+    results: Any,
+) -> npt.NDArray[np.float32] | None:
+    if results.pose_landmarks is None:
+        return None
+    else:
+        landmarks = [results.pose_world_landmarks.landmark[lm] for lm in mp.solutions.pose.PoseLandmark]
+        return np.array([(lm.x, lm.y, lm.z) for lm in landmarks])
+
+
+@dataclass
+class VideoFrame:
+    data: npt.NDArray[np.uint8]
+    time: float
+    idx: int
+
+
+class VideoSource:
+    def __init__(self, path: str):
+        self.capture = cv2.VideoCapture(path)
+
+        if not self.capture.isOpened():
+            logging.error("Couldn't open video at %s", path)
+
+    def close(self) -> None:
+        self.capture.release()
+
+    def stream_bgr(self) -> Iterator[VideoFrame]:
+        while self.capture.isOpened():
+            idx = int(self.capture.get(cv2.CAP_PROP_POS_FRAMES))
+            is_open, bgr = self.capture.read()
+            time_ms = self.capture.get(cv2.CAP_PROP_POS_MSEC)
+
+            if not is_open:
+                break
+
+            yield VideoFrame(data=bgr, time=time_ms * 1e-3, idx=idx)
+
+
+def get_downloaded_path(dataset_dir: Path, video_name: str) -> str:
+    video_file_name = f"{video_name}.mp4"
+    destination_path = dataset_dir / video_file_name
+    if destination_path.exists():
+        logging.info("%s already exists. No need to download", destination_path)
+        return str(destination_path)
+
+    source_path = f"{DATASET_URL_BASE}/{video_file_name}"
+
+    logging.info("Downloading video from %s to %s", source_path, destination_path)
+    os.makedirs(dataset_dir.absolute(), exist_ok=True)
+    with requests.get(source_path, stream=True) as req:
+        req.raise_for_status()
+        with open(destination_path, "wb") as f:
+            for chunk in req.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return str(destination_path)
+
+
+def main() -> None:
+    # Ensure the logging gets written to stderr:
+    logging.getLogger().addHandler(logging.StreamHandler())
+    logging.getLogger().setLevel("INFO")
+
+    parser = argparse.ArgumentParser(description="Uses the MediaPipe Pose solution to track a human pose in video.")
+    parser.add_argument(
+        "--video",
+        type=str,
+        default="backflip",
+        choices=["backflip", "soccer"],
+        help="The example video to run on.",
+    )
+    parser.add_argument("--dataset-dir", type=Path, default=DATASET_DIR, help="Directory to save example videos to.")
+    parser.add_argument("--video-path", type=str, default="", help="Full path to video to run on. Overrides `--video`.")
+    parser.add_argument("--no-segment", action="store_true", help="Don't run person segmentation.")
+    parser.add_argument(
+        "--max-frame",
+        type=int,
+        help="Stop after processing this many frames. If not specified, will run until interrupted.",
+    )
+    rr.script_add_args(parser)
+
+    args = parser.parse_args()
+    rr.script_setup(
+        args,
+        "rerun_example_human_pose_tracking",
+        default_blueprint=rrb.Horizontal(
+            rrb.Vertical(
+                rrb.Spatial2DView(origin="video", name="Result"),
+                rrb.Spatial3DView(origin="person", name="3D pose"),
+            ),
+            rrb.Vertical(
+                rrb.Spatial2DView(origin="video/rgb", name="Raw video"),
+                rrb.TextDocumentView(origin="description", name="Description"),
+                row_shares=[2, 3],
+            ),
+            column_shares=[3, 2],
+        ),
+    )
+
+    video_path = args.video_path  # type: str
+    if not video_path:
+        video_path = get_downloaded_path(args.dataset_dir, args.video)
+
+    track_pose(video_path, segment=not args.no_segment, max_frame_count=args.max_frame)
+
+    rr.script_teardown(args)
+
+
+if __name__ == "__main__":
+    main()
