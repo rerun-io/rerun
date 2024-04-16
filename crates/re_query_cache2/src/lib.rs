d33d0c9bf217ed3c60a116646370c3b25d4624dd
Clement Rey <cr.rey.clement@gmail.com>
2024-04-16 12:23:08 +0200

    set up cache stats and debug formatting

--- crates/re_query_cache2/src/lib.rs
+++ crates/re_query_cache2/src/lib.rs
@@ -1,11 +1,13 @@
 //! Caching datastructures for `re_query`.
 
 mod cache;
+mod cache_stats;
 mod flat_vec_deque;
 mod latest_at;
 mod range;
 
 pub use self::cache::{CacheKey, Caches};
+pub use self::cache_stats::{CachedComponentStats, CachesStats};
 pub use self::flat_vec_deque::{ErasedFlatVecDeque, FlatVecDeque};
 pub use self::latest_at::{
     CachedLatestAtComponentResults, CachedLatestAtMonoResult, CachedLatestAtResults,
@@ -25,4 +27,28 @@ pub mod external {
 
     pub use paste;
     pub use seq_macro;
+}
+
+// ---
+
+use re_data_store::{LatestAtQuery, RangeQuery};
+
+#[derive(Debug)]
+pub enum CachedResults {
+    LatestAt(LatestAtQuery, CachedLatestAtResults),
+    Range(RangeQuery, CachedRangeResults),
+}
+
+impl From<(LatestAtQuery, CachedLatestAtResults)> for CachedResults {
+    #[inline]
+    fn from((query, results): (LatestAtQuery, CachedLatestAtResults)) -> Self {
+        Self::LatestAt(query, results)
+    }
+}
+
+impl From<(RangeQuery, CachedRangeResults)> for CachedResults {
+    #[inline]
+    fn from((query, results): (RangeQuery, CachedRangeResults)) -> Self {
+        Self::Range(query, results)
+    }
 }
