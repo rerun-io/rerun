d46a5ec53470c7fbb3615e25fbba117aad2b55f1
Jeremy Leibs <jeremy@rerun.io>
2024-04-16 17:45:39 -0400

    fail if rerun bin is missing

--- rerun_py/build.rs
+++ rerun_py/build.rs
@@ -3,4 +3,12 @@ fn main() {
     pyo3_build_config::add_extension_module_link_args();
 
     re_build_tools::export_build_info_vars_for_crate("rerun_py");
+
+    let rerun_bin = std::env::current_dir().unwrap().join("rerun_sdk/bin/rerun");
+
+    // Fail if bin/rerun is missing
+    if !rerun_bin.exists() {
+        eprintln!("ERROR: Expected to find `rerun` at `{rerun_bin:?}`.");
+        std::process::exit(1);
+    }
 }
