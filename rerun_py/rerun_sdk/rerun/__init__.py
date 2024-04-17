c3654b5ba50ef26401ceeb58de012afdf804b2a7
Jeremy Leibs <jeremy@rerun.io>
2024-04-17 13:14:59 -0400

    Don't duplicate main

--- rerun_py/rerun_sdk/rerun/__init__.py
+++ rerun_py/rerun_sdk/rerun/__init__.py
@@ -576,17 +576,3 @@ def start_web_viewer_server(port: int = 0) -> None:
     """
 
     bindings.start_web_viewer_server(port)
-
-
-def main() -> int:
-    import os
-    import subprocess
-    import sys
-
-    if "RERUN_CLI_PATH" in os.environ:
-        print(f"Using overridden RERUN_CLI_PATH={os.environ['RERUN_CLI_PATH']}", file=sys.stderr)
-        target_path = os.environ["RERUN_CLI_PATH"]
-    else:
-        target_path = os.path.join(os.path.dirname(__file__), "..", "bin", "rerun")
-
-    return subprocess.call([target_path, *sys.argv[1:]])
