6134571eaabe6eddc6434e79dffd879ed84c2f45
Jeremy Leibs <jeremy@rerun.io>
2024-04-16 21:31:47 -0400

    disable the check for the rerun bin again

--- rerun_py/build.rs
+++ rerun_py/build.rs
@@ -4,7 +4,13 @@ fn main() {
 
     re_build_tools::export_build_info_vars_for_crate("rerun_py");
 
+    // TODO(jleibs): Figure out how to do something like this.
+    // This currently breaks `cargo check` since we build with `--all-features`.
+    // Ideally we would enforce this from the maturin packaging side of
+    // things, but there does not appear to be a hook for doing that.
+    //
     // Fail if bin/rerun is missing and we're building for pypi
+    /*
     #[cfg(feature = "pypi")]
     {
         let rerun_bin = std::env::current_dir().unwrap().join("rerun_sdk/bin/rerun");
@@ -13,4 +19,5 @@ fn main() {
             std::process::exit(1);
         }
     }
+    */
 }
