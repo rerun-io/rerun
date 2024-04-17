93faa61d4a4bc23b448d32c7606939bbe2f2fff7
Jeremy Leibs <jeremy@rerun.io>
2024-04-16 21:11:16 -0400

    Require rerun bin when building for pypi

--- rerun_py/build.rs
+++ rerun_py/build.rs
@@ -1,19 +1,16 @@
-use re_build_tools::is_tracked_env_var_set;
-
 fn main() {
     // Required for `cargo build` to work on mac: https://pyo3.rs/main/building-and-distribution#macos
     pyo3_build_config::add_extension_module_link_args();
 
     re_build_tools::export_build_info_vars_for_crate("rerun_py");
 
-    let rerun_bin = std::env::current_dir().unwrap().join("rerun_sdk/bin/rerun");
-
-    // Fail if bin/rerun is missing and this isn't a maturin dev build
-    if !is_tracked_env_var_set("RERUN_PY_DEV_BUILD") && !rerun_bin.exists() {
-        eprintln!("WARNING: Expected to find `rerun` at `{rerun_bin:?}`.");
-        // TODO(jleibs): we would like to make this fail the build. However, this
-        // causes `cargo check` to fail. This really should be some kind of maturin
-        // check, but there isn't a way to do that yet.
-        //std::process::exit(1);
+    // Fail if bin/rerun is missing and we're building for pypi
+    #[cfg(feature = "pypi")]
+    {
+        let rerun_bin = std::env::current_dir().unwrap().join("rerun_sdk/bin/rerun");
+        if !rerun_bin.exists() {
+            eprintln!("ERROR: Expected to find `rerun` at `{rerun_bin:?}`.");
+            std::process::exit(1);
+        }
     }
 }
