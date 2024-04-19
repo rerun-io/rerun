ff1540b2b636679b76ff0c3bdc600fb1daf3549f
Jeremy Leibs <jeremy@rerun.io>
2024-04-19 12:58:51 -0400

    Clean up code duplication by dispatching to rerun_cli.__main__

--- rerun_py/rerun_sdk/rerun/__main__.py
+++ rerun_py/rerun_sdk/rerun/__main__.py
@@ -8,42 +8,17 @@ importing the module.
 
 from __future__ import annotations
 
-import os
-import subprocess
-import sys
+from rerun_cli.__main__ import main as cli_main
 
 from rerun import unregister_shutdown
-
-
-def exe_suffix() -> str:
-    if sys.platform.startswith("win"):
-        return ".exe"
-    return ""
-
-
-def add_exe_suffix(path: str) -> str:
-    if not path.endswith(exe_suffix()):
-        return path + exe_suffix()
-    return path
 
 
 def main() -> int:
     # Importing of the rerun module registers a shutdown hook that we know we don't
     # need when running the CLI directly. We can safely unregister it.
     unregister_shutdown()
-    if "RERUN_CLI_PATH" in os.environ:
-        print(f"Using overridden RERUN_CLI_PATH={os.environ['RERUN_CLI_PATH']}", file=sys.stderr)
-        target_path = os.environ["RERUN_CLI_PATH"]
-    else:
-        target_path = os.path.join(os.path.dirname(__file__), "..", "rerun_cli", "rerun")
 
-    target_path = add_exe_suffix(target_path)
-
-    if not os.path.exists(target_path):
-        print(f"Error: Could not find rerun binary at {target_path}", file=sys.stderr)
-        return 1
-
-    return subprocess.call([target_path, *sys.argv[1:]])
+    return cli_main()
 
 
 if __name__ == "__main__":
