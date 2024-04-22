6fdf4b86c3b33a3beaf83c496806e116160d2a16
Andreas Reich <r_andreas2@web.de>
2024-04-22 14:57:25 +0200

    fix unreachable statement

--- crates/re_web_viewer_server/src/lib.rs
+++ crates/re_web_viewer_server/src/lib.rs
@@ -222,9 +222,7 @@ impl WebViewerServerInner {
         if false {
             self.on_serve_wasm(); // to silence warning about the function being unused
         }
-
         panic!("web_server compiled with '__ci' feature (or `--all-features`). DON'T DO THAT! It's only for the CI!");
-        std::process::abort();
     }
 
     #[cfg(not(feature = "__ci"))]
