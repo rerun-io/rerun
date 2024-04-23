rerun-io/rerun

commit 5cafbe3a3bb5de95c22e82661572840039e20ef4

Convert Python examples to proper packages (#5966)
### What

This PR does the following:
- Convert all Python examples to proper python packages, replacing the
`requirements.txt` file with a `pyproject.toml` file.
- Applies same conversion to `tests/python/test_api`.
- Add WIP support for running example with pixi
- Add a `all_examples` meta-project.
- Fix `dev-tools build-examples` for the new example format.
- Add a `dev-tools build-examples install` command to `pip install` the
examples for the given channel (prerequisite to `build-examples rrd`).
- Fix CI for the new example format.
- Updates pixi to 0.19.1 in CI.

#### pixi support

This WIP pixi support allows running examples in the `examples`
environment:

```
pixi run -e examples clock
```

Note that by default pixi builds the environment with a shim/empty/fake
rerun package (from `examples/python/_empty_rerun_sdk`), which is a
work-around to allow manual installation of rerun in the environment. As
a result, at first (and whenever a rebuild is needed), this must be run:

```
pixi run -e examples py-build
```

#### `all_examples`

This is a meta-package that dynamically depends on all examples. The
original hope was to be able to depend on it from pixi, avoiding the
need to list example explicitly. However, this is current impossible for
a number a reason:

- doing so means that examples would not be installed as editable,
causing issue with `__file__` relative paths
(https://github.com/rerun-io/rerun/issues/6054)
- doing so we leak full path in the lock file
- pixi currently has an issue where indirect dependency with git
dependencies are broken

`all_examples` also has a very basic CLI tool that allows listing all
examples in a format suitable for pixi: `all_examples list`.

Also, it should be relatively easy to cleanly reimplement the
functionality of `run_all.py`, which is currently broken:
- https://github.com/rerun-io/rerun/issues/6057

examples/python/dicom_mri/dicom_mri.py
@@ -1,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+Example using MRI scan data in the DICOM format.
+
+Run:
+``` sh
+python3 examples/python/dicom/main.py
+```
+"""
+
+from __future__ import annotations
+
+import argparse
+import io
+import os
+import zipfile
+from pathlib import Path
+from typing import Final, Iterable
+
+import dicom_numpy
+import numpy as np
+import numpy.typing as npt
+import pydicom as dicom
+import requests
+import rerun as rr  # pip install rerun-sdk
+
+DESCRIPTION = """
+# Dicom MRI
+This example visualizes an MRI scan using Rerun.
+
+The visualization of the data consists of just the following line
+```python
+rr.log("tensor", rr.Tensor(voxels_volume_u16, dim_names=["right", "back", "up"]))
+```
+
+The full source code for this example is available
+[on GitHub](https://github.com/rerun-io/rerun/blob/latest/examples/python/dicom_mri).
+"""
+
+DATASET_DIR: Final = Path(os.path.dirname(__file__)) / "dataset"
+DATASET_URL: Final = "https://storage.googleapis.com/rerun-example-datasets/dicom.zip"
+
+
+def extract_voxel_data(
+    dicom_files: Iterable[Path],
+) -> tuple[npt.NDArray[np.int16], npt.NDArray[np.float32]]:
+    slices = [dicom.read_file(f) for f in dicom_files]  # type: ignore[misc]
+    try:
+        voxel_ndarray, ijk_to_xyz = dicom_numpy.combine_slices(slices)
+    except dicom_numpy.DicomImportException:
+        raise  # invalid DICOM data
+
+    return voxel_ndarray, ijk_to_xyz
+
+
+def list_dicom_files(dir: Path) -> Iterable[Path]:
+    for path, _, files in os.walk(dir):
+        for f in files:
+            if f.endswith(".dcm"):
+                yield Path(path) / f
+
+
+def read_and_log_dicom_dataset(dicom_files: Iterable[Path]) -> None:
+    rr.log("description", rr.TextDocument(DESCRIPTION, media_type=rr.MediaType.MARKDOWN), static=True)
+
+    voxels_volume, _ = extract_voxel_data(dicom_files)
+
+    # the data is i16, but in range [0, 536].
+    voxels_volume_u16: npt.NDArray[np.uint16] = np.require(voxels_volume, np.uint16)
+
+    rr.log("tensor", rr.Tensor(voxels_volume_u16, dim_names=["right", "back", "up"]))
+
+
+def ensure_dataset_downloaded() -> Iterable[Path]:
+    dicom_files = list(list_dicom_files(DATASET_DIR))
+    if dicom_files:
+        return dicom_files
+    print("downloading dataset…")
+    os.makedirs(DATASET_DIR.absolute(), exist_ok=True)
+    resp = requests.get(DATASET_URL, stream=True)
+    z = zipfile.ZipFile(io.BytesIO(resp.content))
+    z.extractall(DATASET_DIR.absolute())
+
+    return list_dicom_files(DATASET_DIR)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Example using MRI scan data in the DICOM format.")
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+    rr.script_setup(args, "rerun_example_dicom_mri")
+    dicom_files = ensure_dataset_downloaded()
+    read_and_log_dicom_dataset(dicom_files)
+    rr.script_teardown(args)
+
+
+if __name__ == "__main__":
+    main()

examples/python/log_file/log_file.py
@@ -1,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Demonstrates how to log any file from the SDK using the `DataLoader` machinery.
+
+See <https://www.rerun.io/docs/reference/data-loaders/overview> for more information.
+
+Usage:
+```
+python examples/python/log_file/main.py -- examples/assets
+```
+"""
+
+from __future__ import annotations
+
+import argparse
+from pathlib import Path
+
+import rerun as rr  # pip install rerun-sdk
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Demonstrates how to log any file from the SDK using the `DataLoader` machinery."
+    )
+    rr.script_add_args(parser)
+    parser.add_argument(
+        "--from-contents",
+        action="store_true",
+        default=False,
+        help="Log the contents of the file directly (files only -- not supported by external loaders).",
+    )
+    parser.add_argument("filepaths", nargs="+", type=Path, help="The filepaths to be loaded and logged.")
+    args = parser.parse_args()
+
+    rr.script_setup(args, "rerun_example_log_file")
+
+    for filepath in args.filepaths:
+        if not args.from_contents:
+            # Either log the file using its path…
+            rr.log_file_from_path(filepath, entity_path_prefix="log_file_example")
+        else:
+            # …or using its contents if you already have them loaded for some reason.
+            try:
+                with open(filepath, "rb") as file:
+                    rr.log_file_from_contents(filepath, file.read(), entity_path_prefix="log_file_example")
+            except Exception:
+                pass
+
+    rr.script_teardown(args)
+
+
+if __name__ == "__main__":
+    main()

examples/python/objectron/objectron/__main__.py
@@ -1,0 +1,240 @@
+#!/usr/bin/env python3
+"""
+Example of using the Rerun SDK to log the Objectron dataset.
+
+Example: `examples/python/objectron/main.py --recording chair`
+"""
+
+from __future__ import annotations
+
+import argparse
+import logging
+import math
+import os
+import sys
+import time
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Iterable, Iterator
+
+import numpy as np
+import rerun as rr  # pip install rerun-sdk
+import rerun.blueprint as rrb
+from scipy.spatial.transform import Rotation as R
+
+from .download_dataset import (
+    ANNOTATIONS_FILENAME,
+    AVAILABLE_RECORDINGS,
+    GEOMETRY_FILENAME,
+    LOCAL_DATASET_DIR,
+    ensure_recording_available,
+)
+from .proto.objectron.proto import ARCamera, ARFrame, ARPointCloud, Object, ObjectType, Sequence
+
+
+@dataclass
+class SampleARFrame:
+    """An `ARFrame` sample and the relevant associated metadata."""
+
+    index: int
+    timestamp: float
+    dirpath: Path
+    frame: ARFrame
+    image_path: Path
+
+
+def read_ar_frames(
+    dirpath: Path, num_frames: int, run_forever: bool, per_frame_sleep: float
+) -> Iterator[SampleARFrame]:
+    """
+    Loads up to `num_frames` consecutive ARFrames from the given path on disk.
+
+    `dirpath` should be of the form `dataset/bike/batch-8/16/`.
+    """
+
+    path = dirpath / GEOMETRY_FILENAME
+    print(f"loading ARFrames from {path}")
+
+    time_offset = 0
+    frame_offset = 0
+
+    while True:
+        frame_idx = 0
+        data = Path(path).read_bytes()
+        while len(data) > 0 and frame_idx < num_frames:
+            next_len = int.from_bytes(data[:4], byteorder="little", signed=False)
+            data = data[4:]
+
+            frame = ARFrame().parse(data[:next_len])
+            img_path = Path(os.path.join(dirpath, f"video/{frame_idx}.jpg"))
+            yield SampleARFrame(
+                index=frame_idx + frame_offset,
+                timestamp=frame.timestamp + time_offset,
+                dirpath=dirpath,
+                frame=frame,
+                image_path=img_path,
+            )
+
+            data = data[next_len:]
+            frame_idx += 1
+
+            if run_forever and per_frame_sleep > 0.0:
+                time.sleep(per_frame_sleep)
+
+        if run_forever:
+            time_offset += frame.timestamp
+            frame_offset += frame_idx
+        else:
+            break
+
+
+def read_annotations(dirpath: Path) -> Sequence:
+    """
+    Loads the annotations from the given path on disk.
+
+    `dirpath` should be of the form `dataset/bike/batch-8/16/`.
+    """
+
+    path = dirpath / ANNOTATIONS_FILENAME
+    print(f"loading annotations from {path}")
+    data = Path(path).read_bytes()
+
+    seq = Sequence().parse(data)
+
+    return seq
+
+
+def log_ar_frames(samples: Iterable[SampleARFrame], seq: Sequence) -> None:
+    """Logs a stream of `ARFrame` samples and their annotations with the Rerun SDK."""
+
+    rr.log("world", rr.ViewCoordinates.RIGHT_HAND_Y_UP, static=True)
+
+    log_annotated_bboxes(seq.objects)
+
+    frame_times = []
+    for sample in samples:
+        rr.set_time_sequence("frame", sample.index)
+        rr.set_time_seconds("time", sample.timestamp)
+        frame_times.append(sample.timestamp)
+
+        rr.log("world/camera", rr.ImageEncoded(path=sample.image_path))
+        log_camera(sample.frame.camera)
+        log_point_cloud(sample.frame.raw_feature_points)
+
+
+def log_camera(cam: ARCamera) -> None:
+    """Logs a camera from an `ARFrame` using the Rerun SDK."""
+
+    X = np.asarray([1.0, 0.0, 0.0])
+    Z = np.asarray([0.0, 0.0, 1.0])
+
+    world_from_cam = np.asarray(cam.transform).reshape((4, 4))
+    translation = world_from_cam[0:3, 3]
+    intrinsics = np.asarray(cam.intrinsics).reshape((3, 3))
+    rot = R.from_matrix(world_from_cam[0:3, 0:3])
+    (w, h) = (cam.image_resolution_width, cam.image_resolution_height)
+
+    # Because the dataset was collected in portrait:
+    swizzle_x_y = np.asarray([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+    intrinsics = swizzle_x_y @ intrinsics @ swizzle_x_y
+    rot = rot * R.from_rotvec((math.tau / 4.0) * Z)
+    (w, h) = (h, w)
+
+    rot = rot * R.from_rotvec((math.tau / 2.0) * X)  # TODO(emilk): figure out why this is needed
+
+    rr.log(
+        "world/camera",
+        rr.Transform3D(translation=translation, rotation=rr.Quaternion(xyzw=rot.as_quat())),
+    )
+    rr.log(
+        "world/camera",
+        rr.Pinhole(
+            resolution=[w, h],
+            image_from_camera=intrinsics,
+            camera_xyz=rr.ViewCoordinates.RDF,
+        ),
+    )
+
+
+def log_point_cloud(point_cloud: ARPointCloud) -> None:
+    """Logs a point cloud from an `ARFrame` using the Rerun SDK."""
+
+    positions = np.array([[p.x, p.y, p.z] for p in point_cloud.point]).astype(np.float32)
+    rr.log("world/points", rr.Points3D(positions, colors=[255, 255, 255, 255]))
+
+
+def log_annotated_bboxes(bboxes: Iterable[Object]) -> None:
+    """Logs all the bounding boxes annotated in an `ARFrame` sequence using the Rerun SDK."""
+
+    for bbox in bboxes:
+        if bbox.type != ObjectType.BOUNDING_BOX:
+            print(f"err: object type not supported: {bbox.type}")
+            continue
+
+        rot = R.from_matrix(np.asarray(bbox.rotation).reshape((3, 3)))
+        rr.log(
+            f"world/annotations/box-{bbox.id}",
+            rr.Boxes3D(
+                half_sizes=0.5 * np.array(bbox.scale),
+                centers=bbox.translation,
+                rotations=rr.Quaternion(xyzw=rot.as_quat()),
+                colors=[160, 230, 130, 255],
+                labels=bbox.category,
+            ),
+            static=True,
+        )
+
+
+def main() -> None:
+    # Ensure the logging in download_dataset.py gets written to stderr:
+    logging.getLogger().addHandler(logging.StreamHandler())
+    logging.getLogger().setLevel("INFO")
+
+    parser = argparse.ArgumentParser(description="Logs Objectron data using the Rerun SDK.")
+    parser.add_argument(
+        "--frames", type=int, default=sys.maxsize, help="If specified, limits the number of frames logged"
+    )
+    parser.add_argument("--run-forever", action="store_true", help="Run forever, continually logging data.")
+    parser.add_argument(
+        "--per-frame-sleep", type=float, default=0.1, help="Sleep this much for each frame read, if --run-forever"
+    )
+    parser.add_argument(
+        "--recording",
+        type=str,
+        choices=AVAILABLE_RECORDINGS,
+        default=AVAILABLE_RECORDINGS[1],
+        help="The objectron recording to log to Rerun.",
+    )
+    parser.add_argument(
+        "--force-reprocess-video",
+        action="store_true",
+        help="Reprocess video frames even if they already exist",
+    )
+    parser.add_argument(
+        "--dataset-dir", type=Path, default=LOCAL_DATASET_DIR, help="Directory to save example videos to."
+    )
+
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+
+    blueprint = rrb.Horizontal(
+        rrb.Spatial3DView(origin="/world", name="World"),
+        rrb.Spatial2DView(origin="/world/camera", name="Camera", contents=["/world/**"]),
+    )
+    rr.script_setup(
+        args,
+        "rerun_example_objectron",
+        default_blueprint=blueprint,
+    )
+
+    dir = ensure_recording_available(args.recording, args.dataset_dir, args.force_reprocess_video)
+
+    samples = read_ar_frames(dir, args.frames, args.run_forever, args.per_frame_sleep)
+    seq = read_annotations(dir)
+    log_ar_frames(samples, seq)
+
+    rr.script_teardown(args)
+
+
+if __name__ == "__main__":
+    main()

commit bdf386d78ef15becf3f70055ee8b78d8432cc392

Fix links to Python examples
broken by #5966

crates/re_space_view_spatial/benches/bench_points.rs
@@ -35,7 +35,7 @@ use self::constants::*;
 
 // ---
 
-/// Mimics `examples/python/open_photogrammetry_format/main.py`
+/// Mimics `examples/python/open_photogrammetry_format/open_photogrammetry_format.py`
 fn bench_points(c: &mut criterion::Criterion) {
     let timeline = Timeline::log_time();
     let ent_path = EntityPath::from("points");

crates/re_space_view_spatial/src/heuristics.rs
@@ -86,7 +86,7 @@ fn update_pinhole_property_heuristics(
         if properties.pinhole_image_plane_distance.is_auto() {
             let scene_size = scene_bbox_accum.size().length();
             let default_image_plane_distance = if scene_size.is_finite() && scene_size > 0.0 {
-                scene_size * 0.02 // Works pretty well for `examples/python/open_photogrammetry_format/main.py --no-frames`
+                scene_size * 0.02 // Works pretty well for `examples/python/open_photogrammetry_format/open_photogrammetry_format.py --no-frames`
             } else {
                 // This value somewhat arbitrary. In almost all cases where the scene has defined bounds
                 // the heuristic will change it or it will be user edited. In the case of non-defined bounds

examples/python/controlnet/controlnet.py
@@ -3,11 +3,6 @@
 Example running ControlNet conditioned on Canny edges.
 
 Based on https://huggingface.co/docs/diffusers/using-diffusers/controlnet.
-
-Run
-```sh
-examples/python/controlnet/main.py
-```
 """
 
 from __future__ import annotations

examples/python/dicom_mri/dicom_mri.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
-"""
-Example using MRI scan data in the DICOM format.
-
-Run:
-``` sh
-python3 examples/python/dicom/main.py
-```
-"""
+"""Example using MRI scan data in the DICOM format."""
 
 from __future__ import annotations
 

examples/python/dna/dna.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-"""
-The example from our Getting Started page.
-
-`examples/python/dna/main.py`
-"""
+"""The example from our Getting Started page."""
 
 from __future__ import annotations
 

examples/python/llm_embedding_ner/llm_embedding_ner.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
-"""
-Example running BERT-based named entity recognition (NER).
-
-Run
-```sh
-examples/python/llm_embedding_ner/main.py
-```
-"""
+"""Example running BERT-based named entity recognition (NER)."""
 
 from __future__ import annotations
 

examples/python/log_file/log_file.py
@@ -3,11 +3,6 @@
 Demonstrates how to log any file from the SDK using the `DataLoader` machinery.
 
 See <https://www.rerun.io/docs/reference/data-loaders/overview> for more information.
-
-Usage:
-```
-python examples/python/log_file/main.py -- examples/assets
-```
 """
 
 from __future__ import annotations

examples/python/nv12/nv12.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-"""
-Stream NV12 images from a webcam.
-
-Run:
-```sh
-pip install -r examples/python/nv12/requirements.txt
-python examples/python/nv12/main.py
-```
-"""
+"""Stream NV12 images from a webcam."""
 
 from __future__ import annotations
 

examples/python/objectron/objectron/__main__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-"""
-Example of using the Rerun SDK to log the Objectron dataset.
-
-Example: `examples/python/objectron/main.py --recording chair`
-"""
+"""Example of using the Rerun SDK to log the Objectron dataset."""
 
 from __future__ import annotations
 

examples/python/plots/plots.py
@@ -4,7 +4,7 @@ Demonstrates how to log simple plots with the Rerun SDK.
 
 Run:
 ```sh
-./examples/python/plot/main.py
+./examples/python/plot/plots.py
 ```
 """
 

examples/python/raw_mesh/raw_mesh/__main__.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-"""
-Shows how to use the Rerun SDK to log raw 3D meshes (so-called "triangle soups") and their transform hierarchy.
-
-Run:
-```sh
-# assuming your virtual env is up
-examples/python/raw_mesh/main.py
-```
-"""
+"""Shows how to use the Rerun SDK to log raw 3D meshes (so-called "triangle soups") and their transform hierarchy."""
 
 from __future__ import annotations
 

examples/python/rrt_star/rrt_star.py
@@ -10,12 +10,6 @@ This ensures that the algorithm will converge to the optimal path given enough t
 A more detailed explanation can be found in the original paper
 Karaman, S. Frazzoli, S. 2011. "Sampling-based algorithms for optimal motion planning".
 or in the following medium article: https://theclassytim.medium.com/robotic-path-planning-rrt-and-rrt-212319121378
-
-Run:
-```bash
-pip install -r examples/python/rrt-star/requirements.txt
-python examples/python/rrt-star/main.py
-```
 """
 
 from __future__ import annotations

examples/python/segment_anything_model/segment_anything_model.py
@@ -4,15 +4,6 @@ Example of using Rerun to log and visualize the output of [Segment Anything](htt
 
 Can be used to test mask-generation on one or more images. Images can be local file-paths
 or remote urls.
-
-Exa:
-```
-# Run on a remote image:
-python main.py https://raw.githubusercontent.com/facebookresearch/segment-anything/main/notebooks/images/dog.jpg
-
-# Use cuda and a different model on a local image
-python main.py --device cuda --model vit_h /path/to/my_image.jpg
-```
 """
 
 from __future__ import annotations

examples/python/signed_distance_fields/signed_distance_fields/__main__.py
@@ -18,12 +18,6 @@ Using both traditional methods as well as the one described in the DeepSDF paper
     month = {June},
     year = {2019}
 }
-
-Run:
-```sh
-# assuming your virtual env is up
-examples/python/signed_distance_fields/main.py
-```
 """
 
 from __future__ import annotations

examples/python/template/template.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-"""
-Example template.
-
-Run:
-```sh
-pip install -r examples/python/template/requirements.txt
-python examples/python/template/main.py
-```
-"""
+"""Example template."""
 
 from __future__ import annotations
 

examples/rust/objectron/src/main.rs
@@ -324,7 +324,7 @@ fn run(rec: &rerun::RecordingStream, args: &Args) -> anyhow::Result<()> {
         format!(
             "Could not read the recording, have you downloaded the dataset? \
             Try running the python version first to download it automatically \
-            (`examples/python/objectron/main.py --recording {}`).",
+            (`python -m objectron --recording {}`).",
             args.recording.to_possible_value().unwrap().get_name(),
         )
     })?;

examples/rust/raw_mesh/src/main.rs
@@ -148,7 +148,7 @@ impl Args {
             anyhow::bail!(
                 "Could not load the scene, have you downloaded the dataset? \
                 Try running the python version first to download it automatically \
-                (`examples/python/raw_mesh/main.py --scene {scene_name}`).",
+                (`python -m raw_mesh --scene {scene_name}`).",
             )
         }
 

tests/python/test_api/test_api.py
@@ -5,8 +5,8 @@ A collection of many small examples, in one file.
 It uses a lot of different aspects of the Rerun API in order to test it.
 
 Example usage:
-* Run all tests: `examples/python/test_api/main.py`
-* Run specific test: `examples/python/test_api/main.py --test rects`
+* Run all tests: `examples/python/test_api/test_api.py`
+* Run specific test: `examples/python/test_api/test_api.py --test rects`
 """
 
 from __future__ import annotations