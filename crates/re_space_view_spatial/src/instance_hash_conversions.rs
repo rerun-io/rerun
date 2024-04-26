993800cf42d7684d523ae480b0d184d195654c62
Clement Rey <cr.rey.clement@gmail.com>
2024-04-26 10:07:48 +0200

    found the culprit

--- crates/re_space_view_spatial/src/instance_hash_conversions.rs
+++ crates/re_space_view_spatial/src/instance_hash_conversions.rs
@@ -14,6 +14,11 @@ pub fn picking_layer_id_from_instance_path_hash(value: InstancePathHash) -> Pick
 pub fn instance_path_hash_from_picking_layer_id(value: PickingLayerId) -> InstancePathHash {
     InstancePathHash {
         entity_path_hash: EntityPathHash::from_u64(value.object.0),
-        instance: Instance::from(value.instance.0),
+        // `PickingLayerId` uses `u64::MAX` to mean "hover and/or select all instances".
+        instance: if value.instance.0 == u64::MAX {
+            Instance::ALL
+        } else {
+            Instance::from(value.instance.0)
+        },
     }
 }
