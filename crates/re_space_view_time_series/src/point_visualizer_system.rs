d0e9af09b57bc2e80bacb59a27135310a8146849
Clement Rey <cr.rey.clement@gmail.com>
2024-04-26 12:40:54 +0200

    New data APIs 11: port all range-only views (plots, logs…) (#5992)
    Text logs, line plots and scatter plots.
    
    A bit faster than `main`, with a bit less memory overhead.
    
    ---
    
    Part of a PR series to completely revamp the data APIs in preparation
    for the removal of instance keys and the introduction of promises:
    - #5573
    - #5574
    - #5581
    - #5605
    - #5606
    - #5633
    - #5673
    - #5679
    - #5687
    - #5755
    - #5990
    - #5992
    - #5993 
    - #5994
    - #6035
    - #6036
    - #6037
    
    Builds on top of the static data PR series:
    - #5534

--- crates/re_space_view_time_series/src/point_visualizer_system.rs
+++ crates/re_space_view_time_series/src/point_visualizer_system.rs
@@ -1,9 +1,9 @@
 use itertools::Itertools as _;
 
-use re_query_cache::QueryError;
+use re_query_cache2::{PromiseResult, QueryError};
 use re_types::{
     archetypes::{self, SeriesPoint},
-    components::{Color, MarkerShape, MarkerSize, Name, Scalar, StrokeWidth},
+    components::{Color, MarkerShape, MarkerSize, Name, Scalar},
     Archetype as _, ComponentNameSet, Loggable,
 };
 use re_viewer_context::{
@@ -43,7 +43,6 @@ impl VisualizerSystem for SeriesPointSystem {
             .map(ToOwned::to_owned)
             .collect::<ComponentNameSet>();
         query_info.queried.append(&mut series_point_queried);
-        query_info.queried.insert(StrokeWidth::name());
         query_info.indicators = std::iter::once(SeriesPoint::indicator().name()).collect();
         query_info
     }
@@ -100,8 +99,7 @@ impl SeriesPointSystem {
     ) -> Result<(), QueryError> {
         re_tracing::profile_function!();
 
-        let query_caches = ctx.recording().query_caches();
-        let store = ctx.recording_store();
+        let resolver = ctx.recording().resolver();
 
         let (plot_bounds, time_per_pixel) = determine_plot_bounds_and_time_per_pixel(ctx, query);
 
@@ -118,7 +116,7 @@ impl SeriesPointSystem {
                 .map(|c| c.to_array());
             let override_series_name = data_result.lookup_override::<Name>(ctx).map(|t| t.0);
             let override_marker_size = data_result.lookup_override::<MarkerSize>(ctx).map(|r| r.0);
-            let override_marker = data_result.lookup_override::<MarkerShape>(ctx);
+            let override_marker_shape = data_result.lookup_override::<MarkerShape>(ctx);
 
             // All the default values for a `PlotPoint`, accounting for both overrides and default
             // values.
@@ -130,12 +128,12 @@ impl SeriesPointSystem {
                     color: annotation_info.color(override_color, default_color),
                     marker_size: override_marker_size.unwrap_or(DEFAULT_MARKER_SIZE),
                     kind: PlotSeriesKind::Scatter(ScatterAttrs {
-                        marker: override_marker.unwrap_or_default(),
+                        marker: override_marker_shape.unwrap_or_default(),
                     }),
                 },
             };
 
-            let mut points = Vec::new();
+            let mut points;
 
             let time_range = determine_time_range(
                 ctx,
@@ -151,99 +149,162 @@ impl SeriesPointSystem {
                 let entity_path = &data_result.entity_path;
                 let query = re_data_store::RangeQuery::new(query.timeline, time_range);
 
-                // TODO(jleibs): need to do a "joined" archetype query
-                // The `Scalar` archetype queries for `StrokeWidth` in the line visualizer,
-                // and so it must do so here also.
-                // See https://github.com/rerun-io/rerun/pull/5029
-                query_caches.query_archetype_range_pov1_comp4::<
-                    archetypes::Scalar,
-                    Scalar,
-                    Color,
-                    StrokeWidth, // unused
-                    MarkerSize,
-                    MarkerShape,
-                    _,
-                >(
-                    store,
+                let results = ctx.recording().query_caches2().range(
+                    ctx.recording_store(),
                     &query,
                     entity_path,
-                    |entry_range, (times, _, scalars, colors, _, marker_sizes, markers)| {
-                        let times = times.range(entry_range.clone()).map(|(time, _)| time.as_i64());
+                    [
+                        Scalar::name(),
+                        Color::name(),
+                        MarkerSize::name(),
+                        MarkerShape::name(),
+                    ],
+                );
 
-                        // Allocate all points.
-                        points = times.map(|time| PlotPoint {
-                            time,
-                            ..default_point.clone()
-                        }).collect_vec();
+                let all_scalars = results
+                    .get_required(Scalar::name())?
+                    .to_dense::<Scalar>(resolver);
+                let all_scalars_entry_range = all_scalars.entry_range();
 
-                        // Fill in values.
-                        for (i, scalar) in scalars.range(entry_range.clone()).enumerate() {
-                            if scalar.len() > 1 {
-                                re_log::warn_once!("found a scalar batch in {entity_path:?} -- those have no effect");
-                            } else if scalar.is_empty() {
-                                points[i].attrs.kind = PlotSeriesKind::Clear;
-                            } else {
-                                points[i].value = scalar.first().map_or(0.0, |s| s.0);
-                            }
+                if !matches!(
+                    all_scalars.status(),
+                    (PromiseResult::Ready(()), PromiseResult::Ready(()))
+                ) {
+                    // TODO(#5607): what should happen if the promise is still pending?
+                }
+
+                // Allocate all points.
+                points = all_scalars
+                    .range_indices(all_scalars_entry_range.clone())
+                    .map(|(data_time, _)| PlotPoint {
+                        time: data_time.as_i64(),
+                        ..default_point.clone()
+                    })
+                    .collect_vec();
+
+                // Fill in values.
+                for (i, scalars) in all_scalars
+                    .range_data(all_scalars_entry_range.clone())
+                    .enumerate()
+                {
+                    if scalars.len() > 1 {
+                        re_log::warn_once!(
+                            "found a scalar batch in {entity_path:?} -- those have no effect"
+                        );
+                    } else if scalars.is_empty() {
+                        points[i].attrs.kind = PlotSeriesKind::Clear;
+                    } else {
+                        points[i].value = scalars.first().map_or(0.0, |s| s.0);
+                    }
+                }
+
+                // Make it as clear as possible to the optimizer that some parameters
+                // go completely unused as soon as overrides have been defined.
+
+                // Fill in colors -- if available _and_ not overridden.
+                if override_color.is_none() {
+                    if let Some(all_colors) = results.get(Color::name()) {
+                        let all_colors = all_colors.to_dense::<Color>(resolver);
+
+                        if !matches!(
+                            all_colors.status(),
+                            (PromiseResult::Ready(()), PromiseResult::Ready(()))
+                        ) {
+                            // TODO(#5607): what should happen if the promise is still pending?
                         }
 
-                        // Make it as clear as possible to the optimizer that some parameters
-                        // go completely unused as soon as overrides have been defined.
+                        let all_scalars_indexed = all_scalars
+                            .range_indices(all_scalars_entry_range.clone())
+                            .map(|index| (index, ()));
 
-                        // Fill in marker sizes -- if available _and_ not overridden.
-                        if override_marker_size.is_none() {
-                            if let Some(marker_sizes) = marker_sizes {
-                                for (i, marker_size) in marker_sizes.range(entry_range.clone()).enumerate() {
-                                    if i >= points.len() {
-                                        re_log::debug_once!("more marker size attributes than points in {entity_path:?} -- this points to a bug in the query cache");
-                                        break;
+                        let all_frames = re_query_cache2::range_zip_1x1(
+                            all_scalars_indexed,
+                            all_colors.range_indexed(),
+                        )
+                        .enumerate();
+
+                        for (i, (_index, _scalars, colors)) in all_frames {
+                            if let Some(color) = colors.and_then(|colors| {
+                                colors.first().map(|c| {
+                                    let [r, g, b, a] = c.to_array();
+                                    if a == 255 {
+                                        // Common-case optimization
+                                        re_renderer::Color32::from_rgb(r, g, b)
+                                    } else {
+                                        re_renderer::Color32::from_rgba_unmultiplied(r, g, b, a)
                                     }
-                                    if let Some(marker_size) = marker_size.first().copied().flatten() {
-                                        points[i].attrs.marker_size = marker_size.0;
-                                    };
-                                }
+                                })
+                            }) {
+                                points[i].attrs.color = color;
                             }
                         }
+                    }
+                }
 
-                        // Fill in marker shapes -- if available _and_ not overridden.
-                        if override_marker.is_none() {
-                            if let Some(markers) = markers {
-                                for (i, marker) in markers.range(entry_range.clone()).enumerate() {
-                                    if i >= points.len() {
-                                        re_log::debug_once!("more marker attributes than points in {entity_path:?} -- this points to a bug in the query cache");
-                                        break;
-                                    }
-                                    if let Some(marker) = marker.first().copied().flatten() {
-                                        points[i].attrs.kind = PlotSeriesKind::Scatter(ScatterAttrs { marker });
-                                    };
-                                }
-                            }
+                // Fill in marker sizes -- if available _and_ not overridden.
+                if override_marker_size.is_none() {
+                    if let Some(all_marker_sizes) = results.get(MarkerSize::name()) {
+                        let all_marker_sizes = all_marker_sizes.to_dense::<MarkerSize>(resolver);
+
+                        if !matches!(
+                            all_marker_sizes.status(),
+                            (PromiseResult::Ready(()), PromiseResult::Ready(()))
+                        ) {
+                            // TODO(#5607): what should happen if the promise is still pending?
                         }
 
-                        // Fill in colors -- if available _and_ not overridden.
-                        if override_color.is_none() {
-                            if let Some(colors) = colors {
-                                for (i, color) in colors.range(entry_range.clone()).enumerate() {
-                                    if i >= points.len() {
-                                        re_log::debug_once!("more color attributes than points in {entity_path:?} -- this points to a bug in the query cache");
-                                        break;
-                                    }
-                                    if let Some(color) = color.first().copied().flatten().map(|c| {
-                                        let [r,g,b,a] = c.to_array();
-                                        if a == 255 {
-                                            // Common-case optimization
-                                            re_renderer::Color32::from_rgb(r, g, b)
-                                        } else {
-                                            re_renderer::Color32::from_rgba_unmultiplied(r, g, b, a)
-                                        }
-                                    }) {
-                                        points[i].attrs.color = color;
-                                    }
-                                }
+                        let all_scalars_indexed = all_scalars
+                            .range_indices(all_scalars_entry_range.clone())
+                            .map(|index| (index, ()));
+
+                        let all_frames = re_query_cache2::range_zip_1x1(
+                            all_scalars_indexed,
+                            all_marker_sizes.range_indexed(),
+                        )
+                        .enumerate();
+
+                        for (i, (_index, _scalars, marker_sizes)) in all_frames {
+                            if let Some(marker_size) =
+                                marker_sizes.and_then(|marker_sizes| marker_sizes.first().copied())
+                            {
+                                points[i].attrs.marker_size = marker_size.0;
                             }
                         }
-                    },
-                )?;
+                    }
+                }
+
+                // Fill in marker shapes -- if available _and_ not overridden.
+                if override_marker_shape.is_none() {
+                    if let Some(all_marker_shapes) = results.get(MarkerShape::name()) {
+                        let all_marker_shapes = all_marker_shapes.to_dense::<MarkerShape>(resolver);
+
+                        if !matches!(
+                            all_marker_shapes.status(),
+                            (PromiseResult::Ready(()), PromiseResult::Ready(()))
+                        ) {
+                            // TODO(#5607): what should happen if the promise is still pending?
+                        }
+
+                        let all_scalars_indexed = all_scalars
+                            .range_indices(all_scalars_entry_range.clone())
+                            .map(|index| (index, ()));
+
+                        let all_frames = re_query_cache2::range_zip_1x1(
+                            all_scalars_indexed,
+                            all_marker_shapes.range_indexed(),
+                        )
+                        .enumerate();
+
+                        for (i, (_index, _scalars, marker_shapes)) in all_frames {
+                            if let Some(marker) = marker_shapes
+                                .and_then(|marker_shapes| marker_shapes.first().copied())
+                            {
+                                points[i].attrs.kind =
+                                    PlotSeriesKind::Scatter(ScatterAttrs { marker });
+                            }
+                        }
+                    }
+                }
             }
 
             // Check for an explicit label if any.
@@ -264,7 +325,7 @@ impl SeriesPointSystem {
                 data_result,
                 time_per_pixel,
                 points,
-                store,
+                ctx.recording_store(),
                 query,
                 series_name,
                 &mut self.all_series,
