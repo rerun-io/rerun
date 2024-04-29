14c8f6a1aee5ddf090b933b9dcc130e0d21cfa1d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-04-29 12:37:40 +0200

    Reset snippet

--- docs/snippets/all/annotation_context_rects.py
+++ docs/snippets/all/annotation_context_rects.py
@@ -1,5 +1,4 @@
 import rerun as rr
-import rerun.blueprint as rrb
 
 rr.init("rerun_example_annotation_context_rects", spawn=True)
 
@@ -9,12 +8,5 @@ rr.log("/", rr.AnnotationContext([(1, "red", (255, 0, 0)), (2, "green", (0, 255,
 # Log a batch of 2 rectangles with different `class_ids`
 rr.log("detections", rr.Boxes2D(mins=[[-2, -2], [0, 0]], sizes=[[3, 3], [2, 2]], class_ids=[1, 2]))
 
-# Create a Spatial3D View
-blueprint = rrb.Blueprint(
-    rrb.Spatial3DView(
-        origin="/points",
-        background=[80, 80, 80],
-    )
-)
-
-rr.send_blueprint(rrb.Spatial2DView(visual_bounds=rrb.VisualBounds(min=[-4.5, -2.5], max=[2.5, 2.5])))
+# Log an extra rect to set the view bounds
+rr.log("bounds", rr.Boxes2D(half_sizes=[2.5, 2.5]))
