5e613843974106514a65662de7a44c184997ae2b
Antoine Beyeler <antoine@rerun.io>
2024-04-29 11:39:04 +0200

    WIP use in re_ui_example

--- crates/re_ui/examples/re_ui_example/right_panel.rs
+++ crates/re_ui/examples/re_ui_example/right_panel.rs
@@ -1,5 +1,5 @@
 use crate::{drag_and_drop, hierarchical_drag_and_drop, selection_buttons};
-use re_ui::ReUi;
+use re_ui::{list_item2, ReUi};
 
 pub struct RightPanel {
     show_hierarchical_demo: bool,
@@ -25,7 +25,9 @@ impl RightPanel {
     ///
     /// Note: the panel's frame must have a zero inner margin!
     pub fn ui(&mut self, re_ui: &ReUi, ui: &mut egui::Ui) {
+        //TODO(ab): remove this once ListItem 1.0 is phased out.
         ui.set_clip_rect(ui.max_rect());
+        let background_x_range = ui.max_rect().x_range();
 
         //
         // First section - Drag and drop demos
@@ -82,40 +84,78 @@ impl RightPanel {
                 .auto_shrink([false, true])
                 .show(ui, |ui| {
                     re_ui.panel_content(ui, |re_ui, ui| {
-                        for i in 0..10 {
-                            let label = if i == 4 {
-                                "That's one heck of a loooooooong label!".to_owned()
-                            } else {
-                                format!("Some item {i}")
-                            };
+                        list_item2::ListItemContainer::new(background_x_range).ui(
+                            ui,
+                            "list_item_container",
+                            |ui| {
+                                for i in 0..10 {
+                                    let response = if i == 6 {
+                                        list_item2::ListItem::new(re_ui)
+                                            .selected(Some(i) == self.selected_list_item)
+                                            .show_hierarchical_with_children(
+                                                ui,
+                                                "item_with_children",
+                                                true,
+                                                list_item2::CustomListItemContent::new(|_, ui, context| ui.ctx().debug_painter().debug_rect(context.rect, egui::Color32::LIGHT_RED, "proudly produced by a CustomListItemContent's closure")),
+                                                |re_ui, ui| {
+                                                    for _ in 0..3 {
+                                                        list_item2::ListItem::new(re_ui)
+                                                            .interactive(false)
+                                                            .show_hierarchical(
+                                                                ui,
+                                                                list_item2::BasicListItemContent::new("look ma a BasicListItemContent").with_icon(&re_ui::icons::SPACE_VIEW_2D),
+                                                            );
+                                                    }
+                                                },
+                                            )
+                                            .item_response
+                                    } else {
+                                        list_item2::ListItem::new(re_ui)
+                                            .selected(Some(i) == self.selected_list_item)
+                                            .show_hierarchical(ui, list_item2::DebugContent::default())
+                                    };
 
-                            let mut item = re_ui
-                                .list_item(label)
-                                .selected(Some(i) == self.selected_list_item)
-                                .interactive(i != 3)
-                                .with_buttons(|re_ui, ui| {
-                                    re_ui.small_icon_button(ui, &re_ui::icons::ADD)
-                                        | re_ui.small_icon_button(ui, &re_ui::icons::REMOVE)
-                                });
+                                    if response.clicked() {
+                                        self.selected_list_item = Some(i);
+                                    }
+                                }
+                            },
+                        );
 
-                            // demo custom icon
-                            item = if i == 6 {
-                                item.with_icon_fn(|_re_ui, ui, rect, visuals| {
-                                    ui.painter().circle(
-                                        rect.center(),
-                                        rect.width() / 2.0,
-                                        visuals.fg_stroke.color,
-                                        egui::Stroke::NONE,
-                                    );
-                                })
-                            } else {
-                                item.with_icon(&re_ui::icons::SPACE_VIEW_TEXT)
-                            };
-
-                            if item.show_flat(ui).clicked() {
-                                self.selected_list_item = Some(i);
-                            }
-                        }
+                        // for i in 0..10 {
+                        //     let label = if i == 4 {
+                        //         "That's one heck of a loooooooong label!".to_owned()
+                        //     } else {
+                        //         format!("Some item {i}")
+                        //     };
+                        //
+                        //     let mut item = re_ui
+                        //         .list_item(label)
+                        //         .selected(Some(i) == self.selected_list_item)
+                        //         .interactive(i != 3)
+                        //         .with_buttons(|re_ui, ui| {
+                        //             re_ui.small_icon_button(ui, &re_ui::icons::ADD)
+                        //                 | re_ui.small_icon_button(ui, &re_ui::icons::REMOVE)
+                        //         });
+                        //
+                        //     // demo custom icon
+                        //     item = if i == 6 {
+                        //         item.with_icon_fn(|_re_ui, ui, rect, visuals| {
+                        //             ui.painter().circle(
+                        //                 rect.center(),
+                        //                 rect.width() / 2.0,
+                        //                 visuals.fg_stroke.color,
+                        //                 egui::Stroke::NONE,
+                        //             );
+                        //         })
+                        //     } else {
+                        //         item.with_icon(&re_ui::icons::SPACE_VIEW_TEXT)
+                        //     };
+                        //
+                        //     if item.show_flat(ui).clicked() {
+                        //         self.selected_list_item = Some(i);
+                        //     }
+                        // }
                     });
                 });
 
