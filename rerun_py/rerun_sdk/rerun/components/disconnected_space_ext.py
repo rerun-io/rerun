c2995529d2f06ed6f7b98edb409c202b3edf0c1d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-04-29 19:23:02 +0200

    py-fmt of DisconnectedSpace

--- rerun_py/rerun_sdk/rerun/components/disconnected_space_ext.py
+++ rerun_py/rerun_sdk/rerun/components/disconnected_space_ext.py
@@ -1,12 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
-
-import numpy as np
-import pyarrow as pa
-
-if TYPE_CHECKING:
-    from . import DisconnectedSpaceArrayLike
+from typing import Any
 
 
 class DisconnectedSpaceExt:
