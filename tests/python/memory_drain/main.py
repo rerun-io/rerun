5c58524dc1aab7d04f9ccb8f233f442ad018235a
Jeremy Leibs <jeremy@rerun.io>
2024-05-01 09:27:30 -0400

    Also very that thread isolation works as intended

--- tests/python/memory_drain/main.py
+++ tests/python/memory_drain/main.py
@@ -9,12 +9,16 @@ rerun *.rrd
 
 from __future__ import annotations
 
+import threading
+import time
+import uuid
+
 import rerun as rr
 import rerun.blueprint as rrb
 
 
-def main() -> None:
-    with rr.new_recording("rerun_example_memory_drain"):
+def job(name: str) -> None:
+    with rr.new_recording("rerun_example_memory_drain", recording_id=uuid.uuid4()):
         mem = rr.memory_recording()
 
         blueprint = rrb.Blueprint(rrb.TextLogView(name="My Logs", origin="test"))
@@ -22,11 +26,14 @@ def main() -> None:
         rr.send_blueprint(blueprint)
 
         for i in range(5):
-            rr.log("test", rr.TextLog(f"Message {i}"))
+            time.sleep(0.2)
+            rr.log("test", rr.TextLog(f"Job {name} Message {i}"))
 
-            with open(f"output_{i}.rrd", "wb") as f:
+            with open(f"output_{name}_{i}.rrd", "wb") as f:
                 f.write(mem.drain_as_bytes())
 
 
 if __name__ == "__main__":
-    main()
+    threading.Thread(target=job, args=("A",)).start()
+    threading.Thread(target=job, args=("B",)).start()
+    threading.Thread(target=job, args=("C",)).start()
