rerun-io/rerun

commit b7a72cb611612efb22bf80b773757e4b276d762d

Make visual time range on views a view property that can be set from python code (#6164)
### What

* Part of [#6083](https://github.com/rerun-io/rerun/issues/6083)


And _some_ related refactors that I got engulfed with on this quest:
* document fate of remaining `EntityProperties`
* simplify `DataResult` formation by dropping overrides from the
`OverrideContext`
* took the opportunity to call out `EntityProperties` as
`legacy_properties` in a lot of places which makes it easier to read
these changes
* make it explicit when we want to use latest-at query by introducing
`QueryRange` type

Python API not great yet, will improve and provide examples in an
upcoming iteration.


Planned direct follow-ups to this PR:
* finally remove old History types that we still convert to
* should fix the rest of https://github.com/rerun-io/rerun/issues/6135
if it isn't already by this PR
* make python interface actually nice 
* add examples & more docs

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using examples from latest `main` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/6164?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/6164?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG
* [x] If applicable, add a new check to the [release
checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!

- [PR Build Summary](https://build.rerun.io/pr/6164)

crates/re_space_view/src/data_query.rs
@@ -1,18 +1,15 @@
-use nohash_hasher::IntMap;
-
-use re_entity_db::{external::re_data_store::LatestAtQuery, EntityProperties, EntityPropertyMap};
-use re_types::ComponentName;
+use re_entity_db::{external::re_data_store::LatestAtQuery, EntityDb, EntityProperties};
+use re_log_types::Timeline;
 use re_viewer_context::{
-    DataQueryResult, OverridePath, PerVisualizer, StoreContext, VisualizableEntities,
+    DataQueryResult, PerVisualizer, QueryRange, SpaceViewClassRegistry, StoreContext,
+    VisualizableEntities,
 };
 
 pub struct EntityOverrideContext {
-    pub root: EntityProperties,
-    pub individual: EntityPropertyMap,
-    pub recursive: EntityPropertyMap,
+    pub legacy_space_view_properties: EntityProperties,
 
-    /// Base component overrides that are inherited by all entities.
-    pub root_component_overrides: IntMap<ComponentName, OverridePath>,
+    /// Query range that data results should fall back to if they don't specify their own.
+    pub default_query_range: QueryRange,
 }
 
 /// Trait for resolving properties needed by most implementations of [`DataQuery`]
@@ -22,8 +19,10 @@ pub struct EntityOverrideContext {
 pub trait PropertyResolver {
     fn update_overrides(
         &self,
-        ctx: &StoreContext<'_>,
-        query: &LatestAtQuery,
+        blueprint: &EntityDb,
+        blueprint_query: &LatestAtQuery,
+        active_timeline: &Timeline,
+        space_view_class_registry: &SpaceViewClassRegistry,
         query_result: &mut DataQueryResult,
     );
 }

commit 0c14528b9254df5ee60f19e8159165d3b1d4c77a

Fix auto properties no longer being applied for legacy properties (`EntityProperties`) (#6190)
### What

* Fixes #6175

This regressed in #6164. Decided not to roll back things (which would
have been hard) but instead reimplement how these properties are passed
through and make their role more obvious.


### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using examples from latest `main` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/6190?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/6190?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG
* [x] If applicable, add a new check to the [release
checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!

- [PR Build Summary](https://build.rerun.io/pr/6190)
- [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
- [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

To run all checks from `main`, comment on the PR with `@rerun-bot
full-check`.

---------

Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>

crates/re_space_view/src/data_query.rs
@@ -1,12 +1,17 @@
-use re_entity_db::{external::re_data_store::LatestAtQuery, EntityDb, EntityProperties};
+use re_entity_db::{
+    external::re_data_store::LatestAtQuery, EntityDb, EntityProperties, EntityPropertyMap,
+};
 use re_log_types::Timeline;
 use re_viewer_context::{
     DataQueryResult, PerVisualizer, QueryRange, SpaceViewClassRegistry, StoreContext,
     VisualizableEntities,
 };
 
-pub struct EntityOverrideContext {
+pub struct EntityOverrideContext<'a> {
     pub legacy_space_view_properties: EntityProperties,
+
+    /// Provides auto properties for each entity.
+    pub legacy_auto_properties: &'a EntityPropertyMap,
 
     /// Query range that data results should fall back to if they don't specify their own.
     pub default_query_range: QueryRange,
@@ -23,6 +28,7 @@ pub trait PropertyResolver {
         blueprint_query: &LatestAtQuery,
         active_timeline: &Timeline,
         space_view_class_registry: &SpaceViewClassRegistry,
+        legacy_auto_properties: &EntityPropertyMap,
         query_result: &mut DataQueryResult,
     );
 }
@@ -33,8 +39,6 @@ pub trait PropertyResolver {
 /// to be added to a `SpaceView` including both the [`re_log_types::EntityPath`] and context for any overrides.
 pub trait DataQuery {
     /// Execute a full query, returning a `DataResultTree` containing all results.
-    ///
-    /// `auto_properties` is a map containing any heuristic-derived auto properties for the given `SpaceView`.
     ///
     /// This is used when building up the contents for a `SpaceView`.
     fn execute_query(

crates/re_space_view/src/space_view.rs
@@ -330,7 +330,7 @@ impl SpaceViewBlueprint {
         &self,
         ctx: &ViewerContext<'_>,
         view_state: &mut dyn SpaceViewState,
-        view_props: &mut EntityPropertyMap,
+        auto_properties: &mut EntityPropertyMap,
     ) {
         let query_result = ctx.lookup_query_result(self.id).clone();
 
@@ -353,7 +353,7 @@ impl SpaceViewBlueprint {
             ctx,
             view_state,
             &per_system_entities,
-            view_props,
+            auto_properties,
         );
     }
 
@@ -490,6 +490,7 @@ mod tests {
         let timeline = Timeline::new("time", re_log_types::TimeType::Time);
         let mut recording = EntityDb::new(StoreId::random(re_log_types::StoreKind::Recording));
         let mut blueprint = EntityDb::new(StoreId::random(re_log_types::StoreKind::Blueprint));
+        let legacy_auto_properties = EntityPropertyMap::default();
 
         let points = Points3D::new(vec![[1.0, 2.0, 3.0]]);
 
@@ -559,6 +560,7 @@ mod tests {
                 &blueprint_query,
                 &timeline,
                 &space_view_class_registry,
+                &legacy_auto_properties,
                 &mut query_result,
             );
 
@@ -610,6 +612,7 @@ mod tests {
                 &blueprint_query,
                 &timeline,
                 &space_view_class_registry,
+                &legacy_auto_properties,
                 &mut query_result,
             );
 
@@ -667,6 +670,7 @@ mod tests {
                 &blueprint_query,
                 &timeline,
                 &space_view_class_registry,
+                &legacy_auto_properties,
                 &mut query_result,
             );
 
@@ -693,6 +697,7 @@ mod tests {
     fn test_component_overrides() {
         let space_view_class_registry = SpaceViewClassRegistry::default();
         let timeline = Timeline::new("time", re_log_types::TimeType::Time);
+        let legacy_auto_properties = EntityPropertyMap::default();
         let mut recording = EntityDb::new(StoreId::random(re_log_types::StoreKind::Recording));
         let mut visualizable_entities_per_visualizer =
             PerVisualizer::<VisualizableEntities>::default();
@@ -948,6 +953,7 @@ mod tests {
                 &blueprint_query,
                 &timeline,
                 &space_view_class_registry,
+                &legacy_auto_properties,
                 &mut query_result,
             );
 

crates/re_space_view/src/space_view_contents.rs
@@ -4,7 +4,7 @@ use smallvec::SmallVec;
 
 use re_entity_db::{
     external::{re_data_store::LatestAtQuery, re_query::PromiseResult},
-    EntityDb, EntityProperties, EntityPropertiesComponent, EntityTree,
+    EntityDb, EntityProperties, EntityPropertiesComponent, EntityPropertyMap, EntityTree,
 };
 use re_log_types::{
     path::RuleEffect, EntityPath, EntityPathFilter, EntityPathRule, EntityPathSubs, Timeline,
@@ -386,13 +386,14 @@ impl DataQueryPropertyResolver<'_> {
     ///  may include properties from the `SpaceView` or `DataQuery`.
     ///  - The individual overrides are found by walking an override subtree under the `data_query/<id>/individual_overrides`
     ///  - The recursive overrides are found by walking an override subtree under the `data_query/<id>/recursive_overrides`
-    fn build_override_context(
+    fn build_override_context<'a>(
         &self,
         blueprint: &EntityDb,
         blueprint_query: &LatestAtQuery,
         active_timeline: &Timeline,
         space_view_class_registry: &SpaceViewClassRegistry,
-    ) -> EntityOverrideContext {
+        legacy_auto_properties: &'a EntityPropertyMap,
+    ) -> EntityOverrideContext<'a> {
         re_tracing::profile_function!();
 
         let legacy_space_view_properties = self
@@ -410,6 +411,7 @@ impl DataQueryPropertyResolver<'_> {
         EntityOverrideContext {
             legacy_space_view_properties,
             default_query_range,
+            legacy_auto_properties,
         }
     }
 
@@ -424,7 +426,7 @@ impl DataQueryPropertyResolver<'_> {
         blueprint_query: &LatestAtQuery,
         active_timeline: &Timeline,
         query_result: &mut DataQueryResult,
-        override_context: &EntityOverrideContext,
+        override_context: &EntityOverrideContext<'_>,
         recursive_accumulated_legacy_properties: &EntityProperties,
         recursive_property_overrides: &IntMap<ComponentName, OverridePath>,
         handle: DataResultHandle,
@@ -461,11 +463,16 @@ impl DataQueryPropertyResolver<'_> {
                 } else {
                     recursive_accumulated_legacy_properties.clone()
                 };
+            let default_legacy_properties = override_context
+                .legacy_auto_properties
+                .get(&node.data_result.entity_path);
             let accumulated_legacy_properties =
                 if let Some(individual) = individual_legacy_properties.as_ref() {
-                    recursive_accumulated_legacy_properties.with_child(individual)
+                    recursive_accumulated_legacy_properties
+                        .with_child(individual)
+                        .with_child(&default_legacy_properties)
                 } else {
-                    recursive_accumulated_legacy_properties.clone()
+                    recursive_accumulated_legacy_properties.with_child(&default_legacy_properties)
                 };
 
             // Update visualizers from overrides.
@@ -611,6 +618,7 @@ impl<'a> PropertyResolver for DataQueryPropertyResolver<'a> {
         blueprint_query: &LatestAtQuery,
         active_timeline: &Timeline,
         space_view_class_registry: &SpaceViewClassRegistry,
+        legacy_auto_properties: &EntityPropertyMap,
         query_result: &mut DataQueryResult,
     ) {
         re_tracing::profile_function!();
@@ -619,6 +627,7 @@ impl<'a> PropertyResolver for DataQueryPropertyResolver<'a> {
             blueprint_query,
             active_timeline,
             space_view_class_registry,
+            legacy_auto_properties,
         );
 
         if let Some(root) = query_result.tree.root_handle() {

crates/re_space_view_spatial/src/heuristics.rs
@@ -21,29 +21,32 @@ use crate::{
     },
 };
 
-pub fn update_object_property_heuristics(
+pub fn generate_auto_legacy_properties(
     ctx: &ViewerContext<'_>,
     per_system_entities: &PerSystemEntities,
-    entity_properties: &mut re_entity_db::EntityPropertyMap,
     scene_bbox_accum: &macaw::BoundingBox,
     spatial_kind: SpatialSpaceViewKind,
-) {
+) -> re_entity_db::EntityPropertyMap {
     re_tracing::profile_function!();
 
+    let mut auto_properties = re_entity_db::EntityPropertyMap::default();
+
     // Do pinhole properties before, since they may be used in transform3d heuristics.
-    update_pinhole_property_heuristics(per_system_entities, entity_properties, scene_bbox_accum);
+    update_pinhole_property_heuristics(per_system_entities, &mut auto_properties, scene_bbox_accum);
     update_depth_cloud_property_heuristics(
         ctx,
         per_system_entities,
-        entity_properties,
+        &mut auto_properties,
         spatial_kind,
     );
     update_transform3d_lines_heuristics(
         ctx,
         per_system_entities,
-        entity_properties,
+        &mut auto_properties,
         scene_bbox_accum,
     );
+
+    auto_properties
 }
 
 pub fn auto_size_world_heuristic(
@@ -73,35 +76,34 @@ pub fn auto_size_world_heuristic(
 
 fn update_pinhole_property_heuristics(
     per_system_entities: &PerSystemEntities,
-    entity_properties: &mut re_entity_db::EntityPropertyMap,
+    auto_properties: &mut re_entity_db::EntityPropertyMap,
     scene_bbox_accum: &macaw::BoundingBox,
 ) {
     for ent_path in per_system_entities
         .get(&CamerasVisualizer::identifier())
         .unwrap_or(&BTreeSet::new())
     {
-        let mut properties = entity_properties.get(ent_path);
-        if properties.pinhole_image_plane_distance.is_auto() {
-            let scene_size = scene_bbox_accum.size().length();
-            let default_image_plane_distance = if scene_size.is_finite() && scene_size > 0.0 {
-                scene_size * 0.02 // Works pretty well for `examples/python/open_photogrammetry_format/open_photogrammetry_format.py --no-frames`
-            } else {
-                // This value somewhat arbitrary. In almost all cases where the scene has defined bounds
-                // the heuristic will change it or it will be user edited. In the case of non-defined bounds
-                // this value works better with the default camera setup.
-                0.3
-            };
-            properties.pinhole_image_plane_distance =
-                EditableAutoValue::Auto(default_image_plane_distance);
-            entity_properties.overwrite_properties(ent_path.clone(), properties);
-        }
+        let mut properties = auto_properties.get(ent_path);
+
+        let scene_size = scene_bbox_accum.size().length();
+        let default_image_plane_distance = if scene_size.is_finite() && scene_size > 0.0 {
+            scene_size * 0.02 // Works pretty well for `examples/python/open_photogrammetry_format/open_photogrammetry_format.py --no-frames`
+        } else {
+            // This value somewhat arbitrary. In almost all cases where the scene has defined bounds
+            // the heuristic will change it or it will be user edited. In the case of non-defined bounds
+            // this value works better with the default camera setup.
+            0.3
+        };
+        properties.pinhole_image_plane_distance =
+            EditableAutoValue::Auto(default_image_plane_distance);
+        auto_properties.overwrite_properties(ent_path.clone(), properties);
     }
 }
 
 fn update_depth_cloud_property_heuristics(
     ctx: &ViewerContext<'_>,
     per_system_entities: &PerSystemEntities,
-    entity_properties: &mut re_entity_db::EntityPropertyMap,
+    auto_properties: &mut re_entity_db::EntityPropertyMap,
     spatial_kind: SpatialSpaceViewKind,
 ) {
     // TODO(andreas): There should be a depth cloud system
@@ -126,30 +128,23 @@ fn update_depth_cloud_property_heuristics(
             .latest_at_component::<DepthMeter>(ent_path, &ctx.current_query())
             .map(|meter| meter.value.0);
 
-        let mut properties = entity_properties.get(ent_path);
-        if properties.backproject_depth.is_auto() {
-            properties.backproject_depth = EditableAutoValue::Auto(
-                meaning == TensorDataMeaning::Depth && spatial_kind == SpatialSpaceViewKind::ThreeD,
-            );
-        }
+        let mut properties = auto_properties.get(ent_path);
+        properties.backproject_depth = EditableAutoValue::Auto(
+            meaning == TensorDataMeaning::Depth && spatial_kind == SpatialSpaceViewKind::ThreeD,
+        );
 
         if meaning == TensorDataMeaning::Depth {
-            if properties.depth_from_world_scale.is_auto() {
-                let auto = meter.unwrap_or_else(|| {
-                    if tensor.dtype().is_integer() {
-                        1000.0
-                    } else {
-                        1.0
-                    }
-                });
-                properties.depth_from_world_scale = EditableAutoValue::Auto(auto);
-            }
+            let auto = meter.unwrap_or_else(|| {
+                if tensor.dtype().is_integer() {
+                    1000.0
+                } else {
+                    1.0
+                }
+            });
+            properties.depth_from_world_scale = EditableAutoValue::Auto(auto);
+            properties.backproject_radius_scale = EditableAutoValue::Auto(1.0);
 
-            if properties.backproject_radius_scale.is_auto() {
-                properties.backproject_radius_scale = EditableAutoValue::Auto(1.0);
-            }
-
-            entity_properties.overwrite_properties(ent_path.clone(), properties);
+            auto_properties.overwrite_properties(ent_path.clone(), properties);
         }
     }
 }
@@ -157,7 +152,7 @@ fn update_depth_cloud_property_heuristics(
 fn update_transform3d_lines_heuristics(
     ctx: &ViewerContext<'_>,
     per_system_entities: &PerSystemEntities,
-    entity_properties: &mut re_entity_db::EntityPropertyMap,
+    auto_properties: &mut re_entity_db::EntityPropertyMap,
     scene_bbox_accum: &macaw::BoundingBox,
 ) {
     for ent_path in per_system_entities
@@ -186,37 +181,33 @@ fn update_transform3d_lines_heuristics(
             None
         }
 
-        let mut properties = entity_properties.get(ent_path);
-        if properties.transform_3d_visible.is_auto() {
-            // By default show the transform if it is a camera extrinsic,
-            // or if this entity only contains Transform3D components.
-            let only_has_transform_components = ctx
-                .recording_store()
-                .all_components(&ctx.current_query().timeline(), ent_path)
-                .map_or(false, |c| {
-                    c.iter()
-                        .all(|c| re_types::archetypes::Transform3D::all_components().contains(c))
-                });
-            properties.transform_3d_visible = EditableAutoValue::Auto(
-                only_has_transform_components || is_pinhole_extrinsics_of(ent_path, ctx).is_some(),
-            );
+        let mut properties = auto_properties.get(ent_path);
+
+        // By default show the transform if it is a camera extrinsic,
+        // or if this entity only contains Transform3D components.
+        let only_has_transform_components = ctx
+            .recording_store()
+            .all_components(&ctx.current_query().timeline(), ent_path)
+            .map_or(false, |c| {
+                c.iter()
+                    .all(|c| re_types::archetypes::Transform3D::all_components().contains(c))
+            });
+        properties.transform_3d_visible = EditableAutoValue::Auto(
+            only_has_transform_components || is_pinhole_extrinsics_of(ent_path, ctx).is_some(),
+        );
+
+        if let Some(pinhole_path) = is_pinhole_extrinsics_of(ent_path, ctx) {
+            // If there's a pinhole, we orient ourselves on its image plane distance
+            let pinhole_path_props = auto_properties.get(pinhole_path);
+            properties.transform_3d_size =
+                EditableAutoValue::Auto(*pinhole_path_props.pinhole_image_plane_distance * 0.25);
+        } else {
+            // Size should be proportional to the scene extent, here covered by its diagonal
+            let diagonal_length = (scene_bbox_accum.max - scene_bbox_accum.min).length();
+            properties.transform_3d_size = EditableAutoValue::Auto(diagonal_length * 0.05);
         }
 
-        if properties.transform_3d_size.is_auto() {
-            if let Some(pinhole_path) = is_pinhole_extrinsics_of(ent_path, ctx) {
-                // If there's a pinhole, we orient ourselves on its image plane distance
-                let pinhole_path_props = entity_properties.get(pinhole_path);
-                properties.transform_3d_size = EditableAutoValue::Auto(
-                    *pinhole_path_props.pinhole_image_plane_distance * 0.25,
-                );
-            } else {
-                // Size should be proportional to the scene extent, here covered by its diagonal
-                let diagonal_length = (scene_bbox_accum.max - scene_bbox_accum.min).length();
-                properties.transform_3d_size = EditableAutoValue::Auto(diagonal_length * 0.05);
-            }
-        }
-
-        entity_properties.overwrite_properties(ent_path.clone(), properties);
+        auto_properties.overwrite_properties(ent_path.clone(), properties);
     }
 }
 

crates/re_space_view_spatial/src/space_view_2d.rs
@@ -18,7 +18,7 @@ use re_viewer_context::{
 use crate::{
     contexts::{register_spatial_contexts, PrimitiveCounter},
     heuristics::{
-        default_visualized_entities_for_visualizer_kind, update_object_property_heuristics,
+        default_visualized_entities_for_visualizer_kind, generate_auto_legacy_properties,
     },
     max_image_dimension_subscriber::{ImageDimensions, MaxImageDimensions},
     spatial_topology::{SpatialTopology, SubSpaceConnectionFlags},
@@ -156,15 +156,14 @@ impl SpaceViewClass for SpatialSpaceView2D {
         ctx: &ViewerContext<'_>,
         state: &mut dyn SpaceViewState,
         ent_paths: &PerSystemEntities,
-        entity_properties: &mut re_entity_db::EntityPropertyMap,
+        auto_properties: &mut re_entity_db::EntityPropertyMap,
     ) {
         let Ok(state) = state.downcast_mut::<SpatialSpaceViewState>() else {
             return;
         };
-        update_object_property_heuristics(
+        *auto_properties = generate_auto_legacy_properties(
             ctx,
             ent_paths,
-            entity_properties,
             &state.bounding_boxes.accumulated,
             SpatialSpaceViewKind::TwoD,
         );

crates/re_space_view_spatial/src/space_view_3d.rs
@@ -13,7 +13,7 @@ use re_viewer_context::{
 use crate::{
     contexts::{register_spatial_contexts, PrimitiveCounter},
     heuristics::{
-        default_visualized_entities_for_visualizer_kind, update_object_property_heuristics,
+        default_visualized_entities_for_visualizer_kind, generate_auto_legacy_properties,
     },
     spatial_topology::{HeuristicHints, SpatialTopology, SubSpaceConnectionFlags},
     ui::{format_vector, SpatialSpaceViewState},
@@ -278,15 +278,14 @@ impl SpaceViewClass for SpatialSpaceView3D {
         ctx: &ViewerContext<'_>,
         state: &mut dyn SpaceViewState,
         ent_paths: &PerSystemEntities,
-        entity_properties: &mut re_entity_db::EntityPropertyMap,
+        auto_properties: &mut re_entity_db::EntityPropertyMap,
     ) {
         let Ok(state) = state.downcast_mut::<SpatialSpaceViewState>() else {
             return;
         };
-        update_object_property_heuristics(
+        *auto_properties = generate_auto_legacy_properties(
             ctx,
             ent_paths,
-            entity_properties,
             &state.bounding_boxes.accumulated,
             SpatialSpaceViewKind::ThreeD,
         );

crates/re_space_view_spatial/src/visualizers/results_ext.rs
@@ -120,7 +120,8 @@ impl RangeResultsExt for LatestAtResults {
         // In particular, it is pretty common to have a secondary component be more recent than the
         // associated primary component in latest-at contexts, e.g. colors in an otherwise fixed
         // point cloud being changed each frame.
-        let data = RangeData::from_latest_at(resolver, results, Some((TimeInt::MIN, RowId::ZERO)));
+        let data =
+            RangeData::from_latest_at(resolver, results, Some((TimeInt::STATIC, RowId::ZERO)));
 
         // TODO(#5607): what should happen if the promise is still pending?
         let (front_status, back_status) = data.status();

crates/re_viewer/src/app_state.rs
@@ -267,11 +267,13 @@ impl AppState {
                         &visualizable_entities,
                         &indicated_entities_per_visualizer,
                     );
+
                     resolver.update_overrides(
                         store_context.blueprint,
                         &blueprint_query,
                         rec_cfg.time_ctrl.read().timeline(),
                         space_view_class_registry,
+                        viewport.state.legacy_auto_properties(space_view.id),
                         query_result,
                     );
                 }

crates/re_viewer/src/ui/selection_panel.rs
@@ -951,19 +951,20 @@ fn blueprint_ui_for_data_result(
                 .lookup_result_by_path(entity_path)
                 .cloned()
             {
-                let mut props = data_result
-                    .individual_properties()
-                    .cloned()
-                    .unwrap_or_default();
+                let mut accumulated_legacy_props = data_result.accumulated_properties().clone();
+                let accumulated_legacy_props_before = accumulated_legacy_props.clone();
 
                 entity_props_ui(
                     ctx,
                     ui,
                     ctx.lookup_query_result(space_view_id),
                     entity_path,
-                    &mut props,
+                    &mut accumulated_legacy_props,
                 );
-                data_result.save_individual_override_properties(ctx, Some(props));
+                if accumulated_legacy_props != accumulated_legacy_props_before {
+                    data_result
+                        .save_individual_override_properties(ctx, Some(accumulated_legacy_props));
+                }
             }
         }
     }

crates/re_viewport/src/viewport.rs
@@ -75,7 +75,7 @@ impl ViewportState {
             })
     }
 
-    pub fn space_view_props(&self, space_view_id: SpaceViewId) -> &EntityPropertyMap {
+    pub fn legacy_auto_properties(&self, space_view_id: SpaceViewId) -> &EntityPropertyMap {
         self.space_view_states
             .get(&space_view_id)
             .map_or(&DEFAULT_PROPS, |state| &state.auto_properties)