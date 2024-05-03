e4cb2e4c07ed2f797edfce2c2e7385df44668a68
Andreas Reich <r_andreas2@web.de>
2024-05-03 18:58:37 +0200

    document utility methods on time_range_boundary

--- rerun_py/rerun_sdk/rerun/datatypes/time_range_boundary_ext.py
+++ rerun_py/rerun_sdk/rerun/datatypes/time_range_boundary_ext.py
@@ -13,18 +13,44 @@ class TimeRangeBoundaryExt:
 
     @staticmethod
     def cursor_relative(time: datatypes.TimeIntLike = 0) -> TimeRangeBoundary:
+        """
+        Boundary that is relative to the timeline cursor.
+
+        Parameters
+        ----------
+        time:
+            Offset from the cursor time, can be positive or negative.
+
+        """
+
         from .time_range_boundary import TimeRangeBoundary
 
         return TimeRangeBoundary(inner=time, kind="cursor_relative")
 
     @staticmethod
     def infinite() -> TimeRangeBoundary:
+        """
+        Boundary that extends to infinity.
+
+        Depending on the context, this can mean the beginning or the end of the timeline.
+        """
+
         from .time_range_boundary import TimeRangeBoundary
 
         return TimeRangeBoundary(inner=None, kind="infinite")
 
     @staticmethod
     def absolute(time: datatypes.TimeIntLike) -> TimeRangeBoundary:
+        """
+        Boundary that is at an absolute time.
+
+        Parameters
+        ----------
+        time:
+            Absolute time value.
+
+        """
+
         from .time_range_boundary import TimeRangeBoundary
 
         return TimeRangeBoundary(inner=time, kind="absolute")
