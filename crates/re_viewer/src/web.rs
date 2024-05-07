7c7a16218f002d86de655e0232c00063a7b3980b
Jeremy Leibs <jeremy@rerun.io>
2024-05-07 17:03:53 -0400

    Plumb through hide_welcome_screen for javascript APIs

--- crates/re_viewer/src/web.rs
+++ crates/re_viewer/src/web.rs
@@ -46,6 +46,7 @@ impl WebHandle {
     pub async fn start(
         &self,
         canvas_id: &str,
+        hide_welcome_screen: Option<bool>,
         url: Option<String>,
         manifest_url: Option<String>,
         force_wgpu_backend: Option<String>,
@@ -63,7 +64,12 @@ impl WebHandle {
                 canvas_id,
                 web_options,
                 Box::new(move |cc| {
-                    let app = create_app(cc, &url, &manifest_url);
+                    let app = create_app(
+                        cc,
+                        &url,
+                        &manifest_url,
+                        hide_welcome_screen.unwrap_or(false),
+                    );
                     Box::new(app)
                 }),
             )
@@ -213,6 +219,7 @@ fn create_app(
     cc: &eframe::CreationContext<'_>,
     url: &Option<String>,
     manifest_url: &Option<String>,
+    hide_welcome_screen: bool,
 ) -> crate::App {
     let build_info = re_build_info::build_info!();
     let app_env = crate::AppEnvironment::Web {
@@ -228,6 +235,7 @@ fn create_app(
         is_in_notebook: is_in_notebook(&cc.integration_info),
         expect_data_soon: None,
         force_wgpu_backend: None,
+        hide_welcome_screen: hide_welcome_screen,
     };
     let re_ui = crate::customize_eframe(cc);
 
