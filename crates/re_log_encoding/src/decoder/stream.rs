6420faf56f4fc0a5928a40fe0e1fe10aed59e150
Clement Rey <cr.rey.clement@gmail.com>
2024-05-14 19:37:51 +0200

    fix test

--- crates/re_log_encoding/src/decoder/stream.rs
+++ crates/re_log_encoding/src/decoder/stream.rs
@@ -256,7 +256,7 @@ mod tests {
                 is_official_example: false,
                 started: Time::from_ns_since_epoch(0),
                 store_source: StoreSource::Unknown,
-                store_version: None,
+                store_version: Some(CrateVersion::LOCAL),
             },
         })
     }
