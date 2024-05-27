f55124673600d046dd2259662d959102b4d410ef
Clement Rey <cr.rey.clement@gmail.com>
2024-05-27 17:39:17 +0200

    implement chunk micro-batcher

--- crates/re_chunk/src/lib.rs
+++ crates/re_chunk/src/lib.rs
@@ -9,9 +9,17 @@ mod shuffle;
 mod transport;
 mod util;
 
+#[cfg(not(target_arch = "wasm32"))]
+mod batcher;
+
 pub use self::chunk::{Chunk, ChunkError, ChunkId, ChunkResult, ChunkTimeline};
 pub use self::transport::TransportChunk;
 pub use self::util::arrays_to_list_array;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub use self::batcher::{
+    ChunkBatcher, ChunkBatcherConfig, ChunkBatcherError, ChunkBatcherResult, PendingRow,
+};
 
 pub mod external {
     pub use arrow2;
