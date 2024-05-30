d69c795a093cd59941438206b8694c39369efd7f
jprochazk <1665677+jprochazk@users.noreply.github.com>
2024-05-30 09:26:53 +0200

    fix

--- crates/re_types/src/blueprint/components/panel_state_ext.rs
+++ crates/re_types/src/blueprint/components/panel_state_ext.rs
@@ -12,12 +12,6 @@ impl PanelState {
         self == &Self::Hidden
     }
 
-    /// Returns `true` if self is [`PanelState::Expanded`]
-    #[inline]
-    pub fn is_hidden(&self) -> bool {
-        self == &Self::Hidden
-    }
-
     /// Sets the panel to [`Self::Hidden`] if it is collapsed or expanded, and [`Self::Expanded`] if it is hidden.
     #[inline]
     pub fn toggle(self) -> Self {
