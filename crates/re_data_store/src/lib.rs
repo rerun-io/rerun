2f7cfa8ee7a916d74977c02c18df7f989f955c4a
Clement Rey <cr.rey.clement@gmail.com>
2024-06-03 16:16:22 +0200

    wip

--- crates/re_data_store/src/lib.rs
+++ crates/re_data_store/src/lib.rs
@@ -52,3 +52,55 @@ pub use re_log_types::{ResolvedTimeRange, TimeInt, TimeType, Timeline}; // for p
 pub mod external {
     pub use arrow2;
 }
+
+// ---
+
+/// The index of a row's worth of data.
+///
+/// Every row of data in Rerun is uniquely identified, and globally ordered, by
+/// its index: a _data time_ and a row ID.
+///
+/// ## Ordering
+///
+/// The ordering of two `DataIndex`es is di
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct DataIndex(pub TimeInt, pub re_log_types::RowId);
+
+impl PartialOrd for DataIndex {
+    #[inline]
+    fn partial_cmp(&self, rhs: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(rhs))
+    }
+}
+
+impl Ord for DataIndex {
+    #[inline]
+    fn cmp(&self, rhs: &Self) -> std::cmp::Ordering {
+        match (*self, *rhs) {
+            (Self(TimeInt::STATIC, lhs_row_id), Self(TimeInt::STATIC, rhs_row_id)) => {
+                lhs_row_id.cmp(&rhs_row_id)
+            }
+            (_, Self(TimeInt::STATIC, _)) => std::cmp::Ordering::Less,
+            (Self(TimeInt::STATIC, _), _) => std::cmp::Ordering::Greater,
+            (Self(lhs_data_time, lhs_row_id), Self(rhs_data_time, rhs_row_id)) => {
+                (lhs_data_time, lhs_row_id).cmp(&(rhs_data_time, rhs_row_id))
+            }
+        }
+    }
+}
+
+impl DataIndex {
+    #[inline]
+    pub fn data_time(&self) -> TimeInt {
+        self.0
+    }
+
+    #[inline]
+    pub fn row_id(&self) -> re_log_types::RowId {
+        self.1
+    }
+}
+
+// TODO
+#[test]
+fn data_index_ordering() {}
