8730533dcf583f0bfe84ed330b7099b0bb5f0bb7
Clement Rey <cr.rey.clement@gmail.com>
2024-06-03 14:53:11 +0200

    get rid of clear cascades entirely

--- crates/re_entity_db/src/lib.rs
+++ crates/re_entity_db/src/lib.rs
@@ -29,8 +29,6 @@ pub use self::{
     versioned_instance_path::{VersionedInstancePath, VersionedInstancePathHash},
 };
 
-pub(crate) use self::entity_tree::{ClearCascade, CompactedStoreEvents};
-
 use re_log_types::DataTableError;
 pub use re_log_types::{EntityPath, EntityPathPart, TimeInt, Timeline};
 
