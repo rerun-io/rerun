c87d22194ac4cb243308362fd2bf61e6a134f0d8
Antoine Beyeler <antoine@rerun.io>
2024-06-04 17:43:21 +0200

    Toggle switch now becomes blue when ON

--- crates/re_ui/src/toggle_switch.rs
+++ crates/re_ui/src/toggle_switch.rs
@@ -18,11 +18,19 @@ fn toggle_switch_ui(ui: &mut egui::Ui, height: f32, on: &mut bool) -> egui::Resp
         let how_on = ui.ctx().animate_bool(response.id, *on);
         let visuals = ui.style().interact(&response);
         let expanded_rect = visual_rect.expand(visuals.expansion);
-        let fg_fill = visuals.bg_fill;
-        let bg_fill = visuals.text_color();
+        let fg_fill_off = visuals.bg_fill;
+        let fg_fill_on = egui::Color32::from_rgba_premultiplied(0, 128, 255, 255);
+        let fg_fill = egui::Color32::from_rgba_premultiplied(
+            lerp_u8(fg_fill_off.r(), fg_fill_on.r(), how_on),
+            lerp_u8(fg_fill_off.g(), fg_fill_on.g(), how_on),
+            lerp_u8(fg_fill_off.b(), fg_fill_on.b(), how_on),
+            lerp_u8(fg_fill_off.a(), fg_fill_on.a(), how_on),
+        );
+        let bg_fill_off = visuals.text_color();
+
         let rounding = 0.5 * expanded_rect.height();
         ui.painter()
-            .rect(expanded_rect, rounding, bg_fill, egui::Stroke::NONE);
+            .rect_filled(expanded_rect, rounding, bg_fill_off);
         let circle_x = egui::lerp(
             (expanded_rect.left() + rounding)..=(expanded_rect.right() - rounding),
             how_on,
@@ -31,7 +39,7 @@ fn toggle_switch_ui(ui: &mut egui::Ui, height: f32, on: &mut bool) -> egui::Resp
         let circle_center = egui::pos2(circle_x, expanded_rect.center().y);
         let circle_radius = 0.3 * expanded_rect.height();
         ui.painter()
-            .circle(circle_center, circle_radius, fg_fill, egui::Stroke::NONE);
+            .circle_filled(circle_center, circle_radius, fg_fill);
     }
 
     response
@@ -47,4 +55,8 @@ fn toggle_switch_ui(ui: &mut egui::Ui, height: f32, on: &mut bool) -> egui::Resp
 #[allow(clippy::needless_pass_by_ref_mut)] // False positive, toggle_switch_ui needs &mut
 pub fn toggle_switch(height: f32, on: &mut bool) -> impl egui::Widget + '_ {
     move |ui: &mut egui::Ui| toggle_switch_ui(ui, height, on)
+}
+
+fn lerp_u8(a: u8, b: u8, t: f32) -> u8 {
+    (a as f32 + t * (b as f32 - a as f32)).round() as u8
 }
