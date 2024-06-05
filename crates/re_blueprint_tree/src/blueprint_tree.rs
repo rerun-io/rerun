d09048c362aa057a4ee31f862a0aa7a8a8bd461e
Antoine Beyeler <antoine@rerun.io>
2024-06-05 20:48:51 +0200

    post merge

--- crates/re_blueprint_tree/src/blueprint_tree.rs
+++ crates/re_blueprint_tree/src/blueprint_tree.rs
@@ -1043,14 +1043,9 @@ fn remove_button_ui(ui: &mut Ui, tooltip: &str) -> Response {
         .on_hover_text(tooltip)
 }
 
-fn visibility_button_ui(
-    ui: &mut egui::Ui,
-    enabled: bool,
-    visible: &mut bool,
-) -> egui::Response {
+fn visibility_button_ui(ui: &mut egui::Ui, enabled: bool, visible: &mut bool) -> egui::Response {
     ui.add_enabled_ui(enabled, |ui| {
-        ui
-            .visibility_toggle_button(visible)
+        ui.visibility_toggle_button(visible)
             .on_hover_text("Toggle visibility")
             .on_disabled_hover_text("A parent is invisible")
     })
