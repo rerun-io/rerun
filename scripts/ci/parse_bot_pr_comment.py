222bc6acd16c066219fe85f65e7642486eef448a
Andreas Reich <r_andreas2@web.de>
2024-06-17 11:10:27 +0200

    parse_bot_pr_comment.py writes to github_output directly now

--- scripts/ci/parse_bot_pr_comment.py
+++ scripts/ci/parse_bot_pr_comment.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 import os
 import re
-import sys
 
 """
 Script for parsing @rerun-bot PR comments.
@@ -11,19 +10,38 @@ Script for parsing @rerun-bot PR comments.
 In order to avoid executing code that an attacker might embed in a comment,
 the comment body is not passed as an argument, but instead through the `GITHUB_COMMENT_BODY` env
 variable which is set by the GitHub Actions workflow.
-
-Writes the desired content of `GITHUB_OUTPUT` to stdout.
 """
 
 # List of valid inputs for the bot
 valid_inputs = ["full-check"]
 
 
-comment_body = os.environ.get("GITHUB_COMMENT_BODY", "")
-bot_invocation = re.search(r"@rerun-bot\s+([a-zA-Z\-]*)", comment_body)
-if bot_invocation:
-    command = bot_invocation.group(1)
-    if str(command) in valid_inputs:
-        print(f"command={command}")
+def gh_set_output(key: str, text: str) -> None:
+    line = f"{key}={text}"
+    output_file = os.environ.get("GITHUB_OUTPUT")
+    if output_file is not None:
+        # append to output_file
+        with open(output_file, "a", encoding="utf8") as f:
+            f.write(f"{line}{os.linesep}")
     else:
-        print(f"Invalid command: {command}", file=sys.stderr)
+        # print when not running on CI
+        print(line)
+
+
+def main() -> None:
+    comment_body = os.environ.get("GITHUB_COMMENT_BODY", "")
+    bot_invocation = re.search(r"@rerun-bot\s+([a-zA-Z\-]*)", comment_body)
+
+    if bot_invocation:
+        command = bot_invocation.group(1)
+        if str(command) in valid_inputs:
+            print(f"Valid command found: {command}")
+            gh_set_output("command", command)
+        else:
+            print(f"Invalid command: {command}")
+    else:
+        print("No rerun-bot invocation found")
+
+
+if __name__ == "__main__":
+    main()
