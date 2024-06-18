f06fdd22bd94667721b06ae23fbca6657af117ea
Andreas Reich <r_andreas2@web.de>
2024-06-18 17:45:05 +0200

    add another exception to roundtrips.py

--- tests/roundtrips.py
+++ tests/roundtrips.py
@@ -44,6 +44,7 @@ opt_out = {
     "space_view_blueprint": ["cpp", "python", "rust"],
     "space_view_contents": ["cpp", "python", "rust"],
     "tensor_scalar_mapping": ["cpp", "python", "rust"],
+    "tensor_slice_selection": ["cpp", "python", "rust"],
     "tensor_view_fit": ["cpp", "python", "rust"],
     "viewport_blueprint": ["cpp", "python", "rust"],
     "visual_bounds2d": ["cpp", "python", "rust"],
