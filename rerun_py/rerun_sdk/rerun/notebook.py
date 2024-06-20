3a9f6766963bc8826cf3d8c143fec66e822be759
jprochazk <1665677+jprochazk@users.noreply.github.com>
2024-06-20 13:08:55 +0200

    fix lints

--- rerun_py/rerun_sdk/rerun/notebook.py
+++ rerun_py/rerun_sdk/rerun/notebook.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING, Any, Generator
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from .blueprint import BlueprintLike
