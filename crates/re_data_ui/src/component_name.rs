01e49f2eec1376ff71e26e927030de82e005a28c
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-06-23 22:38:26 +0200

    Show component docs when hovering

--- crates/re_data_ui/src/component_name.rs
+++ crates/re_data_ui/src/component_name.rs
@@ -20,6 +20,14 @@ impl DataUi for ComponentName {
                 ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Extend);
                 ui.label(format!("Full name: {}", self.full_name()));
 
+                // Only show the first line of the docs:
+                if let Some(markdown) = re_types_registry::components::registry()
+                    .get(self)
+                    .and_then(|info| info.docstring_md.lines().next())
+                {
+                    ui.markdown_ui(egui::Id::new(self), markdown);
+                }
+
                 if let Some(url) = self.doc_url() {
                     ui.re_hyperlink("Full documentation", url);
                 }
