rerun-io/rerun

commit ef2dbad83765b629d74e527beb29401a7b4b3288

Introduce visualizer blueprint query stack ui (#6605)
### What

* Fixes #4888

The visualizer ui now shows the full stack of
override/store/default/fallback and allows a wide variety with
interaction with it.


https://github.com/rerun-io/rerun/assets/1220815/e2e4900c-dbd4-4310-a547-3f0c014b154f

There's a new "more" button which gives access to various options for
setting the override or setting the view's default (feedback on what
goes on those lines welcome
<img width="207" alt="image"
src="https://github.com/rerun-io/rerun/assets/1220815/d3543026-e4a4-421a-8d9a-4705e635c457">

Furthermore, the "add" visualizer button is now part of the header
section:
<img width="397" alt="image"
src="https://github.com/rerun-io/rerun/assets/1220815/78fd7882-e338-461a-aa3a-ca39a0797595">


### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using examples from latest `main` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/6605?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/6605?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG
* [x] If applicable, add a new check to the [release
checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!

- [PR Build Summary](https://build.rerun.io/pr/6605)
- [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
- [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

To run all checks from `main`, comment on the PR with `@rerun-bot

crates/re_ui/src/ui_ext.rs
@@ -13,6 +13,65 @@ use crate::{
 
 static FULL_SPAN_TAG: &str = "rerun_full_span";
 
+/// Icon button to be used in the header of a panel.
+pub struct HeaderMenuButton<'a> {
+    pub icon: &'static Icon,
+    pub add_contents: Box<dyn FnOnce(&mut egui::Ui) + 'a>,
+    pub enabled: bool,
+    pub hover_text: Option<String>,
+    pub disabled_hover_text: Option<String>,
+}
+
+impl<'a> HeaderMenuButton<'a> {
+    pub fn new(icon: &'static Icon, add_contents: impl FnOnce(&mut egui::Ui) + 'a) -> Self {
+        Self {
+            icon,
+            add_contents: Box::new(add_contents),
+            enabled: true,
+            hover_text: None,
+            disabled_hover_text: None,
+        }
+    }
+
+    /// Sets enable/disable state of the button.
+    #[inline]
+    pub fn enabled(mut self, enabled: bool) -> Self {
+        self.enabled = enabled;
+        self
+    }
+
+    /// Sets text shown when the button hovered.
+    #[inline]
+    pub fn hover_text(mut self, hover_text: impl Into<String>) -> Self {
+        self.hover_text = Some(hover_text.into());
+        self
+    }
+
+    /// Sets text shown when the button is disabled and hovered.
+    #[inline]
+    pub fn disabled_hover_text(mut self, hover_text: impl Into<String>) -> Self {
+        self.disabled_hover_text = Some(hover_text.into());
+        self
+    }
+
+    fn show(self, ui: &mut egui::Ui) {
+        ui.add_enabled_ui(self.enabled, |ui| {
+            let mut response = egui::menu::menu_image_button(
+                ui,
+                ui.small_icon_button_widget(self.icon),
+                self.add_contents,
+            )
+            .response;
+            if let Some(hover_text) = self.hover_text {
+                response = response.on_hover_text(hover_text);
+            }
+            if let Some(disabled_hover_text) = self.disabled_hover_text {
+                response.on_disabled_hover_text(disabled_hover_text);
+            }
+        });
+    }
+}
+
 /// Rerun custom extensions to [`egui::Ui`].
 pub trait UiExt {
     fn ui(&self) -> &egui::Ui;
@@ -455,11 +514,12 @@ pub trait UiExt {
     /// Show a prominent collapsing header to be used as section delimitation in side panels.
     ///
     /// Note that a clip rect must be set (typically by the panel) to avoid any overdraw.
-    fn large_collapsing_header<R>(
+    fn large_collapsing_header_impl<R>(
         &mut self,
         label: &str,
         default_open: bool,
         add_body: impl FnOnce(&mut egui::Ui) -> R,
+        button: Option<HeaderMenuButton<'_>>,
     ) {
         let ui = self.ui_mut();
 
@@ -534,6 +594,12 @@ pub trait UiExt {
                 if header_response.clicked() {
                     state.toggle(ui);
                 }
+
+                if let Some(button) = button {
+                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                        button.show(ui);
+                    });
+                }
             },
         );
         state.show_body_unindented(ui, |ui| {
@@ -541,6 +607,31 @@ pub trait UiExt {
             add_body(ui);
             ui.add_space(4.0); // Same here
         });
+    }
+
+    /// Show a prominent collapsing header to be used as section delimitation in side panels.
+    ///
+    /// Note that a clip rect must be set (typically by the panel) to avoid any overdraw.
+    fn large_collapsing_header<R>(
+        &mut self,
+        label: &str,
+        default_open: bool,
+        add_body: impl FnOnce(&mut egui::Ui) -> R,
+    ) {
+        self.large_collapsing_header_impl(label, default_open, add_body, None);
+    }
+
+    /// Show a prominent collapsing header to be used as section delimitation in side panels with an image button.
+    ///
+    /// Note that a clip rect must be set (typically by the panel) to avoid any overdraw.
+    fn large_collapsing_header_with_button<R>(
+        &mut self,
+        label: &str,
+        default_open: bool,
+        add_body: impl FnOnce(&mut egui::Ui) -> R,
+        button: HeaderMenuButton<'_>,
+    ) {
+        self.large_collapsing_header_impl(label, default_open, add_body, Some(button));
     }
 
     /// Paint a collapsing triangle with rounded corners.

crates/re_selection_panel/Cargo.toml
@@ -23,11 +23,12 @@ re_context_menu.workspace = true
 re_data_store.workspace = true
 re_data_ui.workspace = true
 re_entity_db.workspace = true
-re_log.workspace = true
 re_log_types.workspace = true
-re_space_view.workspace = true
-re_space_view_time_series.workspace = true
+re_log.workspace = true
+re_query.workspace = true
 re_space_view_spatial.workspace = true
+re_space_view_time_series.workspace = true
+re_space_view.workspace = true
 re_tracing.workspace = true
 re_types_core.workspace = true
 re_types.workspace = true

crates/re_selection_panel/src/selection_panel.rs
@@ -159,9 +159,7 @@ impl SelectionPanel {
                     if instance_path.is_all() {
                         if let Some(view) = blueprint.space_views.get(view_id) {
                             let view_ctx = view.bundle_context_with_states(ctx, view_states);
-                            ui.large_collapsing_header("Visualizers", true, |ui| {
-                                visualizer_ui(&view_ctx, view, &instance_path.entity_path, ui);
-                            });
+                            visualizer_ui(&view_ctx, view, &instance_path.entity_path, ui);
                         }
                     }
                 }

crates/re_space_view/src/query.rs
@@ -72,12 +72,15 @@ pub fn range_with_blueprint_resolved_data(
 ///
 /// Data should be accessed via the [`crate::RangeResultsExt`] trait which is implemented for
 /// [`crate::HybridResults`].
+///
+/// If `query_shadowed_defaults` is true, all defaults will be queried, even if they are not used.
 pub fn latest_at_with_blueprint_resolved_data<'a>(
     ctx: &'a ViewContext<'a>,
     _annotations: Option<&'a re_viewer_context::Annotations>,
     latest_at_query: &LatestAtQuery,
     data_result: &'a re_viewer_context::DataResult,
     component_names: impl IntoIterator<Item = ComponentName>,
+    query_shadowed_defaults: bool,
 ) -> HybridLatestAtResults<'a> {
     re_tracing::profile_function!(data_result.entity_path.to_string());
 
@@ -85,8 +88,10 @@ pub fn latest_at_with_blueprint_resolved_data<'a>(
 
     let overrides = query_overrides(ctx.viewer_ctx, data_result, component_set.iter());
 
-    // No need to query for components that have overrides.
-    component_set.retain(|component| !overrides.components.contains_key(component));
+    // No need to query for components that have overrides unless opted in!
+    if !query_shadowed_defaults {
+        component_set.retain(|component| !overrides.components.contains_key(component));
+    }
 
     let results = ctx.viewer_ctx.recording().query_caches().latest_at(
         ctx.viewer_ctx.recording_store(),
@@ -191,12 +196,14 @@ impl DataResultQuery for DataResult {
         ctx: &'a ViewContext<'a>,
         latest_at_query: &'a LatestAtQuery,
     ) -> HybridLatestAtResults<'a> {
+        let query_shadowed_defaults = false;
         latest_at_with_blueprint_resolved_data(
             ctx,
             None,
             latest_at_query,
             self,
             A::all_components().iter().copied(),
+            query_shadowed_defaults,
         )
     }
 

crates/re_space_view_spatial/src/ui.rs
@@ -540,12 +540,14 @@ pub fn picking(
                 store,
             );
 
+            let query_shadowed_defaults = false;
             let results = latest_at_with_blueprint_resolved_data(
                 &view_ctx,
                 None,
                 &ctx.current_query(),
                 data_result,
                 [TensorData::name(), Colormap::name(), DepthMeter::name()],
+                query_shadowed_defaults,
             );
 
             // TODO(andreas): Just calling `results.get_mono::<TensorData>` would be a lot more elegant.

crates/re_space_view_spatial/src/visualizers/entity_iterator.rs
@@ -67,12 +67,14 @@ pub fn query_archetype_with_history<'a, A: Archetype>(
         }
         QueryRange::LatestAt => {
             let latest_query = LatestAtQuery::new(*timeline, timeline_cursor);
+            let query_shadowed_defaults = false;
             let results = latest_at_with_blueprint_resolved_data(
                 ctx,
                 None,
                 &latest_query,
                 data_result,
                 A::all_components().iter().copied(),
+                query_shadowed_defaults,
             );
             (latest_query, results).into()
         }

commit 23797dad7acd3b280e0fd706beb654292d07d269

Fix `large_collapsing_header` implementation (#6622)
### What

Improve `large_collapsing_header[_with_button]` with:
- text truncation when the available width is narrow
- fix sizing issue introduced in #6605 that would induce spurious
horizontal scrolling



https://github.com/rerun-io/rerun/assets/49431240/d298dc17-6065-4297-88b4-e0a67c20846c



### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using examples from latest `main` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/6622?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/6622?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG
* [x] If applicable, add a new check to the [release
checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!

- [PR Build Summary](https://build.rerun.io/pr/6622)
- [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
- [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

To run all checks from `main`, comment on the PR with `@rerun-bot
full-check`.

crates/re_ui/examples/re_ui_example/main.rs
@@ -243,9 +243,16 @@ impl eframe::App for ExampleApp {
 
             // ---
 
-            ui.large_collapsing_header("Data", true, |ui| {
-                ui.label("Some data here");
-            });
+            ui.large_collapsing_header_with_button(
+                "Data",
+                true,
+                |ui| {
+                    ui.label("Some data here");
+                },
+                re_ui::HeaderMenuButton::new(&re_ui::icons::ADD, |ui| {
+                    ui.weak("empty");
+                }),
+            );
             ui.large_collapsing_header("Blueprint", true, |ui| {
                 ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Extend);
                 ui.label("Some blueprint stuff here, that might be wide.");

crates/re_ui/src/ui_ext.rs
@@ -1,3 +1,4 @@
+use eframe::epaint::text::TextWrapping;
 use std::hash::Hash;
 
 use egui::{
@@ -56,6 +57,8 @@ impl<'a> HeaderMenuButton<'a> {
 
     fn show(self, ui: &mut egui::Ui) {
         ui.add_enabled_ui(self.enabled, |ui| {
+            ui.spacing_mut().item_spacing = egui::Vec2::ZERO;
+
             let mut response = egui::menu::menu_image_button(
                 ui,
                 ui.small_icon_button_widget(self.icon),
@@ -512,118 +515,16 @@ pub trait UiExt {
     }
 
     /// Show a prominent collapsing header to be used as section delimitation in side panels.
-    ///
-    /// Note that a clip rect must be set (typically by the panel) to avoid any overdraw.
-    fn large_collapsing_header_impl<R>(
-        &mut self,
-        label: &str,
-        default_open: bool,
-        add_body: impl FnOnce(&mut egui::Ui) -> R,
-        button: Option<HeaderMenuButton<'_>>,
-    ) {
-        let ui = self.ui_mut();
-
-        let mut state = egui::collapsing_header::CollapsingState::load_with_default_open(
-            ui.ctx(),
-            ui.make_persistent_id(label),
-            default_open,
-        );
-
-        let openness = state.openness(ui.ctx());
-
-        let height = DesignTokens::list_item_height();
-        let header_size = egui::vec2(ui.available_width(), height);
-
-        // Draw custom header.
-        ui.allocate_ui_with_layout(
-            header_size,
-            egui::Layout::left_to_right(egui::Align::Center),
-            |ui| {
-                ui.visuals_mut().widgets.hovered.expansion = 0.0;
-                ui.visuals_mut().widgets.active.expansion = 0.0;
-                ui.visuals_mut().widgets.open.expansion = 0.0;
-
-                let background_frame = ui.painter().add(egui::Shape::Noop);
-
-                let space_before_icon = 0.0;
-                let icon_width = ui.spacing().icon_width_inner;
-                let space_after_icon = ui.spacing().icon_spacing;
-
-                let font_id = egui::TextStyle::Button.resolve(ui.style());
-                let galley =
-                    ui.painter()
-                        .layout_no_wrap(label.to_owned(), font_id, Color32::PLACEHOLDER);
-
-                let desired_size = header_size.at_least(
-                    egui::vec2(space_before_icon + icon_width + space_after_icon, 0.0)
-                        + galley.size(),
-                );
-                let header_response = ui.allocate_response(desired_size, egui::Sense::click());
-                let rect = header_response.rect;
-
-                let icon_rect = egui::Rect::from_center_size(
-                    header_response.rect.left_center()
-                        + egui::vec2(space_before_icon + icon_width / 2.0, 0.0),
-                    egui::Vec2::splat(icon_width),
-                );
-                let icon_response = header_response.clone().with_new_rect(icon_rect);
-                ui.paint_collapsing_triangle(
-                    openness,
-                    icon_rect.center(),
-                    ui.style().interact(&icon_response),
-                );
-
-                let visuals = ui.style().interact(&header_response);
-
-                let optical_vertical_alignment = 0.5; // improves perceived vertical alignment
-                let text_pos = icon_response.rect.right_center()
-                    + egui::vec2(
-                        space_after_icon,
-                        -0.5 * galley.size().y + optical_vertical_alignment,
-                    );
-                ui.painter().galley(text_pos, galley, visuals.text_color());
-
-                // Let the rect cover the full panel width:
-                let bg_rect = egui::Rect::from_x_y_ranges(ui.full_span(), rect.y_range());
-
-                ui.painter().set(
-                    background_frame,
-                    Shape::rect_filled(bg_rect, 0.0, visuals.bg_fill),
-                );
-
-                if header_response.clicked() {
-                    state.toggle(ui);
-                }
-
-                if let Some(button) = button {
-                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                        button.show(ui);
-                    });
-                }
-            },
-        );
-        state.show_body_unindented(ui, |ui| {
-            ui.add_space(4.0); // Add space only if there is a body to make minimized headers stick together.
-            add_body(ui);
-            ui.add_space(4.0); // Same here
-        });
-    }
-
-    /// Show a prominent collapsing header to be used as section delimitation in side panels.
-    ///
-    /// Note that a clip rect must be set (typically by the panel) to avoid any overdraw.
     fn large_collapsing_header<R>(
         &mut self,
         label: &str,
         default_open: bool,
         add_body: impl FnOnce(&mut egui::Ui) -> R,
     ) {
-        self.large_collapsing_header_impl(label, default_open, add_body, None);
+        large_collapsing_header_impl(self.ui_mut(), label, default_open, add_body, None);
     }
 
     /// Show a prominent collapsing header to be used as section delimitation in side panels with an image button.
-    ///
-    /// Note that a clip rect must be set (typically by the panel) to avoid any overdraw.
     fn large_collapsing_header_with_button<R>(
         &mut self,
         label: &str,
@@ -631,7 +532,7 @@ pub trait UiExt {
         add_body: impl FnOnce(&mut egui::Ui) -> R,
         button: HeaderMenuButton<'_>,
     ) {
-        self.large_collapsing_header_impl(label, default_open, add_body, Some(button));
+        large_collapsing_header_impl(self.ui_mut(), label, default_open, add_body, Some(button));
     }
 
     /// Paint a collapsing triangle with rounded corners.
@@ -1152,4 +1053,119 @@ impl UiExt for egui::Ui {
     fn ui_mut(&mut self) -> &mut egui::Ui {
         self
     }
+}
+
+fn large_collapsing_header_impl<R>(
+    ui: &mut egui::Ui,
+    label: &str,
+    default_open: bool,
+    add_body: impl FnOnce(&mut egui::Ui) -> R,
+    button: Option<HeaderMenuButton<'_>>,
+) {
+    let mut state = egui::collapsing_header::CollapsingState::load_with_default_open(
+        ui.ctx(),
+        ui.make_persistent_id(label),
+        default_open,
+    );
+
+    let openness = state.openness(ui.ctx());
+
+    let height = DesignTokens::list_item_height();
+
+    // In some cases, the available width is not even, which cause some instability with the nested
+    // left-to-right in right-to-left UIs. Thus the `floor()`.
+    let header_size = egui::vec2(ui.available_width().floor(), height);
+
+    ui.scope(|ui| {
+        ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);
+
+        ui.allocate_ui_with_layout(
+            header_size,
+            egui::Layout::right_to_left(egui::Align::Center),
+            |ui| {
+                ui.visuals_mut().widgets.hovered.expansion = 0.0;
+                ui.visuals_mut().widgets.active.expansion = 0.0;
+                ui.visuals_mut().widgets.open.expansion = 0.0;
+
+                let background_frame = ui.painter().add(egui::Shape::Noop);
+
+                // draw button if any, and extract its width
+                let button_width = if let Some(button) = button {
+                    button.show(ui);
+                    ui.min_rect().width() + ui.spacing().icon_spacing
+                } else {
+                    0.0
+                };
+
+                let header_size_without_button =
+                    egui::vec2((header_size.x - button_width).floor(), header_size.y);
+
+                ui.allocate_ui_with_layout(
+                    header_size_without_button,
+                    egui::Layout::left_to_right(egui::Align::Center),
+                    |ui| {
+                        let space_before_icon = 0.0;
+                        let icon_width = ui.spacing().icon_width_inner;
+                        let space_after_icon = ui.spacing().icon_spacing;
+
+                        let mut layout_job = egui::WidgetText::from(label).into_layout_job(
+                            ui.style(),
+                            egui::FontSelection::Default,
+                            egui::Align::LEFT,
+                        );
+                        layout_job.wrap = TextWrapping::truncate_at_width(
+                            header_size_without_button.x
+                                - (space_before_icon + icon_width + space_after_icon),
+                        );
+                        let galley = ui.fonts(|fonts| fonts.layout_job(layout_job));
+
+                        let header_response =
+                            ui.allocate_response(header_size_without_button, egui::Sense::click());
+                        let rect = header_response.rect;
+
+                        let icon_rect = egui::Rect::from_center_size(
+                            header_response.rect.left_center()
+                                + egui::vec2(space_before_icon + icon_width / 2.0, 0.0),
+                            egui::Vec2::splat(icon_width),
+                        );
+                        let icon_response = header_response.clone().with_new_rect(icon_rect);
+                        ui.paint_collapsing_triangle(
+                            openness,
+                            icon_rect.center(),
+                            ui.style().interact(&icon_response),
+                        );
+
+                        let visuals = ui.style().interact(&header_response);
+
+                        let optical_vertical_alignment = 0.5; // improves perceived vertical alignment
+                        let text_pos = icon_response.rect.right_center()
+                            + egui::vec2(
+                                space_after_icon,
+                                -0.5 * galley.size().y + optical_vertical_alignment,
+                            );
+
+                        ui.painter().galley(text_pos, galley, visuals.text_color());
+
+                        // Let the rect cover the full panel width:
+                        let bg_rect = egui::Rect::from_x_y_ranges(ui.full_span(), rect.y_range());
+
+                        ui.painter().set(
+                            background_frame,
+                            Shape::rect_filled(bg_rect, 0.0, visuals.bg_fill),
+                        );
+
+                        if header_response.clicked() {
+                            state.toggle(ui);
+                        }
+                    },
+                );
+            },
+        );
+    });
+
+    state.show_body_unindented(ui, |ui| {
+        ui.add_space(4.0); // Add space only if there is a body to make minimized headers stick together.
+        add_body(ui);
+        ui.add_space(4.0); // Same here
+    });
 }