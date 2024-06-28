502056296cca283b894f4fdcd211b4d0b5e20af9
Jeremy Leibs <jeremy@rerun.io>
2024-06-28 12:30:35 -0400

    Determine the value of _esm during init

--- rerun_notebook/src/rerun_notebook/__init__.py
+++ rerun_notebook/src/rerun_notebook/__init__.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 import importlib.metadata
 import logging
+import os
 import pathlib
-import sys
 import time
 from typing import Any, Literal
 
@@ -20,10 +20,39 @@ except importlib.metadata.PackageNotFoundError:
 Panel = Literal["top", "blueprint", "selection", "time"]
 PanelState = Literal["expanded", "collapsed", "hidden"]
 
+WIDGET_PATH = pathlib.Path(__file__).parent / "static" / "widget.js"
+CSS_PATH = pathlib.Path(__file__).parent / "static" / "widget.css"
+
+# We need to bootstrap the value of ESM_MOD before the Viewer class is instantiated.
+# This is because AnyWidget will process the resource files during `__init_subclass__`
+
+# We allow customization through the `RERUN_NOTEBOOK_ASSET` environment variable.
+# The default value is hosted at `app.rerun.io`.
+# The value can be set to `serve` to use the hosted asset server.
+# The value can be set to `inline` to use the local widget.js file.
+# The value can be set to a URL to use a custom asset server.
+#  One way to run a custom asset server is to run `python -m rerun_notebook serve`.
+ASSET_MAGIC_SERVE = "serve"
+ASSET_MAGIC_INLINE = "inline"
+
+ASSET_ENV = os.environ.get("RERUN_NOTEBOOK_ASSET", "https://app.rerun.io/version/__version__/widget.js")
+
+if ASSET_ENV == ASSET_MAGIC_SERVE:
+    from .asset_server import serve_assets
+
+    bound_port = serve_assets(background=True)
+    ESM_MOD = f"http://localhost:{bound_port}/widget.js"
+elif ASSET_ENV == ASSET_MAGIC_INLINE:
+    ESM_MOD = WIDGET_PATH
+else:
+    ESM_MOD = ASSET_ENV
+    if not (ASSET_ENV.startswith("http://") or ASSET_ENV.startswith("https://")):
+        raise ValueError(f"RERUN_NOTEBOOK_ASSET_URL should be a URL starting with http or https. Found: {ASSET_ENV}")
+
 
 class Viewer(anywidget.AnyWidget):
-    _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
-    _css = pathlib.Path(__file__).parent / "static" / "widget.css"
+    _esm = ESM_MOD
+    _css = CSS_PATH
 
     _width = traitlets.Int(allow_none=True).tag(sync=True)
     _height = traitlets.Int(allow_none=True).tag(sync=True)
---
+++ rerun_notebook/src/rerun_notebook/__main__.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import argparse
+
+from .asset_server import serve_assets
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest="command", help="Which command to run")
+
+    serve_parser = subparsers.add_parser("serve")
+    serve_parser.add_argument("--bind-address", default="localhost")
+    serve_parser.add_argument("--port", type=int, default=8080)
+
+    args = parser.parse_args()
+
+    if args.command == "serve":
+        serve_assets(args.bind_address, args.port)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
