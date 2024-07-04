a40bba3513f691d256f6f431e51abea543bd0e9a
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-07-04 16:47:45 +0200

    Use it for VisualBounds2D

--- crates/re_edit_ui/src/visual_bounds2d.rs
+++ crates/re_edit_ui/src/visual_bounds2d.rs
@@ -2,83 +2,31 @@ use egui::NumExt as _;
 
 use re_types::{blueprint::components::VisualBounds2D, datatypes::Range2D};
 use re_ui::UiExt as _;
-use re_viewer_context::ViewerContext;
+use re_viewer_context::{MaybeMutRef, ViewerContext};
 
 pub fn multiline_edit_visual_bounds2d(
     _ctx: &ViewerContext<'_>,
     ui: &mut egui::Ui,
-    value: &mut VisualBounds2D,
+    value: &mut MaybeMutRef<'_, VisualBounds2D>,
 ) -> egui::Response {
-    let speed_func = |start: f64, end: f64| ((end - start).abs() * 0.01).at_least(0.001);
-
     let mut any_edit = false;
 
     let response_x = ui.list_item_flat_noninteractive(
         re_ui::list_item::PropertyContent::new("x").value_fn(|ui, _| {
-            let [x_range_start, x_range_end] = &mut value.x_range.0;
-            let speed = speed_func(*x_range_start, *x_range_end);
-
-            let response = ui
-                .horizontal_centered(|ui| {
-                    let response_min = ui.add(
-                        egui::DragValue::new(x_range_start)
-                            .clamp_to_range(false)
-                            .range(f64::MIN..=*x_range_end)
-                            .max_decimals(2)
-                            .speed(speed),
-                    );
-
-                    ui.label("-");
-
-                    let response_max = ui.add(
-                        egui::DragValue::new(x_range_end)
-                            .clamp_to_range(false)
-                            .range(*x_range_start..=f64::MAX)
-                            .max_decimals(2)
-                            .speed(speed),
-                    );
-
-                    response_min | response_max
-                })
-                .inner;
-
-            if response.changed() {
-                any_edit = true;
+            if let Some(value) = value.as_mut() {
+                any_edit |= range_mut_ui(ui, &mut value.x_range.0).changed();
+            } else {
+                range_ui(ui, value.x_range.0);
             }
         }),
     );
 
     let response_y = ui.list_item_flat_noninteractive(
         re_ui::list_item::PropertyContent::new("y").value_fn(|ui, _| {
-            let [y_range_start, y_range_end] = &mut value.y_range.0;
-            let speed = speed_func(*y_range_start, *y_range_end);
-
-            let response = ui
-                .horizontal_centered(|ui| {
-                    let response_min = ui.add(
-                        egui::DragValue::new(y_range_start)
-                            .clamp_to_range(false)
-                            .range(f64::MIN..=*y_range_end)
-                            .max_decimals(2)
-                            .speed(speed),
-                    );
-
-                    ui.label("-");
-
-                    let response_max = ui.add(
-                        egui::DragValue::new(y_range_end)
-                            .clamp_to_range(false)
-                            .range(*y_range_start..=f64::MAX)
-                            .max_decimals(2)
-                            .speed(speed),
-                    );
-
-                    response_min | response_max
-                })
-                .inner;
-
-            if response.changed() {
-                any_edit = true;
+            if let Some(value) = value.as_mut() {
+                any_edit |= range_mut_ui(ui, &mut value.y_range.0).changed();
+            } else {
+                range_ui(ui, value.y_range.0);
             }
         }),
     );
@@ -88,6 +36,45 @@ pub fn multiline_edit_visual_bounds2d(
         response.mark_changed();
     }
     response
+}
+
+fn range_ui(ui: &mut egui::Ui, [start, end]: [f64; 2]) {
+    ui.horizontal_centered(|ui| {
+        ui.label(format!(
+            "{} - {}",
+            re_format::format_f64(start),
+            re_format::format_f64(end)
+        ));
+    });
+}
+
+fn range_mut_ui(ui: &mut egui::Ui, [start, end]: &mut [f64; 2]) -> egui::Response {
+    let speed_func = |start: f64, end: f64| ((end - start).abs() * 0.01).at_least(0.001);
+
+    let speed = speed_func(*start, *end);
+
+    ui.horizontal_centered(|ui| {
+        let response_min = ui.add(
+            egui::DragValue::new(start)
+                .clamp_to_range(false)
+                .range(f64::MIN..=*end)
+                .max_decimals(2)
+                .speed(speed),
+        );
+
+        ui.label("-");
+
+        let response_max = ui.add(
+            egui::DragValue::new(end)
+                .clamp_to_range(false)
+                .range(*start..=f64::MAX)
+                .max_decimals(2)
+                .speed(speed),
+        );
+
+        response_min | response_max
+    })
+    .inner
 }
 
 pub fn singleline_edit_visual_bounds2d(
