29989545198b5e3314e4b8a6879d902259c7204e
Andreas Reich <andreas@rerun.io>
2024-07-09 17:12:43 +0200

    fix radiusext ui_points

--- rerun_py/rerun_sdk/rerun/components/radius_ext.py
+++ rerun_py/rerun_sdk/rerun/components/radius_ext.py
@@ -1,20 +1,16 @@
 from __future__ import annotations
 
 import numbers
-from typing import TYPE_CHECKING
 
 import numpy as np
 import numpy.typing as npt
-
-if TYPE_CHECKING:
-    from . import RadiusBatch
 
 
 class RadiusExt:
     """Extension for [Radius][rerun.components.Radius]."""
 
     @staticmethod
-    def ui_points(radii: numbers.Number | npt.ArrayLike) -> RadiusBatch:
+    def ui_points(radii: numbers.Number | npt.ArrayLike) -> npt.ArrayLike:
         """
         Create a radius or list of radii in UI points.
 
@@ -26,4 +22,4 @@ class RadiusExt:
         Internally, ui radii are stored as negative values.
         Therefore, all this method does is to ensure that all returned values are negative.
         """
-        return RadiusBatch(-np.abs(np.array(radii, dtype=np.float32)))
+        return -np.abs(np.array(radii, dtype=np.float32))
