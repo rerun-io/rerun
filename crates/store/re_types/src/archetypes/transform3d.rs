0939128b6d8b606cfe49ae759109af0c84f8868d
Andreas Reich <andreas@rerun.io>
2024-07-10 16:58:02 +0200

    add transform hierarchy snippet

--- crates/store/re_types/src/archetypes/transform3d.rs
+++ crates/store/re_types/src/archetypes/transform3d.rs
@@ -20,7 +20,7 @@ use ::re_types_core::{DeserializationError, DeserializationResult};
 
 /// **Archetype**: A 3D transform.
 ///
-/// ## Example
+/// ## Examples
 ///
 /// ### Variety of 3D transforms
 /// ```ignore
@@ -60,6 +60,99 @@ use ::re_types_core::{DeserializationError, DeserializationResult};
 ///   <source media="(max-width: 1024px)" srcset="https://static.rerun.io/transform3d_simple/141368b07360ce3fcb1553079258ae3f42bdb9ac/1024w.png">
 ///   <source media="(max-width: 1200px)" srcset="https://static.rerun.io/transform3d_simple/141368b07360ce3fcb1553079258ae3f42bdb9ac/1200w.png">
 ///   <img src="https://static.rerun.io/transform3d_simple/141368b07360ce3fcb1553079258ae3f42bdb9ac/full.png" width="640">
+/// </picture>
+/// </center>
+///
+/// ### Transform hierarchy
+/// ```ignore
+/// fn main() -> Result<(), Box<dyn std::error::Error>> {
+///     let rec = rerun::RecordingStreamBuilder::new("rerun_example_transform3d_hierarchy").spawn()?;
+///
+///     rec.set_time_seconds("sim_time", 0.0);
+///
+///     // Planetary motion is typically in the XY plane.
+///     rec.log_static("/", &rerun::ViewCoordinates::RIGHT_HAND_Z_UP)?;
+///
+///     // Setup points, all are in the center of their own space:
+///     rec.log(
+///         "sun",
+///         &rerun::Points3D::new([[0.0, 0.0, 0.0]])
+///             .with_radii([1.0])
+///             .with_colors([rerun::Color::from_rgb(255, 200, 10)]),
+///     )?;
+///     rec.log(
+///         "sun/planet",
+///         &rerun::Points3D::new([[0.0, 0.0, 0.0]])
+///             .with_radii([0.4])
+///             .with_colors([rerun::Color::from_rgb(40, 80, 200)]),
+///     )?;
+///     rec.log(
+///         "sun/planet/moon",
+///         &rerun::Points3D::new([[0.0, 0.0, 0.0]])
+///             .with_radii([0.15])
+///             .with_colors([rerun::Color::from_rgb(180, 180, 180)]),
+///     )?;
+///
+///     // Draw fixed paths where the planet & moon move.
+///     let d_planet = 6.0;
+///     let d_moon = 3.0;
+///     let angles = (0..=100).map(|i| i as f32 * 0.01 * std::f32::consts::TAU);
+///     let circle: Vec<_> = angles.map(|angle| [angle.sin(), angle.cos()]).collect();
+///     rec.log(
+///         "sun/planet_path",
+///         &rerun::LineStrips3D::new([rerun::LineStrip3D::from_iter(
+///             circle
+///                 .iter()
+///                 .map(|p| [p[0] * d_planet, p[1] * d_planet, 0.0]),
+///         )]),
+///     )?;
+///     rec.log(
+///         "sun/planet/moon_path",
+///         &rerun::LineStrips3D::new([rerun::LineStrip3D::from_iter(
+///             circle.iter().map(|p| [p[0] * d_moon, p[1] * d_moon, 0.0]),
+///         )]),
+///     )?;
+///
+///     // Movement via transforms.
+///     for i in 0..(6 * 120) {
+///         let time = i as f32 / 120.0;
+///         rec.set_time_seconds("sim_time", time);
+///         let r_moon = time * 5.0;
+///         let r_planet = time * 2.0;
+///
+///         rec.log(
+///             "sun/planet",
+///             &rerun::Transform3D::from_translation_rotation(
+///                 [r_planet.sin() * d_planet, r_planet.cos() * d_planet, 0.0],
+///                 rerun::RotationAxisAngle {
+///                     axis: [1.0, 0.0, 0.0].into(),
+///                     angle: rerun::Angle::Degrees(20.0),
+///                 },
+///             ),
+///         )?;
+///         rec.log(
+///             "sun/planet/moon",
+///             &rerun::Transform3D::from_translation([
+///                 r_moon.cos() * d_moon,
+///                 r_moon.sin() * d_moon,
+///                 0.0,
+///             ])
+///             .from_parent(),
+///         )?;
+///     }
+///
+///     // TODO(#5521): log two space views as in the python example
+///
+///     Ok(())
+/// }
+/// ```
+/// <center>
+/// <picture>
+///   <source media="(max-width: 480px)" srcset="https://static.rerun.io/transform_hierarchy/cb7be7a5a31fcb2efc02ba38e434849248f87554/480w.png">
+///   <source media="(max-width: 768px)" srcset="https://static.rerun.io/transform_hierarchy/cb7be7a5a31fcb2efc02ba38e434849248f87554/768w.png">
+///   <source media="(max-width: 1024px)" srcset="https://static.rerun.io/transform_hierarchy/cb7be7a5a31fcb2efc02ba38e434849248f87554/1024w.png">
+///   <source media="(max-width: 1200px)" srcset="https://static.rerun.io/transform_hierarchy/cb7be7a5a31fcb2efc02ba38e434849248f87554/1200w.png">
+///   <img src="https://static.rerun.io/transform_hierarchy/cb7be7a5a31fcb2efc02ba38e434849248f87554/full.png" width="640">
 /// </picture>
 /// </center>
 #[derive(Clone, Debug, PartialEq)]
