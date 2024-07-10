b27737667b4f742674629ab56b5762ae1eb444aa
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-07-10 11:50:02 +0200

    pixi: Add missing feature "cpp" to "wheel-test" environment (#6845)
    ### What
    Should fix red main
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the
    * [x] I've checked that
    https://github.com/rerun-io/rerun/actions/runs/9871678286 is green
    
    To run all checks from `main`, comment on the PR with `@rerun-bot
    full-check`.

--- tests/cpp/plot_dashboard_stress/main.cpp
+++ tests/cpp/plot_dashboard_stress/main.cpp
@@ -2,12 +2,12 @@
 //
 // Usage:
 // ```text
-// pixi run cpp-plot-dashboard --help
+// pixi run -e cpp cpp-plot-dashboard --help
 // ```
 //
 // Example:
 // ```text
-// pixi run cpp-plot-dashboard --num-plots 10 --num-series-per-plot 5 --num-points-per-series 5000 --freq 1000
+// pixi run -e cpp cpp-plot-dashboard --num-plots 10 --num-series-per-plot 5 --num-points-per-series 5000 --freq 1000
 // ```
 
 #include <algorithm>
