ba0cb75c1615b9a1863f9c199574bcb0760f4fc2
Antoine Beyeler <antoine@rerun.io>
2024-07-15 09:13:26 +0200

    Update help markdown

--- crates/viewer/re_space_view_dataframe/src/space_view_class.rs
+++ crates/viewer/re_space_view_dataframe/src/space_view_class.rs
@@ -38,7 +38,23 @@ impl SpaceViewClass for DataframeSpaceView {
     fn help_markdown(&self, _egui_ctx: &egui::Context) -> String {
         "# Dataframe view
 
-Show the data contained in entities in a table. Each entity is represented by as many rows as it has instances."
+This view displays the content of the entities it contains in tabular form. Click on the view and
+use the _Entity path filter_ to control which entities are displayed.
+
+## View types
+
+The Dataframe view operates in two modes: the _latest at_ mode and the _time range_ mode.
+
+In the _latest at_ mode, the view displays the latest data for the timeline and time set in the time
+panel. A row is shown for each entity instance.
+
+The _time range_ mode, the view displays all the data logged within the time range set for each
+view entity. In this mode, each row corresponds to an entity and time pair. Rows are further split
+if multiple `rr.log()` calls were made  for the same entity/time. Static data is also displayed.
+
+The view switches to _time range_ mode as soon as a single one of its entities has its visible time
+range set to _Override_. Each entity may have its own time range setting. (To set the same time range
+for all entities, it is preferable to override the view-level visible time range at the view.)"
             .to_owned()
     }
 
