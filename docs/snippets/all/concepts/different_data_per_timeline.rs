73cfaceec189ad401fea0f9dc1cc8e61f4c36a82
Andreas Reich <andreas@rerun.io>
2024-07-16 15:39:32 +0200

    fix rust example version

--- docs/snippets/all/concepts/different_data_per_timeline.rs
+++ docs/snippets/all/concepts/different_data_per_timeline.rs
@@ -11,12 +11,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Log a red color on one timeline.
     rec.reset_time(); // Clears all set timeline info.
     rec.set_time_seconds("red timeline", 1.0);
-    rec.log("points", &[rerun::components::Color::from_u32(0xFF0000FF)])?;
+    rec.log_component_batches(
+        "points",
+        false,
+        [&rerun::components::Color::from_u32(0xFF0000FF) as &dyn rerun::ComponentBatch],
+    )?;
 
     // And a blue color on the other.
     rec.reset_time(); // Clears all set timeline info.
     rec.set_time_sequence("blue timeline", 1);
-    rec.log("points", &[rerun::components::Color::from_u32(0x0000FFFF)])?;
+    rec.log_component_batches(
+        "points",
+        false,
+        [&rerun::components::Color::from_u32(0x0000FFFF) as &dyn rerun::ComponentBatch],
+    )?;
 
     // TODO(#5521): log VisualBounds2D
 
