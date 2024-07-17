b8b4d0e829e4ac53957d18d5f46595669be9456f
Andreas Reich <andreas@rerun.io>
2024-07-17 15:31:21 +0200

    introduce Rotation3D utility struct for Rust, both for convenience and a more stable api

--- crates/store/re_types/src/archetypes/transform3d_ext.rs
+++ crates/store/re_types/src/archetypes/transform3d_ext.rs
@@ -1,11 +1,66 @@
 use crate::{
-    components::{Scale3D, TransformMat3x3, Translation3D},
-    datatypes::{Rotation3D, TranslationRotationScale3D},
+    components::{RotationAxisAngle, RotationQuat, Scale3D, TransformMat3x3, Translation3D},
+    datatypes,
 };
 
 use super::Transform3D;
 
+/// A 3D rotation.
+///
+/// This is *not* a component, but a helper type for populating `Transform3D` with rotations.
+#[derive(Clone, Debug, Copy, PartialEq)]
+pub enum Rotation3D {
+    /// Rotation defined by a quaternion.
+    Quaternion(RotationQuat),
+
+    /// Rotation defined with an axis and an angle.
+    AxisAngle(RotationAxisAngle),
+}
+
+impl From<RotationQuat> for Rotation3D {
+    #[inline]
+    fn from(quat: RotationQuat) -> Self {
+        Self::Quaternion(quat)
+    }
+}
+
+impl From<RotationAxisAngle> for Rotation3D {
+    #[inline]
+    fn from(axis_angle: RotationAxisAngle) -> Self {
+        Self::AxisAngle(axis_angle)
+    }
+}
+
+impl From<datatypes::Quaternion> for Rotation3D {
+    #[inline]
+    fn from(quat: datatypes::Quaternion) -> Self {
+        Self::Quaternion(quat.into())
+    }
+}
+
+impl From<datatypes::RotationAxisAngle> for Rotation3D {
+    #[inline]
+    fn from(axis_angle: datatypes::RotationAxisAngle) -> Self {
+        Self::AxisAngle(axis_angle.into())
+    }
+}
+
 impl Transform3D {
+    /// Convenience method that takes any kind of (single) rotation representation and sets it on this transform.
+    #[inline]
+    pub fn with_rotation(self, rotation: impl Into<Rotation3D>) -> Self {
+        match rotation.into() {
+            Rotation3D::Quaternion(quaternion) => Self {
+                quaternion: Some(vec![quaternion]),
+                ..self
+            },
+            Rotation3D::AxisAngle(rotation_axis_angle) => Self {
+                rotation_axis_angle: Some(vec![rotation_axis_angle]),
+                ..self
+            },
+        }
+    }
+
     /// From a translation.
     #[inline]
     pub fn from_translation(translation: impl Into<Translation3D>) -> Self {
@@ -27,10 +82,7 @@ impl Transform3D {
     /// From a rotation
     #[inline]
     pub fn from_rotation(rotation: impl Into<Rotation3D>) -> Self {
-        Self {
-            transform: TranslationRotationScale3D::from_rotation(rotation).into(),
-            ..Self::default()
-        }
+        Self::default().with_rotation(rotation)
     }
 
     /// From a scale
@@ -49,10 +101,10 @@ impl Transform3D {
         rotation: impl Into<Rotation3D>,
     ) -> Self {
         Self {
-            transform: TranslationRotationScale3D::from_rotation(rotation).into(),
             translation: Some(vec![translation.into()]),
             ..Self::default()
         }
+        .with_rotation(rotation)
     }
 
     /// From a translation applied after a 3x3 matrix.
@@ -89,21 +141,21 @@ impl Transform3D {
         scale: impl Into<Scale3D>,
     ) -> Self {
         Self {
-            transform: TranslationRotationScale3D::from_rotation(rotation).into(),
             scale: Some(vec![scale.into()]),
             translation: Some(vec![translation.into()]),
             ..Self::default()
         }
+        .with_rotation(rotation)
     }
 
     /// From a rotation & scale
     #[inline]
     pub fn from_rotation_scale(rotation: impl Into<Rotation3D>, scale: impl Into<Scale3D>) -> Self {
         Self {
-            transform: TranslationRotationScale3D::from_rotation(rotation).into(),
             scale: Some(vec![scale.into()]),
             ..Self::default()
         }
+        .with_rotation(rotation)
     }
 
     /// Indicate that this transform is from parent to child.
