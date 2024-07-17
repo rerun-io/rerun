0accdb1778cd2ea4059c18047ed64ec014c2f9c3
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-07-17 10:25:49 +0200

    Add alias for components as "channels"

--- crates/store/re_types/src/components/color_model_ext.rs
+++ crates/store/re_types/src/components/color_model_ext.rs
@@ -2,6 +2,7 @@ use super::ColorModel;
 
 impl ColorModel {
     /// 1 for grayscale, 3 for RGB, etc.
+    #[doc(alias = "components")]
     #[doc(alias = "depth")]
     #[inline]
     pub fn num_channels(self) -> usize {
--- crates/viewer/re_viewer_context/src/image_info.rs
+++ crates/viewer/re_viewer_context/src/image_info.rs
@@ -47,7 +47,7 @@ impl ImageInfo {
     }
 
     /// 1 for grayscale and depth images, 3 for RGB, etc.
-    ///
+    #[doc(alias = "components")]
     #[doc(alias = "depth")]
     #[inline]
     pub fn num_channels(&self) -> usize {
