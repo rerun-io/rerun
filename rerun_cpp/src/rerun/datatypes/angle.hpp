5ceabbf39fc8b39e3b1c6a167c4f71635e6e6bb3
Andreas Reich <andreas@rerun.io>
2024-07-17 10:54:35 +0200

    more codegen!

--- rerun_cpp/src/rerun/datatypes/angle.hpp
+++ rerun_cpp/src/rerun/datatypes/angle.hpp
@@ -30,15 +30,19 @@ namespace rerun::datatypes {
 
         /// New angle in radians.
         static Angle radians(float radians_) {
-            return Angle{radians_};
+            Angle angle;
+            angle.angle_radians = radians_;
+            return angle;
         }
 
         /// New angle in degrees.
         ///
         /// Converts to radians to store the angle.
         static Angle degrees(float degrees_) {
+            Angle angle;
             // Can't use math constants here: `M_PI` doesn't work on all platforms out of the box and std::numbers::pi is C++20.
-            return Angle{degrees_ * (3.14159265358979323846264338327950288f / 180.f)};
+            angle.angle_radians = degrees_ * (3.14159265358979323846264338327950288f / 180.f);
+            return angle;
         }
 
       public:
