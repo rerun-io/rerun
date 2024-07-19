50e1f20969f742444bff8bdd6276a4032f744ef7
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-07-19 09:30:36 +0200

    Fix docstrings for PixelFormat variants

--- crates/store/re_types/src/components/pixel_format.rs
+++ crates/store/re_types/src/components/pixel_format.rs
@@ -31,8 +31,7 @@ use ::re_types_core::{DeserializationError, DeserializationResult};
 /// For more compressed image formats, see [`archetypes::ImageEncoded`][crate::archetypes::ImageEncoded].
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, Default)]
 pub enum PixelFormat {
-    /// NV12 (aka Y_UV12) is a YUV 4:2:0 chroma downsampled format with
-    /// 12 bits per pixel and 8 bits per channel.
+    /// NV12 (aka Y_UV12) is a YUV 4:2:0 chroma downsampled format with 12 bits per pixel and 8 bits per channel.
     ///
     /// First comes entire image in Y in one plane,
     /// followed by a plane with interleaved lines ordered as U0, V0, U1, V1, etc.
@@ -40,8 +39,7 @@ pub enum PixelFormat {
     #[allow(clippy::upper_case_acronyms)]
     NV12 = 1,
 
-    /// YUY2 (aka YUYV or YUYV16), is a YUV 4:2:2 chroma downsampled format with
-    /// 16 bits per pixel and 8 bits per channel.
+    /// YUY2 (aka YUYV or YUYV16), is a YUV 4:2:2 chroma downsampled format with 16 bits per pixel and 8 bits per channel.
     ///
     /// The order of the channels is Y0, U0, Y1, V0, all in the same plane.
     #[allow(clippy::upper_case_acronyms)]
@@ -58,10 +56,10 @@ impl ::re_types_core::reflection::Enum for PixelFormat {
     fn docstring_md(self) -> &'static str {
         match self {
             Self::NV12 => {
-                "NV12 (aka Y_UV12) is a YUV 4:2:0 chroma downsampled format with\n12 bits per pixel and 8 bits per channel.\n\nFirst comes entire image in Y in one plane,\nfollowed by a plane with interleaved lines ordered as U0, V0, U1, V1, etc."
+                "NV12 (aka Y_UV12) is a YUV 4:2:0 chroma downsampled format with 12 bits per pixel and 8 bits per channel.\n\nFirst comes entire image in Y in one plane,\nfollowed by a plane with interleaved lines ordered as U0, V0, U1, V1, etc."
             }
             Self::YUY2 => {
-                "YUY2 (aka YUYV or YUYV16), is a YUV 4:2:2 chroma downsampled format with\n16 bits per pixel and 8 bits per channel.\n\nThe order of the channels is Y0, U0, Y1, V0, all in the same plane."
+                "YUY2 (aka YUYV or YUYV16), is a YUV 4:2:2 chroma downsampled format with 16 bits per pixel and 8 bits per channel.\n\nThe order of the channels is Y0, U0, Y1, V0, all in the same plane."
             }
         }
     }
