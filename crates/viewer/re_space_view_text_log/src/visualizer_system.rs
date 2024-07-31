3137c4a2ec466f75a7b5532f04f005ff58d39f23
Clement Rey <cr.rey.clement@gmail.com>
2024-07-31 16:30:41 +0200

    chunkified text-log view with multi-timeline display

--- crates/viewer/re_space_view_text_log/src/visualizer_system.rs
+++ crates/viewer/re_space_view_text_log/src/visualizer_system.rs
@@ -1,9 +1,11 @@
+use itertools::izip;
 use re_chunk_store::ResolvedTimeRange;
 use re_chunk_store::RowId;
 use re_entity_db::EntityPath;
 use re_log_types::TimeInt;
-use re_query::{clamped_zip_1x2, range_zip_1x2};
-use re_space_view::{range_with_blueprint_resolved_data, RangeResultsExt};
+use re_log_types::TimePoint;
+use re_query2::{clamped_zip_1x2, range_zip_1x2};
+use re_space_view::{range_with_blueprint_resolved_data2, RangeResultsExt2};
 use re_types::{
     archetypes::TextLog,
     components::{Color, Text, TextLogLevel},
@@ -19,6 +21,7 @@ pub struct Entry {
     pub row_id: RowId,
     pub entity_path: EntityPath,
     pub time: TimeInt,
+    pub timepoint: TimePoint,
     pub color: Option<Color>,
     pub body: Text,
     pub level: Option<TextLogLevel>,
@@ -53,13 +56,7 @@ impl VisualizerSystem for TextLogSystem {
             re_chunk_store::RangeQuery::new(view_query.timeline, ResolvedTimeRange::EVERYTHING);
 
         for data_result in view_query.iter_visible_data_results(ctx, Self::identifier()) {
-            if let Err(err) = self.process_entity(ctx, &query, data_result) {
-                re_log::error_once!(
-                    "Error visualizing text logs for {:?}: {:?}",
-                    data_result.entity_path,
-                    err
-                );
-            }
+            self.process_entity(ctx, &query, data_result);
         }
 
         {
@@ -86,11 +83,10 @@ impl TextLogSystem {
         ctx: &ViewContext<'_>,
         query: &re_chunk_store::RangeQuery,
         data_result: &re_viewer_context::DataResult,
-    ) -> Result<(), SpaceViewSystemExecutionError> {
+    ) {
         re_tracing::profile_function!();
-        let resolver = ctx.recording().resolver();
 
-        let results = range_with_blueprint_resolved_data(
+        let results = range_with_blueprint_resolved_data2(
             ctx,
             None,
             query,
@@ -98,24 +94,38 @@ impl TextLogSystem {
             [Text::name(), TextLogLevel::name(), Color::name()],
         );
 
-        let Some(all_texts) = results
-            .get_required_component_dense::<Text>(resolver)
-            .transpose()?
-        else {
-            return Ok(());
+        let Some(all_text_chunks) = results.get_required_chunks(&Text::name()) else {
+            return;
         };
 
-        let all_levels = results.get_or_empty_dense::<TextLogLevel>(resolver)?;
-        let all_colors = results.get_or_empty_dense::<Color>(resolver)?;
+        // TODO(cmc): It would be more efficient (both space and compute) to do this lazily as
+        // we're rendering the table by indexing back into the original chunk etc.
+        // Let's keep it simple for now, until we have data suggested we need the extra perf.
+        let all_timepoints = all_text_chunks
+            .iter()
+            .flat_map(|chunk| chunk.iter_component_timepoints(&Text::name()));
+
+        let timeline = query.timeline();
+        let all_texts = results.iter_as(timeline, Text::name());
+        let all_levels = results.iter_as(timeline, TextLogLevel::name());
+        let all_colors = results.iter_as(timeline, Color::name());
+
         let all_frames = range_zip_1x2(
-            all_texts.range_indexed(),
-            all_levels.range_indexed(),
-            all_colors.range_indexed(),
+            all_texts.string(),
+            all_levels.component(),
+            all_colors.primitive::<u32>(),
         );
 
-        for (&(data_time, row_id), bodies, levels, colors) in all_frames {
-            let levels = levels.unwrap_or(&[]).iter().cloned().map(Some);
-            let colors = colors.unwrap_or(&[]).iter().copied().map(Some);
+        let all_frames = izip!(all_timepoints, all_frames);
+
+        for (timepoint, ((data_time, row_id), bodies, levels, colors)) in all_frames {
+            let levels = levels.as_deref().unwrap_or(&[]).iter().cloned().map(Some);
+            let colors = colors
+                .unwrap_or(&[])
+                .iter()
+                .copied()
+                .map(Into::into)
+                .map(Some);
 
             let level_default_fn = || None;
             let color_default_fn = || None;
@@ -128,14 +138,13 @@ impl TextLogSystem {
                     row_id,
                     entity_path: data_result.entity_path.clone(),
                     time: data_time,
+                    timepoint: timepoint.clone(),
                     color,
-                    body: text.clone(),
+                    body: text.clone().into(),
                     level,
                 });
             }
         }
-
-        Ok(())
     }
 }
 
