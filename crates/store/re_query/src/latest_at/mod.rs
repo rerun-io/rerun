c1989a823883ce240456afbb09846627b2334e85
Clement Rey <cr.rey.clement@gmail.com>
2024-08-01 10:53:47 +0200

    remove re_query

--- crates/store/re_query/src/latest_at/mod.rs
+++
@@ -1,7 +0,0 @@
-mod helpers;
-mod query;
-mod results;
-
-pub use self::helpers::LatestAtMonoResult;
-pub use self::query::{latest_at, LatestAtCache};
-pub use self::results::{LatestAtComponentResults, LatestAtResults};
