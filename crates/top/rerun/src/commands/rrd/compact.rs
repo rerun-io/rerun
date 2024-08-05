1f32f49e7e4d99ebcbeb9895fed2260630941ab3
Clement Rey <cr.rey.clement@gmail.com>
2024-08-05 17:53:06 +0200

    rrd compact: enable wildcard input

--- crates/top/rerun/src/commands/rrd/compact.rs
+++ crates/top/rerun/src/commands/rrd/compact.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use anyhow::Context as _;
+use itertools::Itertools as _;
 
 use re_entity_db::EntityDb;
 use re_log_types::{LogMsg, StoreId};
@@ -10,8 +11,7 @@ use re_sdk::StoreKind;
 
 #[derive(Debug, Clone, clap::Parser)]
 pub struct CompactCommand {
-    #[arg(short = 'i', long = "input", value_name = "src.(rrd|rbl)")]
-    path_to_input_rrd: String,
+    path_to_input_rrds: Vec<String>,
 
     #[arg(short = 'o', long = "output", value_name = "dst.(rrd|rbl)")]
     path_to_output_rrd: String,
@@ -20,16 +20,25 @@ pub struct CompactCommand {
 impl CompactCommand {
     pub fn run(&self) -> anyhow::Result<()> {
         let Self {
-            path_to_input_rrd,
+            path_to_input_rrds,
             path_to_output_rrd,
         } = self;
 
-        let path_to_input_rrd = PathBuf::from(path_to_input_rrd);
+        let path_to_input_rrds = path_to_input_rrds.iter().map(PathBuf::from).collect_vec();
         let path_to_output_rrd = PathBuf::from(path_to_output_rrd);
 
-        let rrd_in = std::fs::File::open(&path_to_input_rrd)
-            .with_context(|| format!("{path_to_input_rrd:?}"))?;
-        let rrd_in_size = rrd_in.metadata().ok().map(|md| md.len());
+        let rrds_in: Result<Vec<_>, _> = path_to_input_rrds
+            .iter()
+            .map(|path_to_input_rrd| {
+                std::fs::File::open(path_to_input_rrd)
+                    .with_context(|| format!("{path_to_input_rrd:?}"))
+            })
+            .collect();
+        let rrds_in = rrds_in?;
+        let rrds_in_size = rrds_in
+            .iter()
+            .map(|rrd_in| rrd_in.metadata().ok().map(|md| md.len()))
+            .sum::<Option<u64>>();
 
         let file_size_to_string = |size: Option<u64>| {
             size.map_or_else(
@@ -45,32 +54,35 @@ impl CompactCommand {
         store_config.enable_changelog = false;
 
         re_log::info!(
-            src = ?path_to_input_rrd,
-            src_size_bytes = %file_size_to_string(rrd_in_size),
+            srcs = ?path_to_input_rrds,
+            src_size_bytes = %file_size_to_string(rrds_in_size),
             dst = ?path_to_output_rrd,
             max_num_rows = %re_format::format_uint(store_config.chunk_max_rows),
             max_num_bytes = %re_format::format_bytes(store_config.chunk_max_bytes as _),
-            "compaction started"
+            "merge started"
         );
 
         let now = std::time::Instant::now();
 
         let mut entity_dbs: std::collections::HashMap<StoreId, EntityDb> = Default::default();
-        let version_policy = re_log_encoding::decoder::VersionPolicy::Warn;
-        let decoder = re_log_encoding::decoder::Decoder::new(version_policy, rrd_in)?;
-        let version = decoder.version();
-        for msg in decoder {
-            let msg = msg.context("decode rrd message")?;
-            entity_dbs
-                .entry(msg.store_id().clone())
-                .or_insert_with(|| {
-                    re_entity_db::EntityDb::with_store_config(
-                        msg.store_id().clone(),
-                        store_config.clone(),
-                    )
-                })
-                .add(&msg)
-                .context("decode rrd file contents")?;
+        let mut version = None;
+        for rrd_in in rrds_in {
+            let version_policy = re_log_encoding::decoder::VersionPolicy::Warn;
+            let decoder = re_log_encoding::decoder::Decoder::new(version_policy, rrd_in)?;
+            version = version.max(Some(decoder.version()));
+            for msg in decoder {
+                let msg = msg.context("decode rrd message")?;
+                entity_dbs
+                    .entry(msg.store_id().clone())
+                    .or_insert_with(|| {
+                        re_entity_db::EntityDb::with_store_config(
+                            msg.store_id().clone(),
+                            store_config.clone(),
+                        )
+                    })
+                    .add(&msg)
+                    .context("decode rrd file contents")?;
+            }
         }
 
         anyhow::ensure!(
@@ -98,6 +110,7 @@ impl CompactCommand {
         let messages_rrd = messages_rrd.iter().flatten();
 
         let encoding_options = re_log_encoding::EncodingOptions::COMPRESSED;
+        let version = version.unwrap_or(re_build_info::CrateVersion::LOCAL);
         re_log_encoding::encoder::encode(
             version,
             encoding_options,
@@ -111,18 +124,18 @@ impl CompactCommand {
         let rrd_out_size = rrd_out.metadata().ok().map(|md| md.len());
 
         let compaction_ratio =
-            if let (Some(rrd_in_size), Some(rrd_out_size)) = (rrd_in_size, rrd_out_size) {
+            if let (Some(rrds_in_size), Some(rrd_out_size)) = (rrds_in_size, rrd_out_size) {
                 format!(
                     "{:3.3}%",
-                    100.0 - rrd_out_size as f64 / (rrd_in_size as f64 + f64::EPSILON) * 100.0
+                    100.0 - rrd_out_size as f64 / (rrds_in_size as f64 + f64::EPSILON) * 100.0
                 )
             } else {
                 "N/A".to_owned()
             };
 
         re_log::info!(
-            src = ?path_to_input_rrd,
-            src_size_bytes = %file_size_to_string(rrd_in_size),
+            srcs = ?path_to_input_rrds,
+            srcs_size_bytes = %file_size_to_string(rrds_in_size),
             dst = ?path_to_output_rrd,
             dst_size_bytes = %file_size_to_string(rrd_out_size),
             time = ?now.elapsed(),
