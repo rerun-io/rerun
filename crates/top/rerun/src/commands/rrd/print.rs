8ea5710cda526115d6328ba1d9391d071600cc1d
Clement Rey <cr.rey.clement@gmail.com>
2024-08-06 15:50:29 +0200

    rrd print: impl support for stdin

--- crates/top/rerun/src/commands/rrd/print.rs
+++ crates/top/rerun/src/commands/rrd/print.rs
@@ -1,85 +1,106 @@
-use std::path::{Path, PathBuf};
-
-use anyhow::Context as _;
+use anyhow::Context;
 use itertools::Itertools as _;
 
 use re_log_types::{LogMsg, SetStoreInfo};
 use re_sdk::log::Chunk;
 use re_types::SizeBytes as _;
 
+use crate::commands::read_rrd_streams_from_file_or_stdin;
+
 // ---
 
 #[derive(Debug, Clone, clap::Parser)]
 pub struct PrintCommand {
+    /// Paths to read from. Reads from standard input if none are specified.
     path_to_input_rrds: Vec<String>,
 
-    /// If specified, print out table contents.
+    /// If set, print out table contents.
     #[clap(long, short, default_value_t = false)]
     verbose: bool,
+
+    /// If set, will try to continue in the face of IO and decoding errors.
+    #[clap(long, default_value_t = true)]
+    best_effort: bool,
 }
 
 impl PrintCommand {
     pub fn run(&self) -> anyhow::Result<()> {
-        let path_to_input_rrds = self.path_to_input_rrds.iter().map(PathBuf::from);
+        let Self {
+            path_to_input_rrds,
+            verbose,
+            best_effort,
+        } = self;
 
-        for rrd_path in path_to_input_rrds {
-            self.print_rrd(&rrd_path)
-                .with_context(|| format!("path: {rrd_path:?}"))?;
+        let version_policy = re_log_encoding::decoder::VersionPolicy::Warn;
+        let rx = read_rrd_streams_from_file_or_stdin(version_policy, path_to_input_rrds);
+
+        let mut is_success = true;
+
+        for res in rx {
+            match res {
+                Ok(msg) => {
+                    if let Err(err) = print_msg(*verbose, msg) {
+                        re_log::error!(err = re_error::format(err));
+                        is_success = false;
+                    }
+                }
+                Err(err) => {
+                    re_log::error!(err = re_error::format(err));
+                    is_success = false;
+                }
+            }
+
+            if !*best_effort && !is_success {
+                break;
+            }
         }
 
-        Ok(())
+        if is_success {
+            Ok(())
+        } else {
+            Err(anyhow::anyhow!(
+                "one or more IO and/or decoding failures (check logs)"
+            ))
+        }
     }
 }
 
-impl PrintCommand {
-    fn print_rrd(&self, rrd_path: &Path) -> anyhow::Result<()> {
-        let rrd_file = std::fs::File::open(rrd_path)?;
-        let version_policy = re_log_encoding::decoder::VersionPolicy::Warn;
-        let decoder = re_log_encoding::decoder::Decoder::new(version_policy, rrd_file)?;
-        println!("Decoded RRD stream v{}\n---", decoder.version());
-        for msg in decoder {
-            let msg = msg.context("decode rrd message")?;
-            match msg {
-                LogMsg::SetStoreInfo(msg) => {
-                    let SetStoreInfo { row_id: _, info } = msg;
-                    println!("{info:#?}");
-                }
+fn print_msg(verbose: bool, msg: LogMsg) -> anyhow::Result<()> {
+    match msg {
+        LogMsg::SetStoreInfo(msg) => {
+            let SetStoreInfo { row_id: _, info } = msg;
+            println!("{info:#?}");
+        }
 
-                LogMsg::ArrowMsg(_row_id, arrow_msg) => {
-                    let chunk = match Chunk::from_arrow_msg(&arrow_msg) {
-                        Ok(chunk) => chunk,
-                        Err(err) => {
-                            eprintln!("discarding broken chunk: {err}");
-                            continue;
-                        }
-                    };
+        LogMsg::ArrowMsg(_row_id, arrow_msg) => {
+            let chunk = Chunk::from_arrow_msg(&arrow_msg).context("skipped corrupt chunk")?;
 
-                    if self.verbose {
-                        println!("{chunk}");
-                    } else {
-                        let column_names = chunk
-                            .component_names()
-                            .map(|name| name.short_name())
-                            .join(" ");
+            if verbose {
+                println!("{chunk}");
+            } else {
+                let column_names = chunk
+                    .component_names()
+                    .map(|name| name.short_name())
+                    .join(" ");
 
-                        println!(
-                            "Chunk with {} rows ({}) - {:?} - columns: [{column_names}]",
-                            chunk.num_rows(),
-                            re_format::format_bytes(chunk.total_size_bytes() as _),
-                            chunk.entity_path(),
-                        );
-                    }
-                }
-
-                LogMsg::BlueprintActivationCommand(re_log_types::BlueprintActivationCommand {
-                    blueprint_id,
-                    make_active,
-                    make_default,
-                }) => {
-                    println!("BlueprintActivationCommand({blueprint_id}, make_active: {make_active}, make_default: {make_default})");
-                }
+                println!(
+                    "Chunk({}) with {} rows ({}) - {:?} - columns: [{column_names}]",
+                    chunk.id(),
+                    chunk.num_rows(),
+                    re_format::format_bytes(chunk.total_size_bytes() as _),
+                    chunk.entity_path(),
+                );
             }
         }
-        Ok(())
+
+        LogMsg::BlueprintActivationCommand(re_log_types::BlueprintActivationCommand {
+            blueprint_id,
+            make_active,
+            make_default,
+        }) => {
+            println!("BlueprintActivationCommand({blueprint_id}, make_active: {make_active}, make_default: {make_default})");
+        }
     }
+
+    Ok(())
 }
