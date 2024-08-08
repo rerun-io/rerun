8eabd1b416a97beb0b72d8247c915765ad38ceec
Andreas Reich <andreas@rerun.io>
2024-08-08 11:23:47 +0200

    try without disabling asan but better speced types - saw suspicious msvc warning indicating that it returned address to local

--- rerun_cpp/src/rerun/indicator_component.cpp
+++ rerun_cpp/src/rerun/indicator_component.cpp
@@ -5,17 +5,14 @@
 
 namespace rerun::components {
     const std::shared_ptr<arrow::DataType>& indicator_arrow_datatype() {
-        static const auto datatype = arrow::null();
+        static const std::shared_ptr<arrow::DataType> datatype = arrow::null();
         return datatype;
     }
 
-    // ASAN indicates that we're leaking memory here.
-    // It's a bit surprising because we use this pattern of single global allocation
-    // for almost all arrow data types to avoid repeated shared_ptr allocations.
-    RR_DISABLE_ADDRESS_SANITIZER
     const std::shared_ptr<arrow::Array>& indicator_arrow_array() {
         // Lazily create an array for the indicator (only once)
-        static const auto single_indicator_array = std::make_shared<arrow::NullArray>(1);
+        static const std::shared_ptr<arrow::Array> single_indicator_array =
+            std::make_shared<arrow::NullArray>(1);
         return single_indicator_array;
     }
 } // namespace rerun::components
