5e2d33977ee8304798737b4ee2bc56028998e701
Clement Rey <cr.rey.clement@gmail.com>
2024-08-08 13:08:40 +0200

    Query: configurable timeline|component eager slicing (#7112)
    This adds range query options that make it possible to discard unwanted
    timeline and component columns as early as possible in the query
    pipeline.
    
    Because the compute cost of slicing chunks grows linearly with the
    number of columns, this has a non-negligible impact for large range
    queries that slice many many chunks.
    
    All queries use these new options by default, except for the text-log
    view that needs all the timeline columns it can get.
    
    This results in 10-15% single-threaded perf improvement for timeseries
    workloads.
    
    ```
    taskset -c 7 target/release/rerun ~/Downloads/vrs.rrd
    ```
    Before:
    ![image
    (6)](https://github.com/user-attachments/assets/1fafadae-3e26-4445-b8be-1e049cdc3c01)
    After:
    ![image
    (7)](https://github.com/user-attachments/assets/1b1558a5-1245-4eab-89be-c14710b16933)

--- crates/viewer/re_space_view_time_series/src/line_visualizer_system.rs
+++ crates/viewer/re_space_view_time_series/src/line_visualizer_system.rs
@@ -1,4 +1,4 @@
-use itertools::Itertools as _;
+use itertools::Itertools;
 
 use re_space_view::range_with_blueprint_resolved_data;
 use re_types::archetypes;
@@ -235,11 +235,11 @@ impl SeriesLineSystem {
                 re_tracing::profile_scope!("fill values");
 
                 debug_assert_eq!(Scalar::arrow_datatype(), ArrowDatatype::Float64);
-                let mut i = 0;
                 all_scalar_chunks
                     .iter()
                     .flat_map(|chunk| chunk.iter_primitive::<f64>(&Scalar::name()))
-                    .for_each(|values| {
+                    .enumerate()
+                    .for_each(|(i, values)| {
                         if !values.is_empty() {
                             if values.len() > 1 {
                                 re_log::warn_once!(
@@ -251,8 +251,6 @@ impl SeriesLineSystem {
                         } else {
                             points[i].attrs.kind = PlotSeriesKind::Clear;
                         }
-
-                        i += 1;
                     });
             }
 
