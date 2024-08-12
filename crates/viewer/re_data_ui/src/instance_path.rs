b1ce9b5fdc29a29c52e4d99a515eff583749f789
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-08-12 16:27:20 +0200

    Show preview of `Image/DepthImage/SegmentationImage` when selected (#7147)
    ### What
    * Part of https://github.com/rerun-io/rerun/issues/6891
    * Fixes https://github.com/rerun-io/rerun/issues/5127
    
    When you select an entity that contains an `Image`, `DepthImage` or
    `SegmentationImage`, a preview of it will be show.
    
    Coming later: button to copy and save/download the image.
    
    #### Toolip
    
    ![image](https://github.com/user-attachments/assets/e3b5bb67-3e66-462b-b908-54e707197983)
    
    
    #### Selection panel
    
    ![image](https://github.com/user-attachments/assets/fc01796a-7c9d-428a-ba71-33f575fe444d)
    
    
    
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested the web demo (if applicable):
    * Using examples from latest `main` build:
    [rerun.io/viewer](https://rerun.io/viewer/pr/7147?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
    * Using full set of examples from `nightly` build:
    [rerun.io/viewer](https://rerun.io/viewer/pr/7147?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
    * [x] The PR title and labels are set such as to maximize their
    usefulness for the next release's CHANGELOG
    * [x] If applicable, add a new check to the [release
    checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
    * [x] If have noted any breaking changes to the log API in
    `CHANGELOG.md` and the migration guide
    
    - [PR Build Summary](https://build.rerun.io/pr/7147)
    - [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
    - [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)
    
    To run all checks from `main`, comment on the PR with `@rerun-bot
    full-check`.

--- crates/viewer/re_data_ui/src/instance_path.rs
+++ crates/viewer/re_data_ui/src/instance_path.rs
@@ -1,7 +1,16 @@
+use nohash_hasher::IntMap;
+
+use re_chunk_store::UnitChunkShared;
 use re_entity_db::InstancePath;
 use re_log_types::ComponentPath;
+use re_types::{archetypes, components, image::ImageKind, Archetype, ComponentName, Loggable};
 use re_ui::{ContextExt as _, UiExt as _};
-use re_viewer_context::{HoverHighlight, Item, UiLayout, ViewerContext};
+use re_viewer_context::{
+    gpu_bridge::image_to_gpu, HoverHighlight, ImageInfo, ImageStatsCache, Item, UiLayout,
+    ViewerContext,
+};
+
+use crate::image::texture_preview_ui;
 
 use super::DataUi;
 
@@ -48,6 +57,8 @@ impl DataUi for InstancePath {
             .filter(|c| c.is_indicator_component())
             .count();
 
+        let components = latest_at(db, query, entity_path, &components);
+
         if ui_layout.is_single_line() {
             ui_layout.label(
                 ui,
@@ -59,106 +70,222 @@ impl DataUi for InstancePath {
                     if indicator_count > 1 { "s" } else { "" }
                 ),
             );
-            return;
+        } else {
+            component_list_ui(
+                ctx,
+                ui,
+                ui_layout,
+                query,
+                db,
+                entity_path,
+                instance,
+                &components,
+            );
         }
 
-        let show_indicator_comps = match ui_layout {
-            UiLayout::Tooltip => {
-                // Skip indicator components in hover ui (unless there are no other
-                // types of components).
-                indicator_count == components.len()
-            }
-            UiLayout::SelectionPanelLimitHeight | UiLayout::SelectionPanelFull => true,
-            UiLayout::List => false, // unreachable
-        };
-
-        let interactive = ui_layout != UiLayout::Tooltip;
-
-        re_ui::list_item::list_item_scope(ui, "component list", |ui| {
-            for component_name in components {
-                if !show_indicator_comps && component_name.is_indicator_component() {
-                    continue;
-                }
-
-                let component_path = ComponentPath::new(entity_path.clone(), component_name);
-                let is_static = db
-                    .store()
-                    .entity_has_static_component(entity_path, &component_name);
-                let icon = if is_static {
-                    &re_ui::icons::COMPONENT_STATIC
-                } else {
-                    &re_ui::icons::COMPONENT_TEMPORAL
-                };
-                let item = Item::ComponentPath(component_path);
-
-                let mut list_item = ui.list_item().interactive(interactive);
-
-                if interactive {
-                    let is_hovered = ctx.selection_state().highlight_for_ui_element(&item)
-                        == HoverHighlight::Hovered;
-                    list_item = list_item.force_hovered(is_hovered);
-                }
-
-                let response = if component_name.is_indicator_component() {
-                    list_item.show_flat(
-                        ui,
-                        re_ui::list_item::LabelContent::new(component_name.short_name())
-                            .with_icon(icon),
-                    )
-                } else {
-                    let results = db.query_caches().latest_at(
-                        db.store(),
-                        query,
-                        entity_path,
-                        [component_name],
-                    );
-                    let Some(unit) = results.components.get(&component_name) else {
-                        continue; // no need to show components that are unset at this point in time
-                    };
-
-                    let content =
-                        re_ui::list_item::PropertyContent::new(component_name.short_name())
-                            .with_icon(icon)
-                            .value_fn(|ui, _| {
-                                if instance.is_all() {
-                                    crate::EntityLatestAtResults {
-                                        entity_path: entity_path.clone(),
-                                        component_name,
-                                        unit,
-                                    }
-                                    .data_ui(
-                                        ctx,
-                                        ui,
-                                        UiLayout::List,
-                                        query,
-                                        db,
-                                    );
-                                } else {
-                                    ctx.component_ui_registry.ui(
-                                        ctx,
-                                        ui,
-                                        UiLayout::List,
-                                        query,
-                                        db,
-                                        entity_path,
-                                        component_name,
-                                        unit,
-                                        instance,
-                                    );
-                                }
-                            });
-
-                    list_item.show_flat(ui, content)
-                };
-
-                let response = response.on_hover_ui(|ui| {
-                    component_name.data_ui_recording(ctx, ui, UiLayout::Tooltip);
-                });
-
-                if interactive {
-                    ctx.select_hovered_on_click(&response, item);
-                }
-            }
-        });
+        if instance.is_all() {
+            let component_map = components.into_iter().collect();
+            preview_if_image_ui(ctx, ui, ui_layout, query, entity_path, &component_map);
+        }
     }
+}
+
+fn latest_at(
+    db: &re_entity_db::EntityDb,
+    query: &re_chunk_store::LatestAtQuery,
+    entity_path: &re_log_types::EntityPath,
+    components: &[ComponentName],
+) -> Vec<(ComponentName, UnitChunkShared)> {
+    let components: Vec<(ComponentName, UnitChunkShared)> = components
+        .iter()
+        .filter_map(|&component_name| {
+            let mut results =
+                db.query_caches()
+                    .latest_at(db.store(), query, entity_path, [component_name]);
+
+            // We ignore components that are unset at this point in time
+            results
+                .components
+                .remove(&component_name)
+                .map(|unit| (component_name, unit))
+        })
+        .collect();
+    components
+}
+
+#[allow(clippy::too_many_arguments)]
+fn component_list_ui(
+    ctx: &ViewerContext<'_>,
+    ui: &mut egui::Ui,
+    ui_layout: UiLayout,
+    query: &re_chunk_store::LatestAtQuery,
+    db: &re_entity_db::EntityDb,
+    entity_path: &re_log_types::EntityPath,
+    instance: &re_log_types::Instance,
+    components: &[(ComponentName, UnitChunkShared)],
+) {
+    let indicator_count = components
+        .iter()
+        .filter(|(c, _)| c.is_indicator_component())
+        .count();
+
+    let show_indicator_comps = match ui_layout {
+        UiLayout::Tooltip => {
+            // Skip indicator components in hover ui (unless there are no other
+            // types of components).
+            indicator_count == components.len()
+        }
+        UiLayout::SelectionPanelLimitHeight | UiLayout::SelectionPanelFull => true,
+        UiLayout::List => false, // unreachable
+    };
+
+    let interactive = ui_layout != UiLayout::Tooltip;
+
+    re_ui::list_item::list_item_scope(ui, "component list", |ui| {
+        for (component_name, unit) in components {
+            let component_name = *component_name;
+            if !show_indicator_comps && component_name.is_indicator_component() {
+                continue;
+            }
+
+            let component_path = ComponentPath::new(entity_path.clone(), component_name);
+            let is_static = db
+                .store()
+                .entity_has_static_component(entity_path, &component_name);
+            let icon = if is_static {
+                &re_ui::icons::COMPONENT_STATIC
+            } else {
+                &re_ui::icons::COMPONENT_TEMPORAL
+            };
+            let item = Item::ComponentPath(component_path);
+
+            let mut list_item = ui.list_item().interactive(interactive);
+
+            if interactive {
+                let is_hovered = ctx.selection_state().highlight_for_ui_element(&item)
+                    == HoverHighlight::Hovered;
+                list_item = list_item.force_hovered(is_hovered);
+            }
+
+            let response = if component_name.is_indicator_component() {
+                list_item.show_flat(
+                    ui,
+                    re_ui::list_item::LabelContent::new(component_name.short_name())
+                        .with_icon(icon),
+                )
+            } else {
+                let content = re_ui::list_item::PropertyContent::new(component_name.short_name())
+                    .with_icon(icon)
+                    .value_fn(|ui, _| {
+                        if instance.is_all() {
+                            crate::EntityLatestAtResults {
+                                entity_path: entity_path.clone(),
+                                component_name,
+                                unit,
+                            }
+                            .data_ui(
+                                ctx,
+                                ui,
+                                UiLayout::List,
+                                query,
+                                db,
+                            );
+                        } else {
+                            ctx.component_ui_registry.ui(
+                                ctx,
+                                ui,
+                                UiLayout::List,
+                                query,
+                                db,
+                                entity_path,
+                                component_name,
+                                unit,
+                                instance,
+                            );
+                        }
+                    });
+
+                list_item.show_flat(ui, content)
+            };
+
+            let response = response.on_hover_ui(|ui| {
+                component_name.data_ui_recording(ctx, ui, UiLayout::Tooltip);
+            });
+
+            if interactive {
+                ctx.select_hovered_on_click(&response, item);
+            }
+        }
+    });
+}
+
+/// If this entity is an image, show it together with buttons to download and copy the image.
+fn preview_if_image_ui(
+    ctx: &ViewerContext<'_>,
+    ui: &mut egui::Ui,
+    ui_layout: UiLayout,
+    query: &re_chunk_store::LatestAtQuery,
+    entity_path: &re_log_types::EntityPath,
+    component_map: &IntMap<ComponentName, UnitChunkShared>,
+) -> Option<()> {
+    let image_buffer = component_map.get(&components::ImageBuffer::name())?;
+    let buffer_row_id = image_buffer.row_id()?;
+    let image_buffer = image_buffer
+        .component_mono::<components::ImageBuffer>()?
+        .ok()?;
+
+    let image_format = component_map
+        .get(&components::ImageFormat::name())?
+        .component_mono::<components::ImageFormat>()?
+        .ok()?;
+
+    let kind = if component_map.contains_key(&archetypes::DepthImage::indicator().name()) {
+        ImageKind::Depth
+    } else if component_map.contains_key(&archetypes::SegmentationImage::indicator().name()) {
+        ImageKind::Segmentation
+    } else {
+        ImageKind::Color
+    };
+
+    let colormap = component_map
+        .get(&components::Colormap::name())
+        .and_then(|colormap| {
+            colormap
+                .component_mono::<components::Colormap>()
+                .transpose()
+                .ok()
+                .flatten()
+        });
+
+    let image = ImageInfo {
+        buffer_row_id,
+        buffer: image_buffer.0,
+        format: image_format.0,
+        kind,
+        colormap,
+    };
+
+    image_preview_ui(ctx, ui, ui_layout, query, entity_path, &image);
+
+    Some(())
+}
+
+/// Show the image.
+fn image_preview_ui(
+    ctx: &ViewerContext<'_>,
+    ui: &mut egui::Ui,
+    ui_layout: UiLayout,
+    query: &re_chunk_store::LatestAtQuery,
+    entity_path: &re_log_types::EntityPath,
+    image: &ImageInfo,
+) -> Option<()> {
+    let render_ctx = ctx.render_ctx?;
+    let image_stats = ctx.cache.entry(|c: &mut ImageStatsCache| c.entry(image));
+    let annotations = crate::annotations(ctx, query, entity_path);
+    let debug_name = entity_path.to_string();
+    let texture = image_to_gpu(render_ctx, &debug_name, image, &image_stats, &annotations).ok()?;
+
+    texture_preview_ui(render_ctx, ui, ui_layout, entity_path, texture);
+
+    Some(())
 }
