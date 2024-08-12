83284ec46e28aff940f3fa9ff53919820979c4b3
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-08-12 11:30:47 +0200

    Refactor `InstancePath::data_ui`

--- crates/viewer/re_data_ui/src/instance_path.rs
+++ crates/viewer/re_data_ui/src/instance_path.rs
@@ -1,5 +1,7 @@
+use re_chunk_store::UnitChunkShared;
 use re_entity_db::InstancePath;
 use re_log_types::ComponentPath;
+use re_types::ComponentName;
 use re_ui::{ContextExt as _, UiExt as _};
 use re_viewer_context::{HoverHighlight, Item, UiLayout, ViewerContext};
 
@@ -62,103 +64,146 @@ impl DataUi for InstancePath {
             return;
         }
 
-        let show_indicator_comps = match ui_layout {
-            UiLayout::Tooltip => {
-                // Skip indicator components in hover ui (unless there are no other
-                // types of components).
-                indicator_count == components.len()
-            }
-            UiLayout::SelectionPanelLimitHeight | UiLayout::SelectionPanelFull => true,
-            UiLayout::List => false, // unreachable
-        };
+        let components = latest_at(db, query, entity_path, &components);
 
-        let interactive = ui_layout != UiLayout::Tooltip;
-
-        re_ui::list_item::list_item_scope(ui, "component list", |ui| {
-            for component_name in components {
-                if !show_indicator_comps && component_name.is_indicator_component() {
-                    continue;
-                }
-
-                let component_path = ComponentPath::new(entity_path.clone(), component_name);
-                let is_static = db
-                    .store()
-                    .entity_has_static_component(entity_path, &component_name);
-                let icon = if is_static {
-                    &re_ui::icons::COMPONENT_STATIC
-                } else {
-                    &re_ui::icons::COMPONENT_TEMPORAL
-                };
-                let item = Item::ComponentPath(component_path);
-
-                let mut list_item = ui.list_item().interactive(interactive);
-
-                if interactive {
-                    let is_hovered = ctx.selection_state().highlight_for_ui_element(&item)
-                        == HoverHighlight::Hovered;
-                    list_item = list_item.force_hovered(is_hovered);
-                }
-
-                let response = if component_name.is_indicator_component() {
-                    list_item.show_flat(
-                        ui,
-                        re_ui::list_item::LabelContent::new(component_name.short_name())
-                            .with_icon(icon),
-                    )
-                } else {
-                    let results = db.query_caches().latest_at(
-                        db.store(),
-                        query,
-                        entity_path,
-                        [component_name],
-                    );
-                    let Some(unit) = results.components.get(&component_name) else {
-                        continue; // no need to show components that are unset at this point in time
-                    };
-
-                    let content =
-                        re_ui::list_item::PropertyContent::new(component_name.short_name())
-                            .with_icon(icon)
-                            .value_fn(|ui, _| {
-                                if instance.is_all() {
-                                    crate::EntityLatestAtResults {
-                                        entity_path: entity_path.clone(),
-                                        component_name,
-                                        unit,
-                                    }
-                                    .data_ui(
-                                        ctx,
-                                        ui,
-                                        UiLayout::List,
-                                        query,
-                                        db,
-                                    );
-                                } else {
-                                    ctx.component_ui_registry.ui(
-                                        ctx,
-                                        ui,
-                                        UiLayout::List,
-                                        query,
-                                        db,
-                                        entity_path,
-                                        component_name,
-                                        unit,
-                                        instance,
-                                    );
-                                }
-                            });
-
-                    list_item.show_flat(ui, content)
-                };
-
-                let response = response.on_hover_ui(|ui| {
-                    component_name.data_ui_recording(ctx, ui, UiLayout::Tooltip);
-                });
-
-                if interactive {
-                    ctx.select_hovered_on_click(&response, item);
-                }
-            }
-        });
+        component_list_ui(
+            ctx,
+            ui,
+            ui_layout,
+            query,
+            db,
+            entity_path,
+            instance,
+            &components,
+        );
     }
+}
+
+fn latest_at(
+    db: &re_entity_db::EntityDb,
+    query: &re_chunk_store::LatestAtQuery,
+    entity_path: &re_log_types::EntityPath,
+    components: &[ComponentName],
+) -> Vec<(ComponentName, UnitChunkShared)> {
+    let components: Vec<(ComponentName, UnitChunkShared)> = components
+        .iter()
+        .filter_map(|&component_name| {
+            let mut results =
+                db.query_caches()
+                    .latest_at(db.store(), query, entity_path, [component_name]);
+
+            // We ignore components that are unset at this point in time
+            results
+                .components
+                .remove(&component_name)
+                .map(|unit| (component_name, unit))
+        })
+        .collect();
+    components
+}
+
+#[allow(clippy::too_many_arguments)]
+fn component_list_ui(
+    ctx: &ViewerContext<'_>,
+    ui: &mut egui::Ui,
+    ui_layout: UiLayout,
+    query: &re_chunk_store::LatestAtQuery,
+    db: &re_entity_db::EntityDb,
+    entity_path: &re_log_types::EntityPath,
+    instance: &re_log_types::Instance,
+    components: &[(ComponentName, UnitChunkShared)],
+) {
+    let indicator_count = components
+        .iter()
+        .filter(|(c, _)| c.is_indicator_component())
+        .count();
+
+    let show_indicator_comps = match ui_layout {
+        UiLayout::Tooltip => {
+            // Skip indicator components in hover ui (unless there are no other
+            // types of components).
+            indicator_count == components.len()
+        }
+        UiLayout::SelectionPanelLimitHeight | UiLayout::SelectionPanelFull => true,
+        UiLayout::List => false, // unreachable
+    };
+
+    let interactive = ui_layout != UiLayout::Tooltip;
+
+    re_ui::list_item::list_item_scope(ui, "component list", |ui| {
+        for (component_name, unit) in components {
+            let component_name = *component_name;
+            if !show_indicator_comps && component_name.is_indicator_component() {
+                continue;
+            }
+
+            let component_path = ComponentPath::new(entity_path.clone(), component_name);
+            let is_static = db
+                .store()
+                .entity_has_static_component(entity_path, &component_name);
+            let icon = if is_static {
+                &re_ui::icons::COMPONENT_STATIC
+            } else {
+                &re_ui::icons::COMPONENT_TEMPORAL
+            };
+            let item = Item::ComponentPath(component_path);
+
+            let mut list_item = ui.list_item().interactive(interactive);
+
+            if interactive {
+                let is_hovered = ctx.selection_state().highlight_for_ui_element(&item)
+                    == HoverHighlight::Hovered;
+                list_item = list_item.force_hovered(is_hovered);
+            }
+
+            let response = if component_name.is_indicator_component() {
+                list_item.show_flat(
+                    ui,
+                    re_ui::list_item::LabelContent::new(component_name.short_name())
+                        .with_icon(icon),
+                )
+            } else {
+                let content = re_ui::list_item::PropertyContent::new(component_name.short_name())
+                    .with_icon(icon)
+                    .value_fn(|ui, _| {
+                        if instance.is_all() {
+                            crate::EntityLatestAtResults {
+                                entity_path: entity_path.clone(),
+                                component_name,
+                                unit,
+                            }
+                            .data_ui(
+                                ctx,
+                                ui,
+                                UiLayout::List,
+                                query,
+                                db,
+                            );
+                        } else {
+                            ctx.component_ui_registry.ui(
+                                ctx,
+                                ui,
+                                UiLayout::List,
+                                query,
+                                db,
+                                entity_path,
+                                component_name,
+                                unit,
+                                instance,
+                            );
+                        }
+                    });
+
+                list_item.show_flat(ui, content)
+            };
+
+            let response = response.on_hover_ui(|ui| {
+                component_name.data_ui_recording(ctx, ui, UiLayout::Tooltip);
+            });
+
+            if interactive {
+                ctx.select_hovered_on_click(&response, item);
+            }
+        }
+    });
 }
