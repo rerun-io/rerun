rerun-io/rerun

commit 8d2e79c2edc38493b3bc1c26889d7180fbf3d1a6

Fix wrong edit UI for `Colormap` (#7171)
### What


![image](https://github.com/user-attachments/assets/d5919dca-2b82-432a-b243-fcb434f782e9)

👇🏻 

<img width="317" alt="image"
src="https://github.com/user-attachments/assets/5d0606d8-4c46-41ac-bcf5-6f90f48c3d00">


### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using examples from latest `main` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/7171?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/7171?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG
* [x] If applicable, add a new check to the [release
checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
* [x] If have noted any breaking changes to the log API in
`CHANGELOG.md` and the migration guide

- [PR Build Summary](https://build.rerun.io/pr/7171)
- [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
- [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

To run all checks from `main`, comment on the PR with `@rerun-bot
full-check`.

---------

Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>

crates/viewer/re_viewer_context/src/gpu_bridge/colormap.rs
@@ -101,7 +101,7 @@ fn colormap_variant_ui(
 }
 
 pub fn colormap_edit_or_view_ui(
-    render_ctx: Option<&re_renderer::RenderContext>,
+    ctx: &crate::ViewerContext<'_>,
     ui: &mut egui::Ui,
     map: &mut MaybeMutRef<'_, re_types::components::Colormap>,
 ) -> egui::Response {
@@ -114,10 +114,10 @@ pub fn colormap_edit_or_view_ui(
                 return ui.label("<no variants>");
             };
 
-            let mut response = colormap_variant_ui(render_ctx, ui, first, map);
+            let mut response = colormap_variant_ui(ctx.render_ctx, ui, first, map);
 
             for option in iter {
-                response |= colormap_variant_ui(render_ctx, ui, option, map);
+                response |= colormap_variant_ui(ctx.render_ctx, ui, option, map);
             }
 
             response
@@ -136,7 +136,7 @@ pub fn colormap_edit_or_view_ui(
         inner_response.response
     } else {
         let map: re_types::components::Colormap = **map;
-        if let Some(render_ctx) = render_ctx {
+        if let Some(render_ctx) = ctx.render_ctx {
             ui.list_item_flat_noninteractive(
                 list_item::PropertyContent::new(map.to_string())
                     .min_desired_width(MIN_WIDTH)

crates/viewer/re_edit_ui/src/lib.rs
@@ -30,7 +30,7 @@ use re_types::{
     },
     Loggable as _,
 };
-
+use re_viewer_context::gpu_bridge::colormap_edit_or_view_ui;
 // ----
 
 /// Registers all editors of this crate in the component UI registry.
@@ -70,10 +70,12 @@ pub fn register_editors(registry: &mut re_viewer_context::ComponentUiRegistry) {
     registry.add_singleline_edit_or_view::<Name>(edit_singleline_string);
     registry.add_multiline_edit_or_view::<Name>(edit_multiline_string);
 
+    // `Colormap` _is_ an enum, but its custom editor is far better.
+    registry.add_singleline_edit_or_view::<Colormap>(colormap_edit_or_view_ui);
+
     // TODO(#6974): Enums editors trivial and always the same, provide them automatically!
     registry.add_singleline_edit_or_view::<AggregationPolicy>(edit_view_enum);
     registry.add_singleline_edit_or_view::<BackgroundKind>(edit_view_enum);
-    registry.add_singleline_edit_or_view::<Colormap>(edit_view_enum);
     registry.add_singleline_edit_or_view::<Corner2D>(edit_view_enum);
     registry.add_singleline_edit_or_view::<FillMode>(edit_view_enum);
     registry.add_singleline_edit_or_view::<MagnificationFilter>(edit_view_enum);

tests/python/release_checklist/check_colormap_edit_ui.py
@@ -1,0 +1,50 @@
+from __future__ import annotations
+
+import os
+from argparse import Namespace
+from uuid import uuid4
+
+import numpy as np
+import rerun as rr
+import rerun.blueprint as rrb
+
+README = """\
+# Colormap edit UI
+
+- Click on the depth image.
+- In the selection panel, check that the `Colormap` menu (in the `Visualizers` section) includes nice previews of the colormaps.
+"""
+
+
+def log_readme() -> None:
+    rr.log("readme", rr.TextDocument(README, media_type=rr.MediaType.MARKDOWN), static=True)
+
+
+def log_depth_image() -> None:
+    depth_image = 65535 * np.ones((200, 300), dtype=np.uint16)
+    depth_image[50:150, 50:150] = 20000
+    depth_image[130:180, 100:280] = 45000
+    rr.log("depth", rr.DepthImage(depth_image, meter=10_000.0))
+
+
+def blueprint() -> rrb.BlueprintLike:
+    return rrb.Horizontal(
+        rrb.TextDocumentView(origin="readme"),
+        rrb.Spatial2DView(origin="/depth"),
+    )
+
+
+def run(args: Namespace) -> None:
+    rr.script_setup(args, f"{os.path.basename(__file__)}", recording_id=uuid4(), default_blueprint=blueprint())
+
+    log_readme()
+    log_depth_image()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Interactive release checklist")
+    rr.script_add_args(parser)
+    args = parser.parse_args()
+    run(args)

commit 78d974fee91fae66e39c9067c87e9d2111f721a4

Fix some component UI (#7174)
### What

This PR fixes the UI for enum components and `Colormap`, which was
broken by #7171

<img width="486" alt="image"
src="https://github.com/user-attachments/assets/e86b22e7-01a7-40f5-b744-940e52b443e4">

<br/><br/>

Note/PSA: we still have a bunch of components that are not truncating
correctly, but most have quite narrow UI, so we can live with it for
bit.

<img width="291" alt="image"
src="https://github.com/user-attachments/assets/00c38da5-e7f6-42b3-94c0-f6ebc8af4551">



### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using examples from latest `main` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/7174?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/7174?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG
* [x] If applicable, add a new check to the [release
checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
* [x] If have noted any breaking changes to the log API in
`CHANGELOG.md` and the migration guide

- [PR Build Summary](https://build.rerun.io/pr/7174)
- [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
- [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

To run all checks from `main`, comment on the PR with `@rerun-bot
full-check`.

crates/viewer/re_edit_ui/src/datatype_editors/enum_combobox.rs
@@ -48,7 +48,7 @@ fn edit_view_enum_impl<EnumT: re_types_core::reflection::Enum>(
 
         response_with_changes_of_inner(combobox_response)
     } else {
-        ui.label(current_value.to_string())
+        ui.add(egui::Label::new(current_value.to_string()).truncate())
     }
 }
 

crates/viewer/re_viewer_context/src/gpu_bridge/colormap.rs
@@ -1,5 +1,5 @@
 use re_types::reflection::Enum as _;
-use re_ui::{list_item, UiExt};
+use re_ui::list_item;
 
 use crate::{
     gpu_bridge::{get_or_create_texture, render_image},
@@ -136,18 +136,21 @@ pub fn colormap_edit_or_view_ui(
         inner_response.response
     } else {
         let map: re_types::components::Colormap = **map;
-        if let Some(render_ctx) = ctx.render_ctx {
-            ui.list_item_flat_noninteractive(
-                list_item::PropertyContent::new(map.to_string())
-                    .min_desired_width(MIN_WIDTH)
-                    .value_fn(|ui, _| {
-                        if let Err(err) = colormap_preview_ui(render_ctx, ui, map) {
-                            re_log::error_once!("Failed to paint colormap preview: {err}");
-                        }
-                    }),
-            )
+        let colormap_response = if let Some(render_ctx) = ctx.render_ctx {
+            let result = colormap_preview_ui(render_ctx, ui, map);
+            if let Err(err) = &result {
+                re_log::error_once!("Failed to paint colormap preview: {err}");
+            }
+            result.ok()
         } else {
-            ui.list_item_flat_noninteractive(list_item::LabelContent::new(map.to_string()))
+            None
+        };
+
+        let label_response = ui.add(egui::Label::new(map.to_string()).truncate());
+
+        match colormap_response {
+            Some(colormap_response) => colormap_response | label_response,
+            None => label_response,
         }
     }
 }