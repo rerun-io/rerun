9df0086d2aacbe8c4c2c852c44a197ffb17341c3
Andreas Reich <andreas@rerun.io>
2024-08-16 11:20:51 +0200

    call out bytes in `image_send_columns.py`

--- docs/snippets/all/archetypes/image_send_columns.py
+++ docs/snippets/all/archetypes/image_send_columns.py
@@ -24,5 +24,9 @@ rr.send_columns(
     "images",
     times=[rr.TimeSequenceColumn("step", times)],
     # Reshape the images so `ImageBufferBatch` can tell that this is several blobs.
+    #
+    # Note that the `ImageBufferBatch` consumes arrays of bytes,
+    # so if you have a different channel datatype than `U8`, you need need to make sure
+    # that the data is converted to arrays of bytes before passing it to `ImageBufferBatch`.
     components=[rr.components.ImageBufferBatch(images.reshape(len(times), -1))],
 )
