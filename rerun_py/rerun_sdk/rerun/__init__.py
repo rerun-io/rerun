5851dc330b71d230336dce2512624839f45df2a4
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-08-16 12:06:49 +0200

    Bump versions to 0.19.0-alpha.1+dev

--- rerun_py/rerun_sdk/rerun/__init__.py
+++ rerun_py/rerun_sdk/rerun/__init__.py
@@ -7,8 +7,8 @@ from uuid import UUID
 
 import numpy as np
 
-__version__ = "0.18.0"
-__version_info__ = (0, 18, 0, "None")
+__version__ = "0.19.0-alpha.1+dev"
+__version_info__ = (0, 19, 0, "alpha.1")
 
 # =====================================
 # API RE-EXPORTS
