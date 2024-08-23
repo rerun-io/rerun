a071f058ab39ee8902989273b3eef5dca8a6c55a
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2024-08-23 11:29:36 +0200

    Fix python version updating script (#7213)
    Fix the python version updating script, which failed the release process
    (it was bugged for not-prerelease builds).

--- rerun_py/tests/unit/test_version.py
+++ rerun_py/tests/unit/test_version.py
@@ -7,6 +7,8 @@ import semver
 def test_version() -> None:
     ver = semver.VersionInfo.parse(rr.__version__)
 
+    assert len(rr.__version_info__) == 4
+
     assert ver.major == rr.__version_info__[0]
     assert ver.minor == rr.__version_info__[1]
     assert ver.patch == rr.__version_info__[2]
@@ -15,9 +17,7 @@ def test_version() -> None:
         assert ver.prerelease == rr.__version_info__[3]
     else:
         # The last field is `None` if there is no prerelease.
-        # TODO(andreas): We released `0.18.0` `"None"` as prerelease, but really it should be `None`.
-        # assert rr.__version_info__[3] is None
-        assert len(rr.__version_info__) == 4
+        assert rr.__version_info__[3] is None
 
     assert rr.__version__ in rr.version()
 
