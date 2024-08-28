a846f3ab38ed8eb444390a6710c667b1913719cd
Jeremy Leibs <jeremy@rerun.io>
2024-08-28 14:44:02 -0400

    Set to alpha.1

--- rerun_cpp/src/rerun/c/sdk_info.h
+++ rerun_cpp/src/rerun/c/sdk_info.h
@@ -2,16 +2,16 @@
 ///
 /// This should match the string returned by `rr_version_string` (C) or `rerun::version_string` (C++).
 /// If not, the SDK's binary and the C header are out of sync.
-#define RERUN_SDK_HEADER_VERSION "0.18.1"
+#define RERUN_SDK_HEADER_VERSION "0.18.1-alpha.1"
 
 /// Major version of the Rerun C SDK.
 #define RERUN_SDK_HEADER_VERSION_MAJOR 0
 
 /// Minor version of the Rerun C SDK.
-#define RERUN_SDK_HEADER_VERSION_MINOR 19
+#define RERUN_SDK_HEADER_VERSION_MINOR 18
 
 /// Patch version of the Rerun C SDK.
-#define RERUN_SDK_HEADER_VERSION_PATCH 0
+#define RERUN_SDK_HEADER_VERSION_PATCH 1
 
 /// Is the Rerun library version greater or equal to this?
 ///
