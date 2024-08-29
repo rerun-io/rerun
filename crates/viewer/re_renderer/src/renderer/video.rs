ea5c0488f4e6cbe8eb3932c9ab64bd64d14a6fe0
jprochazk <pjanv42@gmail.com>
2024-08-29 10:00:28 +0200

    docs

--- crates/viewer/re_renderer/src/renderer/video.rs
+++ crates/viewer/re_renderer/src/renderer/video.rs
@@ -7,11 +7,18 @@ use crate::RenderContext;
 
 use re_video::TimeMs;
 
+/// A video file.
+///
+/// Supports asynchronously decoding video into GPU textures via [`Video::frame_at`].
 pub struct Video {
     decoder: decoder::VideoDecoder,
 }
 
 impl Video {
+    /// Loads a video from the given data.
+    ///
+    /// Currently supports the following media types:
+    /// - `video/mp4`
     pub fn load(
         render_context: &RenderContext,
         media_type: Option<&str>,
@@ -31,14 +38,17 @@ impl Video {
         Ok(Self { decoder })
     }
 
+    /// Duration of the video in milliseconds.
     pub fn duration_ms(&self) -> f64 {
         self.decoder.duration_ms()
     }
 
+    /// Natural width of the video.
     pub fn width(&self) -> u32 {
         self.decoder.width()
     }
 
+    /// Natural height of the video.
     pub fn height(&self) -> u32 {
         self.decoder.height()
     }
@@ -46,6 +56,10 @@ impl Video {
     /// Returns a texture with the latest frame at the given timestamp.
     ///
     /// If the timestamp is negative, a zeroed texture is returned.
+    ///
+    /// This API is _asynchronous_, meaning that the decoder may not yet have decoded the frame
+    /// at the given timestamp. If the frame is not yet available, the returned texture will be
+    /// empty.
     ///
     /// This takes `&mut self` because the decoder maintains a buffer of decoded frames,
     /// which requires mutation. It is also not thread-safe by default.
