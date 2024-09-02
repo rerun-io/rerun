033c12b1b6900936808b9057ee1724392ac5b3f6
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-09-02 09:51:13 +0200

    Use `<url>` in an fbs too

--- rerun_py/rerun_sdk/rerun/datatypes/tensor_data.py
+++ rerun_py/rerun_sdk/rerun/datatypes/tensor_data.py
@@ -42,7 +42,7 @@ class TensorData(TensorDataExt):
 
     It's not currently possible to use `send_columns` with tensors since construction
     of `rerun.components.TensorDataBatch` does not support more than a single element.
-    This will be addressed as part of https://github.com/rerun-io/rerun/issues/6832.
+    This will be addressed as part of <https://github.com/rerun-io/rerun/issues/6832>.
     """
 
     # __init__ can be found in tensor_data_ext.py
