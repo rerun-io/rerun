7372f9d686721a8d394885d5aadf6119e762dd9c
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-09-05 15:17:08 +0200

    Start using egui_table

--- crates/viewer/re_space_view_dataframe/src/dataframe_ui.rs
+++ crates/viewer/re_space_view_dataframe/src/dataframe_ui.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use egui::ahash::HashMap;
 
 use re_chunk_store::external::re_chunk::external::arrow2::array::{
@@ -91,80 +93,112 @@ fn dataframe_ui(
     ctx: &ViewerContext<'_>,
     ui: &mut egui::Ui,
     schema: &[ColumnDescriptor],
-    display_record_batches: &[DisplayRecordBatch],
+    display_record_batches: &[DisplayRecordBatch<'_>],
     query_timeline: Timeline,
     num_rows: u64,
 ) {
-    let get_batch_and_index = |row: usize| -> (&DisplayRecordBatch, usize) {
-        assert!(row < num_rows as usize);
+    re_tracing::profile_function!();
 
-        let mut row = row;
-        for batch in display_record_batches {
-            if row < batch.num_rows() {
-                return (batch, row);
+    // TODO(emilk): hierarchical header rows
+    const NUM_HEADER_ROWS: u64 = 1;
+
+    struct MyTableDelegate<'a> {
+        ctx: &'a ViewerContext<'a>,
+        schema: &'a [ColumnDescriptor],
+        display_record_batches: &'a [DisplayRecordBatch<'a>],
+        query_timeline: Timeline,
+
+        batch_and_row_from_row_nr: BTreeMap<u64, (usize, usize)>,
+    }
+
+    impl<'a> MyTableDelegate<'a> {
+        fn batch_and_index(&self, row: u64) -> (usize, usize) {
+            let mut row = row as usize;
+            for (batch_index, batch) in self.display_record_batches.iter().enumerate() {
+                if row < batch.num_rows() {
+                    return (batch_index, row);
+                }
+                row -= batch.num_rows();
             }
-            row -= batch.num_rows();
+            panic!("row out of bounds");
         }
-        panic!("row out of bounds");
-    };
+    }
 
-    let header_ui = |mut row: egui_extras::TableRow<'_, '_>| {
-        for column in schema {
-            row.col(|ui| {
-                ui.strong(column.short_name());
-            });
-        }
-    };
+    impl<'a> egui_table::TableDelegate for MyTableDelegate<'a> {
+        fn prefetch_rows(&mut self, row_numbers: std::ops::Range<u64>) {
+            re_tracing::profile_function!();
 
-    let row_ui = |mut row: egui_extras::TableRow<'_, '_>| {
-        re_tracing::profile_scope!("row_ui");
-        let (batch, batch_index) = get_batch_and_index(row.index());
-
-        //TODO: wrong!
-        let latest_at_query = LatestAtQuery::new(query_timeline, TimeInt::MAX);
-
-        let row_id = RowId::ZERO;
-
-        for column in batch.columns() {
-            row.col(|ui| column.data_ui(ctx, ui, row_id, &latest_at_query, batch_index));
-        }
-    };
-
-    egui::ScrollArea::horizontal()
-        .auto_shrink([false, false])
-        .show(ui, |ui| {
-            ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Extend);
-
-            egui::Frame {
-                inner_margin: egui::Margin::same(5.0),
-                ..Default::default()
+            for table_row_nr in row_numbers {
+                if table_row_nr < NUM_HEADER_ROWS {
+                    continue; // header row
+                }
+                let row_nr = table_row_nr - NUM_HEADER_ROWS; // ignore header rows
+                self.batch_and_row_from_row_nr
+                    .insert(row_nr, self.batch_and_index(row_nr));
             }
-            .show(ui, |ui| {
-                let mut table_builder = egui_extras::TableBuilder::new(ui)
-                    .columns(
-                        egui_extras::Column::auto_with_initial_suggestion(200.0).clip(true),
-                        schema.len(),
-                    )
-                    .resizable(true)
-                    .vscroll(true)
-                    .auto_shrink([false, false])
-                    .striped(true);
+        }
 
-                // if let Some(scroll_to_row) = scroll_to_row {
-                //     table_builder =
-                //         table_builder.scroll_to_row(scroll_to_row, Some(egui::Align::TOP));
-                // }
+        fn cell_ui(&mut self, ui: &mut egui::Ui, cell: &egui_table::CellInfo) {
+            re_tracing::profile_function!();
 
-                table_builder
-                    .header(re_ui::DesignTokens::table_line_height(), header_ui)
-                    .body(|body| {
-                        body.rows(
-                            re_ui::DesignTokens::table_line_height(),
-                            //TODO: minor annoyance
-                            num_rows as usize,
-                            row_ui,
-                        );
-                    });
-            });
-        });
+            if cell.row_nr % 2 == 1 {
+                // Paint stripes
+                ui.painter()
+                    .rect_filled(ui.max_rect(), 0.0, ui.visuals().faint_bg_color);
+            }
+
+            egui::Frame::none()
+                .inner_margin(egui::Margin::symmetric(4.0, 0.0))
+                .show(ui, |ui| {
+                    if cell.row_nr < NUM_HEADER_ROWS {
+                        // Header row
+                        ui.strong(self.schema[cell.col_nr].short_name());
+                        return;
+                    }
+
+                    let row_nr = cell.row_nr - NUM_HEADER_ROWS; // ignore header rows
+
+                    //TODO: wrong!
+                    let latest_at_query = LatestAtQuery::new(self.query_timeline, TimeInt::MAX);
+
+                    let row_id = RowId::ZERO;
+
+                    let (batch_nr, batch_index) = self.batch_and_row_from_row_nr[&row_nr]; // Will have been pre-fetched
+                    let batch = &self.display_record_batches[batch_nr];
+                    let column = &batch.columns()[cell.col_nr];
+
+                    if ui.is_sizing_pass() {
+                        ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Extend);
+                    } else {
+                        ui.style_mut().wrap_mode = Some(egui::TextWrapMode::Truncate);
+                    }
+                    column.data_ui(self.ctx, ui, row_id, &latest_at_query, batch_index);
+                });
+        }
+    }
+    let mut table_delegate = MyTableDelegate {
+        ctx,
+        schema,
+        display_record_batches,
+        query_timeline,
+
+        batch_and_row_from_row_nr: Default::default(), // Will be filled during pre-fetch
+    };
+
+    let num_sticky_cols = schema
+        .iter()
+        .take_while(|cd| matches!(cd, ColumnDescriptor::Control(_) | ColumnDescriptor::Time(_)))
+        .count();
+
+    egui::Frame::none().inner_margin(5.0).show(ui, |ui| {
+        egui_table::Table {
+            columns: vec![egui_table::Column::new(200.0, 0.0..=f32::INFINITY); schema.len()],
+            num_sticky_cols,
+            sticky_row_heights: vec![20.0],
+            num_rows: NUM_HEADER_ROWS + num_rows,
+            row_height: re_ui::DesignTokens::table_line_height(),
+            ..Default::default()
+        }
+        .show(ui, &mut table_delegate);
+    });
 }
