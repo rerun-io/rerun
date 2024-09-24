a217b4753b168eb17bb534e0af798fd5f5b9554e
Andreas Reich <r_andreas2@web.de>
2024-09-24 12:39:47 +0200

    rounding and impl notes

--- crates/store/re_types/src/components/video_timestamp_ext.rs
+++ crates/store/re_types/src/components/video_timestamp_ext.rs
@@ -4,13 +4,13 @@ impl VideoTimestamp {
     /// Create new timestamp from seconds since video start.
     #[inline]
     pub fn from_seconds(seconds: f64) -> Self {
-        crate::datatypes::VideoTimestamp::from_nanos((seconds * 1e9) as i64).into()
+        crate::datatypes::VideoTimestamp::from_nanos((seconds * 1e9).round() as i64).into()
     }
 
     /// Create new timestamp from milliseconds since video start.
     #[inline]
     pub fn from_milliseconds(milliseconds: f64) -> Self {
-        crate::datatypes::VideoTimestamp::from_nanos((milliseconds * 1e6) as i64).into()
+        crate::datatypes::VideoTimestamp::from_nanos((milliseconds * 1e6).round() as i64).into()
     }
 
     /// Create new timestamp from nanoseconds since video start.
