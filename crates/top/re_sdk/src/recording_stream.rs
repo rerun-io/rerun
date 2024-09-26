233665e80246b33c07306c931b6f2705aad6b807
Andreas Reich <r_andreas2@web.de>
2024-09-26 15:21:02 +0200

    fix missing backticks

--- crates/top/re_sdk/src/recording_stream.rs
+++ crates/top/re_sdk/src/recording_stream.rs
@@ -351,7 +351,7 @@ impl RecordingStreamBuilder {
     ///
     /// The Rerun Viewer is able to read continuously from the resulting rrd file while it is being written.
     /// However, depending on your OS and configuration, changes may not be immediately visible due to file caching.
-    /// This is a common issue on Windows and (to a lesser extent) on MacOS.
+    /// This is a common issue on Windows and (to a lesser extent) on `MacOS`.
     ///
     /// ## Example
     ///
