c0900c9a3f7051e4840058498a28eb694d4ed094
Jochen Görtler <me@jgoertler.com>
2024-09-27 14:29:50 +0200

    WIP: fix lints

--- crates/store/re_types/src/components/graph_edge_ext.rs
+++ crates/store/re_types/src/components/graph_edge_ext.rs
@@ -13,12 +13,14 @@ impl super::GraphEdge {
     }
 
     /// Specifies the entity in which the edge originates.
+    #[inline]
     pub fn with_source_in(mut self, path: impl Into<EntityPath>) -> Self {
         self.0.source_entity = Some(path.into());
         self
     }
 
     /// Specifies the entity in which the edge terminates.
+    #[inline]
     pub fn with_target_in(mut self, path: impl Into<EntityPath>) -> Self {
         self.0.target_entity = Some(path.into());
         self
