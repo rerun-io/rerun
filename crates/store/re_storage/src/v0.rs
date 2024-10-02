0a0a29f84015b869471c5349fe32ea794f5d95dc
Zeljko Mihaljcic <7150613+zehiko@users.noreply.github.com>
2024-10-02 13:21:05 +0200

    add conversion for ResolvedTimeRange

--- crates/store/re_storage/src/v0.rs
+++ crates/store/re_storage/src/v0.rs
@@ -1,1 +1,12 @@
+use re_log_types::ResolvedTimeRange;
+
 tonic::include_proto!("rerun.storage.v0");
+
+impl From<ResolvedTimeRange> for TimeRange {
+    fn from(rtr: ResolvedTimeRange) -> Self {
+        Self {
+            start: rtr.min().as_i64(),
+            end: rtr.max().as_i64(),
+        }
+    }
+}
