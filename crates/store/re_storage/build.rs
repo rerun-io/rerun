8feabe700dfc286d4cbd612e1fba7d1764d0ca3b
Zeljko Mihaljcic <7150613+zehiko@users.noreply.github.com>
2024-10-02 11:25:58 +0200

    incorporate initial feedback and adopt query to query expression's latest definition

--- crates/store/re_storage/build.rs
+++ crates/store/re_storage/build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    // FIXME (zehiko) We should not use build.rs, but instead we should have a small binary that
+    // we call from pixi and this binary calls tonic build prost stuff. Resulting code should be committed
+    // along with the spec file. We should also ensure that prost uses protoc from the pixi env.
     tonic_build::compile_protos("proto/rerun/v0/storage.proto").expect("compile protos failed");
 }
