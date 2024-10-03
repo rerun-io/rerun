3b7d555e0f92caf0bf43e697f6d05e41ae9cb0e1
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2024-10-03 11:00:05 +0200

    Dataframe view update and blueprint API (part 2): UI for the query (#7527)
    ### What
    
    This PR adds a wrapper over the query view property introduced in #7516
    and implement the selection panel UI for it. This UI is functional (e.g.
    filter event entity and component auto-suggestion, etc.), but it is
    still ignored by the view itself and is displayed alongside the original
    (still functional) UI.
    
    <img width="424" alt="image"
    src="https://github.com/user-attachments/assets/edf10b2a-1705-4f4e-93cc-e8064e902626">
    
    <hr>
    
    Part of a series to address #6896 and #7498.
    
    All PRs:
    - #7515
    - #7516
    - #7527 
    - #7545
    - #7551
    - #7572
    - #7573
    
    ### Checklist
    * [x] I have read and agree to [Contributor
    Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
    the [Code of
    Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
    * [x] I've included a screenshot or gif (if applicable)
    * [x] I have tested the web demo (if applicable):
    * Using examples from latest `main` build:
    [rerun.io/viewer](https://rerun.io/viewer/pr/7527?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
    * Using full set of examples from `nightly` build:
    [rerun.io/viewer](https://rerun.io/viewer/pr/7527?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
    * [x] The PR title and labels are set such as to maximize their
    usefulness for the next release's CHANGELOG
    * [x] If applicable, add a new check to the [release
    checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
    * [x] If have noted any breaking changes to the log API in
    `CHANGELOG.md` and the migration guide
    
    - [PR Build Summary](https://build.rerun.io/pr/7527)
    - [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
    - [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)
    
    To run all checks from `main`, comment on the PR with `@rerun-bot
    full-check`.

---
+++ crates/viewer/re_space_view_dataframe/src/view_query_v2/blueprint.rs
@@ -0,0 +1,242 @@
+use std::collections::HashSet;
+
+use crate::dataframe_ui::HideColumnAction;
+use crate::view_query_v2::QueryV2;
+use re_chunk_store::{ColumnDescriptor, ColumnSelector};
+use re_log_types::{TimeInt, TimelineName};
+use re_types::blueprint::{components, datatypes};
+use re_viewer_context::{SpaceViewSystemExecutionError, ViewerContext};
+
+// Accessors wrapping reads/writes to the blueprint store.
+impl QueryV2 {
+    /// Get the query timeline.
+    ///
+    /// This tries to read the timeline name from the blueprint. If missing or invalid, the current
+    /// timeline is used and saved back to the blueprint.
+    pub(crate) fn timeline(
+        &self,
+        ctx: &ViewerContext<'_>,
+    ) -> Result<re_log_types::Timeline, SpaceViewSystemExecutionError> {
+        // read the timeline and make sure it actually exists
+        let timeline = self
+            .query_property
+            .component_or_empty::<components::TimelineName>()?
+            .and_then(|name| {
+                ctx.recording()
+                    .timelines()
+                    .find(|timeline| timeline.name() == &TimelineName::from(name.as_str()))
+                    .copied()
+            });
+
+        // if the timeline is unset, we "freeze" it to the current time panel timeline
+        let save_timeline = timeline.is_none();
+        let timeline = timeline.unwrap_or_else(|| *ctx.rec_cfg.time_ctrl.read().timeline());
+        if save_timeline {
+            self.save_timeline_name(ctx, timeline.name());
+        }
+
+        Ok(timeline)
+    }
+
+    /// Save the timeline to the one specified.
+    ///
+    /// Note: this resets the range filter timestamps to -inf/+inf as any other value might be
+    /// invalidated.
+    pub(super) fn save_timeline_name(&self, ctx: &ViewerContext<'_>, timeline_name: &TimelineName) {
+        self.query_property
+            .save_blueprint_component(ctx, &components::TimelineName::from(timeline_name.as_str()));
+
+        // clearing the range filter is equivalent to setting it to the default -inf/+inf
+        self.query_property
+            .clear_blueprint_component::<components::FilterByRange>(ctx);
+    }
+
+    pub(crate) fn range_filter(&self) -> Result<(TimeInt, TimeInt), SpaceViewSystemExecutionError> {
+        #[allow(clippy::map_unwrap_or)]
+        Ok(self
+            .query_property
+            .component_or_empty::<components::FilterByRange>()?
+            .map(|range_filter| (range_filter.start.into(), range_filter.end.into()))
+            .unwrap_or((TimeInt::MIN, TimeInt::MAX)))
+    }
+
+    pub(super) fn save_range_filter(&self, ctx: &ViewerContext<'_>, start: TimeInt, end: TimeInt) {
+        if (start, end) == (TimeInt::MIN, TimeInt::MAX) {
+            self.query_property
+                .clear_blueprint_component::<components::FilterByRange>(ctx);
+        } else {
+            self.query_property
+                .save_blueprint_component(ctx, &components::FilterByRange::new(start, end));
+        }
+    }
+
+    pub(crate) fn filter_by_event(
+        &self,
+    ) -> Result<Option<components::FilterByEvent>, SpaceViewSystemExecutionError> {
+        Ok(self
+            .query_property
+            .component_or_empty::<components::FilterByEvent>()?)
+    }
+
+    pub(super) fn save_filter_by_event(
+        &self,
+        ctx: &ViewerContext<'_>,
+        filter_by_event: &components::FilterByEvent,
+    ) {
+        self.query_property
+            .save_blueprint_component(ctx, filter_by_event);
+    }
+
+    pub(crate) fn latest_at_enabled(&self) -> Result<bool, SpaceViewSystemExecutionError> {
+        Ok(self
+            .query_property
+            .component_or_empty::<components::ApplyLatestAt>()?
+            .map_or(false, |comp| *comp.0))
+    }
+
+    pub(crate) fn save_latest_at_enabled(&self, ctx: &ViewerContext<'_>, enabled: bool) {
+        self.query_property
+            .save_blueprint_component(ctx, &components::ApplyLatestAt(enabled.into()));
+    }
+
+    pub(super) fn save_selected_columns(
+        &self,
+        ctx: &ViewerContext<'_>,
+        columns: impl IntoIterator<Item = ColumnSelector>,
+    ) {
+        let mut selected_columns = datatypes::SelectedColumns::default();
+        for column in columns {
+            match column {
+                ColumnSelector::Control(_) => {}
+                ColumnSelector::Time(desc) => {
+                    selected_columns
+                        .time_columns
+                        .push(desc.timeline.as_str().into());
+                }
+                ColumnSelector::Component(desc) => {
+                    let blueprint_component_descriptor =
+                        datatypes::ComponentColumnSelector::new(&desc.entity_path, desc.component);
+
+                    selected_columns
+                        .component_columns
+                        .push(blueprint_component_descriptor);
+                }
+            }
+        }
+
+        self.query_property
+            .save_blueprint_component(ctx, &components::SelectedColumns(selected_columns));
+    }
+
+    pub(super) fn save_all_columns_selected(&self, ctx: &ViewerContext<'_>) {
+        self.query_property
+            .clear_blueprint_component::<components::SelectedColumns>(ctx);
+    }
+
+    pub(super) fn save_all_columns_unselected(&self, ctx: &ViewerContext<'_>) {
+        self.query_property
+            .save_blueprint_component(ctx, &components::SelectedColumns::default());
+    }
+
+    /// Given some view columns, list the columns that should be visible (aka "selected columns"),
+    /// according to the blueprint.
+    ///
+    /// This operates by filtering the view columns based on the blueprint specified columns.
+    ///
+    /// Returns `Ok(None)` if all columns should be displayed (aka a column selection isn't provided
+    /// in the blueprint).
+    pub(crate) fn apply_column_visibility_to_view_columns(
+        &self,
+        ctx: &ViewerContext<'_>,
+        view_columns: &[ColumnDescriptor],
+    ) -> Result<Option<Vec<ColumnSelector>>, SpaceViewSystemExecutionError> {
+        let selected_columns = self
+            .query_property
+            .component_or_empty::<components::SelectedColumns>()?;
+
+        // no selected columns means all columns are visible
+        let Some(datatypes::SelectedColumns {
+            time_columns,
+            component_columns,
+        }) = selected_columns.as_deref()
+        else {
+            // select all columns
+            return Ok(None);
+        };
+
+        let selected_time_columns: HashSet<TimelineName> = time_columns
+            .iter()
+            .map(|timeline_name| timeline_name.as_str().into())
+            .collect();
+        let selected_component_columns = component_columns.iter().cloned().collect::<HashSet<_>>();
+
+        let query_timeline_name = *self.timeline(ctx)?.name();
+        let result = view_columns
+            .iter()
+            .filter(|column| match column {
+                ColumnDescriptor::Control(_) => true,
+                ColumnDescriptor::Time(desc) => {
+                    // we always include the query timeline column because we need it for the dataframe ui
+                    desc.timeline.name() == &query_timeline_name
+                        || selected_time_columns.contains(desc.timeline.name())
+                }
+                ColumnDescriptor::Component(desc) => {
+                    let blueprint_component_descriptor = components::ComponentColumnSelector::new(
+                        &desc.entity_path,
+                        desc.component_name,
+                    );
+
+                    selected_component_columns.contains(&blueprint_component_descriptor)
+                }
+            })
+            .cloned()
+            .map(ColumnSelector::from)
+            .collect();
+
+        Ok(Some(result))
+    }
+
+    #[allow(dead_code)] //TODO(ab): used in next PR
+    pub(crate) fn handle_hide_column_actions(
+        &self,
+        ctx: &ViewerContext<'_>,
+        view_columns: &[ColumnDescriptor],
+        actions: Vec<HideColumnAction>,
+    ) -> Result<(), SpaceViewSystemExecutionError> {
+        if actions.is_empty() {
+            return Ok(());
+        }
+
+        let mut selected_columns: Vec<_> = self
+            .apply_column_visibility_to_view_columns(ctx, view_columns)?
+            .map(|columns| columns.into_iter().collect())
+            .unwrap_or_else(|| view_columns.iter().cloned().map(Into::into).collect());
+
+        for action in actions {
+            match action {
+                HideColumnAction::HideTimeColumn { timeline_name } => {
+                    selected_columns.retain(|column| match column {
+                        ColumnSelector::Time(desc) => desc.timeline != timeline_name,
+                        _ => true,
+                    });
+                }
+
+                HideColumnAction::HideComponentColumn {
+                    entity_path,
+                    component_name,
+                } => {
+                    selected_columns.retain(|column| match column {
+                        ColumnSelector::Component(desc) => {
+                            desc.entity_path != entity_path || desc.component != component_name
+                        }
+                        _ => true,
+                    });
+                }
+            }
+        }
+
+        self.save_selected_columns(ctx, selected_columns);
+
+        Ok(())
+    }
+}
