rerun-io/rerun

commit baebd02176537715c086e46169afafea918d3e34

Dataframe v2: make `num_rows` aware of filters and such (#7621)
Fix `num_rows()` returning wrong values when you set filters, PoVs, and
such.

This fixes the `ERROR ERROR ERROR` we see on `main`... but now all I see
is emptiness :thinking:

---------

Co-authored-by: Antoine Beyeler <antoine@rerun.io>

crates/viewer/re_space_view_dataframe/src/view_query/blueprint.rs
@@ -79,7 +79,8 @@ impl Query {
     ) -> Result<Option<components::FilterByEvent>, SpaceViewSystemExecutionError> {
         Ok(self
             .query_property
-            .component_or_empty::<components::FilterByEvent>()?)
+            .component_or_empty::<components::FilterByEvent>()?
+            .filter(|filter_by_event| filter_by_event.active()))
     }
 
     pub(super) fn save_filter_by_event(

crates/store/re_dataframe2/src/query.rs
@@ -6,6 +6,7 @@ use std::sync::{
 use ahash::HashSet;
 use arrow2::{
     array::Array as ArrowArray, chunk::Chunk as ArrowChunk, datatypes::Schema as ArrowSchema,
+    Either,
 };
 use itertools::Itertools;
 
@@ -376,22 +377,44 @@ impl QueryHandle<'_> {
     pub fn num_rows(&self) -> u64 {
         re_tracing::profile_function!();
 
-        let all_unique_timestamps: HashSet<TimeInt> = self
-            .init()
-            .view_chunks
-            .iter()
+        let state = self.init();
+
+        let mut view_chunks = state.view_chunks.iter();
+        let view_chunks = if let Some(view_pov_chunks_idx) = state.view_pov_chunks_idx {
+            Either::Left(view_chunks.nth(view_pov_chunks_idx).into_iter())
+        } else {
+            Either::Right(view_chunks)
+        };
+
+        let mut all_unique_timestamps: HashSet<TimeInt> = view_chunks
             .flat_map(|chunks| {
                 chunks.iter().filter_map(|(_cursor, chunk)| {
-                    chunk
-                        .timelines()
-                        .get(&self.query.filtered_index)
-                        .map(|time_column| time_column.times())
+                    if chunk.is_static() {
+                        Some(Either::Left(std::iter::once(TimeInt::STATIC)))
+                    } else {
+                        chunk
+                            .timelines()
+                            .get(&self.query.filtered_index)
+                            .map(|time_column| Either::Right(time_column.times()))
+                    }
                 })
             })
             .flatten()
             .collect();
 
-        all_unique_timestamps.len() as _
+        if let Some(filtered_index_values) = self.query.filtered_index_values.as_ref() {
+            all_unique_timestamps.retain(|time| filtered_index_values.contains(time));
+        }
+
+        let num_rows = all_unique_timestamps.len() as _;
+
+        if cfg!(debug_assertions) {
+            let expected_num_rows =
+                self.engine.query(self.query.clone()).into_iter().count() as u64;
+            assert_eq!(expected_num_rows, num_rows);
+        }
+
+        num_rows
     }
 
     /// Returns the next row's worth of data.
@@ -886,6 +909,10 @@ mod tests {
         eprintln!("{query:#?}:");
 
         let query_handle = query_engine.query(query.clone());
+        assert_eq!(
+            query_engine.query(query.clone()).into_iter().count() as u64,
+            query_handle.num_rows()
+        );
         let dataframe = concatenate_record_batches(
             query_handle.schema().clone(),
             &query_handle.into_batch_iter().collect_vec(),
@@ -928,6 +955,10 @@ mod tests {
         eprintln!("{query:#?}:");
 
         let query_handle = query_engine.query(query.clone());
+        assert_eq!(
+            query_engine.query(query.clone()).into_iter().count() as u64,
+            query_handle.num_rows()
+        );
         let dataframe = concatenate_record_batches(
             query_handle.schema().clone(),
             &query_handle.into_batch_iter().collect_vec(),
@@ -970,6 +1001,10 @@ mod tests {
         eprintln!("{query:#?}:");
 
         let query_handle = query_engine.query(query.clone());
+        assert_eq!(
+            query_engine.query(query.clone()).into_iter().count() as u64,
+            query_handle.num_rows()
+        );
         let dataframe = concatenate_record_batches(
             query_handle.schema().clone(),
             &query_handle.into_batch_iter().collect_vec(),
@@ -1018,6 +1053,10 @@ mod tests {
         eprintln!("{query:#?}:");
 
         let query_handle = query_engine.query(query.clone());
+        assert_eq!(
+            query_engine.query(query.clone()).into_iter().count() as u64,
+            query_handle.num_rows()
+        );
         let dataframe = concatenate_record_batches(
             query_handle.schema().clone(),
             &query_handle.into_batch_iter().collect_vec(),
@@ -1068,6 +1107,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1091,6 +1134,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1114,6 +1161,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1147,6 +1198,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1198,6 +1253,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1231,6 +1290,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1277,6 +1340,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1306,6 +1373,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1354,6 +1425,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),
@@ -1434,6 +1509,10 @@ mod tests {
             eprintln!("{query:#?}:");
 
             let query_handle = query_engine.query(query.clone());
+            assert_eq!(
+                query_engine.query(query.clone()).into_iter().count() as u64,
+                query_handle.num_rows()
+            );
             let dataframe = concatenate_record_batches(
                 query_handle.schema().clone(),
                 &query_handle.into_batch_iter().collect_vec(),

rerun_py/rerun_bindings/rerun_bindings.pyi
@@ -4,15 +4,6 @@ import pyarrow as pa
 
 from .types import AnyColumn, ComponentLike, ViewContentsLike
 
-class ControlColumnDescriptor:
-    """A control-level column such as `RowId`."""
-
-class ControlColumnSelector:
-    """A selector for a control column."""
-
-    @staticmethod
-    def row_id() -> ControlColumnSelector: ...
-
 class IndexColumnDescriptor:
     """A column containing the index values for when the component data was updated."""
 
@@ -35,7 +26,6 @@ class ComponentColumnSelector:
 class Schema:
     """The schema representing all columns in a [`Recording`][]."""
 
-    def control_columns(self) -> list[ControlColumnDescriptor]: ...
     def index_columns(self) -> list[IndexColumnDescriptor]: ...
     def component_columns(self) -> list[ComponentColumnDescriptor]: ...
     def column_for(self, entity_path: str, component: ComponentLike) -> Optional[ComponentColumnDescriptor]: ...

rerun_py/rerun_bindings/types.py
@@ -8,20 +8,15 @@ if TYPE_CHECKING:
     from .rerun_bindings import (
         ComponentColumnDescriptor as ComponentColumnDescriptor,
         ComponentColumnSelector as ComponentColumnSelector,
-        ControlColumnDescriptor as ControlColumnDescriptor,
-        ControlColumnSelector as ControlColumnSelector,
         TimeColumnDescriptor as TimeColumnDescriptor,
         TimeColumnSelector as TimeColumnSelector,
     )
 
-
 ComponentLike: TypeAlias = Union[str, type["ComponentMixin"]]
 
 AnyColumn: TypeAlias = Union[
-    "ControlColumnDescriptor",
     "TimeColumnDescriptor",
     "ComponentColumnDescriptor",
-    "ControlColumnSelector",
     "TimeColumnSelector",
     "ComponentColumnSelector",
 ]

rerun_py/rerun_sdk/rerun/dataframe.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 from rerun_bindings import (
     ComponentColumnDescriptor as ComponentColumnDescriptor,
     ComponentColumnSelector as ComponentColumnSelector,
-    ControlColumnDescriptor as ControlColumnDescriptor,
-    ControlColumnSelector as ControlColumnSelector,
     Recording as Recording,
     RRDArchive as RRDArchive,
     Schema as Schema,

commit 87e76f8e79f815f65dd33692c123917ef75ca16c

Fix handling of the filter-by-event blueprint data (#7623)
### What

Small API fix for the filter-by-event blueprint data handling in the
dataframe view. Fixes some perma-write-blueprint issue and a minor UX
bug introduced in #7621

### Checklist
* [x] I have read and agree to [Contributor
Guide](https://github.com/rerun-io/rerun/blob/main/CONTRIBUTING.md) and
the [Code of
Conduct](https://github.com/rerun-io/rerun/blob/main/CODE_OF_CONDUCT.md)
* [x] I've included a screenshot or gif (if applicable)
* [x] I have tested the web demo (if applicable):
* Using examples from latest `main` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/7623?manifest_url=https://app.rerun.io/version/main/examples_manifest.json)
* Using full set of examples from `nightly` build:
[rerun.io/viewer](https://rerun.io/viewer/pr/7623?manifest_url=https://app.rerun.io/version/nightly/examples_manifest.json)
* [x] The PR title and labels are set such as to maximize their
usefulness for the next release's CHANGELOG
* [x] If applicable, add a new check to the [release
checklist](https://github.com/rerun-io/rerun/blob/main/tests/python/release_checklist)!
* [x] If have noted any breaking changes to the log API in
`CHANGELOG.md` and the migration guide

- [PR Build Summary](https://build.rerun.io/pr/7623)
- [Recent benchmark results](https://build.rerun.io/graphs/crates.html)
- [Wasm size tracking](https://build.rerun.io/graphs/sizes.html)

To run all checks from `main`, comment on the PR with `@rerun-bot
full-check`.

crates/viewer/re_space_view_dataframe/src/space_view_class.rs
@@ -4,7 +4,7 @@ use crate::{
     dataframe_ui::dataframe_ui, expanded_rows::ExpandedRowsCache, view_query,
     visualizer_system::EmptySystem,
 };
-use re_chunk_store::{ColumnDescriptor, ComponentColumnSelector, SparseFillStrategy};
+use re_chunk_store::{ColumnDescriptor, SparseFillStrategy};
 use re_log_types::EntityPath;
 use re_types_core::SpaceViewClassIdentifier;
 use re_viewer_context::{
@@ -137,13 +137,6 @@ mode sets the default time range to _everything_. You can override this in the s
             .map(|entity| (entity.clone(), None))
             .collect();
 
-        let filtered_point_of_view = view_query.filter_by_event()?.map(|filter| {
-            ComponentColumnSelector::new_for_component_name(
-                filter.entity_path(),
-                filter.component_name(),
-            )
-        });
-
         let sparse_fill_strategy = if view_query.latest_at_enabled()? {
             SparseFillStrategy::LatestAtGlobal
         } else {
@@ -158,7 +151,7 @@ mode sets the default time range to _everything_. You can override this in the s
             view_contents: Some(view_contents),
             filtered_index: view_query.timeline(ctx)?,
             filtered_index_range: Some(view_query.filter_by_range()?),
-            filtered_point_of_view,
+            filtered_point_of_view: view_query.filter_by_event()?,
             sparse_fill_strategy,
             selection: selected_columns,
 

crates/viewer/re_space_view_dataframe/src/view_query/blueprint.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use crate::dataframe_ui::HideColumnAction;
 use crate::view_query::Query;
-use re_chunk_store::{ColumnDescriptor, ColumnSelector};
+use re_chunk_store::{ColumnDescriptor, ColumnSelector, ComponentColumnSelector};
 use re_log_types::{EntityPath, ResolvedTimeRange, TimelineName};
 use re_types::blueprint::{components, datatypes};
 use re_viewer_context::{SpaceViewSystemExecutionError, ViewerContext};
@@ -74,13 +74,28 @@ impl Query {
         }
     }
 
+    /// Get the point of view column for the filter-by-event feature, if active.
     pub(crate) fn filter_by_event(
+        &self,
+    ) -> Result<Option<ComponentColumnSelector>, SpaceViewSystemExecutionError> {
+        Ok(self
+            .filter_by_event_raw()?
+            .filter(|filter_by_event| filter_by_event.active())
+            .map(|filter| {
+                ComponentColumnSelector::new_for_component_name(
+                    filter.entity_path(),
+                    filter.component_name(),
+                )
+            }))
+    }
+
+    /// Get the raw [`components::FilterByEvent`] struct (for ui purposes).
+    pub(super) fn filter_by_event_raw(
         &self,
     ) -> Result<Option<components::FilterByEvent>, SpaceViewSystemExecutionError> {
         Ok(self
             .query_property
-            .component_or_empty::<components::FilterByEvent>()?
-            .filter(|filter_by_event| filter_by_event.active()))
+            .component_or_empty::<components::FilterByEvent>()?)
     }
 
     pub(super) fn save_filter_by_event(

crates/viewer/re_space_view_dataframe/src/view_query/ui.rs
@@ -137,7 +137,7 @@ impl Query {
         // Read stuff
         //
 
-        let original_filter_by_event = self.filter_by_event()?;
+        let original_filter_by_event = self.filter_by_event_raw()?;
 
         let (mut active, event_entity, event_component) = original_filter_by_event
             .as_ref()