8fea1fcd6fa857748a3471dbadbc448c95fc34f3
Jochen Görtler <me@jgoertler.com>
2024-10-08 17:53:56 +0200

    WIP: fix visibility

--- examples/rust/graph_view/src/visualizers/nodes.rs
+++ examples/rust/graph_view/src/visualizers/nodes.rs
@@ -27,7 +27,7 @@ pub struct GraphNodeVisualizer {
 }
 
 pub(crate) struct GraphNodeVisualizerData {
-    entity_path: EntityPath,
+    pub entity_path: EntityPath,
     node_ids: ChunkComponentIterItem<components::GraphNodeId>,
 
     // Clamped
