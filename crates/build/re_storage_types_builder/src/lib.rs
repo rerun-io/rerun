cb9e7a61c0cc55076749b431075f7350ce07bb28
Zeljko Mihaljcic <7150613+zehiko@users.noreply.github.com>
2024-10-09 11:02:54 +0200

    Update crates/build/re_storage_types_builder/src/lib.rs
    Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>

--- crates/build/re_storage_types_builder/src/lib.rs
+++ crates/build/re_storage_types_builder/src/lib.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 /// Generate rust from from protobuf definitions. We rely on `tonic_build` to do the heavy lifting.
 /// `tonic_build` relies on `prost` which itself relies on `protoc`.
 ///
-/// Note: `protoc` that's part of pixi environment will be used.
+/// Note: make sure to invoke this via `pixi run codegen-storage` in order to use the right `protoc` version.
 pub fn generate_rust_code(
     definitions_dir: impl AsRef<Path>,
     proto_paths: &[impl AsRef<Path>],
