76c6255f883c28f651a35acd057434d9dc2710a7
Jeremy Leibs <jeremy@rerun.io>
2024-10-11 11:45:22 -0400

    Fix docstring

--- crates/store/re_types/src/blueprint/datatypes/component_column_selector_ext.rs
+++ crates/store/re_types/src/blueprint/datatypes/component_column_selector_ext.rs
@@ -1,7 +1,7 @@
 use re_log_types::EntityPath;
 
 impl super::ComponentColumnSelector {
-    /// Create a [`Self`] from an [`EntityPath`] and a [`ComponentName`].
+    /// Create a [`Self`] from an [`EntityPath`] and a [`re_types_core::ComponentName`] expressed as string.
     pub fn new(entity_path: &EntityPath, component_name: &str) -> Self {
         Self {
             entity_path: entity_path.into(),
