60cf3c4d7a4ba9aac8fd29c29bece99c2ec5d56f
Zeljko Mihaljcic <7150613+zehiko@users.noreply.github.com>
2024-10-14 10:16:11 +0200

    re-export all the types from re_chunk_store that we use in the dataframe API

--- crates/store/re_dataframe/src/lib.rs
+++ crates/store/re_dataframe/src/lib.rs
@@ -9,6 +9,24 @@ pub use self::query::QueryHandle;
 #[doc(no_inline)]
 pub use self::external::arrow2::chunk::Chunk as ArrowChunk;
 #[doc(no_inline)]
+pub use self::external::re_chunk_store::ColumnSelector;
+#[doc(no_inline)]
+pub use self::external::re_chunk_store::ComponentColumnSelector;
+#[doc(no_inline)]
+pub use self::external::re_chunk_store::Index;
+#[doc(no_inline)]
+pub use self::external::re_chunk_store::IndexRange;
+#[doc(no_inline)]
+pub use self::external::re_chunk_store::JoinEncoding;
+#[doc(no_inline)]
+pub use self::external::re_chunk_store::QueryExpression;
+#[doc(no_inline)]
+pub use self::external::re_chunk_store::SparseFillStrategy;
+#[doc(no_inline)]
+pub use self::external::re_chunk_store::TimeColumnSelector;
+#[doc(no_inline)]
+pub use self::external::re_chunk_store::ViewContentsSelector;
+#[doc(no_inline)]
 pub use self::external::re_query::Caches as QueryCache;
 
 pub mod external {
