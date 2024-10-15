6b8b8e8488904b8295ca99b86166908eac51981d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-10-15 15:20:11 +0200

    Print out which video features is enabled when running `rerun --version`

--- crates/store/re_video/src/lib.rs
+++ crates/store/re_video/src/lib.rs
@@ -12,3 +12,16 @@ pub use self::{
     demux::{Config, Sample, VideoData, VideoLoadError},
     time::{Time, Timescale},
 };
+
+/// Which features was this crate compiled with?
+pub fn features() -> Vec<&'static str> {
+    // TODO(emilk): is there a helper crate for this?
+    let mut features = vec![];
+    if cfg!(feature = "av1") {
+        features.push("av1");
+    }
+    if cfg!(feature = "nasm") {
+        features.push("nasm");
+    }
+    features
+}
