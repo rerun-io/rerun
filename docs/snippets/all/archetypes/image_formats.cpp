9dd17c244b09fe173271ab217daa3c3a454aa0bc
Andreas Reich <r_andreas2@web.de>
2024-10-17 11:35:31 +0200

    fix cpp signeness warnings

--- docs/snippets/all/archetypes/image_formats.cpp
+++ docs/snippets/all/archetypes/image_formats.cpp
@@ -23,7 +23,7 @@ int main() {
 
     // Green channel only (Luminance)
     std::vector<uint8_t> green_channel(256 * 256);
-    for (int i = 0; i < 256 * 256; ++i) {
+    for (size_t i = 0; i < 256 * 256; ++i) {
         green_channel[i] = image[i * 3 + 1];
     }
     rec.log(
@@ -33,7 +33,7 @@ int main() {
 
     // BGR image
     std::vector<uint8_t> bgr_image(256 * 256 * 3);
-    for (int i = 0; i < 256 * 256; ++i) {
+    for (size_t i = 0; i < 256 * 256; ++i) {
         bgr_image[i * 3 + 0] = image[i * 3 + 2];
         bgr_image[i * 3 + 1] = image[i * 3 + 1];
         bgr_image[i * 3 + 2] = image[i * 3 + 0];
@@ -46,10 +46,10 @@ int main() {
     // New image with Separate Y/U/V planes with 4:2:2 chroma downsampling
     std::vector<uint8_t> yuv_bytes(256 * 256 + 128 * 256 * 2);
     std::fill_n(yuv_bytes.begin(), 256 * 256, static_cast<uint8_t>(128)); // Fixed value for Y
-    auto u_plane_offset = 256 * 256;
-    auto v_plane_offset = u_plane_offset + 128 * 256;
-    for (int y = 0; y < 256; ++y) {
-        for (int x = 0; x < 128; ++x) {
+    size_t u_plane_offset = 256 * 256;
+    size_t v_plane_offset = u_plane_offset + 128 * 256;
+    for (size_t y = 0; y < 256; ++y) {
+        for (size_t x = 0; x < 128; ++x) {
             auto coord = y * 128 + x;
             yuv_bytes[u_plane_offset + coord] = static_cast<uint8_t>(x * 2); // Gradient for U
             yuv_bytes[v_plane_offset + coord] = static_cast<uint8_t>(y);     // Gradient for V
