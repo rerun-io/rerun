800ad132fb1c03dfed89ce79f3d49613dce87f3c
jprochazk <pjanv42@gmail.com>
2024-10-22 15:45:56 +0200

    make it a thing

--- crates/store/re_video/benches/video_load_bench.rs
+++ crates/store/re_video/benches/video_load_bench.rs
@@ -1,11 +1,21 @@
+use std::path::Path;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn video_load(c: &mut Criterion) {
-    let video = include_bytes!("../../../../tests/assets/video/Big_Buck_Bunny_1080_10s_av1.mp4");
+    let video_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("tests/assets/video/Big_Buck_Bunny_1080_10s_av1.mp4");
+    let video = std::fs::read(video_path).unwrap();
     c.bench_function("video_load", |b| {
         b.iter_batched(
             || {},
-            |()| re_video::VideoData::load_from_bytes(video, "video/mp4"),
+            |()| re_video::VideoData::load_from_bytes(&video, "video/mp4"),
             criterion::BatchSize::LargeInput,
         );
     });
