5c1005df9b1e55492b141aaf2bb6aefa98d0c5cc
Antoine Beyeler <antoine@rerun.io>
2024-10-24 15:30:39 +0200

    Revert changes to nuscenes

--- examples/python/nuscenes_dataset/nuscenes_dataset/__main__.py
+++ examples/python/nuscenes_dataset/nuscenes_dataset/__main__.py
@@ -12,7 +12,6 @@ import rerun.blueprint as rrb
 from nuscenes import nuscenes
 
 from .download_dataset import MINISPLIT_SCENES, download_minisplit
-from .export_gps import derive_latlon
 
 DESCRIPTION = """
 # nuScenes
@@ -83,8 +82,6 @@ def log_nuscenes(nusc: nuscenes.NuScenes, scene_name: str, max_time_sec: float) 
 
     scene = next(s for s in nusc.scene if s["name"] == scene_name)
 
-    location = nusc.get("log", scene["log_token"])["location"]
-
     rr.log("world", rr.ViewCoordinates.RIGHT_HAND_Z_UP, static=True)
 
     first_sample_token = scene["first_sample_token"]
@@ -107,16 +104,12 @@ def log_nuscenes(nusc: nuscenes.NuScenes, scene_name: str, max_time_sec: float) 
     first_timestamp_us = nusc.get("sample_data", first_lidar_token)["timestamp"]
     max_timestamp_us = first_timestamp_us + 1e6 * max_time_sec
 
-    log_lidar_and_ego_pose(location, first_lidar_token, nusc, max_timestamp_us)
-
     log_cameras(first_camera_tokens, nusc, max_timestamp_us)
     log_radars(first_radar_tokens, nusc, max_timestamp_us)
     log_annotations(first_sample_token, nusc, max_timestamp_us)
 
 
-def log_lidar_and_ego_pose(
-    location: str, first_lidar_token: str, nusc: nuscenes.NuScenes, max_timestamp_us: float
-) -> None:
+def log_lidar_and_ego_pose(first_lidar_token: str, nusc: nuscenes.NuScenes, max_timestamp_us: float) -> None:
     """Log lidar data and vehicle pose."""
     current_lidar_token = first_lidar_token
 
@@ -141,15 +134,6 @@ def log_lidar_and_ego_pose(
             ),
         )
         current_lidar_token = sample_data["next"]
-
-        # log GPS data
-        (lat, long) = derive_latlon(location, ego_pose)
-        rr.log(
-            "world/ego_vehicle/gps",
-            rr.Points3D(
-                [lat, long, ego_pose["translation"][2]],
-            ),
-        )
 
         data_file_path = nusc.dataroot / sample_data["filename"]
         pointcloud = nuscenes.LidarPointCloud.from_file(str(data_file_path))
@@ -311,15 +295,6 @@ def main() -> None:
                 defaults=[rr.components.ImagePlaneDistance(4.0)],
                 # Transform arrows for the vehicle shouldn't be too long.
                 overrides={"world/ego_vehicle": [rr.components.AxisLength(5.0)]},
-            ),
-            rrb.Vertical(
-                rrb.TextDocumentView(origin="description", name="Description"),
-                rrb.MapView(
-                    origin="world/ego_vehicle/gps",
-                    name="MapView",
-                    options=rrb.archetypes.MapOptions(provider=rrb.components.MapProvider.OpenStreetMap, zoom=18),
-                ),
-                row_shares=[1, 1],
             ),
             rrb.TextDocumentView(origin="description", name="Description"),
             column_shares=[3, 1],
