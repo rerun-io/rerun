28d227994d179ace72cae93078854e793cbf4d9f
Antoine Beyeler <antoine@rerun.io>
2024-10-25 11:18:29 +0200

    Appease mypy for the map view snipped and add todo to clean the blueprint api

--- docs/snippets/all/views/map.py
+++ docs/snippets/all/views/map.py
@@ -8,11 +8,12 @@ rr.init("rerun_example_gps_coordinates", spawn=True)
 rr.log("points", rr.GeoPoints([[47.6344, 19.1397], [47.6334, 19.1399]]))
 
 # Create a map view to display the chart.
+# TODO(#7903): cleanup the blueprint API for the map view
 blueprint = rrb.Blueprint(
     rrb.MapView(
         origin="points",
         name="MapView",
-        options=rrb.archetypes.MapOptions(provider=rrb.components.MapProvider.MapboxStreets),
+        options=rrb.archetypes.MapOptions(provider=rrb.components.MapProvider.MapboxStreets, zoom=16.0),
     ),
     collapse_panels=True,
 )
