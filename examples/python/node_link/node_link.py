5613a070efce1f64a701085b1417a359ac916b9f
Jochen Görtler <me@jgoertler.com>
2024-10-25 12:47:39 +0200

    WIP: finish first Python example

--- examples/python/node_link/node_link.py
+++ examples/python/node_link/node_link.py
@@ -5,48 +5,74 @@ from __future__ import annotations
 
 import rerun as rr
 
+s = 3  # scaling factor for the positions
+
+nodes = {
+    "1": {"label": "1", "pos": (0 * s, 0 * s)},
+    "7": {"label": "7", "pos": (-20 * s, 30 * s)},
+    "2": {"label": "2", "pos": (-30 * s, 60 * s)},
+    "6": {"label": "6", "pos": (-10 * s, 60 * s)},
+    "5_0": {"label": "5", "pos": (-20 * s, 90 * s)},
+    "11": {"label": "11", "pos": (0 * s, 90 * s)},
+    "9_0": {"label": "9", "pos": (20 * s, 30 * s)},
+    "9_1": {"label": "9", "pos": (30 * s, 60 * s)},
+    "5_1": {"label": "5", "pos": (20 * s, 90 * s)},
+}
+
+levels = [
+    {"nodes": ["1"], "edges": []},
+    {"nodes": ["1", "7", "9_0"], "edges": [("1", "7"), ("1", "9_0")]},
+    {
+        "nodes": ["1", "7", "9_0", "2", "6", "9_1"],
+        "edges": [("1", "7"), ("1", "9_0"), ("7", "2"), ("7", "6"), ("9_0", "9_1")],
+    },
+    {
+        "nodes": ["1", "7", "9_0", "2", "6", "9_1", "5_0", "11", "5_1"],
+        "edges": [
+            ("1", "7"),
+            ("1", "9_0"),
+            ("7", "2"),
+            ("7", "6"),
+            ("9_0", "9_1"),
+            ("6", "5_0"),
+            ("6", "11"),
+            ("9_1", "5_1"),
+        ],
+    },
+]
+
+
+def to_edge(e: tuple[str, str]) -> rr.components.GraphEdge:
+    return rr.components.GraphEdge(source=e[0], target=e[1])
+
 
 def main() -> None:
     rr.init("rerun_example_py_node_link", spawn=True)
 
-    s = 3  # scaling factor for the positions
-
     # Potentially unbalanced and not sorted :nerd_face:.
-    # :warning: The nodes have to be unique, which is why we use `5_0`...
-    rr.log(
-        "binary_tree",
-        rr.GraphNodes(
-            ["1", "7", "2", "6", "5_0", "11", "9_0", "9_1", "5_1"],
-            labels=["1", "7", "2", "6", "5", "11", "9", "9", "5"],
-            positions=[
-                (0 * s, 0 * s),  # 1
-                (-20 * s, 30 * s),  # 7
-                (-30 * s, 60 * s),  # 2
-                (-10 * s, 60 * s),  # 6
-                (-20 * s, 90 * s),  # 5_0
-                (0 * s, 90 * s),  # 11
-                (20 * s, 30 * s),  # 9_0
-                (30 * s, 60 * s),  # 9_1
-                (20 * s, 90 * s),  # 5_1
-            ],
-        ),
-    )
-    rr.log(
-        "binary_tree",
-        rr.GraphEdges(
-            [
-                rr.components.GraphEdge(source="1", target="7"),
-                rr.components.GraphEdge(source="7", target="2"),
-                rr.components.GraphEdge(source="7", target="6"),
-                rr.components.GraphEdge(source="6", target="5_0"),
-                rr.components.GraphEdge(source="6", target="11"),
-                rr.components.GraphEdge(source="1", target="9_0"),
-                rr.components.GraphEdge(source="9_0", target="9_1"),
-                rr.components.GraphEdge(source="9_1", target="5_1"),
-            ],
-            graph_type="directed",
-        ),
-    )
+    # :warning: The nodes have to be unique, which is why we use `5_0`…
+
+    t = 0
+    for level in levels:
+        if len(level["nodes"]) > 0:
+            t = t + 1
+            rr.set_time_seconds("stable_time", t)
+            rr.log(
+                "binary_tree",
+                rr.GraphNodes(
+                    level["nodes"],
+                    labels=list(map(lambda n: nodes[n]["label"], level["nodes"])),
+                    positions=list(map(lambda n: nodes[n]["pos"], level["nodes"])),
+                ),
+            )
+
+        if len(level["edges"]) > 0:
+            t = t + 1
+            rr.set_time_seconds("stable_time", t)
+            rr.log(
+                "binary_tree",
+                rr.GraphEdges(list(map(to_edge, level["edges"]))),
+            )
 
 
 if __name__ == "__main__":
