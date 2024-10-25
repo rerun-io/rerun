558b98b3673eca8d478a2b219bb4f776968854ae
Jochen Görtler <me@jgoertler.com>
2024-10-25 09:58:20 +0200

    WIP: import rerun

--- examples/python/node_link/node_link.py
+++ examples/python/node_link/node_link.py
@@ -3,10 +3,13 @@
 
 from __future__ import annotations
 
+import rerun as rr
+
 
 def main() -> None:
     print("Hello, world!")
 
+    rr.init("rerun_example_py_node_link", spawn=True)
 
 if __name__ == "__main__":
     main()
