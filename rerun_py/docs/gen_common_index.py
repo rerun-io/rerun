5e9bdcca90d8716d98b584512c16b962e9aba347
Clement Rey <cr.rey.clement@gmail.com>
2024-10-28 09:19:36 +0100

    update python doc index thing too

--- rerun_py/docs/gen_common_index.py
+++ rerun_py/docs/gen_common_index.py
@@ -10,7 +10,7 @@ The top-level index file should look like
 Function | Description
 -------- | -----------
 [rerun.init()](initialization/#rerun.init) | Initialize the Rerun SDK …
-[rerun.connect()](initialization/#rerun.connect) | Connect to a remote Rerun Viewer on the …
+[rerun.connect_tcp()](initialization/#rerun.connect_tcp) | Connect to a remote Rerun Viewer on the …
 [rerun.spawn()](initialization/#rerun.spawn) | Spawn a Rerun Viewer …
 …
 
@@ -84,11 +84,11 @@ SECTION_TABLE: Final[list[Section]] = [
         title="Initialization functions",
         func_list=[
             "init",
-            "connect",
+            "connect_tcp",
             "disconnect",
             "save",
             "send_blueprint",
-            "serve",
+            "serve_web",
             "spawn",
             "memory_recording",
             "notebook_show",
