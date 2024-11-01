3a7d6fcdddba8e1b313f459c99bdfa495ef38678
Antoine Beyeler <antoine@rerun.io>
2024-11-01 16:51:55 +0100

    cpp-fmt

--- docs/snippets/all/archetypes/geo_line_string_simple.cpp
+++ docs/snippets/all/archetypes/geo_line_string_simple.cpp
@@ -6,12 +6,13 @@ int main() {
     const auto rec = rerun::RecordingStream("rerun_example_geo_line_strings");
     rec.spawn().exit_on_failure();
 
-    rerun::GeoLineString line_string({
-        {41.0000, -109.0452},
-        {41.0000, -102.0415},
-        {36.9931, -102.0415},
-        {36.9931, -109.0452},
-        {41.0000, -109.0452}});
+    rerun::GeoLineString line_string(
+        {{41.0000, -109.0452},
+         {41.0000, -102.0415},
+         {36.9931, -102.0415},
+         {36.9931, -109.0452},
+         {41.0000, -109.0452}}
+    );
 
     rec.log(
         "colorado",
