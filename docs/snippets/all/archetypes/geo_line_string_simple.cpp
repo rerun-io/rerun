f2c46a2aa80836819c772e69d827ddac6b5961eb
Antoine Beyeler <antoine@rerun.io>
2024-11-01 16:27:44 +0100

    Attempt to fix C++ ci

--- docs/snippets/all/archetypes/geo_line_string_simple.cpp
+++ docs/snippets/all/archetypes/geo_line_string_simple.cpp
@@ -6,16 +6,16 @@ int main() {
     const auto rec = rerun::RecordingStream("rerun_example_geo_line_strings");
     rec.spawn().exit_on_failure();
 
-    rerun::Collection<rerun::DVec2D> line_string = {
+    rerun::GeoLineString line_string({
         {41.0000, -109.0452},
         {41.0000, -102.0415},
         {36.9931, -102.0415},
         {36.9931, -109.0452},
-        {41.0000, -109.0452}};
+        {41.0000, -109.0452}});
 
     rec.log(
         "colorado",
-        rerun::GeoLineStrings({line_string})
+        rerun::GeoLineStrings(line_string)
             .with_radii(rerun::Radius::ui_points(2.0f))
             .with_colors(rerun::Color(0, 0, 255))
     );
