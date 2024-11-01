1d4cf02762843896516c378b2cdeff4453577bf7
Antoine Beyeler <antoine@rerun.io>
2024-11-01 19:09:34 +0100

    Add python unit tests for `GeoPoints`

--- rerun_py/tests/unit/test_geopoints.py
+++ rerun_py/tests/unit/test_geopoints.py
@@ -9,87 +9,50 @@ import rerun as rr
 from rerun.components import (
     Color,
     ColorBatch,
-    Position2DBatch,
+    LatLonBatch,
 )
 from rerun.datatypes import (
-    ClassIdArrayLike,
-    Float32ArrayLike,
-    KeypointIdArrayLike,
     Rgba32ArrayLike,
-    Utf8ArrayLike,
-    Vec2DArrayLike,
+    Float32ArrayLike,
+    DVec2DArrayLike,
 )
 
 from .common_arrays import (
-    class_ids_arrays,
-    class_ids_expected,
     colors_arrays,
     colors_expected,
-    draw_order_expected,
-    draw_orders,
-    keypoint_ids_arrays,
-    keypoint_ids_expected,
-    labels_arrays,
-    labels_expected,
+    dvec2ds_arrays as positions_arrays,
+    dvec2ds_expected as positions_expected,
     radii_arrays,
     radii_expected,
-    vec2ds_arrays as positions_arrays,
-    vec2ds_expected as positions_expected,
 )
 
 
-def test_points2d() -> None:
+def test_geopoints() -> None:
     all_arrays = itertools.zip_longest(
         positions_arrays,
         radii_arrays,
         colors_arrays,
-        labels_arrays,
-        draw_orders,
-        class_ids_arrays,
-        keypoint_ids_arrays,
     )
 
-    for positions, radii, colors, labels, draw_order, class_ids, keypoint_ids in all_arrays:
+    for positions, radii, colors in all_arrays:
         positions = positions if positions is not None else positions_arrays[-1]
 
         # make Pyright happy as it's apparently not able to track typing info through zip_longest
-        positions = cast(Vec2DArrayLike, positions)
+        positions = cast(DVec2DArrayLike, positions)
         radii = cast(Optional[Float32ArrayLike], radii)
         colors = cast(Optional[Rgba32ArrayLike], colors)
-        labels = cast(Optional[Utf8ArrayLike], labels)
-        draw_order = cast(Optional[Float32ArrayLike], draw_order)
-        class_ids = cast(Optional[ClassIdArrayLike], class_ids)
-        keypoint_ids = cast(Optional[KeypointIdArrayLike], keypoint_ids)
 
-        print(
-            f"rr.Points2D(\n"
-            f"    {positions}\n"
-            f"    radii={radii!r}\n"
-            f"    colors={colors!r}\n"
-            f"    labels={labels!r}\n"
-            f"    draw_order={draw_order!r}\n"
-            f"    class_ids={class_ids!r}\n"
-            f"    keypoint_ids={keypoint_ids!r}\n"
-            f")"
-        )
-        arch = rr.Points2D(
+        print(f"rr.GeoPoints(\n    {positions}\n    radii={radii!r}\n    colors={colors!r}\n)")
+        arch = rr.GeoPoints(
             positions,
             radii=radii,
             colors=colors,
-            labels=labels,
-            draw_order=draw_order,
-            class_ids=class_ids,
-            keypoint_ids=keypoint_ids,
         )
         print(f"{arch}\n")
 
-        assert arch.positions == positions_expected(positions, Position2DBatch)
+        assert arch.positions == positions_expected(positions, LatLonBatch)
         assert arch.radii == radii_expected(radii)
         assert arch.colors == colors_expected(colors)
-        assert arch.labels == labels_expected(labels)
-        assert arch.draw_order == draw_order_expected(draw_order)
-        assert arch.class_ids == class_ids_expected(class_ids)
-        assert arch.keypoint_ids == keypoint_ids_expected(keypoint_ids)
 
 
 @pytest.mark.parametrize(
@@ -102,8 +65,8 @@ def test_points2d() -> None:
         np.array((0.0, 0.5, 0.0, 1.0)),
     ],
 )
-def test_point2d_single_color(data: Rgba32ArrayLike) -> None:
-    pts = rr.Points2D(positions=np.zeros((5, 2)), colors=data)
+def test_geopoint_single_color(data: Rgba32ArrayLike) -> None:
+    pts = rr.GeoPoints(np.zeros((5, 2)), colors=data)
 
     assert pts.colors == ColorBatch(Color([0, 128, 0, 255]))
 
@@ -129,7 +92,7 @@ def test_point2d_single_color(data: Rgba32ArrayLike) -> None:
     ],
 )
 def test_point2d_multiple_colors(data: Rgba32ArrayLike) -> None:
-    pts = rr.Points2D(positions=np.zeros((5, 2)), colors=data)
+    pts = rr.GeoPoints(np.zeros((5, 2)), colors=data)
 
     assert pts.colors == ColorBatch([
         Color([0, 128, 0, 255]),
@@ -138,4 +101,4 @@ def test_point2d_multiple_colors(data: Rgba32ArrayLike) -> None:
 
 
 if __name__ == "__main__":
-    test_points2d()
+    test_geopoints()
