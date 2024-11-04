b262a0029239823867b76ca705ea98d0519db4a1
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2024-11-04 17:42:05 +0100

    Update rerun_cpp/src/rerun/components/geo_line_string_ext.cpp
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- rerun_cpp/src/rerun/components/geo_line_string_ext.cpp
+++ rerun_cpp/src/rerun/components/geo_line_string_ext.cpp
@@ -8,7 +8,7 @@ namespace rerun {
 #ifdef EDIT_EXTENSION
         // <CODEGEN_COPY_TO_HEADER>
 
-        /// Creates a new GeoLineString object based on EPSG:4326 latitude and longitude (North/East-positive degrees).
+        /// Creates a new GeoLineString object based on [EPSG:4326](https://epsg.io/4326) latitude and longitude (North/East-positive degrees).
         static GeoLineString from_lat_lon(Collection<datatypes::DVec2D> lat_lon_) {
             GeoLineString line_string;
             line_string.lat_lon = std::move(lat_lon_);
