778c47c0d1ecc9b11b93480598ec7d48de4c18f1
Antoine Beyeler <antoine@rerun.io>
2024-11-04 16:50:13 +0100

    Custom ui for `LatLon`

--- crates/viewer/re_component_ui/src/lib.rs
+++ crates/viewer/re_component_ui/src/lib.rs
@@ -9,6 +9,7 @@ mod entity_path;
 mod fallback_ui;
 mod geo_line_string;
 mod image_format;
+mod lat_lon;
 mod line_strip;
 mod map_provider;
 mod marker_shape;
@@ -162,6 +163,8 @@ pub fn create_component_ui_registry() -> re_viewer_context::ComponentUiRegistry 
     registry.add_singleline_edit_or_view(entity_path::edit_or_view_entity_path);
 
     registry.add_singleline_edit_or_view(video_timestamp::edit_or_view_timestamp);
+
+    registry.add_singleline_edit_or_view(lat_lon::singleline_view_lat_lon);
 
     registry
 }
