595c331c4d814badbd1f8efb8e03a6dba4fc19ff
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2024-11-04 17:41:55 +0100

    Update rerun_cpp/src/rerun/archetypes/geo_points_ext.cpp
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- rerun_cpp/src/rerun/archetypes/geo_points_ext.cpp
+++ rerun_cpp/src/rerun/archetypes/geo_points_ext.cpp
@@ -8,7 +8,7 @@ namespace rerun {
 #ifdef EDIT_EXTENSION
         // <CODEGEN_COPY_TO_HEADER>
 
-        /// Creates a new GeoPoints object based on EPSG:4326 latitude and longitude (North/East-positive degrees).
+        /// Creates a new GeoPoints object based on [EPSG:4326](https://epsg.io/4326) latitude and longitude (North/East-positive degrees).
         static GeoPoints from_lat_lon(Collection<components::LatLon> positions_) {
             GeoPoints points;
             points.positions = std::move(positions_);
