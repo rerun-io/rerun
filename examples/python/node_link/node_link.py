47c20a5f74aa85f669bcb85a4471d3d89ab199a2
Jochen Görtler <me@jgoertler.com>
2024-11-04 11:54:58 +0100

    WIP: improve Python example

--- examples/python/node_link/node_link.py
+++ examples/python/node_link/node_link.py
@@ -11,33 +11,43 @@ s = 3  # scaling factor for the positions
 # :warning: The nodes have to be unique, which is why we use `5_0`…
 
 NodeId = str
-NodeInfo = dict[NodeId, str | tuple[float, float]]
 
-nodes: dict[NodeId, NodeInfo] = {
-    "1": {"label": "1", "pos": (0 * s, 0 * s)},
-    "7": {"label": "7", "pos": (-20 * s, 30 * s)},
-    "2": {"label": "2", "pos": (-30 * s, 60 * s)},
-    "6": {"label": "6", "pos": (-10 * s, 60 * s)},
-    "5_0": {"label": "5", "pos": (-20 * s, 90 * s)},
-    "11": {"label": "11", "pos": (0 * s, 90 * s)},
-    "9_0": {"label": "9", "pos": (20 * s, 30 * s)},
-    "9_1": {"label": "9", "pos": (30 * s, 60 * s)},
-    "5_1": {"label": "5", "pos": (20 * s, 90 * s)},
+
+class NodeInfo:
+    def __init__(self, label: str, pos: tuple[float, float]) -> None:
+        self.label = label
+        self.pos = pos
+
+
+all_nodes: dict[NodeId, NodeInfo] = {
+    "1": NodeInfo(label="1", pos=(0 * s, 0 * s)),
+    "7": NodeInfo(label="7", pos=(-20 * s, 30 * s)),
+    "2": NodeInfo(label="2", pos=(-30 * s, 60 * s)),
+    "6": NodeInfo(label="6", pos=(-10 * s, 60 * s)),
+    "5_0": NodeInfo(label="5", pos=(-20 * s, 90 * s)),
+    "11": NodeInfo(label="11", pos=(0 * s, 90 * s)),
+    "9_0": NodeInfo(label="9", pos=(20 * s, 30 * s)),
+    "9_1": NodeInfo(label="9", pos=(30 * s, 60 * s)),
+    "5_1": NodeInfo(label="5", pos=(20 * s, 90 * s)),
 }
 
-LevelInfo = dict[str, list[NodeId] | list[tuple[NodeId, NodeId]]]
-Levels = list[LevelInfo]
 
-levels: Levels = [
-    {"nodes": ["1"], "edges": []},
-    {"nodes": ["1", "7", "9_0"], "edges": [("1", "7"), ("1", "9_0")]},
-    {
-        "nodes": ["1", "7", "9_0", "2", "6", "9_1"],
-        "edges": [("1", "7"), ("1", "9_0"), ("7", "2"), ("7", "6"), ("9_0", "9_1")],
-    },
-    {
-        "nodes": ["1", "7", "9_0", "2", "6", "9_1", "5_0", "11", "5_1"],
-        "edges": [
+class Level:
+    def __init__(self, nodes: list[NodeId], edges: list[tuple[NodeId, NodeId]]):
+        self.nodes = nodes
+        self.edges = edges
+
+
+levels: list[Level] = [
+    Level(nodes=["1"], edges=[]),
+    Level(nodes=["1", "7", "9_0"], edges=[("1", "7"), ("1", "9_0")]),
+    Level(
+        nodes=["1", "7", "9_0", "2", "6", "9_1"],
+        edges=[("1", "7"), ("1", "9_0"), ("7", "2"), ("7", "6"), ("9_0", "9_1")],
+    ),
+    Level(
+        nodes=["1", "7", "9_0", "2", "6", "9_1", "5_0", "11", "5_1"],
+        edges=[
             ("1", "7"),
             ("1", "9_0"),
             ("7", "2"),
@@ -47,7 +57,7 @@ levels: Levels = [
             ("6", "11"),
             ("9_1", "5_1"),
         ],
-    },
+    ),
 ]
 
 
@@ -56,22 +66,22 @@ def main() -> None:
 
     t = 0
     for level in levels:
-        if len(level["nodes"]) > 0:
+        if len(level.nodes) > 0:
             t = t + 1
             rr.set_time_seconds("stable_time", t)
             rr.log(
                 "binary_tree",
                 rr.GraphNodes(
-                    level["nodes"],
-                    labels=list(map(lambda n: nodes[n]["label"], level["nodes"])),
-                    positions=list(map(lambda n: nodes[n]["pos"], level["nodes"])),
+                    level.nodes,
+                    labels=list(map(lambda n: all_nodes[n].label, level.nodes)),
+                    positions=list(map(lambda n: all_nodes[n].pos, level.nodes)),
                 ),
             )
 
-        if len(level["edges"]) > 0:
+        if len(level.edges) > 0:
             t = t + 1
             rr.set_time_seconds("stable_time", t)
-            rr.log("binary_tree", rr.GraphEdges(level["edges"]))
+            rr.log("binary_tree", rr.GraphEdges(level.edges))
 
 
 if __name__ == "__main__":
