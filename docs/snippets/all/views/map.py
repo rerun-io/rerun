8e888e7879ebda48610dff79568b4a78dc2b808d
Antoine Beyeler <antoine@rerun.io>
2024-11-07 11:28:38 +0100

    Add view snippet screenshot

--- docs/snippets/all/views/map.py
+++ docs/snippets/all/views/map.py
@@ -5,7 +5,7 @@ import rerun.blueprint as rrb
 
 rr.init("rerun_example_map_view", spawn=True)
 
-rr.log("points", rr.GeoPoints([[47.6344, 19.1397], [47.6334, 19.1399]]))
+rr.log("points", rr.GeoPoints([[47.6344, 19.1397], [47.6334, 19.1399]], radii=rr.Radius.ui_points(20.0)))
 
 # Create a map view to display the chart.
 blueprint = rrb.Blueprint(
--- rerun_py/rerun_sdk/rerun/blueprint/views/map_view.py
+++ rerun_py/rerun_sdk/rerun/blueprint/views/map_view.py
@@ -28,7 +28,7 @@ class MapView(SpaceView):
 
     rr.init("rerun_example_map_view", spawn=True)
 
-    rr.log("points", rr.GeoPoints([[47.6344, 19.1397], [47.6334, 19.1399]]))
+    rr.log("points", rr.GeoPoints([[47.6344, 19.1397], [47.6334, 19.1399]], radii=rr.Radius.ui_points(20.0)))
 
     # Create a map view to display the chart.
     blueprint = rrb.Blueprint(
@@ -43,6 +43,15 @@ class MapView(SpaceView):
 
     rr.send_blueprint(blueprint)
     ```
+    <center>
+    <picture>
+      <source media="(max-width: 480px)" srcset="https://static.rerun.io/map-view/8b2cc15e3f3313e2181667f6c29816ebe75e16e6/480w.png">
+      <source media="(max-width: 768px)" srcset="https://static.rerun.io/map-view/8b2cc15e3f3313e2181667f6c29816ebe75e16e6/768w.png">
+      <source media="(max-width: 1024px)" srcset="https://static.rerun.io/map-view/8b2cc15e3f3313e2181667f6c29816ebe75e16e6/1024w.png">
+      <source media="(max-width: 1200px)" srcset="https://static.rerun.io/map-view/8b2cc15e3f3313e2181667f6c29816ebe75e16e6/1200w.png">
+      <img src="https://static.rerun.io/map-view/8b2cc15e3f3313e2181667f6c29816ebe75e16e6/full.png" width="640">
+    </picture>
+    </center>
 
     """
 
