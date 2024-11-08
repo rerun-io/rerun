182aac8249fa9a07fa02c3ab9a24d1333692867a
Andreas Reich <r_andreas2@web.de>
2024-11-08 11:05:51 +0100

    doc string issues

--- crates/store/re_video/src/demux/mod.rs
+++ crates/store/re_video/src/demux/mod.rs
@@ -278,7 +278,7 @@ impl VideoData {
     ///
     /// Returned timestamps are in nanoseconds since start and are guaranteed to be monotonically increasing.
     /// These are *not* necessarily the same as the presentation timestamps, as the returned timestamps are
-    /// normalized respect to the start of the video, see [`SampleStatistics::minimum_presentation_timestamp`].
+    /// normalized respect to the start of the video, see [`SamplesStatistics::minimum_presentation_timestamp`].
     pub fn frame_timestamps_ns(&self) -> impl Iterator<Item = i64> + '_ {
         // Segments are guaranteed to be sorted among each other, but within a segment,
         // presentation timestamps may not be sorted since this is sorted by decode timestamps.
