2fccc54bf08019cba7d5ab535159bc7c1754e470
Clement Rey <cr.rey.clement@gmail.com>
2024-11-15 10:23:41 +0100

    document TUID name shenaniganeries

--- crates/utils/re_tuid/src/lib.rs
+++ crates/utils/re_tuid/src/lib.rs
@@ -18,6 +18,9 @@ pub struct Tuid {
 }
 
 impl Tuid {
+    /// We give an actual name to [`Tuid`], and inject that name into the Arrow datatype extensions,
+    /// as a hack so that we can compactly format them when printing Arrow data to the terminal.
+    /// Check out `re_format_arrow` for context.
     pub const NAME: &'static str = "rerun.datatypes.TUID";
 
     /// Returns the total size of `self` on the heap, in bytes.
