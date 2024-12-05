c56d4b1a44986692a0e0033a347b6de6de80964a
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-12-05 08:03:43 +0100

    Handle projections

--- crates/viewer/re_selection_panel/src/item_heading_with_breadcrumbs.rs
+++ crates/viewer/re_selection_panel/src/item_heading_with_breadcrumbs.rs
@@ -13,7 +13,7 @@ use re_chunk::EntityPath;
 use re_data_ui::item_ui::cursor_interact_with_selectable;
 use re_entity_db::InstancePath;
 use re_log_types::EntityPathPart;
-use re_ui::{icons, list_item, DesignTokens, UiExt as _};
+use re_ui::{icons, list_item, DesignTokens, SyntaxHighlighting, UiExt as _};
 use re_viewer_context::{Contents, Item, SpaceViewId, ViewerContext};
 use re_viewport_blueprint::ViewportBlueprint;
 
@@ -86,15 +86,22 @@ fn item_heading_contents(
                 if instance.is_all() {
                     // Entity path
                     if let [ancestry @ .., _] = entity_path.as_slice() {
-                        entity_path_breadcrumbs(ctx, ui, None, None, ancestry);
+                        entity_path_breadcrumbs(ctx, ui, None, None, ancestry, true);
                     }
                 } else {
                     // Instance path
-                    entity_path_breadcrumbs(ctx, ui, None, None, entity_path.as_slice());
+                    entity_path_breadcrumbs(ctx, ui, None, None, entity_path.as_slice(), true);
                 }
             }
             Item::ComponentPath(component_path) => {
-                entity_path_breadcrumbs(ctx, ui, None, None, component_path.entity_path.as_slice());
+                entity_path_breadcrumbs(
+                    ctx,
+                    ui,
+                    None,
+                    None,
+                    component_path.entity_path.as_slice(),
+                    true,
+                );
             }
             Item::Container(container_id) => {
                 if let Some(parent) = viewport.parent(&Contents::Container(*container_id)) {
@@ -119,7 +126,7 @@ fn item_heading_contents(
                         let relative = &entity_path.as_slice()[view.space_origin.len()..];
 
                         if instance.is_all() {
-                            // we will show last part in full, later
+                            // we will show last part in full later
                             if let [all_but_last @ .., _] = relative {
                                 entity_path_breadcrumbs(
                                     ctx,
@@ -127,19 +134,50 @@ fn item_heading_contents(
                                     Some(*view_id),
                                     Some(&view.space_origin),
                                     all_but_last,
+                                    true,
                                 );
                             }
                         } else {
+                            // we show the instance number later
                             entity_path_breadcrumbs(
                                 ctx,
                                 ui,
                                 Some(*view_id),
                                 Some(&view.space_origin),
                                 relative,
+                                true,
                             );
                         }
                     } else {
-                        ui.label("TODO"); // TODO
+                        // Projections
+                        let common_ancestor = instance_path
+                            .entity_path
+                            .common_ancestor(&view.space_origin);
+                        let relative = &entity_path.as_slice()[common_ancestor.len()..];
+
+                        if instance.is_all() {
+                            // we will show last part in full later
+                            if let [all_but_last @ .., _] = relative {
+                                entity_path_breadcrumbs(
+                                    ctx,
+                                    ui,
+                                    Some(*view_id),
+                                    Some(&common_ancestor),
+                                    all_but_last,
+                                    false,
+                                );
+                            }
+                        } else {
+                            // we show the instance number later
+                            entity_path_breadcrumbs(
+                                ctx,
+                                ui,
+                                Some(*view_id),
+                                Some(&common_ancestor),
+                                relative,
+                                false,
+                            );
+                        }
                     }
                 }
             }
@@ -172,29 +210,36 @@ fn entity_path_breadcrumbs(
     origin: Option<&EntityPath>,
     // Show crumbs for all of these
     entity_parts: &[EntityPathPart],
+    include_root: bool,
 ) {
-    // Match on everything plus last
-    let button = match entity_parts {
-        [ancestry @ .., last] => {
-            // Recurse!
-            entity_path_breadcrumbs(ctx, ui, view_id, origin, ancestry);
+    if let [ancestry @ .., _] = entity_parts {
+        // Recurse!
 
-            let first_char = last.unescaped_str().chars().next().unwrap_or('?');
-            egui::Button::new(first_char.to_string()).image_tint_follows_text_color(true)
+        if !ancestry.is_empty() || include_root {
+            entity_path_breadcrumbs(ctx, ui, view_id, origin, ancestry, include_root);
         }
-        _ => {
-            // Root
-            egui::Button::image(icons::RECORDING.as_image().fit_to_original_size(ICON_SCALE))
-                .image_tint_follows_text_color(true)
-        }
-    };
+    }
 
     let full_entity_path = if let Some(origin) = origin {
         origin.join(&EntityPath::new(entity_parts.to_vec()))
     } else {
         EntityPath::new(entity_parts.to_vec())
     };
-    let response = ui.add(button).on_hover_text(full_entity_path.to_string());
+
+    let button = if let Some(last) = full_entity_path.last() {
+        let first_char = last.unescaped_str().chars().next().unwrap_or('?');
+        egui::Button::new(first_char.to_string())
+    } else {
+        // Root
+        // TODO: different icon, at least if we are in a space view
+        egui::Button::image(icons::RECORDING.as_image().fit_to_original_size(ICON_SCALE))
+            .image_tint_follows_text_color(true)
+    };
+
+    let response = ui.add(button);
+    let response = response.on_hover_ui(|ui| {
+        ui.label(full_entity_path.syntax_highlighted(ui.style()));
+    });
 
     let item = if let Some(view_id) = view_id {
         Item::DataResult(view_id, full_entity_path.into())
