ca029301ccb4946899d63e3550d8581949baaf6c
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-12-09 12:28:06 +0100

    Add Archetype::from_arrow

--- crates/store/re_types_core/src/archetype.rs
+++ crates/store/re_types_core/src/archetype.rs
@@ -101,6 +101,24 @@ pub trait Archetype {
     /// Arrow arrays that are unknown to this [`Archetype`] will simply be ignored and a warning
     /// logged to stderr.
     #[inline]
+    fn from_arrow(
+        data: impl IntoIterator<Item = (arrow2::datatypes::Field, ::arrow::array::ArrayRef)>,
+    ) -> DeserializationResult<Self>
+    where
+        Self: Sized,
+    {
+        Self::from_arrow_components(
+            data.into_iter()
+                .map(|(field, array)| (field.name.into(), array)),
+        )
+    }
+
+    /// Given an iterator of Arrow arrays and their respective field metadata, deserializes them
+    /// into this archetype.
+    ///
+    /// Arrow arrays that are unknown to this [`Archetype`] will simply be ignored and a warning
+    /// logged to stderr.
+    #[inline]
     fn from_arrow2(
         data: impl IntoIterator<Item = (arrow2::datatypes::Field, Box<dyn ::arrow2::array::Array>)>,
     ) -> DeserializationResult<Self>
