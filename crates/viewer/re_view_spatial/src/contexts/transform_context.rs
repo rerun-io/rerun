4e3e24a320c841cb84f4f987bde3bef3972c50e2
Andreas Reich <andreas@rerun.io>
2024-12-11 13:59:42 +0100

    New `PerStoreStoreSubscriber` wrapper to remove store tracking boilerplate (#8400)
    Most store subscribers don't need a view over all stores. This removes
    this indirection from a whole bunch of store subscriber implementors and
    moves it into a utility.

--- crates/viewer/re_view_spatial/src/contexts/transform_context.rs
+++ crates/viewer/re_view_spatial/src/contexts/transform_context.rs
@@ -17,7 +17,8 @@ use re_viewer_context::{IdentifiedViewSystem, ViewContext, ViewContextSystem};
 use vec1::smallvec_v1::SmallVec1;
 
 use crate::{
-    transform_component_tracker::TransformComponentTracker, visualizers::image_view_coordinates,
+    transform_component_tracker::TransformComponentTrackerStoreSubscriber,
+    visualizers::image_view_coordinates,
 };
 
 #[derive(Clone, Debug)]
@@ -736,7 +737,7 @@ fn transforms_at(
     re_tracing::profile_function!();
 
     let potential_transform_components =
-        TransformComponentTracker::access(&entity_db.store_id(), |tracker| {
+        TransformComponentTrackerStoreSubscriber::access(&entity_db.store_id(), |tracker| {
             tracker.potential_transform_components(entity_path).cloned()
         })
         .flatten()
