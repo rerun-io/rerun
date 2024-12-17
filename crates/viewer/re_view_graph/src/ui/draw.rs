rerun-io/rerun

commit a7acf152786421cd171d81226c38a01cb2d7a95f

Add missing click and hover interactions for views (#8495)
### Related

<!--
Include links to any related issues/PRs in a bulleted list, for example:
* Closes #1234
* Part of #1337
-->

* Closes #8479
* Closes #5138

### What

This adds missing view interactions the following views:

* Map view
* Graph view
* TextDocument view

@Wumpf @abey79 I feel like the pattern repeats itself and it's easy to
miss when writing new views. So I wonder if this could be unified. I see
several options for this:

* repurpose `handle_select_hover_drag_interactions` which only seems to
be used for `DataResult` right now
* Making each view return a `Result<(Response,
ViewSystemExecutionError>` (instead of `()`) and handling it outside of
the view.
* Creating a function analogous to
`handle_select_hover_drag_interactions`.

<!--
Make sure the PR title and labels are set to maximize their usefulness
for the CHANGELOG,
and our `git log`.

If you have noticed any breaking changes, include them in the migration
guide.

We track various metrics at <https://build.rerun.io>.

For maintainers:
* To run all checks from `main`, comment on the PR with `@rerun-bot
full-check`.
* To deploy documentation changes immediately after merging this PR, add
the `deploy docs` label.
-->

crates/viewer/re_view_graph/src/view.rs
@@ -206,6 +206,10 @@ Display a graph of nodes and edges.
             }
         });
 
+        if resp.hovered() {
+            ctx.selection_state().set_hovered(Item::View(query.view_id));
+        }
+
         if resp.clicked() {
             // clicked elsewhere, select the view
             ctx.selection_state()

crates/viewer/re_view_map/src/map_view.rs
@@ -499,6 +499,8 @@ fn handle_ui_interactions(
         // clicked elsewhere, select the view
         ctx.selection_state()
             .set_selection(Item::View(query.view_id));
+    } else if map_response.hovered() {
+        ctx.selection_state().set_hovered(Item::View(query.view_id));
     }
 }
 

crates/viewer/re_view_text_document/src/view_class.rs
@@ -1,10 +1,12 @@
 use egui::Label;
 
+use egui::Sense;
 use re_types::View;
 use re_types::ViewClassIdentifier;
 use re_ui::UiExt as _;
 use re_view::suggest_view_for_each_entity;
 
+use re_viewer_context::Item;
 use re_viewer_context::{
     external::re_log_types::EntityPath, ViewClass, ViewClassRegistryError, ViewId, ViewQuery,
     ViewState, ViewStateExt as _, ViewSystemExecutionError, ViewerContext,
@@ -111,17 +113,16 @@ Displays text from a text component, as raw text or markdown."
 
     fn ui(
         &self,
-        _ctx: &ViewerContext<'_>,
+        ctx: &ViewerContext<'_>,
         ui: &mut egui::Ui,
         state: &mut dyn ViewState,
-
-        _query: &ViewQuery<'_>,
+        query: &ViewQuery<'_>,
         system_output: re_viewer_context::SystemExecutionOutput,
     ) -> Result<(), ViewSystemExecutionError> {
         let state = state.downcast_mut::<TextDocumentViewState>()?;
         let text_document = system_output.view_systems.get::<TextDocumentSystem>()?;
 
-        egui::Frame {
+        let response = egui::Frame {
             inner_margin: re_ui::DesignTokens::view_padding().into(),
             ..egui::Frame::default()
         }
@@ -176,7 +177,18 @@ Displays text from a text component, as raw text or markdown."
                     })
             })
             .response
-        });
+        })
+        .response
+        .interact(Sense::click());
+
+        if response.hovered() {
+            ctx.selection_state().set_hovered(Item::View(query.view_id));
+        }
+
+        if response.clicked() {
+            ctx.selection_state()
+                .set_selection(Item::View(query.view_id));
+        }
 
         Ok(())
     }

tests/python/release_checklist/check_hover_select_reset.py
@@ -29,6 +29,13 @@ For each of the views:
     * If you think this is unexpected, create an issue.
     * Double-click the entity and verify that it becomes selected and highlighted in the blueprint tree.
 
+### Graph Select
+Should work just as 2D/3D views.
+
+### Text view
+Clicking on a text view (what you're reading right now) should select the view.
+Hovering the view should work as well.
+
 ### Reset
 For each of the views:
 * Zoom and/or pan the view
@@ -83,6 +90,14 @@ def log_points_2d() -> None:
     rr.log("2D/points", rr.Points2D(positions, colors=colors, radii=radii))
 
 
+def log_graph() -> None:
+    rr.log("graph", rr.GraphNodes(["a", "b"], labels=["A", "B"]))
+
+
+def log_map() -> None:
+    rr.log("points", rr.GeoPoints(lat_lon=[[47.6344, 19.1397], [47.6334, 19.1399]], radii=rr.Radius.ui_points(20.0)))
+
+
 def run(args: Namespace) -> None:
     rr.script_setup(args, f"{os.path.basename(__file__)}", recording_id=uuid4())
 
@@ -90,6 +105,8 @@ def run(args: Namespace) -> None:
     log_plots()
     log_points_3d()
     log_points_2d()
+    log_graph()
+    log_map()
 
 
 if __name__ == "__main__":

commit 06fed3db111fe3bd958e2cd4a9bcbeddf1d037fe

Fix graph view node highlight on hover (#8512)
### What

This fixes a problem introduced by #8495 that was only partially fixed
by #8508, with graph view highlights flickering (ping-ponging) in the
blueprint view.

crates/viewer/re_view_graph/src/ui/draw.rs
@@ -330,6 +330,7 @@ pub fn draw_graph(
     layout: &Layout,
     query: &ViewQuery<'_>,
     lod: LevelOfDetail,
+    hover_click_item: &mut Option<(Item, Response)>,
 ) -> Rect {
     let entity_path = graph.entity();
     let entity_highlights = query.highlights.entity_highlight(entity_path.hash());
@@ -363,18 +364,17 @@ pub fn draw_graph(
                     });
                 });
 
-                ctx.handle_select_hover_drag_interactions(
-                    &response,
-                    Item::DataResult(query.view_id, instance_path.clone()),
-                    false,
-                );
-
-                // double click selects the entire entity
+                // Warning! The order is very important here.
                 if response.double_clicked() {
                     // Select the entire entity
                     ctx.selection_state().set_selection(Item::DataResult(
                         query.view_id,
                         instance_path.entity_path.clone().into(),
+                    ));
+                } else if response.hovered() || response.clicked() {
+                    *hover_click_item = Some((
+                        Item::DataResult(query.view_id, instance_path.clone()),
+                        response.clone(),
                     ));
                 }
 

crates/viewer/re_view_graph/src/view.rs
@@ -1,3 +1,4 @@
+use egui::Response;
 use re_log_types::EntityPath;
 use re_types::{
     blueprint::{
@@ -191,18 +192,28 @@ Display a graph of nodes and edges.
 
         let level_of_detail = LevelOfDetail::from_scaling(ui_from_world.scaling);
 
+        let mut hover_click_item: Option<(Item, Response)> = None;
+
         let resp = zoom_pan_area(ui, &mut ui_from_world, |ui| {
             let mut world_bounding_rect = egui::Rect::NOTHING;
 
             for graph in &graphs {
-                let graph_rect = draw_graph(ui, ctx, graph, layout, query, level_of_detail);
+                let graph_rect = draw_graph(
+                    ui,
+                    ctx,
+                    graph,
+                    layout,
+                    query,
+                    level_of_detail,
+                    &mut hover_click_item,
+                );
                 world_bounding_rect = world_bounding_rect.union(graph_rect);
             }
         });
 
-        // Don't set the view to hovered if something else was already hovered.
-        // (this can only mean that a graph node/edge was hovered)
-        if resp.hovered() && ctx.selection_state().hovered_items().is_empty() {
+        if let Some((item, response)) = hover_click_item {
+            ctx.handle_select_hover_drag_interactions(&response, item, false);
+        } else if resp.hovered() {
             ctx.selection_state().set_hovered(Item::View(query.view_id));
         }
 