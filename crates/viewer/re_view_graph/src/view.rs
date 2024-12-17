411e1cc51483467f277e584f1ef402b5d93eb68b
Jochen Görtler <me@jgoertler.com>
2024-12-17 17:45:35 +0100

    Fix graph view node selection bug

--- crates/viewer/re_view_graph/src/view.rs
+++ crates/viewer/re_view_graph/src/view.rs
@@ -1,3 +1,4 @@
+use egui::Response;
 use re_log_types::EntityPath;
 use re_types::{
     blueprint::{
@@ -191,20 +192,27 @@ Display a graph of nodes and edges.
 
         let level_of_detail = LevelOfDetail::from_scaling(ui_from_world.scaling);
 
-        let mut hovered: Option<Item> = None;
+        let mut hover_click_item: Option<(Item, Response)> = None;
 
         let resp = zoom_pan_area(ui, &mut ui_from_world, |ui| {
             let mut world_bounding_rect = egui::Rect::NOTHING;
 
             for graph in &graphs {
-                let graph_rect =
-                    draw_graph(ui, ctx, graph, layout, query, level_of_detail, &mut hovered);
+                let graph_rect = draw_graph(
+                    ui,
+                    ctx,
+                    graph,
+                    layout,
+                    query,
+                    level_of_detail,
+                    &mut hover_click_item,
+                );
                 world_bounding_rect = world_bounding_rect.union(graph_rect);
             }
         });
 
-        if let Some(hovered_item) = hovered {
-            ctx.selection_state().set_hovered(hovered_item);
+        if let Some((item, response)) = hover_click_item {
+            ctx.handle_select_hover_drag_interactions(&response, item, false);
         } else if resp.hovered() {
             ctx.selection_state().set_hovered(Item::View(query.view_id));
         }
