e7ab60dd9f77763b0f7b51411ea7fb320ef56659
Clement Rey <cr.rey.clement@gmail.com>
2024-12-19 11:00:49 +0100

    wip

---
+++ crates/store/re_compute_graph/src/bin/main.rs
@@ -0,0 +1,14 @@
+pub trait ComputeNode {}
+
+// TODO:
+// * materialize AggregatedLatestAt
+// * materialize AggregatedRange
+// * materialize Dataframe
+// * materialize the stuff in `Caches`:
+//   * JPEG decoding
+//   * Mesh decoding
+//   * What else?
+
+fn main() {
+    eprintln!("sup boi");
+}
