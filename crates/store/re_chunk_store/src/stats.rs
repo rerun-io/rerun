cb30c0d24e576c845e182c17674306973ee68cb0
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2024-12-19 14:41:01 +0100

    cargo fmt

--- crates/store/re_chunk_store/src/stats.rs
+++ crates/store/re_chunk_store/src/stats.rs
@@ -1,7 +1,7 @@
 use std::sync::{atomic::Ordering, Arc};
 
-use re_chunk::{Chunk, ComponentName, EntityPath, Timeline};
 use re_byte_size::SizeBytes;
+use re_chunk::{Chunk, ComponentName, EntityPath, Timeline};
 
 use crate::ChunkStore;
 
