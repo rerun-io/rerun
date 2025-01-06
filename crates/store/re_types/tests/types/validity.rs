5f77d7bb8bfaca4964dc540f010dca0ede6ee577
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-01-06 14:50:37 +0100

    When checking error type, remove any context

--- crates/store/re_types/tests/types/validity.rs
+++ crates/store/re_types/tests/types/validity.rs
@@ -31,7 +31,7 @@ fn validity_checks() {
     let serialized = Position2D::to_arrow2_opt(bad).unwrap();
     let deserialized = Position2D::from_arrow2(serialized.as_ref());
     assert!(deserialized.is_err());
-    let actual_error = deserialized.err().unwrap();
+    let actual_error = deserialized.err().unwrap().without_context();
     assert!(
         matches!(actual_error, DeserializationError::MissingData { .. }),
         "Expected error MissingData, got {actual_error:?}",
