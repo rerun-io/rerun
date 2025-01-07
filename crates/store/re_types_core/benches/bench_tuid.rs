026b3107a8006ea0c88242280ff1901de69c2924
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-01-07 20:13:04 +0100

    Benchmark arrow1

--- crates/store/re_types_core/benches/bench_tuid.rs
+++ crates/store/re_types_core/benches/bench_tuid.rs
@@ -4,7 +4,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn bench_arrow(c: &mut Criterion) {
-    use arrow2::array::Array;
     use re_types_core::Loggable as _;
 
     for elem_count in [1, 1000] {
@@ -14,9 +13,9 @@ fn bench_arrow(c: &mut Criterion) {
 
             let tuids = vec![re_tuid::Tuid::new(); elem_count as usize];
 
-            group.bench_function("arrow2", |b| {
+            group.bench_function("arrow", |b| {
                 b.iter(|| {
-                    let data: Box<dyn Array> = re_tuid::Tuid::to_arrow2(tuids.clone()).unwrap();
+                    let data = re_tuid::Tuid::to_arrow(tuids.clone()).unwrap();
                     criterion::black_box(data)
                 });
             });
@@ -26,12 +25,12 @@ fn bench_arrow(c: &mut Criterion) {
             let mut group = c.benchmark_group(format!("arrow/deserialize/elem_count={elem_count}"));
             group.throughput(criterion::Throughput::Elements(elem_count));
 
-            let data: Box<dyn Array> =
-                re_tuid::Tuid::to_arrow2(vec![re_tuid::Tuid::new(); elem_count as usize]).unwrap();
+            let data =
+                re_tuid::Tuid::to_arrow(vec![re_tuid::Tuid::new(); elem_count as usize]).unwrap();
 
-            group.bench_function("arrow2", |b| {
+            group.bench_function("arrow", |b| {
                 b.iter(|| {
-                    let tuids = re_tuid::Tuid::from_arrow2(data.as_ref()).unwrap();
+                    let tuids = re_tuid::Tuid::from_arrow(data.as_ref()).unwrap();
                     criterion::black_box(tuids)
                 });
             });
