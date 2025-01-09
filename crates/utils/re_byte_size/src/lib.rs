7f5708e942976278d8964a58b9f6adfad64d4ddb
jprochazk <pjanv42@gmail.com>
2025-01-09 16:05:22 +0100

    Add todo

--- crates/utils/re_byte_size/src/lib.rs
+++ crates/utils/re_byte_size/src/lib.rs
@@ -12,6 +12,7 @@ mod tuple_sizes;
 /// Approximations of stack and heap size for both internal and external types.
 ///
 /// Motly used for statistics and triggering events such as garbage collection.
+// TODO(#8630): Derive macro for this trait.
 pub trait SizeBytes {
     /// Returns the total size of `self` in bytes, accounting for both stack and heap space.
     #[inline]
