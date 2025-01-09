574637d2e5b98081190dc47a502fab8b674c3836
Zeljko Mihaljcic <7150613+zehiko@users.noreply.github.com>
2025-01-09 08:23:31 +0100

    improve ergonomics of encoding and decoding data (using arrow IPC) (#8614)

--- crates/store/re_log_encoding/src/codec/wire/encoder.rs
+++ crates/store/re_log_encoding/src/codec/wire/encoder.rs
@@ -1,9 +1,11 @@
 use crate::codec::arrow::write_arrow_to_bytes;
 use crate::codec::CodecError;
 use re_chunk::TransportChunk;
+use re_protos::common::v0::RerunChunk;
+use re_protos::remote_store::v0::DataframePart;
 
 /// Encode a transport chunk into a byte stream.
-pub fn encode(
+fn encode(
     version: re_protos::common::v0::EncoderVersion,
     chunk: &TransportChunk,
 ) -> Result<Vec<u8>, CodecError> {
@@ -14,5 +16,30 @@ pub fn encode(
 
             Ok(data)
         }
+    }
+}
+
+/// Encode an object into a wire (protobuf) type.
+pub trait Encode<O> {
+    fn encode(&self) -> Result<O, CodecError>;
+}
+
+impl Encode<DataframePart> for TransportChunk {
+    fn encode(&self) -> Result<DataframePart, CodecError> {
+        let payload = encode(re_protos::common::v0::EncoderVersion::V0, self)?;
+        Ok(DataframePart {
+            encoder_version: re_protos::common::v0::EncoderVersion::V0 as i32,
+            payload,
+        })
+    }
+}
+
+impl Encode<RerunChunk> for TransportChunk {
+    fn encode(&self) -> Result<RerunChunk, CodecError> {
+        let payload = encode(re_protos::common::v0::EncoderVersion::V0, self)?;
+        Ok(RerunChunk {
+            encoder_version: re_protos::common::v0::EncoderVersion::V0 as i32,
+            payload,
+        })
     }
 }
