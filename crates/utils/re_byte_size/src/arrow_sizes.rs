d8b7d8f877019ef405bce7d0719785813fadd3f9
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-01-13 11:25:12 +0100

    Remove `impl SizeBytes for &dyn Array`

--- crates/utils/re_byte_size/src/arrow_sizes.rs
+++ crates/utils/re_byte_size/src/arrow_sizes.rs
@@ -13,13 +13,6 @@ impl SizeBytes for dyn Array {
     }
 }
 
-impl SizeBytes for &dyn Array {
-    #[inline]
-    fn heap_size_bytes(&self) -> u64 {
-        self.get_array_memory_size() as u64
-    }
-}
-
 impl<T: Array> SizeBytes for &T {
     #[inline]
     fn heap_size_bytes(&self) -> u64 {
