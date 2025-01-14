b2100393dc683231c65cafb65faff0f525c99b6c
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-01-14 13:42:04 +0100

    Don't use arrow for IPC serialize

--- crates/store/re_log_types/src/arrow_msg.rs
+++ crates/store/re_log_types/src/arrow_msg.rs
@@ -10,7 +10,7 @@ use arrow::array::RecordBatch as ArrowRecordBatch;
 use crate::TimePoint;
 
 // TODO(#3741): Remove once <https://github.com/apache/arrow-rs/issues/6803> is released
-const SERIALIZE_WITH_ARROW_1: bool = true; // We can use arrow1 here, because we don't encounter the above bug in this context
+const SERIALIZE_WITH_ARROW_1: bool = false;
 const DESERIALIZE_WITH_ARROW_1: bool = true; // Both arrow1 and arrow2 should be working fine
 
 /// An arbitrary callback to be run when an [`ArrowMsg`], and more specifically the
