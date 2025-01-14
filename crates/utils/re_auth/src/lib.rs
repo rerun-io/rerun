df46ecd751d3e24d7610bd72e31fd80cc8b2a2a6
Jochen Görtler <me@jgoertler.com>
2025-01-14 13:55:18 +0100

    doc

--- crates/utils/re_auth/src/lib.rs
+++ crates/utils/re_auth/src/lib.rs
@@ -1,3 +1,12 @@
+//! Basic authentication helpers for Rerun.
+//!
+//! Currently, this crate provides a simple JWT-based authentication scheme on
+//! top of a rudimentary [`RedapProvider`] that uses a symmetric key to _both_
+//! generate and sign tokens.
+//!
+//! **Warning!** This approach should only be seen as a stop-gap until we have
+//! integration of _real_ identity-providers, most likely based on OpenID Connect.
+
 pub use error::Error;
 pub use provider::{Claims, RedapProvider, VerificationOptions};
 pub use service::*;
