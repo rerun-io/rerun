eb96124b037ee95faba624fc646ed46b2d0fd6e7
Clement Rey <cr.rey.clement@gmail.com>
2025-01-14 18:48:42 +0100

    update transform3d partial updates python snippet

--- docs/snippets/all/archetypes/transform3d_partial_updates.py
+++ docs/snippets/all/archetypes/transform3d_partial_updates.py
@@ -14,40 +14,36 @@ rr.init("rerun_example_transform3d_partial_updates", spawn=True)
 rr.log(
     "box",
     rr.Boxes3D(half_sizes=[4.0, 2.0, 1.0], fill_mode=rr.components.FillMode.Solid),
-    rr.Transform3D(axis_length=10),
+    rr.Transform3D(clear=False, axis_length=10),
 )
 
 for deg in range(46):
     rad = truncated_radians(deg * 4)
-    # TODO(#8582): update_fields
     rr.log(
         "box",
-        rr.Transform3D(
+        rr.Transform3D.update_fields(
             # TODO(cmc): we should have access to all the fields of the extended constructor too.
             rotation_axis_angle=rr.RotationAxisAngle(axis=[0.0, 1.0, 0.0], radians=rad),
         ),
     )
 
 for t in range(51):
-    # TODO(#8582): update_fields
     rr.log(
         "box",
-        rr.Transform3D(translation=[0, 0, t / 10.0]),
+        rr.Transform3D.update_fields(translation=[0, 0, t / 10.0]),
     )
 
 for deg in range(46):
     rad = truncated_radians((deg + 45) * 4)
-    # TODO(#8582): update_fields
     rr.log(
         "box",
-        rr.Transform3D(
+        rr.Transform3D.update_fields(
             # TODO(cmc): we should have access to all the fields of the extended constructor too.
             rotation_axis_angle=rr.RotationAxisAngle(axis=[0.0, 1.0, 0.0], radians=rad),
         ),
     )
 
-# TODO(#8582): update_fields(clear=True)
 rr.log(
     "box",
-    rr.Transform3D(axis_length=15),
+    rr.Transform3D.update_fields(clear=True, axis_length=15),
 )
