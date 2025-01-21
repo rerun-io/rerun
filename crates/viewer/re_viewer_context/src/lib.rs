13f1ea1a64c6d3eeb8158d796a27bd9e5f65a452
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2025-01-21 18:01:33 +0100

    Move and document `DataResultNodeOrPath` (#8762)

--- crates/viewer/re_viewer_context/src/lib.rs
+++ crates/viewer/re_viewer_context/src/lib.rs
@@ -12,7 +12,6 @@ mod command_sender;
 mod component_fallbacks;
 mod component_ui_registry;
 mod contents;
-mod data_result_node_or_path;
 mod drag_and_drop;
 mod file_dialog;
 mod image_info;
@@ -54,7 +53,6 @@ pub use self::{
     },
     component_ui_registry::{ComponentUiRegistry, ComponentUiTypes},
     contents::{blueprint_id_to_tile_id, Contents, ContentsName},
-    data_result_node_or_path::DataResultNodeOrPath,
     drag_and_drop::{DragAndDropFeedback, DragAndDropManager, DragAndDropPayload},
     file_dialog::santitize_file_name,
     image_info::{ColormapWithRange, ImageInfo},
