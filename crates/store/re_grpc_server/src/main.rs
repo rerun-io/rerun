510fca068fdedb17676f2bcc575f0d11af191cc7
jprochazk <1665677+jprochazk@users.noreply.github.com>
2025-01-22 10:49:32 +0100

    logging for re_grpc_server binary

--- crates/store/re_grpc_server/src/main.rs
+++ crates/store/re_grpc_server/src/main.rs
@@ -7,11 +7,15 @@ use tonic::transport::Server;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), tonic::transport::Error> {
+    re_log::setup_logging();
+
     let tcp_listener = TcpListener::bind("127.0.0.1:1852")
         .await
         .expect("failed to bind listener on 127.0.0.1:1852");
     let incoming =
         TcpIncoming::from_listener(tcp_listener, true, None).expect("failed to init listener");
+
+    re_log::info!("Listening for gRPC connections on 127.0.0.1:1852");
 
     Server::builder()
         .add_service(MessageProxyServer::new(MessageProxy::new(
