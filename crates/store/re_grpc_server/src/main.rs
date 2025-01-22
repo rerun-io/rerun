62242607358245ce666070bf06160c3c8cfc807f
jprochazk <1665677+jprochazk@users.noreply.github.com>
2025-01-22 13:10:14 +0100

    Remove `envoy` config in favor of `tonic-web`

--- crates/store/re_grpc_server/src/main.rs
+++ crates/store/re_grpc_server/src/main.rs
@@ -17,10 +17,27 @@ async fn main() -> Result<(), tonic::transport::Error> {
 
     re_log::info!("Listening for gRPC connections on 127.0.0.1:1852");
 
-    Server::builder()
-        .add_service(MessageProxyServer::new(MessageProxy::new(
+    use tower_http::cors::{Any, CorsLayer};
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    let grpc_web = tonic_web::GrpcWebLayer::new();
+
+    let routes = {
+        let mut routes_builder = tonic::service::Routes::builder();
+        routes_builder.add_service(MessageProxyServer::new(MessageProxy::new(
             MemoryLimit::UNLIMITED,
-        )))
+        )));
+        routes_builder.routes()
+    };
+
+    Server::builder()
+        .accept_http1(true) // Support `grpc-web` clients
+        .layer(cors) // Allow CORS requests from web clients
+        .layer(grpc_web) // Support `grpc-web` clients
+        .add_routes(routes)
         .serve_with_incoming(incoming)
         .await
 }
