rerun-io/rerun

commit a7acf152786421cd171d81226c38a01cb2d7a95f

Add missing click and hover interactions for views (#8495)
### Related

<!--
Include links to any related issues/PRs in a bulleted list, for example:
* Closes #1234
* Part of #1337
-->

* Closes #8479
* Closes #5138

### What

This adds missing view interactions the following views:

* Map view
* Graph view
* TextDocument view

@Wumpf @abey79 I feel like the pattern repeats itself and it's easy to
miss when writing new views. So I wonder if this could be unified. I see
several options for this:

* repurpose `handle_select_hover_drag_interactions` which only seems to
be used for `DataResult` right now
* Making each view return a `Result<(Response,
ViewSystemExecutionError>` (instead of `()`) and handling it outside of
the view.
* Creating a function analogous to
`handle_select_hover_drag_interactions`.

<!--
Make sure the PR title and labels are set to maximize their usefulness
for the CHANGELOG,
and our `git log`.

If you have noticed any breaking changes, include them in the migration
guide.

We track various metrics at <https://build.rerun.io>.

For maintainers:
* To run all checks from `main`, comment on the PR with `@rerun-bot
full-check`.
* To deploy documentation changes immediately after merging this PR, add
the `deploy docs` label.
-->

crates/viewer/re_view_text_document/src/view_class.rs
@@ -1,10 +1,12 @@
 use egui::Label;
 
+use egui::Sense;
 use re_types::View;
 use re_types::ViewClassIdentifier;
 use re_ui::UiExt as _;
 use re_view::suggest_view_for_each_entity;
 
+use re_viewer_context::Item;
 use re_viewer_context::{
     external::re_log_types::EntityPath, ViewClass, ViewClassRegistryError, ViewId, ViewQuery,
     ViewState, ViewStateExt as _, ViewSystemExecutionError, ViewerContext,
@@ -111,17 +113,16 @@ Displays text from a text component, as raw text or markdown."
 
     fn ui(
         &self,
-        _ctx: &ViewerContext<'_>,
+        ctx: &ViewerContext<'_>,
         ui: &mut egui::Ui,
         state: &mut dyn ViewState,
-
-        _query: &ViewQuery<'_>,
+        query: &ViewQuery<'_>,
         system_output: re_viewer_context::SystemExecutionOutput,
     ) -> Result<(), ViewSystemExecutionError> {
         let state = state.downcast_mut::<TextDocumentViewState>()?;
         let text_document = system_output.view_systems.get::<TextDocumentSystem>()?;
 
-        egui::Frame {
+        let response = egui::Frame {
             inner_margin: re_ui::DesignTokens::view_padding().into(),
             ..egui::Frame::default()
         }
@@ -176,7 +177,18 @@ Displays text from a text component, as raw text or markdown."
                     })
             })
             .response
-        });
+        })
+        .response
+        .interact(Sense::click());
+
+        if response.hovered() {
+            ctx.selection_state().set_hovered(Item::View(query.view_id));
+        }
+
+        if response.clicked() {
+            ctx.selection_state()
+                .set_selection(Item::View(query.view_id));
+        }
 
         Ok(())
     }

crates/viewer/re_view_graph/src/view.rs
@@ -206,6 +206,10 @@ Display a graph of nodes and edges.
             }
         });
 
+        if resp.hovered() {
+            ctx.selection_state().set_hovered(Item::View(query.view_id));
+        }
+
         if resp.clicked() {
             // clicked elsewhere, select the view
             ctx.selection_state()

crates/viewer/re_view_map/src/map_view.rs
@@ -499,6 +499,8 @@ fn handle_ui_interactions(
         // clicked elsewhere, select the view
         ctx.selection_state()
             .set_selection(Item::View(query.view_id));
+    } else if map_response.hovered() {
+        ctx.selection_state().set_hovered(Item::View(query.view_id));
     }
 }
 

tests/python/release_checklist/check_hover_select_reset.py
@@ -29,6 +29,13 @@ For each of the views:
     * If you think this is unexpected, create an issue.
     * Double-click the entity and verify that it becomes selected and highlighted in the blueprint tree.
 
+### Graph Select
+Should work just as 2D/3D views.
+
+### Text view
+Clicking on a text view (what you're reading right now) should select the view.
+Hovering the view should work as well.
+
 ### Reset
 For each of the views:
 * Zoom and/or pan the view
@@ -83,6 +90,14 @@ def log_points_2d() -> None:
     rr.log("2D/points", rr.Points2D(positions, colors=colors, radii=radii))
 
 
+def log_graph() -> None:
+    rr.log("graph", rr.GraphNodes(["a", "b"], labels=["A", "B"]))
+
+
+def log_map() -> None:
+    rr.log("points", rr.GeoPoints(lat_lon=[[47.6344, 19.1397], [47.6334, 19.1399]], radii=rr.Radius.ui_points(20.0)))
+
+
 def run(args: Namespace) -> None:
     rr.script_setup(args, f"{os.path.basename(__file__)}", recording_id=uuid4())
 
@@ -90,6 +105,8 @@ def run(args: Namespace) -> None:
     log_plots()
     log_points_3d()
     log_points_2d()
+    log_graph()
+    log_map()
 
 
 if __name__ == "__main__":

commit b8856d822a00aee9aee0f8db150e449a45fc2a29

Fix clicking of links in markdown (#8794)
### Related
* Closes https://github.com/rerun-io/rerun/issues/8574
* Bug introduced in https://github.com/rerun-io/rerun/pull/8495

### What
The text document had a `response.interact(Sense::click())` call _on
top_ of it, catching all clicks that should have ended up on the
hyperlinks. This is now changed into setting `Sense::click` on the
underlying `Ui` when creating it, meaning it will catch clicks _behind_
the hyperlinks, i.e. any click that doesn't interact with the document
iteself.

crates/viewer/re_view_text_document/src/view_class.rs
@@ -122,64 +122,22 @@ Displays text from a text component, as raw text or markdown."
         let state = state.downcast_mut::<TextDocumentViewState>()?;
         let text_document = system_output.view_systems.get::<TextDocumentSystem>()?;
 
-        let response = egui::Frame {
-            inner_margin: re_ui::DesignTokens::view_padding().into(),
-            ..egui::Frame::default()
-        }
-        .show(ui, |ui| {
-            ui.with_layout(egui::Layout::top_down(egui::Align::LEFT), |ui| {
-                egui::ScrollArea::both()
-                    .auto_shrink([false, false])
-                    .show(ui, |ui| {
-                        if text_document.text_entries.is_empty() {
-                            // We get here if we scroll back time to before the first text document was logged.
-                            ui.weak("(empty)");
-                        } else if text_document.text_entries.len() == 1 {
-                            let TextDocumentEntry { body, media_type } =
-                                &text_document.text_entries[0];
+        let frame = egui::Frame::new().inner_margin(re_ui::DesignTokens::view_padding());
+        let response = frame
+            .show(ui, |ui| {
+                let inner_ui_builder = egui::UiBuilder::new()
+                    .layout(egui::Layout::top_down(egui::Align::LEFT))
+                    .sense(Sense::click());
+                ui.allocate_new_ui(inner_ui_builder, |ui| {
+                    egui::ScrollArea::both()
+                        .auto_shrink([false, false])
+                        .show(ui, |ui| text_document_ui(ui, state, text_document));
 
-                            if media_type == &re_types::components::MediaType::markdown() {
-                                re_tracing::profile_scope!("egui_commonmark");
-
-                                // Make sure headers are big:
-                                ui.style_mut()
-                                    .text_styles
-                                    .entry(egui::TextStyle::Heading)
-                                    .or_insert(egui::FontId::proportional(32.0))
-                                    .size = 24.0;
-
-                                egui_commonmark::CommonMarkViewer::new()
-                                    .max_image_width(Some(ui.available_width().floor() as _))
-                                    .show(ui, &mut state.commonmark_cache, body);
-                                return;
-                            }
-
-                            let mut text = egui::RichText::new(body.as_str());
-
-                            if state.monospace {
-                                text = text.monospace();
-                            }
-
-                            ui.add(Label::new(text).wrap_mode(if state.word_wrap {
-                                egui::TextWrapMode::Wrap
-                            } else {
-                                egui::TextWrapMode::Extend
-                            }));
-                        } else {
-                            // TODO(jleibs): better handling for multiple results
-                            ui.error_label(format!(
-                                "Can only show one text document at a time; was given {}. Update \
-                                the query so that it returns a single text document and create \
-                                additional views for the others.",
-                                text_document.text_entries.len()
-                            ));
-                        }
-                    })
+                    ui.response()
+                })
+                .inner
             })
-            .response
-        })
-        .response
-        .interact(Sense::click());
+            .inner;
 
         if response.hovered() {
             ctx.selection_state().set_hovered(Item::View(query.view_id));
@@ -191,5 +149,53 @@ Displays text from a text component, as raw text or markdown."
         }
 
         Ok(())
+    }
+}
+
+fn text_document_ui(
+    ui: &mut egui::Ui,
+    state: &mut TextDocumentViewState,
+    text_document: &TextDocumentSystem,
+) {
+    if text_document.text_entries.is_empty() {
+        // We get here if we scroll back time to before the first text document was logged.
+        ui.weak("(empty)");
+    } else if text_document.text_entries.len() == 1 {
+        let TextDocumentEntry { body, media_type } = &text_document.text_entries[0];
+
+        if media_type == &re_types::components::MediaType::markdown() {
+            re_tracing::profile_scope!("egui_commonmark");
+
+            // Make sure headers are big:
+            ui.style_mut()
+                .text_styles
+                .entry(egui::TextStyle::Heading)
+                .or_insert(egui::FontId::proportional(32.0))
+                .size = 24.0;
+
+            egui_commonmark::CommonMarkViewer::new()
+                .max_image_width(Some(ui.available_width().floor() as _))
+                .show(ui, &mut state.commonmark_cache, body);
+        } else {
+            let mut text = egui::RichText::new(body.as_str());
+
+            if state.monospace {
+                text = text.monospace();
+            }
+
+            ui.add(Label::new(text).wrap_mode(if state.word_wrap {
+                egui::TextWrapMode::Wrap
+            } else {
+                egui::TextWrapMode::Extend
+            }));
+        }
+    } else {
+        // TODO(jleibs): better handling for multiple results
+        ui.error_label(format!(
+            "Can only show one text document at a time; was given {}. Update \
+                                    the query so that it returns a single text document and create \
+                                    additional views for the others.",
+            text_document.text_entries.len()
+        ));
     }
 }