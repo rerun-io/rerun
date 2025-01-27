8ab9f779f6fa3c2b3397604f1c53f3deb7cb70bc
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-01-27 14:16:03 +0100

    Replace `sobet.` prefix with `rerun.`

--- crates/store/re_sorbet/src/index_column_schema.rs
+++ crates/store/re_sorbet/src/index_column_schema.rs
@@ -75,7 +75,7 @@ impl TimeColumnDescriptor {
 
         let metadata = [
             Some(("rerun.kind".to_owned(), "index".to_owned())),
-            Some(("sorbet.index_name".to_owned(), timeline.name().to_string())),
+            Some(("rerun.index_name".to_owned(), timeline.name().to_string())),
         ]
         .into_iter()
         .flatten()
@@ -99,10 +99,10 @@ impl TryFrom<&ArrowField> for TimeColumnDescriptor {
     type Error = UnsupportedTimeType;
 
     fn try_from(field: &ArrowField) -> Result<Self, Self::Error> {
-        let name = if let Some(name) = field.metadata().get("sorbet.index_name") {
+        let name = if let Some(name) = field.metadata().get("rerun.index_name") {
             name.to_owned()
         } else {
-            re_log::warn_once!("Timeline '{}' is missing 'sorbet.index_name' metadata. Falling back on field/column name", field.name());
+            re_log::warn_once!("Timeline '{}' is missing 'rerun.index_name' metadata. Falling back on field/column name", field.name());
             field.name().to_owned()
         };
 
