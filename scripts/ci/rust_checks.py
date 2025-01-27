188c2c7e6ef02b00bb4b9614b66f2279dd0e613e
Andreas Reich <andreas@rerun.io>
2025-01-27 19:52:22 +0100

    py-fmt

--- scripts/ci/rust_checks.py
+++ scripts/ci/rust_checks.py
@@ -93,7 +93,7 @@ def package_name_from_cargo_toml(cargo_toml_path: str) -> str:
 
 def main() -> None:
     # Ensure we can print unicode characters. Has been historically an issue on Windows CI.
-    sys.stdout.reconfigure(encoding='utf-8')
+    sys.stdout.reconfigure(encoding="utf-8")
 
     checks = [
         ("base_checks", base_checks),
