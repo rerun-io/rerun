a8cf09d6d97adedfc7c56071cb9fae3f5f067c12
Gijs de Jong <berichtaangijs@gmail.com>
2025-01-27 19:26:39 +0100

    Disable web viewer check in ci

--- crates/viewer/re_web_viewer_server/build.rs
+++ crates/viewer/re_web_viewer_server/build.rs
@@ -1,14 +1,16 @@
-use std::path::Path;
-
 fn main() {
     // https://blog.rust-lang.org/2024/05/06/check-cfg.html
     println!("cargo::rustc-check-cfg=cfg(disable_web_viewer_server)");
 
-    let viewer_js_path = Path::new("./web_viewer/re_viewer.js");
-    let viewer_wasm_path = Path::new("./web_viewer/re_viewer_bg.wasm");
+    // See `Cargo.toml` docs for the `__ci` feature for more information about the `disable_web_viewer_server` cfg.
+    #[cfg(not(feature = "__ci"))]
+    {
+        let viewer_js_path = std::path::Path::new("./web_viewer/re_viewer.js");
+        let viewer_wasm_path = std::path::Path::new("./web_viewer/re_viewer_bg.wasm");
 
-    assert!(
-        viewer_js_path.exists() && viewer_wasm_path.exists(),
-        "Web viewer not found, run `pixi run rerun-build-web` to build it!"
-    );
+        assert!(
+            viewer_js_path.exists() && viewer_wasm_path.exists(),
+            "Web viewer not found, run `pixi run rerun-build-web` to build it!"
+        );
+    }
 }
