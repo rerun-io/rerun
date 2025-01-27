627050114eefb00b5153dc8aaf00f94c725eb503
Gijs de Jong <14833076+oxkitsune@users.noreply.github.com>
2025-01-27 18:24:59 +0100

    Add hint for missing web viewer when building `re_web_viewer_server` (#8832)

--- crates/viewer/re_web_viewer_server/build.rs
+++ crates/viewer/re_web_viewer_server/build.rs
@@ -1,4 +1,14 @@
+use std::path::Path;
+
 fn main() {
     // https://blog.rust-lang.org/2024/05/06/check-cfg.html
     println!("cargo::rustc-check-cfg=cfg(disable_web_viewer_server)");
+
+    let viewer_js_path = Path::new("./web_viewer/re_viewer.js");
+    let viewer_wasm_path = Path::new("./web_viewer/re_viewer_bg.wasm");
+
+    assert!(
+        viewer_js_path.exists() && viewer_wasm_path.exists(),
+        "Web viewer not found, run `pixi run rerun-build-web` to build it!"
+    );
 }
