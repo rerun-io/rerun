d3d83127fbd43a8bea136a09660b69652ca0f761
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-01-28 10:19:50 +0100

    Add conversions between re_protos Schema and ArrowSchema

--- crates/store/re_protos/src/protobuf_conversions.rs
+++ crates/store/re_protos/src/protobuf_conversions.rs
@@ -1,3 +1,25 @@
+use arrow::{datatypes::Schema as ArrowSchema, error::ArrowError};
+
+impl TryFrom<crate::common::v0::Schema> for ArrowSchema {
+    type Error = ArrowError;
+
+    fn try_from(value: crate::common::v0::Schema) -> Result<Self, Self::Error> {
+        re_sorbet::schema_from_ipc(&value.arrow_schema)
+    }
+}
+
+impl TryFrom<ArrowSchema> for crate::common::v0::Schema {
+    type Error = ArrowError;
+
+    fn try_from(value: ArrowSchema) -> Result<Self, Self::Error> {
+        Ok(Self {
+            arrow_schema: re_sorbet::ipc_from_schema(&value)?,
+        })
+    }
+}
+
+// -------------------------------------------------------------------------------------------
+
 impl From<crate::common::v0::Tuid> for re_tuid::Tuid {
     fn from(value: crate::common::v0::Tuid) -> Self {
         Self::from_nanos_and_inc(value.time_ns, value.inc)
