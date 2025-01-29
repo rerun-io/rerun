857047531dee519bf7e5926f968bdd9eecc18fde
Andreas Reich <r_andreas2@web.de>
2025-01-29 12:24:19 +0100

    extend hack to safari

--- crates/utils/re_video/src/decode/ffmpeg_h264/ffmpeg.rs
+++ crates/utils/re_video/src/decode/ffmpeg_h264/ffmpeg.rs
@@ -884,7 +884,10 @@ impl AsyncDecoder for FFmpegCliH264Decoder {
     }
 
     fn min_num_samples_to_enqueue_ahead(&self) -> usize {
-        // TODO: describe this and add ticket.
+        // TODO(#8848): On some videos (which??) we need to enqueue more samples, otherwise ffmpeg will not provide us with any frames.
+        // The observed behavior is that we continously get frames that are 16 frames older than what we enqueued,
+        // never reaching the frames of all currently enqueued GOPs prior.
+        // (The same happens with webcodec decoder on Safari for affected videos)
         16
     }
 }
