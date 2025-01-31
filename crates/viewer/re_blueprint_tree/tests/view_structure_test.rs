2eeb284384ed5b79e4bd6831b3f408dda6c10b6e
Antoine Beyeler <antoine@rerun.io>
2025-01-31 17:29:35 +0100

    Add multi-keyword test cases for the blueprint tree

--- crates/viewer/re_blueprint_tree/tests/view_structure_test.rs
+++ crates/viewer/re_blueprint_tree/tests/view_structure_test.rs
@@ -89,7 +89,15 @@ fn base_test_cases() -> impl Iterator<Item = TestCase> {
 }
 
 fn filter_queries() -> impl Iterator<Item = Option<&'static str>> {
-    [None, Some("t"), Some("void"), Some("path")].into_iter()
+    [
+        None,
+        Some("t"),
+        Some("void"),
+        Some("path"),
+        Some("ath t"),
+        Some("ath left"),
+    ]
+    .into_iter()
 }
 
 fn test_context(test_case: &TestCase) -> TestContext {
@@ -202,7 +210,7 @@ fn run_test_case(test_case: &TestCase, filter_query: Option<&str>) -> Result<(),
     let options = SnapshotOptions::default().output_path(format!(
         "tests/snapshots/view_structure_test/{}",
         filter_query
-            .map(|query| format!("query-{query}"))
+            .map(|query| format!("query-{}", query.replace(" ", "_")))
             .unwrap_or("no-query".to_owned())
     ));
     harness.try_snapshot_options(test_case.name, &options)
@@ -239,7 +247,7 @@ fn test_all_insta_test_cases() {
             let snapshot_name = format!(
                 "{}-{}",
                 filter_query
-                    .map(|query| format!("query-{query}"))
+                    .map(|query| format!("query-{}", query.replace(" ", "_")))
                     .unwrap_or("no-query".to_owned()),
                 test_case.name
             );
