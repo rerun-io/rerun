bb81b298da74e29ae75e9070a92e43600ba9f1ff
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-02-11 21:25:54 +0100

    Benchmark Tuid::cmp

--- crates/utils/re_tuid/benches/bench_tuid.rs
+++ crates/utils/re_tuid/benches/bench_tuid.rs
@@ -6,6 +6,14 @@ fn bench_tuid(c: &mut Criterion) {
     group.bench_function("Tuid::new", |b| {
         b.iter(|| criterion::black_box(re_tuid::Tuid::new()));
     });
+
+    group.throughput(criterion::Throughput::Elements(1_000));
+    group.bench_function("Tuid::cmp", |b| {
+        use rand::prelude::*;
+        let mut ids = (0..2_000).map(|_| re_tuid::Tuid::new()).collect::<Vec<_>>();
+        ids.shuffle(&mut rand::thread_rng());
+        b.iter(|| criterion::black_box(ids[0..1_000].cmp(&ids[1_000..2_000])));
+    });
 }
 
 criterion_group!(benches, bench_tuid);
