99aaa4d482394d11fb37b592e8ed46637785288a
jprochazk <pjanv42@gmail.com>
2025-02-18 11:19:34 +0100

    add comment

--- crates/store/re_log_encoding/src/lib.rs
+++ crates/store/re_log_encoding/src/lib.rs
@@ -122,6 +122,7 @@ pub enum OptionsError {
     #[error("Unknown compression: {0}")]
     UnknownCompression(u8),
 
+    // TODO(jan): Remove this at some point, realistically 1-2 releases from now
     #[error("Attempted to use the removed MsgPack serializer, which is no longer supported")]
     RemovedMsgPackSerializer,
 
