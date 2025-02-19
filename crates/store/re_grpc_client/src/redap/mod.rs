fdd644ee6839245792bd683fd3d5f3c841245e63
Jochen Görtler <me@jgoertler.com>
2025-02-19 10:41:09 +0100

    dead code

--- crates/store/re_grpc_client/src/redap/mod.rs
+++ crates/store/re_grpc_client/src/redap/mod.rs
@@ -20,47 +20,6 @@ use re_protos::{
 use crate::StreamError;
 use crate::TonicStatusError;
 
-// /// Stream an rrd file or metadata catalog over gRPC from a Rerun Data Platform server.
-// ///
-// /// `on_msg` can be used to wake up the UI thread on Wasm.
-// pub fn stream_from_redap(
-//     url: String,
-//     on_msg: Option<Box<dyn Fn() + Send + Sync>>,
-// ) -> Result<re_smart_channel::Receiver<LogMsg>, ConnectionError> {
-//     re_log::debug!("Loading {url}…");
-
-//     let address = url.as_str().try_into()?;
-
-//     let (tx, rx) = re_smart_channel::smart_channel(
-//         re_smart_channel::SmartMessageSource::RerunGrpcStream { url: url.clone() },
-//         re_smart_channel::SmartChannelSource::RerunGrpcStream { url: url.clone() },
-//     );
-
-//     match address {
-//         RedapAddress::Recording {
-//             origin,
-//             recording_id,
-//         } => {
-//             spawn_future(async move {
-//                 if let Err(err) = stream_recording_async(tx, origin, recording_id, on_msg).await {
-//                     re_log::error!(
-//                         "Error while streaming {url}: {}",
-//                         re_error::format_ref(&err)
-//                     );
-//                 }
-//             });
-//         }
-//         // TODO(#9058): This should be fix by introducing a `RedapRecordingAddress`.
-//         RedapAddress::Catalog { origin } => {
-//             return Err(ConnectionError::CannotLoadUrlAsRecording {
-//                 url: origin.to_string(),
-//             });
-//         }
-//     }
-
-//     Ok(rx)
-// }
-
 #[derive(Debug, thiserror::Error)]
 pub enum ConnectionError {
     /// Native connection error
