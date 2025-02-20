acfaefcfe84bb764b17a7dacdfa0d4d580da144c
Zeljko Mihaljcic <7150613+zehiko@users.noreply.github.com>
2025-02-20 16:07:32 +0100

    pyo3

--- rerun_py/rerun_bindings/rerun_bindings.pyi
+++ rerun_py/rerun_bindings/rerun_bindings.pyi
@@ -629,16 +629,16 @@ class StorageNodeClient:
         """
         ...
 
-    def register(self, collection_name: Optional[str], storage_url: str, metadata: Optional[TableLike] = None) -> str:
+    def register(self, storage_url: str, collection_name: Optional[str] = None, metadata: Optional[TableLike] = None) -> str:
         """
         Register a recording along with some metadata.
 
         Parameters
         ----------
-        collection_name : Optional[str]
-            The name of the collection to register the recording in.
         storage_url : str
             The URL to the storage location.
+        collection_name : Optional[str]
+            The name of the collection to register the recording in.
         metadata : Optional[Table | RecordBatch]
             A pyarrow Table or RecordBatch containing the metadata to update.
             This Table must contain only a single row.
--- rerun_py/src/remote.rs
+++ rerun_py/src/remote.rs
@@ -302,16 +302,16 @@ impl PyStorageNodeClient {
     ///
     /// Parameters
     /// ----------
-    /// collection_name : Optional[str]
-    ///     The name of the collection to register the recording in.
     /// storage_url : str
     ///     The URL to the storage location.
+    /// collection_name : Optional[str]
+    ///     The name of the collection to register the recording in.
     /// metadata : Optional[Table | RecordBatch]
     ///     A pyarrow Table or RecordBatch containing the metadata to update.
     ///     This Table must contain only a single row.
     #[pyo3(signature = (
-        collection_name = None,
         storage_url,
+        collection_name = None,
         metadata = None
     ))]
     fn register(
