f84cf294c8da5b90d9525a707e96c138adb30096
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-02-21 20:01:45 +0100

    Update docstring for TimelineName

--- crates/store/re_log_types/src/time_point/timeline.rs
+++ crates/store/re_log_types/src/time_point/timeline.rs
@@ -4,6 +4,8 @@ use crate::{time::TimeZone, ResolvedTimeRange, TimeType};
 
 re_string_interner::declare_new_type!(
     /// The name of a timeline. Often something like `"log_time"` or `"frame_nr"`.
+    ///
+    /// This uniquely identifies a timeline.
     #[cfg_attr(feature = "serde", derive(::serde::Deserialize, ::serde::Serialize))]
     pub struct TimelineName;
 );
