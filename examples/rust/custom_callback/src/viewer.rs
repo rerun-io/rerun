5b80fa6ec0ccc0954e2b4598abb38405b3452a19
jprochazk <1665677+jprochazk@users.noreply.github.com>
2025-02-24 09:44:36 +0100

    remove unused dep

--- examples/rust/custom_callback/src/viewer.rs
+++ examples/rust/custom_callback/src/viewer.rs
@@ -1,6 +1,6 @@
 use custom_callback::{comms::viewer::ControlViewer, panel::Control};
 
-use rerun::external::re_viewer::{command_channel, AsyncRuntimeHandle};
+use rerun::external::re_viewer::AsyncRuntimeHandle;
 use rerun::external::{eframe, re_log, re_memory, re_viewer};
 
 // By using `re_memory::AccountingAllocator` Rerun can keep track of exactly how much memory it is using,
