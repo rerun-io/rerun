26941f7925325198bbeabe0a8c1fecaf35a22a6e
Gijs de Jong <berichtaangijs@gmail.com>
2025-02-27 08:20:21 +0100

    fmt

--- rerun_py/rerun_sdk/rerun/sinks.py
+++ rerun_py/rerun_sdk/rerun/sinks.py
@@ -8,8 +8,8 @@ import rerun_bindings as bindings  # type: ignore[attr-defined]
 from typing_extensions import deprecated  # type: ignore[misc, unused-ignore]
 
 from rerun.blueprint.api import BlueprintLike, create_in_memory_blueprint
-from rerun.recording_stream import RecordingStream, get_application_id
 from rerun.dataframe import Recording
+from rerun.recording_stream import RecordingStream, get_application_id
 
 from ._spawn import _spawn_viewer
 
