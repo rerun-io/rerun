3427e45a863c9c6b4cbb54ace7cd18c647b03969
Lucas Meurer <lucasmeurer96@gmail.com>
2025-02-27 16:11:30 +0100

    Load all rrds

--- import_with_thumbs.py
+++ import_with_thumbs.py
@@ -10,15 +10,27 @@ import pyarrow as pa
 
 # cargo run -p re_viewer --bin thumbnail_generator --features re_viewer_context/testing --features re_viewport_blueprint/testing ~/2d.rrd thumb.png
 
-rrd_path = Path("episode_1.rrd")
-img_path = Path("thumb.png")
 
-subprocess.run("cargo run -p re_thumbnail_generator {} {}".format(rrd_path, img_path), shell=True)
 
-img = rr.EncodedImage(path=img_path)
-
-field = pa.field("thumb", img.blob.pa_array.type, metadata={"rerun.kind": "data", "rerun.component": "rerun.components.Blob"})
-schema = pa.schema([field])
-
+# rrd_path = Path("/Users/lucas/Downloads/Archive/77abe62c-c767-4e32-b035-27b9fadd8588.rrd")
 conn = rr.remote.connect("http://127.0.0.1:51234")
-conn.register("default", rrd_path.absolute().as_uri(), metadata=pa.RecordBatch.from_arrays([img.blob.pa_array], schema=schema))
+
+for rrd_path in Path("/Users/lucas/Downloads/Archive").glob("*.rrd"):
+
+    img_path = Path("thumb.png")
+
+    subprocess.run("cargo run -p re_thumbnail_generator {} {}".format(rrd_path, img_path), shell=True)
+
+    img = rr.EncodedImage(path=img_path)
+
+    field = pa.field("thumb", img.blob.pa_array.type, metadata={"rerun.kind": "data", "rerun.component": "rerun.components.Blob"})
+    schema = pa.schema([field])
+
+    try :
+        conn.register("default", rrd_path.absolute().as_uri(), metadata=pa.RecordBatch.from_arrays([img.blob.pa_array], schema=schema))
+
+    except Exception as e:
+        print(e)
+
+
+print("Done")
