a8d6c938257623292bf7cab879161e75f597cf64
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-02-28 09:54:07 +0100

    Add example .rrd:s

--- tests/assets/rrd/generate-compatibility-rrds.sh
+++ tests/assets/rrd/generate-compatibility-rrds.sh
@@ -3,23 +3,32 @@
 
 set -eux
 
+
 SOURCE_DIR="docs/snippets/all"
 DEST_DIR="tests/assets/rrd"
+
 
 echo "Removing old rrds…"
 rm -f "${DEST_DIR}/*.rrd"
 
-echo "Removing old output…"
+
+echo "Generating example .rrd files…"
+pixi run -e examples build-examples rrd --channel main ${DEST_DIR}
+
+
+echo "Removing old snippet output…"
 find "${SOURCE_DIR}" -type f -name "*.rrd" -exec rm -f {} +
 
-echo "Generating .rrd files…"
+echo "Generating snippet .rrd files…"
 pixi run -e py docs/snippets/compare_snippet_output.py --no-py --no-cpp
 
 echo "Copying .rrd files to ${DEST_DIR}…"
-find "$SOURCE_DIR" -type f -name "*.rrd" -exec cp {} "$DEST_DIR" \;
+find "$SOURCE_DIR" -type f -name "*.rrd" -exec cp {} "${DEST_DIR}" \;
 
-echo "Tracking .rrd files with Git LFS…"
+
+echo "Tracking .rrd files with git LFS…"
 git lfs track "*.rrd"
 
-echo "Adding new .rrd files to Git…"
+
+echo "Adding new .rrd files to git…"
 git add -f ${DEST_DIR}/*.rrd
