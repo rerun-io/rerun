f25a2e7b2ad35e87ae1e7d768b73c038f632baa8
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-02-28 09:41:01 +0100

    Add all Rust snippet .rrd:s

--- tests/assets/rrd/generate-compatibility-rrds.sh
+++ tests/assets/rrd/generate-compatibility-rrds.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
-# Run stuff that often fails on CI
+# Generate and check in .rrd files used for backwards compatibility tests.
 
 set -eux
 
+SOURCE_DIR="docs/snippets/all"
+DEST_DIR="tests/assets/rrd"
+
 echo "Removing old rrds…"
-rm -f tests/assets/rrd/*.rrd
+rm -f "${DEST_DIR}/*.rrd"
 
 echo "Removing old output…"
-rm -f docs/snippets/all/archetypes/*.rrd
+find "${SOURCE_DIR}" -type f -name "*.rrd" -exec rm -f {} +
 
 echo "Generating .rrd files…"
 pixi run -e py docs/snippets/compare_snippet_output.py --no-py --no-cpp
 
-mv docs/snippets/all/archetypes/*_rust.rrd tests/assets/rrd
+echo "Copying .rrd files to ${DEST_DIR}…"
+find "$SOURCE_DIR" -type f -name "*.rrd" -exec cp {} "$DEST_DIR" \;
 
 echo "Tracking .rrd files with Git LFS…"
 git lfs track "*.rrd"
 
 echo "Adding new .rrd files to Git…"
-git add -f tests/assets/rrd/*.rrd
+git add -f ${DEST_DIR}/*.rrd
