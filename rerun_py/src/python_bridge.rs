c5e618247e1e6e0fb0872108a4c8750da18fcffb
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-03-03 19:03:07 +0100

    Add a TODO

--- rerun_py/src/python_bridge.rs
+++ rerun_py/src/python_bridge.rs
@@ -1062,6 +1062,7 @@ fn set_time_sequence(timeline: &str, sequence: i64, recording: Option<&PyRecordi
     recording.set_time_sequence(timeline, sequence);
 }
 
+// TODO(#8635): distinguish between absolute and relative time
 #[pyfunction]
 #[pyo3(signature = (timeline, seconds, recording=None))]
 fn set_time_seconds(timeline: &str, seconds: f64, recording: Option<&PyRecordingStream>) {
@@ -1071,6 +1072,7 @@ fn set_time_seconds(timeline: &str, seconds: f64, recording: Option<&PyRecording
     recording.set_time_seconds(timeline, seconds);
 }
 
+// TODO(#8635): distinguish between absolute and relative time
 #[pyfunction]
 #[pyo3(signature = (timeline, nanos, recording=None))]
 fn set_time_nanos(timeline: &str, nanos: i64, recording: Option<&PyRecordingStream>) {
