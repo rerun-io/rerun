3ac62ad65fcad3f907d712ba6c1f6c7ed972688f
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-03-06 11:22:46 +0100

    Update MSRV to 1.83

--- scripts/check_env.py
+++ scripts/check_env.py
@@ -7,8 +7,8 @@ from __future__ import annotations
 import subprocess
 
 PIXI_VERSION = "0.41.4"
-CARGO_VERSION = "1.82.0"
-RUST_VERSION = "1.82.0"
+CARGO_VERSION = "1.83.0"
+RUST_VERSION = "1.83.0"
 
 
 def check_version(cmd: str, expected: str, update: str, install: str) -> bool:
