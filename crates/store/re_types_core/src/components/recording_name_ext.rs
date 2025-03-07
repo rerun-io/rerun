f1ece46d88c53ef1b420c6884e887147250dd4e8
Jochen Görtler <me@jgoertler.com>
2025-03-07 12:17:28 +0100

    fix

--- crates/store/re_types_core/src/components/recording_name_ext.rs
+++ crates/store/re_types_core/src/components/recording_name_ext.rs
@@ -1,13 +1,5 @@
-use crate::AsComponents;
-
 impl std::fmt::Display for super::RecordingName {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.0.fmt(f)
-    }
-}
-
-impl AsComponents for super::RecordingName {
-    fn as_serialized_batches(&self) -> Vec<crate::SerializedComponentBatch> {
-        self.as_serialized_batches()
     }
 }
