7873130af3327641bea90947bbac9f8d79bb9309
Jeremy Leibs <jeremy@rerun.io>
2025-03-12 19:24:34 -0400

    Use EntryId type in EntryKey

--- crates/store/re_protos/src/v1alpha1/rerun.catalog.v1alpha1.rs
+++ crates/store/re_protos/src/v1alpha1/rerun.catalog.v1alpha1.rs
@@ -217,8 +217,8 @@ impl ::prost::Name for DatasetManifest {
 /// message.
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct EntryKey {
-    #[prost(string, optional, tag = "1")]
-    pub id: ::core::option::Option<::prost::alloc::string::String>,
+    #[prost(message, optional, tag = "1")]
+    pub id: ::core::option::Option<EntryId>,
     #[prost(string, optional, tag = "2")]
     pub name: ::core::option::Option<::prost::alloc::string::String>,
 }
