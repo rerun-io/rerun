82d45ce7de4334a64c52f14bff61fe030aa664b1
Jan Procházka <honza.spacir@gmail.com>
2025-03-13 13:15:33 +0100

    Add selection change and time(line) change callbacks to Jupyter Notebook and JS APIs (#9147)
    * Part of https://github.com/rerun-io/rerun/issues/9169
    
    ## What
    
    This PR adds basic selection and timeline callbacks to the Viewer:
    * `selectionchange`: Fired when an item is selected.
      * We only expose `View`, `Container`, `Entity`, and `Instance`.
    * `timelinechange`: Fired when the active timeline changes.
    * `timeupdate`: Fired when the time on the active timeline changes.
    
    These callbacks are exposed in the Web Viewer:
    
    ```ts
    // We use the Node `EventEmitter` pattern in JS:
    viewer.on("selectionchange", (selection) => console.log(selection));
    ```
    
    And in Jupyter Notebooks:
    
    ```py
    from rerun_notebook import Viewer, ViewerCallbacks
    
    # We (Jan and Gijs) chose to expose this as a class that you inherit from:
    class MyCallbacks(ViewerCallbacks):
      def on_selection_change(self, selection):
        print(selection)
    
    # Which is then passed to a `Viewer` instance:
    viewer = Viewer()
    viewer.register_callbacks(MyCallbacks())
    ```
    
    Because of how these callbacks pass through the Web Viewer, they are
    technically also exposed in Rust, via `StartupOptions`. I'm not sure how
    far we want to take that, so I didn't expose it all the way through the
    top-level `rerun` crate.
    
    ---------
    
    Co-authored-by: Andreas Reich <r_andreas2@web.de>
    Co-authored-by: Andreas Reich <andreas@rerun.io>
    Co-authored-by: Gijs de Jong <berichtaangijs@gmail.com>

--- crates/viewer/re_viewer/src/app.rs
+++ crates/viewer/re_viewer/src/app.rs
@@ -14,7 +14,7 @@ use re_ui::{notifications, DesignTokens, UICommand, UICommandSender as _};
 use re_viewer_context::{
     command_channel,
     store_hub::{BlueprintPersistence, StoreHub, StoreHubStats},
-    AppOptions, AsyncRuntimeHandle, BlueprintUndoState, CommandReceiver, CommandSender,
+    AppOptions, AsyncRuntimeHandle, BlueprintUndoState, Callbacks, CommandReceiver, CommandSender,
     ComponentUiRegistry, DisplayMode, PlayState, StoreContext, SystemCommand,
     SystemCommandSender as _, ViewClass, ViewClassRegistry, ViewClassRegistryError,
 };
@@ -83,12 +83,8 @@ pub struct StartupOptions {
     /// This also can be changed in the viewer's option menu.
     pub video_decoder_hw_acceleration: Option<re_video::decode::DecodeHardwareAcceleration>,
 
-    /// Interaction between JS and timeline.
-    ///
-    /// This field isn't used directly, but is propagated to all recording configs
-    /// when they are created.
-    #[cfg(target_arch = "wasm32")]
-    pub timeline_options: Option<crate::web::TimelineOptions>,
+    /// External interactions with the Viewer host (JS, custom egui app, notebook, etc.).
+    pub callbacks: Option<Callbacks>,
 
     /// Fullscreen is handled by JS on web.
     ///
@@ -139,8 +135,7 @@ impl Default for StartupOptions {
             force_wgpu_backend: None,
             video_decoder_hw_acceleration: None,
 
-            #[cfg(target_arch = "wasm32")]
-            timeline_options: Default::default(),
+            callbacks: Default::default(),
 
             #[cfg(target_arch = "wasm32")]
             fullscreen_options: Default::default(),
@@ -232,11 +227,8 @@ pub struct App {
 
     reflection: re_types_core::reflection::Reflection,
 
-    /// Interaction between JS and timeline.
-    ///
-    /// This field isn't used directly, but is propagated to all recording configs
-    /// when they are created.
-    pub timeline_callbacks: Option<re_viewer_context::TimelineCallbacks>,
+    /// External interactions with the Viewer host (JS, custom egui app, notebook, etc.).
+    pub callbacks: Option<Callbacks>,
 
     /// The async runtime that should be used for all asynchronous operations.
     ///
@@ -372,45 +364,7 @@ impl App {
             Default::default()
         });
 
-        #[cfg(target_arch = "wasm32")]
-        let timeline_callbacks = {
-            use crate::web_tools::string_from_js_value;
-            use std::rc::Rc;
-            use wasm_bindgen::JsValue;
-
-            startup_options.timeline_options.clone().map(|opts| {
-                re_viewer_context::TimelineCallbacks {
-                    on_timelinechange: Rc::new(move |timeline, time| {
-                        if let Err(err) = opts.on_timelinechange.call2(
-                            &JsValue::from_str(timeline.name().as_str()),
-                            &JsValue::from_f64(time.as_f64()),
-                        ) {
-                            re_log::error!("{}", string_from_js_value(err));
-                        };
-                    }),
-                    on_timeupdate: Rc::new(move |time| {
-                        if let Err(err) =
-                            opts.on_timeupdate.call1(&JsValue::from_f64(time.as_f64()))
-                        {
-                            re_log::error!("{}", string_from_js_value(err));
-                        }
-                    }),
-                    on_play: Rc::new(move || {
-                        if let Err(err) = opts.on_play.call0() {
-                            re_log::error!("{}", string_from_js_value(err));
-                        }
-                    }),
-                    on_pause: Rc::new(move || {
-                        if let Err(err) = opts.on_pause.call0() {
-                            re_log::error!("{}", string_from_js_value(err));
-                        }
-                    }),
-                }
-            })
-        };
-
-        #[cfg(not(target_arch = "wasm32"))]
-        let timeline_callbacks = None;
+        let callbacks = startup_options.callbacks.clone();
 
         Self {
             main_thread_token,
@@ -462,7 +416,7 @@ impl App {
 
             reflection,
 
-            timeline_callbacks,
+            callbacks,
             async_runtime: tokio_runtime,
         }
     }
@@ -1356,7 +1310,7 @@ impl App {
                                 opacity: self.welcome_screen_opacity(egui_ctx),
                             },
                             is_history_enabled,
-                            self.timeline_callbacks.as_ref(),
+                            self.callbacks.as_ref(),
                         );
                         render_ctx.before_submit();
                     }
---
+++ crates/viewer/re_viewer_context/src/callback.rs
@@ -0,0 +1,110 @@
+use crate::Item;
+use crate::ItemCollection;
+use crate::ItemContext;
+use re_log_types::{TimeReal, Timeline};
+use std::rc::Rc;
+
+/// A single item in a selection.
+#[derive(Debug, serde::Serialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+pub enum CallbackSelectionItem {
+    /// Selected an (entire) entity.
+    ///
+    /// Examples:
+    /// * A full point cloud.
+    /// * A mesh.
+    Entity { entity_path: String },
+
+    /// Selected an instance within an entity.
+    ///
+    /// Examples:
+    /// * A single point in a point cloud.
+    Instance {
+        entity_path: String,
+        instance_id: u64,
+    },
+
+    /// Selected a view.
+    View { view_id: String },
+
+    /// Selected a container.
+    Container { container_id: String },
+}
+
+impl CallbackSelectionItem {
+    // TODO(jan): output more things, including parts of context for data results
+    //            and other things not currently available here (e.g. mouse pos)
+    pub fn new(item: &Item, _context: &Option<ItemContext>) -> Option<Self> {
+        match item {
+            Item::StoreId(_) | Item::AppId(_) | Item::ComponentPath(_) | Item::DataSource(_) => {
+                None
+            }
+            Item::View(view_id) => Some(Self::View {
+                view_id: view_id.uuid().to_string(),
+            }),
+            Item::Container(container_id) => Some(Self::Container {
+                container_id: container_id.uuid().to_string(),
+            }),
+            Item::InstancePath(instance_path) | Item::DataResult(_, instance_path) => {
+                if instance_path.is_all() {
+                    Some(Self::Entity {
+                        entity_path: instance_path.entity_path.to_string(),
+                    })
+                } else {
+                    Some(Self::Instance {
+                        entity_path: instance_path.entity_path.to_string(),
+                        instance_id: instance_path.instance.get(),
+                    })
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Callbacks {
+    /// Fired when the selection changes.
+    pub on_selection_change: Rc<dyn Fn(Vec<CallbackSelectionItem>)>,
+
+    /// Fired when a different timeline is selected.
+    pub on_timeline_change: Rc<dyn Fn(Timeline, TimeReal)>,
+
+    /// Fired when the timepoint changes.
+    ///
+    /// Does not fire when `on_seek` is called.
+    pub on_time_update: Rc<dyn Fn(TimeReal)>,
+
+    /// Fired when the timeline is paused.
+    pub on_pause: Rc<dyn Fn()>,
+
+    /// Fired when the timeline is played.
+    pub on_play: Rc<dyn Fn()>,
+}
+
+impl Callbacks {
+    pub fn on_selection_change(&self, items: &ItemCollection) {
+        (self.on_selection_change)(
+            items
+                .iter()
+                .filter_map(|(item, context)| CallbackSelectionItem::new(item, context))
+                .collect(),
+        );
+    }
+
+    pub fn on_timeline_change(&self, timeline: Timeline, time: TimeReal) {
+        (self.on_timeline_change)(timeline, time);
+    }
+
+    pub fn on_time_update(&self, time: TimeReal) {
+        (self.on_time_update)(time);
+    }
+
+    pub fn on_pause(&self) {
+        (self.on_pause)();
+    }
+
+    pub fn on_play(&self) {
+        (self.on_play)();
+    }
+}
