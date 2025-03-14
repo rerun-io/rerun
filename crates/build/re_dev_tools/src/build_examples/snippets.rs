rerun-io/rerun

commit d5482b5bc0f574a079804fc3c42a9ba6d6fe132d

Fix several typing annotations in the SDK and run mypy on snippets (#9260)
### Related

* Some more leftovers from #9209 

### What

Snippets, a key aspect of our documentation, were not typed checked by
mypy so far... 😱 No longer. This lead to several fixes in user-facing
typing annotation in the python SDK. Plus the usual load of fixes in the
snippets themselves.

Note: snippets that are "not runnable" (as per `snippets.toml`) must now
also be mypy-ignore in our `pyproject.toml`. I don't mind this added
complexity a single second: make your snippets runnable! :)

docs/snippets/compare_snippet_output.py
@@ -11,17 +11,21 @@ import os
 import sys
 import time
 from pathlib import Path
+from typing import Any
 
 import tomlkit
+from tomlkit.container import Container
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../scripts/")
 from roundtrip_utils import roundtrip_env, run, run_comparison  # noqa
 
 config_path = Path(__file__).parent / "snippets.toml"
 config = tomlkit.loads(config_path.read_text())
-OPT_OUT_ENTIRELY = config["opt_out"]["run"]
-OPT_OUT_COMPARE = config["opt_out"]["compare"]
-EXTRA_ARGS = config["extra_args"]
+assert isinstance(config["opt_out"], Container)
+
+OPT_OUT_ENTIRELY: dict[str, Any] = config["opt_out"]["run"].value
+OPT_OUT_COMPARE = config["opt_out"]["compare"].value
+EXTRA_ARGS = config["extra_args"].value
 
 
 class Example:
@@ -32,13 +36,13 @@ class Example:
     def opt_out_entirely(self) -> list[str]:
         for key in [self.subdir, self.subdir + "/" + self.name]:
             if key in OPT_OUT_ENTIRELY:
-                return OPT_OUT_ENTIRELY[key]
+                return list(OPT_OUT_ENTIRELY[key])
         return []
 
     def opt_out_compare(self) -> list[str]:
         for key in [self.subdir, self.subdir + "/" + self.name]:
             if key in OPT_OUT_COMPARE:
-                return OPT_OUT_COMPARE[key]
+                return list(OPT_OUT_COMPARE[key])
         return []
 
     def extra_args(self) -> list[str]:

docs/snippets/snippets.toml
@@ -70,6 +70,9 @@ features = [
 
 # These entries won't run at all.
 #
+# NOTE: Non-runnable python snippets will have to also be added the `rerun_py/pyproject.toml`
+# file as mypy excluded.
+#
 # You should only ever use this if the test isn't implemented and cannot yet be implemented
 # for one or more specific SDKs.
 [opt_out.run]

docs/snippets/all/archetypes/scalar_multiple_plots.py
@@ -24,4 +24,4 @@ for t in range(int(tau * 2 * 100.0)):
     rr.log("trig/cos", rr.Scalar(cos(float(t) / 100.0)))
 
     lcg_state = (1140671485 * lcg_state + 128201163) % 16777216  # simple linear congruency generator
-    rr.log("scatter/lcg", rr.Scalar(lcg_state))
+    rr.log("scatter/lcg", rr.Scalar(lcg_state.astype(np.float64)))

docs/snippets/all/archetypes/transform3d_hierarchy.py
@@ -1,4 +1,4 @@
-"""Logs a transforms transform hierarchy."""
+"""Logs a transform hierarchy."""
 
 import numpy as np
 import rerun as rr
@@ -26,7 +26,7 @@ rr.log("sun/planet/moon", rr.Points3D([0.0, 0.0, 0.0], radii=0.15, colors=[180, 
 d_planet = 6.0
 d_moon = 3.0
 angles = np.arange(0.0, 1.01, 0.01) * np.pi * 2
-circle = np.array([np.sin(angles), np.cos(angles), angles * 0.0]).transpose()
+circle = np.array([np.sin(angles), np.cos(angles), angles * 0.0], dtype=np.float32).transpose()
 rr.log("sun/planet_path", rr.LineStrips3D(circle * d_planet))
 rr.log("sun/planet/moon_path", rr.LineStrips3D(circle * d_moon))
 

docs/snippets/all/concepts/different_data_per_timeline.py
@@ -12,12 +12,12 @@ rr.log("points", rr.Points2D([[0, 0], [1, 1]], radii=rr.Radius.ui_points(10.0)))
 # Log a red color on one timeline.
 rr.reset_time()  # Clears all set timeline info.
 rr.set_index("red timeline", timedelta=1.0)
-rr.log("points", [rr.components.Color(0xFF0000FF)])
+rr.log("points", rr.Points2D.from_fields(colors=[255, 0, 0]))
 
 # And a blue color on the other.
 rr.reset_time()  # Clears all set timeline info.
 rr.set_index("blue timeline", sequence=1)
-rr.log("points", [rr.components.Color(0x0000FFFF)])
+rr.log("points", rr.Points2D.from_fields(colors=[0, 0, 255]))
 
 
 # Set view bounds:

docs/snippets/all/concepts/static/log_static.py
@@ -1,1 +1,1 @@
-rr.log("skybox", static=True, generate_skybox_mesh())
+rr.log("skybox", generate_skybox_mesh(), static=True)

docs/snippets/all/concepts/static/log_static_10x.py
@@ -1,2 +1,2 @@
 for _ in range(10):
-    rr.log("camera/image", static=True, camera.save_current_frame())
+    rr.log("camera/image", camera.save_current_frame(), static=True)

rerun_py/pyproject.toml
@@ -208,6 +208,7 @@ norecursedirs = ".* venv* target* build"
 
 [tool.mypy]
 files = [
+  "docs/snippets",
   "rerun_py/rerun_sdk/rerun",
   "rerun_py/rerun_bindings",
   "rerun_py/tests",
@@ -215,7 +216,23 @@ files = [
   "scripts",
   "tests/python",
 ]
-exclude = ["examples/python/objectron", "examples/python/ros_node"]
+exclude = [
+  # snippets that are not runnable
+  "docs/snippets/all/concepts/how_helix_was_logged",
+  "docs/snippets/all/concepts/static/log_static",
+  "docs/snippets/all/concepts/static/log_static_10x",
+  "docs/snippets/all/concepts/static/log_temporal_10x",
+  "docs/snippets/all/concepts/static/send_static",
+  "docs/snippets/all/concepts/static/send_static_10x",
+  "docs/snippets/all/migration/log_line",
+  "docs/snippets/all/tutorials/custom-application-id",
+  "docs/snippets/all/tutorials/custom-recording-id",
+  "docs/snippets/all/tutorials/timelines_example",
+
+  # hopeless examples
+  "examples/python/objectron",
+  "examples/python/ros_node",
+]
 namespace_packages = true
 show_error_codes = true
 strict = true

commit 6630ff796876bcc38b684d73c4d165e43ef1c845

Do not assert on `Container` type in `compare_snippet_output` (#9275)
This assert was introduced in #9260 and is apparently failing in CI. I'm
really not sure why, but anyways it's replaced by a `cast` now.

crates/build/re_dev_tools/src/build_examples/snippets.rs
@@ -85,6 +85,11 @@ fn collect_snippets_recursively(
         let snippet = snippet?;
         let meta = snippet.metadata()?;
         let path = snippet.path();
+
+        if path.file_name().is_some_and(|p| p == "__init__.py") {
+            continue;
+        }
+
         // Compare snippet outputs sometimes leaves orphaned rrd files.
         if path.extension().is_some_and(|p| p == "rrd") {
             continue;

crates/build/re_types_builder/src/codegen/docs/snippets_ref.rs
@@ -398,6 +398,10 @@ fn collect_snippets_recursively<'o>(
         let meta = snippet.metadata()?;
         let path = snippet.path();
 
+        if path.file_name().is_some_and(|name| name == "__init__.py") {
+            continue;
+        }
+
         let name = path.file_stem().unwrap().to_str().unwrap().to_owned();
         let name_qualified = path.strip_prefix(snippet_root_path)?.with_extension("");
         let name_qualified = name_qualified.to_str().unwrap().replace('\\', "/");

docs/snippets/compare_snippet_output.py
@@ -11,7 +11,7 @@ import os
 import sys
 import time
 from pathlib import Path
-from typing import Any
+from typing import Any, cast
 
 import tomlkit
 from tomlkit.container import Container
@@ -21,10 +21,10 @@ from roundtrip_utils import roundtrip_env, run, run_comparison  # noqa
 
 config_path = Path(__file__).parent / "snippets.toml"
 config = tomlkit.loads(config_path.read_text())
-assert isinstance(config["opt_out"], Container)
 
-OPT_OUT_ENTIRELY: dict[str, Any] = config["opt_out"]["run"].value
-OPT_OUT_COMPARE = config["opt_out"]["compare"].value
+OPT_OUT: dict[str, Any] = cast(Container, config["opt_out"])
+OPT_OUT_ENTIRELY: dict[str, Any] = OPT_OUT["run"].value
+OPT_OUT_COMPARE = OPT_OUT["compare"].value
 EXTRA_ARGS = config["extra_args"].value
 
 
@@ -126,6 +126,8 @@ def main() -> None:
         dir = os.path.join(os.path.dirname(__file__), "all")
         for file in glob.glob(dir + "/**", recursive=True):
             name = os.path.basename(file)
+            if name == "__init__.py":
+                continue
             name, extension = os.path.splitext(name)
             if extension == ".cpp" and not args.no_cpp or extension == ".py" and not args.no_py or extension == ".rs":
                 subdir = os.path.relpath(os.path.dirname(file), dir)

docs/snippets/snippets.toml
@@ -76,7 +76,11 @@ features = [
 # You should only ever use this if the test isn't implemented and cannot yet be implemented
 # for one or more specific SDKs.
 [opt_out.run]
-"__init__" = ["py"] # That init file is to make mypy happy.
+"./__init__" = [ # That init file is to make mypy happy.
+  "py",
+  "cpp",
+  "rust",
+]
 "concepts/explicit_recording" = [ # python-specific check
   "cpp",
   "rust",
@@ -214,6 +218,11 @@ features = [
 # You should only ever use this if the test cannot yet be implemented in a way that yields the right
 # data, but you still want to check whether the test runs properly and outputs _something_.
 [opt_out.compare]
+"./__init__" = [ # That init file is to make mypy happy.
+  "py",
+  "cpp",
+  "rust",
+]
 "concepts/explicit_recording" = ["cpp", "rust"] # Only python
 "concepts/how_helix_was_logged" = ["cpp", "rust"] # Only python
 "concepts/different_data_per_timeline" = [

taplo.toml
@@ -9,6 +9,7 @@ include = [
   "rerun_notebook/*.toml",
   "rerun_py/*.toml",
   "tests/rust/**/*.toml",
+  "docs/snippets/**/*.toml",
 ]
 
 [formatting]