89fa8f486f23270212ebccc9c556ff52eff2caa0
Tim Saucer <timsaucer@gmail.com>
2025-03-26 10:37:47 -0400

    Relatively small refactor to make it explicit this struct is used only to find entities in the catalog. Also support filtering

--- crates/store/re_datafusion/src/lib.rs
+++ crates/store/re_datafusion/src/lib.rs
@@ -2,15 +2,15 @@
 
 use std::sync::Arc;
 
+use catalog_find_entries::CatalogFindEntryProvider;
 use datafusion::catalog::TableProvider;
-use grpc_response_provider::GrpcResponseProvider;
 use partition_index_list::PartitionIndexListProvider;
 use partition_list::PartitionListProvider;
 use re_log_types::external::re_tuid::Tuid;
-use re_protos::catalog::v1alpha1::catalog_service_client::CatalogServiceClient;
+use re_protos::catalog::v1alpha1::EntryType;
 use tonic::transport::Channel;
 
-pub mod dataset_catalog_provider;
+pub mod catalog_find_entries;
 pub mod grpc_response_provider;
 pub mod grpc_streaming_provider;
 pub mod partition_index_list;
@@ -30,11 +30,13 @@ impl DataFusionConnector {
 }
 
 impl DataFusionConnector {
-    pub fn get_datasets(&self) -> Arc<dyn TableProvider> {
-        let table_provider: GrpcResponseProvider<CatalogServiceClient<Channel>> =
-            CatalogServiceClient::new(self.channel.clone()).into();
+    pub fn get_all_datasets(&self) -> Arc<dyn TableProvider> {
+        // let table_provider: GrpcResponseProvider<CatalogServiceClient<Channel>> =
+        //     CatalogServiceClient::new(self.channel.clone()).into();
 
-        Arc::new(table_provider)
+        // Arc::new(table_provider)
+        CatalogFindEntryProvider::new(self.channel.clone(), None, None, Some(EntryType::Dataset))
+            .into_provider()
     }
 
     pub fn get_partition_list(&self, tuid: Tuid) -> Arc<dyn TableProvider> {
