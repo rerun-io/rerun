e48aba03d31d7177b99035d438ac7e31136692e2
Jeremy Leibs <jeremy@rerun.io>
2025-03-27 19:11:09 -0400

    Include front-end API for reading tables

--- crates/store/re_protos/src/v1alpha1/rerun.frontend.v1alpha1.rs
+++ crates/store/re_protos/src/v1alpha1/rerun.frontend.v1alpha1.rs
@@ -314,6 +314,29 @@ pub mod frontend_service_client {
             ));
             self.inner.unary(req, path, codec).await
         }
+        pub async fn read_table_entry(
+            &mut self,
+            request: impl tonic::IntoRequest<
+                super::super::super::catalog::v1alpha1::ReadTableEntryRequest,
+            >,
+        ) -> std::result::Result<
+            tonic::Response<super::super::super::catalog::v1alpha1::ReadTableEntryResponse>,
+            tonic::Status,
+        > {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::unknown(format!("Service was not ready: {}", e.into()))
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/rerun.frontend.v1alpha1.FrontendService/ReadTableEntry",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut().insert(GrpcMethod::new(
+                "rerun.frontend.v1alpha1.FrontendService",
+                "ReadTableEntry",
+            ));
+            self.inner.unary(req, path, codec).await
+        }
         /// Register new partitions with the Dataset
         pub async fn register_partitions(
             &mut self,
@@ -499,6 +522,13 @@ pub mod frontend_service_server {
             >,
         ) -> std::result::Result<
             tonic::Response<super::super::super::catalog::v1alpha1::ReadDatasetEntryResponse>,
+            tonic::Status,
+        >;
+        async fn read_table_entry(
+            &self,
+            request: tonic::Request<super::super::super::catalog::v1alpha1::ReadTableEntryRequest>,
+        ) -> std::result::Result<
+            tonic::Response<super::super::super::catalog::v1alpha1::ReadTableEntryResponse>,
             tonic::Status,
         >;
         /// Register new partitions with the Dataset
@@ -802,6 +832,52 @@ pub mod frontend_service_server {
                     let inner = self.inner.clone();
                     let fut = async move {
                         let method = ReadDatasetEntrySvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.unary(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/rerun.frontend.v1alpha1.FrontendService/ReadTableEntry" => {
+                    #[allow(non_camel_case_types)]
+                    struct ReadTableEntrySvc<T: FrontendService>(pub Arc<T>);
+                    impl<T: FrontendService>
+                        tonic::server::UnaryService<
+                            super::super::super::catalog::v1alpha1::ReadTableEntryRequest,
+                        > for ReadTableEntrySvc<T>
+                    {
+                        type Response =
+                            super::super::super::catalog::v1alpha1::ReadTableEntryResponse;
+                        type Future = BoxFuture<tonic::Response<Self::Response>, tonic::Status>;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<
+                                super::super::super::catalog::v1alpha1::ReadTableEntryRequest,
+                            >,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                <T as FrontendService>::read_table_entry(&inner, request).await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let method = ReadTableEntrySvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
