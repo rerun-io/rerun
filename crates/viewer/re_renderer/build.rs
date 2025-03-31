rerun-io/rerun

commit 8ca943fa2df8440050f9b350823e11eddbbbda38

Fix Bazel build: shader reloading only on in Rerun workspace (#9414)
### Related
* Closes https://github.com/rerun-io/rerun/issues/9368

### What
Hot shader reloading is only interesting for us developers.

I've tested that:
* It still works in-repo in debug builds
* It still does not work in release builds

crates/viewer/re_renderer/build.rs
@@ -19,7 +19,9 @@ use std::path::{Path, PathBuf};
 use anyhow::{bail, ensure, Context as _};
 use walkdir::{DirEntry, WalkDir};
 
-use re_build_tools::{get_and_track_env_var, rerun_if_changed, write_file_if_necessary};
+use re_build_tools::{
+    get_and_track_env_var, rerun_if_changed, write_file_if_necessary, Environment,
+};
 
 // ---
 
@@ -101,11 +103,10 @@ fn check_hermeticity(root_path: impl AsRef<Path>, file_path: impl AsRef<Path>) {
 
 // ---
 
-fn should_run() -> bool {
+fn should_run(environment: Environment) -> bool {
     #![allow(clippy::match_same_arms)]
-    use re_build_tools::Environment;
 
-    match Environment::detect() {
+    match environment {
         // we should have been run before publishing
         Environment::PublishingCrates => false,
 
@@ -119,13 +120,23 @@ fn should_run() -> bool {
 }
 
 fn main() {
+    let environment = Environment::detect();
+
     cfg_aliases::cfg_aliases! {
         native: { not(target_arch = "wasm32") },
         web: { target_arch = "wasm32" },
-        load_shaders_from_disk: { all(native, debug_assertions) } // Shader reloading is only supported on native-debug currently.
     }
 
-    if !should_run() {
+    println!("cargo::rustc-check-cfg=cfg(load_shaders_from_disk)");
+    if environment == Environment::DeveloperInWorkspace
+        && cfg!(debug_assertions)
+        && !cfg!(target_arch = "wasm32")
+    {
+        // Enable hot shader reloading:
+        println!("cargo:rustc-cfg=load_shaders_from_disk");
+    }
+
+    if !should_run(environment) {
         return;
     }
 

commit 0640a11e829a2a9a13a863222cf9ae57f0f72a5c

Fix `pixi run rerun-web`
I broke this in https://github.com/rerun-io/rerun/pull/9414

crates/viewer/re_renderer/build.rs
@@ -121,6 +121,10 @@ fn should_run(environment: Environment) -> bool {
 
 fn main() {
     let environment = Environment::detect();
+    let is_release = cfg!(not(debug_assertions)); // This works
+
+    // DO NOT USE `cfg!` for this, that would give you the host's platform!
+    let targets_wasm = get_and_track_env_var("CARGO_CFG_TARGET_FAMILY").unwrap() == "wasm";
 
     cfg_aliases::cfg_aliases! {
         native: { not(target_arch = "wasm32") },
@@ -128,10 +132,7 @@ fn main() {
     }
 
     println!("cargo::rustc-check-cfg=cfg(load_shaders_from_disk)");
-    if environment == Environment::DeveloperInWorkspace
-        && cfg!(debug_assertions)
-        && !cfg!(target_arch = "wasm32")
-    {
+    if environment == Environment::DeveloperInWorkspace && !is_release && !targets_wasm {
         // Enable hot shader reloading:
         println!("cargo:rustc-cfg=load_shaders_from_disk");
     }
@@ -219,10 +220,6 @@ pub fn init() {
         // by the other!
         let virtpath = entry.path().strip_prefix(&manifest_path).unwrap();
         let virtpath = virtpath.to_str().unwrap().replace('\\', "/"); // Force slashes on Windows.
-
-        let is_release = cfg!(not(debug_assertions));
-        // DO NOT USE `cfg!` for this, that would give you the host's platform!
-        let targets_wasm = get_and_track_env_var("CARGO_CFG_TARGET_FAMILY").unwrap() == "wasm";
 
         // Make sure we're not referencing anything outside of the workspace!
         //