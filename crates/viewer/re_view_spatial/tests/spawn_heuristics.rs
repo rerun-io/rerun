rerun-io/rerun

commit b7260548c1aa02888104b923fdfb9bcca0fd0886

Speed up 2d spawn heuristic for many entities & fix heuristic creating overlapping views for `EncodedImage` (#9308)
### Related

* Addresses comment in
https://github.com/rerun-io/rerun/issues/8233#issuecomment-2676186007
* Fixes https://github.com/rerun-io/rerun/issues/9131

### What

Fixes excruciating slow heuristic for finding the right view roots for
2d images. This hit very hard for scenes with many entities containing
images.

Fixed heuristic incorrectly handling `EncodedImage` in the process.

For testing I applied this fix to 0.22.1 and tried the RRD provided
[here](https://github.com/rerun-io/rerun/issues/8233#issuecomment-2684545085)
on a local release build. For my M1 Max Mac I get:

Before: `spawn_heuristics` for 2D takes 94ms 😱  
After:`spawn_heuristics` for 2D takes: 0.290ms

(branch with backported fix is
`andreas/0.22.1/andreas/fix-encodedimage-spawn-heurist-and-make-image-view-heuristics-faster`)

crates/viewer/re_view_spatial/src/max_image_dimension_subscriber.rs
@@ -4,15 +4,28 @@ use once_cell::sync::OnceCell;
 use re_chunk_store::{ChunkStore, ChunkStoreSubscriberHandle, PerStoreChunkSubscriber};
 use re_log_types::{EntityPath, StoreId};
 use re_types::{
+    archetypes,
     components::{Blob, ImageFormat, MediaType},
     external::image,
     Component as _, Loggable as _,
 };
 
+bitflags::bitflags! {
+    #[derive(Debug, Clone, Copy, Default)]
+    pub struct ImageTypes: u8 {
+        const IMAGE = 0b0001;
+        const ENCODED_IMAGE = 0b0010;
+        const SEGMENTATION_IMAGE = 0b0100;
+        const DEPTH_IMAGE = 0b1000;
+        const VIDEO = 0b10000;
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 pub struct MaxDimensions {
     pub width: u32,
     pub height: u32,
+    pub image_types: ImageTypes,
 }
 
 /// The size of the largest image and/or video at a given entity path.
@@ -61,10 +74,8 @@ impl PerStoreChunkSubscriber for MaxImageDimensionsStoreSubscriber {
             }
 
             // Handle `Image`, `DepthImage`, `SegmentationImage`…
-            if let Some(all_dimensions) = event
-                .diff
-                .chunk
-                .components()
+            let components = event.diff.chunk.components();
+            if let Some(all_dimensions) = components
                 .get(&ImageFormat::name())
                 .and_then(|per_desc| per_desc.values().next())
             {
@@ -83,6 +94,32 @@ impl PerStoreChunkSubscriber for MaxImageDimensionsStoreSubscriber {
                     max_dim.height = max_dim.height.max(new_dim.height);
                 }
             }
+            // TODO(andreas): this should be part of the ImageFormat component's tag instead.
+            if components.contains_key(&archetypes::Image::descriptor_indicator().component_name) {
+                self.max_dimensions
+                    .entry(event.diff.chunk.entity_path().clone())
+                    .or_default()
+                    .image_types
+                    .insert(ImageTypes::IMAGE);
+            }
+            if components
+                .contains_key(&archetypes::SegmentationImage::descriptor_indicator().component_name)
+            {
+                self.max_dimensions
+                    .entry(event.diff.chunk.entity_path().clone())
+                    .or_default()
+                    .image_types
+                    .insert(ImageTypes::SEGMENTATION_IMAGE);
+            }
+            if components
+                .contains_key(&archetypes::DepthImage::descriptor_indicator().component_name)
+            {
+                self.max_dimensions
+                    .entry(event.diff.chunk.entity_path().clone())
+                    .or_default()
+                    .image_types
+                    .insert(ImageTypes::DEPTH_IMAGE);
+            }
 
             // Handle `ImageEncoded`, `AssetVideo`…
             let blobs = event.diff.chunk.iter_slices::<&[u8]>(Blob::name());
@@ -91,16 +128,23 @@ impl PerStoreChunkSubscriber for MaxImageDimensionsStoreSubscriber {
                 itertools::izip!(blobs, media_types.map(Some).chain(std::iter::repeat(None)))
             {
                 if let Some(blob) = blob.first() {
-                    if let Some([width, height]) = size_from_blob(
-                        blob,
-                        media_type.and_then(|v| v.first().map(|v| MediaType(v.clone().into()))),
-                    ) {
+                    let media_type =
+                        media_type.and_then(|v| v.first().map(|v| MediaType(v.clone().into())));
+                    let Some(media_type) = MediaType::or_guess_from_data(media_type, blob) else {
+                        continue;
+                    };
+
+                    if let Some([width, height]) = size_from_blob(blob, &media_type) {
                         let max_dim = self
                             .max_dimensions
                             .entry(event.diff.chunk.entity_path().clone())
                             .or_default();
                         max_dim.width = max_dim.width.max(width);
                         max_dim.height = max_dim.height.max(height);
+
+                        // TODO(andreas): this should be part of the Blob component's tag instead.
+                        max_dim.image_types.insert(ImageTypes::ENCODED_IMAGE);
+                        max_dim.image_types.insert(ImageTypes::VIDEO);
                     }
                 }
             }
@@ -108,10 +152,8 @@ impl PerStoreChunkSubscriber for MaxImageDimensionsStoreSubscriber {
     }
 }
 
-fn size_from_blob(blob: &[u8], media_type: Option<MediaType>) -> Option<[u32; 2]> {
+fn size_from_blob(blob: &[u8], media_type: &MediaType) -> Option<[u32; 2]> {
     re_tracing::profile_function!();
-
-    let media_type = MediaType::or_guess_from_data(media_type, blob)?;
 
     if media_type.is_image() {
         re_tracing::profile_scope!("image");
@@ -134,7 +176,7 @@ fn size_from_blob(blob: &[u8], media_type: Option<MediaType>) -> Option<[u32; 2]
         reader.into_dimensions().ok().map(|size| size.into())
     } else if media_type.is_video() {
         re_tracing::profile_scope!("video");
-        re_video::VideoData::load_from_bytes(blob, &media_type)
+        re_video::VideoData::load_from_bytes(blob, media_type)
             .ok()
             .map(|video| video.dimensions())
     } else {

crates/viewer/re_view_spatial/src/view_2d.rs
@@ -1,13 +1,10 @@
-use ahash::HashSet;
 use nohash_hasher::{IntMap, IntSet};
 
 use re_entity_db::{EntityDb, EntityTree};
 use re_log_types::EntityPath;
-use re_types::View as _;
 use re_types::{
-    archetypes::{DepthImage, Image},
     blueprint::archetypes::{Background, NearClipPlane, VisualBounds2D},
-    ComponentName, ViewClassIdentifier,
+    View as _, ViewClassIdentifier,
 };
 use re_ui::{Help, UiExt as _};
 use re_view::view_property_ui;
@@ -20,7 +17,7 @@ use re_viewer_context::{
 use crate::{
     contexts::register_spatial_contexts,
     heuristics::default_visualized_entities_for_visualizer_kind,
-    max_image_dimension_subscriber::MaxDimensions,
+    max_image_dimension_subscriber::{ImageTypes, MaxDimensions},
     spatial_topology::{SpatialTopology, SubSpaceConnectionFlags},
     ui::SpatialViewState,
     view_kind::SpatialViewKind,
@@ -273,78 +270,57 @@ impl ViewClass for SpatialView2D {
     }
 }
 
-// Count the number of image entities with the given component exist that aren't
-// children of other entities in the bucket.
-fn count_non_nested_images_with_component(
-    image_dimensions: &IntMap<EntityPath, MaxDimensions>,
-    entity_bucket: &IntSet<EntityPath>,
-    entity_db: &re_entity_db::EntityDb,
-    subtree: &EntityTree,
-    component_name: &ComponentName,
-) -> usize {
-    if image_dimensions.contains_key(&subtree.path) {
-        // bool true -> 1
-        entity_db
-            .storage_engine()
-            .store()
-            .entity_has_component(&subtree.path, component_name) as usize
-    } else if !entity_bucket
-        .iter()
-        .any(|e| e.is_descendant_of(&subtree.path))
-    {
-        0 // early-out optimization
-    } else {
-        subtree
-            .children
-            .values()
-            .map(|child| {
-                count_non_nested_images_with_component(
-                    image_dimensions,
-                    entity_bucket,
-                    entity_db,
-                    child,
-                    component_name,
-                )
-            })
-            .sum()
-    }
+#[derive(Default, Debug)]
+struct ImageCounts {
+    image: usize,
+    encoded_image: usize,
+    depth: usize,
+    video: usize,
+    // Don't need segmentation image since we allow them to be stacked.
 }
 
-// Find the image dimensions of every image-entity in the bucket that is not
+// Find the shared image dimensions of every image-entity that is not
 // not nested under another image.
-//
-// We track a set of just height/width as different channels could be allowed to
-// stack.
-fn find_non_nested_image_dimensions(
+fn has_single_shared_image_dimensionn(
     image_dimensions: &IntMap<EntityPath, MaxDimensions>,
-    entity_bucket: &IntSet<EntityPath>,
     subtree: &EntityTree,
-    found_image_dimensions: &mut HashSet<[u64; 2]>,
-) {
-    if let Some(dimensions) = image_dimensions.get(&subtree.path) {
-        // If we found an image entity, add its dimensions to the set.
-        found_image_dimensions.insert([dimensions.height as _, dimensions.width as _]);
-    } else if entity_bucket
-        .iter()
-        .any(|e| e.is_descendant_of(&subtree.path))
-    {
-        // Otherwise recurse
-        for child in subtree.children.values() {
-            find_non_nested_image_dimensions(
-                image_dimensions,
-                entity_bucket,
-                child,
-                found_image_dimensions,
-            );
+    image_counts: &mut ImageCounts,
+) -> bool {
+    let mut image_dimension = None;
+
+    let mut pending_subtrees = vec![subtree];
+
+    while let Some(subtree) = pending_subtrees.pop() {
+        if let Some(dimensions) = image_dimensions.get(&subtree.path) {
+            let new_dimension = [dimensions.height, dimensions.width];
+            if let Some(existing_dimension) = image_dimension {
+                if existing_dimension != new_dimension {
+                    return false;
+                }
+            } else {
+                image_dimension = Some(new_dimension);
+            }
+
+            image_counts.image += dimensions.image_types.contains(ImageTypes::IMAGE) as usize;
+            image_counts.encoded_image +=
+                dimensions.image_types.contains(ImageTypes::ENCODED_IMAGE) as usize;
+            image_counts.depth += dimensions.image_types.contains(ImageTypes::DEPTH_IMAGE) as usize;
+            image_counts.video += dimensions.image_types.contains(ImageTypes::VIDEO) as usize;
+
+            // Ignore any nested images.
+        } else {
+            pending_subtrees.extend(subtree.children.values());
         }
     }
+
+    true
 }
 
 fn recommended_views_with_image_splits(
     ctx: &ViewerContext<'_>,
     image_dimensions: &IntMap<EntityPath, MaxDimensions>,
     recommended_root: &EntityPath,
-    entities: &IntSet<EntityPath>,
+    visualizable_entities: &IntSet<EntityPath>,
     recommended: &mut Vec<RecommendedView>,
 ) {
     re_tracing::profile_function!();
@@ -358,43 +334,16 @@ fn recommended_views_with_image_splits(
         return;
     };
 
-    let mut found_image_dimensions = Default::default();
+    let mut image_counts = ImageCounts::default();
 
-    find_non_nested_image_dimensions(
-        image_dimensions,
-        entities,
-        subtree,
-        &mut found_image_dimensions,
-    );
-
-    let image_count = count_non_nested_images_with_component(
-        image_dimensions,
-        entities,
-        ctx.recording(),
-        subtree,
-        &Image::descriptor_indicator().component_name,
-    );
-
-    let depth_count = count_non_nested_images_with_component(
-        image_dimensions,
-        entities,
-        ctx.recording(),
-        subtree,
-        &DepthImage::descriptor_indicator().component_name,
-    );
-
-    let video_count = count_non_nested_images_with_component(
-        image_dimensions,
-        entities,
-        ctx.recording(),
-        subtree,
-        &re_types::archetypes::AssetVideo::descriptor_indicator().component_name,
-    );
-
-    let all_have_same_size = found_image_dimensions.len() <= 1;
+    // Note that since this only finds entities with image dimensions, it naturally filters for `visualizable_entities`.
+    let all_have_same_size =
+        has_single_shared_image_dimensionn(image_dimensions, subtree, &mut image_counts);
 
     // NOTE: we allow stacking segmentation images, since that can be quite useful sometimes.
-    let overlap = all_have_same_size && image_count + video_count <= 1 && depth_count <= 1;
+    let overlap = all_have_same_size
+        && image_counts.encoded_image + image_counts.image + image_counts.video <= 1
+        && image_counts.depth <= 1;
 
     if overlap {
         // If there are multiple images of the same size but of different types, then we can overlap them on top of each other.
@@ -405,27 +354,19 @@ fn recommended_views_with_image_splits(
 
         // If the root also had a visualizable entity, give it its own space.
         // TODO(jleibs): Maybe merge this entity into each child
-        if entities.contains(recommended_root) {
+        if visualizable_entities.contains(recommended_root) {
             recommended.push(RecommendedView::new_single_entity(recommended_root.clone()));
         }
 
         // And then recurse into the children
         for child in subtree.children.values() {
-            let sub_bucket: IntSet<_> = entities
-                .iter()
-                .filter(|e| e.starts_with(&child.path))
-                .cloned()
-                .collect();
-
-            if !sub_bucket.is_empty() {
-                recommended_views_with_image_splits(
-                    ctx,
-                    image_dimensions,
-                    &child.path,
-                    &sub_bucket,
-                    recommended,
-                );
-            }
+            recommended_views_with_image_splits(
+                ctx,
+                image_dimensions,
+                &child.path,
+                visualizable_entities,
+                recommended,
+            );
         }
     }
 }

commit f70fa42261d03d2605f36f74aa06452cc51840e1

Fix 2D image split heuristic (#9439)
### Related

* Fixes https://github.com/rerun-io/rerun/issues/9363
* Regression caused by https://github.com/rerun-io/rerun/pull/9308

### What

As far as I can tell the main breakage occurred because previously we
tracked which visualizable entities remain in the subtree during image
split computation. That's fixed by stopping image split detection when
there's no images anymore (duh! ;-)). There was another issue where we'd
count videos and encoded images both as each other additionally.

Adds a simple snapshot test for the spawn heuristic which was confirmed
catching this! First of its kind 😱

crates/store/re_log_types/src/path/entity_path_filter.rs
@@ -360,11 +360,8 @@ impl EntityPathFilter {
     /// Creates a filter that accepts everything.
     pub fn all() -> Self {
         Self {
-            rules: std::iter::once((
-                EntityPathRule::exact_entity(&EntityPath::root()),
-                RuleEffect::Include,
-            ))
-            .collect(),
+            rules: std::iter::once((EntityPathRule::including_subtree(""), RuleEffect::Include))
+                .collect(),
         }
     }
 

crates/viewer/re_view_spatial/Cargo.toml
@@ -69,6 +69,7 @@ re_viewer_context = { workspace = true, features = ["testing"] }
 re_viewport.workspace = true
 re_viewport_blueprint = { workspace = true, features = ["testing"] }
 
+insta.workspace = true
 ndarray.workspace = true
 
 [lib]

crates/viewer/re_view_spatial/src/max_image_dimension_subscriber.rs
@@ -143,8 +143,11 @@ impl PerStoreChunkSubscriber for MaxImageDimensionsStoreSubscriber {
                         max_dim.height = max_dim.height.max(height);
 
                         // TODO(andreas): this should be part of the Blob component's tag instead.
-                        max_dim.image_types.insert(ImageTypes::ENCODED_IMAGE);
-                        max_dim.image_types.insert(ImageTypes::VIDEO);
+                        if media_type.is_image() {
+                            max_dim.image_types.insert(ImageTypes::ENCODED_IMAGE);
+                        } else if media_type.is_video() {
+                            max_dim.image_types.insert(ImageTypes::VIDEO);
+                        }
                     }
                 }
             }

crates/viewer/re_view_spatial/src/view_2d.rs
@@ -276,12 +276,26 @@ impl ViewClass for SpatialView2D {
 }
 
 #[derive(Default, Debug)]
-struct ImageCounts {
+struct NonNestedImageCounts {
     image: usize,
     encoded_image: usize,
     depth: usize,
     video: usize,
-    // Don't need segmentation image since we allow them to be stacked.
+    segmentation: usize,
+}
+
+impl NonNestedImageCounts {
+    fn has_any_images(&self) -> bool {
+        let Self {
+            image,
+            encoded_image,
+            depth,
+            video,
+            segmentation,
+        } = self;
+
+        *image > 0 || *encoded_image > 0 || *depth > 0 || *video > 0 || *segmentation > 0
+    }
 }
 
 // Find the shared image dimensions of every image-entity that is not
@@ -289,7 +303,7 @@ struct ImageCounts {
 fn has_single_shared_image_dimensionn(
     image_dimensions: &IntMap<EntityPath, MaxDimensions>,
     subtree: &EntityTree,
-    image_counts: &mut ImageCounts,
+    non_nested_image_counts: &mut NonNestedImageCounts,
 ) -> bool {
     let mut image_dimension = None;
 
@@ -306,11 +320,18 @@ fn has_single_shared_image_dimensionn(
                 image_dimension = Some(new_dimension);
             }
 
-            image_counts.image += dimensions.image_types.contains(ImageTypes::IMAGE) as usize;
-            image_counts.encoded_image +=
+            non_nested_image_counts.image +=
+                dimensions.image_types.contains(ImageTypes::IMAGE) as usize;
+            non_nested_image_counts.encoded_image +=
                 dimensions.image_types.contains(ImageTypes::ENCODED_IMAGE) as usize;
-            image_counts.depth += dimensions.image_types.contains(ImageTypes::DEPTH_IMAGE) as usize;
-            image_counts.video += dimensions.image_types.contains(ImageTypes::VIDEO) as usize;
+            non_nested_image_counts.depth +=
+                dimensions.image_types.contains(ImageTypes::DEPTH_IMAGE) as usize;
+            non_nested_image_counts.video +=
+                dimensions.image_types.contains(ImageTypes::VIDEO) as usize;
+            non_nested_image_counts.segmentation += dimensions
+                .image_types
+                .contains(ImageTypes::SEGMENTATION_IMAGE)
+                as usize;
 
             // Ignore any nested images.
         } else {
@@ -318,7 +339,7 @@ fn has_single_shared_image_dimensionn(
         }
     }
 
-    true
+    image_dimension.is_some()
 }
 
 fn recommended_views_with_image_splits(
@@ -339,18 +360,23 @@ fn recommended_views_with_image_splits(
         return;
     };
 
-    let mut image_counts = ImageCounts::default();
+    let mut image_counts = NonNestedImageCounts::default();
 
     // Note that since this only finds entities with image dimensions, it naturally filters for `visualizable_entities`.
     let all_have_same_size =
         has_single_shared_image_dimensionn(image_dimensions, subtree, &mut image_counts);
+    if !image_counts.has_any_images() {
+        // This utility is all about finding views with *image* splits.
+        // If there's no images in this subtree, we're done.
+        return;
+    }
 
     // NOTE: we allow stacking segmentation images, since that can be quite useful sometimes.
-    let overlap = all_have_same_size
+    let has_desired_image_overlap = all_have_same_size
         && image_counts.encoded_image + image_counts.image + image_counts.video <= 1
         && image_counts.depth <= 1;
 
-    if overlap {
+    if has_desired_image_overlap {
         // If there are multiple images of the same size but of different types, then we can overlap them on top of each other.
         // This can be useful for comparing a segmentation image on top of an RGB image, for instance.
         recommended.push(RecommendedView::new_subtree(recommended_root.clone()));

crates/viewer/re_viewer_context/src/view/spawn_heuristics.rs
@@ -13,7 +13,7 @@ pub struct RecommendedView {
 /// Provides information in order to decide whether to spawn a views, putting them in relationship to others and spawning them.
 // TODO(andreas): allow bucketing decisions for 0-n buckets for recommended views.
 // TODO(andreas): Should `ViewClassLayoutPriority` be part of this struct?
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct ViewSpawnHeuristics {
     /// The recommended views to spawn
     recommended_views: Vec<RecommendedView>,

crates/viewer/re_view_spatial/tests/snapshots/spawn_heuristics__detect_and_track_objects_like_scene_2d_view_heuristic.snap
@@ -1,0 +1,28 @@
+---
+source: crates/viewer/re_view_spatial/tests/spawn_heuristics.rs
+expression: recommended_views
+---
+ViewSpawnHeuristics {
+    recommended_views: [
+        RecommendedView {
+            origin: /segmentation,
+            query_filter: EntityPathFilter {
+                rules: {
+                    EntityPathRule(
+                        "$origin/**",
+                    ): Include,
+                },
+            },
+        },
+        RecommendedView {
+            origin: /video,
+            query_filter: EntityPathFilter {
+                rules: {
+                    EntityPathRule(
+                        "$origin/**",
+                    ): Include,
+                },
+            },
+        },
+    ],
+}

crates/viewer/re_view_spatial/tests/spawn_heuristics.rs
@@ -1,0 +1,108 @@
+use re_log_types::Timeline;
+use re_types::archetypes;
+use re_viewer_context::{test_context::TestContext, ViewClass as _};
+
+use ndarray::{Array, ShapeBuilder as _};
+
+// Creates a 2D scene that mimics the `detect_and_track_objects` example.
+#[allow(clippy::unwrap_used)]
+fn build_2d_scene(test_context: &mut TestContext) {
+    let timeline_step = Timeline::new_sequence("step");
+    let time = [(timeline_step, 1)];
+
+    const IMAGE_WIDTH: usize = 64;
+    const IMAGE_HEIGHT: usize = 48;
+
+    test_context.log_entity("segmentation/rgb_scaled".into(), |builder| {
+        builder.with_archetype(
+            re_types::RowId::new(),
+            time,
+            &archetypes::Image::from_color_model_and_tensor(
+                re_types::datatypes::ColorModel::RGB,
+                Array::<u8, _>::zeros((IMAGE_HEIGHT, IMAGE_WIDTH, 3).f()),
+            )
+            .unwrap(),
+        )
+    });
+    test_context.log_entity("segmentation".into(), |builder| {
+        builder.with_archetype(
+            re_types::RowId::new(),
+            time,
+            &archetypes::SegmentationImage::try_from(Array::<u8, _>::zeros(
+                (IMAGE_HEIGHT, IMAGE_WIDTH).f(),
+            ))
+            .unwrap(),
+        )
+    });
+    test_context.log_entity("segmentation/detections/things".into(), |builder| {
+        builder.with_archetype(
+            re_types::RowId::new(),
+            time,
+            &archetypes::Boxes2D::from_centers_and_half_sizes(
+                [(5.0, 5.0), (10.0, 10.0), (15.0, 15.0)],
+                [(10.0, 10.0), (10.0, 10.0), (10.0, 10.0)],
+            ),
+        )
+    });
+
+    test_context.log_entity("video".into(), |builder| {
+        builder
+            .with_archetype(
+                re_types::RowId::new(),
+                time,
+                &archetypes::AssetVideo::from_file_path("../../../tests/assets/empty.mp4").unwrap(),
+            )
+            .with_archetype(
+                re_types::RowId::new(),
+                time,
+                &archetypes::VideoFrameReference::new(0),
+            )
+    });
+    for i in 0..3 {
+        test_context.log_entity(format!("video/tracked/{i}").into(), |builder| {
+            builder.with_archetype(
+                re_types::RowId::new(),
+                time,
+                &archetypes::Boxes2D::from_centers_and_half_sizes(
+                    [(i as f32, i as f32)],
+                    [(1.0, 1.0)],
+                ),
+            )
+        });
+    }
+
+    // Since we haven't registered the text view, it won't show up in automatically generated views at all.
+    // This is just here to mimic an entity the 2D spatial view can't handle at all.
+    test_context.log_entity("description".into(), |builder| {
+        builder.with_archetype(
+            re_types::RowId::new(),
+            re_log_types::TimePoint::default(),
+            &archetypes::TextDocument::new("test document"),
+        )
+    });
+}
+
+#[test]
+fn test_spatial_view_2d_spawn_heuristics() {
+    let mut test_context = TestContext::default();
+    // It's important to first register the view class before adding any entities,
+    // otherwise the `VisualizerEntitySubscriber` for our visualizers doesn't exist yet,
+    // and thus will not find anything applicable to the visualizer.
+    test_context.register_view_class::<re_view_spatial::SpatialView2D>();
+
+    build_2d_scene(&mut test_context);
+
+    let view_class = test_context
+        .view_class_registry
+        .get_class_or_log_error(re_view_spatial::SpatialView2D::identifier());
+
+    test_context.run_in_egui_central_panel(|ctx, _ui| {
+        let recommended_views =
+            view_class.spawn_heuristics(ctx, &re_log_types::ResolvedEntityPathFilter::properties());
+
+        insta::assert_debug_snapshot!(
+            "detect_and_track_objects_like_scene_2d_view_heuristic",
+            recommended_views
+        );
+    });
+}

crates/viewer/re_viewer_context/src/test_context.rs
@@ -315,6 +315,9 @@ impl TestContext {
         let indicated_entities_per_visualizer = self
             .view_class_registry
             .indicated_entities_per_visualizer(&store_context.recording.store_id());
+        let maybe_visualizable_entities_per_visualizer = self
+            .view_class_registry
+            .maybe_visualizable_entities_for_visualizer_systems(&self.recording_store.store_id());
 
         let drag_and_drop_manager = crate::DragAndDropManager::new(ItemCollection::default());
 
@@ -341,7 +344,7 @@ impl TestContext {
                 render_ctx,
             },
             store_context: &store_context,
-            maybe_visualizable_entities_per_visualizer: &Default::default(),
+            maybe_visualizable_entities_per_visualizer: &maybe_visualizable_entities_per_visualizer,
             indicated_entities_per_visualizer: &indicated_entities_per_visualizer,
             query_results: &self.query_results,
             rec_cfg: &self.recording_config,

crates/viewer/re_viewport_blueprint/src/test_context_ext.rs
@@ -61,11 +61,6 @@ impl TestContextExt for TestContext {
                 let viewport_blueprint =
                     ViewportBlueprint::try_from_db(&self.blueprint_store, &self.blueprint_query);
 
-                let maybe_visualizable_entities_per_visualizer = self
-                    .view_class_registry
-                    .maybe_visualizable_entities_for_visualizer_systems(
-                        &self.recording_store.store_id(),
-                    );
                 let mut query_results = HashMap::default();
 
                 self.run(egui_ctx, |ctx| {
@@ -81,7 +76,7 @@ impl TestContextExt for TestContext {
                                 .class(class_identifier)
                                 .unwrap_or_else(|| panic!("The class '{class_identifier}' must be registered beforehand"))
                                 .determine_visualizable_entities(
-                                    &maybe_visualizable_entities_per_visualizer,
+                                    ctx.maybe_visualizable_entities_per_visualizer,
                                     ctx.recording(),
                                     &ctx.view_class_registry()
                                         .new_visualizer_collection(class_identifier),
@@ -102,7 +97,7 @@ impl TestContextExt for TestContext {
                             let resolver = DataQueryPropertyResolver::new(
                                 view_blueprint,
                                 ctx.view_class_registry(),
-                                &maybe_visualizable_entities_per_visualizer,
+                                ctx.maybe_visualizable_entities_per_visualizer,
                                 &visualizable_entities,
                                 &indicated_entities_per_visualizer,
                             );