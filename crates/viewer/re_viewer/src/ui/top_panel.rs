1fb5481a7cc04b2ed4b3a3c23155c8a7247b4d9a
lucasmerlin <hi@lucasmerlin.me>
2025-04-02 09:09:43 +0200

    Remove back button

--- crates/viewer/re_viewer/src/ui/top_panel.rs
+++ crates/viewer/re_viewer/src/ui/top_panel.rs
@@ -86,17 +86,6 @@ fn top_bar_ui(
 ) {
     app.rerun_menu_button_ui(frame.wgpu_render_state(), store_context, ui);
 
-    if app.state.display_mode != DisplayMode::LocalRecordings
-        && store_context
-            .as_ref()
-            .is_some_and(|h| h.app_id != StoreHub::welcome_screen_app_id())
-    {
-        ui.add_space(12.0);
-        if ui.small_icon_button(&icons::ARROW_LEFT).clicked() {
-            app.state.display_mode = DisplayMode::LocalRecordings;
-        }
-    }
-
     ui.add_space(12.0);
     website_link_ui(ui);
 
