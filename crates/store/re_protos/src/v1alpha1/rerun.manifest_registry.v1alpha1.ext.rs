bc13454cfa94db79a4c5504e24cd0f1af6df53b7
Zeljko Mihaljcic <7150613+zehiko@users.noreply.github.com>
2025-04-03 16:24:15 +0200

    add entity paths as param to GetChunksRequest

--- crates/store/re_protos/src/v1alpha1/rerun.manifest_registry.v1alpha1.ext.rs
+++ crates/store/re_protos/src/v1alpha1/rerun.manifest_registry.v1alpha1.ext.rs
@@ -165,6 +165,57 @@ impl TryFrom<crate::manifest_registry::v1alpha1::Query> for Query {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct GetChunksRequest {
+    pub entry: crate::common::v1alpha1::ext::DatasetHandle,
+    pub partition_ids: Vec<crate::common::v1alpha1::ext::PartitionId>,
+    pub chunk_ids: Vec<re_chunk::ChunkId>,
+    pub entity_paths: Vec<EntityPath>,
+    pub query: Option<Query>,
+}
+
+impl TryFrom<crate::manifest_registry::v1alpha1::GetChunksRequest> for GetChunksRequest {
+    type Error = tonic::Status;
+
+    fn try_from(
+        value: crate::manifest_registry::v1alpha1::GetChunksRequest,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            entry: value
+                .entry
+                .ok_or_else(|| tonic::Status::invalid_argument("entry is required"))?
+                .try_into()?,
+
+            partition_ids: value
+                .partition_ids
+                .into_iter()
+                .map(TryInto::try_into)
+                .collect::<Result<Vec<_>, _>>()?,
+
+            chunk_ids: value
+                .chunk_ids
+                .into_iter()
+                .map(|tuid| {
+                    let id: re_tuid::Tuid = tuid.try_into()?;
+                    Ok::<_, tonic::Status>(re_chunk::ChunkId::from_u128(id.as_u128()))
+                })
+                .collect::<Result<Vec<_>, _>>()?,
+
+            entity_paths: value
+                .entity_paths
+                .into_iter()
+                .map(|path| {
+                    path.try_into().map_err(|err| {
+                        tonic::Status::invalid_argument(format!("invalid entity path: {err}"))
+                    })
+                })
+                .collect::<Result<Vec<_>, _>>()?,
+
+            query: value.query.map(|q| q.try_into()).transpose()?,
+        })
+    }
+}
+
 /// A `ComponentDescriptor` meant for querying: all fields are optional.
 ///
 /// This acts as a pattern matcher.
