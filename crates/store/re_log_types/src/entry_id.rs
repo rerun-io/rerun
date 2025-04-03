aef47a8c896d69e7250ef8d8ecf94e3eee2a19a2
Antoine Beyeler <antoine@rerun.io>
2025-04-03 12:21:43 +0200

    review comments

--- crates/store/re_log_types/src/entry_id.rs
+++ crates/store/re_log_types/src/entry_id.rs
@@ -1,3 +1,4 @@
+/// The id for an entry (i.e. a dataset or a table) in a remote catalog.
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[serde(transparent)]
@@ -5,14 +6,9 @@ pub struct EntryId {
     pub id: re_tuid::Tuid,
 }
 
-impl Default for EntryId {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 impl EntryId {
     #[inline]
+    #[expect(clippy::new_without_default)]
     pub fn new() -> Self {
         Self {
             id: re_tuid::Tuid::new(),
