e7b5e0b9ad2268f6bb91a823084aaec619f73387
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2025-04-07 09:59:53 +0200

    Update rerun_py/rerun_bindings/rerun_bindings.pyi
    Co-authored-by: Andreas Reich <andreas@rerun.io>

--- rerun_py/rerun_bindings/rerun_bindings.pyi
+++ rerun_py/rerun_bindings/rerun_bindings.pyi
@@ -1122,7 +1122,7 @@ class DataframeQueryView:
 
     def filter_range_nanos(self, start: int, end: int) -> Self:
         """
-        Filter the view to only include data between the given index values expressed as seconds.
+        Filter the view to only include data between the given index values expressed as nanoseconds.
 
         This range is inclusive and will contain both the value at the start and the value at the end.
 
