b801806c400a36125cca9812ef302ef17becc725
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-04-13 11:24:56 +0200

    Replace uses of `arrow2` in `rerun_c` with `arrow-rs`

--- crates/top/rerun_c/src/arrow_utils.rs
+++ crates/top/rerun_c/src/arrow_utils.rs
@@ -1,32 +1,17 @@
+use arrow::ffi::FFI_ArrowArray;
+
 use crate::{CError, CErrorCode};
 
 /// Converts a C-FFI arrow array into a Rust component batch, taking ownership of the underlying arrow data.
 ///
-/// Safety:
-/// This must only be ever called once for a given ffi array.
-/// Conceptually, this takes ownership of the array, i.e. this should really be a move operation,
-/// but since we have typically pass c arrays (ptr + length), we can't actually move out data.
+/// ### Safety
+/// This struct assumes that the incoming data agrees with the C data interface.
 #[allow(unsafe_code)]
 #[allow(clippy::result_large_err)]
 pub unsafe fn arrow_array_from_c_ffi(
-    array: &arrow2::ffi::ArrowArray,
-    datatype: arrow2::datatypes::DataType,
+    array: FFI_ArrowArray,
+    datatype: arrow::datatypes::DataType,
 ) -> Result<arrow::array::ArrayRef, CError> {
-    unsafe { arrow2_array_from_c_ffi(array, datatype).map(|array| array.into()) }
-}
-
-/// Converts a C-FFI arrow array into a Rust component batch, taking ownership of the underlying arrow data.
-///
-/// Safety:
-/// This must only be ever called once for a given ffi array.
-/// Conceptually, this takes ownership of the array, i.e. this should really be a move operation,
-/// but since we have typically pass c arrays (ptr + length), we can't actually move out data.
-#[allow(unsafe_code)]
-#[allow(clippy::result_large_err)]
-unsafe fn arrow2_array_from_c_ffi(
-    array: &arrow2::ffi::ArrowArray,
-    datatype: arrow2::datatypes::DataType,
-) -> Result<Box<dyn arrow2::array::Array>, CError> {
     // Arrow2 implements drop for Arrow2Array and Arrow2Schema.
     //
     // Therefore, for things to work correctly we have to take ownership of the array!
@@ -35,10 +20,12 @@ unsafe fn arrow2_array_from_c_ffi(
     //
     // This makes sense because from here on out we want to manage the lifetime of the underlying schema and array data
     // from the rust side.
-    unsafe { arrow2::ffi::import_array_from_c(std::ptr::read(array), datatype) }.map_err(|err| {
-        CError::new(
-            CErrorCode::ArrowFfiArrayImportError,
-            &format!("Failed to import ffi array: {err}"),
-        )
-    })
+    unsafe { arrow::ffi::from_ffi_and_data_type(array, datatype) }
+        .map(arrow::array::make_array)
+        .map_err(|err| {
+            CError::new(
+                CErrorCode::ArrowFfiArrayImportError,
+                &format!("Failed to import ffi array: {err}"),
+            )
+        })
 }
