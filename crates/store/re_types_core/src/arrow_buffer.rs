43be650b9ab93cd365e13ff5681466d51824b5e1
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-04-14 10:45:09 +0200

    Make `ArrowBuffer::cast_to_u8` zero-copy

--- crates/store/re_types_core/src/arrow_buffer.rs
+++ crates/store/re_types_core/src/arrow_buffer.rs
@@ -62,26 +62,14 @@ impl<T: ArrowNativeType> ArrowBuffer<T> {
 }
 
 impl<T: bytemuck::Pod + ArrowNativeType> ArrowBuffer<T> {
-    /// Cast POD (plain-old-data) types to another POD type.
-    ///
-    /// For instance: cast a buffer of `u8` to a buffer of `f32`.
-    #[inline]
-    pub fn cast_pod<Target: bytemuck::Pod + ArrowNativeType>(
-        &self,
-    ) -> Result<ArrowBuffer<Target>, bytemuck::PodCastError> {
-        // TODO(#3741): when we switch from arrow2, see if we can make this function zero-copy
-        re_tracing::profile_function!();
-        let target_slice: &[Target] = bytemuck::try_cast_slice(self.as_slice())?;
-        Ok(ArrowBuffer::from(target_slice.to_vec()))
-    }
-
-    /// Cast POD (plain-old-data) types to `u8`.
+    /// Reinterpret POD (plain-old-data) types to `u8`.
     #[inline]
     pub fn cast_to_u8(&self) -> ArrowBuffer<u8> {
-        match self.cast_pod() {
-            Ok(buf) => buf,
-            Err(_) => unreachable!("We can always cast POD types to u8"),
-        }
+        ArrowBuffer::<u8>(arrow::buffer::ScalarBuffer::new(
+            self.0.inner().clone(),
+            0,
+            self.num_instances(),
+        ))
     }
 }
 
