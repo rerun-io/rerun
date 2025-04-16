9be8e59ad4f797d81be5975358ee66cae573750e
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-04-16 20:18:46 +0200

    Run codegen

--- crates/store/re_types/src/archetypes/tensor.rs
+++ crates/store/re_types/src/archetypes/tensor.rs
@@ -25,12 +25,14 @@ use ::re_types_core::{DeserializationError, DeserializationResult};
 /// ### Simple tensor
 /// ```ignore
 /// use ndarray::{Array, ShapeBuilder as _};
+/// use rand::prelude::*;
 ///
 /// fn main() -> Result<(), Box<dyn std::error::Error>> {
 ///     let rec = rerun::RecordingStreamBuilder::new("rerun_example_tensor").spawn()?;
 ///
 ///     let mut data = Array::<u8, _>::default((8, 6, 3, 5).f());
-///     data.map_inplace(|x| *x = rand::random());
+///     let mut rng = rand::rngs::SmallRng::seed_from_u64(42);
+///     data.map_inplace(|x| *x = rng.gen());
 ///
 ///     let tensor =
 ///         rerun::Tensor::try_from(data)?.with_dim_names(["width", "height", "channel", "batch"]);
