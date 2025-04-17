5ced59404387abc2959144116a6f9ec83f1070bc
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-04-17 12:40:26 +0200

    Use approximate equals when comparing floats in snippets (#9750)
    ### Related
    * Closes #3235
    * Part of https://github.com/rerun-io/rerun/issues/9110
    * Unblocks https://github.com/rerun-io/rerun/pull/9751
    * Unblocks https://github.com/rerun-io/rerun/issues/9588
    
    ### What
    In our snippet comparisons, ignore small numeric differences (that often
    occur because of differences in programming language, or
    compiler/interpreter version)
    
    ---------
    
    Co-authored-by: Clement Rey <cr.rey.clement@gmail.com>

--- crates/top/rerun/src/commands/rrd/compare.rs
+++ crates/top/rerun/src/commands/rrd/compare.rs
@@ -83,7 +83,7 @@ impl CompareCommand {
             'outer: for chunk1 in &chunks1 {
                 for chunk2 in chunks2_opt.iter_mut().filter(|c| c.is_some()) {
                     #[allow(clippy::unwrap_used)]
-                    if re_chunk::Chunk::are_similar(chunk1, chunk2.as_ref().unwrap()) {
+                    if re_chunk::Chunk::ensure_similar(chunk1, chunk2.as_ref().unwrap()).is_ok() {
                         *chunk2 = None;
                         continue 'outer;
                     }
@@ -93,18 +93,32 @@ impl CompareCommand {
             }
         }
 
+        fn format_chunk(chunk: &Chunk) -> String {
+            re_format_arrow::format_record_batch_opts(
+                &chunk.to_record_batch().expect("Cannot fail in practice"),
+                &re_format_arrow::RecordBatchFormatOpts {
+                    transposed: false,
+                    width: Some(800),
+                    include_metadata: true,
+                    include_column_metadata: true,
+                },
+            )
+            .to_string()
+        }
+
         if !*unordered || unordered_failed {
             for (chunk1, chunk2) in izip!(chunks1, chunks2) {
-                anyhow::ensure!(
-                    re_chunk::Chunk::are_similar(&chunk1, &chunk2),
-                    "Chunks do not match:\n{}",
-                    similar_asserts::SimpleDiff::from_str(
-                        &format!("{chunk1}"),
-                        &format!("{chunk2}"),
-                        "got",
-                        "expected",
-                    ),
-                );
+                re_chunk::Chunk::ensure_similar(&chunk1, &chunk2).with_context(|| {
+                    format!(
+                        "Chunks diff:\n{}",
+                        similar_asserts::SimpleDiff::from_str(
+                            &format_chunk(&chunk1),
+                            &format_chunk(&chunk2),
+                            "got",
+                            "expected",
+                        ),
+                    )
+                })?;
             }
         }
 
