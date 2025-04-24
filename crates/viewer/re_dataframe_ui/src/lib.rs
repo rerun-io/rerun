4843ea4ef320d9f6582e0cee19ab1f20bc3261aa
Antoine Beyeler <antoine@rerun.io>
2025-04-24 22:09:44 +0200

    Rename

--- crates/viewer/re_dataframe_ui/src/lib.rs
+++ crates/viewer/re_dataframe_ui/src/lib.rs
@@ -1,11 +1,11 @@
 //! Rich table widget over `datafusion`.
 
 mod datafusion_adapter;
+mod datafusion_table_widget;
 mod display_record_batch;
 mod requested_object;
-mod table_ui;
 pub mod table_utils;
 
+pub use datafusion_table_widget::DataFusionTableWidget;
 pub use display_record_batch::{DisplayColumn, DisplayRecordBatch, DisplayRecordBatchError};
 pub use requested_object::RequestedObject;
-pub use table_ui::DataFusionTableWidget;
