c82549383f85b8b928eb01276d496ecc7ef2eed4
Andreas Reich <andreas@rerun.io>
2025-04-25 10:29:27 +0200

    Fix custom view example not instantiating visualizer by default (#9762)
    ### Related
    
    * Fixes https://github.com/rerun-io/rerun/issues/9183
    
    ---------
    
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

--- examples/rust/custom_view/src/color_coordinates_view.rs
+++ examples/rust/custom_view/src/color_coordinates_view.rs
@@ -1,18 +1,18 @@
 use crate::color_coordinates_visualizer_system::{ColorWithInstance, InstanceColorSystem};
-use re_viewer::external::re_log_types::ResolvedEntityPathFilter;
-use re_viewer::external::re_ui::Help;
 use re_viewer::external::{
     egui,
     re_data_ui::{item_ui, DataUi},
     re_entity_db::InstancePath,
-    re_log_types::EntityPath,
+    re_log_types::{EntityPath, ResolvedEntityPathFilter},
     re_types::ViewClassIdentifier,
-    re_ui,
+    re_ui::{self, Help},
     re_viewer_context::{
-        HoverHighlight, IdentifiedViewSystem as _, Item, SelectionHighlight, SystemExecutionOutput,
-        UiLayout, ViewClass, ViewClassLayoutPriority, ViewClassRegistryError, ViewId, ViewQuery,
-        ViewSpawnHeuristics, ViewState, ViewStateExt as _, ViewSystemExecutionError,
-        ViewSystemRegistrator, ViewerContext,
+        HoverHighlight, IdentifiedViewSystem as _, IndicatedEntities, Item,
+        MaybeVisualizableEntities, PerVisualizer, SelectionHighlight, SmallVisualizerSet,
+        SystemExecutionOutput, UiLayout, ViewClass, ViewClassLayoutPriority,
+        ViewClassRegistryError, ViewId, ViewQuery, ViewSpawnHeuristics, ViewState,
+        ViewStateExt as _, ViewSystemExecutionError, ViewSystemRegistrator, ViewerContext,
+        VisualizableEntities,
     },
 };
 
@@ -123,6 +123,28 @@ impl ViewClass for ColorCoordinatesView {
             ViewSpawnHeuristics::default()
         } else {
             ViewSpawnHeuristics::root()
+        }
+    }
+
+    /// Make the viewer use the `ColorCoordinatesVisualizerSystem` by default.
+    ///
+    /// The default implementation of `choose_default_visualizers` activates visualizers only
+    /// if the respective indicator is present.
+    /// We want to enable the visualizer here though for any visualizable entity instead!
+    fn choose_default_visualizers(
+        &self,
+        entity_path: &EntityPath,
+        _maybe_visualizable_entities_per_visualizer: &PerVisualizer<MaybeVisualizableEntities>,
+        visualizable_entities_per_visualizer: &PerVisualizer<VisualizableEntities>,
+        _indicated_entities_per_visualizer: &PerVisualizer<IndicatedEntities>,
+    ) -> SmallVisualizerSet {
+        if visualizable_entities_per_visualizer
+            .get(&InstanceColorSystem::identifier())
+            .is_some_and(|entities| entities.contains(entity_path))
+        {
+            SmallVisualizerSet::from_slice(&[InstanceColorSystem::identifier()])
+        } else {
+            SmallVisualizerSet::new()
         }
     }
 
