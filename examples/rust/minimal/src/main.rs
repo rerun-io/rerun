1f2198aec41f8ebb07f8e71f44adecccf694d12c
jprochazk <pjanv42@gmail.com>
2025-04-28 21:45:02 +0200

    sample bench

--- examples/rust/minimal/src/main.rs
+++ examples/rust/minimal/src/main.rs
@@ -1,20 +1,23 @@
 //! Demonstrates the most barebone usage of the Rerun SDK.
-
+use rerun::RecordingStreamBuilder;
 use rerun::{demo_util::grid, external::glam};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rec = rerun::RecordingStreamBuilder::new("rerun_example_minimal").spawn()?;
+type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
-    let points = grid(glam::Vec3::splat(-10.0), glam::Vec3::splat(10.0), 10);
-    let colors = grid(glam::Vec3::ZERO, glam::Vec3::splat(255.0), 10)
-        .map(|v| rerun::Color::from_rgb(v.x as u8, v.y as u8, v.z as u8));
+fn main() -> Result {
+    let rec = RecordingStreamBuilder::new("tmp").stdout()?;
 
-    rec.log(
-        "my_points",
-        &rerun::Points3D::new(points)
-            .with_colors(colors)
-            .with_radii([0.5]),
-    )?;
+    for i in 0..10000 {
+        let points = grid(
+            glam::Vec3::splat(-(i as f32) / 20.0),
+            glam::Vec3::splat((i as f32) / 20.0),
+            i / 200,
+        );
+        let colors = grid(glam::Vec3::ZERO, glam::Vec3::splat(255.0), i / 200)
+            .map(|v| rerun::Color::from_rgb(v.x as u8, v.y as u8, v.z as u8));
+
+        rec.log("points", &rerun::Points3D::new(points).with_colors(colors))?;
+    }
 
     Ok(())
 }
