e7d9f39f63f39f515b2287f155f0764cbd162072
Gijs de Jong <berichtaangijs@gmail.com>
2025-05-17 21:29:14 +0100

    Replace `tinystl` with `stl_io` to support more stl files

--- crates/viewer/re_renderer/src/importer/stl.rs
+++ crates/viewer/re_renderer/src/importer/stl.rs
@@ -1,13 +1,13 @@
 use itertools::Itertools as _;
 use smallvec::smallvec;
-use tinystl::StlData;
+use stl_io::IndexedMesh;
 
 use crate::{CpuModel, RenderContext, mesh};
 
 #[derive(thiserror::Error, Debug)]
 pub enum StlImportError {
     #[error("Error loading STL mesh: {0}")]
-    TinyStl(tinystl::Error),
+    StlIoError(std::io::Error),
 
     #[error(transparent)]
     MeshError(#[from] mesh::MeshError),
@@ -20,37 +20,46 @@ pub fn load_stl_from_buffer(
 ) -> Result<CpuModel, StlImportError> {
     re_tracing::profile_function!();
 
-    let cursor = std::io::Cursor::new(buffer);
-    let StlData {
-        name,
-        triangles,
-        normals,
-        ..
-    } = StlData::read_buffer(std::io::BufReader::new(cursor)).map_err(StlImportError::TinyStl)?;
+    let mut cursor = std::io::Cursor::new(buffer);
+    let IndexedMesh { vertices, faces } =
+        stl_io::read_stl(&mut cursor).map_err(StlImportError::StlIoError)?;
+    let num_vertices = faces.len() * 3;
 
-    let num_vertices = triangles.len() * 3;
+    // TODO(gijsd): parse name from ASCII?
+    let name = "";
 
     let material = mesh::Material {
-        label: name.clone().into(),
+        label: name.into(),
         index_range: 0..num_vertices as u32,
         albedo: ctx.texture_manager_2d.white_texture_unorm_handle().clone(),
         albedo_factor: crate::Rgba::WHITE,
     };
 
+    // TODO(gijsd): Does this not support sparse meshes/vertex positions?
     let mesh = mesh::CpuMesh {
         label: name.into(),
         triangle_indices: (0..num_vertices as u32)
             .tuples::<(_, _, _)>()
             .map(glam::UVec3::from)
             .collect::<Vec<_>>(),
-        vertex_positions: bytemuck::cast_slice(&triangles).to_vec(),
+
+        vertex_positions: faces
+            .iter()
+            .flat_map(|f| {
+                [
+                    glam::Vec3::from_array(vertices[f.vertices[0]].0),
+                    glam::Vec3::from_array(vertices[f.vertices[1]].0),
+                    glam::Vec3::from_array(vertices[f.vertices[2]].0),
+                ]
+            })
+            .collect(),
 
         // Normals on STL are per triangle, not per vertex.
         // Yes, this makes STL always look faceted.
-        vertex_normals: normals
-            .into_iter()
-            .flat_map(|n| {
-                let n = glam::Vec3::from_array(n);
+        vertex_normals: faces
+            .iter()
+            .flat_map(|f| {
+                let n = glam::Vec3::from_array(f.normal.0);
                 [n, n, n]
             })
             .collect(),
