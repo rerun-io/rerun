640d5f27a588794ac66b8722ade91e509c07cd8a
Andreas Reich <andreas@rerun.io>
2025-05-19 14:43:15 +0100

    Fix time series points querying extra points at start and end of time range (causing stuttering on playback in some scenes) (#9963)

--- crates/viewer/re_view_time_series/src/point_visualizer_system.rs
+++ crates/viewer/re_view_time_series/src/point_visualizer_system.rs
@@ -194,10 +194,7 @@ impl SeriesPointSystem {
             re_tracing::profile_scope!("primary", &data_result.entity_path.to_string());
 
             let entity_path = &data_result.entity_path;
-            let query = re_chunk_store::RangeQuery::new(view_query.timeline, time_range)
-                // We must fetch data with extended bounds, otherwise the query clamping would
-                // cut-off the data early at the edge of the view.
-                .include_extended_bounds(true);
+            let query = re_chunk_store::RangeQuery::new(view_query.timeline, time_range);
 
             let results = range_with_blueprint_resolved_data(
                 ctx,
