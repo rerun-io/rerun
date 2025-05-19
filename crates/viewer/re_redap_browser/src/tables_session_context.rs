5aafd45104f4825e321880fbfc364c13c34411ce
Jochen Görtler <me@jgoertler.com>
2025-05-19 12:38:11 +0200

    lint

--- crates/viewer/re_redap_browser/src/tables_session_context.rs
+++ crates/viewer/re_redap_browser/src/tables_session_context.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 use datafusion::common::DataFusionError;
 use datafusion::prelude::SessionContext;
 
-use re_dataframe_ui::{DataFusionTableWidget, RequestedObject};
+use re_dataframe_ui::RequestedObject;
 use re_datafusion::{PartitionTableProvider, TableEntryTableProvider};
 use re_grpc_client::redap;
 use re_grpc_client::redap::ConnectionError;
@@ -35,6 +35,7 @@ pub enum SessionContextError {
 struct Table {
     #[expect(dead_code)]
     entry_id: EntryId,
+    #[expect(dead_code)]
     name: String,
 }
 
@@ -43,6 +44,7 @@ struct Table {
 //TODO(ab): add support for local caching of table data
 pub struct TablesSessionContext {
     pub ctx: Arc<SessionContext>,
+    #[expect(dead_code)]
     origin: re_uri::Origin,
 
     registered_tables: RequestedObject<Result<Vec<Table>, SessionContextError>>,
@@ -66,7 +68,6 @@ impl TablesSessionContext {
 
         Self {
             ctx,
-
             origin,
             registered_tables,
         }
