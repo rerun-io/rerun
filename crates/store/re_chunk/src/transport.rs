9658071ce46647e430dca20c658aa8eba289cd50
Jochen Görtler <me@jgoertler.com>
2025-05-27 15:55:15 +0200

    Refactor `re_chunk`

--- crates/store/re_chunk/src/transport.rs
+++ crates/store/re_chunk/src/transport.rs
@@ -91,7 +91,9 @@ impl Chunk {
                         component_name,
                     } = *component_desc;
 
-                    component_name.sanity_check();
+                    if let Some(c) = component_name {
+                        c.sanity_check();
+                    }
 
                     let schema = re_sorbet::ComponentColumnDescriptor {
                         store_datatype: list_array.data_type().clone(),
