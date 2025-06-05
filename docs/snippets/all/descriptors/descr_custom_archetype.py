rerun-io/rerun

commit e1b9fa08705fd82f738d2197bbed51e44081403f

Remove `_COMPONENT_DESCRIPTOR` from components in Python (#10147)
### Related

* Closes #10058.
* Precursor to #10082.

### What

Analogous to the Rust API, we guarantee that all batches have a full
descriptor.

docs/snippets/all/descriptors/descr_custom_archetype.py
@@ -10,16 +10,18 @@ import rerun as rr  # pip install rerun-sdk
 
 class CustomPoints3D(rr.AsComponents):
     def __init__(self: Any, positions: npt.ArrayLike, colors: npt.ArrayLike) -> None:
-        self.positions = rr.components.Position3DBatch(positions).with_descriptor(
+        self.positions = rr.components.Position3DBatch(positions).described(
             rr.ComponentDescriptor(
                 "user.CustomPosition3D",
                 archetype_name="user.CustomPoints3D",
                 archetype_field_name="custom_positions",
             ),
         )
-        self.colors = rr.components.ColorBatch(colors).or_with_descriptor_overrides(
-            archetype_name="user.CustomPoints3D",
-            archetype_field_name="colors",
+        self.colors = rr.components.ColorBatch(colors).described(
+            rr.ComponentDescriptor("rerun.components.Colors").with_overrides(
+                archetype_name="user.CustomPoints3D",
+                archetype_field_name="colors",
+            )
         )
 
     def as_component_batches(self) -> list[rr.DescribedComponentBatch]:

commit d70cc96726824bb1ca188840151bed6cd0143144

Fix typo in `descr_custom_archetype.py` snippet
This was introduced by #10147.

docs/snippets/all/descriptors/descr_custom_archetype.py
@@ -18,7 +18,7 @@ class CustomPoints3D(rr.AsComponents):
             ),
         )
         self.colors = rr.components.ColorBatch(colors).described(
-            rr.ComponentDescriptor("rerun.components.Colors").with_overrides(
+            rr.ComponentDescriptor("rerun.components.Color").with_overrides(
                 archetype_name="user.CustomPoints3D",
                 archetype_field_name="colors",
             )