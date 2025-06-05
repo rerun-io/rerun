b9a3870baeada4bc5141fa1002b2c326bba117dd
jprochazk <pjanv42@gmail.com>
2025-06-05 12:28:18 +0200

    update script

--- ci_docker/publish.sh
+++ ci_docker/publish.sh
@@ -6,6 +6,6 @@ VERSION=0.16.0 # Bump on each new version. Remember to update the version in the
 # The build needs to run from top of repo to access the requirements.txt
 cd `git rev-parse --show-toplevel`
 
-# Build the image
+# Build and push the image
 # buildx wants to do all of this in one step
-docker buildx build --pull --platform linux/arm64,linux/amd64 -t rerunio/ci_docker -t rerunio/ci_docker:$VERSION --push -f ci_docker/Dockerfile-manylinux .
+docker buildx build --pull --platform linux/arm64,linux/amd64 -t rerunio/ci_docker -t rerunio/ci_docker:$VERSION --push -f ci_docker/Dockerfile .
