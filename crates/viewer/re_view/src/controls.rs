bae73accf0ab6de0bdce45d6712522c9144cdab3
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-06-07 18:15:09 -0700

    Toggle maximized view with ⌘+M or Ctrl+M

--- crates/viewer/re_view/src/controls.rs
+++ crates/viewer/re_view/src/controls.rs
@@ -1,52 +1,55 @@
-use egui::{Modifiers, os::OperatingSystem};
+use egui::{Key, KeyboardShortcut, Modifiers, PointerButton, os::OperatingSystem};
 
 /// Modifier to press for scroll to zoom.
-pub const ZOOM_SCROLL_MODIFIER: egui::Modifiers = egui::Modifiers::COMMAND;
+pub const ZOOM_SCROLL_MODIFIER: Modifiers = Modifiers::COMMAND;
 
 /// Modifier to press for scroll to change aspect ratio.
-pub const ASPECT_SCROLL_MODIFIER: egui::Modifiers =
-    egui::Modifiers::ALT.plus(egui::Modifiers::COMMAND);
+pub const ASPECT_SCROLL_MODIFIER: Modifiers = Modifiers::ALT.plus(Modifiers::COMMAND);
 
 /// Modifier to press for scroll to pan horizontally.
-pub const HORIZONTAL_SCROLL_MODIFIER: egui::Modifiers = egui::Modifiers::SHIFT;
+pub const HORIZONTAL_SCROLL_MODIFIER: Modifiers = Modifiers::SHIFT;
 
 /// Which mouse button to drag for panning a 2D view.
-pub const DRAG_PAN2D_BUTTON: egui::PointerButton = egui::PointerButton::Primary;
+pub const DRAG_PAN2D_BUTTON: PointerButton = PointerButton::Primary;
 
 /// Rectangles drawn with this mouse button zoom in 2D views.
-pub const SELECTION_RECT_ZOOM_BUTTON: egui::PointerButton = egui::PointerButton::Secondary;
+pub const SELECTION_RECT_ZOOM_BUTTON: PointerButton = PointerButton::Secondary;
 
 /// Clicking this button moves the timeline to where the cursor is.
-pub const MOVE_TIME_CURSOR_BUTTON: egui::PointerButton = egui::PointerButton::Secondary;
+pub const MOVE_TIME_CURSOR_BUTTON: PointerButton = PointerButton::Secondary;
 
 /// Which mouse button to drag for panning a 2D view.
-pub const DRAG_PAN3D_BUTTON: egui::PointerButton = egui::PointerButton::Secondary;
+pub const DRAG_PAN3D_BUTTON: PointerButton = PointerButton::Secondary;
 
 /// Which mouse button to drag for rotating a 3D view.
-pub const ROTATE3D_BUTTON: egui::PointerButton = egui::PointerButton::Primary;
+pub const ROTATE3D_BUTTON: PointerButton = PointerButton::Primary;
 
 /// Which mouse button rolls the camera.
-pub const ROLL_MOUSE: egui::PointerButton = egui::PointerButton::Middle;
+pub const ROLL_MOUSE: PointerButton = PointerButton::Middle;
 
 /// Which mouse button rolls the camera if the roll modifier is pressed.
-pub const ROLL_MOUSE_ALT: egui::PointerButton = egui::PointerButton::Primary;
+pub const ROLL_MOUSE_ALT: PointerButton = PointerButton::Primary;
 
 /// See [`ROLL_MOUSE_ALT`].
-pub const ROLL_MOUSE_MODIFIER: egui::Modifiers = egui::Modifiers::ALT;
+pub const ROLL_MOUSE_MODIFIER: Modifiers = Modifiers::ALT;
 
 /// Which modifier speeds up the 3D camera movement.
-pub const SPEED_UP_3D_MODIFIER: egui::Modifiers = egui::Modifiers::SHIFT;
+pub const SPEED_UP_3D_MODIFIER: Modifiers = Modifiers::SHIFT;
 
 /// Key to restore the camera.
-pub const TRACKED_OBJECT_RESTORE_KEY: egui::Key = egui::Key::Escape;
+pub const TRACKED_OBJECT_RESTORE_KEY: Key = Key::Escape;
+
+/// Toggle the currently selected view to be maximized or not.
+pub const TOGGLE_MAXIMIZE_VIEW: KeyboardShortcut =
+    KeyboardShortcut::new(Modifiers::COMMAND, Key::M);
 
 pub struct RuntimeModifiers {}
 
 impl RuntimeModifiers {
     pub fn slow_down(os: &OperatingSystem) -> Modifiers {
         match os {
-            egui::os::OperatingSystem::Mac => egui::Modifiers::CTRL,
-            _ => egui::Modifiers::ALT,
+            egui::os::OperatingSystem::Mac => Modifiers::CTRL,
+            _ => Modifiers::ALT,
         }
     }
 }
