917963b1200eb5fe7c5e2f6152171fed4f718408
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-06-07 17:45:05 -0700

    Add snapshot tests of view UIs

--- crates/viewer/re_viewer/src/default_views.rs
+++ crates/viewer/re_viewer/src/default_views.rs
@@ -26,3 +26,67 @@ fn populate_view_class_registry_with_builtin(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use egui::Vec2;
+    use egui_kittest::SnapshotOptions;
+    use re_chunk::EntityPath;
+    use re_ui::UiExt as _;
+    use re_viewer_context::{ViewId, test_context::TestContext};
+
+    use super::*;
+
+    #[test]
+    fn test_view_selection_ui() {
+        let view_id = ViewId::random();
+        let mut test_context = TestContext::default();
+        test_context
+            .query_results
+            .insert(view_id, Default::default());
+
+        let view_class_registry = create_view_class_registry().unwrap();
+        for entry in view_class_registry.iter_registry() {
+            let class = &entry.class;
+            let mut state = class.new_state();
+            let space_origin = EntityPath::root();
+
+            let mut did_run = false;
+
+            let mut harness = test_context
+                .setup_kittest_for_rendering()
+                .with_size([400.0, 640.0])
+                .build(|egui_ctx| {
+                    re_ui::apply_style_and_install_loaders(egui_ctx);
+
+                    egui::CentralPanel::default().show(egui_ctx, |ui| {
+                        test_context.run(egui_ctx, |viewer_ctx| {
+                            ui.set_min_size(Vec2::new(400.0, 300.0));
+                            ui.list_item_scope(entry.identifier, |ui| {
+                                class
+                                    .selection_ui(
+                                        viewer_ctx,
+                                        ui,
+                                        state.as_mut(),
+                                        &space_origin,
+                                        view_id,
+                                    )
+                                    .expect("Failed to run selection_ui");
+                                did_run = true;
+                            });
+                        });
+                    });
+                });
+
+            harness.run();
+
+            let snapshot_options =
+                SnapshotOptions::default().output_path("tests/snapshots/all_view_selecion_uis");
+            harness.snapshot_options(&entry.identifier, &snapshot_options);
+
+            drop(harness);
+
+            assert!(did_run);
+        }
+    }
+}
