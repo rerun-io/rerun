a306503bf41110ac04dba641ffc55169c46fa756
Antoine Beyeler <antoine@rerun.io>
2025-06-10 15:58:54 +0200

    publish `GenericConnectionClient`

--- crates/store/re_grpc_client/src/lib.rs
+++ crates/store/re_grpc_client/src/lib.rs
@@ -6,6 +6,7 @@ pub mod message_proxy;
 mod redap;
 
 pub use self::{
+    connection_client::GenericConnectionClient,
     connection_registry::{ConnectionClient, ConnectionRegistry, ConnectionRegistryHandle},
     redap::{
         Command, ConnectionError, RedapClient, channel,
