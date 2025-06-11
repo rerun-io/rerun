24030368dbe5b911ad5e5fc164bcec1e658f9301
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-06-10 20:35:02 -0700

    Explain why we add the metadata to every IPC

--- crates/store/re_log_encoding/src/codec/arrow.rs
+++ crates/store/re_log_encoding/src/codec/arrow.rs
@@ -2,6 +2,13 @@ use super::CodecError;
 
 use arrow::array::RecordBatch as ArrowRecordBatch;
 
+// Insert timestamp of when the IPC encoding and decoding took place?
+// This is used for latency measurements, e.g. in gRPC calls.
+// It is slightly wasteful to add it to each chunk, even in an .rrd file,
+// but the benefit is that we'll always have them, whether we're streaming
+// data through gRPC, piping with stdout/stdin, or writing to a file that a viewer is simultaneously reading.
+const INSERT_TIMING_METADATA: bool = true;
+
 /// Helper function that serializes given arrow schema and record batch into bytes
 /// using Arrow IPC format.
 pub(crate) fn write_arrow_to_bytes<W: std::io::Write>(
@@ -12,12 +19,12 @@ pub(crate) fn write_arrow_to_bytes<W: std::io::Write>(
 
     let mut schema = (*batch.schema()).clone();
 
-    // Remember when the encoding took place,
-    // for ~accurate latency measurement of e.g. gRPC calls.
-    schema.metadata.insert(
-        re_sorbet::timing_metadata::LAST_ENCODED_AT.to_owned(),
-        re_sorbet::timing_metadata::now_timestamp(),
-    );
+    if INSERT_TIMING_METADATA {
+        schema.metadata.insert(
+            re_sorbet::timing_metadata::KEY_TIMESTAMP_IPC_ENCODED.to_owned(),
+            re_sorbet::timing_metadata::now_timestamp(),
+        );
+    }
 
     let mut sw = arrow::ipc::writer::StreamWriter::try_new(writer, &schema)
         .map_err(CodecError::ArrowSerialization)?;
@@ -37,16 +44,18 @@ pub(crate) fn read_arrow_from_bytes<R: std::io::Read>(
     let mut stream = arrow::ipc::reader::StreamReader::try_new(reader, None)
         .map_err(CodecError::ArrowDeserialization)?;
 
-    let record_batch = stream
+    let mut record_batch = stream
         .next()
         .ok_or(CodecError::MissingRecordBatch)?
         .map_err(CodecError::ArrowDeserialization)?;
 
-    let record_batch = re_arrow_util::insert_metadata(
-        record_batch,
-        re_sorbet::timing_metadata::LAST_DECODED_AT.to_owned(),
-        re_sorbet::timing_metadata::now_timestamp(),
-    );
+    if INSERT_TIMING_METADATA {
+        record_batch = re_arrow_util::insert_metadata(
+            record_batch,
+            re_sorbet::timing_metadata::KEY_TIMESTAMP_IPC_DECODED.to_owned(),
+            re_sorbet::timing_metadata::now_timestamp(),
+        );
+    }
 
     Ok(record_batch)
 }
