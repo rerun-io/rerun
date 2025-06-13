rerun-io/rerun

commit c618728fe44896c2d23dababb1271dc34ca3ec5c

Add detailed view of latency (#10173)
## Related
* Investigates https://github.com/rerun-io/rerun/issues/9812
* Closes https://github.com/rerun-io/rerun/issues/9973

## What
This adds a breakdown of where the end-to-end latency is coming from. It
automatically appears in the top panel when latency is high. Hover it to
see details:


![image](https://github.com/user-attachments/assets/f73a0b3a-365a-412b-aa7e-f6e4d3a95620)



## Notable changes
The latency metrics are no longer opt-in. We always show them now if
they get surprisingly high.

The measurements are made by adding extra metadata to the record batches
at IPC encode/decode time.

We already have accurate timestamps at log-time (`RowId`) and batch-time
(`ChunkId`).

## Testing

Apply this diff:

```
diff --git a/tests/rust/log_benchmark/src/image.rs b/tests/rust/log_benchmark/src/image.rs
index e36478262a1..877c3e0bce4 100644
--- a/tests/rust/log_benchmark/src/image.rs
+++ b/tests/rust/log_benchmark/src/image.rs
@@ -26,8 +26,7 @@ fn prepare() -> Vec<u8> {
 fn execute(mut raw_image_data: Vec<u8>) -> anyhow::Result<()> {
     re_tracing::profile_function!();
 
-    let (rec, _storage) =
-        rerun::RecordingStreamBuilder::new("rerun_example_benchmark_").memory()?;
+    let rec = rerun::RecordingStreamBuilder::new("rerun_example_benchmark_").connect_grpc()?;
 
     for i in 0..NUM_LOG_CALLS {
         raw_image_data[i] += 1; // Change a single pixel of the image data, just to make sure we transmit something different each time.
```

The run this in one terminal:

```sh
pixi run rerun-release 
```

and in another:

```sh
cargo run -p log_benchmark --release -- --benchmarks image --profile
```

## Findings
In a test where I encode a bunch of uncompressed 1024² RGBA images
(`tests/rust/log_benchmark/src/image.rs`).
I found that by far the slowest part is the IPC encoding of arrow data
(yes, this is a release build):

<img width="727" alt="Image"

crates/store/re_log_encoding/src/codec/arrow.rs
@@ -2,13 +2,32 @@ use super::CodecError;
 
 use arrow::array::RecordBatch as ArrowRecordBatch;
 
+// Insert timestamp of when the IPC encoding and decoding took place?
+// This is used for latency measurements, e.g. in gRPC calls.
+// It is slightly wasteful to add it to each chunk, even in an .rrd file,
+// but the benefit is that we'll always have them, whether we're streaming
+// data through gRPC, piping with stdout/stdin, or writing to a file that a viewer is simultaneously reading.
+// However, it messes up our roundtrip unit-tests, so we disable it then.
+const INSERT_TIMING_METADATA: bool = !cfg!(feature = "testing");
+
 /// Helper function that serializes given arrow schema and record batch into bytes
 /// using Arrow IPC format.
 pub(crate) fn write_arrow_to_bytes<W: std::io::Write>(
     writer: &mut W,
     batch: &ArrowRecordBatch,
 ) -> Result<(), CodecError> {
-    let mut sw = arrow::ipc::writer::StreamWriter::try_new(writer, batch.schema_ref())
+    re_tracing::profile_function!();
+
+    let mut schema = (*batch.schema()).clone();
+
+    if INSERT_TIMING_METADATA {
+        schema.metadata.insert(
+            re_sorbet::timestamp_metadata::KEY_TIMESTAMP_IPC_ENCODED.to_owned(),
+            re_sorbet::timestamp_metadata::now_timestamp(),
+        );
+    }
+
+    let mut sw = arrow::ipc::writer::StreamWriter::try_new(writer, &schema)
         .map_err(CodecError::ArrowSerialization)?;
     sw.write(batch).map_err(CodecError::ArrowSerialization)?;
     sw.finish().map_err(CodecError::ArrowSerialization)?;
@@ -26,10 +45,20 @@ pub(crate) fn read_arrow_from_bytes<R: std::io::Read>(
     let mut stream = arrow::ipc::reader::StreamReader::try_new(reader, None)
         .map_err(CodecError::ArrowDeserialization)?;
 
-    stream
+    let mut record_batch = stream
         .next()
         .ok_or(CodecError::MissingRecordBatch)?
-        .map_err(CodecError::ArrowDeserialization)
+        .map_err(CodecError::ArrowDeserialization)?;
+
+    if INSERT_TIMING_METADATA {
+        record_batch = re_arrow_util::insert_metadata(
+            record_batch,
+            re_sorbet::timestamp_metadata::KEY_TIMESTAMP_IPC_DECODED.to_owned(),
+            re_sorbet::timestamp_metadata::now_timestamp(),
+        );
+    }
+
+    Ok(record_batch)
 }
 
 #[cfg(feature = "encoder")]
@@ -44,13 +73,18 @@ pub(crate) fn encode_arrow(
     batch: &ArrowRecordBatch,
     compression: crate::Compression,
 ) -> Result<Payload, crate::encoder::EncodeError> {
+    re_tracing::profile_function!();
+
     let mut uncompressed = Vec::new();
     write_arrow_to_bytes(&mut uncompressed, batch)?;
     let uncompressed_size = uncompressed.len();
 
     let data = match compression {
         crate::Compression::Off => uncompressed,
-        crate::Compression::LZ4 => lz4_flex::block::compress(&uncompressed),
+        crate::Compression::LZ4 => {
+            re_tracing::profile_scope!("lz4::compress");
+            lz4_flex::block::compress(&uncompressed)
+        }
     };
 
     Ok(Payload {

crates/store/re_chunk/src/transport.rs
@@ -126,6 +126,7 @@ impl Chunk {
             row_id_schema,
             index_schemas,
             data_schemas,
+            Default::default(),
         )
         .with_heap_size_bytes(heap_size_bytes);
 

crates/store/re_entity_db/Cargo.toml
@@ -38,6 +38,7 @@ re_log_encoding = { workspace = true, features = ["decoder"] }
 re_log_types.workspace = true
 re_query.workspace = true
 re_smart_channel.workspace = true
+re_sorbet.workspace = true
 re_tracing.workspace = true
 re_types_core.workspace = true
 re_uri.workspace = true

crates/store/re_entity_db/src/entity_db.rs
@@ -1,12 +1,11 @@
 use std::sync::Arc;
 
 use nohash_hasher::IntMap;
-use parking_lot::Mutex;
 
 use re_chunk::{Chunk, ChunkResult, LatestAtQuery, RowId, TimeInt, Timeline, TimelineName};
 use re_chunk_store::{
     ChunkStore, ChunkStoreChunkStats, ChunkStoreConfig, ChunkStoreDiffKind, ChunkStoreEvent,
-    ChunkStoreHandle, ChunkStoreSubscriber, GarbageCollectionOptions, GarbageCollectionTarget,
+    ChunkStoreHandle, ChunkStoreSubscriber as _, GarbageCollectionOptions, GarbageCollectionTarget,
 };
 use re_log_types::{
     ApplicationId, EntityPath, EntityPathHash, LogMsg, ResolvedTimeRange, ResolvedTimeRangeF,
@@ -18,7 +17,7 @@ use re_query::{
 };
 use re_smart_channel::SmartChannelSource;
 
-use crate::{Error, TimesPerTimeline};
+use crate::{Error, TimesPerTimeline, ingestion_statistics::IngestionStatistics};
 
 // ----------------------------------------------------------------------------
 
@@ -107,7 +106,7 @@ impl EntityDb {
     }
 
     pub fn with_store_config(store_id: StoreId, store_config: ChunkStoreConfig) -> Self {
-        let store = ChunkStoreHandle::new(ChunkStore::new(store_id.clone(), store_config));
+        let store = ChunkStoreHandle::new(ChunkStore::new(store_id, store_config));
         let cache = QueryCacheHandle::new(QueryCache::new(store.clone()));
 
         // Safety: these handles are never going to be leaked outside of the `EntityDb`.
@@ -124,7 +123,7 @@ impl EntityDb {
             tree: crate::EntityTree::root(),
             time_histogram_per_timeline: Default::default(),
             storage_engine,
-            stats: IngestionStatistics::new(store_id),
+            stats: IngestionStatistics::default(),
         }
     }
 
@@ -453,9 +452,14 @@ impl EntityDb {
             LogMsg::ArrowMsg(_, arrow_msg) => {
                 self.last_modified_at = web_time::Instant::now();
 
-                let mut chunk = re_chunk::Chunk::from_arrow_msg(arrow_msg)?;
+                let chunk_batch = re_sorbet::ChunkBatch::try_from(&arrow_msg.batch)
+                    .map_err(re_chunk::ChunkError::from)?;
+                let mut chunk = re_chunk::Chunk::from_chunk_batch(&chunk_batch)?;
                 chunk.sort_if_unsorted();
-                self.add_chunk(&Arc::new(chunk))?
+                self.add_chunk_with_timestamp_metadata(
+                    &Arc::new(chunk),
+                    &chunk_batch.sorbet_schema().timestamps,
+                )?
             }
 
             LogMsg::BlueprintActivationCommand(_) => {
@@ -468,6 +472,14 @@ impl EntityDb {
     }
 
     pub fn add_chunk(&mut self, chunk: &Arc<Chunk>) -> Result<Vec<ChunkStoreEvent>, Error> {
+        self.add_chunk_with_timestamp_metadata(chunk, &Default::default())
+    }
+
+    pub fn add_chunk_with_timestamp_metadata(
+        &mut self,
+        chunk: &Arc<Chunk>,
+        timestamps: &re_sorbet::TimestampMetadata,
+    ) -> Result<Vec<ChunkStoreEvent>, Error> {
         let mut engine = self.storage_engine.write();
         let store_events = engine.store().insert_chunk(chunk)?;
         engine.cache().on_events(&store_events);
@@ -503,7 +515,7 @@ impl EntityDb {
             }
 
             // We inform the stats last, since it measures e2e latency.
-            self.stats.on_events(&store_events);
+            self.stats.on_events(timestamps, &store_events);
         }
 
         Ok(store_events)
@@ -867,86 +879,5 @@ impl re_byte_size::SizeBytes for EntityDb {
             .stats()
             .total()
             .total_size_bytes
-    }
-}
-
-// ----------------------------------------------------------------------------
-
-pub struct IngestionStatistics {
-    store_id: StoreId,
-    e2e_latency_sec_history: Mutex<emath::History<f32>>,
-}
-
-impl ChunkStoreSubscriber for IngestionStatistics {
-    #[inline]
-    fn name(&self) -> String {
-        "rerun.testing.store_subscribers.IngestionStatistics".into()
-    }
-
-    #[inline]
-    fn as_any(&self) -> &dyn std::any::Any {
-        self
-    }
-
-    #[inline]
-    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
-        self
-    }
-
-    #[inline]
-    fn on_events(&mut self, events: &[ChunkStoreEvent]) {
-        for event in events {
-            if event.store_id == self.store_id {
-                for row_id in event.diff.chunk.row_ids() {
-                    self.on_new_row_id(row_id);
-                }
-            }
-        }
-    }
-}
-
-impl IngestionStatistics {
-    pub fn new(store_id: StoreId) -> Self {
-        let min_samples = 0; // 0: we stop displaying e2e latency if input stops
-        let max_samples = 1024; // don't waste too much memory on this - we just need enough to get a good average
-        let max_age = 1.0; // don't keep too long of a rolling average, or the stats get outdated.
-        Self {
-            store_id,
-            e2e_latency_sec_history: Mutex::new(emath::History::new(
-                min_samples..max_samples,
-                max_age,
-            )),
-        }
-    }
-
-    fn on_new_row_id(&self, row_id: RowId) {
-        if let Ok(duration_since_epoch) = web_time::SystemTime::UNIX_EPOCH.elapsed() {
-            let nanos_since_epoch = duration_since_epoch.as_nanos() as u64;
-
-            // This only makes sense if the clocks are very good, i.e. if the recording was on the same machine!
-            if let Some(nanos_since_log) = nanos_since_epoch.checked_sub(row_id.nanos_since_epoch())
-            {
-                let now = nanos_since_epoch as f64 / 1e9;
-                let sec_since_log = nanos_since_log as f32 / 1e9;
-
-                self.e2e_latency_sec_history.lock().add(now, sec_since_log);
-            }
-        }
-    }
-
-    /// What is the mean latency between the time data was logged in the SDK and the time it was ingested?
-    ///
-    /// This is based on the clocks of the viewer and the SDK being in sync,
-    /// so if the recording was done on another machine, this is likely very inaccurate.
-    pub fn current_e2e_latency_sec(&self) -> Option<f32> {
-        let mut e2e_latency_sec_history = self.e2e_latency_sec_history.lock();
-
-        if let Ok(duration_since_epoch) = web_time::SystemTime::UNIX_EPOCH.elapsed() {
-            let nanos_since_epoch = duration_since_epoch.as_nanos() as u64;
-            let now = nanos_since_epoch as f64 / 1e9;
-            e2e_latency_sec_history.flush(now); // make sure the average is up-to-date.
-        }
-
-        e2e_latency_sec_history.average()
     }
 }

crates/store/re_entity_db/src/ingestion_statistics.rs
@@ -1,0 +1,205 @@
+use emath::History;
+use parking_lot::Mutex;
+
+use re_chunk_store::{ChunkStoreDiffKind, ChunkStoreEvent};
+use re_sorbet::TimestampMetadata;
+
+/// Statistics about the latency of incoming data to a store.
+#[derive(Default)]
+pub struct IngestionStatistics {
+    stats: Mutex<LatencyStats>,
+}
+
+impl IngestionStatistics {
+    #[inline]
+    pub fn on_events(&self, timestamps: &TimestampMetadata, events: &[ChunkStoreEvent]) {
+        let now_nanos = nanos_since_epoch();
+        let mut stats = self.stats.lock();
+        for event in events {
+            if event.diff.kind == ChunkStoreDiffKind::Addition {
+                stats.on_new_chunk(now_nanos, timestamps, &event.diff.chunk);
+            }
+        }
+    }
+}
+
+impl IngestionStatistics {
+    /// The latest (smoothed) reading of the latency of the ingestion pipeline.
+    pub fn latency_snapshot(&self) -> LatencySnapshot {
+        self.stats.lock().snapshot()
+    }
+}
+
+/// Statistics about the latency of incoming data to a store.
+#[derive(Clone, Debug)]
+pub struct LatencyStats {
+    // All latencies measured in seconds.
+    /// Full end-to-end latency, from the time the data was logged in the SDK,
+    /// up until it was added to the store.
+    e2e: History<f32>,
+
+    // All the individual parts:
+    /// Delay between the time `RowId` was created and the `ChunkId` was created,
+    /// i.e. the time it took to get the data from the `log` call to be batched by the batcher.
+    log2chunk: History<f32>,
+
+    /// Time between chunk creation and IPC encoding (start of gRPC transmission).
+    chunk2encode: History<f32>,
+
+    /// Time between encoding to IPC and decoding again,
+    /// e.g. the time it takes to send the data over the network.
+    transmission: History<f32>,
+
+    /// Time from the incoming IPC data being decoded to it being ingested into the store.
+    decode2ingest: History<f32>,
+}
+
+impl Default for LatencyStats {
+    fn default() -> Self {
+        let min_samples = 0; // 0: we stop displaying e2e latency if input stops
+        let max_samples = 8 * 1024; // don't waste too much memory on this - we just need enough to get a good average
+        let max_age = 1.0; // don't keep too long of a rolling average, or the stats get outdated.
+        Self {
+            e2e: History::new(min_samples..max_samples, max_age),
+            log2chunk: History::new(min_samples..max_samples, max_age),
+            chunk2encode: History::new(min_samples..max_samples, max_age),
+            transmission: History::new(min_samples..max_samples, max_age),
+            decode2ingest: History::new(min_samples..max_samples, max_age),
+        }
+    }
+}
+
+impl LatencyStats {
+    fn on_new_chunk(
+        &mut self,
+        now_nanos: i64,
+        timestamps: &TimestampMetadata,
+        chunk: &re_chunk::Chunk,
+    ) {
+        let Self {
+            e2e,
+            log2chunk,
+            chunk2encode,
+            transmission,
+            decode2ingest,
+        } = self;
+
+        let now = now_nanos as f64 / 1e9;
+
+        let chunk_creation_nanos = chunk.id().nanos_since_epoch() as i64;
+
+        let TimestampMetadata {
+            last_encoded_at,
+            last_decoded_at,
+        } = timestamps;
+
+        let last_encoded_at_nanos = last_encoded_at.and_then(system_time_to_nanos);
+        let last_decoded_at_nanos = last_decoded_at.and_then(system_time_to_nanos);
+
+        for row_id in chunk.row_ids() {
+            let row_creation_nanos = row_id.nanos_since_epoch() as i64;
+
+            // Total:
+            if let Some(e2e_nanos) = now_nanos.checked_sub(row_creation_nanos) {
+                e2e.add(now, e2e_nanos as f32 / 1e9);
+            }
+
+            // First step: log() call to chunk creation (batcher latency):
+            if let Some(log2chunk_nanos) = chunk_creation_nanos.checked_sub(row_creation_nanos) {
+                log2chunk.add(now, log2chunk_nanos as f32 / 1e9);
+            }
+        }
+
+        if let Some(last_encoded_at_nanos) = last_encoded_at_nanos {
+            chunk2encode.add(
+                now,
+                (last_encoded_at_nanos - chunk_creation_nanos) as f32 / 1e9,
+            );
+
+            if let Some(last_decoded_at_nanos) = last_decoded_at_nanos {
+                transmission.add(
+                    now,
+                    (last_decoded_at_nanos - last_encoded_at_nanos) as f32 / 1e9,
+                );
+            }
+        }
+
+        if let Some(last_decoded_at_nanos) = last_decoded_at_nanos {
+            decode2ingest.add(now, (now_nanos - last_decoded_at_nanos) as f32 / 1e9);
+        }
+    }
+
+    /// What is the mean latency between the time data was logged in the SDK and the time it was ingested?
+    ///
+    /// This is based on the clocks of the viewer and the SDK being in sync,
+    /// so if the recording was done on another machine, this is likely very inaccurate.
+    pub fn snapshot(&mut self) -> LatencySnapshot {
+        let Self {
+            e2e,
+            log2chunk,
+            chunk2encode,
+            transmission,
+            decode2ingest,
+        } = self;
+
+        {
+            // make sure the averages is up-to-date:
+            let now_nanos = nanos_since_epoch();
+            let now = now_nanos as f64 / 1e9;
+            e2e.flush(now);
+            log2chunk.flush(now);
+            chunk2encode.flush(now);
+            transmission.flush(now);
+            decode2ingest.flush(now);
+        }
+
+        LatencySnapshot {
+            e2e: e2e.average(),
+            log2chunk: log2chunk.average(),
+            chunk2encode: chunk2encode.average(),
+            transmission: transmission.average(),
+            decode2ingest: decode2ingest.average(),
+        }
+    }
+}
+
+fn nanos_since_epoch() -> i64 {
+    if let Ok(duration_since_epoch) = web_time::SystemTime::UNIX_EPOCH.elapsed() {
+        duration_since_epoch.as_nanos() as i64
+    } else {
+        re_log::warn_once!("Broken system clock: unable to get current time since epoch.");
+        0
+    }
+}
+
+fn system_time_to_nanos(system_time: web_time::SystemTime) -> Option<i64> {
+    system_time
+        .duration_since(web_time::SystemTime::UNIX_EPOCH)
+        .ok()
+        .map(|d| d.as_nanos() as i64)
+}
+
+/// The latest (smoothed) reading of the latency of the ingestion pipeline.
+///
+/// All measurements are in seconds, and are only valid if the clocks of the viewer and the SDK are in sync.
+///
+/// They are `None` if unknown.
+#[derive(Clone, Copy, Debug)]
+pub struct LatencySnapshot {
+    /// From the data is logged in the SDK, up until it was added to the store.
+    pub e2e: Option<f32>,
+
+    /// Delay between the time `RowId` was created and the `ChunkId` was created,
+    /// i.e. the time it took to get the data from the `log` call to be batched by the batcher.
+    pub log2chunk: Option<f32>,
+
+    /// Time between chunk creation and IPC encoding (start of gRPC transmission).
+    pub chunk2encode: Option<f32>,
+
+    /// Time between encoding to IPC and decoding again,
+    /// e.g. the time it takes to send the data over the network.
+    pub transmission: Option<f32>,
+
+    /// Time from the incoming IPC data being decoded to it being ingested into the store.
+    pub decode2ingest: Option<f32>,
+}

crates/store/re_entity_db/src/lib.rs
@@ -6,6 +6,7 @@
 
 pub mod entity_db;
 pub mod entity_tree;
+mod ingestion_statistics;
 mod instance_path;
 mod store_bundle;
 mod time_histogram_per_timeline;
@@ -15,6 +16,7 @@ mod versioned_instance_path;
 pub use self::{
     entity_db::{DEFAULT_GC_TIME_BUDGET, EntityDb},
     entity_tree::EntityTree,
+    ingestion_statistics::{IngestionStatistics, LatencySnapshot, LatencyStats},
     instance_path::{InstancePath, InstancePathHash},
     store_bundle::{
         DatasetRecordings, LocalRecordings, RemoteRecordings, SortDatasetsResults, StoreBundle,

crates/store/re_log_encoding/Cargo.toml
@@ -21,6 +21,7 @@ all-features = true
 
 [features]
 default = []
+testing = []
 
 ## Enable loading data from an .rrd file.
 decoder = [
@@ -48,12 +49,14 @@ stream_from_http = [
 [dependencies]
 
 # Rerun:
+re_arrow_util.workspace = true
 re_build_info.workspace = true
 re_chunk.workspace = true
 re_log_types.workspace = true
 re_log.workspace = true
 re_protos.workspace = true
 re_smart_channel.workspace = true
+re_sorbet.workspace = true
 re_tracing.workspace = true
 
 # External:

crates/store/re_log_encoding/src/codec/file/decoder.rs
@@ -50,9 +50,15 @@ pub fn decode_bytes(message_kind: MessageKind, buf: &[u8]) -> Result<Option<LogM
                 .ok_or_else(|| missing_field!(re_protos::log_msg::v1alpha1::ArrowMsg, "store_id"))?
                 .into();
 
-            let chunk = re_chunk::Chunk::from_record_batch(&batch)?;
+            let sorbet_schema = re_sorbet::ChunkSchema::try_from(batch.schema_ref().as_ref())?;
 
-            Some(LogMsg::ArrowMsg(store_id, chunk.to_arrow_msg()?))
+            let arrow_msg = re_log_types::ArrowMsg {
+                chunk_id: sorbet_schema.chunk_id().as_tuid(),
+                batch,
+                on_release: None,
+            };
+
+            Some(LogMsg::ArrowMsg(store_id, arrow_msg))
         }
         MessageKind::BlueprintActivationCommand => {
             let blueprint_activation_command = BlueprintActivationCommand::decode(buf)?;

commit 339239b5bff7388c2f404776368b383ef1603bbd

Disable reading/writing timing metadata to/from encoded record batches (#10222)
### Related

* Fixes test regression introduced in
https://github.com/rerun-io/rerun/pull/10173

### What

Related internal slack discussion
https://rerunio.slack.com/archives/C07T0LFT5BQ/p1749799544492489

* [x] pass full check ci

cc: @emilk

crates/store/re_log_encoding/src/codec/arrow.rs
@@ -3,12 +3,16 @@ use super::CodecError;
 use arrow::array::RecordBatch as ArrowRecordBatch;
 
 // Insert timestamp of when the IPC encoding and decoding took place?
+//
 // This is used for latency measurements, e.g. in gRPC calls.
 // It is slightly wasteful to add it to each chunk, even in an .rrd file,
 // but the benefit is that we'll always have them, whether we're streaming
 // data through gRPC, piping with stdout/stdin, or writing to a file that a viewer is simultaneously reading.
 // However, it messes up our roundtrip unit-tests, so we disable it then.
-const INSERT_TIMING_METADATA: bool = !cfg!(feature = "testing");
+//
+// TODO(emilk, andreas, cmc): This is disabled for now since it messes with our tests and
+// (worse) means that checksums of files become unstable on read _and_ write.
+const INSERT_TIMING_METADATA: bool = false; // !cfg!(feature = "testing");
 
 /// Helper function that serializes given arrow schema and record batch into bytes
 /// using Arrow IPC format.