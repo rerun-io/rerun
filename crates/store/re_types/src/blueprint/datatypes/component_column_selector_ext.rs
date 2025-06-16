9428cdf76806dd42568dd0f2e6323d39f93cd63d
Jochen Görtler <me@jgoertler.com>
2025-06-16 10:08:58 +0200

    rename

--- crates/store/re_types/src/blueprint/datatypes/component_column_selector_ext.rs
+++ crates/store/re_types/src/blueprint/datatypes/component_column_selector_ext.rs
@@ -2,10 +2,10 @@ use re_log_types::EntityPath;
 
 impl super::ComponentColumnSelector {
     /// Create a [`Self`] from an [`EntityPath`] and a column name.
-    pub fn new(entity_path: &EntityPath, column_name: String) -> Self {
+    pub fn new(entity_path: &EntityPath, component: String) -> Self {
         Self {
             entity_path: entity_path.into(),
-            component: column_name.into(),
+            component: component.into(),
         }
     }
 
@@ -17,16 +17,16 @@ impl super::ComponentColumnSelector {
     /// The parsed omponent column selector.
     pub fn column_selector(&self) -> re_sorbet::ComponentColumnSelector {
         let entity_path = EntityPath::from(self.entity_path.as_str());
-        let column_name = self.component.as_str();
-        match column_name.rfind(':') {
+        let component = self.component.as_str();
+        match component.rfind(':') {
             Some(i) => re_sorbet::ComponentColumnSelector {
                 entity_path,
-                archetype_name: Some(column_name[..i].into()),
-                archetype_field_name: column_name[(i + 1)..].into(),
+                archetype_name: Some(component[..i].into()),
+                archetype_field_name: component[(i + 1)..].into(),
             },
             None => re_sorbet::ComponentColumnSelector {
                 entity_path,
-                archetype_field_name: column_name.to_owned(),
+                archetype_field_name: component.to_owned(),
                 archetype_name: None,
             },
         }
