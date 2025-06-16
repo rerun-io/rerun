4e2fa400979347316c9d5110398669edbbef3c9f
lucasmerlin <hi@lucasmerlin.me>
2025-06-16 17:31:52 +0200

    Clippy

--- crates/viewer/re_redap_browser/src/servers.rs
+++ crates/viewer/re_redap_browser/src/servers.rs
@@ -6,9 +6,7 @@ use egui::{Frame, Margin, RichText};
 use re_dataframe_ui::{ColumnBlueprint, default_display_name_for_column};
 use re_grpc_client::ConnectionRegistryHandle;
 use re_log_types::{EntityPathPart, EntryId};
-use re_protos::manifest_registry::v1alpha1::{
-    DATASET_MANIFEST_ID_FIELD_NAME, DATASET_MANIFEST_REGISTRATION_TIME_FIELD_NAME,
-};
+use re_protos::manifest_registry::v1alpha1::DATASET_MANIFEST_ID_FIELD_NAME;
 use re_sorbet::BatchType;
 use re_ui::list_item::ItemActionButton;
 use re_ui::{UiExt as _, icons, list_item};
