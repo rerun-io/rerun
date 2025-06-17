dc52052ca4c378bdb8439b897ce5e359170a09fc
Jeremy Leibs <jeremy@rerun.io>
2025-06-16 20:42:07 -0400

    fmt

--- rerun_py/src/catalog/connection_handle.rs
+++ rerun_py/src/catalog/connection_handle.rs
@@ -3,8 +3,8 @@ use arrow::datatypes::Schema as ArrowSchema;
 use arrow::pyarrow::PyArrowType;
 use pyo3::exceptions::PyValueError;
 use pyo3::{
-    PyErr, PyResult, Python, create_exception, exceptions::PyConnectionError,
-    exceptions::PyRuntimeError,
+    create_exception, exceptions::PyConnectionError, exceptions::PyRuntimeError, PyErr, PyResult,
+    Python,
 };
 use re_log_encoding::codec::wire::decoder::Decode as _;
 use re_protos::common::v1alpha1::ext::ScanParameters;
@@ -18,14 +18,14 @@ use re_chunk::{LatestAtQuery, RangeQuery};
 use re_chunk_store::ChunkStore;
 use re_dataframe::ViewContentsSelector;
 use re_grpc_client::{
-    ConnectionClient, ConnectionRegistryHandle, get_chunks_response_to_chunk_and_partition_id,
+    get_chunks_response_to_chunk_and_partition_id, ConnectionClient, ConnectionRegistryHandle,
 };
 use re_log_types::{ApplicationId, EntryId, StoreId, StoreInfo, StoreKind, StoreSource};
 use re_protos::catalog::v1alpha1::ext::DatasetDetails;
 use re_protos::{
     catalog::v1alpha1::{
-        EntryFilter, ReadTableEntryRequest,
         ext::{DatasetEntry, EntryDetails, TableEntry},
+        EntryFilter, ReadTableEntryRequest,
     },
     common::v1alpha1::{IfDuplicateBehavior, TaskId},
     frontend::v1alpha1::{
