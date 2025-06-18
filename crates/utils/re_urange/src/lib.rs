1ff2c2c4ea79675c07cff7752e8619c33ededa46
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-06-18 16:36:58 -0700

    Add crate `re_urange`

---
+++ crates/utils/re_urange/src/lib.rs
@@ -0,0 +1,73 @@
+use std::ops::{Mul, Range};
+
+use num_traits::Unsigned;
+
+/// An integer range who's length is always at least zero.
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct URange<Idx: Unsigned + Copy> {
+    /// The index of the first element.
+    pub start: Idx,
+
+    /// The number of elements in the range.
+    pub len: Idx,
+}
+
+impl<Idx: Unsigned + Copy> URange<Idx> {
+    /// The next element, just outside the range.
+    #[inline]
+    pub fn end(&self) -> Idx {
+        self.start + self.len
+    }
+
+    /// Useful when slicing a slice
+    #[inline]
+    pub fn range(self) -> Range<Idx> {
+        let Self { start, len } = self;
+        Range {
+            start,
+            end: start + len,
+        }
+    }
+
+    pub fn try_cast<Narrow>(self) -> Option<URange<Narrow>>
+    where
+        Narrow: TryFrom<Idx> + Unsigned + Copy,
+    {
+        Some(URange {
+            start: self.start.try_into().ok()?,
+            len: self.len.try_into().ok()?,
+        })
+    }
+}
+
+impl URange<u32> {
+    /// Widening cast; useful for indexing.
+    #[inline]
+    pub fn range_usize(self) -> Range<usize> {
+        let Self { start, len } = self;
+        Range {
+            start: start as usize,
+            end: start as usize + len as usize,
+        }
+    }
+}
+
+impl<Idx: Unsigned + Copy> From<URange<Idx>> for Range<Idx> {
+    #[inline]
+    fn from(value: URange<Idx>) -> Self {
+        value.range()
+    }
+}
+
+/// urange * scalar
+impl<Idx: Unsigned + Copy + Mul> Mul<Idx> for URange<Idx> {
+    type Output = Self;
+
+    fn mul(self, rhs: Idx) -> Self::Output {
+        let Self { start, len } = self;
+        Self {
+            start: rhs * start,
+            len: rhs * len,
+        }
+    }
+}
