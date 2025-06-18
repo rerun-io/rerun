8349ebb315fa2bf863c5966b58d23dffd2c6daa9
jprochazk <pjanv42@gmail.com>
2025-06-18 13:21:46 +0200

    use stateless api in `tee` snippet

--- docs/snippets/all/howto/tee.py
+++ docs/snippets/all/howto/tee.py
@@ -4,15 +4,14 @@ import numpy as np
 import rerun as rr
 
 # Initialize the SDK and give our recording a unique name
-rr.init("rerun_example_tee")
+rec = rr.RecordingStream("rerun_example_tee")
 
-rr.get_data_recording().tee(
+rec.tee(
     # Connect to a local viewer using the default URL
     rr.GrpcSink(),
     # Write data to a `data.rrd` file in the current directory
     rr.FileSink("data.rrd"),
 )
-
 
 # Create some data
 SIZE = 10
@@ -30,4 +29,3 @@ rr.log(
     # log data as a 3D point cloud archetype
     rr.Points3D(positions, colors=colors, radii=0.5),
 )
-
