feff6ee1b29a1d138eb032898cde31a1a5f0bbf9
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-06-19 12:25:43 +0200

    Link to an issue with more info

--- crates/viewer/re_viewer/src/native.rs
+++ crates/viewer/re_viewer/src/native.rs
@@ -16,7 +16,7 @@ pub fn run_native_app(
 ) -> eframe::Result {
     if crate::docker_detection::is_docker() {
         re_log::warn_once!(
-            "It looks like you are running the Rerun Viewer inside a Docker container. This is not officially supported, and may lead to performance issues and bugs.",
+            "It looks like you are running the Rerun Viewer inside a Docker container. This is not officially supported, and may lead to performance issues and bugs. See https://github.com/rerun-io/rerun/issues/6835 for more.",
         );
     }
 
