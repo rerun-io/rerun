e4ec6dd33680105e82de66413332b31866349d72
jprochazk <pjanv42@gmail.com>
2025-06-20 13:18:55 +0200

    document memory limit in c api

--- rerun_cpp/src/rerun/c/rerun.h
+++ rerun_cpp/src/rerun/c/rerun.h
@@ -447,6 +447,9 @@ extern void rr_recording_stream_connect_grpc(
 /// The gRPC server will buffer all log data in memory so that late connecting viewers will get all the data.
 /// You can limit the amount of data buffered by the gRPC server with the `server_memory_limit` argument.
 /// Once reached, the earliest logged data will be dropped. Static data is never dropped.
+/// 
+/// It is highly recommended that you set the memory limit to `0B` if both the server and client are running
+/// on the same machine, otherwise you're potentially doubling your memory usage!
 extern void rr_recording_stream_serve_grpc(
     rr_recording_stream stream, rr_string bind_ip, uint16_t port, rr_string server_memory_limit,
     rr_error* error
