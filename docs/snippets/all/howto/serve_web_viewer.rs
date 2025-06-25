f32f5ae809049e0f125bf710bb9826d36adc8275
Andreas Reich <r_andreas2@web.de>
2025-06-25 16:31:02 +0200

    fix serve_web_viewer ci build and use it as opportunity for even more docs!

--- docs/snippets/all/howto/serve_web_viewer.rs
+++ docs/snippets/all/howto/serve_web_viewer.rs
@@ -1,10 +1,16 @@
 //! Demonstrates how to log data to a gRPC server and connect the web viewer to it.
+//!
+//! You need to enable the `web_viewer` feature on the `rerun` crate to use this.
+//! For the in-repository build, you can run this snippet using `cargo run -p snippets --features web_viewer -- serve_web_viewer`.
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Start a gRPC server and use it as log sink.
     let rec = rerun::RecordingStreamBuilder::new("rerun_example_serve_web_viewer").serve_grpc()?;
 
     // Connect the web viewer to the gRPC server and open it in the browser.
+    // Feature check here is for the in-repository build.
+    // For your own code remove it and enable the `web_viewer` feature on the `rerun` crate.
+    #[cfg(feature = "web_viewer")]
     let _server_guard = rerun::serve_web_viewer(rerun::web_viewer::WebViewerConfig {
         connect_to: Some("rerun+http://localhost/proxy".to_owned()),
         ..Default::default()
