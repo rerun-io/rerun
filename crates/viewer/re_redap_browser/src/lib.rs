901eef0eb25b95a9a37a196eb1681e973206ce3c
Lucas Meurer <lucasmeurer96@gmail.com>
2025-06-25 13:57:34 +0200

    Allow editing connection settings for an existing redap server (#10300)
    ### Related
    
    * closes https://github.com/rerun-io/rerun/issues/10247
    
    ### What
    
    Makes it possible to edit an existing redap server

--- crates/viewer/re_redap_browser/src/lib.rs
+++ crates/viewer/re_redap_browser/src/lib.rs
@@ -1,9 +1,9 @@
 //! This crates implements the Redap browser feature, including the communication and UI aspects of
 //! it.
 
-mod add_server_modal;
 mod context;
 mod entries;
+mod server_modal;
 mod servers;
 mod tables_session_context;
 
