1a5fcd1e1e617f66ebff06b31df5b3173a60547c
Andreas Reich <r_andreas2@web.de>
2025-06-25 13:32:58 +0200

    new snippets for serve_web

---
+++ docs/snippets/all/howto/serve_web_viewer.rs
@@ -0,0 +1,22 @@
+//! Demonstrate how to log data to a gRPC server and connect the web viewer to it.
+
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Start a gRPC server and use it as log sink.
+    let rec = rerun::RecordingStreamBuilder::new("rerun_example_serve_web_viewer").serve_grpc()?;
+
+    // Connect the web viewer to the gRPC server and open it in the browser.
+    let _server_guard = rerun::serve_web_viewer(rerun::web_viewer::WebViewerConfig {
+        connect_to: Some("rerun+http://localhost/proxy".to_owned()),
+        ..Default::default()
+    })?;
+
+    // Log some data to the gRPC server.
+    rec.log("data", &rerun::Boxes3D::from_half_sizes([(2.0, 2.0, 1.0)]))?;
+
+    // Keep server running. If we cancel it too early, data may never arrive in the browser.
+    std::thread::sleep(Duration::from_secs(u64::MAX));
+
+    Ok(())
+}
