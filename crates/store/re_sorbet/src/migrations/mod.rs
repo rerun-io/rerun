de9ae2b2ea2a630d285b31f63dd91268062dfd5d
Jochen Görtler <me@jgoertler.com>
2025-06-26 16:34:01 +0200

    Future-proof to some degree with semver

--- crates/store/re_sorbet/src/migrations/mod.rs
+++ crates/store/re_sorbet/src/migrations/mod.rs
@@ -1,17 +1,42 @@
-//! These are the migrations that were introduced for each version.
-//!
-//! For example [`v0_24`] contains the migrations that are needed
-//! to go from [`v0_23`] to [`v0_24`].
+#![allow(non_snake_case)]
+
+//! These are the migrations that are introduced for each Sorbet version.
 
 use arrow::array::RecordBatch;
+use semver::Version;
 
 mod make_list_arrays;
-mod v0_23;
-mod v0_24;
+
+// We introduce artifical names here for consistency. `v0_0_1` corresponds to
+// Rerun versions pre-`v0.23` and `v0_0_2` corresponds to Rerun version
+// `v0.23`. Starting with Rerun `v0.24`, we will track the Sorbet version
+// separately, starting at `v0.1.0`.
+
+mod v0_0_1__to__v0_0_2;
+mod v0_0_2__to__v0_1_0;
 
 // TODO(grtlr): Eventually, we should have a trait the abstracts over
 // migrations so that they will be easier to manage. But let's follow
 // the rule of three here.
+
+fn needs_migration_to_version(batch: &RecordBatch, target_semver: &'static str) -> bool {
+    // Expect is fine here, because `target_semver` is a static string.
+    let target_version = Version::parse(target_semver).expect("has to be a valid semver");
+
+    let Some(version_found) = batch.schema_ref().metadata().get("sorbet:version") else {
+        re_log::debug!("Encountered batch without 'sorbet:version' metadata.");
+        // We still do our best effort and try to perform the migration.
+        return true;
+    };
+
+    match Version::parse(version_found) {
+        Ok(version_found) => version_found < target_version,
+        Err(err) => {
+            re_log::error!("Could not parse 'sorbet:version': {err}");
+            false
+        }
+    }
+}
 
 /// Migrate a sorbet record batch of unknown version to the latest version.
 #[tracing::instrument(level = "trace", skip_all)]
@@ -20,20 +45,19 @@ pub fn migrate_record_batch(mut batch: RecordBatch) -> RecordBatch {
 
     re_tracing::profile_function!();
 
-    if v0_23::matches_schema(&batch) {
-        // Migrations from pre-`v0.23` to `v0.23`:
-        batch = v0_23::reorder_columns(&batch);
-        batch = v0_23::migrate_tuids(&batch);
-        batch = v0_23::migrate_record_batch(&batch);
+    if v0_0_1__to__v0_0_2::matches_schema(&batch) {
+        // Corresponds to migrations from pre-`v0.23` to `v0.23`:
+        batch = v0_0_1__to__v0_0_2::reorder_columns(&batch);
+        batch = v0_0_1__to__v0_0_2::migrate_tuids(&batch);
+        batch = v0_0_1__to__v0_0_2::migrate_record_batch(&batch);
     }
 
-    if true {
-        // TODO(#10322): only do this if needed
-        // Migrations from `v0.23` to `v0.24`:
-        batch = v0_24::rewire_tagged_components(&batch);
+    if needs_migration_to_version(&batch, "0.1.0") {
+        // Corresponds to migrations from `v0.23` to `v0.24`:
+        batch = v0_0_2__to__v0_1_0::rewire_tagged_components(&batch);
     }
 
-    batch = make_all_data_columns_list_arrays(&batch, v0_24::is_component_column);
+    batch = make_all_data_columns_list_arrays(&batch, v0_0_2__to__v0_1_0::is_component_column);
 
     batch
 }
