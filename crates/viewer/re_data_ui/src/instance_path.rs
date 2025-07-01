a56922e20817a4acaa31c8563a4c6a04b7a07824
Andreas Reich <andreas@rerun.io>
2025-07-01 10:37:15 +0200

    Adjust wording when hovering empty parent entity in timeline (#10452)

--- crates/viewer/re_data_ui/src/instance_path.rs
+++ crates/viewer/re_data_ui/src/instance_path.rs
@@ -59,7 +59,7 @@ impl DataUi for InstancePath {
             ui_layout.label(
                 ui,
                 format!(
-                    "{self} has no components on timeline {:?}",
+                    "{self} has no own components on timeline {:?}, but its children do",
                     query.timeline()
                 ),
             );
