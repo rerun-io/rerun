af1f20bd51f06626cb11295dda9e46718b2f29c7
Jochen Görtler <me@jgoertler.com>
2025-07-01 13:55:37 +0200

    Remove indicator component UI from `DataPath`

--- crates/viewer/re_data_ui/src/component_path.rs
+++ crates/viewer/re_data_ui/src/component_path.rs
@@ -20,38 +20,30 @@ impl DataUi for ComponentPath {
 
         let engine = db.storage_engine();
 
-        if let Some(archetype_name) =
-            component_descriptor.indicator_component_archetype_short_name()
-        {
-            ui.label(format!(
-                "Indicator component for the {archetype_name} archetype"
-            ));
-        } else {
-            let results = engine
-                .cache()
-                .latest_at(query, entity_path, [component_descriptor]);
+        let results = engine
+            .cache()
+            .latest_at(query, entity_path, [component_descriptor]);
 
-            if let Some(unit) = results.components.get(component_descriptor) {
-                crate::ComponentPathLatestAtResults {
-                    component_path: self.clone(),
-                    unit,
-                }
-                .data_ui(ctx, ui, ui_layout, query, db);
-            } else if ctx.recording().tree().subtree(entity_path).is_some() {
-                if engine.store().entity_has_component_on_timeline(
-                    &query.timeline(),
-                    entity_path,
-                    component_descriptor,
-                ) {
-                    ui.label("<unset>");
-                } else {
-                    ui.label(format!(
-                        "Entity {entity_path:?} has no component {component_descriptor:?}"
-                    ));
-                }
-            } else {
-                ui.error_label(format!("Unknown component path: {self}"));
+        if let Some(unit) = results.components.get(component_descriptor) {
+            crate::ComponentPathLatestAtResults {
+                component_path: self.clone(),
+                unit,
             }
+            .data_ui(ctx, ui, ui_layout, query, db);
+        } else if ctx.recording().tree().subtree(entity_path).is_some() {
+            if engine.store().entity_has_component_on_timeline(
+                &query.timeline(),
+                entity_path,
+                component_descriptor,
+            ) {
+                ui.label("<unset>");
+            } else {
+                ui.label(format!(
+                    "Entity {entity_path:?} has no component {component_descriptor:?}"
+                ));
+            }
+        } else {
+            ui.error_label(format!("Unknown component path: {self}"));
         }
     }
 }
