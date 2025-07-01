5bee168c6c0011af2d90e68003d2fce9200f79d2
Jochen Görtler <me@jgoertler.com>
2025-07-01 13:33:05 +0200

    try again..

--- rerun_py/src/python_bridge.rs
+++ rerun_py/src/python_bridge.rs
@@ -1340,10 +1340,10 @@ struct PyComponentDescriptor(pub ComponentDescriptor);
 
 #[pymethods]
 impl PyComponentDescriptor {
-    /// Creates a new component descriptor.
     #[new]
     #[pyo3(signature = (component, archetype=None, component_type=None))]
     #[pyo3(text_signature = "(self, archetype=None, component_type=None)")]
+    /// Creates a new component descriptor.
     fn partial(component: &str, archetype: Option<&str>, component_type: Option<&str>) -> Self {
         let descr = ComponentDescriptor {
             archetype: archetype.map(Into::into),
