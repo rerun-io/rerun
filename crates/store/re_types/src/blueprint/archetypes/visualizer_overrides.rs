1e1764a321eb582002ddcdce1b9a9216f2ae6fdb
Jochen Görtler <me@jgoertler.com>
2025-07-02 17:34:42 +0200

    update `.fbs` and remove outdated issue

--- crates/store/re_types/src/blueprint/archetypes/visualizer_overrides.rs
+++ crates/store/re_types/src/blueprint/archetypes/visualizer_overrides.rs
@@ -25,7 +25,8 @@ use ::re_types_core::{DeserializationError, DeserializationResult};
 /// of the visualizer system. It is not intended to be a long-term solution, but provides
 /// enough utility to be useful in the short term.
 ///
-/// The long-term solution is likely to be based off: <https://github.com/rerun-io/rerun/issues/6626>
+/// ⚠️ NOTE: Rerun `v0.24` changed the behavior of [`archetypes::VisualizerOverrides`][crate::blueprint::archetypes::VisualizerOverrides], so that currently they only
+/// work with time series views. We plan to bring this feature for all views in future versions.
 ///
 /// This can only be used as part of blueprints. It will have no effect if used
 /// in a regular entity.
