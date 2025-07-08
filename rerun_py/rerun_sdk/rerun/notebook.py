2d92205483e034b89007f2018460e8a8dcd129bc
jprochazk <pjanv42@gmail.com>
2025-07-08 17:48:48 +0200

    documentation

--- rerun_py/rerun_sdk/rerun/notebook.py
+++ rerun_py/rerun_sdk/rerun/notebook.py
@@ -83,10 +83,14 @@ class Viewer:
 
         Parameters
         ----------
-        width : int
-            The width of the viewer in pixels.
-        height : int
-            The height of the viewer in pixels.
+        width:
+            The width of the viewer in pixels, or "auto".
+
+            When set to "auto", scales to 100% of the notebook cell's width.
+        height:
+            The height of the viewer in pixels, or "auto".
+
+            When set to "auto", scales using a 16:9 aspect ratio with `width`.
         url:
             Optional URL passed to the viewer for displaying its contents.
         recording:
