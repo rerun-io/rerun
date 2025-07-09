acb670559a29d9206137bf1c4588f5e0f8474da2
Jeremy Leibs <jeremy@rerun.io>
2025-07-09 06:22:54 -0400

    Fix the ref_count impl

--- crates/top/re_sdk/src/recording_stream.rs
+++ crates/top/re_sdk/src/recording_stream.rs
@@ -816,11 +816,7 @@ impl RecordingStream {
     pub fn ref_count(&self) -> usize {
         match &self.inner {
             Either::Left(strong) => Arc::strong_count(strong),
-            Either::Right(weak) => weak
-                .upgrade()
-                .map_or(0, |this: Arc<Option<RecordingStreamInner>>| {
-                    Arc::strong_count(&this)
-                }),
+            Either::Right(weak) => weak.strong_count(),
         }
     }
 }
