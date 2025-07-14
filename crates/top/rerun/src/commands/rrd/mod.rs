632a5cb74b0850eb4d2e5c96d8a45aff154fbd26
Jochen Görtler <grtlr@users.noreply.github.com>
2025-07-14 16:56:28 +0200

    `rerun rrd route` command to manipulate `.rrd`s on transport level (#10607)
    ### Related
    
    * CLI part of #8860.
    * Supersedes #10596.
    
    ### What
    
    This adds a new `rerun rrd route` command that can be used to route one
    or multiple `.rrd`s to a new one (via `--output`) or to stdout.
    
    This provides a concise way to merge recordings using the CLI. In future
    versions, we also want to add that functionality to the UI.
    
    > [!IMPORTANT]
    > The `route` command will never decode the Arrow data, therefore no
    migration or verification will happen, which is good for performance. If
    you still want migration, merging, and compaction, the result can be
    piped into `rerun rrd merge`.
    
    Here is an example of the interface, which pipes directly to the viewer:
    
    ```sh
    rerun rrd route \
      --application-id <new-app-id> \
      --recording-id <new-recording-id> \
      a.rrd b.rrd | rerun -
    ```
    
    Or you can write the `.rrd` back out:
    
    ```sh
    rerun rrd route \
      --application-id <new-app-id> \
      --recording-id <new-recording-id> \
      --output <result.rrd> \
      a.rrd b.rrd
    ```

--- crates/top/rerun/src/commands/rrd/mod.rs
+++ crates/top/rerun/src/commands/rrd/mod.rs
@@ -3,6 +3,7 @@ mod filter;
 mod merge_compact;
 mod migrate;
 mod print;
+mod route;
 mod stats;
 mod verify;
 
@@ -12,6 +13,7 @@ use self::{
     merge_compact::{CompactCommand, MergeCommand},
     migrate::MigrateCommand,
     print::PrintCommand,
+    route::RouteCommand,
     stats::StatsCommand,
     verify::VerifyCommand,
 };
@@ -79,6 +81,14 @@ pub enum RrdCommands {
     /// Example: `rerun rrd print /my/recordings/*.rrd`
     Print(PrintCommand),
 
+    /// Manipulates the metadata of log message streams without decoding the payloads.
+    ///
+    /// This can be used to combine multiple .rrd files into a single recording.
+    /// Example: `rerun rrd route --recording-id my_recording /my/recordings/*.rrd > output.rrd`
+    ///
+    /// Note: Because the payload of the messages is never decoded, no migration or verification will performed.
+    Route(RouteCommand),
+
     /// Compute important statistics for one or more .rrd/.rbl files/streams.
     ///
     /// Reads from standard input if no paths are specified.
@@ -105,6 +115,7 @@ impl RrdCommands {
             Self::Merge(cmd) => cmd.run(),
             Self::Migrate(cmd) => cmd.run(),
             Self::Print(cmd) => cmd.run(),
+            Self::Route(cmd) => cmd.run(),
             Self::Stats(cmd) => cmd.run(),
             Self::Verify(cmd) => cmd.run(),
         }
