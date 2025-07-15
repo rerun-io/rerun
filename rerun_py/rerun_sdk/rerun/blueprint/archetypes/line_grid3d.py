033b966ab94987b2e76fce264d442f6b834cf0e1
Andreas Reich <andreas@rerun.io>
2025-07-15 17:34:41 +0200

    Make it easier to disable the 3D line grid in Python (blueprint) (#10621)

--- rerun_py/rerun_sdk/rerun/blueprint/archetypes/line_grid3d.py
+++ rerun_py/rerun_sdk/rerun/blueprint/archetypes/line_grid3d.py
@@ -5,8 +5,6 @@
 
 from __future__ import annotations
 
-from typing import Any
-
 from attrs import define, field
 
 from ... import components, datatypes
@@ -15,62 +13,20 @@ from ..._baseclasses import (
 )
 from ...blueprint import components as blueprint_components
 from ...error_utils import catch_and_log_exceptions
+from .line_grid3d_ext import LineGrid3DExt
 
 __all__ = ["LineGrid3D"]
 
 
 @define(str=False, repr=False, init=False)
-class LineGrid3D(Archetype):
+class LineGrid3D(LineGrid3DExt, Archetype):
     """
     **Archetype**: Configuration for the 3D line grid.
 
     ⚠️ **This type is _unstable_ and may change significantly in a way that the data won't be backwards compatible.**
     """
 
-    def __init__(
-        self: Any,
-        *,
-        visible: datatypes.BoolLike | None = None,
-        spacing: datatypes.Float32Like | None = None,
-        plane: datatypes.Plane3DLike | None = None,
-        stroke_width: datatypes.Float32Like | None = None,
-        color: datatypes.Rgba32Like | None = None,
-    ) -> None:
-        """
-        Create a new instance of the LineGrid3D archetype.
-
-        Parameters
-        ----------
-        visible:
-            Whether the grid is visible.
-
-            Defaults to true.
-        spacing:
-            Space between grid lines spacing of one line to the next in scene units.
-
-            As you zoom out, successively only every tenth line is shown.
-            This controls the closest zoom level.
-        plane:
-            In what plane the grid is drawn.
-
-            Defaults to whatever plane is determined as the plane at zero units up/down as defined by [`components.ViewCoordinates`][rerun.components.ViewCoordinates] if present.
-        stroke_width:
-            How thick the lines should be in ui units.
-
-            Default is 1.0 ui unit.
-        color:
-            Color used for the grid.
-
-            Transparency via alpha channel is supported.
-            Defaults to a slightly transparent light gray.
-
-        """
-
-        # You can define your own __init__ function as a member of LineGrid3DExt in line_grid3d_ext.py
-        with catch_and_log_exceptions(context=self.__class__.__name__):
-            self.__attrs_init__(visible=visible, spacing=spacing, plane=plane, stroke_width=stroke_width, color=color)
-            return
-        self.__attrs_clear__()
+    # __init__ can be found in line_grid3d_ext.py
 
     def __attrs_clear__(self) -> None:
         """Convenience method for calling `__attrs_init__` with all `None`s."""
