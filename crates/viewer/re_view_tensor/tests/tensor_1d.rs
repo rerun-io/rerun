b67878d4f960dac2908f76082e6690f837008607
Jochen Görtler <grtlr@users.noreply.github.com>
2025-07-16 14:53:46 +0200

    Automate `check_1d_tensor_data.py` (#10659)

---
+++ crates/viewer/re_view_tensor/tests/tensor_1d.rs
@@ -0,0 +1,56 @@
+use re_chunk_store::RowId;
+use re_log_types::TimePoint;
+use re_types::archetypes;
+use re_view_tensor::TensorView;
+use re_viewer_context::{ViewClass as _, ViewId, test_context::TestContext};
+use re_viewport::test_context_ext::TestContextExt as _;
+use re_viewport_blueprint::ViewBlueprint;
+
+fn x() -> Vec<f32> {
+    (0..100).map(|i| i as f32 * 100.0 / 99.0).collect()
+}
+
+#[test]
+fn test_tensor() {
+    let mut test_context = TestContext::new_with_view_class::<TensorView>();
+
+    test_context.log_entity("tensor", |builder| {
+        builder.with_archetype(
+            RowId::new(),
+            TimePoint::STATIC,
+            &archetypes::Tensor::new(x()),
+        )
+    });
+
+    let view_id = setup_blueprint(&mut test_context);
+    run_view_ui_and_save_snapshot(
+        &mut test_context,
+        view_id,
+        "tensor_1d",
+        egui::vec2(300.0, 50.0),
+    );
+}
+
+fn setup_blueprint(test_context: &mut TestContext) -> ViewId {
+    test_context.setup_viewport_blueprint(|_ctx, blueprint| {
+        blueprint.add_view_at_root(ViewBlueprint::new_with_root_wildcard(
+            TensorView::identifier(),
+        ))
+    })
+}
+
+fn run_view_ui_and_save_snapshot(
+    test_context: &mut TestContext,
+    view_id: ViewId,
+    name: &str,
+    size: egui::Vec2,
+) {
+    let mut harness = test_context
+        .setup_kittest_for_rendering()
+        .with_size(size)
+        .build(|ctx| {
+            test_context.run_with_single_view(ctx, view_id);
+        });
+    harness.run();
+    harness.snapshot(name);
+}
