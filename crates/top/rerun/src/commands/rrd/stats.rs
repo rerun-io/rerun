485ed1762f6a9ae95b7d244ab9483edf85a66a19
Jeremy Leibs <jeremy@rerun.io>
2025-07-17 08:27:19 -0400

    Improve count display in rrd compact and stats

--- crates/top/rerun/src/commands/rrd/stats.rs
+++ crates/top/rerun/src/commands/rrd/stats.rs
@@ -146,13 +146,14 @@ impl StatsCommand {
                 )
             }
 
+            let msg_count = num_msgs + 1;
             let check_in_interval = 10_000;
-            if (num_msgs + 1) % check_in_interval == 0 {
+            if msg_count % check_in_interval == 0 {
                 let msgs_per_sec =
                     check_in_interval as f64 / last_checkpoint.elapsed().as_secs_f64();
                 last_checkpoint = std::time::Instant::now();
                 re_log::info!(
-                    "processed {num_msgs} messages so far, current speed is {msgs_per_sec:.2} msg/s"
+                    "processed {msg_count} messages so far, current speed is {msgs_per_sec:.2} msg/s"
                 );
                 re_tracing::reexports::puffin::GlobalProfiler::lock().new_frame();
             }
