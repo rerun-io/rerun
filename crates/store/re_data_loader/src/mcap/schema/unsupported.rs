c7ff2815981b1b36b9ac8fb9319a1bbbde8172f7
Jochen Görtler <me@jgoertler.com>
2025-07-25 12:22:43 +0200

    Log `TextLog` when encountering unsupported schema

--- crates/store/re_data_loader/src/mcap/schema/unsupported.rs
+++ crates/store/re_data_loader/src/mcap/schema/unsupported.rs
@@ -2,7 +2,7 @@ use re_chunk::{
     ChunkId,
     external::arrow::array::{FixedSizeListBuilder, StringBuilder, UInt16Builder},
 };
-use re_types::ComponentDescriptor;
+use re_types::{ComponentDescriptor, archetypes};
 
 use crate::mcap::decode::{McapMessageParser, ParserContext, PluginError};
 
@@ -10,6 +10,8 @@ pub struct UnsupportedSchemaMessageParser {
     schema_ids: FixedSizeListBuilder<UInt16Builder>,
     schema_names: FixedSizeListBuilder<StringBuilder>,
     encodings: FixedSizeListBuilder<StringBuilder>,
+    text_log_msg: FixedSizeListBuilder<StringBuilder>,
+    text_log_level: FixedSizeListBuilder<StringBuilder>,
 }
 
 impl UnsupportedSchemaMessageParser {
@@ -20,6 +22,9 @@ impl UnsupportedSchemaMessageParser {
             schema_ids: FixedSizeListBuilder::with_capacity(UInt16Builder::new(), 1, num_rows),
             schema_names: FixedSizeListBuilder::with_capacity(StringBuilder::new(), 1, num_rows),
             encodings: FixedSizeListBuilder::with_capacity(StringBuilder::new(), 1, num_rows),
+            //
+            text_log_msg: FixedSizeListBuilder::with_capacity(StringBuilder::new(), 1, num_rows),
+            text_log_level: FixedSizeListBuilder::with_capacity(StringBuilder::new(), 1, num_rows),
         }
     }
 }
@@ -27,22 +32,32 @@ impl UnsupportedSchemaMessageParser {
 impl McapMessageParser for UnsupportedSchemaMessageParser {
     fn append(&mut self, _ctx: &mut ParserContext, msg: &mcap::Message<'_>) -> anyhow::Result<()> {
         if let Some(schema) = msg.channel.schema.as_ref() {
+            let schema_name = schema.name.as_str();
             self.schema_ids.values().append_value(schema.id);
-            self.schema_names
+            self.schema_names.values().append_value(schema_name);
+            self.text_log_msg
                 .values()
-                .append_value(schema.name.as_str());
+                .append_value(format!("Unsupported schema: {schema_name}"));
         } else {
             self.schema_ids.values().append_null();
             self.schema_names.values().append_null();
+            self.text_log_msg
+                .values()
+                .append_value("Unsupported message");
         };
 
         self.encodings
             .values()
             .append_value(msg.channel.message_encoding.as_str());
 
+        self.text_log_level.values().append_value("WARN");
+
         self.schema_ids.append(true);
         self.schema_names.append(true);
         self.encodings.append(true);
+
+        self.text_log_msg.append(true);
+        self.text_log_level.append(true);
 
         Ok(())
     }
@@ -52,10 +67,31 @@ impl McapMessageParser for UnsupportedSchemaMessageParser {
             mut schema_ids,
             mut schema_names,
             mut encodings,
+            mut text_log_msg,
+            mut text_log_level,
         } = *self;
 
         let entity_path = ctx.entity_path().clone();
         let timelines = ctx.build_timelines();
+
+        let log_chunk = re_chunk::Chunk::from_auto_row_ids(
+            ChunkId::new(),
+            entity_path.clone(),
+            Default::default(),
+            [
+                (
+                    archetypes::TextLog::descriptor_text(),
+                    text_log_msg.finish().into(),
+                ),
+                (
+                    archetypes::TextLog::descriptor_level(),
+                    text_log_level.finish().into(),
+                ),
+            ]
+            .into_iter()
+            .collect(),
+        )
+        .map_err(|err| PluginError::Other(anyhow::anyhow!(err)))?;
 
         let chunk = re_chunk::Chunk::from_auto_row_ids(
             ChunkId::new(),
@@ -83,6 +119,6 @@ impl McapMessageParser for UnsupportedSchemaMessageParser {
         )
         .map_err(|err| PluginError::Other(anyhow::anyhow!(err)))?;
 
-        Ok(vec![chunk])
+        Ok(vec![log_chunk, chunk])
     }
 }
