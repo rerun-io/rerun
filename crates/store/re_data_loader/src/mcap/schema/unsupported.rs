5d79280a368d4ae15377d16bc318d8699453c1d9
Jochen Görtler <me@jgoertler.com>
2025-07-28 15:50:43 +0200

    hack around unsupported

--- crates/store/re_data_loader/src/mcap/schema/unsupported.rs
+++ crates/store/re_data_loader/src/mcap/schema/unsupported.rs
@@ -1,8 +1,9 @@
+use arrow::array::{ListBuilder, UInt8Builder};
 use re_chunk::{
     ChunkId,
     external::arrow::array::{FixedSizeListBuilder, StringBuilder, UInt16Builder},
 };
-use re_types::{ComponentDescriptor, archetypes};
+use re_types::{Component as _, ComponentDescriptor, archetypes, components};
 
 use crate::mcap::decode::{McapMessageParser, ParserContext, PluginError};
 
@@ -10,6 +11,9 @@ pub struct UnsupportedSchemaMessageParser {
     schema_ids: FixedSizeListBuilder<UInt16Builder>,
     schema_names: FixedSizeListBuilder<StringBuilder>,
     encodings: FixedSizeListBuilder<StringBuilder>,
+    data: FixedSizeListBuilder<ListBuilder<UInt8Builder>>,
+    msg_data: FixedSizeListBuilder<ListBuilder<UInt8Builder>>,
+
     text_log_msg: FixedSizeListBuilder<StringBuilder>,
     text_log_level: FixedSizeListBuilder<StringBuilder>,
 }
@@ -22,7 +26,9 @@ impl UnsupportedSchemaMessageParser {
             schema_ids: FixedSizeListBuilder::with_capacity(UInt16Builder::new(), 1, num_rows),
             schema_names: FixedSizeListBuilder::with_capacity(StringBuilder::new(), 1, num_rows),
             encodings: FixedSizeListBuilder::with_capacity(StringBuilder::new(), 1, num_rows),
+            data: FixedSizeListBuilder::with_capacity(Default::default(), 1, num_rows),
             //
+            msg_data: FixedSizeListBuilder::with_capacity(Default::default(), 1, num_rows),
             text_log_msg: FixedSizeListBuilder::with_capacity(StringBuilder::new(), 1, num_rows),
             text_log_level: FixedSizeListBuilder::with_capacity(StringBuilder::new(), 1, num_rows),
         }
@@ -35,6 +41,8 @@ impl McapMessageParser for UnsupportedSchemaMessageParser {
             let schema_name = schema.name.as_str();
             self.schema_ids.values().append_value(schema.id);
             self.schema_names.values().append_value(schema_name);
+            self.data.values().values().append_slice(&schema.data);
+
             self.text_log_msg
                 .values()
                 .append_value(format!("Unsupported schema: {schema_name}"));
@@ -46,6 +54,10 @@ impl McapMessageParser for UnsupportedSchemaMessageParser {
                 .append_value("Unsupported message");
         };
 
+        self.msg_data.values().values().append_slice(&msg.data);
+        self.msg_data.values().append(true);
+        self.msg_data.append(true);
+
         self.encodings
             .values()
             .append_value(msg.channel.message_encoding.as_str());
@@ -55,6 +67,8 @@ impl McapMessageParser for UnsupportedSchemaMessageParser {
         self.schema_ids.append(true);
         self.schema_names.append(true);
         self.encodings.append(true);
+        self.data.values().append(true);
+        self.data.append(true);
 
         self.text_log_msg.append(true);
         self.text_log_level.append(true);
@@ -64,6 +78,8 @@ impl McapMessageParser for UnsupportedSchemaMessageParser {
 
     fn finalize(self: Box<Self>, ctx: ParserContext) -> anyhow::Result<Vec<re_chunk::Chunk>> {
         let Self {
+            mut msg_data,
+            mut data,
             mut schema_ids,
             mut schema_names,
             mut encodings,
@@ -112,6 +128,22 @@ impl McapMessageParser for UnsupportedSchemaMessageParser {
                     ComponentDescriptor::partial("encoding")
                         .with_archetype(Self::ARCHETYPE_NAME.into()),
                     encodings.finish().into(),
+                ),
+                (
+                    ComponentDescriptor {
+                        archetype: Some("rerun.mcap.Schema".into()),
+                        component: "data".into(),
+                        component_type: Some(components::Blob::name()),
+                    },
+                    data.finish().into(),
+                ),
+                (
+                    ComponentDescriptor {
+                        archetype: Some("rerun.mcap.Message".into()),
+                        component: "data".into(),
+                        component_type: Some(components::Blob::name()),
+                    },
+                    msg_data.finish().into(),
                 ),
             ]
             .into_iter()
