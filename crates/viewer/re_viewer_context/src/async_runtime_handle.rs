dee443463fb8f98f39525b2b640017683a2412b3
jprochazk <pjanv42@gmail.com>
2025-07-28 18:27:56 +0200

    expose inner runtime on native

--- crates/viewer/re_viewer_context/src/async_runtime_handle.rs
+++ crates/viewer/re_viewer_context/src/async_runtime_handle.rs
@@ -39,6 +39,11 @@ impl AsyncRuntimeHandle {
         Self {}
     }
 
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn inner(&self) -> &tokio::runtime::Handle {
+        &self.tokio
+    }
+
     /// Create an `AsyncRuntime` from the current tokio runtime on native.
     #[cfg_attr(target_arch = "wasm32", expect(clippy::unnecessary_wraps))]
     pub fn from_current_tokio_runtime_or_wasmbindgen() -> Result<Self, AsyncRuntimeError> {
