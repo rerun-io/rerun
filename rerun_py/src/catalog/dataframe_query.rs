980943de87867f27c1efed1a7995ad2bb9222abf
Tim Saucer <timsaucer@gmail.com>
2025-08-05 11:32:11 -0400

    Remove cruft

--- rerun_py/src/catalog/dataframe_query.rs
+++ rerun_py/src/catalog/dataframe_query.rs
@@ -481,29 +481,6 @@ impl PyDataframeQueryView {
         let dataset_id = entry.details.id;
         let connection = entry.client.borrow(py).connection().clone();
 
-        //
-        // Fetch relevant chunks
-        //
-
-        // let chunk_stores = connection.get_chunks_for_dataframe_query(
-        //     py,
-        //     dataset_id,
-        //     &self.query_expression,
-        //     self.partition_ids.as_slice(),
-        // )?;
-        //
-        // let query_engines = chunk_stores
-        //     .into_iter()
-        //     .map(|(partition_id, store_handle)| {
-        //         let query_engine = QueryEngine::new(
-        //             store_handle.clone(),
-        //             QueryCache::new_handle(store_handle.clone()),
-        //         );
-        //
-        //         (partition_id, query_engine)
-        //     })
-        //     .collect();
-
         wait_for_future(py, async {
             DataframeQueryTableProvider::new(
                 connection.origin().clone(),
@@ -515,10 +492,6 @@ impl PyDataframeQueryView {
         })
         .map(|p| Arc::new(p) as Arc<dyn TableProvider>)
         .map_err(to_py_err)
-
-        // DataframeQueryTableProvider::new(query_engines, &self.query_expression)
-        //     .map(|p| Arc::new(p) as Arc<dyn TableProvider>)
-        //     .map_err(to_py_err)
     }
 }
 
