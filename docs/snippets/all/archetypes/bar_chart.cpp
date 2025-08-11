2464c0901721589ff3b7d70cf0eb550a88088627
Andreas Reich <r_andreas2@web.de>
2025-08-11 11:01:40 +0200

    more trying to make gcc happy

--- docs/snippets/all/archetypes/bar_chart.cpp
+++ docs/snippets/all/archetypes/bar_chart.cpp
@@ -10,9 +10,9 @@ int main() {
     rec.log("bar_chart", rerun::BarChart::i64({8, 4, 0, 9, 1, 4, 1, 6, 9, 0}));
 
     auto abscissa = std::vector<int64_t>{0, 1, 3, 4, 7, 11};
+    auto abscissa_data = rerun::TensorData(rerun::Collection{abscissa.size()}, abscissa);
     rec.log(
         "bar_chart_custom_abscissa",
-        rerun::BarChart::i64({8, 4, 0, 9, 1, 4})
-            .with_abscissa(rerun::TensorData({abscissa.size()}, abscissa))
+        rerun::BarChart::i64({8, 4, 0, 9, 1, 4}).with_abscissa(abscissa_data)
     );
 }
--- rerun_cpp/src/rerun/archetypes/bar_chart.hpp
+++ rerun_cpp/src/rerun/archetypes/bar_chart.hpp
@@ -36,10 +36,10 @@ namespace rerun::archetypes {
     ///     rec.log("bar_chart", rerun::BarChart::i64({8, 4, 0, 9, 1, 4, 1, 6, 9, 0}));
     ///
     ///     auto abscissa = std::vector<int64_t>{0, 1, 3, 4, 7, 11};
+    ///     auto abscissa_data = rerun::TensorData(rerun::Collection{abscissa.size()}, abscissa);
     ///     rec.log(
     ///         "bar_chart_custom_abscissa",
-    ///         rerun::BarChart::i64({8, 4, 0, 9, 1, 4})
-    ///             .with_abscissa(rerun::TensorData({abscissa.size()}, abscissa))
+    ///         rerun::BarChart::i64({8, 4, 0, 9, 1, 4}).with_abscissa(abscissa_data)
     ///     );
     /// }
     /// ```
