785f9edb2d1d2886995c05daec852582a598b8bb
Tim Saucer <timsaucer@gmail.com>
2025-08-12 14:36:36 -0400

    Add test for to validate partitions come in order

--- tests/python/server_integration/server_integration.py
+++ tests/python/server_integration/server_integration.py
@@ -45,9 +45,50 @@ def aggregation_test() -> None:
     assert results[0][1][0] == pa.scalar(50.0, type=pa.float32())
 
 
+def partition_ordering_test() -> None:
+
+    client = rr.catalog.CatalogClient(CATALOG_URL)
+    dataset = client.get_dataset(name=DATASET)
+
+
+    for time_index in ["time_1", "time_2", "time_3"]:
+        streams = (
+            dataset
+            .dataframe_query_view(index=time_index, contents="/**")
+            .fill_latest_at()
+            .df()
+            .select("rerun_partition_id", time_index)
+            .execute_stream_partitioned()
+        )
+
+        prior_partition_ids = set()
+        for rb_reader in streams:
+
+            prior_partition = ""
+            prior_timestamp = 0
+            for rb in iter(rb_reader):
+
+                rb = rb.to_pyarrow()
+                for idx in range(rb.num_rows):
+                    partition = rb[0][idx].as_py()
+                    timestamp = rb[1][idx].as_py()
+
+                    assert partition >= prior_partition
+                    if partition == prior_partition and timestamp is not None:
+                        assert timestamp >= prior_timestamp
+                    else:
+                        assert partition not in prior_partition_ids
+                        prior_partition_ids.add(partition)
+
+                    prior_partition = partition
+                    if timestamp is not None:
+                        prior_timestamp = timestamp
+
+
 def main() -> None:
     tests = {
         "aggregation_test": aggregation_test,
+        "test_partition_ordering": partition_ordering_test,
     }
 
     parser = argparse.ArgumentParser(description="Tests the gRPC interface between rerun SDK and server")
