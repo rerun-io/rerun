d9154259a9d9b1d19b0d7f76f4963338df7ce6ab
Andrea Reale <andrea@rerun.io>
2025-08-19 16:55:28 +0200

    dataplatform: protos for manifest_registry cleanup routine
    Introduce a new endpoint to trigger dataplatform cleanup routines
    
    Signed-off-by: Andrea Reale <andrea@rerun.io>

--- crates/store/re_protos/src/v1alpha1/rerun.frontend.v1alpha1.rs
+++ crates/store/re_protos/src/v1alpha1/rerun.frontend.v1alpha1.rs
@@ -390,6 +390,18 @@ impl ::prost::Name for DoMaintenanceRequest {
         "/rerun.frontend.v1alpha1.DoMaintenanceRequest".into()
     }
 }
+#[derive(Clone, Copy, PartialEq, ::prost::Message)]
+pub struct CleanupDatasetsRequest {}
+impl ::prost::Name for CleanupDatasetsRequest {
+    const NAME: &'static str = "CleanupDatasetsRequest";
+    const PACKAGE: &'static str = "rerun.frontend.v1alpha1";
+    fn full_name() -> ::prost::alloc::string::String {
+        "rerun.frontend.v1alpha1.CleanupDatasetsRequest".into()
+    }
+    fn type_url() -> ::prost::alloc::string::String {
+        "/rerun.frontend.v1alpha1.CleanupDatasetsRequest".into()
+    }
+}
 /// Generated client implementations.
 pub mod frontend_service_client {
     #![allow(
@@ -1080,6 +1092,30 @@ pub mod frontend_service_client {
             ));
             self.inner.unary(req, path, codec).await
         }
+        /// Cleanup internal data for datasets that are no longer referenced by any entries in the catalog.
+        pub async fn cleanup_datasets(
+            &mut self,
+            request: impl tonic::IntoRequest<super::CleanupDatasetsRequest>,
+        ) -> std::result::Result<
+            tonic::Response<
+                super::super::super::manifest_registry::v1alpha1::CleanupDatasetsResponse,
+            >,
+            tonic::Status,
+        > {
+            self.inner.ready().await.map_err(|e| {
+                tonic::Status::unknown(format!("Service was not ready: {}", e.into()))
+            })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/rerun.frontend.v1alpha1.FrontendService/CleanupDatasets",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut().insert(GrpcMethod::new(
+                "rerun.frontend.v1alpha1.FrontendService",
+                "CleanupDatasets",
+            ));
+            self.inner.unary(req, path, codec).await
+        }
     }
 }
 /// Generated server implementations.
@@ -1354,6 +1390,16 @@ pub mod frontend_service_server {
         ) -> std::result::Result<
             tonic::Response<
                 super::super::super::manifest_registry::v1alpha1::DoMaintenanceResponse,
+            >,
+            tonic::Status,
+        >;
+        /// Cleanup internal data for datasets that are no longer referenced by any entries in the catalog.
+        async fn cleanup_datasets(
+            &self,
+            request: tonic::Request<super::CleanupDatasetsRequest>,
+        ) -> std::result::Result<
+            tonic::Response<
+                super::super::super::manifest_registry::v1alpha1::CleanupDatasetsResponse,
             >,
             tonic::Status,
         >;
@@ -2531,6 +2577,48 @@ pub mod frontend_service_server {
                     let inner = self.inner.clone();
                     let fut = async move {
                         let method = DoMaintenanceSvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.unary(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/rerun.frontend.v1alpha1.FrontendService/CleanupDatasets" => {
+                    #[allow(non_camel_case_types)]
+                    struct CleanupDatasetsSvc<T: FrontendService>(pub Arc<T>);
+                    impl<T: FrontendService>
+                        tonic::server::UnaryService<super::CleanupDatasetsRequest>
+                        for CleanupDatasetsSvc<T>
+                    {
+                        type Response = super::super::super::manifest_registry::v1alpha1::CleanupDatasetsResponse;
+                        type Future = BoxFuture<tonic::Response<Self::Response>, tonic::Status>;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<super::CleanupDatasetsRequest>,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                <T as FrontendService>::cleanup_datasets(&inner, request).await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let method = CleanupDatasetsSvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
