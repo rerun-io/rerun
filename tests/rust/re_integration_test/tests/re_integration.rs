b947cb6d16fac899115ba5bcbebff8f37bfebc37
lucasmerlin <hi@lucasmerlin.me>
2025-08-20 17:09:50 +0200

    Add snapshot test

--- tests/rust/re_integration_test/tests/re_integration.rs
+++ tests/rust/re_integration_test/tests/re_integration.rs
@@ -1,15 +1,13 @@
+use insta::with_settings;
+use re_integration_test::{TestServer, load_test_data};
+use std::process::{Command, Stdio};
+use std::thread;
+use std::time::Duration;
+
 #[test]
 pub fn integration_test() {
-    let mut server = std::process::Command::new("pixi");
-    server.args(["run", "rerun", "server"]);
-    let mut server = server.spawn().unwrap();
+    let _server = TestServer::spawn();
+    let test_output = load_test_data();
 
-    let mut script = std::process::Command::new(
-        "pixi run python tests/rust/re_integration_test/tests/re_integration.py",
-    );
-    let mut script = script.spawn().unwrap();
-
-    script.wait().unwrap();
-
-    server.wait().unwrap();
+    insta::assert_snapshot!(test_output);
 }
