19985163e908cf57aa7f254bc4fb8741504c834d
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-08-22 13:02:07 +0200

    add link to new issue

--- crates/viewer/re_viewer/tests/app_kittest.rs
+++ crates/viewer/re_viewer/tests/app_kittest.rs
@@ -5,7 +5,7 @@ use egui_kittest::kittest::Queryable as _;
 use re_viewer::viewer_test_utils;
 
 /// Navigates from welcome to settings screen and snapshots it.
-#[cfg(not(windows))] // TODO(emilk): Fails on windows because the metrics are unexpectedly shown
+#[cfg(not(windows))] // TODO(#10971): Fix it
 #[tokio::test]
 async fn settings_screen() {
     let mut harness = viewer_test_utils::viewer_harness();
