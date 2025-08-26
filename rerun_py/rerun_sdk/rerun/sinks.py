2ae6d64199d005a3617ac48ce3a82becc3b34789
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-08-26 19:56:20 +0200

    Remove docstring for non-existing parameter

--- rerun_py/rerun_sdk/rerun/sinks.py
+++ rerun_py/rerun_sdk/rerun/sinks.py
@@ -136,11 +136,7 @@ def connect_grpc(
         and the pathname must be `/proxy`.
 
         The default is `rerun+http://127.0.0.1:9876/proxy`.
-    flush_timeout_sec:
-        The minimum time the SDK will wait during a flush before potentially
-        dropping data if progress is not being made. Passing `None` indicates no timeout,
-        and can cause a call to `flush` to block indefinitely.
-    default_blueprint
+    default_blueprint:
         Optionally set a default blueprint to use for this application. If the application
         already has an active blueprint, the new blueprint won't become active until the user
         clicks the "reset blueprint" button. If you want to activate the new blueprint
