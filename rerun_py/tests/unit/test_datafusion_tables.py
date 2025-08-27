517ba2ebd76881cf0f6b30049ff65af064c20821
Tim Saucer <timsaucer@gmail.com>
2025-08-27 13:30:07 -0400

    Add unit test for url generation

--- rerun_py/tests/unit/test_datafusion_tables.py
+++ rerun_py/tests/unit/test_datafusion_tables.py
@@ -183,3 +183,28 @@ def test_partition_ordering(server_instance: tuple[subprocess.Popen[str], Datase
                     prior_partition = partition
                     if timestamp is not None:
                         prior_timestamp = timestamp
+
+
+def test_url_generation(server_instance: tuple[subprocess.Popen[str], DatasetEntry]) -> None:
+    from rerun.utilities.datafusion.functions import url_generation
+
+    (_process, dataset) = server_instance
+
+    udf = url_generation.partition_url_with_timeref_udf(dataset, "time_1")
+
+    results = (
+        dataset.dataframe_query_view(index="time_1", contents="/**")
+        .df()
+        .with_column("url", udf(col("rerun_partition_id"), col("time_1")))
+        .sort(col("rerun_partition_id"), col("time_1"))
+        .limit(1)
+        .select("url")
+        .collect()
+    )
+
+    # Since the OSS server will generate a random dataset ID at startup, we can only check part of
+    # the generated URL
+    assert (
+        "partition_id=0cd72aae349f46bc97540d144582ff15#when=time_1@2024-01-15T10:30:45.123457000Z"
+        in results[0][0][0].as_py()
+    )
