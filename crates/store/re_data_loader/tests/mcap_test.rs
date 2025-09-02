93ef65851a4590afe1454bbbc9115027a149612a
Gábor Gyebnár <korteur@gmail.com>
2025-09-02 13:07:06 +0200

    Draft version

--- crates/store/re_data_loader/tests/mcap_test.rs
+++ crates/store/re_data_loader/tests/mcap_test.rs
@@ -1,9 +1,44 @@
 #[cfg(test)]
 mod tests {
+    use arrow::array::Float64Array;
+    use re_chunk::{ArchetypeName, Chunk, ComponentIdentifier};
     use re_data_loader::{DataLoaderSettings, LoadedData, loader_mcap::load_mcap};
-    use re_mcap::{cdr::try_encode_message, layers::SelectedLayers};
+    use re_mcap::{cdr::try_encode_message, layers::SelectedLayers, parsers::ros2msg};
 
-    fn generate_ros2_mcap() -> Vec<u8> {
+    fn make_imu() -> ros2msg::definitions::sensor_msgs::Imu {
+        let header = ros2msg::definitions::std_msgs::Header {
+            stamp: ros2msg::definitions::builtin_interfaces::Time {
+                sec: 123,
+                nanosec: 45,
+            },
+            frame_id: "boo".to_owned(),
+        };
+        let orientation = ros2msg::definitions::geometry_msgs::Quaternion {
+            x: std::f64::consts::PI,
+            y: 0.0,
+            z: 0.0,
+            w: 1.125,
+        };
+        ros2msg::definitions::sensor_msgs::Imu {
+            header,
+            orientation,
+            orientation_covariance: [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0],
+            angular_velocity: ros2msg::definitions::geometry_msgs::Vector3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            angular_velocity_covariance: [0.0; 9],
+            linear_acceleration: ros2msg::definitions::geometry_msgs::Vector3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            linear_acceleration_covariance: [0.0; 9],
+        }
+    }
+
+    fn generate_ros2_mcap(imu: &ros2msg::definitions::sensor_msgs::Imu) -> Vec<u8> {
         let mut buffer = Vec::new();
         {
             let mut writer = mcap::Writer::new(std::io::Cursor::new(&mut buffer)).unwrap();
@@ -11,13 +46,13 @@ mod tests {
             // Register a schema
             let schema_name = "sensor_msgs/msg/Imu";
             let schema_encoding = "ros2msg";
-            let schema_data = br#"..."#;
+            let schema_data = br#"ignored_schema_description"#;
             let schema_id = writer
                 .add_schema(schema_name, schema_encoding, schema_data)
                 .expect("Failed to add schema");
 
             // Register a channel
-            let channel_topic = "/test/points";
+            let channel_topic = "/test/imu";
             let message_encoding = "cdr";
             let channel_id = writer
                 .add_channel(
@@ -28,42 +63,9 @@ mod tests {
                 )
                 .expect("Failed to add channel");
 
-            let header = re_mcap::parsers::ros2msg::definitions::std_msgs::Header {
-                stamp: re_mcap::parsers::ros2msg::definitions::builtin_interfaces::Time {
-                    sec: 123,
-                    nanosec: 45,
-                },
-                frame_id: "boo".to_owned(),
-            };
-            let orientation = re_mcap::parsers::ros2msg::definitions::geometry_msgs::Quaternion {
-                x: std::f64::consts::PI,
-                y: 0.0,
-                z: 0.0,
-                w: 1.125,
-            };
-            let imu = re_mcap::parsers::ros2msg::definitions::sensor_msgs::Imu {
-                header,
-                orientation,
-                orientation_covariance: [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0],
-                angular_velocity: re_mcap::parsers::ros2msg::definitions::geometry_msgs::Vector3 {
-                    x: 0.0,
-                    y: 0.0,
-                    z: 0.0,
-                },
-                angular_velocity_covariance: [0.0; 9],
-                linear_acceleration:
-                    re_mcap::parsers::ros2msg::definitions::geometry_msgs::Vector3 {
-                        x: 0.0,
-                        y: 0.0,
-                        z: 0.0,
-                    },
-                linear_acceleration_covariance: [0.0; 9],
-            };
-
             let message_data = try_encode_message(&imu).expect("Failed to encode message");
 
             let timestamp = 1000000000;
-
             let message_header = mcap::records::MessageHeader {
                 channel_id,
                 sequence: 0,
@@ -74,52 +76,91 @@ mod tests {
                 .write_to_known_channel(&message_header, &message_data)
                 .expect("Failed to write message");
 
-            // // Write some test messages
-            // for i in 0..5 {
-            //     let timestamp = 1000000000 + i * 100000000; // nanoseconds
-
-            //     // Simple CDR-encoded point data (x=i, y=i*2, z=i*3)
-            //     let mut message_data = Vec::new();
-            //     message_data.extend_from_slice(&(i as f64).to_le_bytes()); // x
-            //     message_data.extend_from_slice(&((i * 2) as f64).to_le_bytes()); // y
-            //     message_data.extend_from_slice(&((i * 3) as f64).to_le_bytes()); // z
-
-            //     let message_header = mcap::records::MessageHeader {
-            //         channel_id,
-            //         sequence: i as u32,
-            //         log_time: timestamp,
-            //         publish_time: timestamp,
-            //     };
-            //     writer
-            //         .write_to_known_channel(&message_header, &message_data)
-            //         .expect("Failed to write message");
-            // }
-
             writer.finish().unwrap();
         }
         buffer
     }
 
+    fn assert_component(chunks: &[Chunk], archetype: &str, component: &str, value: &[f64]) {
+        let archetype_name = ArchetypeName::new(archetype);
+        let component_identifier = ComponentIdentifier::new(&format!("{archetype}:{component}"));
+
+        for chunk in chunks {
+            for (component_desc, rows) in chunk.components().iter() {
+                if component_desc.archetype == Some(archetype_name)
+                    && component_desc.component == component_identifier
+                {
+                    println!("Found component {archetype}:{component}");
+                    // Only check the first row for now
+                    let cell = rows
+                        .iter()
+                        .flatten()
+                        .next()
+                        .expect("Expected at least one row");
+                    let float_array = cell
+                        .as_any()
+                        .downcast_ref::<Float64Array>()
+                        .expect("Expected cell to be a Float64Array");
+                    let cell_value = float_array.values().as_ref();
+                    assert_eq!(cell_value, value);
+                    return;
+                }
+            }
+        }
+        panic!("Component {archetype}:{component} not found");
+    }
+
     #[test]
     fn test_load_generated_mcap_file() {
         env_logger::init();
+        let imu = make_imu();
 
-        let mcap_data = generate_ros2_mcap();
+        let mcap_data = generate_ros2_mcap(&imu);
         let (tx, rx) = std::sync::mpsc::channel();
         let settings = DataLoaderSettings::recommended("test");
         load_mcap(&mcap_data, &settings, &tx, SelectedLayers::All).unwrap();
         drop(tx);
 
+        // Collect chunks
+        let mut chunks = vec![];
         while let Ok(res) = rx.recv() {
-            match res {
-                LoadedData::Chunk(_, _, chunk) => {
-                    if &format!("{}", chunk.entity_path()) == "/test/points" {
-                        println!("\n\nGenerated MCAP chunk: {:?}", chunk);
-                    }
-                }
-                _ => {} // LoadedData::LogMsg(_, log_msg) => println!("Generated MCAP result: {log_msg:#?}"),
-                        // LoadedData::ArrowMsg(_, _, _) => println!("Generated MCAP arrow msg"),
+            if let LoadedData::Chunk(_, _, chunk) = res {
+                chunks.push(chunk);
             }
         }
+
+        println!("CHUNKS: {chunks:?}");
+
+        // Assert chunk contents
+        assert_component(
+            &chunks,
+            "sensor_msgs.msg.Imu",
+            "orientation",
+            &[
+                imu.orientation.x,
+                imu.orientation.y,
+                imu.orientation.z,
+                imu.orientation.w,
+            ],
+        );
+
+        assert_component(
+            &chunks,
+            "sensor_msgs.msg.Imu",
+            "orientation_covariance",
+            &imu.orientation_covariance,
+        );
+
+        // FOUND A BUG! We don't load angular_velocity.
+        assert_component(
+            &chunks,
+            "sensor_msgs.msg.Imu",
+            "angular_velocity",
+            &[
+                imu.angular_velocity.x,
+                imu.angular_velocity.y,
+                imu.angular_velocity.z,
+            ],
+        );
     }
 }
