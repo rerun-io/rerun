b2f843969bc393d13c2de72002aa65de213e1d1b
Lucas Meurer <lucasmeurer96@gmail.com>
2025-09-12 13:56:01 +0200

    Add arrow `DisplayOptions` and make limits configurable (#11139)
    Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
    Co-authored-by: Jochen Görtler <grtlr@users.noreply.github.com>

--- crates/viewer/re_arrow_ui/src/lib.rs
+++ crates/viewer/re_arrow_ui/src/lib.rs
@@ -6,3 +6,9 @@ mod list_item_ranges;
 mod show_index;
 
 pub use arrow_ui::arrow_ui;
+
+pub fn arrow_syntax_highlighted(
+    data: &dyn arrow::array::Array,
+) -> Result<re_ui::syntax_highlighting::SyntaxHighlightedBuilder, arrow::error::ArrowError> {
+    show_index::ArrayUi::try_new(data, &show_index::DisplayOptions::default())?.highlighted()
+}
