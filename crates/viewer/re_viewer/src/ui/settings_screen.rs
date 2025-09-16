37242158bb30a52bbc0a5c482e5b091a8d417c9a
Antoine Beyeler <antoine@rerun.io>
2025-09-16 18:20:56 +0200

    Update settings ui

--- crates/viewer/re_viewer/src/ui/settings_screen.rs
+++ crates/viewer/re_viewer/src/ui/settings_screen.rs
@@ -2,7 +2,8 @@ use std::str::FromStr as _;
 
 use egui::{NumExt as _, Ui};
 
-use re_log_types::TimestampFormat;
+use re_log_types::{Timestamp, TimestampFormat};
+use re_ui::syntax_highlighting::SyntaxHighlightedBuilder;
 use re_ui::{DesignTokens, UiExt as _};
 use re_viewer_context::AppOptions;
 
@@ -88,80 +89,61 @@ fn settings_screen_ui_impl(ui: &mut egui::Ui, app_options: &mut AppOptions, keep
 
     separator_with_some_space(ui);
 
+    ui.strong("Timestamp format");
+
+    fn timestamp_example_ui(
+        ui: &mut egui::Ui,
+        timestamp: Timestamp,
+        timestamp_format: TimestampFormat,
+    ) {
+        ui.horizontal(|ui| {
+            ui.add_space(ui.spacing().icon_width + ui.spacing().icon_spacing);
+            egui::Frame::new()
+                .fill(ui.visuals().text_edit_bg_color())
+                .corner_radius(2.0)
+                .inner_margin(egui::Margin::symmetric(4, 2))
+                .show(ui, |ui| {
+                    ui.label(
+                        SyntaxHighlightedBuilder::primitive(&timestamp.format(timestamp_format))
+                            .into_widget_text(ui.style()),
+                    );
+                });
+        });
+    }
+
     let timestamp = re_log_types::Timestamp::from(
-        jiff::Timestamp::from_str("2023-02-14T22:47:18+01").expect("valid timestamp"),
+        jiff::Timestamp::from_str("2023-02-14 21:47:18Z").expect("the timestamp is valid"),
     );
 
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.strong("Timestamp format");
-        ui.help_button(|ui| {
-            ui.set_max_width(400.0);
-            ui.markdown_ui(&format!(
-                "\
-This setting controls how timestamps are displayed and parsed in the viewer.
-
-#### UTC
-
-All timestamps are displayed in UTC, with the standard `Z` suffix.
-
-```
-{}
-```
-
-#### Local (show time zone information)
-
-All timestamps are displayed in the local time zone (as provided by your OS).
-
-```
-{}
-```
-
-#### Local (hide time zone information)
-
-All timestamps are displayed in the local time zone, but the time zone information is not displayed.
-
-```
-{}
-```
-
-**Note**: timestamps representations lacking time zone information are ambiguous.
-They may lead to incorrect interpretation by third party systems, e.g. when copy-pasting them.
-
-#### Seconds since Unix epoch
-
-All timestamps are displayed as seconds since the Unix epoch (1970-01-01 00:00:00 UTC).
-
-```
-{}
-```",
-                timestamp.format(TimestampFormat::Utc),
-                timestamp.format(TimestampFormat::LocalTimezone),
-                timestamp.format(TimestampFormat::LocalTimezoneImplicit),
-                timestamp.format(TimestampFormat::UnixEpoch)
-            ));
-        })
-    });
     ui.re_radio_value(
         &mut app_options.timestamp_format,
         TimestampFormat::Utc,
         "UTC",
     );
+    timestamp_example_ui(ui, timestamp, TimestampFormat::Utc);
     ui.re_radio_value(
         &mut app_options.timestamp_format,
         TimestampFormat::LocalTimezone,
-        "Local (show time zone information)",
+        "Local (show time zone)",
     );
+    timestamp_example_ui(ui, timestamp, TimestampFormat::LocalTimezone);
     ui.re_radio_value(
         &mut app_options.timestamp_format,
         TimestampFormat::LocalTimezoneImplicit,
-        "Local (hide time zone information)",
+        "Local (hide time zone)",
     );
+    ui.horizontal(|ui| {
+        ui.add_space(ui.spacing().icon_width + ui.spacing().icon_spacing);
+        ui.label("Note: timestamps without time zone are ambiguous when copied elsewhere.");
+    });
+    timestamp_example_ui(ui, timestamp, TimestampFormat::LocalTimezoneImplicit);
+
     ui.re_radio_value(
         &mut app_options.timestamp_format,
         TimestampFormat::UnixEpoch,
         "Seconds since Unix epoch",
     );
+    timestamp_example_ui(ui, timestamp, TimestampFormat::UnixEpoch);
 
     //
     // Map view
