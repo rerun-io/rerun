ce56f69857428d8bfd436cb2467da733e09ce680
Gábor Gyebnár <korteur@gmail.com>
2025-09-22 12:01:28 +0200

    Runs all formats

--- crates/viewer/re_view_spatial/tests/bgr_images.rs
+++ crates/viewer/re_view_spatial/tests/bgr_images.rs
@@ -1,59 +1,32 @@
 #![expect(clippy::unwrap_used)] // It's a test!
 
-use std::cell::Cell;
-
-use image::GenericImageView;
+use image::GenericImageView as _;
 use itertools::Itertools as _;
 use re_chunk_store::RowId;
-use re_log_types::{NonMinI64, TimeInt, TimePoint};
+use re_log_types::TimePoint;
 use re_test_context::{
     TestContext,
     external::egui_kittest::{OsThreshold, SnapshotOptions},
 };
 use re_test_viewport::TestContextExt as _;
 use re_types::{
-    archetypes::{AssetVideo, VideoFrameReference, VideoStream},
-    components::{self, MediaType, VideoTimestamp},
-    datatypes,
+    datatypes::{self, ColorModel},
+    image::ImageChannelType,
 };
-use re_video::{VideoCodec, VideoDataDescription};
 use re_viewer_context::{ViewClass as _, ViewId};
 use re_viewport_blueprint::ViewBlueprint;
 
-fn workspace_dir() -> std::path::PathBuf {
-    std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .parent()
-        .and_then(|p| p.parent())
-        .and_then(|p| p.parent())
-        .unwrap()
-        .to_path_buf()
+fn convert_pixels_to<T: From<u8> + Copy>(u8s: &[u8]) -> Vec<T> {
+    u8s.iter().map(|u| T::from(*u)).collect()
 }
 
-#[test]
-fn test_bgr_image() {
+fn run_bgr_test<T: ImageChannelType>(image: &[T], size: [u32; 2], color_model: ColorModel) {
     let mut test_context = TestContext::new_with_view_class::<re_view_spatial::SpatialView2D>();
-
-    let test_image = image::load_from_memory(include_bytes!("assets/grinda.jpg")).unwrap();
-    let size = test_image.dimensions();
-    let rgb_u8 = test_image.to_rgb8().into_raw();
-    let bgr_u8 = rgb_u8
-        .chunks(3)
-        .flat_map(|p| [p[2], p[1], p[0]])
-        .collect_vec();
-    let bgra_u8 = rgb_u8
-        .chunks(3)
-        .flat_map(|p| [p[2], p[1], p[0], 255])
-        .collect_vec();
-
     test_context.log_entity("2d_layering/middle_blue", |builder| {
         builder.with_archetype(
             RowId::new(),
             TimePoint::default(),
-            &re_types::archetypes::Image::from_elements(
-                &bgr_u8,
-                size.into(),
-                re_types::datatypes::ColorModel::BGR,
-            ),
+            &re_types::archetypes::Image::from_elements(image, size, color_model),
         )
     });
 
@@ -63,12 +36,51 @@ fn test_bgr_image() {
         ))
     });
 
+    let type_name = std::any::type_name::<T>();
+    let snapshot_name = format!(
+        "bgr_images_{}_{}",
+        color_model.to_string().to_lowercase(),
+        type_name
+    );
+
     run_view_ui_and_save_snapshot(
         &mut test_context,
         view_id,
-        "bgr_image",
+        &snapshot_name,
         egui::vec2(160.0, 120.0),
     );
+}
+
+fn run_all_formats(image: &[u8], size: [u32; 2], color_model: ColorModel) {
+    run_bgr_test(image, size, color_model);
+    run_bgr_test(&convert_pixels_to::<u16>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<u32>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<u64>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<i16>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<i32>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<i64>(image), size, color_model);
+    // run_bgr_test(&convert_pixels_to::<f16>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<f32>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<f64>(image), size, color_model);
+}
+
+#[test]
+fn test_bgr_image() {
+    let test_image = image::load_from_memory(include_bytes!("assets/grinda.jpg")).unwrap();
+    let size = test_image.dimensions().into();
+    let rgb_u8 = test_image.to_rgb8().into_raw();
+
+    let bgr_u8 = rgb_u8
+        .chunks(3)
+        .flat_map(|p| [p[2], p[1], p[0]])
+        .collect_vec();
+    run_all_formats(&bgr_u8, size, ColorModel::BGR);
+
+    let bgra_u8 = rgb_u8
+        .chunks(3)
+        .flat_map(|p| [p[2], p[1], p[0], 255])
+        .collect_vec();
+    run_all_formats(&bgra_u8, size, ColorModel::BGRA);
 }
 
 fn run_view_ui_and_save_snapshot(
