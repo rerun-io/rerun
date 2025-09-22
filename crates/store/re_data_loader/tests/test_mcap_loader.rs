3d17d7f9096e8ffbfa39fb7c1881856f5e775134
Jochen Görtler <grtlr@users.noreply.github.com>
2025-09-22 17:38:02 +0200

    Add support for `enum` in protobuf MCAP messages (#11280)
    ### Related
    
    * Related #11221. Did not end up using `UnionBuilder` in this PR, but
    will be required for `oneof`.
    * [x] Requires #11278.
    * [x] Requires #11283.
    
    ### What
    
    This adds `enum` support to the decoding of protobuf MCAP messages. It
    does not make use of `UnionBuilder` as originally planned, because that
    would be more inefficient then using plain `StringBuilder` (we can't
    share union variants, or Arrow dictionaries across lists in our data
    model).
    
    This also replaces our asset-based protobuf snapshot test with a basic
    in-memory test harness. This will allow iterating on the tests in the
    future more quickly.

--- crates/store/re_data_loader/tests/test_mcap_loader.rs
+++ crates/store/re_data_loader/tests/test_mcap_loader.rs
@@ -5,7 +5,8 @@ mod tests {
     use re_mcap::layers::SelectedLayers;
 
     // Load an MCAP file into a list of chunks.
-    fn load_mcap_chunks(path: &std::path::Path) -> Vec<Chunk> {
+    fn load_mcap_chunks(path: impl AsRef<std::path::Path>) -> Vec<Chunk> {
+        let path = path.as_ref();
         println!("Loading MCAP file: {}", path.display());
         let mcap_data = std::fs::read(path).unwrap();
         let (tx, rx) = std::sync::mpsc::channel();
@@ -25,9 +26,12 @@ mod tests {
             .collect()
     }
 
-    // Compare chunks based on their debug representation.
-    // Chunks are sorted by entity path and row ids are cleared to make comparison stable.
-    fn assert_chunk_snapshot(mut chunks: Vec<Chunk>) {
+    #[test]
+    fn test_mcap_loader_ros2() {
+        let mut chunks = load_mcap_chunks("tests/assets/supported_ros2_messages.mcap");
+
+        // Compare chunks based on their debug representation.
+        // Chunks are sorted by entity path and row ids are cleared to make comparison stable.
         chunks.sort_by_key(|chunk| chunk.entity_path().to_string());
         let clean_chunks: Vec<Chunk> = chunks
             .into_iter()
@@ -37,14 +41,7 @@ mod tests {
                     .zeroed()
             })
             .collect();
-        insta::assert_debug_snapshot!(clean_chunks);
-    }
 
-    #[test]
-    fn test_mcap_loader() {
-        insta::glob!("assets/*.mcap", |path| {
-            let chunks = load_mcap_chunks(path);
-            assert_chunk_snapshot(chunks);
-        });
+        insta::assert_debug_snapshot!("ros2", clean_chunks);
     }
 }
