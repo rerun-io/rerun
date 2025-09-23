36a494ad33ddb5b145de2920eab555f7b683911b
Gábor Gyebnár <korteur@gmail.com>
2025-09-23 13:48:54 +0200

    Removes theme in favor of purplish background

--- crates/viewer/re_view_spatial/tests/bgr_images.rs
+++ crates/viewer/re_view_spatial/tests/bgr_images.rs
@@ -9,7 +9,10 @@ use re_test_context::{
     external::egui_kittest::{OsThreshold, SnapshotOptions},
 };
 use re_test_viewport::TestContextExt as _;
-use re_types::{datatypes::ColorModel, image::ImageChannelType};
+use re_types::{
+    Archetype, blueprint::components::BackgroundKind, datatypes::ColorModel,
+    image::ImageChannelType,
+};
 use re_viewer_context::{ViewClass as _, ViewId};
 use re_viewport_blueprint::ViewBlueprint;
 
@@ -17,12 +20,7 @@ fn convert_pixels_to<T: From<u8> + Copy>(u8s: &[u8]) -> Vec<T> {
     u8s.iter().map(|u| T::from(*u)).collect()
 }
 
-fn run_bgr_test<T: ImageChannelType>(
-    image: &[T],
-    size: [u32; 2],
-    color_model: ColorModel,
-    is_light_theme: bool,
-) {
+fn run_bgr_test<T: ImageChannelType>(image: &[T], size: [u32; 2], color_model: ColorModel) {
     let mut test_context = TestContext::new_with_view_class::<re_view_spatial::SpatialView2D>();
     test_context.log_entity("bgr_image", |builder| {
         builder.with_archetype(
@@ -32,23 +30,28 @@ fn run_bgr_test<T: ImageChannelType>(
         )
     });
 
-    let view_id = test_context.setup_viewport_blueprint(|_ctx, blueprint| {
-        blueprint.add_view_at_root(ViewBlueprint::new_with_root_wildcard(
-            re_view_spatial::SpatialView2D::identifier(),
-        ))
+    // Set up a view with a purple-ish background to catch alpha blending issues
+    let view_id = test_context.setup_viewport_blueprint(|ctx, blueprint| {
+        let view =
+            ViewBlueprint::new_with_root_wildcard(re_view_spatial::SpatialView2D::identifier());
+        let property_path = re_viewport_blueprint::entity_path_for_view_property(
+            view.id,
+            ctx.store_context.blueprint.tree(),
+            re_types::blueprint::archetypes::Background::name(),
+        );
+        ctx.save_blueprint_archetype(
+            property_path.clone(),
+            &re_types::blueprint::archetypes::Background::new(BackgroundKind::SolidColor)
+                .with_color(re_types::components::Color::from_rgb(200, 100, 200)),
+        );
+        blueprint.add_view_at_root(view)
     });
 
     // type_name of half is "half::binary16::f16"
     let pixel_type_name = std::any::type_name::<T>().split("::").last().unwrap();
 
-    let (theme, theme_name) = if is_light_theme {
-        (egui::Theme::Light, "light")
-    } else {
-        (egui::Theme::Dark, "dark")
-    };
-
     let snapshot_name = format!(
-        "bgr_images_{}_{pixel_type_name}_{theme_name}",
+        "bgr_images_{}_{pixel_type_name}",
         color_model.to_string().to_lowercase(),
     );
 
@@ -57,7 +60,6 @@ fn run_bgr_test<T: ImageChannelType>(
         view_id,
         &snapshot_name,
         egui::vec2(160.0, 120.0),
-        theme,
     );
 }
 
@@ -66,7 +68,6 @@ fn run_view_ui_and_save_snapshot(
     view_id: ViewId,
     name: &str,
     size: egui::Vec2,
-    theme: egui::Theme,
 ) {
     let mut harness = test_context
         .setup_kittest_for_rendering()
@@ -74,71 +75,24 @@ fn run_view_ui_and_save_snapshot(
         .build_ui(|ui| {
             test_context.run_with_single_view(ui, view_id);
         });
-    harness.ctx.set_theme(theme);
-
     harness.run();
     harness.snapshot_options(
         name,
-        &SnapshotOptions::new().failed_pixel_count_threshold(OsThreshold::new(2).macos(2)),
+        &SnapshotOptions::new().failed_pixel_count_threshold(OsThreshold::new(2)),
     );
 }
 
-fn run_all_formats(image: &[u8], size: [u32; 2], color_model: ColorModel, is_light_theme: bool) {
-    run_bgr_test(image, size, color_model, is_light_theme);
-    run_bgr_test(
-        &convert_pixels_to::<u16>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
-    run_bgr_test(
-        &convert_pixels_to::<u32>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
-    run_bgr_test(
-        &convert_pixels_to::<u64>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
-    run_bgr_test(
-        &convert_pixels_to::<i16>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
-    run_bgr_test(
-        &convert_pixels_to::<i32>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
-    run_bgr_test(
-        &convert_pixels_to::<i64>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
-    run_bgr_test(
-        &convert_pixels_to::<half::f16>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
-    run_bgr_test(
-        &convert_pixels_to::<f32>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
-    run_bgr_test(
-        &convert_pixels_to::<f64>(image),
-        size,
-        color_model,
-        is_light_theme,
-    );
+fn run_all_formats(image: &[u8], size: [u32; 2], color_model: ColorModel) {
+    run_bgr_test(image, size, color_model);
+    run_bgr_test(&convert_pixels_to::<u16>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<u32>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<u64>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<i16>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<i32>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<i64>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<half::f16>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<f32>(image), size, color_model);
+    run_bgr_test(&convert_pixels_to::<f64>(image), size, color_model);
 }
 
 #[test]
@@ -153,13 +107,11 @@ fn test_bgr_images() {
         .chunks(3)
         .flat_map(|p| [p[2], p[1], p[0]])
         .collect_vec();
-    run_all_formats(&bgr_u8, size, ColorModel::BGR, true);
-    run_all_formats(&bgr_u8, size, ColorModel::BGR, false);
+    run_all_formats(&bgr_u8, size, ColorModel::BGR);
 
     let bgra_u8 = rgb_u8
         .chunks(3)
         .flat_map(|p| [p[2], p[1], p[0], 255])
         .collect_vec();
-    run_all_formats(&bgra_u8, size, ColorModel::BGRA, true);
-    run_all_formats(&bgra_u8, size, ColorModel::BGRA, false);
+    run_all_formats(&bgra_u8, size, ColorModel::BGRA);
 }
