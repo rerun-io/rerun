6de78400177d661ccde4d8dc5a1f68c4d5d7db24
Andreas Reich <r_andreas2@web.de>
2025-09-29 12:12:03 +0200

    Fix web build

--- crates/viewer/re_viewer_context/src/open_url.rs
+++ crates/viewer/re_viewer_context/src/open_url.rs
@@ -692,7 +692,7 @@ fn handle_web_event_listener(egui_ctx: &egui::Context, command_sender: &CommandS
 
             match msg {
                 HttpMessage::LogMsg(msg) => {
-                    if tx.send(msg).is_ok() {
+                    if tx.send(msg.into()).is_ok() {
                         ControlFlow::Continue(())
                     } else {
                         re_log::info_once!(
