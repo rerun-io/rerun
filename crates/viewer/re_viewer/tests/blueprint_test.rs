d3d669f355cb6dfcd78c8ccd925f6a670957ff5c
Gábor Gyebnár <korteur@gmail.com>
2025-10-01 16:51:27 +0200

    Work

--- crates/viewer/re_viewer/tests/blueprint_test.rs
+++ crates/viewer/re_viewer/tests/blueprint_test.rs
@@ -1,93 +1,66 @@
 // #![cfg(feature = "testing")]
 
-use std::time::Duration;
-
-use egui::accesskit::Role;
-use egui_kittest::{SnapshotOptions, kittest::Queryable as _};
+use std::path::Path;
 
 use re_chunk::{RowId, TimePoint};
 use re_test_context::TestContext;
 use re_test_viewport::TestContextExt as _;
-use re_types::{Archetype as _, components::Colormap};
-// use re_viewer::viewer_test_utils;
-use re_viewer_context::{MaybeMutRef, ViewClass, ViewerContext};
+use re_viewer_context::ViewClass as _;
 use re_viewport::ViewportUi;
 use re_viewport_blueprint::ViewBlueprint;
 
-fn x() -> Vec<f32> {
-    (0..100).map(|i| i as f32 * 100.0 / 99.0).collect()
-}
+fn setup_viewport(test_context: &mut TestContext) {
+    test_context.register_view_class::<re_view_dataframe::DataframeView>();
+    test_context.register_view_class::<re_view_bar_chart::BarChartView>();
 
-/// Navigates from welcome to settings screen and snapshots it.
-#[test]
-fn blueprint_test_ponies() {
-    let mut test_context = TestContext::new_with_view_class::<re_view_bar_chart::BarChartView>();
-
-    test_context.log_entity("tensor", |builder| {
+    let timeline_a = re_chunk::Timeline::new_sequence("timeline_a");
+    test_context.log_entity("scalar", |builder| {
         builder.with_archetype(
             RowId::new(),
-            TimePoint::STATIC,
-            &re_types::archetypes::BarChart::new(x()),
+            [(timeline_a, 0)],
+            &re_types::archetypes::Scalars::single(10.0),
         )
     });
 
-    let rbl_path = "temp42.rbl";
+    let vector = (0..16).map(|i| i as f32).collect::<Vec<_>>();
 
-    let bp1 = ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier());
-    let bp2 = ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier());
-    let bp3 = ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier());
-    let bp4 = ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier());
-    let bp5 = ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier());
+    test_context.log_entity("vector", |builder| {
+        builder.with_archetype(
+            RowId::new(),
+            TimePoint::STATIC,
+            &re_types::archetypes::BarChart::new(vector),
+        )
+    });
 
-    let bp1_id = bp1.id;
-    // let bp2_id = bp2.id;
+    let view_1 =
+        ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier());
+    let view_2 =
+        ViewBlueprint::new_with_root_wildcard(re_view_dataframe::DataframeView::identifier());
 
     test_context.setup_viewport_blueprint(|ctx, blueprint| {
-        let color_override = re_types::archetypes::BarChart::default().with_color([0, 155, 0]);
+        // Set the color override for the bar chart view.
+        let color_override = re_types::archetypes::BarChart::default().with_color([255, 200, 50]);
         let override_path = re_viewport_blueprint::ViewContents::override_path_for_entity(
-            bp1.id,
-            &re_chunk::EntityPath::from("tensor"),
+            view_1.id,
+            &re_chunk::EntityPath::from("vector"),
         );
         ctx.save_blueprint_archetype(override_path.clone(), &color_override);
 
-        blueprint.add_views([bp1, bp2].into_iter(), None, None);
+        // Set the timeline for the dataframe view.
+        let query = re_view_dataframe::Query::from_blueprint(ctx, view_2.id);
+        query.save_timeline_name(ctx, &re_chunk::TimelineName::from("timeline_a"));
+
+        blueprint.add_views([view_1, view_2].into_iter(), None, None);
     });
-
-    save_blueprint_to_file(&test_context, rbl_path);
-
-    test_context.setup_viewport_blueprint(|_ctx, blueprint| {
-        blueprint.remove_contents(re_viewer_context::Contents::View(bp1_id));
-        blueprint.add_views([bp3, bp4, bp5].into_iter(), None, None);
-    });
-
-    load_blueprint_from_file(&mut test_context, rbl_path);
-
-    let mut harness = test_context
-        .setup_kittest_for_rendering()
-        .with_size(egui::vec2(600.0, 400.0))
-        .build_ui(|ui| {
-            test_context.run_ui(ui, |ctx, ui| {
-                let viewport_blueprint = re_viewport_blueprint::ViewportBlueprint::from_db(
-                    ctx.blueprint_db(),
-                    &test_context.blueprint_query,
-                );
-                let viewport_ui = ViewportUi::new(viewport_blueprint);
-                viewport_ui.viewport_ui(ui, ctx, &mut test_context.view_states.lock());
-            });
-
-            test_context.handle_system_commands();
-        });
-    harness.run();
-    harness.snapshot("xblueprint_test_ponies");
 }
 
-fn save_blueprint_to_file(test_context: &TestContext, path: &str) {
+fn save_blueprint_to_file(test_context: &TestContext, path: &Path) {
     test_context
         .save_blueprint_to_file(path)
         .expect("Failed to save blueprint to file.");
 }
 
-fn load_blueprint_from_file(test_context: &mut TestContext, path: &str) {
+fn load_blueprint_from_file(test_context: &mut TestContext, path: &Path) {
     let file = std::fs::File::open(path).expect("Failed to open blueprint file.");
     let rbl_store =
         re_entity_db::StoreBundle::from_rrd(file).expect("Failed to load blueprint store");
@@ -120,4 +93,28 @@ fn take_snapshot(test_context: &mut TestContext, snapshot_name: &str) {
         });
     harness.run();
     harness.snapshot(snapshot_name);
+}
+
+#[test]
+fn test_blueprint_change_and_restore() {
+    let mut test_context = TestContext::new();
+    let rbl_file = tempfile::NamedTempFile::new().unwrap();
+    let rbl_path = rbl_file.path();
+
+    setup_viewport(&mut test_context);
+    save_blueprint_to_file(&test_context, rbl_path);
+
+    // Remove the first view and add 3 new ones.
+    test_context.setup_viewport_blueprint(|_ctx, blueprint| {
+        let first_view_id = *blueprint.view_ids().next().unwrap();
+        blueprint.remove_contents(re_viewer_context::Contents::View(first_view_id));
+        blueprint.add_views([
+            ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier()),
+            ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier()),
+            ViewBlueprint::new_with_root_wildcard(re_view_bar_chart::BarChartView::identifier()),
+        ].into_iter(), None, None);
+    });
+
+    load_blueprint_from_file(&mut test_context, rbl_path);
+    take_snapshot(&mut test_context, "blueprint_change_and_restore");
 }
