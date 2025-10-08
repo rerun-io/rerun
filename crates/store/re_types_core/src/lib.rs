6a5f3bdde5d9cde2ec3b9d409d45ba306367d1b2
Andreas Reich <r_andreas2@web.de>
2025-10-08 19:14:06 +0200

    introduce wrapper component to cut down on codegen boilerplate

--- crates/store/re_types_core/src/lib.rs
+++ crates/store/re_types_core/src/lib.rs
@@ -33,6 +33,7 @@ pub mod reflection;
 mod result;
 mod tuid;
 mod view;
+mod wrapper_component;
 
 pub use self::{
     archetype::{Archetype, ArchetypeName, ArchetypeReflectionMarker, ComponentIdentifier},
@@ -54,6 +55,7 @@ pub use self::{
     },
     tuid::tuids_to_arrow,
     view::{View, ViewClassIdentifier},
+    wrapper_component::WrapperComponent,
 };
 
 /// Fundamental [`Archetype`]s that are implemented in `re_types_core` directly for convenience and
