03ae3c2a02785c21e2182d4b7892fe9bb5516255
Tim Saucer <timsaucer@gmail.com>
2025-10-09 07:25:49 -0400

    Pass around tokio runtime handle

--- crates/viewer/re_redap_browser/src/entries.rs
+++ crates/viewer/re_redap_browser/src/entries.rs
@@ -106,8 +106,12 @@ impl Entries {
         origin: re_uri::Origin,
         session_context: Arc<SessionContext>,
     ) -> Self {
-        let entries_fut =
-            fetch_entries_and_register_tables(connection_registry, origin, session_context);
+        let entries_fut = fetch_entries_and_register_tables(
+            connection_registry,
+            origin,
+            session_context,
+            runtime.clone(),
+        );
 
         Self {
             entries: RequestedObject::new_with_repaint(runtime, egui_ctx.clone(), entries_fut),
@@ -136,6 +140,7 @@ async fn fetch_entries_and_register_tables(
     connection_registry: ConnectionRegistryHandle,
     origin: re_uri::Origin,
     session_ctx: Arc<SessionContext>,
+    runtime: AsyncRuntimeHandle,
 ) -> EntryResult<HashMap<EntryId, Entry>> {
     let mut client = connection_registry.client(origin.clone()).await?;
 
@@ -150,7 +155,7 @@ async fn fetch_entries_and_register_tables(
     let origin_ref = &origin;
     let futures_iter = entries
         .into_iter()
-        .filter_map(move |e| fetch_entry_details(client.clone(), origin_ref, e));
+        .filter_map(move |e| fetch_entry_details(client.clone(), origin_ref, e, runtime.clone()));
 
     let mut entries = HashMap::default();
 
@@ -192,6 +197,7 @@ fn fetch_entry_details(
     client: ConnectionClient,
     origin: &re_uri::Origin,
     entry: EntryDetails,
+    runtime: AsyncRuntimeHandle,
 ) -> Option<impl Future<Output = FetchEntryDetailsOutput>> {
     // We could also box the future but then we'd need to use `.boxed()` natively and
     // `.boxed_local()` on wasm. Either passes the `Send` type info transparently.
@@ -208,7 +214,7 @@ fn fetch_entry_details(
                 .map(move |res| (entry, res)),
         ))),
         EntryKind::Table => Some(Left(Right(
-            fetch_table_details(client, entry.id, origin)
+            fetch_table_details(client, entry.id, origin, runtime.clone())
                 .map_ok(|(table, table_provider)| (EntryInner::Table(table), table_provider))
                 .map(move |res| (entry, res)),
         ))),
@@ -252,13 +258,14 @@ async fn fetch_table_details(
     mut client: ConnectionClient,
     id: EntryId,
     origin: &re_uri::Origin,
+    runtime: AsyncRuntimeHandle,
 ) -> EntryResult<(Table, Arc<dyn TableProvider>)> {
     let result = client.read_table_entry(id).await.map(|table_entry| Table {
         table_entry,
         origin: origin.clone(),
     })?;
 
-    let table_provider = TableEntryTableProvider::new(client, id)
+    let table_provider = TableEntryTableProvider::new(client, id, runtime.inner().clone())
         .into_provider()
         .await
         .map_err(|err| ApiError::internal(err, "failed creating table-entry table provider"))?;
