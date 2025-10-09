6dd57167e7474d03bae2745ef39082c041d0e2fc
Andreas Reich <r_andreas2@web.de>
2025-10-09 09:51:06 +0200

    add missing "don't inline" comments

--- crates/store/re_types_core/src/wrapper_component.rs
+++ crates/store/re_types_core/src/wrapper_component.rs
@@ -37,6 +37,7 @@ impl<T: WrapperComponent> Loggable for T {
         <Self as WrapperComponent>::Datatype::arrow_datatype()
     }
 
+    // NOTE: Don't inline this, this gets _huge_.
     fn to_arrow_opt<'a>(
         data: impl IntoIterator<Item = Option<impl Into<::std::borrow::Cow<'a, Self>>>>,
     ) -> SerializationResult<arrow::array::ArrayRef>
@@ -53,6 +54,7 @@ impl<T: WrapperComponent> Loggable for T {
         }))
     }
 
+    // NOTE: Don't inline this, this gets _huge_.
     fn from_arrow_opt(
         arrow_data: &dyn arrow::array::Array,
     ) -> DeserializationResult<Vec<Option<Self>>>
