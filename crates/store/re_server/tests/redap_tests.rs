53294de21644d0888e9848162317c65fb834cbbc
Antoine Beyeler <antoine@rerun.io>
2025-10-10 09:42:26 +0200

    lint

--- crates/store/re_server/tests/redap_tests.rs
+++ crates/store/re_server/tests/redap_tests.rs
@@ -1,5 +1,6 @@
 use re_server::{RerunCloudHandler, RerunCloudHandlerBuilder};
 
+#[expect(clippy:unused_async)] // needed by the macro
 async fn build() -> RerunCloudHandler {
     RerunCloudHandlerBuilder::new().build()
 }
