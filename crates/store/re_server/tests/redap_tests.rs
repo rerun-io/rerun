f12325e01a73151f0c9eb2a8f5d5b1cc175446f8
Antoine Beyeler <antoine@rerun.io>
2025-10-13 10:29:00 +0200

    revert disabling the compaction in test

--- crates/store/re_server/tests/redap_tests.rs
+++ crates/store/re_server/tests/redap_tests.rs
@@ -1,17 +1,7 @@
-use re_chunk_store::ChunkStoreConfig;
 use re_server::{RerunCloudHandler, RerunCloudHandlerBuilder};
 
 #[expect(clippy::unused_async)] // needed by the macro
 async fn build() -> RerunCloudHandler {
-    // SAFETY: it's a test
-    #[expect(unsafe_code)]
-    unsafe {
-        // Mimic the behavior of cloud
-        std::env::set_var(ChunkStoreConfig::ENV_CHUNK_MAX_BYTES, "0");
-        std::env::set_var(ChunkStoreConfig::ENV_CHUNK_MAX_ROWS, "0");
-        std::env::set_var(ChunkStoreConfig::ENV_CHUNK_MAX_ROWS_IF_UNSORTED, "0");
-    }
-
     RerunCloudHandlerBuilder::new().build()
 }
 
