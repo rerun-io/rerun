40a31018c219f0fef7187c544842d509a7d203aa
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-10-14 09:13:43 +0200

    wording
    Co-authored-by: Antoine Beyeler <49431240+abey79@users.noreply.github.com>

--- crates/utils/re_perf_telemetry/src/grpc.rs
+++ crates/utils/re_perf_telemetry/src/grpc.rs
@@ -684,7 +684,7 @@ impl tonic::service::Interceptor for TracingExtractorInterceptor {
 
         // Convert the trace information back into `tracing` and inject it into the current span (if any).
         use tracing_opentelemetry::OpenTelemetrySpanExt as _;
-        tracing::Span::current().set_parent(parent_ctx).ok(); // Errors are expect if telemetry is disabled
+        tracing::Span::current().set_parent(parent_ctx).ok(); // Errors are expected if telemetry is disabled
 
         Ok(req)
     }
