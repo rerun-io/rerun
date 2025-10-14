3cc2d532e707ec0b4ffbdbad9e7073cd3e6c4bab
Isse <git@isse.rs>
2025-10-14 10:57:45 +0200

    Blueprint plot background (#11494)
    ### Related
    
    * Part of RR-2350
    
    ### What
    
    Adds blueprint background options for time series view, and bar view. To
    change background color and enable/disable grid.
    
    <img width="1145" height="890" alt="Screenshot 2025-10-10 at 13 50 46"
    src="https://github.com/user-attachments/assets/90c84b0f-0fbb-496e-a2e2-d0439aff1e8c"
    />

--- crates/store/re_types/src/blueprint/views/time_series_view.rs
+++ crates/store/re_types/src/blueprint/views/time_series_view.rs
@@ -35,6 +35,9 @@ pub struct TimeSeriesView {
     /// Configures the legend of the plot.
     pub plot_legend: crate::blueprint::archetypes::PlotLegend,
 
+    /// Configures the background of the plot.
+    pub background: crate::blueprint::archetypes::PlotBackground,
+
     /// Configures which range on each timeline is shown by this view (unless specified differently per entity).
     ///
     /// If not specified, the default is to show the entire timeline.
@@ -55,6 +58,7 @@ impl ::re_byte_size::SizeBytes for TimeSeriesView {
         self.axis_x.heap_size_bytes()
             + self.axis_y.heap_size_bytes()
             + self.plot_legend.heap_size_bytes()
+            + self.background.heap_size_bytes()
             + self.time_ranges.heap_size_bytes()
     }
 
@@ -63,6 +67,7 @@ impl ::re_byte_size::SizeBytes for TimeSeriesView {
         <crate::blueprint::archetypes::TimeAxis>::is_pod()
             && <crate::blueprint::archetypes::ScalarAxis>::is_pod()
             && <crate::blueprint::archetypes::PlotLegend>::is_pod()
+            && <crate::blueprint::archetypes::PlotBackground>::is_pod()
             && <crate::blueprint::archetypes::VisibleTimeRanges>::is_pod()
     }
 }
