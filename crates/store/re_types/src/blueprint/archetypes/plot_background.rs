rerun-io/rerun

commit 3cc2d532e707ec0b4ffbdbad9e7073cd3e6c4bab

Blueprint plot background (#11494)
### Related

* Part of RR-2350

### What

Adds blueprint background options for time series view, and bar view. To
change background color and enable/disable grid.

<img width="1145" height="890" alt="Screenshot 2025-10-10 at 13 50 46"
src="https://github.com/user-attachments/assets/90c84b0f-0fbb-496e-a2e2-d0439aff1e8c"
/>

crates/store/re_types/src/blueprint/archetypes/plot_background.rs
@@ -4,7 +4,6 @@
 #![allow(unused_braces)]
 #![allow(unused_imports)]
 #![allow(unused_parens)]
-#![allow(clippy::allow_attributes)]
 #![allow(clippy::clone_on_copy)]
 #![allow(clippy::cloned_instead_of_copied)]
 #![allow(clippy::map_flatten)]
@@ -13,7 +12,6 @@
 #![allow(clippy::redundant_closure)]
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::too_many_lines)]
-#![allow(clippy::wildcard_imports)]
 
 use ::re_types_core::SerializationResult;
 use ::re_types_core::try_serialize_field;
@@ -21,75 +19,80 @@ use ::re_types_core::{ComponentBatch as _, SerializedComponentBatch};
 use ::re_types_core::{ComponentDescriptor, ComponentType};
 use ::re_types_core::{DeserializationError, DeserializationResult};
 
-/// **Archetype**: Configuration for the background of a spatial view.
+/// **Archetype**: Configuration of a background in a plot view.
 ///
 /// ⚠️ **This type is _unstable_ and may change significantly in a way that the data won't be backwards compatible.**
 #[derive(Clone, Debug, Default)]
-pub struct Background {
-    /// The type of the background.
-    pub kind: Option<SerializedComponentBatch>,
-
-    /// Color used for the solid background type.
+pub struct PlotBackground {
+    /// Color used for the background.
     pub color: Option<SerializedComponentBatch>,
+
+    /// Should the grid be drawn?
+    pub show_grid: Option<SerializedComponentBatch>,
 }
 
-impl Background {
-    /// Returns the [`ComponentDescriptor`] for [`Self::kind`].
-    ///
-    /// The corresponding component is [`crate::blueprint::components::BackgroundKind`].
-    #[inline]
-    pub fn descriptor_kind() -> ComponentDescriptor {
-        ComponentDescriptor {
-            archetype: Some("rerun.blueprint.archetypes.Background".into()),
-            component: "Background:kind".into(),
-            component_type: Some("rerun.blueprint.components.BackgroundKind".into()),
-        }
-    }
-
+impl PlotBackground {
     /// Returns the [`ComponentDescriptor`] for [`Self::color`].
     ///
     /// The corresponding component is [`crate::components::Color`].
     #[inline]
     pub fn descriptor_color() -> ComponentDescriptor {
         ComponentDescriptor {
-            archetype: Some("rerun.blueprint.archetypes.Background".into()),
-            component: "Background:color".into(),
+            archetype: Some("rerun.blueprint.archetypes.PlotBackground".into()),
+            component: "PlotBackground:color".into(),
             component_type: Some("rerun.components.Color".into()),
+        }
+    }
+
+    /// Returns the [`ComponentDescriptor`] for [`Self::show_grid`].
+    ///
+    /// The corresponding component is [`crate::blueprint::components::Enabled`].
+    #[inline]
+    pub fn descriptor_show_grid() -> ComponentDescriptor {
+        ComponentDescriptor {
+            archetype: Some("rerun.blueprint.archetypes.PlotBackground".into()),
+            component: "PlotBackground:show_grid".into(),
+            component_type: Some("rerun.blueprint.components.Enabled".into()),
         }
     }
 }
 
-static REQUIRED_COMPONENTS: std::sync::LazyLock<[ComponentDescriptor; 1usize]> =
-    std::sync::LazyLock::new(|| [Background::descriptor_kind()]);
+static REQUIRED_COMPONENTS: std::sync::LazyLock<[ComponentDescriptor; 0usize]> =
+    std::sync::LazyLock::new(|| []);
 
 static RECOMMENDED_COMPONENTS: std::sync::LazyLock<[ComponentDescriptor; 0usize]> =
     std::sync::LazyLock::new(|| []);
 
-static OPTIONAL_COMPONENTS: std::sync::LazyLock<[ComponentDescriptor; 1usize]> =
-    std::sync::LazyLock::new(|| [Background::descriptor_color()]);
+static OPTIONAL_COMPONENTS: std::sync::LazyLock<[ComponentDescriptor; 2usize]> =
+    std::sync::LazyLock::new(|| {
+        [
+            PlotBackground::descriptor_color(),
+            PlotBackground::descriptor_show_grid(),
+        ]
+    });
 
 static ALL_COMPONENTS: std::sync::LazyLock<[ComponentDescriptor; 2usize]> =
     std::sync::LazyLock::new(|| {
         [
-            Background::descriptor_kind(),
-            Background::descriptor_color(),
+            PlotBackground::descriptor_color(),
+            PlotBackground::descriptor_show_grid(),
         ]
     });
 
-impl Background {
-    /// The total number of components in the archetype: 1 required, 0 recommended, 1 optional
+impl PlotBackground {
+    /// The total number of components in the archetype: 0 required, 0 recommended, 2 optional
     pub const NUM_COMPONENTS: usize = 2usize;
 }
 
-impl ::re_types_core::Archetype for Background {
+impl ::re_types_core::Archetype for PlotBackground {
     #[inline]
     fn name() -> ::re_types_core::ArchetypeName {
-        "rerun.blueprint.archetypes.Background".into()
+        "rerun.blueprint.archetypes.PlotBackground".into()
     }
 
     #[inline]
     fn display_name() -> &'static str {
-        "Background"
+        "Plot background"
     }
 
     #[inline]
@@ -119,82 +122,84 @@ impl ::re_types_core::Archetype for Background {
         re_tracing::profile_function!();
         use ::re_types_core::{Loggable as _, ResultExt as _};
         let arrays_by_descr: ::nohash_hasher::IntMap<_, _> = arrow_data.into_iter().collect();
-        let kind = arrays_by_descr
-            .get(&Self::descriptor_kind())
-            .map(|array| SerializedComponentBatch::new(array.clone(), Self::descriptor_kind()));
         let color = arrays_by_descr
             .get(&Self::descriptor_color())
             .map(|array| SerializedComponentBatch::new(array.clone(), Self::descriptor_color()));
-        Ok(Self { kind, color })
+        let show_grid = arrays_by_descr
+            .get(&Self::descriptor_show_grid())
+            .map(|array| {
+                SerializedComponentBatch::new(array.clone(), Self::descriptor_show_grid())
+            });
+        Ok(Self { color, show_grid })
     }
 }
 
-impl ::re_types_core::AsComponents for Background {
+impl ::re_types_core::AsComponents for PlotBackground {
     #[inline]
     fn as_serialized_batches(&self) -> Vec<SerializedComponentBatch> {
         use ::re_types_core::Archetype as _;
-        [self.kind.clone(), self.color.clone()]
+        [self.color.clone(), self.show_grid.clone()]
             .into_iter()
             .flatten()
             .collect()
     }
 }
 
-impl ::re_types_core::ArchetypeReflectionMarker for Background {}
+impl ::re_types_core::ArchetypeReflectionMarker for PlotBackground {}
 
-impl Background {
-    /// Create a new `Background`.
+impl PlotBackground {
+    /// Create a new `PlotBackground`.
     #[inline]
-    pub fn new(kind: impl Into<crate::blueprint::components::BackgroundKind>) -> Self {
+    pub fn new() -> Self {
         Self {
-            kind: try_serialize_field(Self::descriptor_kind(), [kind]),
             color: None,
+            show_grid: None,
         }
     }
 
-    /// Update only some specific fields of a `Background`.
+    /// Update only some specific fields of a `PlotBackground`.
     #[inline]
     pub fn update_fields() -> Self {
         Self::default()
     }
 
-    /// Clear all the fields of a `Background`.
+    /// Clear all the fields of a `PlotBackground`.
     #[inline]
     pub fn clear_fields() -> Self {
         use ::re_types_core::Loggable as _;
         Self {
-            kind: Some(SerializedComponentBatch::new(
-                crate::blueprint::components::BackgroundKind::arrow_empty(),
-                Self::descriptor_kind(),
-            )),
             color: Some(SerializedComponentBatch::new(
                 crate::components::Color::arrow_empty(),
                 Self::descriptor_color(),
             )),
+            show_grid: Some(SerializedComponentBatch::new(
+                crate::blueprint::components::Enabled::arrow_empty(),
+                Self::descriptor_show_grid(),
+            )),
         }
     }
 
-    /// The type of the background.
-    #[inline]
-    pub fn with_kind(
-        mut self,
-        kind: impl Into<crate::blueprint::components::BackgroundKind>,
-    ) -> Self {
-        self.kind = try_serialize_field(Self::descriptor_kind(), [kind]);
-        self
-    }
-
-    /// Color used for the solid background type.
+    /// Color used for the background.
     #[inline]
     pub fn with_color(mut self, color: impl Into<crate::components::Color>) -> Self {
         self.color = try_serialize_field(Self::descriptor_color(), [color]);
         self
     }
+
+    /// Should the grid be drawn?
+    #[inline]
+    pub fn with_show_grid(
+        mut self,
+        show_grid: impl Into<crate::blueprint::components::Enabled>,
+    ) -> Self {
+        self.show_grid = try_serialize_field(Self::descriptor_show_grid(), [show_grid]);
+        self
+    }
 }
 
-impl ::re_byte_size::SizeBytes for Background {
+impl ::re_byte_size::SizeBytes for PlotBackground {
     #[inline]
     fn heap_size_bytes(&self) -> u64 {
-        self.kind.heap_size_bytes() + self.color.heap_size_bytes()
+        self.color.heap_size_bytes() + self.show_grid.heap_size_bytes()
     }
 }

crates/store/re_types/definitions/rerun/blueprint/archetypes/background.fbs
@@ -1,7 +1,7 @@
 namespace rerun.blueprint.archetypes;
 
 
-/// Configuration for the background of a view.
+/// Configuration for the background of a spatial view.
 table Background (
   "attr.python.aliases": "datatypes.Rgba32Like, blueprint_components.BackgroundKindLike",
   "attr.rerun.scope": "blueprint"
@@ -16,4 +16,15 @@ table Background (
   /// Color used for the solid background type.
   // TODO(andreas): Can't link to [components.BackgroundKind.SolidColor] since blueprint components aren't part of the doc page yet.
   color: rerun.components.Color ("attr.rerun.component_optional", nullable, order: 2000);
+}
+
+/// Configuration of a background in a plot view.
+table PlotBackground (
+  "attr.rerun.scope": "blueprint"
+) {
+  /// Color used for the background.
+  color: rerun.components.Color ("attr.rerun.component_optional", nullable, order: 1000);
+
+  /// Should the grid be drawn?
+  show_grid: rerun.blueprint.components.Enabled ("attr.rerun.component_optional", nullable, order: 2000);
 }

crates/store/re_types/definitions/rerun/blueprint/views/bar_chart.fbs
@@ -8,4 +8,8 @@ table BarChartView (
 ) {
     /// Configures the legend of the plot.
     plot_legend: rerun.blueprint.archetypes.PlotLegend (order: 2000);
+
+
+    /// Configures the background of the plot.
+    background: rerun.blueprint.archetypes.PlotBackground (order: 3000);
 }

crates/store/re_types/definitions/rerun/blueprint/views/time_series.fbs
@@ -15,6 +15,9 @@ table TimeSeriesView (
     /// Configures the legend of the plot.
     plot_legend: rerun.blueprint.archetypes.PlotLegend (order: 2000);
 
+    /// Configures the background of the plot.
+    background: rerun.blueprint.archetypes.PlotBackground (order: 3000);
+
     /// Configures which range on each timeline is shown by this view (unless specified differently per entity).
     ///
     /// If not specified, the default is to show the entire timeline.

crates/store/re_types/src/blueprint/archetypes/.gitattributes
@@ -17,6 +17,7 @@ map_zoom.rs linguist-generated=true
 mod.rs linguist-generated=true
 near_clip_plane.rs linguist-generated=true
 panel_blueprint.rs linguist-generated=true
+plot_background.rs linguist-generated=true
 plot_legend.rs linguist-generated=true
 scalar_axis.rs linguist-generated=true
 tensor_scalar_mapping.rs linguist-generated=true

crates/store/re_types/src/blueprint/archetypes/mod.rs
@@ -15,6 +15,7 @@ mod map_background;
 mod map_zoom;
 mod near_clip_plane;
 mod panel_blueprint;
+mod plot_background;
 mod plot_legend;
 mod scalar_axis;
 mod tensor_scalar_mapping;
@@ -44,6 +45,7 @@ pub use self::map_background::MapBackground;
 pub use self::map_zoom::MapZoom;
 pub use self::near_clip_plane::NearClipPlane;
 pub use self::panel_blueprint::PanelBlueprint;
+pub use self::plot_background::PlotBackground;
 pub use self::plot_legend::PlotLegend;
 pub use self::scalar_axis::ScalarAxis;
 pub use self::tensor_scalar_mapping::TensorScalarMapping;

crates/store/re_types/src/blueprint/views/bar_chart_view.rs
@@ -28,6 +28,9 @@ use ::re_types_core::{DeserializationError, DeserializationResult};
 pub struct BarChartView {
     /// Configures the legend of the plot.
     pub plot_legend: crate::blueprint::archetypes::PlotLegend,
+
+    /// Configures the background of the plot.
+    pub background: crate::blueprint::archetypes::PlotBackground,
 }
 
 impl ::re_types_core::View for BarChartView {
@@ -37,45 +40,15 @@ impl ::re_types_core::View for BarChartView {
     }
 }
 
-impl<T: Into<crate::blueprint::archetypes::PlotLegend>> From<T> for BarChartView {
-    fn from(v: T) -> Self {
-        Self {
-            plot_legend: v.into(),
-        }
-    }
-}
-
-impl std::borrow::Borrow<crate::blueprint::archetypes::PlotLegend> for BarChartView {
-    #[inline]
-    fn borrow(&self) -> &crate::blueprint::archetypes::PlotLegend {
-        &self.plot_legend
-    }
-}
-
-impl std::ops::Deref for BarChartView {
-    type Target = crate::blueprint::archetypes::PlotLegend;
-
-    #[inline]
-    fn deref(&self) -> &crate::blueprint::archetypes::PlotLegend {
-        &self.plot_legend
-    }
-}
-
-impl std::ops::DerefMut for BarChartView {
-    #[inline]
-    fn deref_mut(&mut self) -> &mut crate::blueprint::archetypes::PlotLegend {
-        &mut self.plot_legend
-    }
-}
-
 impl ::re_byte_size::SizeBytes for BarChartView {
     #[inline]
     fn heap_size_bytes(&self) -> u64 {
-        self.plot_legend.heap_size_bytes()
+        self.plot_legend.heap_size_bytes() + self.background.heap_size_bytes()
     }
 
     #[inline]
     fn is_pod() -> bool {
         <crate::blueprint::archetypes::PlotLegend>::is_pod()
+            && <crate::blueprint::archetypes::PlotBackground>::is_pod()
     }
 }

crates/store/re_types/src/blueprint/views/time_series_view.rs
@@ -35,6 +35,9 @@ pub struct TimeSeriesView {
     /// Configures the legend of the plot.
     pub plot_legend: crate::blueprint::archetypes::PlotLegend,
 
+    /// Configures the background of the plot.
+    pub background: crate::blueprint::archetypes::PlotBackground,
+
     /// Configures which range on each timeline is shown by this view (unless specified differently per entity).
     ///
     /// If not specified, the default is to show the entire timeline.
@@ -55,6 +58,7 @@ impl ::re_byte_size::SizeBytes for TimeSeriesView {
         self.axis_x.heap_size_bytes()
             + self.axis_y.heap_size_bytes()
             + self.plot_legend.heap_size_bytes()
+            + self.background.heap_size_bytes()
             + self.time_ranges.heap_size_bytes()
     }
 
@@ -63,6 +67,7 @@ impl ::re_byte_size::SizeBytes for TimeSeriesView {
         <crate::blueprint::archetypes::TimeAxis>::is_pod()
             && <crate::blueprint::archetypes::ScalarAxis>::is_pod()
             && <crate::blueprint::archetypes::PlotLegend>::is_pod()
+            && <crate::blueprint::archetypes::PlotBackground>::is_pod()
             && <crate::blueprint::archetypes::VisibleTimeRanges>::is_pod()
     }
 }

commit d1cff2030de79464e1796a3a3256d4c18d65c72b

Fix codegen pipeline (#11523)
Fix pipeline by running codegen

- Caused by #11494

crates/store/re_types/src/blueprint/archetypes/plot_background.rs
@@ -4,6 +4,7 @@
 #![allow(unused_braces)]
 #![allow(unused_imports)]
 #![allow(unused_parens)]
+#![allow(clippy::allow_attributes)]
 #![allow(clippy::clone_on_copy)]
 #![allow(clippy::cloned_instead_of_copied)]
 #![allow(clippy::map_flatten)]
@@ -12,6 +13,7 @@
 #![allow(clippy::redundant_closure)]
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::too_many_lines)]
+#![allow(clippy::wildcard_imports)]
 
 use ::re_types_core::SerializationResult;
 use ::re_types_core::try_serialize_field;