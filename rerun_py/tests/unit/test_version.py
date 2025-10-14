af22c2978129eed2eece56f3ca617696cc751d1f
Gijs de Jong <berichtaangijs@gmail.com>
2025-10-14 23:47:34 +0200

    add comment

--- rerun_py/tests/unit/test_version.py
+++ rerun_py/tests/unit/test_version.py
@@ -9,6 +9,7 @@ import tomli
 
 def test_version() -> None:
     cargo_toml_path = Path(__file__).parent.parent.parent.parent / "Cargo.toml"
+    # ensure Cargo.toml file is loaded as UTF-8 (this can fail on Windows otherwise)
     cargo_toml = tomli.loads(cargo_toml_path.read_text(encoding="utf-8"))
     assert rr.__version__ == cargo_toml["workspace"]["package"]["version"]
 
