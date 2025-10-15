4cb5083a93ab1561b9081a72a06dd9fa27bc965b
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-10-15 09:17:36 +0200

    Remove unused import

--- crates/viewer/re_view_spatial/tests/draw_order.rs
+++ crates/viewer/re_view_spatial/tests/draw_order.rs
@@ -1,9 +1,6 @@
 use re_chunk_store::RowId;
 use re_log_types::TimePoint;
-use re_test_context::{
-    TestContext,
-    external::egui_kittest::{OsThreshold, SnapshotOptions},
-};
+use re_test_context::{TestContext, external::egui_kittest::SnapshotOptions};
 use re_test_viewport::TestContextExt as _;
 use re_viewer_context::{ViewClass as _, ViewId};
 use re_viewport_blueprint::ViewBlueprint;
