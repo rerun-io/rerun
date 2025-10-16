d90db284d3132afe57581e0d2184687b25e5d633
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-10-16 17:24:11 +0200

    Update rand

--- crates/utils/re_tuid/benches/bench_tuid.rs
+++ crates/utils/re_tuid/benches/bench_tuid.rs
@@ -11,7 +11,7 @@ fn bench_tuid(c: &mut Criterion) {
     group.bench_function("Tuid::cmp", |b| {
         use rand::prelude::*;
         let mut ids = (0..2_000).map(|_| re_tuid::Tuid::new()).collect::<Vec<_>>();
-        ids.shuffle(&mut rand::thread_rng());
+        ids.shuffle(&mut rand::rng());
         b.iter(|| criterion::black_box(ids[0..1_000].cmp(&ids[1_000..2_000])));
     });
 }
--- tests/rust/plot_dashboard_stress/src/main.rs
+++ tests/rust/plot_dashboard_stress/src/main.rs
@@ -94,8 +94,8 @@ fn run(rec: &rerun::RecordingStream, args: &Args) -> anyhow::Result<()> {
     let expected_total_freq = args.freq * num_series as f64;
 
     use rand::Rng as _;
-    let mut rng = rand::thread_rng();
-    let distr_uniform_pi = rand::distributions::Uniform::new(0f64, std::f64::consts::PI);
+    let mut rng = rand::rng();
+    let distr_uniform_pi = rand::distr::Uniform::new(0f64, std::f64::consts::PI)?;
     let distr_std_normal = rand_distr::StandardNormal;
 
     let mut sim_times: Vec<f64> = (0..args.num_points_per_series as i64)
