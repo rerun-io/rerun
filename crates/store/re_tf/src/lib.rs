d68d6d061810422f35d5b845fe97a2a19401c798
Andreas Reich <r_andreas2@web.de>
2025-10-17 18:25:12 +0200

    split out transform_aspect module

--- crates/store/re_tf/src/lib.rs
+++ crates/store/re_tf/src/lib.rs
@@ -139,6 +139,7 @@
 //!
 
 mod component_type_info;
+mod transform_aspect;
 mod transform_forest;
 mod transform_resolution_cache;
 
--- crates/store/re_tf/src/transform_resolution_cache.rs
+++ crates/store/re_tf/src/transform_resolution_cache.rs
@@ -1,9 +1,4 @@
-#![expect(clippy::iter_over_hash_type)] //  TODO(#6198): enable everywhere
-
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    hash::Hash,
-};
+use std::collections::{BTreeMap, BTreeSet};
 
 use ahash::{HashMap, HashSet};
 use glam::Affine3A;
@@ -14,14 +9,14 @@ use re_chunk_store::{Chunk, LatestAtQuery};
 use re_entity_db::EntityDb;
 use re_log_types::{EntityPath, TimeInt, TimelineName};
 use re_types::{
-    Archetype as _, ArchetypeName, Component as _, ComponentDescriptor, ComponentType,
+    Archetype as _, ArchetypeName, Component as _, ComponentDescriptor,
     archetypes::{self, InstancePoses3D},
     components::{self},
     reflection::ComponentDescriptorExt as _,
 };
 use vec1::smallvec_v1::SmallVec1;
 
-use crate::{TransformFrameIdHash, component_type_info::TransformComponentTypeInfo};
+use crate::{TransformFrameIdHash, transform_aspect::TransformAspect};
 
 /// Resolves all transform relationship defining components to affine transforms for fast lookup.
 ///
@@ -53,56 +48,6 @@ impl Default for TransformResolutionCache {
                 recursive_clears: Default::default(), // Unused for static timeline.
             },
         }
-    }
-}
-
-bitflags::bitflags! {
-    /// Flags for the different kinds of independent transforms that the transform cache handles.
-    #[derive(Debug, Clone, Copy)]
-    pub struct TransformAspect: u8 {
-        /// The entity defines one of more frame relationships, i.e. any non-style component of [`archetypes::Transform3D`].
-        // TODO(RR-2511): Add other components here.
-        const Frame = 1 << 0;
-
-        /// The entity has instance poses, i.e. any non-style component of [`archetypes::InstancePoses3D`].
-        const Pose = 1 << 1;
-
-        /// The entity has a pinhole projection or view coordinates, i.e. either [`components::PinholeProjection`] or [`components::ViewCoordinates`].
-        const PinholeOrViewCoordinates = 1 << 2;
-
-        /// The entity has a clear component.
-        const Clear = 1 << 3;
-    }
-}
-
-impl TransformAspect {
-    /// Converts a component type to a transform aspect.
-    pub fn from_component_type(component_type: ComponentType) -> Self {
-        let component_info = TransformComponentTypeInfo::get();
-
-        if component_info.transform.contains(&component_type) {
-            Self::Frame
-        } else if component_info.pose.contains(&component_type) {
-            Self::Pose
-        } else if component_info.pinhole.contains(&component_type) {
-            Self::PinholeOrViewCoordinates
-        } else if component_type == re_types::components::ClearIsRecursive::name() {
-            Self::Clear
-        } else {
-            Self::empty()
-        }
-    }
-
-    /// Collects the transform aspects a chunk covers.
-    pub fn transform_aspects_of(chunk: &Chunk) -> Self {
-        let mut aspects = Self::empty();
-        for component_type in chunk
-            .component_descriptors()
-            .filter_map(|c| c.component_type)
-        {
-            aspects |= Self::from_component_type(component_type);
-        }
-        aspects
     }
 }
 
