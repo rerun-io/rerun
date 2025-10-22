6e3c99dae6a4198fe9ea7fcdc2999045caede174
Isse <git@isse.rs>
2025-10-22 16:14:46 +0200

    Fixes hiding all tabs in a tab view spamming an undo warning (#11619)

--- crates/viewer/re_viewport_blueprint/src/container.rs
+++ crates/viewer/re_viewport_blueprint/src/container.rs
@@ -204,11 +204,12 @@ impl ContainerBlueprint {
             arch = arch.with_display_name(display_name.clone());
         }
 
-        // TODO(jleibs): The need for this pattern is annoying. Should codegen
-        // a version of this that can take an Option.
-        if let Some(active_tab) = &active_tab {
-            arch = arch.with_active_tab(&active_tab.as_entity_path());
-        }
+        // We want to write an empty array if `active_tab` is none. So can't use `arch.with_active_tab`
+        // here.
+        arch.active_tab = re_types::try_serialize_field::<re_types::blueprint::components::ActiveTab>(
+            re_types::blueprint::archetypes::ContainerBlueprint::descriptor_active_tab(),
+            active_tab.map(|c| c.as_entity_path()).as_ref(),
+        );
 
         if let Some(cols) = grid_columns {
             arch = arch.with_grid_columns(*cols);
@@ -411,11 +412,7 @@ impl ContainerBlueprint {
         let container = match self.container_kind {
             egui_tiles::ContainerKind::Tabs => {
                 let mut tabs = egui_tiles::Tabs::new(children);
-                tabs.active = self
-                    .active_tab
-                    .as_ref()
-                    .map(|id| id.as_tile_id())
-                    .or_else(|| tabs.children.first().copied());
+                tabs.active = self.active_tab.as_ref().map(|id| id.as_tile_id());
                 egui_tiles::Container::Tabs(tabs)
             }
             egui_tiles::ContainerKind::Horizontal | egui_tiles::ContainerKind::Vertical => {
