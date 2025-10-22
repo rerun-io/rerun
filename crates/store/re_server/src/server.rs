bef9ef1738099e5232e9f64c41e89a5b1cdc66ca
Gijs de Jong <berichtaangijs@gmail.com>
2025-10-22 15:04:39 +0200

    CorsLayer::permissive()

--- crates/store/re_server/src/server.rs
+++ crates/store/re_server/src/server.rs
@@ -129,7 +129,7 @@ impl Server {
                     let is_client = false;
                     re_protos::headers::new_rerun_headers_layer(name, version, is_client)
                 })
-                .layer(tower_http::cors::CorsLayer::permissive().allow_methods([Method::OPTIONS])) // Allow CORS preflight requests
+                .layer(tower_http::cors::CorsLayer::permissive()) // Allow CORS for all origins (to support web clients)
                 .layer(tonic_web::GrpcWebLayer::new()) // Support `grpc-web` clients
                 .into_inner();
 
