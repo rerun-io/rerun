801f0762b5732a0971f6a5a950d5bd44e804c604
jprochazk <pjanv42@gmail.com>
2025-10-27 16:19:52 +0100

    asdf

--- scripts/upload_image.py
+++ scripts/upload_image.py
@@ -127,7 +127,7 @@ class Uploader:
         gcs = storage.Client("rerun-open")
         self.bucket = gcs.bucket("rerun-static-img")
 
-    def _check_aspect_ratio(self, image: Path | Image) -> None:
+    def _check_image_aspect_ratio(self, image: Path | Image) -> None:
         if isinstance(image, Path):
             image = PIL.Image.open(image)
 
@@ -147,14 +147,15 @@ class Uploader:
 
         """
 
-        self._check_aspect_ratio(path)
-
-        image_data = path.read_bytes()
-        digest = data_hash(image_data)
+        
+        data = path.read_bytes()
+        digest = data_hash(data)
         object_name = f"{digest}_{path.name}"
         content_type, content_encoding = mimetypes.guess_type(path)
+        if content_type and content_type.startswith("image/"):
+            self._check_image_aspect_ratio(path)
 
-        self.upload_data(image_data, object_name, content_type, content_encoding)
+        self.upload_data(data, object_name, content_type, content_encoding)
 
         return object_name
 
@@ -176,7 +177,7 @@ class Uploader:
 
         """
         image = PIL.Image.open(image_path)
-        self._check_aspect_ratio(image)
+        self._check_image_aspect_ratio(image)
 
         content_type, _ = mimetypes.guess_type(image_path)
 
@@ -207,7 +208,7 @@ class Uploader:
         clipboard = image_from_clipboard()
         if isinstance(clipboard, PIL.Image.Image):
             image = clipboard
-            self._check_aspect_ratio(image)
+            self._check_image_aspect_ratio(image)
 
             return self.upload_stack(
                 image,
