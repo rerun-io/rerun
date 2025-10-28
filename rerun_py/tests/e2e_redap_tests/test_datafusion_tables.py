16e0b85c4097fa5d9f8b9ea28b2713d01d7787ee
Tim Saucer <timsaucer@gmail.com>
2025-10-28 06:49:08 -0400

    Add unit test to verify we have rerun metadata as expected

--- rerun_py/tests/e2e_redap_tests/test_datafusion_tables.py
+++ rerun_py/tests/e2e_redap_tests/test_datafusion_tables.py
@@ -284,3 +284,30 @@ def test_datafusion_create_table(server_instance: ServerInstance) -> None:
         returned_schema = df.schema().remove_metadata()
 
         assert returned_schema == original_schema
+
+
+def test_datafusion_create_table_from_dataset(server_instance: ServerInstance) -> None:
+    table_name = "dataset_to_table"
+
+    df = server_instance.dataset.dataframe_query_view(index="time_1", contents="/**").df()
+    original_schema = df.schema()
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_path = pathlib.Path(temp_dir).as_uri()
+
+        table_entry = server_instance.client.create_table(table_name, temp_path, original_schema)
+        df = table_entry.df()
+
+        # Due to https://github.com/lancedb/lance/issues/2304 we cannot
+        # directly compare the returned schema. Verify we at least
+        # get back the same columns and metadata
+
+        returned_schema = df.schema()
+        for field in returned_schema:
+            assert original_schema.field(field.name) is not None
+        for field in original_schema:
+            assert returned_schema.field(field.name) is not None
+
+        for returned_field in returned_schema:
+            original_field = original_schema.field(returned_field.name)
+            assert returned_field.metadata == original_field.metadata
