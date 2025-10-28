8625a3923f4cd2fe07f879e1dd99327d9d4d8670
Antoine Beyeler <antoine@rerun.io>
2025-10-28 18:54:35 +0100

    Better tests

---
+++ rerun_py/tests/unit/e2e_redap_tests/test_partition_id.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+
+from .conftest import ServerInstance, DATASET_FILEPATH
+
+
+def test_partition_ids(server_instance: ServerInstance, snapshot) -> None:
+    """Test that we can successfully collect information about partitions"""
+    client = server_instance.client
+
+    ds = client.create_dataset("test_dataset")
+    tasks = ds.register_prefix(f"file://{DATASET_FILEPATH.absolute()}")
+    tasks.wait(timeout_secs=50)
+
+    assert (
+        ds.partition_table().df().drop("rerun_storage_urls", "rerun_last_updated_at").sort("rerun_partition_id")
+        == snapshot
+    )
+    assert sorted(ds.partition_ids()) == snapshot
