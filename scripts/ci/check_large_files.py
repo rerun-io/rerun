0d51427d42693607dfef19756355a4c630282970
Andreas Reich <andreas@rerun.io>
2025-10-30 19:10:39 +0100

    More codegen & formatting

--- scripts/ci/check_large_files.py
+++ scripts/ci/check_large_files.py
@@ -13,7 +13,7 @@ FILES_ALLOWED_TO_BE_LARGE = {
     "crates/store/re_dataframe/src/query.rs",
     "crates/store/re_protos/proto/schema_snapshot.yaml",
     "crates/store/re_protos/src/v1alpha1/rerun.cloud.v1alpha1.rs",
-    "crates/store/re_tf/src/transform_resolution_cache.rs", # TODO(andreas): Should move tests out to standalone files.
+    "crates/store/re_tf/src/transform_resolution_cache.rs",  # TODO(andreas): Should move tests out to standalone files.
     "crates/store/re_types/src/datatypes/tensor_buffer.rs",
     "crates/store/re_types/src/reflection/mod.rs",
     "crates/top/re_sdk/src/recording_stream.rs",
