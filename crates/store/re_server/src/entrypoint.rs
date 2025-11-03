2b0228cb2b6eb1aea49d6b283a7c6e29b10e3896
Sylvain Wallez <sylvain@bluxte.net>
2025-11-03 19:51:10 +0100

    Remove "run" function

--- crates/store/re_server/src/entrypoint.rs
+++ crates/store/re_server/src/entrypoint.rs
@@ -60,11 +60,6 @@ impl FromStr for NamedPath {
 }
 
 impl Args {
-    pub fn run(self) -> anyhow::Result<()> {
-        let rt = tokio::runtime::Runtime::new()?;
-        rt.block_on(self.run_async())
-    }
-
     pub async fn create_server_handle(self) -> anyhow::Result<ServerHandle> {
         let rerun_cloud_server = {
             use re_protos::cloud::v1alpha1::rerun_cloud_service_server::RerunCloudServiceServer;
