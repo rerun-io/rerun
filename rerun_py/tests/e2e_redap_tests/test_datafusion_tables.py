14ae3b730eea568dacad06a7dea0eeb3fc629a8b
Tim Saucer <timsaucer@gmail.com>
2025-11-05 09:28:21 -0500

    Update unit test for storage url of table

--- rerun_py/tests/e2e_redap_tests/test_datafusion_tables.py
+++ rerun_py/tests/e2e_redap_tests/test_datafusion_tables.py
@@ -14,10 +14,10 @@ def test_urls(server_instance: ServerInstance) -> None:
     """Tests the url property on the catalog and dataset."""
 
     catalog = server_instance.dataset.catalog
-    assert re.match("^rerun\\+http://localhost:[0-9]+$", catalog.url)
+    assert re.match("^rerun\\+http://(localhost|127.0.0.1):[0-9]+$", catalog.url)
 
-    # Dataset also has a manifest_url looking like "memory:///[0-9a-zA-Z]+"
-    assert re.match("^rerun\\+http://localhost:[0-9]+/entry/[0-9a-zA-Z]+$", server_instance.dataset.url)
+    table = server_instance.client.get_table_entry(name="simple_datatypes")
+    assert re.match("file:///[-_./0-9a-zA-Z]+/simple_datatypes/", table.url)
 
 
 def test_df_count(server_instance: ServerInstance) -> None:
