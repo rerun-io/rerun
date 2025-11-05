2479e83def026a90b09fe7e2beac9942e03f9cc8
Tim Saucer <timsaucer@gmail.com>
2025-11-05 10:01:56 -0500

    Update regex

--- rerun_py/tests/e2e_redap_tests/test_datafusion_tables.py
+++ rerun_py/tests/e2e_redap_tests/test_datafusion_tables.py
@@ -17,7 +17,7 @@ def test_urls(server_instance: ServerInstance) -> None:
     assert re.match("^rerun\\+http://(localhost|127.0.0.1):[0-9]+$", catalog.url)
 
     table = server_instance.client.get_table_entry(name="simple_datatypes")
-    assert re.match("file:///[-_./0-9a-zA-Z]+/simple_datatypes/", table.url)
+    assert re.match("^file:///[-_./0-9a-zA-Z]+/simple_datatypes/$", table.url)
 
 
 def test_df_count(server_instance: ServerInstance) -> None:
