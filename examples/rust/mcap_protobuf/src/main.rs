b7bfffd085ec6185d343579069a0caaeca6f4676
Michael Grupp <michael@rerun.io>
2025-11-10 13:14:56 +0100

    finishing touches ...

--- examples/rust/mcap_protobuf/src/main.rs
+++ examples/rust/mcap_protobuf/src/main.rs
@@ -1,14 +1,23 @@
+use arrow::array::{Float32Array, Float64Array, ListArray};
+
+use re_log_types::TimeType;
 use rerun::{
     EncodedImage, InstancePoses3D, Points3D, VideoStream,
     dataframe::EntityPathFilter,
     external::re_log,
-    lenses::{LensBuilder, LensesSink, Op},
+    lenses::{Error, LensBuilder, LensesSink, Op},
     sink::GrpcSink,
 };
 
-use re_log_types::TimeType;
-
-mod conversions;
+// `re_arrow_combinators` provides the building blocks from which we compose the conversions.
+use re_arrow_combinators::{
+    Transform as _,
+    cast::PrimitiveCast,
+    map::MapFixedSizeList,
+    map::MapList,
+    reshape::StructToFixedList,
+    semantic::{BinaryToListUInt8, StringToVideoCodecUInt32, TimeSpecToNanos},
+};
 
 #[derive(Debug, clap::Parser)]
 #[clap(author, version, about)]
@@ -18,6 +27,32 @@ struct Args {
 
     /// The path to the MCAP file.
     filepath: std::path::PathBuf,
+}
+
+/// Converts a list of binary arrays to a list of uint8 arrays.
+pub fn list_binary_to_list_uint8(input: &ListArray) -> Result<ListArray, Error> {
+    Ok(MapList::new(BinaryToListUInt8::<i32>::new()).transform(input)?)
+}
+
+/// Converts a list of structs with `x`, `y`, `z` fields to a list of fixed-size lists with 3 f32 values.
+pub fn list_xyz_struct_to_list_fixed(list_array: &ListArray) -> Result<ListArray, Error> {
+    // Arrow transformations can work on any Arrow-level.
+    let pipeline = MapList::new(StructToFixedList::new(["x", "y", "z"]).then(
+        MapFixedSizeList::new(PrimitiveCast::<Float64Array, Float32Array>::new()),
+    ));
+    Ok(pipeline.transform(list_array)?)
+}
+
+/// Converts a list of video codec strings to Rerun `VideoCodec` values (as u32).
+pub fn list_string_to_list_codec_uint32(list_array: &ListArray) -> Result<ListArray, Error> {
+    let pipeline = MapList::new(StringToVideoCodecUInt32::default());
+    Ok(pipeline.transform(list_array)?)
+}
+
+/// Converts a list of structs with i64 `seconds` and i32 `nanos` fields to a list of timestamps in nanoseconds (i64).
+pub fn list_timespec_to_list_nanos(list_array: &ListArray) -> Result<ListArray, Error> {
+    let pipeline = MapList::new(TimeSpecToNanos::default());
+    Ok(pipeline.transform(list_array)?)
 }
 
 // TODO(grtlr): This example is still missing `tf`-style transforms.
@@ -45,7 +80,7 @@ fn main() -> anyhow::Result<()> {
                     Op::access_field("poses"),
                     Op::flatten(),
                     Op::access_field("position"),
-                    Op::func(conversions::list_xyz_struct_to_list_fixed),
+                    Op::func(list_xyz_struct_to_list_fixed),
                 ],
             )
             .add_static_component_column(
@@ -63,7 +98,7 @@ fn main() -> anyhow::Result<()> {
                     // Lens operations always work on component-column level.
                     Op::access_field("pose"),
                     Op::access_field("position"),
-                    Op::func(conversions::list_xyz_struct_to_list_fixed),
+                    Op::func(list_xyz_struct_to_list_fixed),
                 ],
             )
             .add_static_component_column(
@@ -79,7 +114,7 @@ fn main() -> anyhow::Result<()> {
                 EncodedImage::descriptor_blob(),
                 [
                     Op::access_field("data"),
-                    Op::func(conversions::list_binary_to_list_uint8),
+                    Op::func(list_binary_to_list_uint8),
                 ],
             )
             .build();
@@ -92,21 +127,21 @@ fn main() -> anyhow::Result<()> {
                 TimeType::TimestampNs,
                 [
                     Op::access_field("timestamp"),
-                    Op::func(conversions::list_timespec_to_list_nanos),
+                    Op::func(list_timespec_to_list_nanos),
                 ],
             )
             .add_component_column(
                 VideoStream::descriptor_codec(),
                 [
                     Op::access_field("format"),
-                    Op::func(conversions::list_string_to_list_codec_uint32),
+                    Op::func(list_string_to_list_codec_uint32),
                 ],
             )
             .add_component_column(
                 VideoStream::descriptor_sample(),
                 [
                     Op::access_field("data"),
-                    Op::func(conversions::list_binary_to_list_uint8),
+                    Op::func(list_binary_to_list_uint8),
                 ],
             )
             .build();
@@ -117,7 +152,7 @@ fn main() -> anyhow::Result<()> {
         .with_lens(instance_poses_lens)
         .with_lens(video_lens);
 
-    let (rec, _serve_guard) = args.rerun.init("rerun_example_foxglove_lenses")?;
+    let (rec, _serve_guard) = args.rerun.init("rerun_example_mcap_protobuf")?;
     rec.set_sink(Box::new(lenses_sink));
     rec.log_file_from_path(args.filepath, None, false)?;
 
