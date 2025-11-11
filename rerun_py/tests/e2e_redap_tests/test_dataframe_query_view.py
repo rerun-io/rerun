e87fe84ca8f729e0f0e908d00d020e7aa34ac110
Antoine Beyeler <antoine@rerun.io>
2025-11-11 11:03:53 +0100

    Make the e2e redap test more idiomatic

--- rerun_py/tests/e2e_redap_tests/test_dataframe_query_view.py
+++ rerun_py/tests/e2e_redap_tests/test_dataframe_query_view.py
@@ -5,18 +5,14 @@ from typing import TYPE_CHECKING
 import pytest
 
 if TYPE_CHECKING:
-    from .conftest import ServerInstance
-
+    from rerun.catalog import CatalogClient
 
 # TODO(ab): quite obviously, there needs to be many more tests here.
 
 
 @pytest.mark.parametrize("index", [None, "does_not_exist"])
-def test_dataframe_query_empty_dataset(index: str | None, server_instance: ServerInstance) -> None:
-    client = server_instance.client
-
-    ds = client.create_dataset("empty_dataset")
-
+def test_dataframe_query_empty_dataset(index: str | None, catalog_client: CatalogClient) -> None:
+    ds = catalog_client.create_dataset("empty_dataset")
     df = ds.dataframe_query_view(index=index, contents="/**").df()
 
     assert df.count() == 0
--- rerun_py/tests/e2e_redap_tests/test_table_write.py
+++ rerun_py/tests/e2e_redap_tests/test_table_write.py
@@ -7,12 +7,12 @@ from datafusion import DataFrameWriteOptions, InsertOp, SessionContext, col
 from rerun.catalog import TableInsertMode
 
 if TYPE_CHECKING:
-    from .conftest import ServerInstance
+    from .conftest import PrefilledCatalog
 
 
-def test_datafusion_write_table(server_instance: ServerInstance) -> None:
+def test_datafusion_write_table(prefilled_catalog: PrefilledCatalog) -> None:
     table_name = "simple_datatypes"
-    ctx: SessionContext = server_instance.client.ctx
+    ctx: SessionContext = prefilled_catalog.client.ctx
 
     df_prior = ctx.table(table_name)
     prior_count = df_prior.count()
@@ -29,9 +29,9 @@ def test_datafusion_write_table(server_instance: ServerInstance) -> None:
     assert ctx.table(table_name).count() == smaller_count
 
 
-def test_client_write_table(server_instance: ServerInstance) -> None:
+def test_client_write_table(prefilled_catalog: PrefilledCatalog) -> None:
     table_name = "simple_datatypes"
-    ctx: SessionContext = server_instance.client.ctx
+    ctx: SessionContext = prefilled_catalog.client.ctx
 
     df_prior = ctx.table(table_name)
     original_count = df_prior.count()
@@ -52,41 +52,41 @@ def test_client_write_table(server_instance: ServerInstance) -> None:
 
     # Test with a record batch reader
     reader = pa.RecordBatchReader.from_batches(schema, [batch1, batch2, batch3])
-    server_instance.client.write_table(table_name, reader, TableInsertMode.APPEND)
+    prefilled_catalog.client.write_table(table_name, reader, TableInsertMode.APPEND)
     final_count = ctx.table(table_name).count()
     assert final_count == original_count + 9
 
     # Test with a list of list of record batches, like a collect() will give you
-    server_instance.client.write_table(table_name, [[batch1, batch2], [batch3]], TableInsertMode.APPEND)
+    prefilled_catalog.client.write_table(table_name, [[batch1, batch2], [batch3]], TableInsertMode.APPEND)
     final_count = ctx.table(table_name).count()
     assert final_count == original_count + 18
 
     # Test with a list of record batches
-    server_instance.client.write_table(table_name, [batch1, batch2, batch3], TableInsertMode.APPEND)
+    prefilled_catalog.client.write_table(table_name, [batch1, batch2, batch3], TableInsertMode.APPEND)
     final_count = ctx.table(table_name).count()
     assert final_count == original_count + 27
 
     # Test with a single record batch
-    server_instance.client.write_table(table_name, batch1, TableInsertMode.APPEND)
+    prefilled_catalog.client.write_table(table_name, batch1, TableInsertMode.APPEND)
     final_count = ctx.table(table_name).count()
     assert final_count == original_count + 30
 
     # Test overwrite method
-    server_instance.client.write_table(table_name, batch1, TableInsertMode.OVERWRITE)
+    prefilled_catalog.client.write_table(table_name, batch1, TableInsertMode.OVERWRITE)
     final_count = ctx.table(table_name).count()
     assert final_count == 3
 
 
-def test_client_append_to_table(server_instance: ServerInstance) -> None:
+def test_client_append_to_table(prefilled_catalog: PrefilledCatalog) -> None:
     table_name = "simple_datatypes"
-    ctx: SessionContext = server_instance.client.ctx
+    ctx: SessionContext = prefilled_catalog.client.ctx
 
     original_rows = ctx.table(table_name).count()
 
-    server_instance.client.append_to_table(table_name, id=3, bool_col=True, double_col=2.0)
+    prefilled_catalog.client.append_to_table(table_name, id=3, bool_col=True, double_col=2.0)
     assert ctx.table(table_name).count() == original_rows + 1
 
-    server_instance.client.append_to_table(
+    prefilled_catalog.client.append_to_table(
         table_name, id=[3, 4, 5], bool_col=[False, True, None], double_col=[2.0, None, 1.0]
     )
     assert ctx.table(table_name).count() == original_rows + 4
