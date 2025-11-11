a0098474c7879c87462c434d6784d7c67a636e02
Andreas Reich <r_andreas2@web.de>
2025-11-11 15:24:59 +0100

    Rename `SpaceCamera` and make its pinhole non-optional

--- crates/viewer/re_view_spatial/src/pinhole_wrapper.rs
+++ crates/viewer/re_view_spatial/src/pinhole_wrapper.rs
@@ -7,9 +7,9 @@ use re_types::components::ViewCoordinates;
 
 use crate::Pinhole;
 
-/// A logged camera that connects spaces.
+/// A logged pinhole camera with some extra information.
 #[derive(Clone, PartialEq)]
-pub struct SpaceCamera3D {
+pub struct PinholeWrapper {
     /// Path to the entity which has the projection (pinhole, ortho or otherwise) transforms.
     ///
     /// We expect the camera transform to apply to this instance and every path below it.
@@ -24,13 +24,13 @@ pub struct SpaceCamera3D {
     // -------------------------
     // Optional projection-related things:
     /// The projection transform of a child-entity.
-    pub pinhole: Option<Pinhole>,
+    pub pinhole: Pinhole,
 
     /// Distance of a picture plane from the camera.
     pub picture_plane_distance: f32,
 }
 
-impl SpaceCamera3D {
+impl PinholeWrapper {
     /// Where in scene-space is the camera origin?
     pub fn position(&self) -> Vec3 {
         self.world_from_camera.translation()
@@ -56,8 +56,7 @@ impl SpaceCamera3D {
     }
 
     /// Returns x, y, and depth in image/pixel coordinates.
-    pub fn project_onto_2d(&self, point_in_world: Vec3) -> Option<Vec3> {
-        let pinhole = self.pinhole.as_ref()?;
+    pub fn project_onto_2d(&self, point_in_world: Vec3) -> Vec3 {
         let point_in_cam = self.cam_from_world().transform_point3(point_in_world);
 
         // The pinhole view-coordinates are important here because they define how the image plane is aligned
@@ -73,7 +72,6 @@ impl SpaceCamera3D {
         let point_in_image_unprojected =
             image_view_coordinates().from_other(&self.pinhole_view_coordinates) * point_in_cam;
 
-        let point_in_image = pinhole.project(point_in_image_unprojected);
-        Some(point_in_image)
+        self.pinhole.project(point_in_image_unprojected)
     }
 }
