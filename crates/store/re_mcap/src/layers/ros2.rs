c1afe5e6cd1dcd2d123842b565d172e55648f460
Michael Grupp <michael@rerun.io>
2025-11-12 18:04:44 +0100

    WIP re_mcap layer for PoseStamped
    Visualizes an arrow like RViz, but has no handling of frame ID.
    
    Only experimental playground so far...
    
    Relates to: https://linear.app/rerun/issue/RR-2852

--- crates/store/re_mcap/src/layers/ros2.rs
+++ crates/store/re_mcap/src/layers/ros2.rs
@@ -1,8 +1,7 @@
 use std::collections::BTreeMap;
 
 use super::MessageLayer;
-use crate::parsers::MessageParser;
-use crate::parsers::ros2msg::Ros2MessageParser;
+use crate::parsers::ros2msg::geometry_msgs::PoseStampedMessageParser;
 use crate::parsers::ros2msg::rcl_interfaces::LogMessageParser;
 use crate::parsers::ros2msg::sensor_msgs::{
     BatteryStateMessageParser, CameraInfoMessageParser, CompressedImageMessageParser,
@@ -12,6 +11,8 @@ use crate::parsers::ros2msg::sensor_msgs::{
     TemperatureMessageParser,
 };
 use crate::parsers::ros2msg::std_msgs::StringMessageParser;
+use crate::parsers::ros2msg::Ros2MessageParser;
+use crate::parsers::MessageParser;
 
 type ParserFactory = fn(usize) -> Box<dyn MessageParser>;
 
@@ -32,6 +33,8 @@ impl McapRos2Layer {
     /// Creates a new [`McapRos2Layer`] with all supported message types pre-registered
     pub fn new() -> Self {
         Self::empty()
+            // geometry_msgs
+            .register_parser::<PoseStampedMessageParser>("geometry_msgs/msg/PoseStamped")
             // rcl_interfaces
             .register_parser::<LogMessageParser>("rcl_interfaces/msg/Log")
             // sensor_msgs
