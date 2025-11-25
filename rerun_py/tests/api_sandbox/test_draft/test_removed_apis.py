d28db7abe735471069030b30172dc088fe47f1ce
Antoine Beyeler <antoine@rerun.io>
2025-11-25 09:33:06 +0100

    add a test to ensure removed apis are indeed removed

---
+++ rerun_py/tests/api_sandbox/test_draft/test_removed_apis.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import rerun_draft as rr
+
+
+def test_removed_apis() -> None:
+    """Ensure that removed APIs are indeed removed."""
+    with rr.server.Server() as server:
+        client = server.client()
+
+        assert "all_entries" not in dir(client)
+        assert "dataset_entries" not in dir(client)
+        assert "table_entries" not in dir(client)
+
+        assert "get_dataset_entry" not in dir(client)
+
+        assert "write_table" not in dir(client)
+        assert "append_to_table" not in dir(client)
+
+        ds = client.create_dataset("my_dataset")
+
+        assert "update" not in dir(ds)  # now: `set_name`
