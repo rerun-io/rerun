rerun-io/rerun

commit 34400e3c236bbd59e3c8c950d9e440bc589ae6de

Show progress bar when loading recordings from a server (#12012)

crates/store/re_redap_client/src/grpc.rs
@@ -1,12 +1,14 @@
 use std::sync::Arc;
 
+use arrow::{array::RecordBatch, error::ArrowError};
+use itertools::Itertools as _;
+
 use re_auth::client::AuthDecorator;
 use re_chunk::Chunk;
 use re_log_types::{
     AbsoluteTimeRange, BlueprintActivationCommand, DataSourceMessage, DataSourceUiCommand, EntryId,
     LogMsg, SetStoreInfo, StoreId, StoreInfo, StoreKind, StoreSource,
 };
-use re_protos::cloud::v1alpha1::ext::{Query, QueryLatestAt, QueryRange};
 use re_protos::cloud::v1alpha1::rerun_cloud_service_client::RerunCloudServiceClient;
 use re_protos::common::v1alpha1::ext::PartitionId;
 use re_uri::{Origin, TimeSelection};
@@ -397,6 +399,8 @@ async fn stream_partition_from_server(
 ) -> Result<(), ApiError> {
     let store_id = store_info.store_id.clone();
 
+    re_log::debug!("Streaming {store_id:?}…");
+
     if tx
         .send(
             LogMsg::SetStoreInfo(SetStoreInfo {
@@ -450,100 +454,117 @@ async fn stream_partition_from_server(
         }
     }
 
-    // TODO(RR-2976): Do not, under any circumstances, try to chain the static and temporal streams
+    // TODO(RR-2976): Do not, under any circumstances, try to chain gRPC streams
     // together. Interlaced streams are a giant footgun that will invariably lead to the exhaustion
     // of client's HTTP2 connection window, and ultimately to a complete stall of the entire system.
     // See the attached issues for more information.
 
-    let forward_chunk = |chunk: Chunk| {
-        if tx
-            .send(
-                LogMsg::ArrowMsg(
-                    store_id.clone(),
-                    chunk.to_arrow_msg().map_err(|err| {
-                        ApiError::serialization(
-                            err,
-                            "failed to parse chunk in /FetchChunks response stream",
-                        )
-                    })?,
-                )
-                .into(),
-            )
-            .is_err()
-        {
-            re_log::debug!("Receiver disconnected");
-            return Ok::<_, ApiError>(false);
-        }
-
-        if let Some(on_msg) = &on_msg {
-            on_msg();
-        }
-
-        Ok(true)
-    };
-
-    let static_chunk_stream = client
-        .fetch_partition_chunks(PartitionQueryParams {
+    // Retrieve the chunk IDs we're interested in (all of them):
+    let chunk_index_messages = client
+        .query_dataset_chunk_index(PartitionQueryParams {
             dataset_id,
             partition_id: partition_id.clone(),
             include_static_data: true,
-            include_temporal_data: false,
+            include_temporal_data: true,
             query: None,
         })
         .await?;
-    let mut static_chunk_stream =
-        fetch_chunks_response_to_chunk_and_partition_id(static_chunk_stream);
 
-    // TODO(#10229): this looks to be converting back and forth?
-    while let Some(chunks) = static_chunk_stream.next().await {
-        for chunk in chunks? {
-            let (chunk, _partition_id) = chunk;
-            if !forward_chunk(chunk)? {
-                return Ok(()); // cancelled
-            }
+    let batches = chunk_index_messages
+        .iter()
+        .map(|m| m.record_batch().clone())
+        .collect_vec();
+
+    if batches.is_empty() {
+        re_log::info!("Empty recording"); // We likely won't get here even on empty recording
+        return Ok(());
+    }
+
+    let batch = arrow::compute::concat_batches(&batches[0].schema(), &batches)
+        .map_err(|err| ApiError::invalid_arguments(err, "Failed to concat chunk index batches"))?;
+
+    // Prioritize the chunks:
+    let batch = sort_batch(&batch)
+        .map_err(|err| ApiError::invalid_arguments(err, "Failed to sort chunk index"))?;
+
+    for msg in chunk_index_messages {
+        if tx
+            .send(DataSourceMessage::ChunkIndexMessage(store_id.clone(), msg))
+            .is_err()
+        {
+            re_log::debug!("Receiver disconnected");
+            return Ok(()); // cancelled
         }
     }
 
-    let temporal_chunk_stream = client
-        .fetch_partition_chunks(PartitionQueryParams {
-            dataset_id,
-            partition_id,
-            include_static_data: false,
-            include_temporal_data: true,
-            query: time_range.map(|time_range| {
-                Query {
-                    range: Some(QueryRange {
-                        index: time_range.timeline.name().to_string(),
-                        index_range: time_range.into(),
-                    }),
-                    latest_at: Some(QueryLatestAt {
-                        index: Some(time_range.timeline.name().to_string()),
-                        at: time_range.range.min(),
-                    }),
-                    columns_always_include_everything: false,
-                    columns_always_include_chunk_ids: false,
-                    columns_always_include_byte_offsets: false,
-                    columns_always_include_entity_paths: false,
-                    columns_always_include_static_indexes: false,
-                    columns_always_include_global_indexes: false,
-                    columns_always_include_component_indexes: false,
-                }
-                .into()
-            }),
-        })
-        .await?;
-    let mut temporal_chunk_stream =
-        fetch_chunks_response_to_chunk_and_partition_id(temporal_chunk_stream);
+    // Fetch the chunks base on the ids:
+    let chunk_stream = client.fetch_partition_chunks_by_id(&batch).await?;
+
+    let mut chunk_stream = fetch_chunks_response_to_chunk_and_partition_id(chunk_stream);
 
     // TODO(#10229): this looks to be converting back and forth?
-    while let Some(chunks) = temporal_chunk_stream.next().await {
+    while let Some(chunks) = chunk_stream.next().await {
         for chunk in chunks? {
             let (chunk, _partition_id) = chunk;
-            if !forward_chunk(chunk)? {
+
+            if tx
+                .send(
+                    LogMsg::ArrowMsg(
+                        store_id.clone(),
+                        chunk.to_arrow_msg().map_err(|err| {
+                            ApiError::serialization(
+                                err,
+                                "failed to parse chunk in /FetchChunks response stream",
+                            )
+                        })?,
+                    )
+                    .into(),
+                )
+                .is_err()
+            {
+                re_log::debug!("Receiver disconnected");
                 return Ok(()); // cancelled
+            }
+
+            if let Some(on_msg) = &on_msg {
+                on_msg();
             }
         }
     }
 
     Ok(())
+}
+
+fn sort_batch(batch: &RecordBatch) -> Result<RecordBatch, ArrowError> {
+    use std::sync::Arc;
+
+    let schema = batch.schema();
+
+    // Get column indices:
+    let chunk_is_static = schema.index_of("chunk_is_static")?;
+    let chunk_id = schema.index_of("chunk_id")?;
+
+    let sort_keys = vec![
+        // Static first:
+        arrow::compute::SortColumn {
+            values: Arc::new(batch.column(chunk_is_static).clone()),
+            options: Some(arrow::compute::SortOptions {
+                descending: true,
+                nulls_first: true,
+            }),
+        },
+        // Then sort by chunk id (~time)
+        arrow::compute::SortColumn {
+            values: Arc::new(batch.column(chunk_id).clone()),
+            options: Some(arrow::compute::SortOptions {
+                descending: false,
+                nulls_first: true,
+            }),
+        },
+    ];
+
+    let indices = arrow::compute::lexsort_to_indices(&sort_keys, None)?;
+    let sorted = arrow::compute::take_record_batch(batch, &indices)?;
+
+    Ok(sorted)
 }

crates/store/re_chunk_store/src/events.rs
@@ -136,6 +136,14 @@ pub struct ChunkStoreDiff {
     // deallocated.
     pub chunk: Arc<Chunk>,
 
+    /// If the an added chunk is a smaller piece of a split chunk,
+    /// then this is the original chunk.
+    ///
+    /// In other words, this is the chunk that someone called [`ChunkStore::insert_chunk`] on,
+    /// but that never made it into the store as is, but got split into multiple pieces,
+    /// out of which [`Self::chunk`] is one.
+    pub split_source: Option<ChunkId>, // TODO(#11971): Better lineage tracking
+
     /// Reports which [`Chunk`]s were merged into a new [`Chunk`] during a compaction.
     ///
     /// This is only specified if an addition to the store triggered a compaction.
@@ -153,9 +161,13 @@ impl PartialEq for ChunkStoreDiff {
         let Self {
             kind,
             chunk,
+            split_source,
             compacted,
         } = self;
-        *kind == rhs.kind && chunk.id() == rhs.chunk.id() && compacted == &rhs.compacted
+        *kind == rhs.kind
+            && chunk.id() == rhs.chunk.id()
+            && split_source == &rhs.split_source
+            && compacted == &rhs.compacted
     }
 }
 
@@ -167,6 +179,7 @@ impl ChunkStoreDiff {
         Self {
             kind: ChunkStoreDiffKind::Addition,
             chunk,
+            split_source: None,
             compacted,
         }
     }
@@ -176,6 +189,7 @@ impl ChunkStoreDiff {
         Self {
             kind: ChunkStoreDiffKind::Deletion,
             chunk,
+            split_source: None,
             compacted: None,
         }
     }

crates/store/re_chunk_store/src/writes.rs
@@ -8,8 +8,9 @@ use re_byte_size::SizeBytes;
 use re_chunk::{Chunk, EntityPath, RowId};
 
 use crate::{
-    ChunkId, ChunkStore, ChunkStoreChunkStats, ChunkStoreConfig, ChunkStoreDiff, ChunkStoreError,
-    ChunkStoreEvent, ChunkStoreResult, ColumnMetadataState, store::ChunkIdSetPerTime,
+    ChunkId, ChunkStore, ChunkStoreChunkStats, ChunkStoreConfig, ChunkStoreDiff,
+    ChunkStoreDiffKind, ChunkStoreError, ChunkStoreEvent, ChunkStoreResult, ColumnMetadataState,
+    store::ChunkIdSetPerTime,
 };
 
 // ---
@@ -58,6 +59,12 @@ impl ChunkStore {
             for split_chunk in split_chunks {
                 let events = self.insert_chunk(&split_chunk)?;
                 all_events.extend(events);
+            }
+
+            for event in &mut all_events {
+                if event.diff.kind == ChunkStoreDiffKind::Addition {
+                    event.diff.split_source = Some(chunk.id());
+                }
             }
 
             return Ok(all_events);
@@ -347,7 +354,7 @@ impl ChunkStore {
                     .chain(
                         self.remove_chunk(elected_chunk.id())
                             .into_iter()
-                            .filter(|diff| diff.kind == crate::ChunkStoreDiffKind::Deletion)
+                            .filter(|diff| diff.kind == ChunkStoreDiffKind::Deletion)
                             .map(|diff| (diff.chunk.id(), diff.chunk)),
                     )
                     .collect();

crates/store/re_entity_db/src/chunk_index.rs
@@ -1,0 +1,135 @@
+use std::collections::hash_map::Entry;
+
+use ahash::{HashMap, HashSet};
+use itertools::Itertools as _;
+
+use re_chunk::ChunkId;
+use re_chunk_store::ChunkStoreEvent;
+use re_types_core::ChunkIndexMessage;
+
+/// Info about a single chunk that we know ahead of loading it.
+#[derive(Clone, Debug, Default)]
+pub struct ChunkInfo {
+    /// Do we have the whole chunk in memory?
+    pub fully_loaded: bool,
+}
+
+/// A secondary index that keeps track of which chunks have been loaded into memory.
+///
+/// This is currently used to show a progress bar.
+///
+/// This is constructed from one ore more [`ChunkIndexMessage`], which is what
+/// the server sends to the client/viewer.
+/// TODO(RR-2999): use this for larger-than-RAM.
+#[derive(Default, Debug, Clone)]
+pub struct ChunkIndex {
+    /// These are the chunks known to exist in the data source (e.g. remote server).
+    ///
+    /// The chunk store may split large chunks and merge (compact) small ones,
+    /// so what's in the chunk store can differ significantally.
+    remote_chunks: HashMap<ChunkId, ChunkInfo>,
+
+    /// The chunk store may split large chunks and merge (compact) small ones.
+    /// When we later drop a chunk, we need to know which other chunks to invalidate.
+    parents: HashMap<ChunkId, HashSet<ChunkId>>,
+
+    /// Have we ever deleted a chunk?
+    ///
+    /// If so, we have run some GC and should not show progress bar.
+    has_deleted: bool,
+}
+
+impl ChunkIndex {
+    #[expect(clippy::needless_pass_by_value)] // In the future we may want to store them as record batches
+    pub fn append(&mut self, msg: ChunkIndexMessage) {
+        re_tracing::profile_function!();
+        for chunk_id in msg.chunk_ids() {
+            match self.remote_chunks.entry(*chunk_id) {
+                Entry::Occupied(_occupied_entry) => {
+                    // TODO(RR-2999): update time range index for the chunk
+                }
+                Entry::Vacant(vacant_entry) => {
+                    vacant_entry.insert(ChunkInfo {
+                        fully_loaded: false,
+                    });
+                }
+            }
+        }
+    }
+
+    /// How many chunks are in the index?
+    ///
+    /// Not all of them are necessarily loaded.
+    pub fn num_chunks(&self) -> usize {
+        self.remote_chunks.len()
+    }
+
+    /// [0, 1], how many chunks have been loaded?
+    ///
+    /// Returns `None` if we have already started garbage-collecting some chunks.
+    pub fn progress(&self) -> Option<f32> {
+        if self.has_deleted {
+            None
+        } else if self.num_chunks() == 0 {
+            Some(1.0)
+        } else {
+            let num_loaded = self
+                .remote_chunks
+                .values()
+                .filter(|c| c.fully_loaded)
+                .count();
+            Some(num_loaded as f32 / self.num_chunks() as f32)
+        }
+    }
+
+    pub fn mark_as_loaded(&mut self, chunk_id: ChunkId) {
+        let chunk_info = self.remote_chunks.entry(chunk_id).or_default();
+        chunk_info.fully_loaded = true;
+    }
+
+    pub fn on_events(&mut self, store_events: &[ChunkStoreEvent]) {
+        re_tracing::profile_function!();
+
+        for event in store_events {
+            let chunk_id = event.chunk.id();
+            match event.kind {
+                re_chunk_store::ChunkStoreDiffKind::Addition => {
+                    if let Some(chunk_info) = self.remote_chunks.get_mut(&chunk_id) {
+                        chunk_info.fully_loaded = true;
+                    } else if let Some(source) = event.split_source {
+                        // The added chunk was the result of splitting another chunk:
+                        self.parents.entry(chunk_id).or_default().insert(source);
+                    } else {
+                        re_log::warn!("Added chunk that was not part of the index");
+                    }
+                }
+                re_chunk_store::ChunkStoreDiffKind::Deletion => {
+                    self.mark_deleted(&chunk_id);
+                }
+            }
+        }
+    }
+
+    fn mark_deleted(&mut self, chunk_id: &ChunkId) {
+        self.has_deleted = true;
+
+        if let Some(chunk_info) = self.remote_chunks.get_mut(chunk_id) {
+            chunk_info.fully_loaded = false;
+        } else if let Some(parents) = self.parents.remove(chunk_id) {
+            // Mark all ancestors as not being fully loaded:
+
+            let mut ancestors = parents.into_iter().collect_vec();
+            while let Some(chunk_id) = ancestors.pop() {
+                if let Some(chunk_info) = self.remote_chunks.get_mut(&chunk_id) {
+                    chunk_info.fully_loaded = false;
+                } else if let Some(grandparents) = self.parents.get(&chunk_id) {
+                    ancestors.extend(grandparents);
+                } else {
+                    re_log::warn!("Removed chunk that was not part of the index");
+                }
+            }
+        } else {
+            re_log::warn!("Removed chunk that was not part of the index");
+        }
+    }
+}

crates/store/re_entity_db/src/entity_db.rs
@@ -17,11 +17,13 @@ use re_log_types::{
 };
 use re_query::{
     QueryCache, QueryCacheHandle, StorageEngine, StorageEngineArcReadGuard, StorageEngineReadGuard,
-    StorageEngineWriteGuard,
 };
 use re_smart_channel::SmartChannelSource;
+use re_types_core::ChunkIndexMessage;
 
-use crate::{Error, TimesPerTimeline, ingestion_statistics::IngestionStatistics};
+use crate::{
+    Error, TimesPerTimeline, chunk_index::ChunkIndex, ingestion_statistics::IngestionStatistics,
+};
 
 // ----------------------------------------------------------------------------
 
@@ -71,6 +73,8 @@ pub struct EntityDb {
     ///
     /// Clones of an [`EntityDb`] gets a `None` source.
     pub data_source: Option<re_smart_channel::SmartChannelSource>,
+
+    chunk_index: ChunkIndex,
 
     /// Comes in a special message, [`LogMsg::SetStoreInfo`].
     set_store_info: Option<SetStoreInfo>,
@@ -142,6 +146,7 @@ impl EntityDb {
         Self {
             store_id,
             data_source: None,
+            chunk_index: Default::default(),
             set_store_info: None,
             last_modified_at: web_time::Instant::now(),
             latest_row_id: None,
@@ -238,6 +243,11 @@ impl EntityDb {
     #[inline]
     pub fn storage_engine_arc(&self) -> StorageEngineArcReadGuard {
         self.storage_engine.read_arc()
+    }
+
+    #[inline]
+    pub fn chunk_index(&self) -> &ChunkIndex {
+        &self.chunk_index
     }
 
     #[inline]
@@ -542,6 +552,10 @@ impl EntityDb {
         self.entity_path_from_hash.contains_key(&entity_path.hash())
     }
 
+    pub fn add_chunk_index_message(&mut self, chunk_index: ChunkIndexMessage) {
+        self.chunk_index.append(chunk_index);
+    }
+
     pub fn add(&mut self, msg: &LogMsg) -> Result<Vec<ChunkStoreEvent>, Error> {
         re_tracing::profile_function!();
 
@@ -575,6 +589,7 @@ impl EntityDb {
         Ok(store_events)
     }
 
+    /// Used mostly for tests
     pub fn add_chunk(&mut self, chunk: &Arc<Chunk>) -> Result<Vec<ChunkStoreEvent>, Error> {
         self.add_chunk_with_timestamp_metadata(chunk, &Default::default())
     }
@@ -582,47 +597,61 @@ impl EntityDb {
     fn add_chunk_with_timestamp_metadata(
         &mut self,
         chunk: &Arc<Chunk>,
-        timestamps: &re_sorbet::TimestampMetadata,
+        chunk_timestamps: &re_sorbet::TimestampMetadata,
     ) -> Result<Vec<ChunkStoreEvent>, Error> {
-        let mut engine = self.storage_engine.write();
-        let store_events = engine.store().insert_chunk(chunk)?;
-        engine.cache().on_events(&store_events);
+        let store_events = self.storage_engine.write().store().insert_chunk(chunk)?;
 
         self.entity_path_from_hash
             .entry(chunk.entity_path().hash())
             .or_insert_with(|| chunk.entity_path().clone());
 
-        let engine = engine.downgrade();
-
         if self.latest_row_id < chunk.row_id_range().map(|(_, row_id_max)| row_id_max) {
             self.latest_row_id = chunk.row_id_range().map(|(_, row_id_max)| row_id_max);
         }
 
-        {
-            // Update our internal views by notifying them of resulting [`ChunkStoreEvent`]s.
-            self.times_per_timeline.on_events(&store_events);
-            self.time_histogram_per_timeline.on_events(&store_events);
-            self.tree.on_store_additions(&store_events);
+        self.chunk_index.mark_as_loaded(chunk.id());
 
-            // It is possible for writes to trigger deletions: specifically in the case of
-            // overwritten static data leading to dangling chunks.
-            let entity_paths_with_deletions = store_events
-                .iter()
-                .filter(|event| event.kind == ChunkStoreDiffKind::Deletion)
-                .map(|event| event.chunk.entity_path().clone())
-                .collect();
+        self.on_store_events(&store_events);
 
-            {
-                re_tracing::profile_scope!("on_store_deletions");
-                self.tree
-                    .on_store_deletions(&engine, &entity_paths_with_deletions, &store_events);
-            }
-
-            // We inform the stats last, since it measures e2e latency.
-            self.stats.on_events(timestamps, &store_events);
-        }
+        // We inform the stats last, since it measures e2e latency.
+        // We only care about latency metrics during ingestion (adding a chunk)
+        // which is why we only call it here, and not inside of `on_store_events`
+        // (we need the `chunk_timestamps`).
+        self.stats.on_events(chunk_timestamps, &store_events);
 
         Ok(store_events)
+    }
+
+    /// We call this on any changes, before returning the store events to the outsider caller.
+    fn on_store_events(&mut self, store_events: &[ChunkStoreEvent]) {
+        re_tracing::profile_function!();
+
+        let mut engine = self.storage_engine.write();
+
+        engine.cache().on_events(store_events);
+
+        let engine = engine.downgrade();
+
+        self.chunk_index.on_events(store_events);
+
+        // Update our internal views by notifying them of resulting [`ChunkStoreEvent`]s.
+        self.times_per_timeline.on_events(store_events);
+        self.time_histogram_per_timeline.on_events(store_events);
+        self.tree.on_store_additions(store_events);
+
+        // It is possible for writes to trigger deletions: specifically in the case of
+        // overwritten static data leading to dangling chunks.
+        let entity_paths_with_deletions = store_events
+            .iter()
+            .filter(|event| event.kind == ChunkStoreDiffKind::Deletion)
+            .map(|event| event.chunk.entity_path().clone())
+            .collect();
+
+        {
+            re_tracing::profile_scope!("on_store_deletions");
+            self.tree
+                .on_store_deletions(&engine, &entity_paths_with_deletions, store_events);
+        }
     }
 
     pub fn set_store_info(&mut self, store_info: SetStoreInfo) {
@@ -657,6 +686,8 @@ impl EntityDb {
                 .write()
                 .cache()
                 .purge_fraction_of_ram(fraction_to_purge);
+        } else {
+            self.on_store_events(&store_events);
         }
 
         store_events
@@ -665,8 +696,7 @@ impl EntityDb {
     pub fn gc(&mut self, gc_options: &GarbageCollectionOptions) -> Vec<ChunkStoreEvent> {
         re_tracing::profile_function!();
 
-        let mut engine = self.storage_engine.write();
-        let (store_events, stats_diff) = engine.store().gc(gc_options);
+        let (store_events, stats_diff) = self.storage_engine.write().store().gc(gc_options);
 
         re_log::trace!(
             num_row_ids_dropped = store_events.len(),
@@ -674,13 +704,7 @@ impl EntityDb {
             "purged datastore"
         );
 
-        Self::on_store_deletions(
-            &mut self.times_per_timeline,
-            &mut self.time_histogram_per_timeline,
-            &mut self.tree,
-            engine,
-            &store_events,
-        );
+        self.on_store_events(&store_events);
 
         store_events
     }
@@ -695,16 +719,13 @@ impl EntityDb {
     ) -> Vec<ChunkStoreEvent> {
         re_tracing::profile_function!();
 
-        let mut engine = self.storage_engine.write();
+        let store_events = self
+            .storage_engine
+            .write()
+            .store()
+            .drop_time_range(timeline, drop_range);
 
-        let store_events = engine.store().drop_time_range(timeline, drop_range);
-        Self::on_store_deletions(
-            &mut self.times_per_timeline,
-            &mut self.time_histogram_per_timeline,
-            &mut self.tree,
-            engine,
-            &store_events,
-        );
+        self.on_store_events(&store_events);
 
         store_events
     }
@@ -717,16 +738,13 @@ impl EntityDb {
     pub fn drop_entity_path(&mut self, entity_path: &EntityPath) {
         re_tracing::profile_function!();
 
-        let mut engine = self.storage_engine.write();
+        let store_events = self
+            .storage_engine
+            .write()
+            .store()
+            .drop_entity_path(entity_path);
 
-        let store_events = engine.store().drop_entity_path(entity_path);
-        Self::on_store_deletions(
-            &mut self.times_per_timeline,
-            &mut self.time_histogram_per_timeline,
-            &mut self.tree,
-            engine,
-            &store_events,
-        );
+        self.on_store_events(&store_events);
     }
 
     /// Unconditionally drops all the data for a given [`EntityPath`] and all its children.
@@ -744,28 +762,6 @@ impl EntityDb {
         for entity_path in to_drop {
             self.drop_entity_path(&entity_path);
         }
-    }
-
-    // NOTE: Parameters deconstructed instead of taking `self`, because borrowck cannot understand
-    // partial borrows on methods.
-    fn on_store_deletions(
-        times_per_timeline: &mut TimesPerTimeline,
-        time_histogram_per_timeline: &mut crate::TimeHistogramPerTimeline,
-        tree: &mut crate::EntityTree,
-        mut engine: StorageEngineWriteGuard<'_>,
-        store_events: &[ChunkStoreEvent],
-    ) {
-        engine.cache().on_events(store_events);
-        times_per_timeline.on_events(store_events);
-        time_histogram_per_timeline.on_events(store_events);
-
-        let engine = engine.downgrade();
-        let entity_paths_with_deletions = store_events
-            .iter()
-            .filter(|event| event.kind == ChunkStoreDiffKind::Deletion)
-            .map(|event| event.chunk.entity_path().clone())
-            .collect();
-        tree.on_store_deletions(&engine, &entity_paths_with_deletions, store_events);
     }
 
     /// Export the contents of the current database to a sequence of messages.

crates/store/re_entity_db/src/entity_tree.rs
@@ -168,6 +168,11 @@ impl EntityTree {
         entity_paths_with_deletions: &IntSet<EntityPath>,
         events: &[ChunkStoreEvent],
     ) {
+        // NOTE: no re_tracing here because this is a recursive function
+        if entity_paths_with_deletions.is_empty() {
+            return; // early-out
+        }
+
         // We don't actually use the events for anything, we just want to
         // have a direct dependency on the chunk store which must have
         // produced them by the time this function was called.

commit 9ca72255970d47642f7daf846faea705bd3f0dde

Fix only loading the chunks required for a time selection (#12062)
### Related
* I accidentally broke this in
https://github.com/rerun-io/rerun/pull/12012

crates/store/re_redap_client/src/grpc.rs
@@ -9,6 +9,7 @@ use re_log_types::{
     AbsoluteTimeRange, BlueprintActivationCommand, DataSourceMessage, DataSourceUiCommand, EntryId,
     LogMsg, SetStoreInfo, StoreId, StoreInfo, StoreKind, StoreSource,
 };
+use re_protos::cloud::v1alpha1::ext::{Query, QueryLatestAt, QueryRange};
 use re_protos::cloud::v1alpha1::rerun_cloud_service_client::RerunCloudServiceClient;
 use re_protos::common::v1alpha1::ext::SegmentId;
 use re_uri::{Origin, TimeSelection};
@@ -459,14 +460,35 @@ async fn stream_segment_from_server(
     // of client's HTTP2 connection window, and ultimately to a complete stall of the entire system.
     // See the attached issues for more information.
 
-    // Retrieve the chunk IDs we're interested in (all of them):
+    // Retrieve the chunk IDs we're interested in:
     let chunk_index_messages = client
         .query_dataset_chunk_index(SegmentQueryParams {
             dataset_id,
             segment_id: segment_id.clone(),
             include_static_data: true,
             include_temporal_data: true,
-            query: None,
+            query: time_range.map(|time_range| {
+                Query {
+                    // So that we can show the state at the start:
+                    latest_at: Some(QueryLatestAt {
+                        index: Some(time_range.timeline.name().to_string()),
+                        at: time_range.range.min(),
+                    }),
+                    // Show we can show everything in the range:
+                    range: Some(QueryRange {
+                        index: time_range.timeline.name().to_string(),
+                        index_range: time_range.into(),
+                    }),
+                    columns_always_include_everything: false,
+                    columns_always_include_chunk_ids: false,
+                    columns_always_include_byte_offsets: false,
+                    columns_always_include_entity_paths: false,
+                    columns_always_include_static_indexes: false,
+                    columns_always_include_global_indexes: false,
+                    columns_always_include_component_indexes: false,
+                }
+                .into()
+            }),
         })
         .await?;
 