33e4c6c6a03810f22c70a4dcdb3484876ef1c238
Tim Saucer <timsaucer@gmail.com>
2025-12-02 09:28:44 -0500

    Update unit test to add prefix

--- rerun_py/tests/e2e_redap_tests/test_table_read.py
+++ rerun_py/tests/e2e_redap_tests/test_table_read.py
@@ -48,31 +48,31 @@ def test_datafusion_catalog_get_tables(prefilled_catalog: PrefilledCatalog) -> N
 
     # Note: as of DataFusion 50.0.0 this is not a DataFrame
     # but rather a python object that describes the table.
-    table = schema_provider.table("simple_datatypes")
+    table = schema_provider.table(prefilled_catalog.factory.apply_prefix("simple_datatypes"))
     assert table is not None
 
     schema_provider = catalog_provider.schema("second_schema")
-    assert schema_provider.table("second_table") is not None
+    assert schema_provider.table(prefilled_catalog.factory.apply_prefix("second_table")) is not None
 
     catalog_provider = ctx.catalog("alternate_catalog")
     schema_provider = catalog_provider.schema("third_schema")
-    assert schema_provider.table("third_table") is not None
+    assert schema_provider.table(prefilled_catalog.factory.apply_prefix("third_table")) is not None
 
     # Get by table name since it should be in the default catalog/schema
-    df = ctx.table("simple_datatypes")
+    df = ctx.table(prefilled_catalog.factory.apply_prefix("simple_datatypes"))
     rb = pa.Table.from_batches(df.collect())
     assert rb.num_rows > 0
 
     # Get table by fully qualified name
-    df = ctx.table("datafusion.public.simple_datatypes")
+    df = ctx.table(prefilled_catalog.factory.apply_prefix("datafusion.public.simple_datatypes"))
     rb = pa.Table.from_batches(df.collect())
     assert rb.num_rows > 0
 
     # Verify SQL parsing for catalog provider works as expected
-    df = ctx.sql("SELECT * FROM simple_datatypes")
+    df = ctx.sql(f"SELECT * FROM {prefilled_catalog.factory.apply_prefix('simple_datatypes')}")
     rb = pa.Table.from_batches(df.collect())
     assert rb.num_rows > 0
 
-    df = ctx.sql("SELECT * FROM datafusion.public.simple_datatypes")
+    df = ctx.sql(f"SELECT * FROM {prefilled_catalog.factory.apply_prefix('datafusion.public.simple_datatypes')}")
     rb = pa.Table.from_batches(df.collect())
     assert rb.num_rows > 0
