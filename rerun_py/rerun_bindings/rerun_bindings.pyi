a5a56688c823d06fbaa6ec273007654efd5c8d49
Antoine Beyeler <antoine@rerun.io>
2025-12-04 16:21:26 +0100

    fix signature mismatch

--- rerun_py/rerun_bindings/rerun_bindings.pyi
+++ rerun_py/rerun_bindings/rerun_bindings.pyi
@@ -520,7 +520,7 @@ class Recording:
         monotonically increasing when data is sent from a single process.
 
         If `None` is passed as the index, the view will contain only static columns (among those
-        specified) and no index columns. It will also contain a single row per partition.
+        specified) and no index columns. It will also contain a single row per segment.
 
         Parameters
         ----------
