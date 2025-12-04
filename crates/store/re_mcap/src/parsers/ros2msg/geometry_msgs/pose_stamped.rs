60c7cd41d8c05c76a0bf0a7b7343d198fe163e67
Michael Grupp <michael@rerun.io>
2025-12-04 10:27:41 +0100

    Update to InstancePoses with CoordinateFrame

--- crates/store/re_mcap/src/parsers/ros2msg/geometry_msgs/pose_stamped.rs
+++ crates/store/re_mcap/src/parsers/ros2msg/geometry_msgs/pose_stamped.rs
@@ -1,10 +1,8 @@
 use super::super::definitions::geometry_msgs::PoseStamped;
 use re_chunk::{Chunk, ChunkId};
 
-// TODO: archetype is just an example to visualize _something_. Transform3D would be not entirely correct here,
-// as a pose is not a frame-to-frame relationship (pose vs transform is similar to point vs vector). tbd!
-use re_types::components::Vector3D;
-use re_types::{archetypes::Arrows3D, components::Position3D};
+use re_types::archetypes::{CoordinateFrame, InstancePoses3D};
+use re_types::components::{PoseRotationQuat, PoseTranslation3D};
 
 use super::super::Ros2MessageParser;
 use crate::parsers::{
@@ -13,20 +11,18 @@ use crate::parsers::{
 };
 use crate::util::TimestampCell;
 
-use glam::{DQuat, DVec3};
-
 pub struct PoseStampedMessageParser {
-    // Here we just use the oriented x vector of the pose, mimicking the RViz posestamped display
-    // TODO: use sth better than arrows
-    x_vectors: Vec<Vector3D>,
-    origins: Vec<Position3D>,
+    translations: Vec<PoseTranslation3D>,
+    quaternions: Vec<PoseRotationQuat>,
+    frame_ids: Vec<String>,
 }
 
 impl Ros2MessageParser for PoseStampedMessageParser {
     fn new(num_rows: usize) -> Self {
         Self {
-            x_vectors: Vec::with_capacity(num_rows),
-            origins: Vec::with_capacity(num_rows),
+            translations: Vec::with_capacity(num_rows),
+            quaternions: Vec::with_capacity(num_rows),
+            frame_ids: Vec::with_capacity(num_rows),
         }
     }
 }
@@ -36,48 +32,41 @@ impl MessageParser for PoseStampedMessageParser {
         re_tracing::profile_function!();
         let PoseStamped { header, pose } = cdr::try_decode_message::<PoseStamped>(&msg.data)?;
 
-        // TODO: use frame_id
-
         // Add the header timestamp to the context, `log_time` and `publish_time` are added automatically
         ctx.add_timestamp_cell(TimestampCell::guess_from_nanos_ros2(
             header.stamp.as_nanos() as u64,
         ));
-        let position = Position3D::new(
+
+        self.frame_ids.push(header.frame_id);
+        self.translations.push(PoseTranslation3D::new(
             pose.position.x as f32,
             pose.position.y as f32,
             pose.position.z as f32,
-        );
-        // TODO: normalize or error if not normalized?
-        let orientation = DQuat::from_xyzw(
-            pose.orientation.x,
-            pose.orientation.y,
-            pose.orientation.z,
-            pose.orientation.w,
-        )
-        .normalize();
-
-        let direction_x = orientation * DVec3::X;
-        self.x_vectors.push(Vector3D::from([
-            direction_x.x as f32,
-            direction_x.y as f32,
-            direction_x.z as f32,
-        ]));
-
-        self.origins.push(position);
+        ));
+        // TODO!!
+        self.quaternions.push(PoseRotationQuat::IDENTITY);
 
         Ok(())
     }
 
     fn finalize(self: Box<Self>, ctx: ParserContext) -> anyhow::Result<Vec<re_chunk::Chunk>> {
         re_tracing::profile_function!();
-        let Self { x_vectors, origins } = *self;
+        let Self {
+            translations,
+            quaternions,
+            frame_ids: frames,
+        } = *self;
 
         let entity_path = ctx.entity_path().clone();
         let timelines = ctx.build_timelines();
 
-        let components = Arrows3D::update_fields()
-            .with_vectors(x_vectors)
-            .with_origins(origins)
+        let pose_components: Vec<_> = InstancePoses3D::update_fields()
+            .with_translations(translations)
+            .with_quaternions(quaternions)
+            .columns_of_unit_batches()?
+            .collect();
+        let frame_components: Vec<_> = CoordinateFrame::update_fields()
+            .with_many_frame(frames)
             .columns_of_unit_batches()?
             .collect();
 
@@ -85,7 +74,10 @@ impl MessageParser for PoseStampedMessageParser {
             ChunkId::new(),
             entity_path.clone(),
             timelines.clone(),
-            components,
+            pose_components
+                .into_iter()
+                .chain(frame_components.into_iter())
+                .collect(),
         )?;
 
         Ok(vec![chunk])
