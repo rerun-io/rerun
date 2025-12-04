32919461d938155d31a69fa8994c03f45dbd384e
Andrea Reale <154321632+andrea-reale@users.noreply.github.com>
2025-12-04 19:06:01 +0100

    grpc client: set TCP connection timeout (#12098)

--- rerun_py/tests/e2e_redap_tests/test_client.py
+++ rerun_py/tests/e2e_redap_tests/test_client.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
 import re
+import signal
 from typing import TYPE_CHECKING
 
+import pytest
+from rerun.catalog import CatalogClient
+
 if TYPE_CHECKING:
+    import types
+
     from .conftest import ServerInstance
 
 
@@ -15,3 +21,22 @@ def test_urls(server_instance: ServerInstance) -> None:
 
     table = server_instance.client.get_table_entry(name="simple_datatypes")
     assert re.match("^file:///[-_:./0-9a-zA-Z]+/simple_datatypes/$", table.storage_url)
+
+
+def test_network_unreachable() -> None:
+    """Tests that the client raises an error when the server is unreachable."""
+
+    def timeout_handler(_signal_num: int, _frame: types.FrameType | None) -> None:
+        raise TimeoutError("the operation did not time out on time")
+
+    signal.signal(signal.SIGALRM, timeout_handler)
+    # It'd be nice if the connection timeout was configurable, so we would not have to wait for 10 seconds for this test. Another day.
+    signal.alarm(15)  # Our connection timeout is 10 seconds. Let's be generous to avoid flakiness
+
+    try:
+        with pytest.raises(ConnectionError, match=r"failed to connect to server"):  # Adjust exception type as needed
+            # This works because 192.0.2.0 is a reserved address block for documentation and examples.
+            # ISPs should not route traffic to this block and just drop SYN packets.
+            CatalogClient(address="rerun+http://192.0.2.1")
+    finally:
+        signal.alarm(0)  # Cancel the alarm
