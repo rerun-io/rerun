b59260c37eee38e93e25446409437057f8b8f3b8
jprochazk <pjanv42@gmail.com>
2025-12-05 17:48:46 +0100

    simpler output

--- rerun_py/rerun_sdk/rerun/auth.py
+++ rerun_py/rerun_sdk/rerun/auth.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-from IPython.core.display import HTML
-from IPython.display import clear_output, display
-
 from rerun_bindings import init_login_flow
 
 """
@@ -23,12 +20,14 @@ def login() -> None:
 
     # Automatically open the login URL in the default browser
     login_url = flow.login_url()
-    print("Click the button to login in your browser:")
-    display(HTML(f'<a href="{login_url}" target="_blank" style="">Login</a>'))
+    print("Open the following URL in your browser:")
+    print()
+    print(f"  {login_url}")
+    print()
     print("You should see the following code:")
-    print(flow.user_code())
+    print()
+    print(f"  {flow.user_code()}")
 
     # Wait for the flow to complete and store credentials
     credentials = flow.finish_login_flow()
-    clear_output(wait=True)
     print(f"Success! You're logged in as '{credentials.user_email}'.")
