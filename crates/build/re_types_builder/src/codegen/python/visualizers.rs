6b83f5d6a39cad3231958f35dd2e3f8fb5899101
Jochen Görtler <me@jgoertler.com>
2025-12-11 15:41:15 +0100

    new experimental api

--- crates/build/re_types_builder/src/codegen/python/visualizers.rs
+++ crates/build/re_types_builder/src/codegen/python/visualizers.rs
@@ -58,8 +58,52 @@ pub fn generate_visualizers_file(reporter: &Reporter, objects: &Objects) -> Stri
 
     visualizers.sort_by(|a, b| a.0.cmp(&b.0));
 
-    for (archetype_name, visualizer_id) in visualizers {
-        code.push_indented(0, format!("{} = \"{}\"", archetype_name, visualizer_id), 1);
+    // Generate string constants
+    for (archetype_name, visualizer_id) in &visualizers {
+        code.push_indented(0, format!("{archetype_name} = \"{visualizer_id}\""), 1);
+    }
+
+    // Generate experimental module with visualizer classes
+    code.push_unindented("\n\n# Experimental API for configuring visualizers", 1);
+    code.push_indented(0, "class experimental:", 1);
+    code.push_indented(
+        1,
+        "\"\"\"Experimental APIs for configuring visualizers.\"\"\"",
+        2,
+    );
+
+    // Base class
+    code.push_indented(1, "# This part is handwritten", 1);
+    code.push_indented(1, "class Visualizer:", 1);
+    code.push_indented(
+        2,
+        "def __init__(self, visualizer_type: str, *, overrides=None, mappings=None):",
+        1,
+    );
+    code.push_indented(3, "self.visualizer_type = visualizer_type", 1);
+    code.push_indented(3, "self.overrides = overrides", 1);
+    code.push_indented(3, "self.mappings = mappings or []", 2);
+    code.push_indented(
+        2,
+        "# TODO(grtlr,andreas): Implementation of new blueprint API goes here",
+        2,
+    );
+
+    // Generated visualizer classes
+    for (archetype_name, visualizer_id) in &visualizers {
+        code.push_indented(1, format!("class {archetype_name}(Visualizer):"), 1);
+        code.push_indented(
+            2,
+            "def __init__(self, *, overrides=None, mappings=None):",
+            1,
+        );
+        code.push_indented(
+            3,
+            format!(
+                "super().__init__(\"{visualizer_id}\", overrides=overrides, mappings=mappings)",
+            ),
+            2,
+        );
     }
 
     code
