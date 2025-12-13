bc60856272fd7d438c4e055effe46864feaf3f35
Jeremy (Rerun) <jeremy@rerun.io>
2025-12-13 11:23:39 +0100

    Detect isolated uv build environments more directly

--- scripts/ci/build_and_upload_wheels.py
+++ scripts/ci/build_and_upload_wheels.py
@@ -80,10 +80,6 @@ def build_and_upload(
 
     compatibility = f"--compatibility {compatibility}" if compatibility is not None else ""
 
-    # Build into `dist`
-    env = os.environ.copy()
-    env["RERUN_MATURIN_BUILD"] = "1"
-
     run(
         "maturin build "
         f"{compatibility} "
@@ -92,7 +88,6 @@ def build_and_upload(
         f"--target {target} "
         f"{maturin_feature_flags} "
         f"--out {dist}",
-        env=env,
     )
 
     pkg = os.listdir(dist)[0]
