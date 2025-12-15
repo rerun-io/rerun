d1c7b7e44d4c42117ec6f8f3aceac4a716e09162
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2025-12-15 19:28:04 +0100

    Fix double separator in top panel in release builds

--- crates/viewer/re_viewer/src/ui/top_panel.rs
+++ crates/viewer/re_viewer/src/ui/top_panel.rs
@@ -206,9 +206,14 @@ fn show_warnings(frame: &eframe::Frame, ui: &mut egui::Ui, app_env: &crate::AppE
     }
 
     if !app_env.is_test() {
-        show_warning(ui, &mut has_shown_warning, |ui| {
-            show_software_rasterizer_warning(frame, ui);
-        });
+        if let Some(wgpu) = frame.wgpu_render_state()
+            && let info = wgpu.adapter.get_info()
+            && info.device_type == wgpu::DeviceType::Cpu
+        {
+            show_warning(ui, &mut has_shown_warning, |ui| {
+                software_rasterizer_warning_ui(ui, &info);
+            });
+        }
     }
 
     if crate::docker_detection::is_docker() {
@@ -225,30 +230,23 @@ fn show_warnings(frame: &eframe::Frame, ui: &mut egui::Ui, app_env: &crate::AppE
     }
 }
 
-fn show_software_rasterizer_warning(frame: &eframe::Frame, ui: &mut egui::Ui) {
-    if let Some(wgpu) = frame.wgpu_render_state() {
-        let info = wgpu.adapter.get_info();
-        if info.device_type == wgpu::DeviceType::Cpu {
-            ui.hyperlink_to(
-                egui::RichText::new("⚠ Software rasterizer")
-                    .small()
-                    .color(ui.visuals().warn_fg_color),
-                "https://www.rerun.io/docs/getting-started/troubleshooting#graphics-issues",
-            )
-            .on_hover_ui(|ui| {
-                ui.label("Software rasterizer detected - expect poor performance.");
-                ui.label(
-                    "Rerun requires hardware accelerated graphics (i.e. a GPU) for good performance.",
-                );
-                ui.label("Click for troubleshooting.");
-                ui.add_space(8.0);
-                ui.label(format!(
-                    "wgpu adapter {}",
-                    re_renderer::adapter_info_summary(&info)
-                ));
-            });
-        }
-    }
+fn software_rasterizer_warning_ui(ui: &mut egui::Ui, info: &wgpu::AdapterInfo) {
+    ui.hyperlink_to(
+        egui::RichText::new("⚠ Software rasterizer")
+            .small()
+            .color(ui.visuals().warn_fg_color),
+        "https://www.rerun.io/docs/getting-started/troubleshooting#graphics-issues",
+    )
+    .on_hover_ui(|ui| {
+        ui.label("Software rasterizer detected - expect poor performance.");
+        ui.label("Rerun requires hardware accelerated graphics (i.e. a GPU) for good performance.");
+        ui.label("Click for troubleshooting.");
+        ui.add_space(8.0);
+        ui.label(format!(
+            "wgpu adapter {}",
+            re_renderer::adapter_info_summary(info)
+        ));
+    });
 }
 
 /// Show an orange dot to warn about multi-pass layout in egui.
