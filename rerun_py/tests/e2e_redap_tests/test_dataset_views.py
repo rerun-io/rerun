e63d9b4ea23b0c52dae61fd20535724b559b74d2
Sylvain Wallez <sylvain@bluxte.net>
2025-12-15 18:40:35 +0100

    py-fmt

--- rerun_py/tests/e2e_redap_tests/test_dataset_views.py
+++ rerun_py/tests/e2e_redap_tests/test_dataset_views.py
@@ -68,19 +68,20 @@ def sort_schema(schema: pa.Schema) -> str:
     for field in schema:
         # Sorted field metadata
         field_meta = {}
-        for (k, v) in field.metadata.items():
+        for k, v in field.metadata.items():
             field_meta[k] = v
         all_fields[field.name] = dict(sorted(field_meta.items(), key=lambda item: item[0]))
 
     sorted_fields = dict(sorted(all_fields.items(), key=lambda item: item[0]))
 
     output = ""
-    for (name, field_meta) in sorted_fields.items():
+    for name, field_meta in sorted_fields.items():
         output += f"{name}:\n"
-        for (k, v) in field_meta.items():
+        for k, v in field_meta.items():
             output += f"    {k}: {v}\n"
 
     return output
+
 
 def test_dataset_view_filter_contents(readonly_test_dataset: DatasetEntry, snapshot: SnapshotAssertion) -> None:
     """Test filtering a dataset by entity paths."""
