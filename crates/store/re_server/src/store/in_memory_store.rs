c8733db0a0e87747b66ec3e59fa964feaf84fc73
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2025-12-16 14:59:24 +0100

    Add support for creating dataset from list of RRDs to `rerun.server.Server` (#12225)

--- crates/store/re_server/src/store/in_memory_store.rs
+++ crates/store/re_server/src/store/in_memory_store.rs
@@ -140,7 +140,7 @@ impl InMemoryStore {
         };
 
         let dataset = self
-            .create_dataset(&entry_name, None, StoreKind::Recording, None)
+            .create_dataset(entry_name.into(), None)
             .expect("Name cannot yet exist");
 
         for entry in std::fs::read_dir(&directory)? {
@@ -306,20 +306,52 @@ impl InMemoryStore {
         self.update_entries_table()
     }
 
+    /// Create a (regular) dataset with a matching blueprint dataset.
+    ///
+    /// The server is typically responsible for setting the dataset id, so use `Some` at your own
+    /// risk for `dataset_id`.
     pub fn create_dataset(
         &mut self,
-        name: &str,
-        id: Option<EntryId>,
+        dataset_name: String,
+        dataset_id: Option<EntryId>,
+    ) -> Result<&mut Dataset, Error> {
+        let dataset_id = dataset_id.unwrap_or_else(EntryId::new);
+        let blueprint_dataset_id = EntryId::new();
+        let blueprint_dataset_name = format!("__bp_{dataset_id}");
+
+        self.create_dataset_impl(
+            blueprint_dataset_name,
+            blueprint_dataset_id,
+            StoreKind::Blueprint,
+            None,
+        )?;
+
+        let dataset_details = DatasetDetails {
+            blueprint_dataset: Some(blueprint_dataset_id),
+            default_blueprint_segment: None,
+        };
+
+        self.create_dataset_impl(
+            dataset_name,
+            dataset_id,
+            StoreKind::Recording,
+            Some(dataset_details),
+        )
+    }
+
+    /// Create a dataset of the given kind with the given details.
+    fn create_dataset_impl(
+        &mut self,
+        name: String,
+        entry_id: EntryId,
         store_kind: StoreKind,
         details: Option<DatasetDetails>,
     ) -> Result<&mut Dataset, Error> {
         re_log::debug!(name, "create_dataset");
-        let name = name.to_owned();
         if self.id_by_name.contains_key(&name) {
             return Err(Error::DuplicateEntryNameError(name));
         }
 
-        let entry_id = id.unwrap_or_else(EntryId::new);
         if self.id_exists(&entry_id) {
             return Err(Error::DuplicateEntryIdError(entry_id));
         }
