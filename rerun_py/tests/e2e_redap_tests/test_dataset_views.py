8298d4d8b81480dbf9dd51659eab2886700740c1
Sylvain Wallez <sylvain@bluxte.net>
2025-12-16 18:31:35 +0100

    Compare sorted results in e2e tests

--- rerun_py/tests/e2e_redap_tests/test_dataset_views.py
+++ rerun_py/tests/e2e_redap_tests/test_dataset_views.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import datafusion
 import pyarrow as pa
 import pytest
 
@@ -106,4 +107,10 @@ def test_dataset_view_reader(readonly_test_dataset: DatasetEntry, snapshot: Snap
     view = readonly_test_dataset.filter_segments([first_segment]).filter_contents(["/obj1/**"])
     df = view.reader(index="time_1")
 
+    df = sorted_df(df)
+
     assert str(df) == snapshot()
+
+def sorted_df(df: datafusion.DataFrame) -> datafusion.DataFrame:
+    sorted_fields = sorted([field.name for field in df.schema()])
+    return df.select(*sorted_fields)
