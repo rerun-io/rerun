rerun-io/rerun

commit 85720900d3319da5b5f4e1eeef2170872a28c4db

Allow visualizers to report runtime errors (#11962)

crates/viewer/re_view_spatial/src/visualizers/encoded_image.rs
@@ -7,7 +7,8 @@ use re_view::HybridResults;
 use re_viewer_context::{
     IdentifiedViewSystem, ImageDecodeCache, MaybeVisualizableEntities, QueryContext, ViewContext,
     ViewContextCollection, ViewQuery, ViewSystemExecutionError, VisualizableEntities,
-    VisualizableFilterContext, VisualizerQueryInfo, VisualizerSystem, typed_fallback_for,
+    VisualizableFilterContext, VisualizerExecutionOutput, VisualizerQueryInfo, VisualizerSystem,
+    typed_fallback_for,
 };
 
 use crate::{
@@ -56,11 +57,14 @@ impl VisualizerSystem for EncodedImageVisualizer {
         ctx: &ViewContext<'_>,
         view_query: &ViewQuery<'_>,
         context_systems: &ViewContextCollection,
-    ) -> Result<Vec<re_renderer::QueueableDrawData>, ViewSystemExecutionError> {
+    ) -> Result<VisualizerExecutionOutput, ViewSystemExecutionError> {
+        let mut output = VisualizerExecutionOutput::default();
+
         process_archetype::<Self, EncodedImage, _>(
             ctx,
             view_query,
             context_systems,
+            &mut output,
             |ctx, spatial_ctx, results| {
                 self.process_encoded_image(ctx, results, spatial_ctx);
                 Ok(())
@@ -81,10 +85,10 @@ impl VisualizerSystem for EncodedImageVisualizer {
             )
         });
 
-        Ok(vec![PickableTexturedRect::to_draw_data(
+        Ok(output.with_draw_data([PickableTexturedRect::to_draw_data(
             ctx.viewer_ctx.render_ctx(),
             &self.data.pickable_rects,
-        )?])
+        )?]))
     }
 
     fn data(&self) -> Option<&dyn std::any::Any> {

crates/viewer/re_view_spatial/src/visualizers/images.rs
@@ -8,7 +8,8 @@ use re_view::HybridResults;
 use re_viewer_context::{
     IdentifiedViewSystem, ImageInfo, MaybeVisualizableEntities, QueryContext, ViewContext,
     ViewContextCollection, ViewQuery, ViewSystemExecutionError, VisualizableEntities,
-    VisualizableFilterContext, VisualizerQueryInfo, VisualizerSystem, typed_fallback_for,
+    VisualizableFilterContext, VisualizerExecutionOutput, VisualizerQueryInfo, VisualizerSystem,
+    typed_fallback_for,
 };
 
 use crate::{
@@ -62,11 +63,14 @@ impl VisualizerSystem for ImageVisualizer {
         ctx: &ViewContext<'_>,
         view_query: &ViewQuery<'_>,
         context_systems: &ViewContextCollection,
-    ) -> Result<Vec<re_renderer::QueueableDrawData>, ViewSystemExecutionError> {
+    ) -> Result<VisualizerExecutionOutput, ViewSystemExecutionError> {
+        let mut output = VisualizerExecutionOutput::default();
+
         process_archetype::<Self, Image, _>(
             ctx,
             view_query,
             context_systems,
+            &mut output,
             |ctx, spatial_ctx, results| {
                 self.process_image(ctx, results, spatial_ctx);
                 Ok(())
@@ -87,10 +91,10 @@ impl VisualizerSystem for ImageVisualizer {
             )
         });
 
-        Ok(vec![PickableTexturedRect::to_draw_data(
+        Ok(output.with_draw_data([PickableTexturedRect::to_draw_data(
             ctx.viewer_ctx.render_ctx(),
             &self.data.pickable_rects,
-        )?])
+        )?]))
     }
 
     fn data(&self) -> Option<&dyn std::any::Any> {

crates/viewer/re_view_spatial/src/visualizers/segmentation_images.rs
@@ -7,7 +7,7 @@ use re_types::{
 use re_viewer_context::{
     IdentifiedViewSystem, ImageInfo, MaybeVisualizableEntities, ViewContext, ViewContextCollection,
     ViewQuery, ViewSystemExecutionError, VisualizableEntities, VisualizableFilterContext,
-    VisualizerQueryInfo, VisualizerSystem, typed_fallback_for,
+    VisualizerExecutionOutput, VisualizerQueryInfo, VisualizerSystem, typed_fallback_for,
 };
 
 use crate::{
@@ -60,12 +60,15 @@ impl VisualizerSystem for SegmentationImageVisualizer {
         ctx: &ViewContext<'_>,
         view_query: &ViewQuery<'_>,
         context_systems: &ViewContextCollection,
-    ) -> Result<Vec<re_renderer::QueueableDrawData>, ViewSystemExecutionError> {
+    ) -> Result<VisualizerExecutionOutput, ViewSystemExecutionError> {
+        let mut output = VisualizerExecutionOutput::default();
+
         use super::entity_iterator::{iter_component, iter_slices, process_archetype};
         process_archetype::<Self, SegmentationImage, _>(
             ctx,
             view_query,
             context_systems,
+            &mut output,
             |ctx, spatial_ctx, results| {
                 use re_view::RangeResultsExt as _;
 
@@ -160,10 +163,10 @@ impl VisualizerSystem for SegmentationImageVisualizer {
             )
         });
 
-        Ok(vec![PickableTexturedRect::to_draw_data(
+        Ok(output.with_draw_data([PickableTexturedRect::to_draw_data(
             ctx.viewer_ctx.render_ctx(),
             &self.data.pickable_rects,
-        )?])
+        )?]))
     }
 
     fn data(&self) -> Option<&dyn std::any::Any> {

crates/viewer/re_selection_panel/src/selection_panel.rs
@@ -15,9 +15,9 @@ use re_ui::{
     list_item::{self, PropertyContent},
 };
 use re_viewer_context::{
-    ContainerId, Contents, DataQueryResult, DataResult, HoverHighlight, Item, SystemCommand,
-    SystemCommandSender as _, TimeControlCommand, UiLayout, ViewContext, ViewId, ViewStates,
-    ViewerContext, contents_name_style, icon_for_container_kind,
+    ContainerId, Contents, DataQueryResult, DataResult, HoverHighlight, Item, PerVisualizer,
+    SystemCommand, SystemCommandSender as _, TimeControlCommand, UiLayout, ViewContext, ViewId,
+    ViewStates, ViewerContext, contents_name_style, icon_for_container_kind,
 };
 use re_viewport_blueprint::{ViewportBlueprint, ui::show_add_view_or_container_modal};
 
@@ -635,8 +635,19 @@ fn entity_selection_ui(
         .cloned();
 
     if let Some(view) = viewport.view(view_id) {
-        let view_ctx = view.bundle_context_with_states(ctx, view_states);
-        visualizer_ui(&view_ctx, view, entity_path, ui);
+        let class = view.class(ctx.view_class_registry());
+        view_states.ensure_state_exists(*view_id, class);
+        let view_state = view_states
+            .get(*view_id)
+            .expect("State got created just now"); // Convince borrow checker we're not mutating `view_states` anymore.
+        let view_ctx = view.bundle_context_with_state(ctx, view_state);
+
+        let empty_errors = PerVisualizer::default();
+        let visualizer_errors = view_states
+            .visualizer_errors(*view_id)
+            .unwrap_or(&empty_errors);
+
+        visualizer_ui(&view_ctx, view, visualizer_errors, entity_path, ui);
     }
 
     if let Some(data_result) = &data_result {

crates/viewer/re_selection_panel/src/visualizer_ui.rs
@@ -11,14 +11,15 @@ use re_ui::list_item::ListItemContentButtonsExt as _;
 use re_ui::{OnResponseExt as _, UiExt as _, design_tokens_of_visuals, list_item};
 use re_view::latest_at_with_blueprint_resolved_data;
 use re_viewer_context::{
-    BlueprintContext as _, DataResult, QueryContext, UiLayout, ViewContext, ViewSystemIdentifier,
-    VisualizerCollection, VisualizerSystem,
+    BlueprintContext as _, DataResult, PerVisualizer, QueryContext, UiLayout, ViewContext,
+    ViewSystemIdentifier, VisualizerCollection, VisualizerExecutionErrorState, VisualizerSystem,
 };
 use re_viewport_blueprint::ViewBlueprint;
 
 pub fn visualizer_ui(
     ctx: &ViewContext<'_>,
     view: &ViewBlueprint,
+    visualizer_errors: &PerVisualizer<VisualizerExecutionErrorState>,
     entity_path: &EntityPath,
     ui: &mut egui::Ui,
 ) {
@@ -79,7 +80,14 @@ specific to the visualizer and the current view type.";
         .with_button(button)
         .with_help_markdown(markdown)
         .show(ui, |ui| {
-            visualizer_ui_impl(ctx, ui, &data_result, &active_visualizers, &all_visualizers);
+            visualizer_ui_impl(
+                ctx,
+                ui,
+                &data_result,
+                &active_visualizers,
+                &all_visualizers,
+                visualizer_errors,
+            );
         });
 }
 
@@ -89,6 +97,7 @@ pub fn visualizer_ui_impl(
     data_result: &DataResult,
     active_visualizers: &[ViewSystemIdentifier],
     all_visualizers: &VisualizerCollection,
+    visualizer_errors: &PerVisualizer<VisualizerExecutionErrorState>,
 ) {
     let override_path = data_result.override_path();
 
@@ -120,6 +129,14 @@ pub fn visualizer_ui_impl(
             ui.push_id(visualizer_id, |ui| {
                 // List all components that the visualizer may consume.
                 if let Ok(visualizer) = all_visualizers.get_by_identifier(visualizer_id) {
+                    // Report whether this visualizer failed running.
+                    let error_string =
+                        visualizer_errors
+                            .get(&visualizer_id)
+                            .and_then(|error_state| {
+                                error_state.error_string_for(&data_result.entity_path)
+                            });
+
                     ui.list_item()
                         .with_y_offset(1.0)
                         .with_height(20.0)
@@ -140,6 +157,11 @@ pub fn visualizer_ui_impl(
                             })
                             .with_always_show_buttons(true),
                         );
+
+                    if let Some(error_string) = error_string {
+                        ui.error_label(error_string);
+                    }
+
                     visualizer_components(ctx, ui, data_result, visualizer);
                 } else {
                     ui.list_item_flat_noninteractive(

crates/viewer/re_test_viewport/Cargo.toml
@@ -16,7 +16,6 @@ workspace = true
 
 [dependencies]
 re_log_types.workspace = true
-re_renderer.workspace = true
 re_test_context.workspace = true
 re_types.workspace = true
 re_ui.workspace = true

crates/viewer/re_test_viewport/src/test_view.rs
@@ -5,7 +5,7 @@ use re_viewer_context::external::re_chunk_store::external::re_chunk;
 
 use re_viewer_context::{
     IdentifiedViewSystem, ViewClass, ViewSpawnHeuristics, ViewState, ViewerContext,
-    VisualizerQueryInfo, VisualizerSystem, suggest_view_for_each_entity,
+    VisualizerExecutionOutput, VisualizerQueryInfo, VisualizerSystem, suggest_view_for_each_entity,
 };
 
 #[derive(Default)]
@@ -36,9 +36,8 @@ impl VisualizerSystem for TestSystem {
         _ctx: &re_viewer_context::ViewContext<'_>,
         _query: &re_viewer_context::ViewQuery<'_>,
         _context_systems: &re_viewer_context::ViewContextCollection,
-    ) -> Result<Vec<re_renderer::QueueableDrawData>, re_viewer_context::ViewSystemExecutionError>
-    {
-        Ok(Vec::new())
+    ) -> Result<VisualizerExecutionOutput, re_viewer_context::ViewSystemExecutionError> {
+        Ok(VisualizerExecutionOutput::default())
     }
 
     fn as_any(&self) -> &dyn std::any::Any {

crates/viewer/re_view_bar_chart/Cargo.toml
@@ -22,7 +22,6 @@ all-features = true
 re_chunk_store.workspace = true
 re_entity_db.workspace = true
 re_log_types.workspace = true
-re_renderer.workspace = true
 re_view.workspace = true
 re_tracing.workspace = true
 re_types = { workspace = true, features = ["egui_plot"] }

commit 8a65f789e9282eba82737279a1963c83db4b910d

Fix transparency in 2D views (#12214)
* Closes https://github.com/rerun-io/rerun/issues/12023
* Closes https://github.com/rerun-io/rerun/issues/12206
* Closes https://github.com/rerun-io/rerun/issues/11156
* Closes RR-3179
* Closes RR-3057
* Bug introduced in https://github.com/rerun-io/rerun/pull/11962

~I don't understand _why_ this PR fixes the problem, but it does.~

----

@Wumpf : reverted the original fix and applied a more forward looking
fix in the re_renderer: we're now doing the same transparency handling
on `re_renderer::Rectangle` (the underlying primitive) that we do on
other primitives like meshes. I previously assumed that this isn't
necessary because we sort the rectangle draw data, was apparently wrong
either way.

The situation is still a bit weird because we still rely on depth based
draw ordering - our draw order gets translated into a depth offset. With
this change, the ordering for transparent objects take care of it. But
drawbacks with this approach remain:
* depth offset is inaccurate and causes glitches especially on web. This
is probably reduced now since we no longer draw transparent rectangles
with depth (which we did before, relying on their correct ordering). See
#4787
* draw order doesn't always interact correctly between opaque and
non-opaque rectangles
* draw order doesn't always interact correctly between rectangles and
other things with draw order

Solution to all these problems is to have a special 2D draw phase in
which we don't rely on the depth buffer at all but exclusively on an
explicit draw order (which the Viewer already has for 2D objects!).
Other related issues: #1025, #11156

---------

Co-authored-by: Gijs de Jong <berichtaangijs@gmail.com>
Co-authored-by: Andreas Reich <r_andreas2@web.de>

crates/viewer/re_renderer/src/mesh.rs
@@ -385,7 +385,7 @@ impl GpuMesh {
                     },
                 );
 
-                // TODO(andreas): handle texture transparency
+                // TODO(#12223): handle texture transparency
                 let is_transparent = material.albedo_factor.a() < 1.0;
 
                 materials.push(GpuMaterial {

crates/viewer/re_renderer/src/renderer/rectangles.rs
@@ -405,6 +405,7 @@ struct RectangleInstance {
     center_position: glam::Vec3A,
     bind_group: GpuBindGroup,
     draw_outline_mask: bool,
+    has_transparency: bool,
 }
 
 #[derive(Clone)]
@@ -420,12 +421,17 @@ impl DrawData for RectangleDrawData {
         view_info: &DrawableCollectionViewInfo,
         collector: &mut DrawableCollector<'_>,
     ) {
-        // TODO(#1025, #4787, #11156): Better handling of 2D objects, use per-2D layer sorting instead of depth offsets.
-        // This is extra hacky here since we actually have transparent objects, but putting them on the transparency layer messes with the
-        // 2D setup we have so far.
+        // TODO(#1025, #4787): Better handling of 2D objects, use per-2D layer sorting instead of depth offsets.
+        // For 2D draw order based sorting, we should never enable depth write and always perform back to front sorting.
 
         for (index, instance) in self.instances.iter().enumerate() {
-            let mut phases = DrawPhase::Opaque | DrawPhase::PickingLayer;
+            let mut phases = enumset::EnumSet::new();
+            phases.insert(if instance.has_transparency {
+                DrawPhase::Transparent
+            } else {
+                DrawPhase::Opaque
+            });
+            phases.insert(DrawPhase::PickingLayer);
             if instance.draw_outline_mask {
                 phases.insert(DrawPhase::OutlineMask);
             }
@@ -529,6 +535,7 @@ impl RectangleDrawData {
                     },
                 ),
                 draw_outline_mask: rectangle.options.outline_mask.is_some(),
+                has_transparency: rectangle.options.multiplicative_tint.a() < 1.0, // TODO(#12223): what about textures with alpha?
             });
         }
 
@@ -537,7 +544,8 @@ impl RectangleDrawData {
 }
 
 pub struct RectangleRenderer {
-    render_pipeline_color: GpuRenderPipelineHandle,
+    render_pipeline_color_opaque: GpuRenderPipelineHandle,
+    render_pipeline_color_transparent: GpuRenderPipelineHandle,
     render_pipeline_picking_layer: GpuRenderPipelineHandle,
     render_pipeline_outline_mask: GpuRenderPipelineHandle,
     bind_group_layout: GpuBindGroupLayoutHandle,
@@ -636,20 +644,15 @@ impl Renderer for RectangleRenderer {
             &include_shader_module!("../../shader/rectangle_fs.wgsl"),
         );
 
-        let render_pipeline_desc_color = RenderPipelineDesc {
-            label: "RectangleRenderer::render_pipeline_color".into(),
+        let render_pipeline_desc_color_opaque = RenderPipelineDesc {
+            label: "RectangleRenderer::render_pipeline_color_opaque".into(),
             pipeline_layout,
             vertex_entrypoint: "vs_main".into(),
             vertex_handle: shader_module_vs,
             fragment_entrypoint: "fs_main".into(),
             fragment_handle: shader_module_fs,
             vertex_buffers: smallvec![],
-            render_targets: smallvec![Some(wgpu::ColorTargetState {
-                format: ViewBuilder::MAIN_TARGET_COLOR_FORMAT,
-                // TODO(andreas): have two render pipelines, an opaque one and a transparent one. Transparent shouldn't write depth!
-                blend: Some(wgpu::BlendState::PREMULTIPLIED_ALPHA_BLENDING),
-                write_mask: wgpu::ColorWrites::ALL,
-            })],
+            render_targets: smallvec![Some(ViewBuilder::MAIN_TARGET_COLOR_FORMAT.into())],
             primitive: wgpu::PrimitiveState {
                 topology: wgpu::PrimitiveTopology::TriangleStrip,
                 cull_mode: None,
@@ -658,8 +661,26 @@ impl Renderer for RectangleRenderer {
             depth_stencil: Some(ViewBuilder::MAIN_TARGET_DEFAULT_DEPTH_STATE),
             multisample: ViewBuilder::main_target_default_msaa_state(ctx.render_config(), false),
         };
-        let render_pipeline_color =
-            render_pipelines.get_or_create(ctx, &render_pipeline_desc_color);
+        let render_pipeline_color_opaque =
+            render_pipelines.get_or_create(ctx, &render_pipeline_desc_color_opaque);
+
+        let render_pipeline_desc_color_transparent = RenderPipelineDesc {
+            label: "RectangleRenderer::render_pipeline_color_transparent".into(),
+            render_targets: smallvec![Some(wgpu::ColorTargetState {
+                format: ViewBuilder::MAIN_TARGET_COLOR_FORMAT,
+                blend: Some(wgpu::BlendState::PREMULTIPLIED_ALPHA_BLENDING),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleStrip,
+                cull_mode: None,
+                ..Default::default()
+            },
+            ..render_pipeline_desc_color_opaque.clone()
+        };
+        let render_pipeline_color_transparent =
+            render_pipelines.get_or_create(ctx, &render_pipeline_desc_color_transparent);
+
         let render_pipeline_picking_layer = render_pipelines.get_or_create(
             ctx,
             &(RenderPipelineDesc {
@@ -668,7 +689,7 @@ impl Renderer for RectangleRenderer {
                 render_targets: smallvec![Some(PickingLayerProcessor::PICKING_LAYER_FORMAT.into())],
                 depth_stencil: PickingLayerProcessor::PICKING_LAYER_DEPTH_STATE,
                 multisample: PickingLayerProcessor::PICKING_LAYER_MSAA_STATE,
-                ..render_pipeline_desc_color.clone()
+                ..render_pipeline_desc_color_opaque.clone()
             }),
         );
         let render_pipeline_outline_mask = render_pipelines.get_or_create(
@@ -679,12 +700,13 @@ impl Renderer for RectangleRenderer {
                 render_targets: smallvec![Some(OutlineMaskProcessor::MASK_FORMAT.into())],
                 depth_stencil: OutlineMaskProcessor::MASK_DEPTH_STATE,
                 multisample: OutlineMaskProcessor::mask_default_msaa_state(ctx.device_caps().tier),
-                ..render_pipeline_desc_color
+                ..render_pipeline_desc_color_opaque.clone()
             }),
         );
 
         Self {
-            render_pipeline_color,
+            render_pipeline_color_opaque,
+            render_pipeline_color_transparent,
             render_pipeline_picking_layer,
             render_pipeline_outline_mask,
             bind_group_layout,
@@ -701,7 +723,8 @@ impl Renderer for RectangleRenderer {
         re_tracing::profile_function!();
 
         let pipeline_handle = match phase {
-            DrawPhase::Opaque => self.render_pipeline_color,
+            DrawPhase::Transparent => self.render_pipeline_color_transparent,
+            DrawPhase::Opaque => self.render_pipeline_color_opaque,
             DrawPhase::PickingLayer => self.render_pipeline_picking_layer,
             DrawPhase::OutlineMask => self.render_pipeline_outline_mask,
             _ => unreachable!("We were called on a phase we weren't subscribed to: {phase:?}"),

crates/viewer/re_view_spatial/src/visualizers/encoded_image.rs
@@ -58,20 +58,6 @@ impl VisualizerSystem for EncodedImageVisualizer {
             },
         )?;
 
-        // TODO(#1025): draw order is translated to depth offset, which works fine for opaque images,
-        // but for everything with transparency, actual drawing order is still important.
-        // We mitigate this a bit by at least sorting the images within each other.
-        // Sorting of Images vs DepthImage vs SegmentationImage uses the fact that
-        // visualizers are executed in the order of their identifiers.
-        // -> The draw order is always DepthImage then Image then SegmentationImage,
-        //    which happens to be exactly what we want 🙈
-        self.data.pickable_rects.sort_by_key(|image| {
-            (
-                image.textured_rect.options.depth_offset,
-                egui::emath::OrderedFloat(image.textured_rect.options.multiplicative_tint.a()),
-            )
-        });
-
         Ok(output.with_draw_data([PickableTexturedRect::to_draw_data(
             ctx.viewer_ctx.render_ctx(),
             &self.data.pickable_rects,

crates/viewer/re_view_spatial/src/visualizers/images.rs
@@ -64,20 +64,6 @@ impl VisualizerSystem for ImageVisualizer {
             },
         )?;
 
-        // TODO(#1025, #11156): draw order is translated to depth offset, which works fine for opaque images,
-        // but for everything with transparency, actual drawing order is still important.
-        // We mitigate this a bit by at least sorting the images within each other.
-        // Sorting of Images vs DepthImage vs SegmentationImage uses the fact that
-        // visualizers are executed in the order of their identifiers.
-        // -> The draw order is always DepthImage then Image then SegmentationImage,
-        //    which happens to be exactly what we want 🙈
-        self.data.pickable_rects.sort_by_key(|image| {
-            (
-                image.textured_rect.options.depth_offset,
-                egui::emath::OrderedFloat(image.textured_rect.options.multiplicative_tint.a()),
-            )
-        });
-
         Ok(output.with_draw_data([PickableTexturedRect::to_draw_data(
             ctx.viewer_ctx.render_ctx(),
             &self.data.pickable_rects,

crates/viewer/re_view_spatial/src/visualizers/segmentation_images.rs
@@ -136,20 +136,6 @@ impl VisualizerSystem for SegmentationImageVisualizer {
             },
         )?;
 
-        // TODO(#1025): draw order is translated to depth offset, which works fine for opaque images,
-        // but for everything with transparency, actual drawing order is still important.
-        // We mitigate this a bit by at least sorting the segmentation images within each other.
-        // Sorting of Images vs DepthImage vs SegmentationImage uses the fact that
-        // visualizers are executed in the order of their identifiers.
-        // -> The draw order is always DepthImage then Image then SegmentationImage,
-        //    which happens to be exactly what we want 🙈
-        self.data.pickable_rects.sort_by_key(|image| {
-            (
-                image.textured_rect.options.depth_offset,
-                egui::emath::OrderedFloat(image.textured_rect.options.multiplicative_tint.a()),
-            )
-        });
-
         Ok(output.with_draw_data([PickableTexturedRect::to_draw_data(
             ctx.viewer_ctx.render_ctx(),
             &self.data.pickable_rects,

crates/viewer/re_view_spatial/tests/opacity_2d.rs
@@ -1,0 +1,84 @@
+use re_chunk_store::RowId;
+use re_log_types::TimePoint;
+use re_test_context::TestContext;
+use re_test_viewport::TestContextExt as _;
+use re_viewer_context::{ViewClass as _, ViewId};
+use re_viewport_blueprint::ViewBlueprint;
+
+#[test]
+pub fn test_opacity_2d() {
+    let mut test_context = TestContext::new_with_view_class::<re_view_spatial::SpatialView2D>();
+
+    // Tests
+    // * the heuristics (the RGB should become transparent)
+    // * actual opacity
+
+    {
+        use ndarray::{Array, ShapeBuilder as _, s};
+
+        test_context.log_entity("2d_layering/gradient", |builder| {
+            let (width, height) = (100, 50);
+            let mut image = Array::<u8, _>::zeros((height, width, 3).f());
+            image.slice_mut(s![.., .., 0]).fill(0);
+            image.slice_mut(s![.., .., 1]).fill(255);
+            image.slice_mut(s![.., .., 2]).fill(127);
+
+            builder.with_archetype(
+                RowId::new(),
+                TimePoint::default(),
+                &re_sdk_types::archetypes::Image::from_color_model_and_tensor(
+                    re_sdk_types::datatypes::ColorModel::RGB,
+                    image,
+                )
+                .unwrap()
+                .with_draw_order(1.0),
+            )
+        });
+
+        test_context.log_entity("2d_layering/depth", |builder| {
+            let (width, height) = (50, 100);
+            let mut image = Array::<u16, _>::zeros((height, width).f());
+            image.slice_mut(s![.., ..]).fill(255);
+
+            let (vec, offset) = image.into_raw_vec_and_offset();
+            assert_eq!(offset, Some(0));
+
+            builder.with_archetype(
+                RowId::new(),
+                TimePoint::default(),
+                &re_sdk_types::archetypes::DepthImage::from_gray16(
+                    bytemuck::cast_slice::<u16, u8>(&vec),
+                    [width as _, height as _],
+                ),
+            )
+        });
+    }
+
+    let view_id = test_context.setup_viewport_blueprint(|_ctx, blueprint| {
+        blueprint.add_view_at_root(ViewBlueprint::new_with_root_wildcard(
+            re_view_spatial::SpatialView2D::identifier(),
+        ))
+    });
+    run_view_ui_and_save_snapshot(
+        &test_context,
+        view_id,
+        "opacity_2d",
+        egui::vec2(100.0, 100.0) * 2.0,
+    );
+}
+
+fn run_view_ui_and_save_snapshot(
+    test_context: &TestContext,
+    view_id: ViewId,
+    name: &str,
+    size: egui::Vec2,
+) {
+    let mut harness = test_context
+        .setup_kittest_for_rendering_3d(size)
+        .build_ui(|ui| {
+            test_context.run_with_single_view(ui, view_id);
+        });
+
+    harness.run();
+    harness.snapshot(name);
+}

crates/viewer/re_view_spatial/tests/snapshots/opacity_2d.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7e6308f9e0c42023d3e37da89fd34698e4df0de240b0fd5d5b493776eccab3d9
+size 1466