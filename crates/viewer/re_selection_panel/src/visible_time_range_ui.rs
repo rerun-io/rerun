rerun-io/rerun

commit 62019c5f7c5985c2daa586d680ea6e3a28cb7d46

Fix pending timeline (#12186)
### Related

* Introduced in https://github.com/rerun-io/rerun/pull/12172

### What

Fixes the timeline from pending timeline being used directly.

---------

Co-authored-by: Emil Ernerfeldt <emil.ernerfeldt@gmail.com>

crates/viewer/re_selection_panel/src/visible_time_range_ui.rs
@@ -22,6 +22,10 @@ pub fn visible_time_range_ui_for_view(
     if !view_class.supports_visible_time_range() {
         return;
     }
+    let Some(timeline) = ctx.time_ctrl.timeline() else {
+        ui.weak("No active timeline");
+        return;
+    };
 
     let property_path = entity_path_for_view_property(
         view.id,
@@ -32,7 +36,7 @@ pub fn visible_time_range_ui_for_view(
     let query_range = view.query_range(
         ctx.store_context.blueprint,
         ctx.blueprint_query,
-        ctx.time_ctrl.timeline(),
+        timeline,
         ctx.view_class_registry(),
         view_state,
     );
@@ -71,7 +75,7 @@ fn visible_time_range_ui(
         )
         .unwrap_or_default();
 
-    let timeline_name = *ctx.time_ctrl.timeline().name();
+    let timeline_name = *ctx.time_ctrl.timeline_name();
     let mut has_individual_range = visible_time_ranges
         .iter()
         .any(|range| range.timeline.as_str() == timeline_name.as_str());
@@ -159,7 +163,10 @@ fn query_range_ui(
     is_view: bool,
 ) {
     let time_ctrl = &ctx.time_ctrl;
-    let timeline = *time_ctrl.timeline();
+    let Some(&timeline) = time_ctrl.timeline() else {
+        ui.weak("No active timeline");
+        return;
+    };
     let time_type = timeline.typ();
 
     let markdown = "# Visible time range\n
@@ -190,7 +197,7 @@ Notes:
                     });
             });
             let time_drag_value =
-                if let Some(range) = ctx.recording().time_range_for(time_ctrl.timeline().name()) {
+                if let Some(range) = ctx.recording().time_range_for(time_ctrl.timeline_name()) {
                     TimeDragValue::from_abs_time_range(range)
                 } else {
                     TimeDragValue::from_time_range(0..=0)

crates/viewer/re_test_viewport/src/lib.rs
@@ -110,7 +110,7 @@ impl TestContextExt for TestContext {
                             resolver.update_overrides(
                                 ctx.store_context.blueprint,
                                 ctx.blueprint_query,
-                                ctx.time_ctrl.timeline(),
+                                ctx.time_ctrl.timeline().expect("Should have a valid timeline"),
                                 class_registry,
                                 &mut data_query_result,
                                 self.view_states.lock().get_mut_or_create(*view_id, class),

crates/viewer/re_viewer/src/app_state.rs
@@ -187,7 +187,7 @@ impl AppState {
         // is there an active loop selection?
         time_ctrl
             .time_selection()
-            .map(|q| (*time_ctrl.timeline().name(), q))
+            .map(|q| (*time_ctrl.timeline_name(), q))
     }
 
     #[expect(clippy::too_many_arguments)]
@@ -908,14 +908,16 @@ fn update_overrides(
                     ctx.indicated_entities_per_visualizer,
                 );
 
-                resolver.update_overrides(
-                    ctx.store_context.blueprint,
-                    ctx.blueprint_query,
-                    ctx.time_ctrl.timeline(),
-                    ctx.view_class_registry,
-                    &mut query_result,
-                    view_state,
-                );
+                if let Some(timeline) = ctx.time_ctrl.timeline() {
+                    resolver.update_overrides(
+                        ctx.store_context.blueprint,
+                        ctx.blueprint_query,
+                        timeline,
+                        ctx.view_class_registry,
+                        &mut query_result,
+                        view_state,
+                    );
+                }
 
                 (view.id, query_result)
             },

crates/viewer/re_viewport_blueprint/src/view.rs
@@ -767,14 +767,16 @@ mod tests {
                     .expect("view class should be registered"),
             );
 
-            resolver.update_overrides(
-                ctx.blueprint_db(),
-                ctx.blueprint_query,
-                ctx.time_ctrl.timeline(),
-                ctx.view_class_registry(),
-                &mut query_result,
-                view_state,
-            );
+            if let Some(timeline) = ctx.time_ctrl.timeline() {
+                resolver.update_overrides(
+                    ctx.blueprint_db(),
+                    ctx.blueprint_query,
+                    timeline,
+                    ctx.view_class_registry(),
+                    &mut query_result,
+                    view_state,
+                );
+            }
 
             result = Some(query_result.clone());
         });

crates/viewer/re_test_context/src/lib.rs
@@ -414,8 +414,8 @@ impl TestContext {
     }
 
     /// Timeline the recording config is using by default.
-    pub fn active_timeline(&self) -> re_chunk::Timeline {
-        *self.time_ctrl.read().timeline()
+    pub fn active_timeline(&self) -> Option<re_chunk::Timeline> {
+        self.time_ctrl.read().timeline().copied()
     }
 
     pub fn active_blueprint(&mut self) -> &mut EntityDb {

crates/viewer/re_component_ui/src/time_range.rs
@@ -14,11 +14,13 @@ pub fn time_range_multiline_edit_or_view_ui(
     ui: &mut egui::Ui,
     value: &mut MaybeMutRef<'_, TimeRange>,
 ) -> egui::Response {
-    let time_type = ctx.time_ctrl.time_type();
+    let Some(time_type) = ctx.time_ctrl.time_type() else {
+        return ui.weak("No active timeline");
+    };
 
     let time_drag_value = if let Some(range) = ctx
         .recording()
-        .time_range_for(ctx.time_ctrl.timeline().name())
+        .time_range_for(ctx.time_ctrl.timeline_name())
     {
         TimeDragValue::from_abs_time_range(range)
     } else {
@@ -103,9 +105,13 @@ pub fn time_range_singleline_view_ui(
     ui: &mut egui::Ui,
     value: &mut MaybeMutRef<'_, TimeRange>,
 ) -> egui::Response {
+    let Some(time_type) = ctx.time_ctrl.time_type() else {
+        return ui.weak("No active timeline");
+    };
+
     let time_drag_value = if let Some(range) = ctx
         .recording()
-        .time_range_for(ctx.time_ctrl.timeline().name())
+        .time_range_for(ctx.time_ctrl.timeline_name())
     {
         TimeDragValue::from_abs_time_range(range)
     } else {
@@ -118,8 +124,6 @@ pub fn time_range_singleline_view_ui(
             .unwrap_or_default()
             .at_least(*time_drag_value.range.start()),
     ); // accounts for static time (TimeInt::MIN)
-
-    let time_type = ctx.time_ctrl.time_type();
 
     let (text, on_hover) = relative_time_range_label_text(
         current_time,

crates/viewer/re_data_ui/src/item_ui.rs
@@ -514,7 +514,7 @@ pub fn timeline_button_to(
     text: impl Into<egui::WidgetText>,
     timeline_name: &TimelineName,
 ) -> egui::Response {
-    let is_selected = ctx.time_ctrl.timeline().name() == timeline_name;
+    let is_selected = ctx.time_ctrl.timeline_name() == timeline_name;
 
     let response = ui
         .selectable_label(is_selected, text)

crates/viewer/re_data_ui/src/transform_frames.rs
@@ -77,7 +77,7 @@ impl TransformFramesUi {
         });
 
         let frame_ids = transform_cache.frame_id_registry();
-        let transforms = transform_cache.transforms_for_timeline(*ctx.time_ctrl.timeline().name());
+        let transforms = transform_cache.transforms_for_timeline(*ctx.time_ctrl.timeline_name());
 
         let mut frames = Vec::new();
 

commit 819335b24925d82e1ea8ded749c97bf473c2b66e

Fix skipping updating overrides if no timeline is active (#12250)
### Related

* Fixes bug introduced in https://github.com/rerun-io/rerun/pull/12186

### What

At the start `QueryRange` is assigned to default, with the expectation
that it should be overriden before used. But if the current timeline is
pending we currently skipped the update that would override it. Causing
time series view to receive an unexpected `QueryRange::LatestAt`

crates/viewer/re_selection_panel/src/visible_time_range_ui.rs
@@ -22,10 +22,6 @@ pub fn visible_time_range_ui_for_view(
     if !view_class.supports_visible_time_range() {
         return;
     }
-    let Some(timeline) = ctx.time_ctrl.timeline() else {
-        ui.weak("No active timeline");
-        return;
-    };
 
     let property_path = entity_path_for_view_property(
         view.id,
@@ -36,7 +32,7 @@ pub fn visible_time_range_ui_for_view(
     let query_range = view.query_range(
         ctx.store_context.blueprint,
         ctx.blueprint_query,
-        timeline,
+        ctx.time_ctrl.timeline(),
         ctx.view_class_registry(),
         view_state,
     );

crates/viewer/re_test_viewport/src/lib.rs
@@ -110,7 +110,7 @@ impl TestContextExt for TestContext {
                             resolver.update_overrides(
                                 ctx.store_context.blueprint,
                                 ctx.blueprint_query,
-                                ctx.time_ctrl.timeline().expect("Should have a valid timeline"),
+                                ctx.time_ctrl.timeline(),
                                 class_registry,
                                 &mut data_query_result,
                                 self.view_states.lock().get_mut_or_create(*view_id, class),

crates/viewer/re_view_time_series/src/util.rs
@@ -43,10 +43,12 @@ pub fn determine_time_range(
     let visible_time_range = match query_range {
         re_viewer_context::QueryRange::TimeRange(time_range) => time_range.clone(),
         re_viewer_context::QueryRange::LatestAt => {
-            re_log::error_once!(
-                "Unexpected LatestAt query for time series data result at path {:?}",
-                data_result.entity_path
-            );
+            if cfg!(debug_assertions) {
+                re_log::error_once!(
+                    "[DEBUG] Unexpected LatestAt query for time series data result at path {:?}",
+                    data_result.entity_path
+                );
+            }
             TimeRange {
                 start: TimeRangeBoundary::AT_CURSOR,
                 end: TimeRangeBoundary::AT_CURSOR,

crates/viewer/re_viewer/src/app_state.rs
@@ -908,16 +908,14 @@ fn update_overrides(
                     ctx.indicated_entities_per_visualizer,
                 );
 
-                if let Some(timeline) = ctx.time_ctrl.timeline() {
-                    resolver.update_overrides(
-                        ctx.store_context.blueprint,
-                        ctx.blueprint_query,
-                        timeline,
-                        ctx.view_class_registry,
-                        &mut query_result,
-                        view_state,
-                    );
-                }
+                resolver.update_overrides(
+                    ctx.store_context.blueprint,
+                    ctx.blueprint_query,
+                    ctx.time_ctrl.timeline(),
+                    ctx.view_class_registry,
+                    &mut query_result,
+                    view_state,
+                );
 
                 (view.id, query_result)
             },

crates/viewer/re_viewport_blueprint/src/view.rs
@@ -384,7 +384,7 @@ impl ViewBlueprint {
         &self,
         blueprint: &EntityDb,
         blueprint_query: &LatestAtQuery,
-        active_timeline: &Timeline,
+        active_timeline: Option<&Timeline>,
         view_class_registry: &ViewClassRegistry,
         view_state: &dyn ViewState,
     ) -> QueryRange {
@@ -403,12 +403,14 @@ impl ViewBlueprint {
             blueprint_archetypes::VisibleTimeRanges::descriptor_ranges().component,
         );
 
-        let time_range = ranges.ok().flatten().and_then(|ranges| {
+        let time_range = active_timeline.and_then(|active_timeline| {
             ranges
+                .ok()??
                 .iter()
                 .find(|range| range.timeline.as_str() == active_timeline.name().as_str())
                 .map(|range| range.range.clone())
         });
+
         time_range.map_or_else(
             || {
                 let view_class = view_class_registry.get_class_or_log_error(self.class_identifier);
@@ -767,16 +769,14 @@ mod tests {
                     .expect("view class should be registered"),
             );
 
-            if let Some(timeline) = ctx.time_ctrl.timeline() {
-                resolver.update_overrides(
-                    ctx.blueprint_db(),
-                    ctx.blueprint_query,
-                    timeline,
-                    ctx.view_class_registry(),
-                    &mut query_result,
-                    view_state,
-                );
-            }
+            resolver.update_overrides(
+                ctx.blueprint_db(),
+                ctx.blueprint_query,
+                ctx.time_ctrl.timeline(),
+                ctx.view_class_registry(),
+                &mut query_result,
+                view_state,
+            );
 
             result = Some(query_result.clone());
         });

crates/viewer/re_viewport_blueprint/src/view_contents.rs
@@ -479,7 +479,7 @@ impl<'a> DataQueryPropertyResolver<'a> {
         &self,
         blueprint: &EntityDb,
         blueprint_query: &LatestAtQuery,
-        active_timeline: &Timeline,
+        active_timeline: Option<&Timeline>,
         query_result: &mut DataQueryResult,
         handle: DataResultHandle,
         default_query_range: &QueryRange,
@@ -556,8 +556,11 @@ impl<'a> DataQueryPropertyResolver<'a> {
                             == blueprint_archetypes::VisibleTimeRanges::descriptor_ranges()
                                 .component
                         {
-                            if let Ok(visible_time_ranges) =
-                                blueprint_components::VisibleTimeRange::from_arrow(&component_data)
+                            if let Some(active_timeline) = active_timeline
+                                && let Ok(visible_time_ranges) =
+                                    blueprint_components::VisibleTimeRange::from_arrow(
+                                        &component_data,
+                                    )
                                 && let Some(time_range) = visible_time_ranges.iter().find(|range| {
                                     range.timeline.as_str() == active_timeline.name().as_str()
                                 })
@@ -618,7 +621,7 @@ impl<'a> DataQueryPropertyResolver<'a> {
         &self,
         blueprint: &EntityDb,
         blueprint_query: &LatestAtQuery,
-        active_timeline: &Timeline,
+        active_timeline: Option<&Timeline>,
         view_class_registry: &ViewClassRegistry,
         query_result: &mut DataQueryResult,
         view_state: &dyn ViewState,