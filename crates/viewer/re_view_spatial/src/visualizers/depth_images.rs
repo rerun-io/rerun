rerun-io/rerun

commit c8d169b237fcf9fc72942ee2a53d316d6f3635b6

Fix missing error when drawing depth image for point cloud without pinhole (#12246)

crates/viewer/re_view_spatial/src/visualizers/depth_images.rs
@@ -15,7 +15,6 @@ use re_viewer_context::{
 use super::entity_iterator::process_archetype;
 use super::{SpatialViewVisualizerData, textured_rect_from_image};
 use crate::contexts::{SpatialSceneEntityContext, TransformTreeContext};
-use crate::view_kind::SpatialViewKind;
 use crate::visualizers::first_copied;
 use crate::{PickableRectSourceData, PickableTexturedRect, SpatialView3D};
 
@@ -35,7 +34,7 @@ pub struct DepthImageVisualizer {
 impl Default for DepthImageVisualizer {
     fn default() -> Self {
         Self {
-            data: SpatialViewVisualizerData::new(Some(SpatialViewKind::TwoD)),
+            data: SpatialViewVisualizerData::new(None),
             depth_cloud_entities: IntMap::default(),
         }
     }
@@ -52,12 +51,12 @@ pub struct DepthImageComponentData {
 #[expect(clippy::too_many_arguments)]
 pub fn process_depth_image_data(
     ctx: &QueryContext<'_>,
-    ent_context: &SpatialSceneEntityContext<'_>,
+    ent_context: &mut SpatialSceneEntityContext<'_>,
     data_store: &mut SpatialViewVisualizerData,
     depth_cloud_entities: &mut IntMap<EntityPathHash, DepthImageProcessResult>,
     depth_clouds: &mut Vec<DepthCloud>,
     transforms: &TransformTreeContext,
-    data: DepthImageComponentData,
+    component_data: DepthImageComponentData,
     archetype_name: ArchetypeName,
     depth_meter_identifier: ComponentIdentifier,
     colormap_identifier: ComponentIdentifier,
@@ -71,7 +70,7 @@ pub fn process_depth_image_data(
         fill_ratio,
         colormap,
         value_range,
-    } = data;
+    } = component_data;
 
     let depth_meter =
         depth_meter.unwrap_or_else(|| typed_fallback_for(ctx, depth_meter_identifier));
@@ -96,7 +95,7 @@ pub fn process_depth_image_data(
     // First try to create a textured rect for this image.
     // Even if we end up only showing a depth cloud,
     // we still need most of this for ui interaction which still shows the image!
-    let Some(textured_rect) = textured_rect_from_image(
+    let textured_rect = match textured_rect_from_image(
         ctx.viewer_ctx(),
         entity_path,
         ent_context,
@@ -104,58 +103,71 @@ pub fn process_depth_image_data(
         Some(&colormap_with_range),
         re_renderer::Rgba::WHITE,
         archetype_name,
-    ) else {
-        // If we can't create a textured rect from this, we don't have to bother with clouds either.
-        return;
+    ) {
+        Ok(textured_rect) => textured_rect,
+        Err(err) => {
+            ent_context
+                .output
+                .report_error_for(ctx.target_entity_path.clone(), re_error::format(err));
+
+            // If we can't create a textured rect from this, we don't have to bother with clouds either.
+            return;
+        }
     };
 
-    if is_3d_view
-        && let Some(pinhole_tree_root_info) =
+    if is_3d_view {
+        // In 3D views we should show depth images as a depth cloud and no textured rect.
+        // For that we need a pinhole at or above that entity in the transform tree.
+        if let Some(pinhole_tree_root_info) =
             transforms.pinhole_tree_root_info(ent_context.transform_info.tree_root())
-    {
-        let fill_ratio = fill_ratio.unwrap_or_default();
+        {
+            let fill_ratio = fill_ratio.unwrap_or_default();
 
-        // NOTE: we don't pass in `world_from_obj` because this corresponds to the
-        // transform of the projection plane, which is of no use to us here.
-        // What we want are the extrinsics of the depth camera!
-        let cloud = process_entity_view_as_depth_cloud(
-            ent_context,
-            entity_path,
-            pinhole_tree_root_info,
-            depth_meter,
-            fill_ratio,
-            &textured_rect.colormapped_texture,
-        );
-        data_store.add_bounding_box(
-            entity_path.hash(),
-            cloud.world_space_bbox(),
-            glam::Affine3A::IDENTITY,
-        );
-        depth_cloud_entities.insert(
-            entity_path.hash(),
-            DepthImageProcessResult {
-                image_info,
+            // NOTE: we don't pass in `world_from_obj` because this corresponds to the
+            // transform of the projection plane, which is of no use to us here.
+            // What we want are the extrinsics of the depth camera!
+            let cloud = process_entity_view_as_depth_cloud(
+                ent_context,
+                entity_path,
+                pinhole_tree_root_info,
                 depth_meter,
-                colormap: textured_rect.colormapped_texture,
+                fill_ratio,
+                &textured_rect.colormapped_texture,
+            );
+            data_store.add_bounding_box(
+                entity_path.hash(),
+                cloud.world_space_bbox(),
+                glam::Affine3A::IDENTITY,
+            );
+            depth_cloud_entities.insert(
+                entity_path.hash(),
+                DepthImageProcessResult {
+                    image_info,
+                    depth_meter,
+                    colormap: textured_rect.colormapped_texture,
+                },
+            );
+            depth_clouds.push(cloud);
+        } else {
+            ent_context.output.report_error_for(
+                ctx.target_entity_path.clone(),
+                "Cannot draw depth image as 3D point cloud since it is not under a pinhole camera."
+                    .to_owned(),
+            );
+        }
+    } else {
+        data_store.add_pickable_rect(
+            PickableTexturedRect {
+                ent_path: entity_path.clone(),
+                textured_rect,
+                source_data: PickableRectSourceData::Image {
+                    image: image_info,
+                    depth_meter: Some(depth_meter),
+                },
             },
+            ent_context.view_class_identifier,
         );
-        depth_clouds.push(cloud);
-
-        // Skip creating a textured rect.
-        return;
     }
-
-    data_store.add_pickable_rect(
-        PickableTexturedRect {
-            ent_path: entity_path.clone(),
-            textured_rect,
-            source_data: PickableRectSourceData::Image {
-                image: image_info,
-                depth_meter: Some(depth_meter),
-            },
-        },
-        ent_context.view_class_identifier,
-    );
 }
 
 fn process_entity_view_as_depth_cloud(

crates/viewer/re_view_spatial/src/visualizers/encoded_depth_image.rs
@@ -18,7 +18,6 @@ use super::{
 };
 use crate::{
     contexts::TransformTreeContext,
-    view_kind::SpatialViewKind,
     visualizers::{
         depth_images::{DepthImageProcessResult, populate_depth_visualizer_execution_result},
         first_copied,
@@ -35,7 +34,7 @@ pub struct EncodedDepthImageVisualizer {
 impl Default for EncodedDepthImageVisualizer {
     fn default() -> Self {
         Self {
-            data: SpatialViewVisualizerData::new(Some(SpatialViewKind::TwoD)),
+            data: SpatialViewVisualizerData::new(None),
             depth_cloud_entities: Default::default(),
         }
     }

crates/viewer/re_view_spatial/tests/snapshots/topology_errors__3d_view_at_pinhole.snap
@@ -4,9 +4,9 @@ expression: visualizer_errors
 ---
 PerVisualizer(
     {
-        "Points3D": PerEntity(
+        "Image": PerEntity(
             {
-                /points3d_entity: "The origin of the 3D view (\"pinhole\") is under pinhole projection which is not supported by most 3D visualizations.",
+                /misplaced_image_entity: "The origin of the 3D view (\"pinhole\") is under pinhole projection which is not supported by most 3D visualizations.",
             },
         ),
         "Ellipsoids3D": PerEntity(
@@ -19,9 +19,19 @@ PerVisualizer(
                 /misplaced_boxes3d_entity: "The origin of the 3D view (\"pinhole\") is under pinhole projection which is not supported by most 3D visualizations.",
             },
         ),
+        "Points3D": PerEntity(
+            {
+                /points3d_entity: "The origin of the 3D view (\"pinhole\") is under pinhole projection which is not supported by most 3D visualizations.",
+            },
+        ),
         "Points2D": PerEntity(
             {
                 /points2d_entity: "The origin of the 3D view (\"pinhole\") is under pinhole projection which is not supported by most 3D visualizations.",
+            },
+        ),
+        "DepthImage": PerEntity(
+            {
+                /misplaced_depth_image_entity: "Cannot draw depth image as 3D point cloud since it is not under a pinhole camera.",
             },
         ),
     },

crates/viewer/re_view_spatial/tests/snapshots/topology_errors__3d_view_at_root.snap
@@ -4,6 +4,11 @@ expression: visualizer_errors
 ---
 PerVisualizer(
     {
+        "Image": PerEntity(
+            {
+                /misplaced_image_entity: "2D visualizers require a pinhole ancestor to be shown in a 3D view.",
+            },
+        ),
         "Ellipsoids3D": PerEntity(
             {
                 /disconnected_entity: "No transform path from \"disconnected\" to the view's origin frame (\"world\").",
@@ -12,6 +17,11 @@ PerVisualizer(
         "Boxes3D": PerEntity(
             {
                 /misplaced_boxes3d_entity: "Can't visualize 3D content that is under a pinhole projection.",
+            },
+        ),
+        "DepthImage": PerEntity(
+            {
+                /misplaced_depth_image_entity: "Cannot draw depth image as 3D point cloud since it is not under a pinhole camera.",
             },
         ),
     },

crates/viewer/re_view_spatial/src/visualizers/encoded_image.rs
@@ -78,7 +78,7 @@ impl EncodedImageVisualizer {
         &mut self,
         ctx: &QueryContext<'_>,
         results: &HybridResults<'_>,
-        spatial_ctx: &SpatialSceneEntityContext<'_>,
+        spatial_ctx: &mut SpatialSceneEntityContext<'_>,
     ) {
         use re_view::RangeResultsExt as _;
 
@@ -139,7 +139,7 @@ impl EncodedImageVisualizer {
                 re_renderer::Rgba::from_white_alpha(opacity.0.clamp(0.0, 1.0));
             let colormap = None;
 
-            if let Some(textured_rect) = textured_rect_from_image(
+            match textured_rect_from_image(
                 ctx.viewer_ctx(),
                 entity_path,
                 spatial_ctx,
@@ -148,17 +148,22 @@ impl EncodedImageVisualizer {
                 multiplicative_tint,
                 EncodedImage::name(),
             ) {
-                self.data.add_pickable_rect(
-                    PickableTexturedRect {
-                        ent_path: entity_path.clone(),
-                        textured_rect,
-                        source_data: PickableRectSourceData::Image {
-                            image,
-                            depth_meter: None,
+                Ok(textured_rect) => {
+                    self.data.add_pickable_rect(
+                        PickableTexturedRect {
+                            ent_path: entity_path.clone(),
+                            textured_rect,
+                            source_data: PickableRectSourceData::Image {
+                                image,
+                                depth_meter: None,
+                            },
                         },
-                    },
-                    spatial_ctx.view_class_identifier,
-                );
+                        spatial_ctx.view_class_identifier,
+                    );
+                }
+                Err(err) => spatial_ctx
+                    .output
+                    .report_error_for(entity_path.clone(), re_error::format(err)),
             }
         }
     }

crates/viewer/re_view_spatial/src/visualizers/images.rs
@@ -84,7 +84,7 @@ impl ImageVisualizer {
         &mut self,
         ctx: &QueryContext<'_>,
         results: &HybridResults<'_>,
-        spatial_ctx: &SpatialSceneEntityContext<'_>,
+        spatial_ctx: &mut SpatialSceneEntityContext<'_>,
     ) {
         use re_view::RangeResultsExt as _;
 
@@ -136,7 +136,7 @@ impl ImageVisualizer {
                 re_renderer::Rgba::from_white_alpha(opacity.0.clamp(0.0, 1.0));
             let colormap = None;
 
-            if let Some(textured_rect) = textured_rect_from_image(
+            match textured_rect_from_image(
                 ctx.viewer_ctx(),
                 entity_path,
                 spatial_ctx,
@@ -145,17 +145,24 @@ impl ImageVisualizer {
                 multiplicative_tint,
                 Image::name(),
             ) {
-                self.data.add_pickable_rect(
-                    PickableTexturedRect {
-                        ent_path: entity_path.clone(),
-                        textured_rect,
-                        source_data: PickableRectSourceData::Image {
-                            image,
-                            depth_meter: None,
+                Ok(textured_rect) => {
+                    self.data.add_pickable_rect(
+                        PickableTexturedRect {
+                            ent_path: entity_path.clone(),
+                            textured_rect,
+                            source_data: PickableRectSourceData::Image {
+                                image,
+                                depth_meter: None,
+                            },
                         },
-                    },
-                    spatial_ctx.view_class_identifier,
-                );
+                        spatial_ctx.view_class_identifier,
+                    );
+                }
+                Err(err) => {
+                    spatial_ctx
+                        .output
+                        .report_error_for(entity_path.clone(), re_error::format(err));
+                }
             }
         }
     }

crates/viewer/re_view_spatial/src/visualizers/segmentation_images.rs
@@ -109,7 +109,7 @@ impl VisualizerSystem for SegmentationImageVisualizer {
                         re_renderer::Rgba::from_white_alpha(opacity.0.clamp(0.0, 1.0));
                     let colormap = None;
 
-                    if let Some(textured_rect) = textured_rect_from_image(
+                    match textured_rect_from_image(
                         ctx.viewer_ctx(),
                         entity_path,
                         spatial_ctx,
@@ -118,20 +118,26 @@ impl VisualizerSystem for SegmentationImageVisualizer {
                         multiplicative_tint,
                         SegmentationImage::name(),
                     ) {
-                        self.data.add_pickable_rect(
-                            PickableTexturedRect {
-                                ent_path: entity_path.clone(),
-                                textured_rect,
-                                source_data: PickableRectSourceData::Image {
-                                    image,
-                                    depth_meter: None,
+                        Ok(textured_rect) => {
+                            self.data.add_pickable_rect(
+                                PickableTexturedRect {
+                                    ent_path: entity_path.clone(),
+                                    textured_rect,
+                                    source_data: PickableRectSourceData::Image {
+                                        image,
+                                        depth_meter: None,
+                                    },
                                 },
-                            },
-                            spatial_ctx.view_class_identifier,
-                        );
+                                spatial_ctx.view_class_identifier,
+                            );
+                        }
+                        Err(err) => {
+                            spatial_ctx
+                                .output
+                                .report_error_for(entity_path.clone(), re_error::format(err));
+                        }
                     }
                 }
-
                 Ok(())
             },
         )?;

crates/viewer/re_view_spatial/src/visualizers/utilities/textured_rect.rs
@@ -14,66 +14,58 @@ pub fn textured_rect_from_image(
     colormap: Option<&ColormapWithRange>,
     multiplicative_tint: egui::Rgba,
     archetype_name: ArchetypeName,
-) -> Option<renderer::TexturedRect> {
+) -> anyhow::Result<renderer::TexturedRect> {
     let debug_name = ent_path.to_string();
     let tensor_stats = ctx
         .store_context
         .caches
         .entry(|c: &mut ImageStatsCache| c.entry(image));
 
-    match gpu_bridge::image_to_gpu(
+    gpu_bridge::image_to_gpu(
         ctx.render_ctx(),
         &debug_name,
         image,
         &tensor_stats,
         &ent_context.annotations,
         colormap,
-    ) {
-        Ok(colormapped_texture) => {
-            // TODO(emilk): let users pick texture filtering.
-            // Always use nearest for magnification: let users see crisp individual pixels when they zoom
-            let texture_filter_magnification = renderer::TextureFilterMag::Nearest;
+    )
+    .map(|colormapped_texture| {
+        // TODO(emilk): let users pick texture filtering.
+        // Always use nearest for magnification: let users see crisp individual pixels when they zoom
+        let texture_filter_magnification = renderer::TextureFilterMag::Nearest;
 
-            // For minimization: we want a smooth linear (ideally mipmapped) filter for color images.
-            // Note that this filtering is done BEFORE applying the color map!
-            // For labeled/annotated/class_Id images we want nearest, because interpolating classes makes no sense.
-            // Interpolating depth images _can_ make sense, but can also produce weird artifacts when there are big jumps (0.1m -> 100m),
-            // so it's usually safer to turn off.
-            // The best heuristic is this: if there is a color map being applied, use nearest.
-            // TODO(emilk): apply filtering _after_ the color map?
-            let texture_filter_minification = if colormapped_texture.color_mapper.is_on() {
-                renderer::TextureFilterMin::Nearest
-            } else {
-                renderer::TextureFilterMin::Linear
-            };
+        // For minimization: we want a smooth linear (ideally mipmapped) filter for color images.
+        // Note that this filtering is done BEFORE applying the color map!
+        // For labeled/annotated/class_Id images we want nearest, because interpolating classes makes no sense.
+        // Interpolating depth images _can_ make sense, but can also produce weird artifacts when there are big jumps (0.1m -> 100m),
+        // so it's usually safer to turn off.
+        // The best heuristic is this: if there is a color map being applied, use nearest.
+        // TODO(emilk): apply filtering _after_ the color map?
+        let texture_filter_minification = if colormapped_texture.color_mapper.is_on() {
+            renderer::TextureFilterMin::Nearest
+        } else {
+            renderer::TextureFilterMin::Linear
+        };
 
-            let world_from_entity = ent_context
-                .transform_info
-                .single_transform_required_for_entity(ent_path, archetype_name)
-                .as_affine3a();
+        let world_from_entity = ent_context
+            .transform_info
+            .single_transform_required_for_entity(ent_path, archetype_name)
+            .as_affine3a();
 
-            let textured_rect = renderer::TexturedRect {
-                top_left_corner_position: world_from_entity.transform_point3(Vec3::ZERO),
-                extent_u: world_from_entity.transform_vector3(Vec3::X * image.width() as f32),
-                extent_v: world_from_entity.transform_vector3(Vec3::Y * image.height() as f32),
+        renderer::TexturedRect {
+            top_left_corner_position: world_from_entity.transform_point3(Vec3::ZERO),
+            extent_u: world_from_entity.transform_vector3(Vec3::X * image.width() as f32),
+            extent_v: world_from_entity.transform_vector3(Vec3::Y * image.height() as f32),
 
-                colormapped_texture,
+            colormapped_texture,
 
-                options: renderer::RectangleOptions {
-                    texture_filter_magnification,
-                    texture_filter_minification,
-                    multiplicative_tint,
-                    depth_offset: ent_context.depth_offset,
-                    outline_mask: ent_context.highlight.overall,
-                },
-            };
-
-            Some(textured_rect)
+            options: renderer::RectangleOptions {
+                texture_filter_magnification,
+                texture_filter_minification,
+                multiplicative_tint,
+                depth_offset: ent_context.depth_offset,
+                outline_mask: ent_context.highlight.overall,
+            },
         }
-
-        Err(err) => {
-            re_log::error_once!("Failed to create texture for {debug_name:?}: {err}");
-            None
-        }
-    }
+    })
 }

commit df270520542860de68d2d8b6e0712dad0b004059

Restore depth image visualizers to 2D view kind (fix heuristic) (#12290)
### Related

* Fixes small regression from #12246

### What

Fixes depth image visualizer view spawn heuristics having no preferred
view kind set by explicitly setting `SpatialViewKind::TwoD`, so they are
correctly classified as 2D again.

crates/viewer/re_view_spatial/src/visualizers/depth_images.rs
@@ -15,6 +15,7 @@ use re_viewer_context::{
 use super::entity_iterator::process_archetype;
 use super::{SpatialViewVisualizerData, textured_rect_from_image};
 use crate::contexts::{SpatialSceneEntityContext, TransformTreeContext};
+use crate::view_kind::SpatialViewKind;
 use crate::visualizers::first_copied;
 use crate::{PickableRectSourceData, PickableTexturedRect, SpatialView3D};
 
@@ -34,7 +35,7 @@ pub struct DepthImageVisualizer {
 impl Default for DepthImageVisualizer {
     fn default() -> Self {
         Self {
-            data: SpatialViewVisualizerData::new(None),
+            data: SpatialViewVisualizerData::new(Some(SpatialViewKind::TwoD)),
             depth_cloud_entities: IntMap::default(),
         }
     }

crates/viewer/re_view_spatial/src/visualizers/encoded_depth_image.rs
@@ -18,6 +18,7 @@ use super::{
 };
 use crate::{
     contexts::TransformTreeContext,
+    view_kind::SpatialViewKind,
     visualizers::{
         depth_images::{DepthImageProcessResult, populate_depth_visualizer_execution_result},
         first_copied,
@@ -34,7 +35,7 @@ pub struct EncodedDepthImageVisualizer {
 impl Default for EncodedDepthImageVisualizer {
     fn default() -> Self {
         Self {
-            data: SpatialViewVisualizerData::new(None),
+            data: SpatialViewVisualizerData::new(Some(SpatialViewKind::TwoD)),
             depth_cloud_entities: Default::default(),
         }
     }

crates/viewer/re_view_spatial/tests/snapshots/topology_errors__3d_view_at_pinhole.snap
@@ -31,7 +31,7 @@ PerVisualizer(
         ),
         "DepthImage": PerEntity(
             {
-                /misplaced_depth_image_entity: "Cannot draw depth image as 3D point cloud since it is not under a pinhole camera.",
+                /misplaced_depth_image_entity: "The origin of the 3D view (\"pinhole\") is under pinhole projection which is not supported by most 3D visualizations.",
             },
         ),
     },

crates/viewer/re_view_spatial/tests/snapshots/topology_errors__3d_view_at_root.snap
@@ -21,7 +21,7 @@ PerVisualizer(
         ),
         "DepthImage": PerEntity(
             {
-                /misplaced_depth_image_entity: "Cannot draw depth image as 3D point cloud since it is not under a pinhole camera.",
+                /misplaced_depth_image_entity: "2D visualizers require a pinhole ancestor to be shown in a 3D view.",
             },
         ),
     },