81296b6de80d6b3e632e3501c6805f8e6ede421b
Gijs de Jong <berichtaangijs@gmail.com>
2026-01-08 10:29:50 +0100

    Add main.py to `animated_urdf` example for execution

---
+++ examples/python/animated_urdf/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+An example of how to load and animate a URDF given some changing joint angles.
+
+Usage:
+python -m animated_urdf
+"""
+
+from __future__ import annotations
+
+from animated_urdf import main
+
+if __name__ == "__main__":
+    main()
