f5a741e28f70be4baacc8f9a4d2f2319916a8627
Isse <git@isse.rs>
2026-01-15 16:39:19 +0100

    Fix time point sharing for duration timelines (#12466)
    ### Related
    
    - Closes RR-3348
    
    ### What
    
    Fixes & cleans up time formatting in urls.

--- crates/store/re_uri/src/time_selection.rs
+++ crates/store/re_uri/src/time_selection.rs
@@ -9,6 +9,9 @@ pub struct TimeSelection {
     pub range: AbsoluteTimeRange,
 }
 
+// We shouldn't implement display for this as it's too ambiguous, instead create specific functions.
+static_assertions::assert_not_impl_any!(TimeSelection: std::fmt::Display);
+
 impl TimeSelection {
     pub fn format(&self, timestamp_format: re_log_types::TimestampFormat) -> String {
         format!(
@@ -16,6 +19,21 @@ impl TimeSelection {
             TimeCell::new(self.timeline.typ(), self.range.min).format_compact(timestamp_format),
             TimeCell::new(self.timeline.typ(), self.range.max).format_compact(timestamp_format),
         )
+    }
+
+    /// Special format which avoids forbidden & special characters in a url.
+    pub fn format_url(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self { timeline, range } = self;
+
+        let min = TimeCell::new(timeline.typ(), range.min());
+        let max = TimeCell::new(timeline.typ(), range.max());
+
+        let name = timeline.name();
+        write!(f, "{name}@")?;
+
+        min.format_url(f)?;
+        write!(f, "..")?;
+        max.format_url(f)
     }
 }
 
@@ -28,19 +46,6 @@ impl From<TimeSelection> for AbsoluteTimeRangeF {
 impl From<TimeSelection> for AbsoluteTimeRange {
     fn from(range: TimeSelection) -> Self {
         range.range
-    }
-}
-
-impl std::fmt::Display for TimeSelection {
-    /// Used for formatting time ranges in URLs
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self { timeline, range } = self;
-
-        let min = TimeCell::new(timeline.typ(), range.min());
-        let max = TimeCell::new(timeline.typ(), range.max());
-
-        let name = timeline.name();
-        write!(f, "{name}@{min}..{max}")
     }
 }
 
@@ -131,10 +136,18 @@ mod tests {
             ),
         ];
 
+        struct UrlFormat(TimeSelection);
+
+        impl std::fmt::Display for UrlFormat {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                self.0.format_url(f)
+            }
+        }
+
         for (string, selection) in test_cases {
             assert_eq!(TimeSelection::from_str(string), Ok(selection));
             assert_eq!(
-                TimeSelection::from_str(&selection.to_string()).unwrap(),
+                TimeSelection::from_str(&UrlFormat(selection).to_string()).unwrap(),
                 selection
             );
         }
