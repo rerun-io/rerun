8e9845d400dc65d2d72ec0ea698f7194eda71dcd
Antoine Beyeler <49431240+abey79@users.noreply.github.com>
2026-01-15 12:13:01 +0100

    Update `app-model.md` (#12413)
    Co-authored-by: Nick <24689722+ntjohnson1@users.noreply.github.com>

--- docs/snippets/all/concepts/how-does-rerun-work/log-to-grpc.py
+++ docs/snippets/all/concepts/how-does-rerun-work/log-to-grpc.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-
 import rerun as rr
 
-rr.init("rerun_example_native_sync")
+rr.init("rerun_example_log_to_grpc")
 
 # Connect to the Rerun gRPC server using the default address and url: rerun+http://localhost:9876/proxy
 rr.connect_grpc()
