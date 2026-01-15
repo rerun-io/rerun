rerun-io/rerun

commit 5862d3d02c5b88c83f1e0589b19e8af83a5f925a

Add documentation to dataframe query workflow and fix api rendering for bindings (#11650)
### Related

* Closes RR-2251

### What
* Primary task: first pass through query-data.md to give an overview of
how to start and connect to OSS server and get to a dataframe with
pointers to datafusion for follow on work
* Will fill an issue to make this into a snippet/tutorial to make
testable after we get a python object for the server
* Secondary task: Render docs for DataframeQueryView and lots of other
bindings exposed through catalog
* This scope creeped a lot so I just did the manual class list will file
a follow on ticket for proposal to fix this more globally
1. Add lint codes so I can separate out my ignores for pyclass_eq and
pyclass_module requirements
1. The lint.py changes (which aren't great since I'm not an experience
parser writer but don't seem awful)
3. Add a pyclass module requirement to specify
`rerun_bindings.rerun_bindings` see
https://github.com/rerun-io/rerun/pull/11268 for more context
      1. All the rust file changes 
   4. Fight mkdocs to render bindings properly
      1. The mkdocs related files
2. Adds the ability to generate our docs just off of the stubs without
the shared object being present at all. NOTE: The docs actually look
better this way, but I added some accommodation to make them look less
awful if the shared object is present.

Filed an issue to fix all the other missing classes from our rendered
api docs:
https://linear.app/rerun/issue/RR-2766/cover-remaining-classes-in-python-api-rendered-docs
and an issue with a proposal to avoid this problem moving forward:

https://linear.app/rerun/issue/RR-2765/change-python-public-api-surface-pattern-to-support-easier-docs

rerun_py/docs/gen_common_index.py
@@ -400,9 +400,22 @@ SECTION_TABLE: Final[list[Section]] = [
     ),
     Section(
         title="Catalog",
-        show_tables=False,
+        show_tables=True,
         mod_path="rerun.catalog",
         show_submodules=True,
+        class_list=[
+            "AlreadyExistsError",
+            "DataframeQueryView",
+            "DatasetEntry",
+            "CatalogClient",
+            "Entry",
+            "EntryId",
+            "EntryKind",
+            "NotFoundError",
+            "TableEntry",
+            "Task",
+            "VectorDistanceMetric",
+        ],
     ),
     Section(
         title="Utilities",
@@ -565,13 +578,25 @@ of Python, you can use the table below to make sure you choose the proper Rerun 
                         mod_tail = section.mod_path.split(".")[1:]
                         class_name = ".".join([*mod_tail, class_name])
                     cls = rerun_pkg[class_name]
+                    bindings_class = False
+                    if "rerun_bindings" in cls.canonical_path:
+                        bindings_class = True
+                        cls = bindings_pkg[cls.canonical_path[len("rerun_bindings.") :]]
+                        class_name = cls.canonical_path
                     show_class = class_name
                     for maybe_strip in ["archetypes.", "components.", "datatypes."]:
                         if class_name.startswith(maybe_strip):
                             stripped = class_name.replace(maybe_strip, "")
                             if stripped in rerun_pkg.classes:
                                 show_class = stripped
-                    index_file.write(f"[`rerun.{show_class}`][rerun.{class_name}] | {cls.docstring.lines[0]}\n")
+                    if bindings_class:
+                        show_class = class_name  # don't strip anything for bindings
+                    else:
+                        show_class = "rerun." + show_class
+                        class_name = "rerun." + class_name
+                    if cls.docstring is None:
+                        raise ValueError(f"No docstring for class {class_name}")
+                    index_file.write(f"[`{show_class}`][{class_name}] | {cls.docstring.lines[0]}\n")
 
         index_file.write("\n")
 

rerun_py/mkdocs.yml
@@ -37,6 +37,9 @@ plugins:
                 "!^_[^_]", # Hide things starting with a single underscore
                 "!as_component_batches", # Inherited from AsComponents
                 "!num_instances", # Inherited from AsComponents
+                "!__doc__", # griffe merges extension and stubs :(
+                "!__module__", # griffe merges extension and stubs :(
+                "!__weakref__", # griffe merges extension and stubs :(
               ]
             inherited_members: true
             members_order: source # The order of class members
@@ -48,6 +51,7 @@ plugins:
               - rerun_bindings
             annotations_path: brief
             signature_crossrefs: true
+            find_stubs_package: true
             extensions:
               - griffe_warnings_deprecated
 

rerun_py/rerun_bindings/rerun_bindings.pyi
@@ -1313,6 +1313,8 @@ class Entry:
         """
 
 class DatasetEntry(Entry):
+    """A dataset entry in the catalog."""
+
     @property
     def manifest_url(self) -> str:
         """Return the dataset manifest URL."""
@@ -1607,6 +1609,8 @@ class TableEntry(Entry):
         """Convert this table to a [`pyarrow.RecordBatchReader`][]."""
 
 class DataframeQueryView:
+    """View into a remote dataset acting as DataFusion table provider."""
+
     def filter_partition_id(self, partition_id: str, *args: Iterable[str]) -> Self:
         """Filter by one or more partition ids. All partition ids are included if not specified."""
 

rerun_py/src/catalog/catalog_client.rs
@@ -18,7 +18,11 @@ use crate::catalog::{
 use crate::utils::{get_tokio_runtime, wait_for_future};
 
 /// Client for a remote Rerun catalog server.
-#[pyclass(name = "CatalogClientInternal")] // NOLINT: skip pyclass_eq, non-trivial implementation
+#[pyclass(  // NOLINT: ignore[py-cls-eq] non-trivial implementation
+    name = "CatalogClientInternal",
+    module = "rerun_bindings.rerun_bindings"
+)]
+
 pub struct PyCatalogClientInternal {
     origin: re_uri::Origin,
 

rerun_py/src/catalog/dataframe_query.rs
@@ -25,7 +25,7 @@ use crate::catalog::{PyDatasetEntry, to_py_err};
 use crate::utils::{get_tokio_runtime, wait_for_future};
 
 /// View into a remote dataset acting as DataFusion table provider.
-#[pyclass(name = "DataframeQueryView")] // NOLINT: skip pyclass_eq, non-trivial implementation
+#[pyclass(name = "DataframeQueryView", module = "rerun_bindings.rerun_bindings")] // NOLINT: ignore[py-cls-eq] non-trivial implementation
 pub struct PyDataframeQueryView {
     dataset: Py<PyDatasetEntry>,
 

rerun_py/src/catalog/dataframe_rendering.rs
@@ -4,7 +4,7 @@ use pyo3::{Bound, PyAny, PyResult, pyclass, pymethods};
 
 use re_format_arrow::{RecordBatchFormatOpts, format_record_batch_opts};
 
-#[pyclass(eq, name = "RerunHtmlTable")]
+#[pyclass(eq, name = "RerunHtmlTable", module = "rerun_bindings.rerun_bindings")]
 #[derive(Clone, PartialEq, Eq)]
 pub struct PyRerunHtmlTable {
     max_width: Option<usize>,

rerun_py/src/catalog/datafusion_catalog.rs
@@ -10,7 +10,12 @@ use re_redap_client::ConnectionClient;
 
 use crate::utils::get_tokio_runtime;
 
-#[pyclass(frozen, eq, name = "DataFusionCatalog")]
+#[pyclass(
+    frozen,
+    eq,
+    name = "DataFusionCatalog",
+    module = "rerun_bindings.rerun_bindings"
+)]
 pub(crate) struct PyDataFusionCatalogProvider {
     pub provider: Arc<RedapCatalogProvider>,
 }

rerun_py/src/catalog/datafusion_table.rs
@@ -10,7 +10,12 @@ use tracing::instrument;
 use crate::catalog::PyCatalogClientInternal;
 use crate::utils::get_tokio_runtime;
 
-#[pyclass(frozen, name = "DataFusionTable")] // NOLINT: skip pyclass_eq, non-trivial implementation
+#[pyclass( // NOLINT: ignore[py-cls-eq] non-trivial implementation
+    frozen,
+    name = "DataFusionTable",
+    module = "rerun_bindings.rerun_bindings"
+)]
+
 pub struct PyDataFusionTable {
     pub provider: Arc<dyn TableProvider + Send>,
     pub name: String,

commit 42f2479854d591c21eea1eccb2c47a4a9b7ae39d

Fix re-exported symbols being listed with `rerun_bindings.rerun_bindings` prefix in the Python docs (#12448)
Fix an issue which I believe was introduced in #11650 where some symbols
where displayed with `rerun_bindings.rerun_bindings` prefix in the front
page tables of the Python docs:

<img width="773" height="530" alt="image"
src="https://github.com/user-attachments/assets/ff9a877d-5f3b-4cfa-86da-6edfdb62b26d"
/>

My fix appears to do the right thing based on `pixi run py-docs-serve`.

rerun_py/docs/gen_common_index.py
@@ -593,19 +593,18 @@ of Python, you can use the table below to make sure you choose the proper Rerun 
                     bindings_class = False
                     if "rerun_bindings" in cls.canonical_path:
                         bindings_class = True
+                        # Get the docstring from the bindings package, but keep the rerun display path
                         cls = bindings_pkg[cls.canonical_path[len("rerun_bindings.") :]]
-                        class_name = cls.canonical_path
+                        # Don't overwrite class_name - keep the rerun module path for display
                     show_class = class_name
                     for maybe_strip in ["archetypes.", "components.", "datatypes."]:
                         if class_name.startswith(maybe_strip):
                             stripped = class_name.replace(maybe_strip, "")
                             if stripped in rerun_pkg.classes:
                                 show_class = stripped
-                    if bindings_class:
-                        show_class = class_name  # don't strip anything for bindings
-                    else:
-                        show_class = "rerun." + show_class
-                        class_name = "rerun." + class_name
+                    # Always show as rerun.* in documentation, even for bindings classes
+                    show_class = "rerun." + show_class
+                    class_name = "rerun." + class_name
                     if cls.docstring is None:
                         raise ValueError(f"No docstring for class {class_name}")
                     index_file.write(f"[`{show_class}`][{class_name}] | {cls.docstring.lines[0]}\n")