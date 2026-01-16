1d941687ec8e787c98048c45479dc60cda2be6ad
Andreas Reich <r_andreas2@web.de>
2026-01-16 17:59:00 +0100

    re-enable datatype casting on time series

--- crates/viewer/re_view_time_series/src/point_visualizer_system.rs
+++ crates/viewer/re_view_time_series/src/point_visualizer_system.rs
@@ -16,6 +16,7 @@ use crate::series_query::{
     all_scalars_indices, allocate_plot_points, collect_colors, collect_radius_ui, collect_scalars,
     collect_series_name, collect_series_visibility, determine_num_series,
 };
+use crate::util::series_supported_datatypes;
 use crate::{
     LoadSeriesError, PlotPoint, PlotPointAttrs, PlotSeries, PlotSeriesKind, ScatterAttrs, util,
 };
@@ -34,27 +35,17 @@ impl IdentifiedViewSystem for SeriesPointsSystem {
 
 impl VisualizerSystem for SeriesPointsSystem {
     fn visualizer_query_info(&self) -> VisualizerQueryInfo {
-        let mut query_info = VisualizerQueryInfo::from_archetype::<archetypes::Scalars>();
-        query_info
-            .queried
-            .extend(archetypes::SeriesPoints::all_components().iter().cloned());
-
-        query_info.relevant_archetype = archetypes::SeriesPoints::name().into();
-
-        query_info
-
-        // TODO(RR-3318): Enable this.
-        // VisualizerQueryInfo {
-        //     relevant_archetype: archetypes::SeriesPoints::name().into(),
-        //     required: re_viewer_context::RequiredComponents::AnyPhysicalDatatype(
-        //         supported_datatypes().into_iter().collect(),
-        //     ),
-        //     queried: archetypes::Scalars::all_components()
-        //         .iter()
-        //         .chain(archetypes::SeriesPoints::all_components().iter())
-        //         .cloned()
-        //         .collect(),
-        // }
+        VisualizerQueryInfo {
+            relevant_archetype: archetypes::SeriesPoints::name().into(),
+            required: re_viewer_context::RequiredComponents::AnyPhysicalDatatype(
+                series_supported_datatypes().into_iter().collect(),
+            ),
+            queried: archetypes::Scalars::all_components()
+                .iter()
+                .chain(archetypes::SeriesPoints::all_components().iter())
+                .cloned()
+                .collect(),
+        }
     }
 
     fn execute(
