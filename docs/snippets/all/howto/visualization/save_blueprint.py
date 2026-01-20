c0a3f4ddc5a70dccdb4120eefe06691c96df4d5f
Antoine Beyeler <antoine@rerun.io>
2026-01-20 10:20:18 +0100

    remove log-blueprint-from-rust-or-cpp and integrate into build-a-blueprint-programmatically.md

--- docs/snippets/all/howto/visualization/save_blueprint.py
+++ docs/snippets/all/howto/visualization/save_blueprint.py
@@ -6,12 +6,9 @@ import rerun.blueprint as rrb
 
 path_to_rbl = sys.argv[1]
 
-rrb.Blueprint(
-    rrb.DataframeView(
-        origin="/",
-        query=rrb.archetypes.DataframeQuery(
-            timeline="log_time",
-            apply_latest_at=True,
-        ),
-    ),
-).save("rerun_example_dataframe_view_query", path_to_rbl)
+blueprint = rrb.Blueprint(
+    rrb.TimeSeriesView(name="AAPL", origin="/stocks/AAPL"),
+)
+
+# Save to a file
+blueprint.save("rerun_example_blueprint_stocks", path_to_rbl)
