66b7cb82bef85eb1abf57fe4c21a4ea20d5a1ec1
lucasmerlin <hi@lucasmerlin.me>
2026-01-30 17:41:45 +0100

    Mark temporarily broken links as speculative

--- crates/viewer/re_viewer/src/ui/memory_panel.rs
+++ crates/viewer/re_viewer/src/ui/memory_panel.rs
@@ -554,7 +554,7 @@ pub fn memory_tree_ui(
         ui.label("Memory flamegraph visualizing the memory usage tree.");
         ui.hyperlink_to(
             "Learn more",
-            "https://docs.rs/re_byte_size/latest/re_byte_size/trait.MemUsageTreeCapture.html?speculative_link",
+            "https://docs.rs/re_byte_size/latest/re_byte_size/trait.MemUsageTreeCapture.html?speculative-link",
         );
 
         #[expect(dead_code)]
