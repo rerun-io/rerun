fa285fdc8f9d7d921e776fa50d9bb281b6b273b5
Michael Grupp <michael@rerun.io>
2026-04-02 17:32:43 +0200

    Add tests for Foxglove lenses that didn't have tests yet
    Closes: https://linear.app/rerun/issue/RR-3444
    
    Source-Ref: 61521993a3b8b46de12bb34a01142ed0cb215d3f

--- crates/store/re_data_loader/src/loader_mcap/tests/foxglove/mod.rs
+++ crates/store/re_data_loader/src/loader_mcap/tests/foxglove/mod.rs
@@ -1,8 +1,11 @@
 //! Tests for Foxglove message lenses.
 
+mod test_camera_calibration;
+mod test_compressed_image;
 mod test_compressed_video;
 mod test_frame_transforms;
 mod test_location_fixes;
 mod test_log;
 mod test_point_cloud;
+mod test_poses_in_frame;
 mod test_raw_image;
