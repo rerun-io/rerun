1a8c6d94112e37c075c486452bdd26e72bbf606d
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-04-12 17:40:57 +0200

    Add support for 2D point clouds and 3D meshes based on the PLY header, and extend tests for path vs contents parsing, optional properties, ignored extra data, malformed payload diagnostics, and loader dispatch.
    Preparing to use ply-rs-bw 4.0.0

--- crates/store/re_sdk_types/tests/types/mesh3d.rs
+++ crates/store/re_sdk_types/tests/types/mesh3d.rs
@@ -72,3 +72,224 @@ fn roundtrip() {
     let deserialized = Mesh3D::from_arrow(serialized).unwrap();
     similar_asserts::assert_eq!(expected, deserialized);
 }
+
+#[test]
+fn ply_parses_mesh_with_normals_colors_and_triangulates_faces() {
+    let contents = br#"ply
+format ascii 1.0
+element vertex 4
+property float x
+property float y
+property float z
+property float nx
+property float ny
+property float nz
+property uchar red
+property uchar green
+property uchar blue
+property float temperature
+element face 2
+property list uchar int vertex_indices
+property uchar material_index
+end_header
+0 0 0 0 0 1 255 0 0 10
+1 0 0 0 0 1 0 255 0 11
+1 1 0 0 0 1 0 0 255 12
+0 1 0 0 0 1 255 255 0 13
+3 0 1 2 7
+4 0 2 3 1 8
+"#;
+
+    let parsed = Mesh3D::from_file_contents(contents).unwrap();
+    let expected = Mesh3D::new([
+        [0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+    ])
+    .with_vertex_normals([[0.0, 0.0, 1.0]; 4])
+    .with_vertex_colors([0xFF0000FF, 0x00FF00FF, 0x0000FFFF, 0xFFFF00FF])
+    .with_triangle_indices([[0, 1, 2], [0, 2, 3], [0, 3, 1]]);
+
+    similar_asserts::assert_eq!(parsed, expected);
+}
+
+#[test]
+fn ply_parses_xy_mesh_as_z0_mesh3d() {
+    let contents = br#"ply
+format ascii 1.0
+element vertex 4
+property float x
+property float y
+property uchar red
+property uchar green
+property uchar blue
+element face 1
+property list uchar int vertex_indices
+end_header
+0 0 255 0 0
+1 0 0 255 0
+1 1 0 0 255
+0 1 255 255 0
+4 0 1 2 3
+"#;
+
+    let parsed = Mesh3D::from_file_contents(contents).unwrap();
+    let expected = Mesh3D::new([
+        [0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+    ])
+    .with_vertex_colors([0xFF0000FF, 0x00FF00FF, 0x0000FFFF, 0xFFFF00FF])
+    .with_triangle_indices([[0, 1, 2], [0, 2, 3]]);
+
+    similar_asserts::assert_eq!(parsed, expected);
+}
+
+#[test]
+fn ply_parses_mesh_with_vertex_index_alias() {
+    let contents = br#"ply
+format ascii 1.0
+element vertex 3
+property float x
+property float y
+property float z
+element face 1
+property list uchar int vertex_index
+end_header
+0 0 0
+1 0 0
+0 1 0
+3 0 1 2
+"#;
+
+    let parsed = Mesh3D::from_file_contents(contents).unwrap();
+    let expected = Mesh3D::new([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+        .with_triangle_indices([[0, 1, 2]]);
+
+    similar_asserts::assert_eq!(parsed, expected);
+}
+
+#[test]
+fn ply_prefers_vertex_indices_over_vertex_index_when_both_are_present() {
+    let contents = br#"ply
+format ascii 1.0
+element vertex 3
+property float x
+property float y
+property float z
+element face 1
+property list uchar float vertex_index
+property list uchar int vertex_indices
+end_header
+0 0 0
+1 0 0
+0 1 0
+3 9 8 7 3 0 1 2
+"#;
+
+    let parsed = Mesh3D::from_file_contents(contents).unwrap();
+    let expected = Mesh3D::new([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
+        .with_triangle_indices([[0, 1, 2]]);
+
+    similar_asserts::assert_eq!(parsed, expected);
+}
+
+#[test]
+fn ply_rejects_zero_face_mesh() {
+    let contents = br#"ply
+format ascii 1.0
+element vertex 4
+property float x
+property float y
+property uchar red
+property uchar green
+property uchar blue
+element face 0
+property list uchar int vertex_indices
+end_header
+0 0 255 0 0
+1 0 0 255 0
+1 1 0 0 255
+0 1 255 255 0
+"#;
+
+    let err = Mesh3D::from_file_contents(contents).unwrap_err();
+
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidData);
+    assert!(
+        err.to_string()
+            .contains("at least one face with 3 or more vertex indices")
+    );
+}
+
+#[test]
+fn ply_rejects_zero_face_mesh_without_face_indices() {
+    let contents = br#"ply
+format ascii 1.0
+element vertex 4
+property float x
+property float y
+property uchar red
+property uchar green
+property uchar blue
+element face 0
+property int material_index
+end_header
+0 0 255 0 0
+1 0 0 255 0
+1 1 0 0 255
+0 1 255 255 0
+"#;
+
+    let err = Mesh3D::from_file_contents(contents).unwrap_err();
+
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidData);
+    assert!(
+        err.to_string()
+            .contains("require \"vertex_indices\" or \"vertex_index\"")
+    );
+}
+
+#[test]
+fn ply_rejects_supported_face_properties_with_unsupported_types() {
+    let contents = br#"ply
+format ascii 1.0
+element vertex 3
+property float x
+property float y
+property float z
+element face 1
+property list uchar float vertex_indices
+end_header
+0 0 0
+1 0 0
+0 1 0
+3 0 1 2
+"#;
+
+    let err = Mesh3D::from_file_contents(contents).unwrap_err();
+
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidData);
+    assert!(err.to_string().contains("PLY property 'vertex_indices'"));
+}
+
+#[test]
+fn ply_rejects_missing_face_element() {
+    let contents = br#"ply
+format ascii 1.0
+element vertex 3
+property float x
+property float y
+property float z
+end_header
+0 0 0
+1 0 0
+0 1 0
+"#;
+
+    let err = Mesh3D::from_file_contents(contents).unwrap_err();
+
+    assert_eq!(err.kind(), std::io::ErrorKind::InvalidData);
+}
