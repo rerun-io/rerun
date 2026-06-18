e068ce834d84ede9dfa420bcfcf2553f693c93ed
Andreas Reich <andreas@rerun.io>
2026-06-18 18:41:12 +0200

    Fix custom visualizer example showing a black viewer
    slipped through the cracks since wgsl is compiled at runtime only.
    
    ... so we're also fixing that here and add a test that validates all
    wgsl shaders as part of re_renderer's test suite!
    
    Source-Ref: 70f5f0ea869906cac65adea5ee21d880adede2cd

--- crates/viewer/re_renderer/src/renderer/point_cloud.rs
+++ crates/viewer/re_renderer/src/renderer/point_cloud.rs
@@ -565,20 +565,11 @@ impl Renderer for PointCloudRenderer {
             .shader_modules
             .get_or_create(ctx, &shader_module_desc);
 
-        // WORKAROUND for https://github.com/gfx-rs/naga/issues/1743
-        let mut shader_module_desc_vertex = shader_module_desc.clone();
-        shader_module_desc_vertex.extra_workaround_replacements =
-            vec![("fwidth(".to_owned(), "f32(".to_owned())];
-        let shader_module_vertex = ctx
-            .gpu_resources
-            .shader_modules
-            .get_or_create(ctx, &shader_module_desc_vertex);
-
         let render_pipeline_desc_color = RenderPipelineDesc {
             label: "PointCloudRenderer::render_pipeline_color".into(),
             pipeline_layout,
             vertex_entrypoint: "vs_main".into(),
-            vertex_handle: shader_module_vertex,
+            vertex_handle: shader_module,
             fragment_entrypoint: "fs_main".into(),
             fragment_handle: shader_module,
             vertex_buffers: smallvec![],
