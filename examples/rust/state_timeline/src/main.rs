84163e2f1c5c5bac67c31e95ea6bbce1138b3c28
Jochen Görtler <grtlr@users.noreply.github.com>
2026-07-07 13:10:38 +0200

    Clean up state view and graph lattice examples
    ### What
    
    Remove unnecessary code from the state view and graph lattice examples,
    to help with clarity.
    
    Source-Ref: 362886d6c0cc3d94d5ef804a52b50d1d58b28f78

--- examples/rust/state_timeline/src/main.rs
+++ examples/rust/state_timeline/src/main.rs
@@ -1,57 +1,9 @@
-//! Example app that opens a Rerun Viewer with the state timeline view showing test state data.
+//! Logs test state data for the state timeline view.
 
 use std::sync::Arc;
 
-use rerun::external::{re_crash_handler, re_grpc_server, re_log, re_viewer, tokio};
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let main_thread_token = rerun::MainThreadToken::i_promise_i_am_on_the_main_thread();
-
-    re_log::setup_logging();
-    re_crash_handler::install_crash_handlers(re_viewer::build_info());
-
-    // Listen for gRPC connections.
-    let (rx, _grpc_server_handle) = re_grpc_server::spawn_with_recv(
-        "0.0.0.0:9876".parse()?,
-        Default::default(),
-        re_grpc_server::shutdown::never(),
-    );
-
-    let startup_options = re_viewer::StartupOptions::default();
-    let app_env = re_viewer::AppEnvironment::Custom("State timeline view example".to_owned());
-
-    // Log some state change data via SDK so the state timeline view has something to show.
-    log_state_data()?;
-
-    re_viewer::run_native_app(
-        main_thread_token,
-        Box::new(move |cc| {
-            let mut app = re_viewer::App::new(
-                main_thread_token,
-                re_viewer::build_info(),
-                app_env,
-                startup_options,
-                cc,
-                None,
-                re_viewer::AsyncRuntimeHandle::from_current_tokio_runtime_or_wasmbindgen().expect(
-                    "Could not get a runtime handle from the current Tokio runtime or Wasm bindgen.",
-                ),
-            );
-            app.add_log_receiver(rx);
-            Ok(Box::new(app))
-        }),
-        None,
-    )?;
-
-    Ok(())
-}
-
-fn log_state_data() -> Result<(), Box<dyn std::error::Error>> {
-    let rec = rerun::RecordingStreamBuilder::new("rerun_example_state_timeline")
-        .default_enabled(true)
-        .connect_grpc()
-        .map_err(|err| format!("Failed to connect: {err}"))?;
+fn main() -> anyhow::Result<()> {
+    let rec = rerun::RecordingStreamBuilder::new("rerun_example_state_timeline").spawn()?;
 
     // An example of a static annotation context. An edge case for the state timeline view.
     rec.log_static(
@@ -156,5 +108,6 @@ fn log_state_data() -> Result<(), Box<dyn std::error::Error>> {
     )?;
 
     let _ = rec.flush_blocking();
+
     Ok(())
 }
