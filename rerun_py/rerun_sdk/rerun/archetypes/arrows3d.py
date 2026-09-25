0e2bc0f34e681f5102c36750c33e4c7220ac1831
Klaus Ondrag <klaus.ondrag@outlook.com>
2026-09-25 11:27:19 +0800

    Add codegen

--- rerun_py/rerun_sdk/rerun/archetypes/arrows3d.py
+++ rerun_py/rerun_sdk/rerun/archetypes/arrows3d.py
@@ -45,14 +45,15 @@ class Arrows3D(Arrows3DExt, Archetype, VisualizableArchetype):
     rr.init("rerun_example_arrow3d", spawn=True)
 
     lengths = np.log2(np.arange(0, 100) + 1)
-    angles = np.arange(start=0, stop=tau, step=tau * 0.01)
+    angles = tau * np.arange(100, dtype=np.float32) * 0.01
     origins = np.zeros((100, 3))
     vectors = np.column_stack([
         np.sin(angles) * lengths,
         np.zeros(100),
         np.cos(angles) * lengths,
     ])
-    colors = [[1.0 - c, c, 0.5, 0.5] for c in angles / tau]
+    greens = [round(c * 255.0) for c in angles / tau]
+    colors = [[255 - g, g, 128, 128] for g in greens]
 
     rr.log("arrows", rr.Arrows3D(origins=origins, vectors=vectors, colors=colors))
     ```
